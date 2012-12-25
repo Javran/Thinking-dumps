e4f3fb67ad2dc58560c63fb0c3c25db3451a972a
Javran Cheng <Javran.c@gmail.com>
2012-12-25 23:39:33 +0800

    tests about classes

---
+++ 7-lang-in-7-wk/ch2-ruby/day-2-try-tree.rb
@@ -0,0 +1,31 @@
+#!/bin/env ruby
+
+class Tree
+	attr_accessor :children, :node_name
+
+	def initialize(name, children=[])
+		@children = children
+		@node_name = name
+	end
+
+	def visit_all(&block)
+		visit &block
+		children.each { |c| c.visit_all &block }
+	end
+
+	def visit(&block)
+		block.call self
+	end
+end
+
+ruby_tree = Tree.new( 
+	"Ruby",
+	[ Tree.new( "Reia" ), Tree.new( "Sanka" ), ])
+
+puts "Visiting a node"
+ruby_tree.visit {|node| puts "Visit: #{node.node_name}"}
+puts
+
+puts "Visiting entire tree"
+ruby_tree.visit_all {|node| puts "Visit: #{node.node_name}"}
+puts
--- 7-lang-in-7-wk/ch2-ruby/day-2-try.rb
+++ 7-lang-in-7-wk/ch2-ruby/day-2-try.rb
@@ -45,3 +45,125 @@ while a.length > 0
 end
 
 put_line
+
+a = []
+a.push( [0,1,2] )
+a.push( [3,4,5] )
+a.push( [6,7,8] )
+puts a.to_s
+puts a[2][2]
+
+put_line
+
+numbers = {1=>"one", 2=>"two"}
+puts numbers
+puts numbers[1]
+
+stuff = {:array => [1,2,3], :string => "Hi, mom!"}
+puts stuff[:array]
+puts stuff[:string]
+
+put_line
+puts "Test symbol"
+str = "aaa"
+puts str.object_id
+puts "aaa".object_id
+
+puts :aaa.object_id
+puts :aaa.object_id
+
+put_line
+
+def tell_the_truth(options={})
+	if options[:profession] == :lawyer
+		'it could be believed that this is almost certainly not false.'
+	else
+		true
+	end
+end
+
+puts tell_the_truth
+puts tell_the_truth :profession => :lawyer
+
+put_line
+puts "Block!"
+
+3.times { puts "nice boat!" }
+3.times do
+	puts "nice BOAT!"
+end
+
+animals.each do
+	|animal|
+	puts "There are lots of #{animal}!"
+end
+
+put_line
+puts "Modify class Fixnum!"
+
+class Fixnum
+	def my_times
+		i = self
+		while i > 1
+			yield i, "Y", "!"
+			i -= 1
+		end
+	end
+end
+
+10.my_times { |n, y, ed| puts y + "o"*n + ed }
+
+puts "OK, What is <del>yoooo</del>yield?"
+i = 9
+def test_yield(range)
+	i = 0
+	while i < range 
+		yield i * 2
+		i += 1
+	end
+end
+# just like a different form of 'foreach'?
+test_yield(10) { |n| puts n }
+
+put_line
+puts "Call Block"
+
+def call_block(&block)
+	puts "block->"
+	block.call
+end
+
+call_block { puts "block code here." }
+
+def begin_transaction
+	puts "Transaction Begin ==\\"
+
+end
+
+def end_transaction
+	puts "Transaction End   ==/"
+end
+
+def emulate_transaction
+	begin_transaction
+	yield
+	end_transaction
+end
+
+emulate_transaction do
+	(0..4).each { |n| puts ">>> Operation ##{n}" }
+end
+
+put_line
+it = 4.class
+while it
+	puts it
+	it = it.superclass
+end
+
+put_line
+it = 4.class.class
+while it
+	puts it
+	it = it.superclass
+end
