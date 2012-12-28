f1a554605f90ee9edaea2e6e1d58be7d2e029b36
Javran Cheng <Javran.c@gmail.com>
2012-12-29 00:02:36 +0800

    day 2 exercise - find

---
+++ 7-lang-in-7-wk/ch2-ruby/day-2-find-code.rb
@@ -0,0 +1,56 @@
+#!/bin/env ruby
+
+puts "Task #1: Read Files"
+
+puts "Read myself!"
+
+puts "[1: without code block]"
+puts "*" * 10 + " BEGIN " + "*" * 10
+line_counter = 1
+file = File.new("day-2-find-code.rb", "r")
+while (line = file.gets)
+	puts "#{line_counter}: #{line}"
+	line_counter += 1
+end
+file.close
+puts "*" * 10 + " END " + "*" * 10
+
+puts "[2: with code block]"
+puts "*" * 10 + " BEGIN " + "*" * 10
+line_counter = 1
+File.open("day-2-find-code.rb", "r") do |f|
+	while (line = f.gets)
+		puts "#{line_counter}: #{line}"
+		line_counter += 1
+	end
+end
+puts "*" * 10 + " END " + "*" * 10
+
+puts "Task #2: Hash tables to arrays"
+
+h = { "a" => "AAA", "b" => "BBB", "c"=>"CCC" }
+puts "origin data:"
+puts "\t#{h}"
+
+puts "to (key,value) pairs:"
+puts "\t#{h.to_a.to_s}"
+puts "to values:"
+puts "\t#{h.values.to_s}"
+puts "to keys:"
+puts "\t#{h.keys.to_s}"
+
+puts "Task #3: Arrays to hash tables"
+
+arr = ["a", "AAA", "b", "BBB"]
+puts "origin data:"
+
+puts "\t#{arr}"
+puts "to hash table:"
+puts "\t#{Hash[*arr]}"
+
+arr = [["a", "AAA"], ["b", "BBB"]]
+puts "origin data:"
+puts "\t#{arr}"
+
+puts "to hash table:"
+puts "\t#{Hash[arr]}"
---
+++ 7-lang-in-7-wk/ch2-ruby/day-2-find.md
@@ -0,0 +1,26 @@
+- Read file with/without code blocks
+
+[Ruby: The Basics of Reading a File](http://www.abbeyworkshop.com/howto/ruby/rb-readfile/index.html)
+
+Task #1 in day-2-find-code.rb
+
+Advantage of code block: no sense, google it and pick some points that you agree.
+
+- Conversion between hash table and array
+
+hash table to array:
+
+it depends on how the 'conversion' is defined.
+hash table can convert to 
+1. an array of its keys
+2. an array of its values
+3. an array that contains all its (key,value) pairs
+
+Task #2 in day-2-find-code.rb
+
+and array can be converted to hash table:
+
+[convert an array to a hash in Ruby](http://stackoverflow.com/questions/39567/what-is-the-best-way-to-convert-an-array-to-a-hash-in-ruby)
+[Array to Hash Ruby](http://stackoverflow.com/questions/4028329/array-to-hash-ruby)
+
+Task #3 in day-2-find-code.rb
