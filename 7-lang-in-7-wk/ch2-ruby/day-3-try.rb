f3d017f8032dfe9e96a7002821e492fd92c29444
Javran Cheng <Javran.c@gmail.com>
2013-01-01 00:47:16 +0800

    day 3, at the end of ch 2.4.2

---
+++ 7-lang-in-7-wk/ch2-ruby/day-3-try.rb
@@ -0,0 +1,94 @@
+#!/bin/env ruby
+
+def putline
+	puts "=" * 20
+end
+
+class NilClass
+	def blank?
+		true
+	end
+end
+
+class String
+	def blank?
+		0 == self.size
+	end
+end
+
+puts "define methods for existing classes"
+
+["", "person", nil].each do |element|
+	puts element unless element.blank?
+end
+# prints "person" only
+
+["", "person", nil]
+	.select{|e| e.blank?}
+	.each{|e| puts "Empty, class=#{e.class}"} 
+# prints class names of "" and nil
+
+putline
+
+puts "Units"
+
+class Numeric
+	def inches
+		self
+	end
+	
+	def feet
+		self * 12.inches
+	end
+
+	def yards
+		self * 3.feet
+	end
+
+	def miles
+		self * 5280.feet
+	end
+
+	def back
+		self * -1
+	end
+
+	def forward
+		self
+	end
+end
+
+puts "10 miles, backward: #{10.miles.back}"
+puts "2 feet, forward: #{2.feet.forward}"
+
+putline
+
+puts "Roman numbers, using method_missing"
+
+class Roman
+	def self.method_missing name, *args
+		roman = name.to_s
+		roman.gsub!("IV", "IIII" )
+		roman.gsub!("IX", "VIIII")
+		roman.gsub!("XL", "XXXX" )
+		roman.gsub!("XC", "LXXXX")
+
+		[ 
+			["I",   1],
+			["V",   5],
+			["X",  10],
+			["L",  50],
+			["C", 100],
+		]
+			.collect{ | ch,w| roman.count(ch)*w }
+			.inject{  |acc,i| acc + i }
+	end
+end
+
+
+puts "X=   #{Roman.X}"
+puts "XC=  #{Roman.XC}"
+puts "XII= #{Roman.XII}"
+puts "X=   #{Roman.X}"
+
+putline
