5a4fb6e2c2207043e7a7f8b51455b674718495b1
Javran Cheng <Javran.c@gmail.com>
2013-01-18 01:43:32 +0800

    don't know how to get things right, waiting for answers

--- 7-lang-in-7-wk/ch4-prolog/day-1-try-friends-facts.pl
+++ 7-lang-in-7-wk/ch4-prolog/day-1-try-friends-facts.pl
@@ -4,6 +4,4 @@ likes(wallace, cheese).
 likes(grommit, cheese).
 likes(wendolene, sheep).
 
-q1 :- likes(wallace, cheese).
-
 friend(X, Y) :- \+(X = Y), likes(X, Z), likes(Y, Z).
--- 7-lang-in-7-wk/ch4-prolog/day-1-try-friends.pl
+++ 7-lang-in-7-wk/ch4-prolog/day-1-try-friends.pl
@@ -1,5 +1,3 @@
-% prolog query
-
 % how to run code from file?
 % please refer to:
 %     http://stackoverflow.com/questions/10059701/simple-prolog-issue-how-do-you-test-multiple-queries-against-your-prolog-databa
@@ -7,12 +5,15 @@
 %     http://stackoverflow.com/questions/2556253/prolog-newbie-question-making-a-procedure-to-print-hello-world
 % so, 'write' will output things and 'nl' prints a newlin 
 % 'halt' will quit the program
+% an useful link about some prolog tutorials:
+%     http://stackoverflow.com/questions/401635/good-beginners-material-on-prolog
 
+% currently 'write' print the argument as if it is a string
+%    (but strangely removed white space between "," and "c")
+% how make it work here?
+main :-
+	write(likes(wallace, cheese)),
+	halt.
 
-% TODO: why prints 'q1' while the result expected? 
-test :-
-	write(q1),
-	nl.
-
-:- initialization(['day-1-try-friends-db']).
-:- initialization(test).
+:- initialization(['day-1-try-friends-facts']).
+:- initialization(main).
