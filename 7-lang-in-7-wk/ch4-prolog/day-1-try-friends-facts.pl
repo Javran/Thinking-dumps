7ca4dc491b915bcf7c0c9b908cb19814d5587567
Javran Cheng <Javran.c@gmail.com>
2013-01-17 23:56:42 +0800

    attempts to run prolog script

--- 7-lang-in-7-wk/ch4-prolog/day-1-try-friends-facts.pl
+++ 7-lang-in-7-wk/ch4-prolog/day-1-try-friends-facts.pl
@@ -4,4 +4,6 @@ likes(wallace, cheese).
 likes(grommit, cheese).
 likes(wendolene, sheep).
 
+q1 :- likes(wallace, cheese).
+
 friend(X, Y) :- \+(X = Y), likes(X, Z), likes(Y, Z).
--- 7-lang-in-7-wk/ch4-prolog/day-1-try-friends.pl
+++ 7-lang-in-7-wk/ch4-prolog/day-1-try-friends.pl
@@ -1,6 +1,18 @@
 % prolog query
-test :-
-	likes(wallance, sheep).
 
-:- initialization([day-1-try-friends-db]).
+% how to run code from file?
+% please refer to:
+%     http://stackoverflow.com/questions/10059701/simple-prolog-issue-how-do-you-test-multiple-queries-against-your-prolog-databa
+% how to print things:
+%     http://stackoverflow.com/questions/2556253/prolog-newbie-question-making-a-procedure-to-print-hello-world
+% so, 'write' will output things and 'nl' prints a newlin 
+% 'halt' will quit the program
+
+
+% TODO: why prints 'q1' while the result expected? 
+test :-
+	write(q1),
+	nl.
+
+:- initialization(['day-1-try-friends-db']).
 :- initialization(test).
