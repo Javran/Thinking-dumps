3e93b136e768724e83bbbbf675f2b9011e624046
Javran Cheng <Javran.c@gmail.com>
2013-01-20 02:22:10 +0800

    unification output attempts

--- 7-lang-in-7-wk/ch4-prolog/common.pl
+++ 7-lang-in-7-wk/ch4-prolog/common.pl
@@ -8,9 +8,20 @@ writeln(T) :- write(T), nl.
 % query all tagged 'query' and quit
 queryAll :-
 	forall(query(Q), (Q ->
-		writeln('yes':Q) ;
-		writeln('no ':Q))).
+		format('yes:~w~n',[Q]) ;
+		format('no :~w~n',[Q]))).
+
+queryAllU :-
+	forall(queryU(Q), 
+        ( read_term_from_atom(Q, T, [variable_names(L)]),
+          ( T -> writeln('yes':L) ; writeln('no ':Q) )
+        )).
+
+errorHandler :- catch(run, X, error_process(X)).
+
+error_process(existence_error) :- write('err:'),nl,errorHandler.
 
 run :-
 	queryAll,
+	queryAllU,
 	halt.
