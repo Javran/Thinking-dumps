5c9ab8e4b62f064d590c5e11f933242dd5856d3f
Javran Cheng <Javran.c@gmail.com>
2013-01-19 23:39:00 +0800

    4.3.1 done

---
+++ 7-lang-in-7-wk/ch4-prolog/day-2-try-family.pl
@@ -0,0 +1,32 @@
+% init
+:- initialization(['common.pl']).
+:- initialization(run).
+
+% facts
+
+% fatherOf(X,Y): X is father of Y
+fatherOf(zeb,         john_boy_sr).
+fatherOf(john_boy_sr, john_boy_jr).
+
+ancestorOf(X, Y) :-
+	fatherOf(X, Y).
+
+% if X is Y's father, and Z is Y's ancestor
+%     then Z is X's ancestor
+ancestorOf(X, Y) :-
+	fatherOf(X, Z), ancestorOf(Z, Y).
+
+% queries
+
+query(ancestorOf(john_boy_sr, john_boy_jr)).
+% yes
+query(ancestorOf(john_boy_jr, john_boy_sr)).
+% no
+query(ancestorOf(zeb,         john_boy_jr)).
+% yes
+
+% zeb is the ancestor of whom?
+query(findall(Who, ancestorOf(zeb, Who), _)).
+
+% find all ancestors of john_boy_jr 
+query(findall(Who, ancestorOf(Who, john_boy_jr), _)).
