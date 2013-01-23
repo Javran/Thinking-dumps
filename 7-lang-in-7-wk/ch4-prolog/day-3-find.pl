6909dc60736b737f56f3fd3f18184eae7e5020eb
Javran Cheng <Javran.c@gmail.com>
2013-01-24 01:10:07 +0800

    day 3 find -> done

--- 7-lang-in-7-wk/ch4-prolog/day-3-find.pl
+++ 7-lang-in-7-wk/ch4-prolog/day-3-find.pl
@@ -5,9 +5,22 @@
 % facts
 
 % Task #1: find predication that can read/write variables
+read_and_write :-
+	% use '.' to terminate your input
+	read(X),
+	format('user input: ~w~n',[X]).
+
+% Task #2: print things only when query succeeds
+print_if_succeed(X) :-
+	X,
+	format('query ok: ~w~n',[X]).
+
+is_ok(ok).
 
 % queries
-query( format('thats it!~n',[]) ).
+query( read_and_write ).
+query( print_if_succeed(is_ok(ok)) ).
+query( print_if_succeed(is_ok(no)) ).
 
 % dummy query
 query( true ).
