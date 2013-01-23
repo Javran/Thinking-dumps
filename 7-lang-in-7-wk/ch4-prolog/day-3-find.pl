071ef74b9a3b970528db923deeb86ea6cddafe7c
Javran Cheng <Javran.c@gmail.com>
2013-01-23 23:59:07 +0800

    start day 3 exercise

---
+++ 7-lang-in-7-wk/ch4-prolog/day-3-find.pl
@@ -0,0 +1,16 @@
+% init
+:- initialization(['common.pl']).
+:- initialization(run).
+
+% facts
+
+% Task #1: find predication that can read/write variables
+
+% queries
+query( format('thats it!~n',[]) ).
+
+% dummy query
+query( true ).
+
+% dummy query
+query_u( '_=_.' ).
