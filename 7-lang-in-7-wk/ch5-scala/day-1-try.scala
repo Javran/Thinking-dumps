bd75f82e8726ae4552d27086b797e95a49f2dc77
Javran Cheng <Javran.c@gmail.com>
2013-01-26 16:09:26 +0800

    5.2.1

--- 7-lang-in-7-wk/ch5-scala/day-1-try.scala
+++ 7-lang-in-7-wk/ch5-scala/day-1-try.scala
@@ -21,5 +21,43 @@ println(5 + 4 * 3)
 println(5.+(4.*(3)))
 // 17.0
 
-println((5).+(4.*(3)))
+println((5).+((4).*(3)))
 // 17
+
+println((5.).+(4.*(3)))
+// 17.0
+
+println( "abc".size )
+// 3
+
+println( "abc" + 4 )
+// abc4
+
+println( 4 + "abc" )
+// 4abc
+
+println( 4 + "1.0" )
+// 41.0 <- note that this is a string
+
+println( "abc" * 4 )
+// it will produce 4 "abc"s
+
+//println( 4 * "abc" )
+// the line above will cause an error
+//     which I guess is a compilation error
+//     because try-catch statement 
+//     cannot catch this kind of error
+
+// FYI:
+// the block comment below shows one way to do error handling
+/*
+try {
+	throw new Exception
+} catch {
+	case ex: Exception => {
+		println("An exception occurred.")
+	}
+}
+*/
+
+
