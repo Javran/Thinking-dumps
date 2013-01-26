4cf85384f15215614c84e3dfd16e272b833fd3b6
Javran Cheng <Javran.c@gmail.com>
2013-01-26 15:56:43 +0800

    found way to get scala script work

--- 7-lang-in-7-wk/ch5-scala/day-1-try.scala
+++ 7-lang-in-7-wk/ch5-scala/day-1-try.scala
@@ -1,4 +1,25 @@
-#!/usr/bin/env scala
+#!/usr/bin/env scala "$0" "$@"
 !#
 
+// if you encounter some problem like:
+//     "Unable to establish connection to compilation daemon"
+// please make sure to set your ip-hostname map properly in /etc/hosts
+// related link:
+//     http://stackoverflow.com/questions/7714696/scala-hello-world-script-does-not-work
 println("nice boat!")
+// nice boat!
+
+println(1+1)
+// 2
+
+println((1).+(1))
+// 2
+
+println(5 + 4 * 3)
+// 17
+
+println(5.+(4.*(3)))
+// 17.0
+
+println((5).+(4.*(3)))
+// 17
