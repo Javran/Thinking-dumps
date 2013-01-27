564876aaa20a9d581152b6b7dfaf29cb2588ebb2
Javran Cheng <Javran.c@gmail.com>
2013-01-28 01:35:08 +0800

    start working on "do"

---
+++ 7-lang-in-7-wk/ch5-scala/day-1-do.scala
@@ -0,0 +1,11 @@
+#!/usr/bin/env scala
+!#
+
+// Task #1: Tic-Tac-Toe Game
+class TicTacToe {
+
+	// 1. newBoard => create a new game board
+	// 2. move => player(X/O) given, apply move
+	// 3. isPlaying => is playing or is done(Tie/X Win/O Win)
+
+}
