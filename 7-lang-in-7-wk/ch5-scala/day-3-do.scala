e039211af2d79d61a1eff1a5e588dc51f1d0e948
Javran Cheng <Javran.c@gmail.com>
2013-02-04 00:44:56 +0800

    day 3 -> done

--- 7-lang-in-7-wk/ch5-scala/day-3-do.scala
+++ 7-lang-in-7-wk/ch5-scala/day-3-do.scala
@@ -10,57 +10,111 @@
 	After getting tagsoup ready, please modify TAGSOUP_LIB accordingly
 */
 
+// Task #1: count the number of links
+// Task #2: make sizer follow links and compute size for all pages it links to
+
 import scala.io._
 import scala.actors._
+import scala.xml.parsing.NoBindingFactoryAdapter;
 import Actor._
 
-import scala.xml.XML
+import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl;
+import org.xml.sax.InputSource;
+import java.io.ByteArrayInputStream;
+
+// judge if we should follow the URL or not
+def shouldFollowURL(url:String) = {
+	( ! url.contains( '#' ) ) && /* no tags */
+	( url != "/" ) && /* no self-link */
+	( url.startsWith( "/" ) ) /* relative links only */
+}
+
+class PageInformation(
+	val url: String,
+	val linkCount: Int,
+	val followedLinkCount: Int,
+	val pageSize: Int,
+	val followedLinkSize: Int,
+	val error: Boolean) {
+
+	def print() = {
+		println( "URL: " + url )
+
+		if (error) {
+			println( "<No information avaliable>" )
+		} else {
+
+			println( "Link count: " + linkCount )
+			println( "Page size: " + pageSize )
+			println( "Link count (followed): " + followedLinkCount )
+			println( "Page size (followed): " + followedLinkSize )
+			println( "Total downloaded size: " + (pageSize + followedLinkSize) )
+		}
+
+		println()
+	}
+
+}
 
 object PageLoader {
-	def getPageSize(url: String) = {
+	def getPageInformation(url: String): PageInformation = {
 		try {
 			val content = Source
 				.fromURL(url)
 				.mkString
 
-			val parserFactory = new org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl()
-			val parser = parserFactory.newSAXParser()
-			val source = new org.xml.sax.InputSource(new java.io.ByteArrayInputStream(content.getBytes()))
-			val adapter = new scala.xml.parsing.NoBindingFactoryAdapter
-			val data = adapter.loadXML(source, parser)
+			// parse HTML page using tagsoup
+			val adapter = new NoBindingFactoryAdapter()
 
-			println( "links: %d".format( (data \\ "a" \\ "@href").length ) )
+			val data = adapter.loadXML(
+				/* source */
+				new InputSource(new ByteArrayInputStream(content.getBytes())),
+				/* parser */
+				new SAXFactoryImpl().newSAXParser())
 
-			content.length
+			val links = ( data \\ "a" \\ "@href" ).map( l => l.toString() ) 
+
+			val followedLinkSize = links
+				.filter( u => shouldFollowURL( u ) ) /* filter links first */
+				.map( u => { /* download all links */
+					try {
+						Source.fromURL( url + u ).mkString.length
+					} catch {
+						case _ => -1
+					}	
+				})
+				.filter( x => x >= 0 )
+
+			val subSizeCount = followedLinkSize.foldLeft( 0 )( (acc, i) => acc + i )
+
+			new PageInformation(
+				url, 
+				links.length, 			// linkCount
+				followedLinkSize.length,  	// followedLinkCount
+				content.length, 		// pageSize
+				subSizeCount, 			// followedLinkSize
+				false)
+
 		} catch {
-			case e:Exception => {
-				println( "Error occurred:" + e.getMessage() )
-				-1
-			}
+			case _ => new PageInformation(url, 0, 0, 0, 0, true)
 		}
 	}
 }
 
 val urls = List(
-	"http://www.example.com/",
-	"http://www.w3schools.com/",
-	"http://www.gnu.org/")
+	"http://www.google.com/",
+	"http://www.amazon.com/",
+	"http://www.gnu.org/",
+	"http://www.bing.com/")
 
-def getPageSizeConcurrently() = {
-	val caller = self
+val caller = self
 
-	urls.foreach( u => {
-		actor { caller ! (u, PageLoader.getPageSize(u)) }
-	} )
+urls.foreach( u => {
+	actor { caller ! PageLoader.getPageInformation(u) }
+} )
 
-	(0 until urls.size).foreach( _ => {
-		receive {
-			case (url, size) =>
-			{
-				println( "Url: \"%s\"\nSize: %d".format(url, size) )
-			}
-		}
-	})
-}
-
-getPageSizeConcurrently
+(0 until urls.size).foreach( _ => {
+	receive {
+		case pageInfo:PageInformation => pageInfo.print()
+	}
+})
