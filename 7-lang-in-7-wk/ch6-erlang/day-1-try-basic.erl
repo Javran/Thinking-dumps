f42b1684d281d34fc2901eb8cb47571365edb095
Javran Cheng <Javran.c@gmail.com>
2013-02-04 21:50:18 +0800

    create template

--- 7-lang-in-7-wk/ch6-erlang/day-1-try-basic.erl
+++
@@ -1,6 +0,0 @@
--module(basic).
--export([mirror/1]).
-
-mirror(Anything) -> Anything.
-
-main(_) -> io:format("Result: ~w~n", [mirror(smilling_mug)]).
---
+++ 7-lang-in-7-wk/ch6-erlang/template.erl
@@ -0,0 +1,4 @@
+#!/usr/bin/env escript
+
+main(_) -> 
+	io:format("print things like this", []).
