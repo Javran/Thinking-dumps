1bd1cbe951adfdb49f2fe04c5ddbf9b06d055403
Javran Cheng <Javran.c@gmail.com>
2013-02-08 23:33:53 +0800

    task #3 roughtly done

--- 7-lang-in-7-wk/ch6-erlang/day-2-do.erl
+++ 7-lang-in-7-wk/ch6-erlang/day-2-do.erl
@@ -60,6 +60,82 @@ task2() ->
 		lists:map(fun({_,T}) -> T end, Target))),
 	ok.
 
+% Board is a list of list, for example: 
+% Board =
+%     [
+%         [x,o,o],
+%         [e,x,e],
+%         [x,e,o]]
+%     where
+%         x for player X,
+%         o for player O,
+%         e for empty
+judge_tictactoe(Board) ->
+	% define what is line
+	Lines = [
+		% rows
+		[{1,1},{1,2},{1,3}],	
+		[{2,1},{2,2},{2,3}],	
+		[{3,1},{3,2},{3,3}],	
+		% cols
+		[{1,1},{2,1},{3,1}],	
+		[{1,2},{2,2},{3,2}],	
+		[{1,3},{2,3},{3,3}],	
+		% diags
+		[{1,1},{2,2},{3,3}],	
+		[{1,3},{2,2},{3,1}]	
+	],
+	% if a line is given, turn coords into values(i.e. x/o/e)
+	LineToValues =
+		fun(Line) ->
+			lists:map(
+				fun({X,Y}) -> lists:nth(Y, lists:nth(X,Board)) end,
+				Line)
+		end,
+
+	% if a list of values is given(i.e. [x,x,x]), then returns:
+	% if all elements are x -> x
+	% if all elements are o -> o
+	% if none of the element is e -> full
+	% otherwise -> mix
+	ValuesToJudgment =
+		fun(Values) ->
+			case Values of
+				[x,x,x] -> x;
+				[o,o,o] -> o;
+				_ -> 
+					IsFull = lists:all(fun(X) -> X /= e end, Values),
+					case IsFull of
+						true -> full;
+						false -> mix
+					end
+			end
+		end,
+
+	% get judgment on all lines
+	Results = lists:map(
+		fun(L) -> ValuesToJudgment(LineToValues(L)) end,
+		Lines),
+	XWin = lists:any(fun(X) -> X == x end, Results),
+	OWin = lists:any(fun(X) -> X == o end, Results),
+	Cat  = lists:all(fun(X) -> X == full end, Results),
+
+	if
+		% both x and o owns a line, it's impossible
+		XWin and OWin -> impossible;
+		XWin -> x;
+		OWin -> o;
+		Cat -> cat;
+		true -> no_winner
+	end.
+
+
 task3() ->
-	% TODO
+	% TODO pretty print
+	u:wl(
+	judge_tictactoe([ 
+		[o,x,o],
+		[x,o,x],
+		[x,o,x]])),
+	
 	ok.
