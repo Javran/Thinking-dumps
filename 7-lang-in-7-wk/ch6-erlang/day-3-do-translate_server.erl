eeb1c01df15e383535ec7f3b9bdd52c0231c6634
Javran Cheng <Javran.c@gmail.com>
2013-02-12 16:20:56 +0800

    translate_supervisor -> done

--- 7-lang-in-7-wk/ch6-erlang/day-3-do-translate_server.erl
+++ 7-lang-in-7-wk/ch6-erlang/day-3-do-translate_server.erl
@@ -4,7 +4,20 @@ main(_) ->
 	c:c(u),
 	c:c("./modules/day-3/do/translate_server/translator"),
 	c:c("./modules/day-3/do/translate_server/translate_service"),
+	c:c("./modules/day-3/do/translate_server/translate_supervisor"),
 
-	{ok, S} = translate_service:start_link(),
+	{ok, S} = translate_supervisor:start_link(),
+	Service = translate_supervisor:get_service(S),
+	
+	translate_service:crash(Service),
+
+	timer:sleep(100),
+
+	u:wl(translate_service:translate(translate_supervisor:get_service(S), "casa")),
+
+	translate_service:stop( translate_supervisor:get_service(S) ),
+
+	timer:sleep(100),
+	
 
 	ok.
--- 7-lang-in-7-wk/ch6-erlang/modules/day-3/do/translate_server/translate_supervisor.erl
+++ 7-lang-in-7-wk/ch6-erlang/modules/day-3/do/translate_server/translate_supervisor.erl
@@ -1,14 +1,29 @@
--module(translate_service_monitor_otp).
+-module(translate_supervisor).
 -behavior(supervisor).
 -export([
+	init/1
+]).
+
+-export([
 	start_link/0,
-	init/1]).
+	get_service/1
+]).
 
 start_link() ->
-	supervisor:start_link({local, monitor}, ?MODULE, []).
+	supervisor:start_link(?MODULE, []).
+
+get_service(SupRef) ->
+	% find the child by id 'translate_service_id'
+	{_, Pid, _, _} = hd(
+		lists:filter(
+			fun({Id, _, _, _}) -> 
+				Id == translate_service_id
+			end,
+			supervisor:which_children(SupRef))
+	),
+	Pid.
 
 init(_Args) ->
 	{ok, {{one_for_one, 10, 1},
-		[{translator, {translate_service_otp, start_link, [translator]},
-			permanent, brutal_kill, worker, [translate_service_otp]}]}}.
-
+		[{translate_service_id, {translate_service, start_link, []},
+			permanent, brutal_kill, worker, [translate_service]}]}}.
