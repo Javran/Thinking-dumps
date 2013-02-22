8abc5f118a47f82b4c4659ebf075895ec82b9e41
Javran Cheng <Javran.c@gmail.com>
2013-02-22 23:05:36 +0800

    add dependency for clojure-contrib

---
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/do.clj
@@ -0,0 +1,9 @@
+(ns clojure-codes.day-3.do.do
+  (:require 
+    [clojure-codes.utils :as utils]))
+
+(defn -main [& args]
+  (utils/eval-and-println
+    "codes here"
+  )
+)
---
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/find/find.clj
@@ -0,0 +1,10 @@
+(ns clojure-codes.day-3.find.find
+  (:require 
+    [clojure-codes.utils :as utils]
+    [clojure.contrib.seq-utils]))
+
+(defn -main [& args]
+  (utils/eval-and-println
+    "codes here"
+  )
+)
