c80ea0b4b3c966bf44e2c304d0f5be13c894613c
Javran Cheng <Javran.c@gmail.com>
2013-02-24 01:54:39 +0800

    seems not working

--- 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
@@ -25,13 +25,16 @@
   (defn request-cut-hair
     [done_count]
     (println :w @waiting-chair-avaliable)
+    (println :o @open-flag)
+
     (loop [d_c done_count]
-      (if (and @open-flag (< @waiting-chair-avaliable 3))
+      (if 
+        (and @open-flag (< @waiting-chair-avaliable 3))
         ; we have work to do now
         (do
           (println :working d_c)
           (dosync (alter waiting-chair-avaliable inc))  
-          (Thread/sleep 20)
+          (Thread/sleep 2)
           (recur (inc d_c))
         )
         ; nothing to do, just keep sleeping and return done_count  
@@ -40,45 +43,44 @@
           d_c)
       )))
 
-  (defn fuckyou
-    [done_count]
-    (+ done_count 10))
-  ; create an agent that keeps hair-cutting service alive for 10 sec
   (start-service)
+  (def service
+    (send 
+      (agent ())
+      (fn
+        [_]
+        (Thread/sleep (* 1 1000))
+        (stop-service))))
 
-  (def service (agent ()))
-  (send 
-    service
-    (fn
-      [_]
-      (Thread/sleep (* 1 1000))
-      (stop-service)))
+  (def filler
+    (send
+      (agent ())
+      (fn
+        [_]
+        (loop []
+          (if @open-flag
+            (do
+              (Thread/sleep (+ 10 (rand-int 21)))
+              (dosync
+                (if (> @waiting-chair-avaliable 0)
+                  (alter waiting-chair-avaliable dec)))
+              ; ask barber to do hair cutting
+              ;     in case barber is sleeping
+              (println @waiting-chair-avaliable)
 
-  (send 
-    (agent ())
-    (fn
-      [_]
-      (loop []
-        (if @open-flag
-          (do
-            (Thread/sleep (+ 10 (rand-int 21)))
-            (dosync
-              (if (> @waiting-chair-avaliable 0)
-                (alter waiting-chair-avaliable dec)))
-            ; ask barber to do hair cutting
-            ;     in case barber is sleeping
-            ; (send barber request-cut-hair)
-            (send barber #(+ 10 %))
+              (send barber request-cut-hair)
 
-            ; 10 - 30 
-            ; -> 10 + random [ 0 - 20 ] (in)
-            ; -> 10 + random [ 0 - 21 ] (ex)
-            (if @open-flag 
-              (recur))
-            )))))
+              ; 10 - 30 
+              ; -> 10 + random [ 0 - 20 ] (in)
+              ; -> 10 + random [ 0 - 21 ] (ex)
+              (if @open-flag 
+                (recur)
+                ())
+              )
+            ())))))
 
+  (Thread/sleep 100)
   (await service)
-  (await barber)
 
   (println @barber)
 
