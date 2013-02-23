bbdd68b5a6deb55354ec88b185e0bd3a12c9b05f
Javran Cheng <Javran.c@gmail.com>
2013-02-23 23:19:38 +0800

    day 3 exercise 1 -> done

--- 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/do.clj
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/do.clj
@@ -2,8 +2,33 @@
   (:require 
     [clojure-codes.utils :as utils]))
 
+; Task #1: use ref to create a group of accounts,
+;     implement `credit` and `debit` for these accounts
+
+(defn credit 
+  [account-list who amount]
+  (dosync
+    (alter account-list 
+           assoc who (+ amount (nth @account-list who)))))
+
+(defn debit
+  [account-list who amount]
+  (credit account-list who (- amount)))
+
 (defn -main [& args]
   (utils/eval-and-println
-    "codes here"
+    ; create accounts
+    (def account-list (ref [0 0 0]))
+    @account-list
+
+    (credit account-list 0 10)
+    (debit account-list 0 1)
+    (debit account-list 0 2)
+    (debit account-list 1 10)
+    (debit account-list 0 3)
+    (credit account-list 2 20)
+    (debit account-list 0 4)
+    (debit account-list 2 20)
+    (credit account-list 1 10)
   )
 )
