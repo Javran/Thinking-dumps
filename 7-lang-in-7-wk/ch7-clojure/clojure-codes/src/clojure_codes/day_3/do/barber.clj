e3021cbdc6b8334be6022b843c6139b496d3a5d7
Javran Cheng <Javran.c@gmail.com>
2013-02-24 14:04:04 +0800

    day 3 exercise 2 -> almost done

--- 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
@@ -2,86 +2,70 @@
   (:require 
     [clojure-codes.utils :as utils]))
 
-; Task #2: solve sleeping barber problem
-;     * 3 chairs in waiting room
-;     * 1 barber and 1 chair in barber shop
-;     * 20 ms to cut hair
-;     * consumer arriving interval 10 - 30 ms
-;     * the barber shop will be open for 10 sec
-
 (defn -main [& args]
   (def open-flag (ref false))
-  (defn start-service
-    []
-    (dosync (ref-set open-flag true)))
-  (defn stop-service
-    []
-    (dosync (ref-set open-flag false)))
 
-  (def waiting-chair-avaliable (ref 3))
+  (defn start-service [] (dosync (ref-set open-flag true)))
+  (defn stop-service [] (dosync (ref-set open-flag false)))
 
-  (def barber (agent 0))
+  (def wait-chairs-avaliable (ref 3))
 
-  (defn request-cut-hair
-    [done_count]
-    (println :w @waiting-chair-avaliable)
-    (println :o @open-flag)
+  (def barber
+    (agent 0))
 
-    (loop [d_c done_count]
-      (if 
-        (and @open-flag (< @waiting-chair-avaliable 3))
-        ; we have work to do now
-        (do
-          (println :working d_c)
-          (dosync (alter waiting-chair-avaliable inc))  
-          (Thread/sleep 2)
-          (recur (inc d_c))
+  (defn request-hair-cut
+    [cut_count]
+    (loop [current_cut_count cut_count]
+      (if @open-flag
+        ; if true
+        (if (dosync
+              (ensure wait-chairs-avaliable)
+              (if (< @wait-chairs-avaliable 3)
+                (do
+                  (alter wait-chairs-avaliable inc)
+                  true)
+                false))
+          ; if true
+          (do
+            (Thread/sleep 20)
+            (recur (inc current_cut_count))
+          )
+          ; if false
+          (do
+            current_cut_count)
         )
-        ; nothing to do, just keep sleeping and return done_count  
-        (do
-          (println :sleep)
-          d_c)
-      )))
+        ; if false
+        current_cut_count)))
+
 
   (start-service)
   (def service
-    (send 
-      (agent ())
-      (fn
-        [_]
-        (Thread/sleep (* 1 1000))
-        (stop-service))))
+    (future 
+      ; keep service alive for a while
+      (Thread/sleep 10000)
+      (stop-service)))
 
   (def filler
-    (send
-      (agent ())
-      (fn
-        [_]
-        (loop []
-          (if @open-flag
-            (do
-              (Thread/sleep (+ 10 (rand-int 21)))
-              (dosync
-                (if (> @waiting-chair-avaliable 0)
-                  (alter waiting-chair-avaliable dec)))
-              ; ask barber to do hair cutting
-              ;     in case barber is sleeping
-              (println @waiting-chair-avaliable)
+    ; put consumers to waiting chairs if possible
+    (future
+      (loop []
+        (if @open-flag
+          (do
+            (Thread/sleep (+ 10 (rand-int 21)))
+            (dosync
+              (ensure wait-chairs-avaliable)
+              (if (> @wait-chairs-avaliable 0)
+                (alter wait-chairs-avaliable dec)))
+            (send barber request-hair-cut)
+            (recur))))))
 
-              (send barber request-cut-hair)
-
-              ; 10 - 30 
-              ; -> 10 + random [ 0 - 20 ] (in)
-              ; -> 10 + random [ 0 - 21 ] (ex)
-              (if @open-flag 
-                (recur)
-                ())
-              )
-            ())))))
-
-  (Thread/sleep 100)
-  (await service)
+  @service
+  (await barber)
 
   (println @barber)
-
+  ; the result will be no more than 500
+  ;     because the barber will always spend 20ms to cut hairs
+  ; even assume full-speed of consumer's arrival
+  ;     i.e. consumers' arrival interval is 10ms
+  ; this program produces `497` as output
 )
