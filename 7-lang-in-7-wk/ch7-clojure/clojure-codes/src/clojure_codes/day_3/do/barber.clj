509ade50236966ddadb3c990cc59a4131e4a2463
Javran Cheng <Javran.c@gmail.com>
2013-02-24 14:20:50 +0800

    more statistic info

--- 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
+++ 7-lang-in-7-wk/ch7-clojure/clojure-codes/src/clojure_codes/day_3/do/barber.clj
@@ -2,6 +2,8 @@
   (:require 
     [clojure-codes.utils :as utils]))
 
+; TODO: description here
+
 (defn -main [& args]
   (def open-flag (ref false))
 
@@ -11,11 +13,11 @@
   (def wait-chairs-avaliable (ref 3))
 
   (def barber
-    (agent 0))
+    (agent [0,0]))
 
   (defn request-hair-cut
-    [cut_count]
-    (loop [current_cut_count cut_count]
+    [[cut_count, sleep_count]]
+    (loop [current_cut_count cut_count current_sleep_count sleep_count]
       (if @open-flag
         ; if true
         (if (dosync
@@ -28,14 +30,14 @@
           ; if true
           (do
             (Thread/sleep 20)
-            (recur (inc current_cut_count))
+            (recur (inc current_cut_count) sleep_count)
           )
           ; if false
           (do
-            current_cut_count)
+            [current_cut_count (inc sleep_count)] )
         )
         ; if false
-        current_cut_count)))
+        [current_cut_count sleep_count])))
 
 
   (start-service)
@@ -48,24 +50,43 @@
   (def filler
     ; put consumers to waiting chairs if possible
     (future
-      (loop []
+      (loop [consumer_lost 0]
         (if @open-flag
+          ; if true
           (do
             (Thread/sleep (+ 10 (rand-int 21)))
-            (dosync
-              (ensure wait-chairs-avaliable)
-              (if (> @wait-chairs-avaliable 0)
-                (alter wait-chairs-avaliable dec)))
-            (send barber request-hair-cut)
-            (recur))))))
+            (if
+              (dosync
+                (ensure wait-chairs-avaliable)
+                (if (> @wait-chairs-avaliable 0)
+                  (do
+                    (alter wait-chairs-avaliable dec)
+                    true)
+                  false))
+              ; if true
+              (do
+                (send barber request-hair-cut)
+                (recur consumer_lost))
+              ; if false
+              (recur (inc consumer_lost))))
+          ; if false
+          consumer_lost))))
 
   @service
   (await barber)
 
-  (println @barber)
+  (def result @barber)
+  (def consumer_lost @filler)
+
+  (println "Consumer served count: " (first result))
+  (println "Barber inactive count: " (second result))
   ; the result will be no more than 500
   ;     because the barber will always spend 20ms to cut hairs
   ; even assume full-speed of consumer's arrival
   ;     i.e. consumers' arrival interval is 10ms
-  ; this program produces `497` as output
+  ; this program produces `497` as output with barber keeping busy (no inactive time)
+
+  ; 'consumer lost' stands for those consumers 
+  ;     who have come to the barber shop but found no free chairs and had to leave
+  (println "Consumer lost   count: " consumer_lost)
 )
