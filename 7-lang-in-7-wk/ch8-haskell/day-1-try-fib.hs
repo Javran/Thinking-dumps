95065b26886c1415c9fb2dde380c13dcac9b7568
Javran Cheng <Javran.c@gmail.com>
2013-02-25 23:12:28 +0800

    working on 8.2.3

---
+++ 7-lang-in-7-wk/ch8-haskell/day-1-try-fib.hs
@@ -0,0 +1,35 @@
+import Utils
+
+fib :: Integer -> Integer
+fib 0 = 0 -- yes, here should be 0 actually
+fib 1 = 1
+fib x = fib (x-1) + fib (x-2)
+
+testFib :: (Integer -> Integer) -> [Integer] -> IO ()
+testFib fib testCase = do
+	putStrLn "Test Fibinacci sequence:"
+	mapM_ (putExprLn . fib) testCase
+
+fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+fibTuple (x, y, 0) = (x, y, 0)
+fibTuple (x, y, index) = fibTuple (y, x+y, index - 1)
+
+fibQuicker :: Integer -> Integer
+fibQuicker x = fibResult $ fibTuple (0, 1, x)
+	where
+		fibResult (x, _, _) = x
+
+
+main = do
+	testFib fib [1..10]
+
+	putExprLn $ fibTuple(0,1,4)
+	-- (3, 5, 0)
+
+	testFib fibQuicker [1..10]
+
+	putStrLn "Fib 100:"
+	putExprLn $ fibQuicker 100
+
+	putStrLn "Fib 1000:"
+	putExprLn $ fibQuicker 1000
