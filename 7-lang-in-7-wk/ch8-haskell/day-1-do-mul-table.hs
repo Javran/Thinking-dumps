4134c547c0c1761a12bc7f2d0f24cf2df2cff9a9
Javran Cheng <Javran.c@gmail.com>
2013-02-27 23:51:05 +0800

    add comment

--- 7-lang-in-7-wk/ch8-haskell/day-1-do-mul-table.hs
+++ 7-lang-in-7-wk/ch8-haskell/day-1-do-mul-table.hs
@@ -2,9 +2,17 @@ import Utils
 
 -- make a multiplication table: (x, y, x*y)
 --     where x = start, y <- [start .. end]
+-- e.g. when (start, end) = (2, 4), result = [(2,2,4),(2,3,6],(2,4,8)]
 mkMultiplicationTable start end =
 	[ (x, y, x*y) | x <- return start, y <-[start..end]]
 
+-- the table will be:
+-- [
+--     [ (1,1,1), (1,2,2) ..., (1,12,12) ]
+--     [ (2,2,4), (2,3,6) ..., (2,12,24) ]
+--     ...
+--     [ (12,12,144) ]
+-- ]
 multiplicationTable = map (\row -> mkMultiplicationTable row 12) [1..12]
 
 stringJoin :: String -> [String] -> String
@@ -12,5 +20,7 @@ stringJoin separator xs = foldl (\acc i -> acc ++ separator ++ i) (head xs) (tai
 
 main = do
 	mapM_ putTableLineLn  multiplicationTable where
+		-- format and print each line, e.g. [ (2,2,4), (2,3,6) ..., (2,12,24) ]
+		--     will turns into "2x2=4 2x3=6 ... 2x12=24"
 		putTableLineLn tableLine = putStrLn $ stringJoin " " $ map tupleToStr tableLine
 		tupleToStr (x,y,z) = show x ++ "x" ++ show y ++ "=" ++ show z
