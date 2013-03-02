5d0ea626bae95c9cc8299b5ddf9b2f20fd8a18eb
Javran Cheng <Javran.c@gmail.com>
2013-03-02 23:28:12 +0800

    selection sort

--- 7-lang-in-7-wk/ch8-haskell/day-2-do-my-sort.hs
+++ 7-lang-in-7-wk/ch8-haskell/day-2-do-my-sort.hs
@@ -1,12 +1,37 @@
+import Data.List
 import Utils
 
-quickSortBy :: (Ord a) => (a -> a -> Ordering) -> [a] -> [a]
+type SortByFunction a = (a -> a -> Ordering) -> [a] -> [a]
+
+-- http://en.wikipedia.org/wiki/Quick_sort
+quickSortBy :: (Ord a) => SortByFunction a
 quickSortBy _ [] = []
 quickSortBy cmp (x:xs) = (quickSortBy cmp leftPart) ++ [x] ++ (quickSortBy cmp rightPart) where
 	leftPart  = [ e | e <- xs, e `cmp` x /= GT]
 	rightPart = [ e | e <- xs, e `cmp` x == GT] 
 
-quickSort = quickSortBy compare
+-- http://en.wikipedia.org/wiki/Selection_sort
+selectionSortBy :: (Ord a) => SortByFunction a
+selectionSortBy _ [] = []
+selectionSortBy cmp xs = minElem : (selectionSortBy cmp restXs) where
+	-- separate the min element from the list
+	(minElem, restXs) = pickMin xs
+
+	-- pick the minimum element from the list
+	pickMin (x:xs) = foldl collectMin (x, []) xs where
+		collectMin (curMin, restElements) curValue =
+			if (curValue < curMin)
+			   	-- use curValue and put back curMin
+				then (curValue, curMin:restElements)
+				-- else keep going
+				else (curMin, curValue:restElements)
+
+
+testSortBy sortByFunc cmp inList =
+	if (sortByFunc cmp inList) == (sortBy cmp inList)
+		then putStrLn "Test passed"
+		else putStrLn "Test failed"
 
 main = do
-	putExprLn $ quickSort [1,9,2,8,3,7,4,6,5]
+	let testCase = [1,9,2,8,3,7,4,6,5]
+	testSortBy selectionSortBy compare testCase
