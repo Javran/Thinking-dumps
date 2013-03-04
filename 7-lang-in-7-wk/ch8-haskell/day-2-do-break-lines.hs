9376d31693d1df9fc124a4a2c06da9376d3970fc
Javran Cheng <Javran.c@gmail.com>
2013-03-05 00:33:40 +0800

    day 2 exercise 8 -> done

--- 7-lang-in-7-wk/ch8-haskell/day-2-do-break-lines.hs
+++ 7-lang-in-7-wk/ch8-haskell/day-2-do-break-lines.hs
@@ -4,14 +4,12 @@ import Control.Monad
 import Utils
 
 -- I'd like to treat my line breaker as some kind of printer
+--     by giving words one-by-one, the printer will finally produce the desired output
 
 testFile = "day-2-do-break-lines-test.txt"
 
-processLine :: String -> [String]
-processLine = words
-
 -- status definition:
--- (restWordList printedLines, currentLine)
+-- (printedLines, currentLine, widthLimit)
 data PrintStatus = 
 	PrintStatus
 		{ printedLines :: [String]
@@ -19,7 +17,7 @@ data PrintStatus =
 		, widthLimit   :: Int
 		} deriving (Show)
 
--- initialize printer
+-- initialize printer with widthLimit
 printInit :: Int -> PrintStatus
 printInit widthLimit = PrintStatus
 		{ printedLines = []
@@ -32,24 +30,41 @@ printWord :: PrintStatus -> [String] -> (PrintStatus, [String])
 printWord printStatus (word:restWordList) = 
 	if currentLine printStatus == []
 		then
-			-- nothing in current line, simply printing the word will do
-			(
-				  PrintStatus
-					{ printedLines = printedLines printStatus
-					, currentLine = word
-					, widthLimit = widthLimit printStatus
-					}
-				, restWordList
-			)
+			if length word <= sWidthLimit
+				then
+				-- nothing in current line, and word does not exceed line limit,
+				--     simply printing the word will do
+					(
+						  PrintStatus
+							{ printedLines = sPrintedLines
+							, currentLine = word
+							, widthLimit = sWidthLimit
+							}
+						, restWordList
+					)
+     				else
+				-- the word cannot be contained in a single line
+				--     we have no choice but break the word
+				-- TODO: can string be broken at some position
+				--     that we can get the first part & another part at same time
+				--     instead of use `take` and `drop` ?
+					(
+						  PrintStatus
+						  	{ printedLines = sPrintedLines
+							, currentLine = take sWidthLimit word
+							, widthLimit = sWidthLimit
+							}
+						, (drop sWidthLimit word):restWordList
+					)
 		else
-			if (length $ currentLine printStatus) + (length word) + 1 <= (widthLimit printStatus)
+			if (length sCurrentLine) + (length word) + 1 <= sWidthLimit
 				then
 			   	-- simply append current word to the current line if possible
 					(
 						  PrintStatus
-							{ printedLines = printedLines printStatus
-							, currentLine = (currentLine printStatus) ++ " " ++ word
-							, widthLimit = widthLimit printStatus
+							{ printedLines = sPrintedLines
+							, currentLine = sCurrentLine ++ " " ++ word
+							, widthLimit = sWidthLimit
 							}
 						, restWordList
 					)
@@ -59,7 +74,12 @@ printWord printStatus (word:restWordList) =
 						  printNewline printStatus
 						, word:restWordList
 					)
+     		where
+		     	sPrintedLines = printedLines printStatus
+		     	sCurrentLine = currentLine printStatus
+			sWidthLimit = widthLimit printStatus
 
+-- put current line into printed lines
 printNewline :: PrintStatus -> PrintStatus
 printNewline printStatus =
 	PrintStatus
@@ -68,11 +88,13 @@ printNewline printStatus =
 	, widthLimit = widthLimit printStatus
 	}
 
+-- tell a 'printer' print all words in a list
 printAllWords :: PrintStatus -> [String] -> PrintStatus
 -- all words' been printed, no more things to do
 printAllWords printStatus [] = printStatus
-printAllWords printStatus (wordList) = finalPrintStatus where
-	(newPrintStatus, newRestWordList) = printWord printStatus (wordList)
+-- attempt to feed the 'printer' with new word, recursively
+printAllWords printStatus wordList = finalPrintStatus where
+	(newPrintStatus, newRestWordList) = printWord printStatus wordList
 	finalPrintStatus = printAllWords newPrintStatus newRestWordList
 
 printStatusToLines :: PrintStatus -> [String]
@@ -82,16 +104,18 @@ printStatusToLines printStatus = (printedLines printStatus) ++ [currentLine prin
 breakLine :: Int -> String -> [String]
 breakLine widthLimit rawLine = outputLines where
 	outputLines = printStatusToLines printStatus
-	printStatus = printAllWords (printInit widthLimit) (words rawLine)--printNewline $ foldl printWord (printInit widthLimit) (words rawLine)
+	printStatus = printNewline $ printAllWords (printInit widthLimit) (words rawLine)
 
 -- if you want to know how to deal with files in haskell
 -- please refer to:
 --     http://learnyouahaskell.com/input-and-output#files-and-streams
 main = do
+	putStrLn "Task #8: break long strings"
+
+	putStrLn "====== Output begin ======"
 	hFile <- openFile testFile ReadMode
 	content <- hGetContents hFile
 	let fileLines = lines content
-
-	putStrLn $ unlines $ join $ map (breakLine 80) fileLines 
-
+	putStr $ unlines $ join $ map (breakLine 80) fileLines 
 	hClose hFile
+	putStrLn "======  Output end  ======"
