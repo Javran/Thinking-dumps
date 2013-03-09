c915a5e0657e8bf782f967e4a1226cec3ed985ee
Javran Cheng <Javran.C@gmail.com>
2013-03-09 16:44:37 +0800

    day 3 exercise 2 -> done

--- 7-lang-in-7-wk/ch8-haskell/working/day-3-do-maze.hs
+++ 7-lang-in-7-wk/ch8-haskell/working/day-3-do-maze.hs
@@ -1,4 +1,5 @@
 import Data.Ix
+import Control.Monad
 
 import Utils
 
@@ -19,7 +20,9 @@ data Wall = Wall
 instance Eq Wall where
 	x == y =
 		   (node1 x, node2 x) == (node1 y, node2 y)
+		--                            ^        ^
 		|| (node1 x, node2 x) == (node2 y, node1 y) 
+		--                            ^        ^
 
 data Maze = Maze
 	{ width 	:: Int
@@ -44,6 +47,17 @@ node `inside` maze =
 	inRange (1, width maze) (col node) && 
 	inRange (1, height maze) (row node)
 
+-- judge if two neighboring node is connected
+isNeighborConnected :: Maze -> Node -> Node -> Bool
+isNeighborConnected maze n1 n2 =
+	-- if n1 n2 are both inside the maze 
+	if all (`inside` maze) [n1,n2]
+		-- if Wall n1 n2 is an element of walls
+		--     they are not connected
+		then not $ (Wall n1 n2) `elem` (walls maze)
+		-- n1 or n2 is outside the maze, they are not collected
+		else False
+
 -- returns all reachable neighbors of a node in maze
 neighbors :: Maze -> Node -> [Node]
 neighbors maze node = reachableNeighbors where
@@ -51,29 +65,65 @@ neighbors maze node = reachableNeighbors where
 	validNeighbors = filter (`inside` maze) allNeighbors
 	reachableNeighbors = [ nNode | nNode <- validNeighbors, not $ (Wall node nNode) `elem` (walls maze) ]
 
--- let's make a test case
--- +-+-+-+
--- | |   |
--- + + + +
--- |   | |
--- +-+-+-+
--- maze: width = 3, height = 2
---     2 walls: (1,1)-(2,1) and (2,2)-(3,2)
-testMaze = Maze 
-	{ width 	= 3
-	, height 	= 2
-	, walls 	= 
-		[ Wall 
-			{ node1 = Node 1 1
-			, node2 = Node 2 1}
-		, Wall
-			{ node1 = Node 2 2
-			, node2 = Node 3 2}
-		]
-	}
-
--- TODO: pretty print for maze
+-- return lines that pretty prints the maze when outputed line-by-line
+prettyLinesMaze :: Maze -> [String]
+prettyLinesMaze maze = header : body where
+	mWidth = width maze
+	mHeight = height maze
+	header = foldl (++) "+" $ replicate mWidth "-+"
+	body = concat $ do -- flatten results
+		line <- [1..mHeight]
+		let cellInLine = [ Node x line | x <- [1..mWidth] ]
+		-- now for each cell we can verify the connectivity of bottom side & right side	
+		-- returns a tuple: (bottomSide, rightSide),
+		--     bottomSide :: Bool
+		--     rightSide :: Bool
+		let cellInfos = do
+			(Node x y) <- cellInLine
+			let bottomSide = not $ isNeighborConnected maze (Node x y) (Node x (y+1))
+			let rightSide = not $ isNeighborConnected maze (Node x y) (Node (x+1) y)
+			return (bottomSide, rightSide)
+		-- `toCellStrings` covers any cellInfo to a tuple: (cell1, cell2) where 
+		--     cell1 = " |" or "  "
+		--     cell2 = "-+" or " +"
+		let toCellStrings (bottomSide, rightSide) =
+			( if rightSide then " |" else "  "
+			, if bottomSide then "-+" else " +") 
+		let cellStrings = map toCellStrings cellInfos
+		-- reduce strings, and decorate with appropriate chars
+		let line1 = ('|' :) $ foldl1 (++) $ map fst cellStrings
+		let line2 = ('+' :) $ foldl1 (++) $ map snd cellStrings
+		return [ line1, line2]
 
 main = do
+	-- let's make a test case
+	-- +-+-+-+
+	-- | | | |
+	-- + + + +
+	-- |   | |
+	-- +-+-+-+
+	-- maze: width = 3, height = 2
+	--     3 walls:
+	--         (1,1)-(2,1)
+	--         (2,2)-(3,2)
+	--         (2,1)-(3,1)
+	let testMaze = Maze 
+		{ width 	= 3
+		, height 	= 2
+		, walls 	= 
+			[ Wall ( Node 1 1 ) ( Node 2 1 )
+			, Wall ( Node 2 2 ) ( Node 3 2 )
+			, Wall ( Node 2 1 ) ( Node 3 1 )
+			]
+		}
 	let testNodes = [ Node x y | x <- [1..3], y <- [1..2] ]
-	mapM_ (putExprLn.(neighbors testMaze)) testNodes
+	-- testNodes' connectivity
+	let testConnectivityIO n = do
+		putExprLn n
+		putStr "reachable neighbors: "
+		putExprLn $ neighbors testMaze n
+
+	putStrLn "Task #2: represent a maze"
+		
+	mapM_ testConnectivityIO testNodes
+	mapM_ putStrLn $ prettyLinesMaze testMaze
