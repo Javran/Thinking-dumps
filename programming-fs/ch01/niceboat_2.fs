6da6413b2ac8bc2698751034dbdb2423dffb6105
Javran Cheng <Javran.C@gmail.com>
2013-03-11 19:43:59 +0800

    ch01 - done, just some basic stuffs

--- programming-fs/ch01/niceboat_2.fs
+++ programming-fs/ch01/niceboat_2.fs
@@ -4,6 +4,10 @@ open System
     block comments
 *)
 
+// to run 'main' with proper arguments, you can type in:
+// main [| "Nice"; "boat" |];;
+// in interactive mode
+
 [<EntryPoint>]
 let main (args: string[]) =
     if args.Length <> 2 then
@@ -13,6 +17,11 @@ let main (args: string[]) =
     let timeOfDay = DateTime.Now.ToString("hh:mm tt")
 
     printfn "%s, %s at %s" greeting thing timeOfDay
+
+    // variable name is case sensitive
+    let number = 1
+    let Number = 2
+    printfn "number is %d, Number is %d" number Number
 
     // Program exit code
     0
