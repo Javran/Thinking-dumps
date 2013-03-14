7c8faa24b880da9c9c8c9cedab36d0065b7b17db
Javran Cheng <Javran.c@gmail.com>
2013-03-14 23:54:22 +0800

    3.2

---
+++ t-y-scheme/ch03/apply.scm
@@ -0,0 +1,29 @@
+(define out
+  (lambda (what)
+    (begin
+      (display what)
+      (newline))))
+
+(newline)
+
+(define x '(1 2 3 4))
+
+(out (apply + x))
+; 10
+
+(out (apply * x))
+; 24
+
+(define var-arg
+  (lambda (a b . c)
+    (begin
+      (out a)
+      (out b)
+      (out c))))
+
+(apply var-arg 10 20 30 x)
+(apply var-arg 10 20 30 '(1 2 3 4))
+; 10, 20, (30 1 2 3 4)
+
+(out (apply + 1 x))
+; 11
