50b7ab76561f92ae696b0a2e3c7895470309460e
Javran Cheng <Javran.C@gmail.com>
2013-03-17 16:52:40 +0800

    lists -> done

--- programming-fs/ch02/core-types.fs
+++ programming-fs/ch02/core-types.fs
@@ -47,4 +47,115 @@ add 3 7;;
 tupledAdd (3,7);;
 // 10
 
+let vowels = ['a'; 'e'; 'i'; 'o'; 'u'];;
+let emptyList = [];;
+
+// cons operator
+let sometimes = 'y' :: vowels;;
+
+// append operator
+let odds = [1; 3; 5; 7; 9]
+let evens = [2; 4; 6; 8; 10]
+
+odds @ evens;;
+
+// list ranges
+
+let x = [1 .. 10];;
+let tens = [0 .. 10 .. 50];;
+let countDown = [ 5L .. -1L .. 0L ];;
+let countDownBy x = [ 100 .. (-x) .. 0 ];;
+
+countDownBy 7;;
+// [100; 93; 86; .. ]
+
+// list comprehension
+let numbersNear x =
+    [
+        yield x - 1
+        yield x
+        yield x + 1
+    ];;
+
+numbersNear 3;;
+// [2; 3; 4];;
+
+// more complex list comprehension
+let x =
+    [
+        let negate x = -x
+        for i in 1..10 do
+            if i % 2 = 0 then
+                yield negate i
+            else
+                yield i
+    ];;
+
+let multipesOf1 x = [ for i in 1 .. 10 do yield x * i ]
+let multipesOf2 x = [ for i in 1 .. 10 -> x * i ]
+
+multipesOf1 10;;
+multipesOf2 11;;
+
+// example of using list comprehension to compute primes
+let primesUnder max =
+    [
+        for n in 1 .. max do
+            let factorsOfN =
+                [
+                    for i in 1 .. n do
+                        if n % i = 0 then
+                            yield i
+                ]
+            if List.length factorsOfN = 2 then
+                yield n
+    ];;
+
+primesUnder 100;;
+
+// it's high time that we got along with list module functions
+let testList = [1 .. 100]
+
+List.length testList;;
+// 100
+List.head testList;;
+// 1
+List.tail testList;;
+// [2..100]
+List.head (List.tail (List.tail testList));;
+// 3
+
+List.exists ((=) 10) testList;;
+// true
+
+// if there's any element in list that satisfies (< 100 x) (i.e. 100 < x)
+List.exists ((<) 100) testList;;
+// false
+
+// reverse a list
+List.rev testList;;
+
+let greaterThan x y = y > x
+    
+// note here is 'tryFind' rather than 'tryfind' in book
+List.tryFind (greaterThan 50) testList;;
+// returns Some 51
+
+List.tryFind (greaterThan 100) testList;;
+// return None because nothing can be greater than 100 in this list
+
+// zip 2 lists to produce a list of tuples
+//     whose values are extracted from the 2 given lists
+List.zip testList [ for i in 1..100 -> i * 2];;
+
+// hello, filter!
+List.filter (greaterThan 90) testList;;
+
+// break a list into two using a given condition
+List.partition (greaterThan 49) testList;;
+
+let isMultipleOf5 x = (x % 5 = 0)
+
+let multOf5, nonMultipleOf5 = List.partition isMultipleOf5 [1..15];;
+
 #quit;;
