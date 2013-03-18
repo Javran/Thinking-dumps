49fb25c5206b124e81f7b515c3c85a15501108d0
Javran Cheng <Javran.c@gmail.com>
2013-03-19 00:16:21 +0800

    6.1 done

--- ty-scheme/ch06/recursion.scm
+++ ty-scheme/ch06/recursion.scm
@@ -17,6 +17,7 @@
 ; I'd like to ask why can mutually recursive work,
 ;     given that the time when is-even? is defined,
 ;     it have no idea about what is 'is-odd?'
+
 (define is-even?
   (lambda (n)
     (if (= n 0) #t
@@ -32,3 +33,32 @@
 ; #t
 (out (is-odd? 9))
 ; #t
+
+; letrec
+
+; lines below will not work, since 'local-odd?' cannot be seen outside
+;     and 'let*' will not work for the same reason
+
+; (let (
+;   (local-even?
+;     (lambda (n)
+;       (if (= n 0) #t
+;         (local-odd? (- n 1)))))
+;   (local-odd?
+;     (lambda (n)
+;       (if (= n 0) #f
+;         (local-odd? (- n 1))))))
+;   (list (local-even? 23) (local-odd? 23)))
+
+; 'letrec' make indroduction visible in letrec body & all initialization
+(letrec (
+  (local-even?
+    (lambda (n)
+       (if (= n 0) #t
+         (local-odd? (- n 1)))))
+   (local-odd?
+     (lambda (n)
+       (if (= n 0) #f
+         (local-odd? (- n 1))))))
+   (out (list (local-even? 23) (local-odd? 23))))
+; '(#f #f)
