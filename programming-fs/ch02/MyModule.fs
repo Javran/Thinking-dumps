ac37b82ff892cab1c458c27734ff22fc3d6a2f25
Javran Cheng <Javran.C@gmail.com>
2013-03-20 21:11:07 +0800

    make modules work in interactive mode

---
+++ programming-fs/ch02/MyModule.fs
@@ -0,0 +1,5 @@
+module MyModule
+
+let a = 1
+let b = "2"
+let c = '3'
---
+++ programming-fs/ch02/modules.fs
@@ -0,0 +1,5 @@
+// note this file only works in interactive mode
+
+#load "MyModule.fs"
+
+printfn "%d %s %c" MyModule.a MyModule.b MyModule.c
