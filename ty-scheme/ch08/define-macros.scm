1382828b4f240c56ca67c69a3bc6e48b5a15db7e
Javran Cheng <Javran.c@gmail.com>
2013-03-22 01:57:24 +0800

    found good solution for define-macro equivalent

--- ty-scheme/ch08/define-macros.scm
+++ ty-scheme/ch08/define-macros.scm
@@ -1,13 +1,23 @@
 (load "../common/utils.scm")
 
-; TODO: can we simplify the macro definition?
-(define-syntax when
-  (rsc-macro-transformer
-    (let ((xfmr (lambda (test . branch)
-		  (list 'if test
-                    (cons 'begin branch)))))
-      (lambda (e r)
-	(apply xfmr (cdr e))))))
+; just follow the suggestion here:
+; http://stackoverflow.com/questions/15552057/is-it-possible-to-implement-define-macro-in-mit-scheme/
+; we should prefer syntax-rules over define-macro
+;     so that we'll get clearer codes.
+(define-syntax define-syntax-rule
+  (syntax-rules ()
+    ((define-syntax-rule (name . pattern) template)
+     (define-syntax name
+       (syntax-rules ()
+         ((name . pattern) template))))))
 
-(when #t
-  (out "this will be displayed when the condition is met"))
+(define-syntax-rule (my-when test branch)
+  (if test
+    (begin branch)))
+
+(my-when #t
+  (display "This string should be printed"))
+
+(my-when #f
+  (display "This string should not be printed"))
+
