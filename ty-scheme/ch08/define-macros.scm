3a51363804eb08b18be74a14eafc679c6d98ddf5
Javran Cheng <Javran.c@gmail.com>
2013-03-22 22:42:28 +0800

    def-macro unavailable, feel struggle to keep going

--- ty-scheme/ch08/define-macros.scm
+++ ty-scheme/ch08/define-macros.scm
@@ -10,5 +10,15 @@
 (my-when #f
   (display "This string should not be printed"))
 
+(newline)
 
+(define-syntax-rule (my-unless test branch)
+  (my-when (not test) branch))
 
+(my-unless #t
+  (display "This should not be printed"))
+
+(my-unless #f
+  (display "This should be printed"))
+
+(newline)
