910c5b48a0afd58a9d6948ed8a3568e67befa2ae
Javran Cheng <Javran.c@gmail.com>
2013-03-22 22:20:29 +0800

    move define-syntax-rule definition to utils

--- ty-scheme/ch08/define-macros.scm
+++ ty-scheme/ch08/define-macros.scm
@@ -1,16 +1,5 @@
 (load "../common/utils.scm")
 
-; just follow the suggestion here:
-; http://stackoverflow.com/questions/15552057/is-it-possible-to-implement-define-macro-in-mit-scheme/
-; we should prefer syntax-rules over define-macro
-;     so that we'll get clearer codes.
-(define-syntax define-syntax-rule
-  (syntax-rules ()
-    ((define-syntax-rule (name . pattern) template)
-     (define-syntax name
-       (syntax-rules ()
-         ((name . pattern) template))))))
-
 (define-syntax-rule (my-when test branch)
   (if test
     (begin branch)))
@@ -20,4 +9,6 @@
 
 (my-when #f
   (display "This string should not be printed"))
+
+
 
--- ty-scheme/common/utils.scm
+++ ty-scheme/common/utils.scm
@@ -1,3 +1,16 @@
+
+; just follow the suggestion here:
+; http://stackoverflow.com/questions/15552057/is-it-possible-to-implement-define-macro-in-mit-scheme/
+; we should prefer syntax-rules over define-macro
+;     so that we'll get clearer codes.
+(define-syntax define-syntax-rule
+  (syntax-rules ()
+    ((define-syntax-rule (name . pattern) template)
+     (define-syntax name
+       (syntax-rules ()
+         ((name . pattern) template))))))
+
+
 (define out
   (lambda (what)
     (begin
