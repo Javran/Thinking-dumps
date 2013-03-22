10f612e81c61e03336f1b68b9ea6e693b7572c10
Javran Cheng <Javran.c@gmail.com>
2013-03-22 23:21:06 +0800

    right before 8.1

--- ty-scheme/ch08/define-macros.scm
+++ ty-scheme/ch08/define-macros.scm
@@ -22,3 +22,43 @@
   (display "This should be printed"))
 
 (newline)
+
+(define-syntax when
+  (rsc-macro-transformer
+    (let ((xfmr (lambda (test . branch)
+                  (list 'if test
+                    (cons 'begin branch)))))
+      (lambda (e r)
+        (apply xfmr (cdr e))))))
+
+(when #t
+  (out "This appears to be true"))
+
+(define-syntax unless1
+  (rsc-macro-transformer
+    (let ((xfmr (lambda (test . branch)
+                  (list 'if
+                    (list 'not test)
+                    (cons 'begin branch)))))
+      (lambda (e r)
+        (apply xfmr (cdr e))))))
+
+(unless1 #t
+  (out "unless1-#t"))
+
+(unless1 #f
+  (out "unless1-#f"))
+
+(define-syntax unless2
+  (rsc-macro-transformer
+    (let ((xfmr (lambda (test . branch)
+                  (cons 'when
+                    (cons (list 'not test) branch)))))
+      (lambda (e r)
+        (apply xfmr (cdr e))))))
+
+(unless2 #t
+  (out "unless2-#t"))
+
+(unless2 #f
+  (out "unless2-#f"))
