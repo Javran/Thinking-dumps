ed32750598237ee95dc90465fae100f3d2599341
Javran Cheng <Javran.c@gmail.com>
2013-03-23 23:28:36 +0800

    get defstruct work

---
+++ ty-scheme/ch09/defstruct.scm
@@ -0,0 +1,65 @@
+(define list-position
+  (lambda (o l)
+    (let loop ((i 0) (l l))
+      (if (null? l) #f
+          (if (eqv? (car l) o) i
+              (loop (+ i 1) (cdr l)))))))
+
+(define-syntax defstruct
+  (rsc-macro-transformer 
+    (let ((xfmr (lambda (s . ff)
+    (let ((s-s (symbol->string s)) (n (length ff)))
+      (let* ((n+1 (+ n 1))
+             (vv (make-vector n+1)))
+        (let loop ((i 1) (ff ff))
+          (if (<= i n)
+            (let ((f (car ff)))
+              (vector-set! vv i 
+                (if (pair? f) (cadr f) '(if #f #f)))
+              (loop (+ i 1) (cdr ff)))))
+        (let ((ff (map (lambda (f) (if (pair? f) (car f) f))
+                       ff)))
+          `(begin
+             (define ,(string->symbol 
+                       (string-append "make-" s-s))
+               (lambda fvfv
+                 (let ((st (make-vector ,n+1)) (ff ',ff))
+                   (vector-set! st 0 ',s)
+                   ,@(let loop ((i 1) (r '()))
+                       (if (>= i n+1) r
+                           (loop (+ i 1)
+                                 (cons `(vector-set! st ,i 
+                                          ,(vector-ref vv i))
+                                       r))))
+                   (let loop ((fvfv fvfv))
+                     (if (not (null? fvfv))
+                         (begin
+                           (vector-set! st 
+                               (+ (list-position (car fvfv) ff)
+                                  1)
+                             (cadr fvfv))
+                           (loop (cddr fvfv)))))
+                   st)))
+             ,@(let loop ((i 1) (procs '()))
+                 (if (>= i n+1) procs
+                     (loop (+ i 1)
+                           (let ((f (symbol->string
+                                     (list-ref ff (- i 1)))))
+                             (cons
+                              `(define ,(string->symbol 
+                                         (string-append
+                                          s-s "." f))
+                                 (lambda (x) (vector-ref x ,i)))
+                              (cons
+                               `(define ,(string->symbol
+                                          (string-append 
+                                           "set!" s-s "." f))
+                                  (lambda (x v) 
+                                    (vector-set! x ,i v)))
+                               procs))))))
+             (define ,(string->symbol (string-append s-s "?"))
+               (lambda (x)
+                 (and (vector? x)
+                      (eqv? (vector-ref x 0) ',s)))))))))))
+        (lambda (e r)
+          (apply xfmr (cdr e))))))
