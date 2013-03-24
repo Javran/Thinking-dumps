fc254409893fc0d8f69f49528db1f4e6e9b96ac9
Javran Cheng <Javran.c@gmail.com>
2013-03-24 21:43:19 +0800

    ch10 - done

--- ty-scheme/ch10/alist.scm
+++ ty-scheme/ch10/alist.scm
@@ -8,10 +8,23 @@
 (table-put! my-table 'two 2)
 (table-put! my-table 'three 3)
 
-(table-for-each my-table out)
+(define out-key-value
+  (lambda (k v)
+    (display k)
+    (display " -> ")
+    (display v)
+    (newline)))
+
+(out
+   (table-get my-table 'four 'default 'ignore 'ignore))
+; symbol "default"
+
+(table-for-each my-table out-key-value)
 (newline)
 
 (out
+  my-table
+  ; the underlying structure
   (table-get my-table 'two)
   ; 2
   (table-get my-table 'three)
--- ty-scheme/common/alist.scm
+++ ty-scheme/common/alist.scm
@@ -2,27 +2,32 @@
 
 (defstruct table (equ eqv?) (alist '()))
 
-(define lassoc
+; find 'k in k-v list 'al' using predicate 'equ?'
+(define find-in-table
   (lambda (k al equ?)
     (let loop ((al al))
       (if (null? al) #f
-          (let ((c (car al)))
-            (if (equ? (car c) k) c
+          (let ((first-pair (car al)))
+            (if (equ? (car first-pair) k) first-pair
                 (loop (cdr al))))))))
 
+; get value from the table, or default value 'd' is returned
 (define table-get
   (lambda (tbl k . d)
-    (let ((c (lassoc k (table.alist tbl) (table.equ tbl))))
+    ; try to find the k-v pair in list
+    (let ((c (find-in-table k (table.alist tbl) (table.equ tbl))))
       (cond (c (cdr c))
             ((pair? d) (car d))))))
 
+; put key-value pair into a dict
 (define table-put!
   (lambda (tbl k v)
     (let ((al (table.alist tbl)))
-      (let ((c (lassoc k al (table.equ tbl))))
+      (let ((c (find-in-table k al (table.equ tbl))))
         (if c (set-cdr! c v)
             (set!table.alist tbl (cons (cons k v) al)))))))
 
+; call 'p' for each k-v pair in a dict
 (define table-for-each
   (lambda (tbl p)
     (for-each
