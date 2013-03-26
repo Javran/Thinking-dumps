af9a292e5d650a30a11aef21d2a9553066061e11
Javran Cheng <Javran.c@gmail.com>
2013-03-27 00:11:09 +0800

    13.1 - working

--- ty-scheme/ch13/call-cc.scm
+++ ty-scheme/ch13/call-cc.scm
@@ -13,3 +13,30 @@
 ; the computation "(+ 2 ...)" is aborted, and replaced with "(k 3)"
 ;     so the final result is "(+ 1 3)", i.e. 4
 
+(define r #f)
+
+(out
+  (+ 1
+     (call/cc
+       (lambda (k)
+         (set! r k)
+         (+ 2 (k 3))))))
+; 4
+
+; r seems to be called "escaping procedure":
+; http://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Continuations.html
+
+(out 1)
+(out (r 5))
+; 6
+(out 2)
+(out (+ 3 (r 5)))
+; 6
+
+; why everything is aborted except "out"?
+; why can we still see the output?
+(out 3)
+(out
+  (r 5)
+  (+ 3 (r 5)))
+; 6
