c811a70a5890469ce02deacf7d7c0f928dd14f05
Javran Cheng <Javran.c@gmail.com>
2013-03-28 23:36:57 +0800

    need more comment to understand what's going on

--- ty-scheme/common/coroutines.scm
+++ ty-scheme/common/coroutines.scm
@@ -1,6 +1,7 @@
 (define-syntax coroutine
   (rsc-macro-transformer
     (let ((xfmr 
+            ; x: initial argument
             (lambda (x . body)
               `(letrec ((+local-control-state
                           (lambda (,x) ,@body))
@@ -9,6 +10,7 @@
                             (call/cc 
                               (lambda (k)
                                 (set! +local-control-state k)
+                                ; what?
                                 (c v))))))
                  (lambda (v)
                    (+local-control-state v))))))
