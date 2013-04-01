4f11253a27708e602642530fa773395eade08673
Javran Cheng <Javran.c@gmail.com>
2013-04-02 00:40:15 +0800

    14.4.1 - done

--- ty-scheme/ch14/kalotan.scm
+++ ty-scheme/ch14/kalotan.scm
@@ -23,11 +23,112 @@
 (out (distinct? '(f m f)))
 ; #f
 
-;(define solve-kalotan-puzzle
-;  (lambda ()
-;    (let ((parent1 (amb 'm 'f))
-;          (parent2 (amb 'm 'f))
-;          (kibi (amb 'm 'f))
-;          (kibi-self-desc (amb 'm 'f))
-;          (kibi-lied? (amb #t #f)))
+(newline)
 
+(define xor
+  (lambda (a b)
+    (if (eqv? a b)
+      #f
+      #t)))
+
+; somewhat similiar to list comprehension with guards
+(define solve-kalotan-puzzle
+  (lambda ()
+    (let ((parent1 (amb 'm 'f))
+          (parent2 (amb 'm 'f))
+          (kibi (amb 'm 'f))
+          (kibi-self-desc (amb 'm 'f))
+          (kibi-lied? (amb #t #f)))
+
+      (assert
+        (distinct? (list parent1 parent2)))
+      (assert
+        ; if kibi is male, he'll never tell lie
+        (if (eqv? kibi 'm)
+          (not kibi-lied?)))
+      (assert
+        ; if kibi lied, self-desc would not correspond with truth
+        (if kibi-lied?
+          (xor
+            ; either case #1 or case #2
+            ; case #1
+            (and (eqv? kibi-self-desc 'm)
+                 (eqv? kibi 'f))
+            ; case #2
+            (and (eqv? kibi-self-desc 'f)
+                 (eqv? kibi 'm)))))
+      (assert
+        (if (not kibi-lied?)
+          ; elsewise, self-desc would correspond with truth
+          (xor
+            ; either case #1 or case #2
+            ; case #1
+            (and (eqv? kibi-self-desc 'm)
+                 (eqv? kibi 'm))
+            ; case #2
+            (and (eqv? kibi-self-desc 'f)
+                 (eqv? kibi 'f)))))
+      (assert
+        ; male would not lie
+        (if (eqv? parent1 'm)
+          (and
+            ; and kibi's desc was "I am a boy"
+            (eqv? kibi-self-desc 'm)
+
+            ; parent2 said:
+            (xor
+              ; 1: kibi is a girl
+              ; 2: kibi lied
+              (and (eqv? kibi 'f)         ; 1
+                   (eqv? kibi-lied? #f))  ; 2
+
+              (and (eqv? kibi 'm)         ; 1 
+                   (eqv? kibi-lied? #t)))); 2
+          ))
+
+      (assert
+        ; parent1 -> femail
+        (if (eqv? parent1 'f)
+          ; male would not lie
+          (and
+            (eqv? kibi 'f)
+            (eqv? kibi-lied? #t))))
+      (list parent1 parent2 kibi))))
+
+(out (bag-of (solve-kalotan-puzzle)))
+; (f m f)
+
+; FYI: what if no limitation is set?
+(define no-limit-test
+  (lambda ()
+    (let ((parent1 (amb 'm 'f))
+          (parent2 (amb 'm 'f))
+          (kibi (amb 'm 'f))
+          (kibi-self-desc (amb 'm 'f))
+          (kibi-lied? (amb #t #f)))
+
+      (list parent1 parent2 kibi))))
+
+(out (bag-of (no-limit-test)))
+; all combinations are shown
+
+; let's try something easier!
+(define number-between
+  (lambda (lo hi)
+    (let loop ((i lo))
+      (if (> i hi) (amb)
+        (amb i (loop (+ i 1)))))))
+; for 3 numbers, a, b, c, s.t. 1 <= a <= b <= c <= 10 & a^2 + b^2 = c^2
+
+(define right-triangle
+  (lambda ()
+    (let* ((a (number-between 1 10))
+           (b (number-between a 10))
+           (c (number-between b 10)))
+      (assert (= 
+                (+ (* a a) (* b b))
+                (* c c)))
+      (list a b c))))
+
+(out (bag-of (right-triangle)))
+; well, we have (3 4 5) and (6 8 10) :)
