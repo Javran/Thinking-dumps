8377b095e6cc2f0d9ecbe518a4a5e7ef2821b962
Javran Cheng <Javran.C@gmail.com>
2013-04-12 21:12:52 +0800

    mutable rec done, fix filename arranging

--- programming-fs/ch04/understanding_memory.fs
+++ programming-fs/ch04/understanding_memory.fs
@@ -40,49 +40,4 @@ y.[0] <- 1;;
 printfn "%A %A" x y;;
 // all changes to [|1|]
 
-// "mutable variable" ... I think two words are essentially the same... anyway
-// to change contents of a mutable value, use "<-"
-
-let mutable message = "Boat";;
-
-printfn "Nice %s" message;;
-// Nice Boat!
-
-message <- "Universe";;
-printfn "Nice %s" message;;
-// Nice Universe!
-// that's why they are called "mutable" :)
-
-let invalidUseOfMutable () =
-    let mutable x = 0
-    // following line will fail, mutable values cannot be captured in a closure
-    //let incrementX() = x <- x + 1
-    // incrementX()
-    x;;
-
-// reference cells
-
-let planets =
-    ref [
-        "Mercury";  "Venus";    "Earth";
-        "Mars";     "Jupiter";  "Saturn";
-        "Uranus";   "Neptune";  "Pluto"
-    ];;
-
-// think "!" something like de-reference
-planets := ! planets |> List.filter (fun p -> p <> "Pluto")
-
-Seq.iter (printfn "%A") <| !planets;;
-
-// we have "decr" and "incr" to simply modify contents inside int ref
-let x = ref 0;;
-
-incr x;;
-incr x;;
-decr x;;
-printfn "%A" !x;;
-// 1
-
-// mutable records ... TODO
-
 #quit;;
