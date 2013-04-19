469feeab8ed78b597750c1da8740cf55150d5bff
Javran Cheng <Javran.C@gmail.com>
2013-04-19 13:58:41 +0800

    right before "Understanding classes"

--- programming-fs/ch05/objects.fs
+++ programming-fs/ch05/objects.fs
@@ -71,7 +71,67 @@ type ClassType2(x:int) =
 
 let a1,a2,a3 = new ClassType2(31), new ClassType2(31), new ClassType2(10000)
 
-printfn "%A,%A,%A" (a1=a2) (a2=a3) (a3=a1)
+printfn "%A,%A,%A" (a1=a2) (a2=a3) (a3=a1);;
 // true, false, false
+
+// generated equality
+
+// tuples are considered equal if they have same number of elements and each element is equal
+let x = (1, 'a', "str");;
+printfn "%A,%A,%A"
+    (x=x) // true
+    (x = (1, 'a', "str")) // true
+    ((x,x) = (x, (1, 'a', "str"))) // true
+
+// tuple with different number of elements might not pass the compiler,
+// for example, the next line causes error rather than returning a value
+// I guess "=" requires both of its argument are of same type
+// (x = (1, 'a', "str", ()));;
+
+// check the type signature
+(=);;
+// 'a -> 'a -> bool
+
+// thus: equality includes type equality, which is checked at complie time.
+
+// record equality = all fields are equal
+type RecType = { Field1 : string; Field2 : float }
+
+let testRecEqu =
+    let x,y,z =
+        { Field1 = "abc"; Field2 = 3.5 },
+        { Field1 = "abc"; Field2 = 3.5 },
+        { Field1 = "XXX"; Field2 = 0.0 }
+
+    printfn "%A,%A,%A" (x=y) (y=z) (x=z) // true, false, false
+
+// discriminated union: it might be verbose to give an exact definition of equality,
+// but what it means is obvious :)
+
+type DUType =
+    | A of int * char
+    | B
+
+let testDUnion =
+    let x,y,z =
+        A (1,'k'),
+        A (1,'k'),
+        B
+
+    printfn "%A,%A,%A" (x=y) (y=z) (x=z) // true, false, false
+
+// we can use referential equality as well:
+[<ReferenceEquality>]
+type RefDUType =
+    | A of int * char
+    | B
+
+let testRefDUnion =
+    let x,y,z =
+        A (1,'k'),
+        A (1,'k'),
+        B
+
+    printfn "%A,%A,%A" (x=y) (y=z) (x=z) // false, false, false
 
 #quit;;
