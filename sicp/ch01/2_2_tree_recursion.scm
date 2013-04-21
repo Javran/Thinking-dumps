bf247ade238beb6db77105404afb45c8839d69aa
Javran Cheng <Javran.c@gmail.com>
2013-04-21 23:47:25 +0800

    tree recursion add explanation

---
+++ sicp/ch01/2_2_tree_recursion.scm
@@ -0,0 +1,57 @@
+(load "../common/utils.scm")
+
+; definition of Fibonacci numbers:
+; fib 0 = 0
+; fib 1 = 1
+; fib n = fib (n-1) + fib (n-2)
+
+; fibonacci
+(define (fib-1 n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        ; note fib-1 calls itself twice here
+        (else (+ (fib-1 (- n 1))
+                 (fib-1 (- n 2))))))
+; defect: redundant computation
+; to calculate fib 40, fib 39 and fib 38 is called,
+; to calculate fib 39, fib 38 and fib 37 is called,
+; ...
+; so fib 38 is called at least 2 times, each time it need to do the same task
+; when fib is applied with a big number, the time consumption might not be affordable.
+
+; calculate time difference
+(define time-test
+  (lambda (f . args)
+    (let ((start-time (real-time-clock)))
+      ; require the value immediately to force it
+      start-time
+      (apply f args)
+      (let* ((end-time (real-time-clock))
+             (diff-time (- end-time start-time)))
+        ; force it
+        diff-time
+        (display "Time elapsed: ")
+        (display diff-time)
+        (newline)
+        diff-time))))
+
+(define (print-func func testcases)
+  (out "== Print Begin ==")
+  (for-each
+    (lambda (x)
+      (display x)
+      (display ":\t")
+      (out (func x)))
+    testcases)
+  (out "== Print End =="))
+(time-test print-func fib-1 '(0 1 2 3 4 5 6 7 8 9 20))
+
+(define (fib-2 n)
+  (define (fib-iter a b count)
+    (if (= count 0)
+      b
+      (fib-iter (+ a b) a (- count 1))))
+  (fib-iter 1 ; fib 1 = 1
+            0 ; fib 0 = 0
+            n))
+(time-test print-func fib-2 '(0 1 2 3 4 5 6 7 8 9 20))
