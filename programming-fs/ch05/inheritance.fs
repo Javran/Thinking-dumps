ed408c91dde841a8c612385485f6e3b2e531d1b5
Javran Cheng <Javran.C@gmail.com>
2013-04-25 21:37:38 +0800

    ch05 - done

--- programming-fs/ch05/inheritance.fs
+++ programming-fs/ch05/inheritance.fs
@@ -54,6 +54,13 @@ type TurkeySwissSandWich() =
         ; "Swiss" ]
     override this.Calories = 330
 
+type BLTWithPickleSandWich() =
+    inherit BLTSandWich()
+
+    // use "base" to access the base class
+    override this.Ingredients = "Pickles" :: base.Ingredients
+    override this.Calories = 50 + base.Calories
+
 let inheritTest1 =
     let showSandWich (s:SandWich) =
         printfn "Ingredients: %A" s.Ingredients
@@ -62,5 +69,75 @@ let inheritTest1 =
     showSandWich <| new SandWich()
     showSandWich <| new BLTSandWich()
     showSandWich <| new TurkeySwissSandWich()
+    showSandWich <| new BLTWithPickleSandWich()
+
+// define an abstract class
+[<AbstractClass>]
+type AbstractClass() = 
+    member this.Alpha() = true
+    abstract member Bravo : unit -> bool;;
+
+// implement the abstract method
+// please refer to:
+// http://msdn.microsoft.com/en-us/library/dd483471.aspx
+type Foobar() =
+    inherit AbstractClass()
+    
+    override this.Bravo () = true;;
+
+[<Sealed>]
+type SealedClass () =
+    member this.Alpha() = true;;
+
+// static upcase: derived -> base
+[<AbstractClass>]
+type Animal () =
+    abstract member Legs : int
+
+[<AbstractClass>]
+type Dog () =
+    inherit Animal()
+
+    abstract member Description : string
+    override this.Legs = 4
+
+type Pomeranian () =
+    inherit Dog()
+    override this.Description = "Furry"
+
+let castTest =
+    let steve = new Pomeranian()
+    let steveAsDog = steve :> Dog
+    let steveAsAnimal = steve :> Animal
+    let steveAsObject = steve :> obj
+    printfn "%s,%s" (steve.Description) (steveAsDog.Description)
+    // Furry,Furry
+    // dynamic cast: cannot be checked statically by compiler, e.g. base -> derived
+    let steveAsDog1 = steveAsObject :?> Dog
+    printfn "%s,%d" (steveAsDog1.Description) (steveAsDog1.Legs)
+    // Furry, 4
+    try
+        let x = steveAsObject :?> string
+        printfn "%s" x
+    with
+    | _ as ex ->
+        printfn "Error: %s" ex.Message
+
+// pattern matching against types
+let whatIs (x : obj) =
+    match x with
+    | :? string as s ->
+        printfn "x is a string: \"%s\"" s
+    | :? int as i ->
+        printfn "x is an int %d" i
+    | :? list<int> as l ->
+        printfn "x is an int list '%A'" l
+    | _ ->
+        printfn "x is a '%s'" <| x.GetType().Name;;
+
+whatIs <| [1..5];;
+whatIs <| "Rosebud";;
+whatIs <| new System.IO.FileInfo(@"c:\config.sys");;
+whatIs <| new Pomeranian();;
 
 #quit;;
