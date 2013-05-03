1aef09bf78444675299eab4b93adfb255cae1e04
Javran Cheng <Javran.c@gmail.com>
2013-05-04 00:17:13 +0800

    ex 1.33.a - done

--- sicp/ch01/exercise_1_33.scm
+++ sicp/ch01/exercise_1_33.scm
@@ -28,6 +28,50 @@
                          acc))))
   (accumulate-iter a null-value))
 
-(out (accumulate-rec + 0 identity 1 inc 1000 (lambda (x) (<= x 100))))
-(out (accumulate-itr + 0 identity 1 inc 1000 (lambda (x) (<= x 100))))
-; 5050
+(out (accumulate-rec + 0 identity 1 inc 100 odd?))
+(out (accumulate-itr + 0 identity 1 inc 100 odd?))
+; 2500
+
+(out (accumulate-rec + 0 identity 1 inc 100 even?))
+(out (accumulate-itr + 0 identity 1 inc 100 even?))
+; 2550
+
+(out (accumulate-rec * 1 identity 1 inc 10 odd?))
+(out (accumulate-itr * 1 identity 1 inc 10 odd?))
+; 945
+
+(out (accumulate-rec * 1 identity 1 inc 10 even?))
+(out (accumulate-itr * 1 identity 1 inc 10 even?))
+; 3840
+
+(out (* 945 3840))
+; 3628800
+
+(define (smallest-divisor n)
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (define (square x) (* x x))
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n) ; impossible
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (find-divisor n 2))
+
+(define (prime? n)
+  (if (< n 2)
+    #f
+    (= n (smallest-divisor n))))
+
+(out (filter prime? (list-in-range 1 100)))
+; print primes less than 100
+
+(define (sum-of-square-of-prime-in-range a b)
+  (accumulate-itr + 0 square a inc b prime?))
+
+(out (sum-of-square-of-prime-in-range 20 500))
+
+; verification
+(out (apply + 
+            (map square 
+                 (filter prime? 
+                         (list-in-range 20 500)))))
