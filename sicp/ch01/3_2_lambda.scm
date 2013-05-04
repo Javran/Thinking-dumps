da1cce0df790c8c4d4ed71a8f32d2329c0d078a1
Javran Cheng <Javran.c@gmail.com>
2013-05-04 21:58:39 +0800

    1.3.2 - done

--- sicp/ch01/3_2_lambda.scm
+++ sicp/ch01/3_2_lambda.scm
@@ -28,3 +28,21 @@
 ; a lambda can be used any where a procedure can be used:
 (out ((lambda (x y z) (+ x y (square z))) 1 2 3))
 ; 12
+
+; let expression is interpreted as:
+; ((lambda (<var1> ... <varn>)
+;    <body>)
+;   <expr1>
+;   ...
+;   <exprn>)
+; * "let" allows one to bind variables *as locally as possible*
+; * variables' value are computed outside the "let"
+; 
+
+(let ()
+  (define x 2)
+  (out (let ((x 3)
+             (y (+ x 2)))
+         (* x y))))
+; the output will be "12" because y = x+2 is calculated outside the "let"
+;     where x is "2" rather than "3"
