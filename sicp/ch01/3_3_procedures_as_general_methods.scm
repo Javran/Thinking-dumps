3b476134fb2aca02ea242cb7fdf6ac607f5f8cb4
Javran Cheng <Javran.c@gmail.com>
2013-05-05 22:20:50 +0800

    1.3.3 - done

--- sicp/ch01/3_3_procedures_as_general_methods.scm
+++ sicp/ch01/3_3_procedures_as_general_methods.scm
@@ -30,3 +30,65 @@
   ; test finding root of f(x) = (x-1)*(x+2) in range (0..100)
   (out (search test-f neg-point pos-point)))
 ; the output should roughly be 1.0
+
+; "search" is awkward to use directly,
+;     because we might accidentally give it points
+;     at which f's value do not have the required sign
+; we can come up with a function that help us by calling "search" accordingly
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+            (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+            (search f b a))
+          (else (error "Values are not of opposite sign" a b)))))
+
+(out (half-interval-method sin 2.0 4.0))
+(out (half-interval-method sin 4.0 2.0))
+; ~= pi
+
+(let ((f (lambda (x) (- (* x x x) (* 2 x) 3))))
+  (let ((result (half-interval-method f 1.0 2.0)))
+    ; print and verify the result
+    (out result (f result))))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(out (fixed-point cos 10.0))
+; ~= 0.739 ; refer to: http://en.wikipedia.org/wiki/Fixed_point_%28mathematics%29
+
+(out (fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0))
+; ~= 1.259
+
+; "repeatedly improving a guess util the result satifies some criterion"
+
+; we can define "sqrt" in terms of fixed-point search:
+; finding the square root of x, requires finding a number y that x = y * y
+; and x = y * y => y = x / y, we are looking for a fixed point of the function y = x/y
+
+; the procedure below won't work, since y1 = x/y0, y2=x/y1=x/(x/y0)=y0
+; (define (my-sqrt x)
+;   (fixed-point (lambda (y) (/ x y)) 1.0))
+
+; TODO: "the answer is always between our guess y and x/y" why?
+
+(define (my-sqrt x)
+  (fixed-point (lambda (y) (mid y (/ x y))) 1.0))
+
+(out (my-sqrt 9))
+; ~= 3
+
+; TODO: search "average damping" for more details
