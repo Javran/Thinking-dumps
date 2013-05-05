a53b8b3024f5f65d3d69c487ec07f361505c1817
Javran Cheng <Javran.c@gmail.com>
2013-05-05 23:37:01 +0800

    ex 1.35 - done

---
+++ sicp/ch01/exercise_1_35.scm
@@ -0,0 +1,27 @@
+(load "../common/utils.scm")
+
+(define phi (/ (+ (sqrt 5) 1) 2))
+
+; (sqrt 5 + 1)/2 is a root of equation x*x - x - 1 = 0
+(out (+ (* phi phi) (- phi) (- 1)))
+
+; x*x - x - 1 = 0
+; => x - 1 - 1/x = 0
+; => x = 1 + 1/x 
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(out phi
+     (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0)
+     (fixed-point (lambda (x) (mid (+ 1 (/ 1 x)) x)) 1.0)) ; avarage damping
