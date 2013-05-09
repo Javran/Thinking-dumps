45e4e9f604a5727763c6c02613ebf99937f81ca8
Javran Cheng <Javran.c@gmail.com>
2013-05-09 22:27:02 +0800

    impl a `fast-repeated`(inspired by `fast-expt`)

--- sicp/ch01/exercise_1_43.scm
+++ sicp/ch01/exercise_1_43.scm
@@ -28,3 +28,25 @@
 
 ((repeated-2 p-with-side-effect 4) 'stub)
 ; 4 times
+(newline)
+
+; it's possible that we can achieve `repeated` in a more efficient way
+;     when using the method we've seen in `fast-expt`
+(define (fast-repeated f n)
+  (define (repeated-iter f acc counter)
+    (cond ((= counter 0)
+            acc)
+          ((odd? counter)
+            (repeated-iter f (compose f acc) (- counter 1)))
+          (else ; even
+            (repeated-iter (compose f f) acc (/ counter 2)))))
+  (repeated-iter f identity n))
+
+(let ((test (lambda (repeated-impl)
+              (out ((repeated-impl inc 10000) 0)))))
+  (time-test test repeated-1)
+  (time-test test repeated-2)
+  (time-test test fast-repeated))
+
+; TODO: we've seen that `fast-repeated` works as expected
+;     I think I've been little more closer to be able to abstract `fast-expt`, `fast-mul` ... etc. as one function
