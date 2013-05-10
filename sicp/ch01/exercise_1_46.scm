e2cf2dcf5f5537ea563734ef03f8c478b182331e
Javran Cheng <Javran.c@gmail.com>
2013-05-11 00:55:21 +0800

    ex 1.46 - fixed-point impl

--- sicp/ch01/exercise_1_46.scm
+++ sicp/ch01/exercise_1_46.scm
@@ -27,4 +27,43 @@
 (out (my-sqrt (square 1234)))
 ; ~= 1234
 
+; if v-pair is a pair, it stores (old-value . cur-value)
+; else v-pair is the init guess
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (good-enough? v-pair)
+    (if (pair? v-pair)
+      (< (abs (- (car v-pair)
+                 (cdr v-pair)))
+         tolerance)
+      ; else v-pair is the init guess, if we are lucky enough:
+      (= (f v-pair) 0)))
+
+  ; extract value from a v-pair
+  (define (extract-value v-pair)
+    (if (pair? v-pair)
+      (cdr v-pair)
+      v-pair))
+
+  (define (improve v-pair)
+    (if (pair? v-pair)
+      (cons (cdr v-pair)
+            (f (cdr v-pair)))
+      (cons v-pair
+            (f v-pair))))
+
+  (extract-value
+    ((iterative-improve good-enough? improve) 1.0)))
+
+; reimpl my-sqrt-fp using fixed-point
+(define (my-sqrt-fp x)
+  (fixed-point (average-damping
+                 (lambda (y) (/ x y)))
+               1.0))
+
+(out (my-sqrt-fp 1234))
+; ~= 35.128
+
+; TODO: further more ... reimpl ex 1.7
+
 (end-script)
