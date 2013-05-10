432522ec2a380c388cc0bc35c611c36c8f5c23e7
Javran Cheng <Javran.c@gmail.com>
2013-05-10 23:30:39 +0800

    ex 1.45 - seems it works

--- sicp/ch01/exercise_1_45.scm
+++ sicp/ch01/exercise_1_45.scm
@@ -1,31 +1,60 @@
 (load "../common/utils.scm")
 
+; actually I think this solution is far from perfect
+;     which sounds like just keep trying to use average-damping again and again
+;     until it works ... too bad to call it a solution ...
+
 (define (average-damping f)
   (lambda (x)
     (mid x (f x))))
 
 (define tolerance 0.00001)
 
-(define (fixed-point f first-guess)
+; we need a modified version that can keep a hint for how many times we've tried
+; I'd like to try a limited time and if it doesn't work, we do average-damping on f
+(define (fixed-point f first-guess allowed-try-time)
   (define (close-enough? v1 v2)
     (< (abs (- v1 v2))
        tolerance))
-  (define (try guess)
-    (let ((next (f guess)))
-      (if (close-enough? guess next)
-        next
-        (try next))))
-  (try first-guess))
+  (define (try f guess rest-try-time)
+    (if (<= rest-try-time 0)
+      ; try-time exhausted -> recharge and try again
+      (begin
+        (out "applying average-damping")
+        (try (average-damping f) 1.0 allowed-try-time))
+      (let ((next (f guess)))
+        (if (close-enough? guess next)
+          next
+          (try f next (- rest-try-time 1))))))
+  (try f first-guess allowed-try-time))
 
 (define (compose f g)
   (lambda (x) (f (g x))))
 
-(define (repeated f n)
-  (define (repeated-iter f acc counter)
-    (cond ((= counter 0)
-           acc)
-          ((odd? counter)
-           (repeated-iter f (compose f acc) (- counter 1)))
-          (else ; even
-            (repeated-iter (compose f f) acc (/ counter 2)))))
-  (repeated-iter f identity n))
+(define (repeated f time)
+  (if (= time 0) 
+    identity
+    (lambda (x)
+      (f ((repeated f (- time 1)) x)))))
+
+(out (fixed-point
+       (lambda (y) (/ 123456 (* y y y)))
+       1.0
+       16384))
+; 2 average-damping is performed
+; ~= 18.7447
+
+; nth-root: find a value y that y^n = x 
+; y^2 = x => y -> x/ y
+; y^3 = x => y -> x/(y^2)
+; y^n = x => y -> x/(y^(n-1))
+(define (nth-root x n)
+  (define (f y)
+    (/ x (expt y (- n 1))))
+  (fixed-point f 1.0 16384))
+
+(out (nth-root (expt 123.4567 23) 23))
+; 4 average-damping is performed
+
+
+(end-script)
