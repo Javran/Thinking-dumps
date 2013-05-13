9b8e413acc856d6d82d192dd07e404dcc4debb18
Javran Cheng <Javran.c@gmail.com>
2013-05-13 23:22:18 +0800

    2.1.2 - some important points

---
+++ sicp/ch02/1_2_abstraction_barriers.scm
@@ -0,0 +1,6 @@
+(load "../common/utils.scm")
+
+; * isolate different "levels" of the system
+; * procedures at each level are the interfaces
+;     that define the abstraction barriers
+;     and connect the different levels
