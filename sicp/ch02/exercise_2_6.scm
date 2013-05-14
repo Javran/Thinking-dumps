8578759b266e1573da810797fd8367506afd1bcc
Javran Cheng <Javran.c@gmail.com>
2013-05-14 23:56:12 +0800

    ex 2.6 - impl add

--- sicp/ch02/exercise_2_6.scm
+++ sicp/ch02/exercise_2_6.scm
@@ -31,8 +31,46 @@
 
 ; to verify it ... we can apply "inc" and "0"
 ;     to "convert" a Church numerals into a number:
-(out (map (lambda (c) ((c inc) 0))
+(define (to-num c) ((c inc) 0))
+
+(out (map to-num
           (list zero one two)))
 ; (0 1 2)
+
+; define "add" without the help of "add-1" ...
+; to begin with, I'd like to see what will (add-1 (add-1 n)) become:
+; now I'll express "lambda" as "\"
+; add-1 = \n.\f.\x. f (n f x)
+; add-1 n = \f.\x. f (n f x)
+; add-1 (add-1 n) = \f.\x. f ((\f.\x. f (n f x)) f x)
+;                 = \f.\x. f (        f (n f x)     )
+;                 = \f.\x. f (f (n f x))
+; add-2 = \f.\x. f (f (n f x))
+; so add-2 is just applying f again on the output of add-1
+; given that:
+; "zero" when applied with anything will become a "identity function"
+; "one" applies x to f
+; "two" applies the result of "one" to f  ...
+; we can come up with add:
+; add-m n = \f.\x. f (f (f ... (f (n f x)) ...))
+;                  ^  f*m times ^
+; replace "f" with "f1"
+; replace "n f x" with "x1"
+; we have:
+; add-m n = \f.\x. (\f1.\x1. f1 (f1 (f1 ... (f1 x1) ...))) f (n f x)
+; add-m n = \f.\x. m f (n f x)
+; to sum up:
+; add = \m.\n.\f.\x. m f (n f x)
+
+(define add
+  (lambda (m n)
+    (lambda (f)
+      (lambda (x)
+        ((m f) ((n f) x))))))
+
+(out (to-num (add one two)))
+; 3
+
+
 
 (end-script)
