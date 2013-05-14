fad6a8302badd38f8a6c5976c15bc388f571e299
Javran Cheng <Javran.C@gmail.com>
2013-05-14 19:44:40 +0800

    async lib - working

---
+++ programming-fs/ch09/aync_workflows.fs
@@ -0,0 +1,84 @@
+// note this file only works in interactive mode
+
+open System.IO
+
+let asyncProcessFile 
+        (srcFilePath : string)
+        (dstFilePath : string) 
+        (processBytes : byte [] -> byte []) =
+    async {
+        printfn "Processing file: [%s]" (Path.GetFileName(srcFilePath))
+
+        let fileStream = new FileStream(srcFilePath, FileMode.Open)
+        let bytesToRead = int fileStream.Length
+
+        let! data = fileStream.AsyncRead(bytesToRead)
+        // we need to close file as soon as we finish using it
+        fileStream.Close()
+
+        printfn "Opened [%s], read [%d] bytes" (Path.GetFileName(srcFilePath)) data.Length
+
+        let data' = processBytes data
+
+        use resultFile = new FileStream(dstFilePath, FileMode.Create)
+        do! resultFile.AsyncWrite(data',0, data'.Length)
+        // we need to close file as soon as we finish using it
+        resultFile.Close()
+
+        printfn "Finished processing file [%s]" <| Path.GetFileName(srcFilePath)
+    } |> Async.RunSynchronously;; // seems when we make it run synchronously, the output will be as expected
+
+let asyncIOTest () =
+
+    // create some file for testing
+    let testIn = "infile.tmp"
+    let testOut = "outfile.tmp"
+
+    // data processing is ignored here, so what we just do is simply making a copy of the original file
+    let processBytes b = b
+
+    File.WriteAllLines(testIn, seq {for i = 1 to 1000 do yield sprintf "%d" i} )
+
+    asyncProcessFile testIn testOut processBytes
+
+    File.Delete(testIn)
+    File.Delete(testOut)
+
+// uncomment to see the example:
+//asyncIOTest();;
+
+open System.Net
+
+let getHtml (url : string) =
+    async {
+        let req = WebRequest.Create(url)
+        let! rsp = req.AsyncGetResponse()
+
+        use stream = rsp.GetResponseStream()
+        use reader = new StreamReader(stream)
+        return reader.ReadToEndAsync().Result
+        // I guess here we call "Result" to wait for the result
+    }
+
+let testDownload () =
+    let content = 
+        getHtml "http://en.wikipedia.org/"
+        |> Async.RunSynchronously
+    printfn "Length of en.wikipedia.org: %d" content.Length
+
+    let webPages : string [] =
+        [ "http://www.google.com"
+        ; "http://www.bing.com"
+        ; "http://www.yahoo.com" ]
+        |> List.map getHtml
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> Array.map (fun x -> sprintf "Length: %d" x.Length)
+        
+    webPages;;
+        
+// uncomment to see the example
+testDownload();;
+
+
+#quit;;
