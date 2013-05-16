df656c8730c4a6aa808a627be2fac0805e051059
Javran Cheng <Javran.c@gmail.com>
2013-05-16 23:39:42 +0800

    ex 2.9 - done

--- sicp/ch02/exercise_2_9.scm
+++ sicp/ch02/exercise_2_9.scm
@@ -28,22 +28,28 @@
         (lower-bound x))
      2.0))
 
-(define iv1 (make-interval 3.9 4.1))
-(define iv2 (make-interval 0.8 1.2))
-
-(define add-res (add-interval iv1 iv2))
-(define sub-res (sub-interval iv1 iv2))
+(define iv1 (make-interval 3.9 4.1)) ; 0.1
+(define iv2 (make-interval 0.8 1.2)) ; 0.2
+(define iv3 (make-interval 100 120)) ; 10
 
 (out 
   (width-interval iv1)
   ; ~= 0.1
   (width-interval iv2)
   ; ~= 0.2
-  (width-interval add-res)
+  (width-interval (add-interval iv1 iv2))
   ; ~= 0.3
-  (width-interval sub-res)
+  (width-interval (sub-interval iv1 iv2))
   ; ~= 0.3
+  (width-interval (add-interval iv3 iv2))
+  ; = 10 + 0.2 ~= 10.2 ; the range expands
+  (width-interval (sub-interval iv3 iv1))
+  ; = 10 + 0.1 ~= 10.1
+  ""
+  (width-interval (mul-interval iv1 iv2))
+  (width-interval (div-interval iv1 iv2))
+  (width-interval (mul-interval iv1 iv3))
+  (width-interval (div-interval iv2 iv3))
   )
-
 
 (end-script)
