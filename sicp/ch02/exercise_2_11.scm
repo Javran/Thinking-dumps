d6b5c031e03a479d9e1ab25dc6663adf3a5ac226
Javran Cheng <Javran.c@gmail.com>
2013-05-18 00:22:19 +0800

    ex 2.11 - test passed

--- sicp/ch02/exercise_2_11.scm
+++ sicp/ch02/exercise_2_11.scm
@@ -33,18 +33,18 @@
         (lb2 (lower-bound y))
         (ub2 (upper-bound y)))
     (cond 
-      ((<= ub1 0)
+      ((< ub1 0)
         (cond
-          ((<= ub2 0)
-            ; case #1: inv 1 <= 0, inv 2 <= 0
+          ((< ub2 0)
+            ; case #1: inv 1 < 0, inv 2 < 0
             ; [inv 1]
             ; ----------0---------->
             ; [inv 2]
             ; result: [ub1*ub2 - lb1*lb2]
             (make-interval (* ub1 ub2) (* lb1 lb2)))
 
-          ((>= lb2 0)
-            ; case #3: inv 1 <= 0, inv 2 >= 0
+          ((> lb2 0)
+            ; case #3: inv 1 < 0, inv 2 > 0
             ; [inv 1]
             ; ----------0---------->
             ;                [inv 2]
@@ -52,24 +52,24 @@
             (make-interval (* lb1 ub2) (* ub1 lb2)))
             
           (else 
-            ; case #2: inv 1 <= 0, inv 2 spans 0
+            ; case #2: inv 1 < 0, inv 2 spans 0
             ; [inv 1]
             ; ----------0---------->
             ;        [inv 2]
             ; result: [lb1*ub2 - lb1*lb2]
             (make-interval (* lb1 ub2) (* lb1 lb2)))))
 
-      ((>= lb1 0)
+      ((> lb1 0)
         (cond
-          ((<= ub2 0)
+          ((< ub2 0)
             ; case #7:
             ;                [inv 1]
             ; ----------0---------->
             ; [inv 2]
-            ; result: [lb1*ub2 - ub1*lb2]
-            (make-interval (* lb1 ub2) (* ub1 lb2)))
+            ; result: [ub1*lb2 - lb1*ub2]
+            (make-interval (* ub1 lb2) (* lb1 ub2)))
 
-          ((>= lb2 0)
+          ((> lb2 0)
             ; case #9:
             ;                [inv 1]
             ; ----------0---------->
@@ -87,7 +87,7 @@
 
       (else
         (cond
-          ((<= ub2 0)
+          ((< ub2 0)
             ; case #4:
             ;        [inv 1]
             ; ----------0---------->
@@ -95,7 +95,7 @@
             ; result: [ub1*lb2 - lb1*lb2]
             (make-interval (* ub1 lb2) (* lb1 lb2)))
 
-          ((>= lb2 0)
+          ((> lb2 0)
             ; case #6:
             ;        [inv 1]
             ; ----------0---------->
@@ -125,31 +125,54 @@
 
 ; should have same output for each case
 
-; pick up nums in [-4 - 4], make intervals to test
+; pick up nums in [-9 - 9], make intervals to test
 (define (mul-test)
   (define (interval-eq? a b)
     (and (= (lower-bound a) (lower-bound b))
          (= (upper-bound a) (upper-bound b))))
 
-  (let loop-i1 ((i1 -4))
-    (if (<= i1 4)
-      (let loop-j1 ((j1 i1))
-        (if (<= j1 4)
-          ; make interval-1
-          (let ((iv1 (make-interval i1 j1)))
-            (let loop-i2 ((i2 -4))
-              (if (<= i2 4)
-                (let loop-j2 ((j2 i2))
-                  (if (<= j2 4)
-                    ; make interval-2
-                    (let ((iv2 (make-interval i2 j2)))
-                      (if (interval-eq? (mul-interval-1 iv1 iv2)
-                                        (mul-interval-2 iv1 iv2))
-                        ; test passed, next j
-                        (loop-j2 (+ j2 1))
-                        ; else
-                        (error "result mismatch"))))
-                  (loop-i2 (+ i2 1)))))
-            (loop-j1 (+ j1 1))))
-        (loop-i1 (+ i1 1))))))
-(mul-test)
+  ; code here might be confusing ... 
+  ; because loop & i & j are masked in inner scopes
+  ; loop of 1st i
+  (let loop ((i -9))
+    (if (<= i 9)
+      (begin
+        ; loop of 1st j
+        (let loop ((j i))
+          (if (<= j 9)
+            ; make interval-1
+            (begin
+              (let ((iv1 (make-interval i j)))
+                ; loop of 2nd i
+                (let loop ((i -9))
+                  (if (<= i 9)
+                    (begin
+                      ; loop of 2nd j
+                      (let loop ((j i))
+                        (if (<= j 9)
+                          ; make interval-2
+                          (let ((iv2 (make-interval i j)))
+                            (if (interval-eq? (mul-interval-1 iv1 iv2)
+                                              (mul-interval-2 iv1 iv2))
+                              ; next of 2nd j
+                              (loop (+ j 1))
+                              ; else
+                              (begin
+                                (out "error occurred:")
+                                (display "iv1: ")
+                                (print-interval iv1)
+                                (display "iv2: ")
+                                (print-interval iv2)
+                                (out "results:")
+                                (print-interval (mul-interval-1 iv1 iv2))
+                                (print-interval (mul-interval-2 iv1 iv2))
+                                (error "result mismatch"))))))
+                      ; next of 2nd i
+                      (loop (+ i 1))))))
+              ; next of 1st j
+              (loop (+ j 1)))))
+        ; next of 1st i
+        (loop (+ i 1))))))
+
+; uncomment the next line for tests
+; (mul-test)
