0eb924c896a26d707a65ef42379e090e0010a838
Javran Cheng <Javran.c@gmail.com>
2013-05-18 22:55:07 +0800

    ex 2.12 - done

--- sicp/ch02/exercise_2_12.scm
+++ sicp/ch02/exercise_2_12.scm
@@ -6,6 +6,15 @@
 (define lower-bound car)
 (define upper-bound cdr)
 
+; pretty-print
+(define (print-interval a)
+  (display "[")
+  (display (lower-bound a))
+  (display "-")
+  (display (upper-bound a))
+  (display "]")
+  (newline))
+
 (define (make-center-width c w)
   (make-interval (- c w) (+ c w)))
 
@@ -20,4 +29,19 @@
 
 (print-interval (make-center-width 3.5 0.15)) ; [3.35-3.65]
 
+; uncertainty(percentage) = width / center
+; => width = center * uncertainty(percentage)
+(define (make-center-percent c p)
+  (make-center-width c (* c p)))
 
+(define (percent x)
+  (/ (width x)
+     (center x)))
+
+(define test-cp (make-center-percent 3.5 0.005))
+(print-interval test-cp)
+; [3.4825-3.5175]
+(out (width test-cp)   ; ~= 0.0175
+     (center test-cp)  ; ~= 3.5
+     (percent test-cp) ; ~= 0.005
+     )
