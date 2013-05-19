f8d037b433b3e6162252e13170f6ca8ceeee4829
Javran Cheng <Javran.c@gmail.com>
2013-05-20 00:09:44 +0800

    2.2.1 - done

--- sicp/ch02/2_1_representing_seq.scm
+++ sicp/ch02/2_1_representing_seq.scm
@@ -36,4 +36,18 @@
      ; 16
      )
 
+(define (my-length items)
+  (if (null? items)
+    0
+    (+ 1 (my-length (cdr items)))))
+
+(out (my-length squares)) ; 5
+
+(define (my-append list1 list2)
+  (if (null? list1)
+    list2
+    (cons (car list1) (my-append (cdr list1) list2))))
+
+(out (my-append '(1 2 3 4) '(5 6 7 8 9)))
+
 (end-script)
