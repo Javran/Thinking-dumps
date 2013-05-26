33dc7da45b668399e94cb8cabac30ab51b6c0f03
Javran Cheng <Javran.c@gmail.com>
2013-05-26 21:21:14 +0800

    ex 2.30 - done

--- sicp/ch02/exercise_2_30.scm
+++ sicp/ch02/exercise_2_30.scm
@@ -1,11 +1,19 @@
 (load "../common/utils.scm")
 
-(define (square-tree tree)
+(define (square-tree-1 tree)
   (map (lambda (sub-tree)
          (if (list? sub-tree)
-           (square-tree sub-tree)
+           (square-tree-1 sub-tree)
            (square sub-tree)))
        tree))
+
+(define (square-tree-2 tree)
+  (if (list? tree)
+    (if (null? tree)
+      nil
+      (cons (square-tree-2 (car tree))
+            (square-tree-2 (cdr tree))))
+    (square tree)))
 
 (define x
   (list 1
@@ -14,7 +22,8 @@
 
 (out x
      ; (1 (2 (3 4) 5) (6 7))
-     (square-tree x)
+     (square-tree-1 x)
+     (square-tree-2 x)
      ; (1 (4 (9 16) 25) (36 49))
      )
 
