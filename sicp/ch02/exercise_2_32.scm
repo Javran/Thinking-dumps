1a9f283fd0e13e3238130b7b8394fae240ac698b
Javran Cheng <Javran.c@gmail.com>
2013-05-27 22:59:03 +0800

    ex 2.32 - done

--- sicp/ch02/exercise_2_32.scm
+++ sicp/ch02/exercise_2_32.scm
@@ -7,11 +7,16 @@
     (list nil)
     ; else
     (let ((rest (subsets (cdr s))))
-      (append rest
-              (map (lambda (x) (cons (car s) x))
+      ; two situations are taken into account
+      ;     very similar to what we've done in example "counting change"
+      (append 
+          ; we ignore the first element and calculate the subset of rest elements
+          rest
+          ; and now we have to consider the first element:
+          ;     simply `cons` the first element into every list contains in the list `rest`
+          (map (lambda (x) (cons (car s) x))
                    rest)))))
 
 (out (subsets (list 1 2 3)))
-
 
 (end-script)
