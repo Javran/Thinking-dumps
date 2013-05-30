e89f8a3ac8c387d88c54a44d8147cbdf59c7d11d
Javran Cheng <Javran.c@gmail.com>
2013-05-30 23:41:28 +0800

    ex 2.37 - done

--- sicp/ch02/exercise_2_37.scm
+++ sicp/ch02/exercise_2_37.scm
@@ -58,4 +58,35 @@
   (out (transpose x))
   (out (transpose (transpose x))))
 
+; matrix * matrix
+;                / a b \
+; / A B C D \  x | c d |
+; \ E F G H /    | e f |
+; (m)            \ g h /
+; cols:
+; / a c e g \
+; \ b d f h /
+;
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    ; this `map` turns each row of `m` into each row of the result
+    ;     so the lambda passed to `map` should have each col of `n` reduced to a number
+    (map (lambda (m-row)
+           (map (lambda (col)
+                  (dot-product m-row col))
+                cols))
+         m)))
+
+(let ((x (list (list 1 2 3 4)
+               (list 5 6 7 8)))
+      (y (list (list 1 2)
+               (list 2 3)
+               (list 3 4)
+               (list 4 5))))
+  (out (matrix-*-matrix x y))
+  ; ((30 40) (70 96))
+  (out (matrix-*-matrix y x))
+  ; ((11 14 17 20) (17 22 27 32) (23 30 37 44) (29 38 47 56))
+  )
+
 (end-script)
