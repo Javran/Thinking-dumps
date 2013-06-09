1f9e2292e525f2237621df5f67e883c9550e8c6b
Javran Cheng <Javran.c@gmail.com>
2013-06-09 22:42:53 +0800

    attempt to simplify the result

--- sicp/ch02/3_2_replesent_algebra_expr.scm
+++ sicp/ch02/3_2_replesent_algebra_expr.scm
@@ -30,9 +30,41 @@
 ; multiplicand: third
 (define (multiplicand p) (caddr p))
 
-(out (deriv '(+ x 3) 'x)
-     (deriv '(* x y) 'x)
-     (deriv '(* (* x y) (+ x 3)) 'x))
+(define (do-test)
+  (out (deriv '(+ x 3) 'x)
+       (deriv '(* x y) 'x)
+       (deriv '(* (* x y) (+ x 3)) 'x)))
 
+(do-test)
+
+; redefine make-sum and make-product hoping to simplify the result
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0)
+             (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(newline)
+(do-test)
+
+(newline)
+(out (deriv 'x 'x)
+     ; 1
+     (deriv '(* x x) 'x)
+     ; = (* x 2)
+     (deriv '(* (* x x) x) 'x)
+     ; = (* 3 (* x x))
+     )
 
 (end-script)
