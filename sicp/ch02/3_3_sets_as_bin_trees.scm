61fe9e216c5d88f6d3e8cbcf18ac7fca20c59b7a
Javran Cheng <Javran.c@gmail.com>
2013-06-19 00:21:32 +0800

    2.3.3 sets as bin tree - done

--- sicp/ch02/3_3_sets_as_bin_trees.scm
+++ sicp/ch02/3_3_sets_as_bin_trees.scm
@@ -30,7 +30,30 @@
            (element-of-set? x set))
          (list-in-range 1 4))))
   ; (#t #t #t #f)
+(newline)
 
+(define (adjoin-set x set)
+  (cond ((null? set)
+          (make-tree x nil nil))
+        ((= x (entry set))
+          set)
+        ((< x (entry set))
+          (make-tree (entry set)
+                     (adjoin-set x (left-branch set))
+                     (right-branch set)))
+        ((> x (entry set))
+          (make-tree (entry set)
+                     (left-branch set)
+                     (adjoin-set x (right-branch set))))))
 
+; make a full bin tree in range 3-9
+(let ((s (fold-right adjoin-set nil (reverse '(6 4 8 3 7 5 9)))))
+  (out s)
+  (out (map
+         (lambda (x) (element-of-set? x s))
+         (list-in-range 1 10))))
+  ; #f for first two and the last element
+
+; TODO: impl B-trees / red-black trees if possible?
 
 (end-script)
