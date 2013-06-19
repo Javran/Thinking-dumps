060cd84e210d907f03e57486339501e221ad95f9
Javran Cheng <Javran.c@gmail.com>
2013-06-19 23:45:16 +0800

    ex 2.63 - example code

--- sicp/ch02/exercise_2_63.scm
+++ sicp/ch02/exercise_2_63.scm
@@ -14,6 +14,28 @@
 (define left-branch cadr)
 (define right-branch caddr)
 
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1 (right-branch tree))))))
 
+(define tree-123 (make-tree 2 (make-tree 1 nil nil)
+                              (make-tree 3 nil nil)))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                    (cons (entry tree)
+                          (copy-to-list (right-branch tree)
+                                        result-list)))))
+  (copy-to-list tree nil))
+
+(out (tree->list-1 tree-123))
+(out (tree->list-2 tree-123))
+; (1 2 3)
 
 (end-script)
