0116fd147e955371b9fea0db7d7065a06f067330
Javran Cheng <Javran.c@gmail.com>
2013-06-26 00:40:02 +0800

    2.3.3 sets -> impl lookup totally

--- sicp/ch02/3_3_sets_and_info_retrieval.scm
+++ sicp/ch02/3_3_sets_and_info_retrieval.scm
@@ -6,4 +6,26 @@
           (car set-of-records))
         (else (lookup given-key (cdr set-of-records)))))
 
+; extra task: implement 'lookup' for unordered list
+
+(define make-entry cons)
+(define key car)
+(define value cdr)
+
+(let ((records '((1 . one)
+                 (2 . two)
+                 (3 . three)
+                 (4 . four)
+                 (5 . five)
+                 (6 . six))))
+  ; lookup elements and display the result
+  (for-each
+    (lambda (entry)
+      (if entry
+        (out (value entry))
+        (out "<No record>")))
+    (map
+      (lambda (k) (lookup k records))
+      (list-in-range 1 10))))
+
 (end-script)
