3920ab78111430e349185a4bc438deeb065b4ebf
Javran Cheng <Javran.c@gmail.com>
2013-06-27 22:44:12 +0800

    2.3.4 - procedures for leaves and trees

--- sicp/ch02/3_4_representing_huffman_trees.scm
+++ sicp/ch02/3_4_representing_huffman_trees.scm
@@ -1,11 +1,18 @@
 (load "../common/utils.scm")
 
 (define (make-leaf symbol weight)
+  ; * signature `leaf` indicating data type 
+  ; * the symbol of the leaf
+  ; * weight
   (list 'leaf symbol weight))
+
+; the first element of the list is used as a hint of data structure
 (define (leaf? object)
   (eq? (car object) 'leaf))
   
-(define (symbol-leaf x) (cadr x))
-(define (weight-leaf x) (caddr x))
+; getters 
+(define symbol-leaf cadr)
+(define weight-leaf caddr)
+
 
 (end-script)
