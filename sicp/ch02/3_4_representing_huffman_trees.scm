2a07b502f3a1625e4376ae53c8a63e03af9b83ab
Javran Cheng <Javran.c@gmail.com>
2013-06-27 22:53:12 +0800

    2.3.4 - made an example to test procedures

--- sicp/ch02/3_4_representing_huffman_trees.scm
+++ sicp/ch02/3_4_representing_huffman_trees.scm
@@ -10,9 +10,51 @@
 (define (leaf? object)
   (eq? (car object) 'leaf))
   
-; getters 
+; getters
 (define symbol-leaf cadr)
 (define weight-leaf caddr)
 
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left)
+                (symbols right))
+        (+ (weight left)
+           (weight right))))
+
+(define left-branch car)
+(define right-branch cadr)
+
+(define symbols-code-tree caddr)
+(define weight-code-tree cadddr)
+
+; `symbols` and `weight` will have different behavior accordingly
+;   to the argument given
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (symbols-code-tree tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (weight-code-tree tree)))
+
+; => ((a (c d)) (b e)): 15
+
+; let's make a tree: a:3 b:5 c:2 d:1 e:4
+(let* ((ta (make-leaf 'a 3))
+       (tb (make-leaf 'b 5))
+       (tc (make-leaf 'c 2))
+       (td (make-leaf 'd 1))
+       (te (make-leaf 'e 4))
+       (tree-1 (make-code-tree tc td))
+       ; => a:3 b:5 (c d):3 e:4
+       (tree-2 (make-code-tree ta tree-1))
+       ; => (a (c d)):6 b:5 e:4
+       (tree-3 (make-code-tree tb te))
+       ; => (a (c d)):6 (b e):9
+       (tree-4 (make-code-tree tree-2 tree-3)))
+  (out tree-4))
 
 (end-script)
