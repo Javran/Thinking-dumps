18466fa0b871d0771961a904dbd3c8365283e8f8
Javran Cheng <Javran.c@gmail.com>
2013-07-01 22:40:30 +0800

    ex 2.71 - done

--- sicp/ch02/exercise_2_71.scm
+++ sicp/ch02/exercise_2_71.scm
@@ -3,6 +3,10 @@
 ; relative frequency: 1, 2, 4, 8 ...
 ; for n = 5, the tree is like:
 ; ((((a:1 b:2):3 c:4):7 d:8):15 e:16):31
-; ...
+; for n = 10, the tree is like:
+; ((((( ((((a:1 b:2):3 c:4):7 d:8):15 e:16):31
+;   f:32):63 g:64):127 h:128):255 i:256):511 j:512):1023
+; the most frequent symbol needs only 1 bit to encode
+; whereas the least frequent symbol needs (n-1) bits to encode
 
 (end-script)
