61519ae213c5e4045d69e5c4eee777af4554007f
Javran Cheng <Javran.c@gmail.com>
2013-07-02 21:25:13 +0800

    back to 2.2.4 - picture language

---
+++ sicp/ch02/piclang_template.rkt
@@ -0,0 +1,11 @@
+#lang racket
+(require (planet soegaard/sicp:2:1/sicp))
+
+(define (p->file painter filename)
+  (send (send (paint painter) get-bitmap)
+        save-file
+        (string-append "./pic_out/" filename ".png")
+        'png))
+
+(p->file einstein "einstein")
+(p->file diagonal-shading "ds")
