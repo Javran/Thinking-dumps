c8140475e40db6bdfc0bd3c3a8a86872c6858a2a
Javran Cheng <Javran.c@gmail.com>
2013-07-02 21:57:51 +0800

    before ex 2.44 - done

--- sicp/ch02/2_4_the_pic_lang.rkt
+++ sicp/ch02/2_4_the_pic_lang.rkt
@@ -19,3 +19,40 @@
 
 (p->file (flipped-pairs einstein4)
          "2_4_einstein16")
+
+(define (right-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (right-split painter (- n 1))))
+      (beside painter (below smaller smaller)))))
+
+(p->file (right-split einstein 4)
+         "2_4_einstein_rsplit")
+
+(define (up-split painter n)
+  (if (= n 0)
+    painter
+    (let ((smaller (up-split painter (- n 1))))
+      (below painter (beside smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+    painter
+    (let* ((up (up-split painter (- n 1)))
+           (rt (right-split painter (- n 1)))
+           (top-left (beside up up))
+           (bottom-right (below rt rt))
+           (corner (corner-split painter (- n 1))))
+      (beside (below painter top-left)
+              (below bottom-right corner)))))
+
+(p->file (corner-split einstein 4)
+         "2_4_einstein_csplit")
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
+
+(p->file (square-limit einstein 4)
+         "2_4_einstein_slimit")
