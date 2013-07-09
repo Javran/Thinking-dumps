87f17872a1afd6ffec7072c63824467fd87c279d
Javran Cheng <Javran.c@gmail.com>
2013-07-09 23:24:03 +0800

    ex 2.51 - slight change on test & related proc

--- sicp/ch02/exercise_2_51.rkt
+++ sicp/ch02/exercise_2_51.rkt
@@ -21,25 +21,56 @@
 ; below1: analogous to the beside
 
 (define (below1 painter1 painter2)
-  (let ((split-point (make-vect 0 0.5)))
-    (let ((paint-down
-            (transform-painter1
-              painter1
-              (make-vect 0 0)
-              (make-vect 1 0)
-              split-point))
-          (paint-up
-            (transform-painter1
-              painter2
-              split-point
-              (make-vect 1 0.5)
-              (make-vect 0 1))))
-      (lambda (frame)
-        (paint-down frame)
-        (paint-up frame)))))
+  (let* ((split-point (make-vect 0 0.5))
+         (paint-down
+           (transform-painter1
+             painter1
+             (make-vect 0 0)
+             (make-vect 1 0)
+             split-point))
+         (paint-up
+           (transform-painter1
+             painter2
+             split-point
+             (make-vect 1 0.5)
+             (make-vect 0 1))))
+    (lambda (frame)
+      (paint-down frame)
+      (paint-up frame))))
 
 ; the upper would have two painters piled
-(p->file (below1 einstein (below1 einstein einstein))
+(p->file (below1 einstein (beside einstein einstein))
          "ex_2_51_einstein_below1")
 
 ; below2: in terms of beside and suitable rotation operations
+(define (rotate-cc-270 painter)
+  (transform-painter1
+    painter
+    (make-vect 0 1)
+    (make-vect 0 0)
+    (make-vect 1 1)))
+
+(define (rotate-cc-90 painter)
+  (transform-painter1
+    painter
+    (make-vect 1 0)
+    (make-vect 1 1)
+    (make-vect 0 0)))
+
+(define (beside1 painter1 painter2)
+  (let* ((split-point (make-vect 0.5 0))
+         (paint-left
+           (transform-painter1
+             painter1
+             (make-vect 0 0)
+             split-point
+             (make-vect 0 1)))
+         (paint-right
+           (transform-painter1
+             painter2
+             split-point
+             (make-vect 1 0)
+             (make-vect 0.5 1))))
+    (lambda (frame)
+      (paint-left frame)
+      (paint-right frame))))
