787ef04f620b7598c4cda14fb473d105cc24ce47
Javran Cheng <Javran.c@gmail.com>
2013-07-13 23:42:39 +0800

    2.4.3 - idea of data directed programming

---
+++ sicp/ch02/4_3_data_directed.scm
@@ -0,0 +1,12 @@
+(load "../common/utils.scm")
+
+; dispatching type: 
+;   checking the type of a datum and calling an appropriate procedure
+
+; data-directed programming:
+;   deal with a two dimension table:
+;   * one axis for possible operations
+;   * one axis for possible types
+; to add a new representation doesn't need to change any existing procedures
+
+(end-script)
