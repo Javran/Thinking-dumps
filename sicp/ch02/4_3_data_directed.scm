d56e83b4fa0a4747afaa7d7a11b3e391937998d1
Javran Cheng <Javran.c@gmail.com>
2013-07-14 23:30:50 +0800

    apply-generic

--- sicp/ch02/4_3_data_directed.scm
+++ sicp/ch02/4_3_data_directed.scm
@@ -21,6 +21,15 @@
 
 (load "./4_3_data_directed_put_get.scm")
 
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (error
+          "No method for these types: APPLY-GENERIC"
+          (list op type-tags))))))
+
 (load "./4_3_data_directed_ben_impl.scm")
 (load "./4_3_data_directed_aly_impl.scm")
 
