35987d38f4c98b60ef2e41109e4ee29a5b6f0783
Javran Cheng <Javran.c@gmail.com>
2013-07-14 23:50:18 +0800

    2.4.3 - tests from 2.4.2

--- sicp/ch02/4_3_data_directed.scm
+++ sicp/ch02/4_3_data_directed.scm
@@ -53,4 +53,48 @@
   (get 'make-from-mag-ang 'polar))
 (newline)
 
+; excerpted tests from "./4_2_tagged_data.scm"
+(define (add-complex z1 z2)
+  (make-from-real-imag (+ (real-part z1) (real-part z2))
+                       (+ (imag-part z1) (imag-part z2))))
+
+(define (sub-complex z1 z2)
+  (make-from-mag-ang (- (real-part z1) (real-part z2))
+                     (- (imag-part z1) (imag-part z2))))
+
+(define (mul-complex z1 z2)
+  (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                     (+ (angle z1) (angle z2))))
+
+(define (div-complex z1 z2)
+  (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                     (- (angle z1) (angle z2))))
+
+(let ((c1 (make-from-real-imag 3 4))
+      (c2 (make-from-mag-ang 3 4)))
+  (out c1 ; (rect 3 . 4)
+       c2 ; (polar 3 . 4)
+       (magnitude c1) ; 5
+       (magnitude c2) ; 3
+       ))
+
+(define (print-complex z)
+  (display (real-part z))
+  (display "+ ")
+  (display (imag-part z))
+  (display "i\n"))
+
+(newline)
+(let ((c1 (make-from-mag-ang (sqrt 2) (* pi (/ 3 4))))
+      (c2 (make-from-real-imag 1 1)))
+  ; c1 = -1+i
+  ; c2 = 1+i
+  (print-complex c1)
+  (print-complex c2)
+  (print-complex (add-complex c1 c2)) ;  0+2i
+  (print-complex (sub-complex c1 c2)) ; -2+0i
+  (print-complex (mul-complex c1 c2)) ; -2+0i
+  (print-complex (div-complex c1 c2)) ;  0+1i
+  )
+
 (end-script)
