504cd96058df5bca7911d342dc1f7be41d909ab9
Javran Cheng <Javran.c@gmail.com>
2013-07-16 23:49:14 +0800

    ex 2.74 - my plan

--- sicp/ch02/exercise_2_74.scm
+++ sicp/ch02/exercise_2_74.scm
@@ -7,4 +7,14 @@
 ;     * address
 ;     * salary
 
+; each record will be stored as an s-expr
+;   and put into a single file for each division
+;   so that the I/O and file parsing is omitted here to simplify the code
+; I'll design 3 different data structures for 3 indenpendent divisions
+;   and then implement procedures required to demonstrate the ability of
+;   data-directed programming
+
+; furthermore, I assume the name is unique to all the employees represented.
+;   so we don't need to care about the name confliction 
+
 (end-script)
