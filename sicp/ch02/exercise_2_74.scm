0200534591bce669aba61351a7ced887e05741a7
Javran Cheng <Javran.c@gmail.com>
2013-07-16 23:44:11 +0800

    ex 2.74 - summarize the requirement

---
+++ sicp/ch02/exercise_2_74.scm
@@ -0,0 +1,10 @@
+(load "../common/utils.scm")
+
+; * one record file for each division
+; * keyed on employees' names
+; * each employees' record is a set (I think it's actually a dict)
+;   * according to the requirement, we should at least have these fields:
+;     * address
+;     * salary
+
+(end-script)
