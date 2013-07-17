970789f9282e93a4ec886b504d74f9fd91a9aac0
Javran Cheng <Javran.c@gmail.com>
2013-07-17 23:45:00 +0800

    2.4.3 - msg passing

---
+++ sicp/ch02/4_3_message_passing.scm
@@ -0,0 +1,22 @@
+(load "../common/utils.scm")
+
+; pass operations to the object
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle)
+           (atan y x))
+          (else (error "Unknown op: MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
+(let ((x (make-from-real-imag 3 4)))
+  (out (x 'real-part)
+       (x 'imag-part)
+       (x 'magnitude)
+       (x 'angle)
+       ))
+
+(end-script)
