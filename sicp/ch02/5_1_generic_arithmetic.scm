88448398561de6b3110d98fa4cb34777ffa8eae9
Javran Cheng <Javran.c@gmail.com>
2013-07-19 21:08:24 +0800

    2.5.1 - impl scheme-number

--- sicp/ch02/5_1_generic_arithmetic.scm
+++ sicp/ch02/5_1_generic_arithmetic.scm
@@ -1,6 +1,6 @@
 (load "../common/utils.scm")
 
-; basic supports from previous codes
+; basic supports from previous code
 (load "./4_3_data_directed_put_get.scm")
 
 (define (apply-generic op . args)
@@ -11,5 +11,30 @@
         (error
           "No method for these types: APPLY-GENERIC"
           (list op type-tags))))))
+
+; generic arithmetic procedures:
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+; TODO: we don't actually need to give the implementation of `sub` and `div`
+;   and if we can find the negative of any valid number x, say `(neg x)`
+;   then we can have (define (sub x y) (add x (neg y))).
+;   Similarly, we can have `div` redefined.
+
+(load "./5_1_generic_arithmetic_scheme_number.scm")
+(install-scheme-number-package)
+(define make-scheme-number
+  (get 'make 'scheme-number))
+
+; test scheme-number
+(let ((a (make-scheme-number 20))
+      (b (make-scheme-number 10)))
+  (out (add a b)
+       (sub a b)
+       (mul a b)
+       (div a b)))
+(newline)
 
 (end-script)
---
+++ sicp/ch02/5_1_generic_arithmetic_scheme_number.scm
@@ -0,0 +1,16 @@
+(define (install-scheme-number-package)
+  (define (tag x)
+    ; use symbol `scheme-number` as tag
+    (attach-tag 'scheme-number x))
+
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+
+  (put 'make 'scheme-number (lambda (x) (tag x)))
+  'done)
