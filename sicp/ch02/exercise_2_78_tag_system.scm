239908e1f34a8f71afce04a962be4b8e812bb507
Javran Cheng <Javran.c@gmail.com>
2013-07-21 19:04:40 +0800

    ex 2.78 - modified type-tag/contents/attach-tag

---
+++ sicp/ch02/exercise_2_78_tag_system.scm
@@ -0,0 +1,12 @@
+(define (attach-tag tag content)
+  (if (and (symbol? tag) (equal? tag 'scheme-number))
+    content
+    (cons tag content)))
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else (error "Bad tagged datum: TYPE-TAG" datum))))
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else (error "Bad tagged datum: CONTENTS" datum))))
