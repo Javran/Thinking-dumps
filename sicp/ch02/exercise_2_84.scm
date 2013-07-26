986cb28784281ec0a5b68311643bdbb9d0bb9ffa
Javran Cheng <Javran.c@gmail.com>
2013-07-26 21:32:50 +0800

    ex 2.84 - type comparison

--- sicp/ch02/exercise_2_84.scm
+++ sicp/ch02/exercise_2_84.scm
@@ -10,4 +10,37 @@
 ;   * keep raising if we cannot find a desired procedure
 ;   * abort if we've reached the highest level and find no result
 
+(define (higher-type? a b)
+  ; the type tower, higher types are always placed in front of lower types
+  (define type-tower
+    '(complex real rational integer))
+
+  ; find index of a symbol
+  (define (find-index x ls)
+    (let ((tail (member x ls)))
+      (if tail
+        (- (length ls) (length tail))
+        #f)))
+
+  ; comparison
+  (< (find-index a type-tower)
+     (find-index b type-tower)))
+
+(define (test-type-cmp)
+  (define (test testcase)
+    (if (not (equal? (apply higher-type? (car testcase))
+                     (cdr testcase)))
+      (error "Test failed")))
+  (for-each 
+    test
+    '(((complex real)   . #t)
+      ((real real)      . #f)
+      ((real rational)  . #t)
+      ((rational real)  . #f)
+      ((real integer)   . #t)
+      )))
+
+; uncomment next line to see the test
+; (test-type-cmp)
+
 (end-script)
