1d44e3bc714c58162649a0ac29c91f725f9d6b34
Javran Cheng <Javran.c@gmail.com>
2013-07-27 22:51:21 +0800

    ex 2.84 - "raise type to" & "the highest type"

--- sicp/ch02/exercise_2_84.scm
+++ sicp/ch02/exercise_2_84.scm
@@ -7,8 +7,7 @@
 ;   * get a list of types
 ;   * pick up the highest type in the type list
 ;   * every argument needs to be raised to be of that type
-;   * keep raising if we cannot find a desired procedure
-;   * abort if we've reached the highest level and find no result
+;   * we don't need to raise beyond the highest one
 
 (load "./4_3_data_directed_put_get.scm")
 (load "./exercise_2_83_num_all.scm")
@@ -16,5 +15,58 @@
 ; type comparison
 (load "./exercise_2_84_type_cmp.scm")
 
+; installation
+(define (add a b)
+  (apply-generic 'add a b))
+
+(define (sub a b)
+  (apply-generic 'sub a b))
+
+(define (raise x)
+  (apply-generic 'raise x))
+
+(install-integer-package)
+(install-rational-package)
+(install-real-package)
+(install-complex-package)
+
+(define make-integer (get 'make 'integer))
+(define make-rational (get 'make 'rational))
+(define make-real (get 'make 'real))
+(define make-complex (get 'make 'complex))
+
+; raise to a higher type for data (currying)
+(define (raise-to type)
+  (lambda (data)
+    (let ((data-type (type-tag data)))
+      (cond ((equal? type data-type) data)
+            ((higher-type? type data-type)
+             ((raise-to type) (raise data)))
+            (else (error "RAISE-TO: no way to raise downsides"))))))
+
+; pick up the highest type in the list
+(define (highest-type ls)
+  (fold-left
+    (lambda (cur-highest cur-type)
+      (if (higher-type? cur-type cur-highest)
+        cur-type
+        cur-highest))
+    (car ls)
+    (cdr ls)))
+
+(define (apply-generic op . args)
+  (define (raise-and-apply op type-list data-list)
+    #f)
+
+  (let ((type-list (map type-tag args))
+        (data-list (map contents args)))
+    (let ((proc (get op type-list)))
+      (if proc
+        (apply proc data-list)
+        ; else try to raise all types and look for a procedure again
+        (raise-and-apply op type-list data-list)))))
+
+(out ((raise-to 'rational) (make-integer 30)))
+(out (highest-type '(rational integer real)))
 
 (end-script)
