20b337ac84e850a037ad8fe31305bc15ca66d0ac
Javran Cheng <Javran.c@gmail.com>
2013-07-30 23:41:51 +0800

    ex 2.86 - comments

---
+++ sicp/ch02/exercise_2_86.scm
@@ -0,0 +1,10 @@
+(load "../common/utils.scm")
+
+; changes to the system needed to accommodate this:
+; * modify constructors to prevent building a complex number
+;   from complex numbers
+; * use `add` `sub` ... instead of `+` `-` to deal with mathmatical operations
+; * insert `sine` and `cosine` to take the place of `sin` and `cos`
+; * (optional) implement `print-num` to visualize a number
+
+(end-script)
