a0e6a524b5254adcd4096fb4984c1d39c91362c9
Javran Cheng <Javran.c@gmail.com>
2013-08-04 21:18:01 +0800

    2.5.3 - basic poly operations, add and mul
    we have not yet come up with a representation for terms

--- sicp/ch02/5_3_polynominal_package.scm
+++ sicp/ch02/5_3_polynominal_package.scm
@@ -9,12 +9,31 @@
   (define (same-varialbe? v1 v2)
     (and (variable? v1) (variable? v2) (equal? v1 v2)))
 
+  (define (add-terms l1 l2)
+    (cond ((empty-termlist? l1) l2)
+          ((empty-termlist? l2) l1)
+          (else
+            (let ((t1 (first-term l1))
+                  (t2 (first-term l2)))
+              (cond ((> (order t1) (order t2))
+                      (adjoin-term
+                        t1 (add-terms (rest-terms l1) l2)))
+                    ((< (order t1) (order t2))
+                      (adjoin-term 
+                        t2 (add-terms l1 (rest-terms l2))))
+                    (else
+                      adjoin-term
+                      (make-term (order t1)
+                                 (add (coeff t1) (coeff t2)))
+                      (add-terms (rest-terms l1)
+                                 (rest-terms l2))))))))
+
   (define (add-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
                  (add-terms (term-list p1)
                             (term-list p2)))
-      (error "Polys not in same ver: ADD-POLY"
+      (error "Polys not in same var: ADD-POLY"
              (list p1 p2))))
 
   (define (mul-poly p1 p2)
@@ -22,7 +41,7 @@
       (make-poly (variable p1)
                  (mul-terms (term-list p1)
                             (term-list p2)))
-      (error "Polys not in same ver: MUL-POLY"
+      (error "Polys not in same var: MUL-POLY"
              (list p1 p2))))
 
   (put 'add '(polynominal polynominal) (tagged 'polynominal add-poly))
