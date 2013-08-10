8305b7605fcba54ff6f764d6fdc0e9f863bf1de1
Javran Cheng <Javran.c@gmail.com>
2013-08-10 23:41:21 +0800

    number system - link generic procedures

--- sicp/ch02/number_system.scm
+++ sicp/ch02/number_system.scm
@@ -17,6 +17,30 @@
 (install-native-num-test)
 (install-coercion-test)
 
+(define make-rational (get 'make 'rational))
+(define make-scheme-number (get 'make 'scheme-number))
+(define make-complex-ri (get 'make-ri 'complex))
+(define make-complex-ma (get 'make-ma 'complex))
+
+(define (add a b) (apply-generic 'add a b))
+(define (sub a b) (apply-generic 'sub a b))
+(define (mul a b) (apply-generic 'mul a b))
+(define (div a b) (apply-generic 'div a b))
+(define (equ? a b) (apply-generic 'equ? a b))
+
+(define (numer a) (apply-generic 'numer a))
+(define (denom a) (apply-generic 'denom a))
+(define (real-part a) (apply-generic 'real-part a))
+(define (imag-part a) (apply-generic 'imag-part a))
+(define (magnitude a) (apply-generic 'magnitude a))
+(define (angle a) (apply-generic 'angle a))
+
+(define (=zero? a) (apply-generic '=zero? a))
+(define (to-string a) (apply-generic 'to-string a))
+
+(define (raise a) (apply-generic 'raise a))
+(define (project a) (apply-generic 'project a))
+
 (define (run-test tag) ((get 'test tag)))
 (define (run-all-test)
   (for-each run-test
