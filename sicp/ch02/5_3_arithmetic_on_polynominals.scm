f0d78f858b69602e53bf5eb215601a1819436508
Javran Cheng <Javran.c@gmail.com>
2013-08-12 20:57:54 +0800

    2.5.3 - generic add/mul test for poly

--- sicp/ch02/5_3_arithmetic_on_polynominals.scm
+++ sicp/ch02/5_3_arithmetic_on_polynominals.scm
@@ -82,4 +82,25 @@
     (out (mul-terms l1 l2))
     ))
 
+; t1 = 3x^2
+; t2 = (2+3i)x^1
+; t3 = 7x^0
+; t4 = x^4
+; t5 = 2/3 x^2
+; t6 = (5+3i)x^0
+(let ((t1 (adjoin-term (make-term 2 (make-scheme-number 3)) (the-empty-termlist)))
+      (t2 (adjoin-term (make-term 1 (make-complex-ri 2 3)) (the-empty-termlist)))
+      (t3 (adjoin-term (make-term 0 (make-scheme-number 7)) (the-empty-termlist)))
+      (t4 (adjoin-term (make-term 4 (make-scheme-number 1)) (the-empty-termlist)))
+      (t5 (adjoin-term (make-term 2 (make-rational 2 3)) (the-empty-termlist)))
+      (t6 (adjoin-term (make-term 0 (make-complex-ri 5 3)) (the-empty-termlist))))
+  (let ((l1 (add-terms (add-terms t1 t2) t3))
+        (l2 (add-terms (add-terms t4 t5) t6)))
+    (let ((p1 (make-poly 'x l1))
+          (p2 (make-poly 'x l2)))
+      (out p1
+           p2
+           (add p1 p2)
+           (mul p1 p2)))))
+
 (end-script)
