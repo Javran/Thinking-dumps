0694f88a1a1ba359d92bb0e9286e6e6a9a0ade09
Javran Cheng <Javran.c@gmail.com>
2013-08-13 22:19:09 +0800

    2.5.3 - package poly-termlist

--- sicp/ch02/5_3_arithmetic_on_polynominals.scm
+++ sicp/ch02/5_3_arithmetic_on_polynominals.scm
@@ -10,63 +10,16 @@
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (equal? v1 v2)))
 
-(define (adjoin-term term term-list)
-  (if (=zero? (coeff term))
-    term-list
-    (cons term term-list)))
-(define (the-empty-termlist) nil)
-(define first-term car)
-(define rest-terms cdr)
-(define empty-termlist? null?)
-
-(define make-term list)
-(define order car)
-(define coeff cadr)
-
 (load "./5_3_poly_term_package.scm")
+(load "./5_3_poly_termlist_package.scm")
 (install-poly-term-package)
+(install-poly-termlist-package)
+
+(define make-term (get 'make 'poly-term))
+(define (order x) (apply-generic 'order x))
+(define (coeff x) (apply-generic 'coeff x))
 
 (run-test 'poly-term-package)
-
-; join two term-lists
-(define (add-terms l1 l2)
-  (cond ((empty-termlist? l1) l2)
-        ((empty-termlist? l2) l1)
-        (else
-          (let ((t1 (first-term l1))
-                (t2 (first-term l2)))
-            ; terms of the bigger order always are placed
-            ;   in front of lower ones
-            (cond ((> (order t1) (order t2))
-                    (adjoin-term
-                      t1 (add-terms (rest-terms l1) l2)))
-                  ((< (order t1) (order t2))
-                    (adjoin-term 
-                      t2 (add-terms l1 (rest-terms l2))))
-                  ; of the same order, merge t1 and t2
-                  (else
-                    (adjoin-term
-                      (make-term (order t1)
-                                 (add (coeff t1) (coeff t2)))
-                      (add-terms (rest-terms l1)
-                                 (rest-terms l2)))))))))
-(define (mul-terms l1 l2)
-  (if (empty-termlist? l1)
-    ; 0 * x = 0
-    (the-empty-termlist)
-    ; (a1+a2+...)*l2 = a1*l2 + (a2+...)*l2
-    (add-terms (mul-term-by-all-terms (first-term l1) l2)
-               (mul-terms (rest-terms l1) l2))))
-
-(define (mul-term-by-all-terms t1 l)
-  (if (empty-termlist? l)
-    (the-empty-termlist)
-    ; t1*(a1+a2...) = t1*a1+t1*(a2+...)
-    (let ((t2 (first-term l)))
-      (adjoin-term
-        (make-term (+ (order t1) (order t2))
-                   (mul (coeff t1) (coeff t2)))
-        (mul-term-by-all-terms t1 (rest-terms l))))))
 
 (define (make-poly v t)
   ((get 'make 'polynominal) v t))
@@ -75,37 +28,42 @@
 ; t2 = 4x^1
 ; t3 = 2x^0
 ; t4 = 2x^1
-(let ((t1 (adjoin-term (make-term 2 (make-scheme-number 3)) (the-empty-termlist)))
-      (t2 (adjoin-term (make-term 1 (make-scheme-number 4)) (the-empty-termlist)))
-      (t3 (adjoin-term (make-term 0 (make-scheme-number 2)) (the-empty-termlist)))
-      (t4 (adjoin-term (make-term 1 (make-scheme-number 2)) (the-empty-termlist))))
-  (let ((l1 (add-terms t1 t2))
-        (l2 (add-terms t3 t4)))
-    ; l1 = 3x^2+4x
-    ; l2 = 2x+2
-    (out (add-terms l1 l2))
-    (out (mul-terms l1 l2))
-    ))
-
+;(let ((t1 (adjoin-term (make-term 2 (make-scheme-number 3)) (the-empty-termlist)))
+;      (t2 (adjoin-term (make-term 1 (make-scheme-number 4)) (the-empty-termlist)))
+;      (t3 (adjoin-term (make-term 0 (make-scheme-number 2)) (the-empty-termlist)))
+;      (t4 (adjoin-term (make-term 1 (make-scheme-number 2)) (the-empty-termlist))))
+;  (let ((l1 (add-terms t1 t2))
+;        (l2 (add-terms t3 t4)))
+;    ; l1 = 3x^2+4x
+;    ; l2 = 2x+2
+;    (out (add-terms l1 l2))
+;    (out (mul-terms l1 l2))
+;    ))
+;
 ; t1 = 3x^2
 ; t2 = (2+3i)x^1
 ; t3 = 7x^0
 ; t4 = x^4
 ; t5 = 2/3 x^2
 ; t6 = (5+3i)x^0
-(let ((t1 (adjoin-term (make-term 2 (make-scheme-number 3)) (the-empty-termlist)))
-      (t2 (adjoin-term (make-term 1 (make-complex-ri 2 3)) (the-empty-termlist)))
-      (t3 (adjoin-term (make-term 0 (make-scheme-number 7)) (the-empty-termlist)))
-      (t4 (adjoin-term (make-term 4 (make-scheme-number 1)) (the-empty-termlist)))
-      (t5 (adjoin-term (make-term 2 (make-rational 2 3)) (the-empty-termlist)))
-      (t6 (adjoin-term (make-term 0 (make-complex-ri 5 3)) (the-empty-termlist))))
-  (let ((l1 (add-terms (add-terms t1 t2) t3))
-        (l2 (add-terms (add-terms t4 t5) t6)))
-    (let ((p1 (make-poly 'x l1))
-          (p2 (make-poly 'x l2)))
-      (out p1
-           p2
-           (add p1 p2)
-           (mul p1 p2)))))
+;(let ((t1 (adjoin-term (make-term 2 (make-scheme-number 3)) (the-empty-termlist)))
+;      (t2 (adjoin-term (make-term 1 (make-complex-ri 2 3)) (the-empty-termlist)))
+;      (t3 (adjoin-term (make-term 0 (make-scheme-number 7)) (the-empty-termlist)))
+;      (t4 (adjoin-term (make-term 4 (make-scheme-number 1)) (the-empty-termlist)))
+;      (t5 (adjoin-term (make-term 2 (make-rational 2 3)) (the-empty-termlist)))
+;      (t6 (adjoin-term (make-term 0 (make-complex-ri 5 3)) (the-empty-termlist))))
+;  (let ((l1 (add-terms (add-terms t1 t2) t3))
+;        (l2 (add-terms (add-terms t4 t5) t6)))
+;    (let ((p1 (make-poly 'x l1))
+;          (p2 (make-poly 'x l2)))
+;      (out p1
+;           p2
+;           (add p1 p2)
+;           (mul p1 p2)))))
+
+(out ((get 'make-from-args 'poly-termlist)
+       1 (make-scheme-number 5)
+       1 (make-scheme-number 5)
+       3 (make-rational 2 3)))
 
 (end-script)
---
+++ sicp/ch02/5_3_poly_termlist_package.scm
@@ -0,0 +1,81 @@
+(define (install-poly-termlist-package)
+  (define (make-empty) nil)
+  (define first-term car)
+  (define rest-terms cdr)
+  (define empty-termlist? null?)
+
+  ; (make-from-args order1 coeff1 order2 coeff2 ...)
+  (define (make-from-args . args)
+    (define (list-to-pair-list ls)
+      (cond ((null? ls) '())
+            ((>= (length ls) 2)
+              (cons 
+                (cons (car ls) (cadr ls))
+                (list-to-pair-list (cddr ls))))
+            (else
+              (error "invalid list length"))))
+    (define (pair-to-term p)
+      ((get 'make 'poly-term) (car p) (cdr p)))
+    (define (term-to-termlist t)
+      (adjoin-term t (make-empty)))
+
+    (let ((terms (map (compose
+                        term-to-termlist
+                        pair-to-term)
+                      (list-to-pair-list args))))
+      (fold-left add-terms (make-empty) terms)))
+
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+      term-list
+      (cons term term-list)))
+
+  ; join two term-lists
+  (define (add-terms l1 l2)
+    (cond ((empty-termlist? l1) l2)
+          ((empty-termlist? l2) l1)
+          (else
+            (let ((t1 (first-term l1))
+                  (t2 (first-term l2)))
+              ; terms of the bigger order always are placed
+              ;   in front of lower ones
+              (cond ((> (order t1) (order t2))
+                     (adjoin-term
+                       t1 (add-terms (rest-terms l1) l2)))
+                    ((< (order t1) (order t2))
+                     (adjoin-term 
+                       t2 (add-terms l1 (rest-terms l2))))
+                    ; of the same order, merge t1 and t2
+                    (else
+                      (adjoin-term
+                        (make-term (order t1)
+                                   (add (coeff t1) (coeff t2)))
+                        (add-terms (rest-terms l1)
+                                   (rest-terms l2)))))))))
+  (define (mul-terms l1 l2)
+    (if (empty-termlist? l1)
+      ; 0 * x = 0
+      (the-empty-termlist)
+      ; (a1+a2+...)*l2 = a1*l2 + (a2+...)*l2
+      (add-terms (mul-term-by-all-terms (first-term l1) l2)
+                 (mul-terms (rest-terms l1) l2))))
+
+  (define (mul-term-by-all-terms t1 l)
+    (if (empty-termlist? l)
+      (the-empty-termlist)
+      ; t1*(a1+a2...) = t1*a1+t1*(a2+...)
+      (let ((t2 (first-term l)))
+        (adjoin-term
+          (make-term (+ (order t1) (order t2))
+                     (mul (coeff t1) (coeff t2)))
+          (mul-term-by-all-terms t1 (rest-terms l))))))
+
+  (put 'make 'poly-termlist (tagged 'poly-termlist make-empty))
+  (put 'make-from-args 'poly-termlist (tagged 'poly-termlist make-from-args))
+  (put 'first-term '(poly-termlist) first-term)
+  (put 'rest-terms '(poly-termlist) rest-terms)
+  (put 'add '(poly-termlist poly-termlist) (tagged 'poly-termlist add-terms))
+  (put 'mul '(poly-termlist poly-termlist) (tagged 'poly-termlist mul-terms))
+  (put 'empty? '(poly-termlist) empty-termlist?)
+
+  'done)
