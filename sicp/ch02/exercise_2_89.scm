b577637184d13949a45a3e2f22fab90fc4f8cf1d
Javran Cheng <Javran.c@gmail.com>
2013-08-15 22:12:13 +0800

    ex 2.89 - make room

---
+++ sicp/ch02/exercise_2_89.scm
@@ -0,0 +1,11 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./5_3_polynominal_setup.scm")
+(load "./exercise_2_87_changes.scm")
+(load "./exercise_2_88_changes.scm")
+
+(load "./exercise_2_89_changes.scm")
+
+
+(end-script)
---
+++ sicp/ch02/exercise_2_89_changes.scm
@@ -0,0 +1,3 @@
+(define (install-poly-termlist-dense-package)
+
+  'done)
