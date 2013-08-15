e9a5bd6de691d29a28d6188ced35151dacfdc7de
Javran Cheng <Javran.c@gmail.com>
2013-08-15 23:28:35 +0800

    ex 2.89 - merge-term (not verified)

--- sicp/ch02/exercise_2_89_changes.scm
+++ sicp/ch02/exercise_2_89_changes.scm
@@ -7,15 +7,60 @@
   (define the-empty-termlist make-empty)
   (define empty-termlist? null?)
 
+  (define (first-term-order ls) (- (length ls) 1))
   (define (first-term ls)
     ; to obtain a term, we should combine the coeff with its order
-    (let ((term-order (- (length ls) 1))
+    (let ((term-order (first-term-order ls))
           (term-coeff (car ls)))
       ((get 'make 'poly-term) term-order term-coeff)))
   (define rest-terms cdr)
 
   (define coeff ((curry2 apply-generic) 'coeff))
   (define order ((curry2 apply-generic) 'order))
+
+  ; merge a term into the list
+  (define (merge-term term termlist)
+    (let ((const-zero (const (make-scheme-number 0)))
+          (t-coeff (coeff term))
+          (t-order (order term)))
+      (cond
+        ; case #1: the term is zero
+        ;   nothing to do
+        ((=zero? t-coeff) termlist)
+        ; + precond: term is non-zero
+        ; case #2: the term list is empty
+        ;   make a placeholder list of length {t-order}
+        ;   e.g. to insert a term of order=3, coeff=x we need an empty list (0 0 0)
+        ;         after that we simply put x in front of this list and produce (x 0 0 0)
+        ((empty-termlist? termlist)
+              (cons t-coeff
+                    (map const-zero (list-in-range 1 t-order))))
+        ; + precond: termlist is non-empty
+        (else
+          (let ((ft-order (first-term-order termlist)))
+            ; case #3:
+            ;   assume we are inserting a term into an empty list,
+            ;   we need a place-holder list of length {t-order}
+            ;   now we already have a list of length {ft-order + 1}
+            ;   when t-order >= ft-order + 1, we need extra spaces for padding (including zero)
+            (if (>= t-order (+ ft-order 1))
+              (cons t-coeff
+                    (append (map const-zero (list-in-range 1 (- t-order (+ ft-order 1))))
+                            termlist))
+              ; else we simple find the corresponding position and add coeff to it
+              ; * note it's possible in this case 
+              ;   that the rule of non-zero first term might be violated,
+              ;   so we will try to remove leading zeros when the merge is done
+              (drop-while
+                =zero?
+                (if (= t-order ft-order)
+                  ; case #4: t-order = ft-order
+                  (cons (add (coeff (first-term termlist))
+                             t-coeff)
+                        (rest-terms termlist))
+                  ; case #5: t-order < ft-order
+                  (cons (first-term termlist)
+                        (merge-term term (rest-terms termlist)))))))))))
 
   (put 'make 'poly-termlist-dense (tagged 'poly-termlist-dense make-empty))
   (put 'first-term '(poly-termlist-dense) first-term)
