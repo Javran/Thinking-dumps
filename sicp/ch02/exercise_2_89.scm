a2522d182b889d0320f433520affb7d22081f10d
Javran Cheng <Javran.c@gmail.com>
2013-08-17 23:45:10 +0800

    ex 2.89 - done

--- sicp/ch02/exercise_2_89.scm
+++ sicp/ch02/exercise_2_89.scm
@@ -9,14 +9,39 @@
 
 (run-test 'poly-termlist-dense-package)
 
-(let ((test-list (attach-tag 'poly-termlist-dense (list-in-range 1 4))))
-  (out test-list
-       (apply-generic 'first-term test-list)
-       (apply-generic 'rest-terms test-list)))
+; show comparison of the underlying structures
+(out "empty-termlist:"
+     (the-empty-term-list)
+     (the-empty-term-list-dense))
 (newline)
-(let* ((make (get 'make 'poly-term))
-       (x1 (make 3 (make-complex-ri 3  4)))
-       (x2 (make 5 (make-complex-ri 3 -4))))
-  ((compose out to-string coeff) (mul x1 x2)))
+
+(let ((t1-args (list 
+                 ; 1/2 x^3 + 0.4 x^2 + 1
+                 3 (make-rational 1 2)
+                 2 (make-scheme-number 0.4)
+                 0 (make-scheme-number 1)))
+      (t2-args (list 
+                 ; (5+3i) x^5 + (-6/7) x
+                 5 (make-complex-ri 5 3)
+                 1 (make-rational -6 7))))
+  (let ((p1  (make-poly 'x
+                        (apply make-termlist-from-args t1-args)))
+        (p1d (make-poly 'x
+                        (apply make-termlist-from-args-dense t1-args)))
+        (p2  (make-poly 'x
+                        (apply make-termlist-from-args t2-args)))
+        (p2d (make-poly 'x
+                        (apply make-termlist-from-args-dense t2-args))))
+    (out "structure dump:" p1 p1d p2 p2d)
+    (out "to-string:")
+    (for-each (compose out to-string) (list p1 p1d
+                                            p2 p2d))
+    (out "add:")
+    (for-each (compose out to-string) (list (add p1  p2 )
+                                            (add p1d p2d)))
+    (out "mul:")
+    (for-each (compose out to-string) (list (mul p1  p2 )
+                                            (mul p1d p2d)))
+    ))
 
 (end-script)
