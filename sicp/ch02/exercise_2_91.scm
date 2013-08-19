440c926e86edc9020b3ea4b818e1c399b63abd8c
Javran Cheng <Javran.c@gmail.com>
2013-08-19 21:36:26 +0800

    ex 2.91 - sub support

---
+++ sicp/ch02/exercise_2_91.scm
@@ -0,0 +1,29 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+(load "./5_3_polynominal_setup.scm")
+(load "./exercise_2_87_changes.scm")
+(load "./exercise_2_88_changes.scm")
+(load "./exercise_2_89_changes.scm")
+(load "./exercise_2_90_changes.scm")
+
+(load "./exercise_2_91_changes.scm")
+
+(let ((x1 (make-termlist-from-args
+            ; x^5 - 1
+            5 (make-scheme-number 1)
+            0 (make-scheme-number -1)))
+      (x2 (make-termlist-from-args
+            ; x^2 - 1
+            2 (make-scheme-number 1)
+            0 (make-scheme-number -1)))
+      (x3 (make-termlist-from-args
+            ; x^3 + x
+            3 (make-scheme-number 1)
+            1 (make-scheme-number 1)))
+      (x4 (make-termlist-from-args
+            ; x - 1
+            1 (make-scheme-number 1)
+            0 (make-scheme-number -1))))
+  (out (sub (sub x1 (mul x2 x3)) x4)))
+
+(end-script)
---
+++ sicp/ch02/exercise_2_91_changes.scm
@@ -0,0 +1,98 @@
+(define (install-poly-termlist-package-2)
+  (define (make-empty) nil)
+  (define the-empty-termlist make-empty)
+  (define first-term car)
+  (define rest-terms cdr)
+  (define empty-termlist? null?)
+
+  (define coeff ((curry2 apply-generic) 'coeff))
+  (define order ((curry2 apply-generic) 'order))
+
+  (define coeff-list
+    ((curry2 map) coeff))
+  (define order-list
+    ((curry2 map) order))
+
+  (define (make-from-args . args)
+    (define (list-to-pair-list ls)
+      (cond ((null? ls) '())
+            ((>= (length ls) 2)
+              (cons 
+                (cons (car ls) (cadr ls))
+                (list-to-pair-list (cddr ls))))
+            (else
+              (error "invalid list length"))))
+    (define (pair-to-term p)
+      ((get 'make 'poly-term) (car p) (cdr p)))
+    (define (term-to-termlist t)
+      (adjoin-term t (make-empty)))
+
+    (let ((terms (map (compose
+                        term-to-termlist
+                        pair-to-term)
+                      (list-to-pair-list args))))
+      (fold-left add-terms (make-empty) terms)))
+
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+      term-list
+      (cons term term-list)))
+
+  ; join two term-lists
+  (define (add-terms l1 l2)
+    (cond ((empty-termlist? l1) l2)
+          ((empty-termlist? l2) l1)
+          (else
+            (let ((t1 (first-term l1))
+                  (t2 (first-term l2)))
+              (cond ((> (order t1) (order t2))
+                     (adjoin-term
+                       t1 (add-terms (rest-terms l1) l2)))
+                    ((< (order t1) (order t2))
+                     (adjoin-term 
+                       t2 (add-terms l1 (rest-terms l2))))
+                    (else
+                      (adjoin-term
+                        (make-term (order t1)
+                                   (add (coeff t1) (coeff t2)))
+                        (add-terms (rest-terms l1)
+                                   (rest-terms l2)))))))))
+
+  (define (mul-terms l1 l2)
+    (if (empty-termlist? l1)
+      (the-empty-termlist)
+      (add-terms (mul-term-by-all-terms (first-term l1) l2)
+                 (mul-terms (rest-terms l1) l2))))
+
+  (define (mul-term-by-all-terms t1 l)
+    (if (empty-termlist? l)
+      (the-empty-termlist)
+      (let ((t2 (first-term l)))
+        (adjoin-term
+          (make-term (+ (order t1) (order t2))
+                     (mul (coeff t1) (coeff t2)))
+          (mul-term-by-all-terms t1 (rest-terms l))))))
+
+  (define (neg-terms tl)
+    (let ((neg-one (make-term 0 (make-scheme-number -1))))
+      (mul-term-by-all-terms neg-one tl)))
+
+  (define (sub-terms l1 l2)
+    (add-terms l1 (neg-terms l2)))
+
+  (put 'make 'poly-termlist (tagged 'poly-termlist make-empty))
+  (put 'make-from-args 'poly-termlist (tagged 'poly-termlist make-from-args))
+  (put 'first-term '(poly-termlist) first-term)
+  (put 'rest-terms '(poly-termlist) (tagged 'poly-termlist rest-terms))
+  (put 'add '(poly-termlist poly-termlist) (tagged 'poly-termlist add-terms))
+  (put 'sub '(poly-termlist poly-termlist) (tagged 'poly-termlist sub-terms))
+  (put 'mul '(poly-termlist poly-termlist) (tagged 'poly-termlist mul-terms))
+  (put 'neg '(poly-termlist) (tagged 'poly-termlist neg-terms))
+  (put 'empty? '(poly-termlist) empty-termlist?)
+
+  (put 'order-list '(poly-termlist) order-list)
+  (put 'coeff-list '(poly-termlist) coeff-list)
+
+  'done)
+
+(install-poly-termlist-package-2)
