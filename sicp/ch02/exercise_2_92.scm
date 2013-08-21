8bff4e12732ad0a143822ec443a4970603139c12
Javran Cheng <Javran.c@gmail.com>
2013-08-21 19:20:55 +0800

    ex 2.92 - plan to re-organize code

---
+++ sicp/ch02/exercise_2_92.scm
@@ -0,0 +1,15 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; let's get a fresh start, summarize what we've done
+;   before this exercise and organize codes together
+;   for the incoming task
+
+(load "./5_3_polynominal_setup.scm")
+(load "./exercise_2_87_changes.scm")
+(load "./exercise_2_88_changes.scm")
+(load "./exercise_2_89_changes.scm")
+(load "./exercise_2_90_changes.scm")
+(load "./exercise_2_91_changes.scm")
+
+(end-script)
