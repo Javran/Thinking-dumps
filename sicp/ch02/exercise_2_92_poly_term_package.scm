185aea7d296696e3ff0f10de7252df5c7acb9289
Javran Cheng <Javran.c@gmail.com>
2013-08-22 18:31:37 +0800

    ex 2.92 - zero-term & equ? impl

--- sicp/ch02/exercise_2_92_poly_term_package.scm
+++ sicp/ch02/exercise_2_92_poly_term_package.scm
@@ -7,7 +7,9 @@
   (define (test)
     (let ((make (get 'make 'poly-term)))
       (let ((x1 (make 1 (make-rational 2 3)))
-            (x2 (make 5 (make-scheme-number 5))))
+            (x2 (make 5 (make-scheme-number 5)))
+            (x3 (make 5 (make-rational 25 5)))
+            (x4 (make 1 (make-rational 25 5))))
         ; test accessors
         (let ((testcases 
                 (list (mat 'order x1 1)
@@ -17,12 +19,31 @@
                 (list (mat 'coeff x1 (make-scheme-number (/ 2 3)))
                       (mat 'coeff x2 (make-complex-ri 5 0)))))
           (do-test-q apply-generic testcases equ?))
-        )
-      ))
+        ; test =zero?
+        (let ((testcases
+                (list (mat '=zero? x1 #f)
+                      (mat '=zero? x2 #f)
+                      (mat '=zero? (make 100 (make-scheme-number 0)) #t))))
+          (do-test-q apply-generic testcases))
+        ; test equ?
+        (let ((testcases
+                (list (mat 'equ? x1 x2 #f)
+                      (mat 'equ? x1 x3 #f)
+                      (mat 'equ? x2 x3 #t)
+                      (mat 'equ? x3 x4 #f))))
+          (do-test-q apply-generic testcases))
+        )))
+
+  (define zero-term? (compose =zero? coeff))
+  (define (term-equ? a b)
+    (and (=    (order a) (order b))
+         (equ? (coeff a) (coeff b))))
 
   (put 'make 'poly-term (tagged 'poly-term make))
   (put 'order '(poly-term) order)
   (put 'coeff '(poly-term) coeff)
+  (put '=zero? '(poly-term) zero-term?)
+  (put 'equ? '(poly-term poly-term) term-equ?)
 
   (put 'test 'poly-term-package test)
 
