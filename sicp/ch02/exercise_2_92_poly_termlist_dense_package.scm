bdc69ccd08905d272ca94637851df21650d436aa
Javran Cheng <Javran.c@gmail.com>
2013-08-25 16:16:43 +0800

    ex 2.92 - placeholder for tl-dense

---
+++ sicp/ch02/exercise_2_92_poly_termlist_dense_package.scm
@@ -0,0 +1,18 @@
+(define (install-poly-termlist-dense-package)
+ 
+;  (put 'make 'poly-termlist-dense (tagged 'poly-termlist-dense make-empty))
+;  (put 'make-from-args 'poly-termlist-dense (tagged 'poly-termlist-dense make-from-args))
+;  (put 'first-term '(poly-termlist-dense) first-term)
+;  (put 'rest-terms '(poly-termlist-dense) (tagged 'poly-termlist-dense rest-terms))
+;  (put 'add '(poly-termlist-dense poly-termlist-dense) (tagged 'poly-termlist-dense add-terms))
+;  (put 'sub '(poly-termlist-dense poly-termlist-dense) (tagged 'poly-termlist-dense sub-terms))
+;  (put 'mul '(poly-termlist-dense poly-termlist-dense) (tagged 'poly-termlist-dense mul-terms))
+;  (put 'empty? '(poly-termlist-dense) empty-termlist?)
+;  (put '=zero? '(poly-termlist-dense) empty-termlist?)
+;  (put 'order-list '(poly-termlist-dense) order-list)
+;  (put 'coeff-list '(poly-termlist-dense) coeff-list)
+;  (put 'equ? '(poly-termlist-dense poly-termlist-dense) termlist-equ?)
+;
+;  (put 'test 'poly-termlist-dense-package test)
+
+  'done)
