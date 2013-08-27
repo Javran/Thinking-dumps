c1e01994be133108991a606d87fe459dce2c98ce
Javran Cheng <Javran.c@gmail.com>
2013-08-27 16:55:50 +0800

    change type

--- all-about-monads/2_1_type_constructors.hs
+++ all-about-monads/2_1_type_constructors.hs
@@ -5,5 +5,5 @@ main = do
         -- types can be wrapped inside Maybe1
         print $ Just1 "A"
         print $ Just1 1
-        print $ (Nothing1 :: Maybe1 Int)
+        print $ (Nothing1 :: Maybe1 ())
         print $ Just1 $ Just1 "A"
