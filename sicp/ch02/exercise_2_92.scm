c21807fe365c8462df2004f089af1c58351fff4b
Javran Cheng <Javran.c@gmail.com>
2013-09-01 21:18:48 -0400

    ex 2.92 - demo for type conversion

--- sicp/ch02/exercise_2_92.scm
+++ sicp/ch02/exercise_2_92.scm
@@ -15,6 +15,7 @@
 (define variable ((curry2 apply-generic) 'variable))
 (define term-list ((curry2 apply-generic) 'term-list))
 
+; demo for automatic termlist type conversion
 (let ((args1 (list 
                1 (make-scheme-number 2)
                3 (make-scheme-number 4)))
@@ -24,30 +25,28 @@
                0 (make-scheme-number -2)))
       (types (list 'poly-termlist-sparse
                    'poly-termlist-dense)))
-  (let loop ((t types))
-    (if (null? t)
+  (let loop1 ((type1 types))
+    (if (null? type1)
       'done
-      (let ((p1 (make-poly
-                  'x
-                  (apply make-tl-from-args
-                         (cons (car t) args1))))
-            (p2 (make-poly
-                  'x
-                  (apply make-tl-from-args
-                         (cons (car t) args2)))))
-        (format #t "impl ~A:~%" (car t))
-        (out (to-string (sub p1 p2)))
-        (loop (cdr t)))))
-  (newline)
-  (out 
-    (to-order-coeff-list
-      (apply make-tl-from-args
-             (cons 'poly-termlist-sparse
-                   args1)))
-    (to-order-coeff-list
-      (apply make-tl-from-args
-             (cons 'poly-termlist-dense
-                   args2))))
-  )
+      (begin 
+        (let loop2 ((type2 types))
+          (if (null? type2)
+            'done
+            (begin
+              (let ((pa1 (make-poly
+                           'x
+                           (apply
+                             make-tl-from-args
+                             (cons (car type1)
+                                   args1))))
+                    (pa2 (make-poly
+                           'x
+                           (apply
+                             make-tl-from-args
+                             (cons (car type2)
+                                   args2)))))
+                (out (sub pa1 pa2)))
+              (loop2 (cdr type2)))))
+        (loop1 (cdr type1))))))
 
 (end-script)
