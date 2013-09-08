6b24819f28f53a2524eda6574b5958f51b1c3593
Javran Cheng <Javran.c@gmail.com>
2013-09-07 21:27:37 -0400

    ex 2.92 - remove unused tests

--- sicp/ch02/exercise_2_92.scm
+++ sicp/ch02/exercise_2_92.scm
@@ -49,55 +49,6 @@
               (loop2 (cdr type2)))))
         (loop1 (cdr type1))))))
 
-(let ((x1 (make-tl-from-args 'poly-termlist-sparse
-            ; x^5 - 1
-            5 (make-scheme-number 1)
-            0 (make-scheme-number -1)))
-      (x2 (make-tl-from-args 'poly-termlist-dense
-            ; x^2 - 1
-            2 (make-scheme-number 1)
-            0 (make-scheme-number -1)))
-      )
-  (let ((p1 (make-poly 'x x1))
-        (p2 (make-poly 'x x2)))
-    (let* ((result (div p1 p2))
-           (q (car result))
-           (r (cadr result)))
-      (out "divisor:" (to-string p1)
-           "dividend:" (to-string p2)
-           "quotient:" (to-string q)
-           "remainder:" (to-string r))
-      )))
-
-(let ((tl1 (make-tl-from-cseq-num
-             'poly-termlist-sparse
-             9 -6 -4 -2 1 2 4))
-      (tl2 (make-tl-from-cseq-num
-             'poly-termlist-dense
-             3 2 1)))
-  (let ((p1 (make-poly 'x tl1))
-        (p2 (make-poly 'x tl2)))
-    (let* ((result (div p1 p2))
-           (q (car result))
-           (r (cadr result)))
-      (out "divisor:" (to-string p1)
-           "dividend:" (to-string p2)
-           "quotient:" (to-string q)
-           "remainder:" (to-string r))
-      )))
-
-(let ((p1 (make-poly 'wildcard
-                     (make-tl-from-cseq-num
-                       'poly-termlist-sparse
-                       1 2 3 4 5)))
-      (p2 (make-poly 'x
-                     (make-tl-from-cseq-num
-                       'poly-termlist-sparse
-                       1 2 3 4 5))))
-  (out (mul p1 p2))
-  (out (mul p1 (make-scheme-number 10)))
-  (out (mul p2 (make-scheme-number 10))))
-
 ; TODO:
 ; * test if now we can enable something like:
 ;   (y^2+y+1) x^3 + 2 x^3
