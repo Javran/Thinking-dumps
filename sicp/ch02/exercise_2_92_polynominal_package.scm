4e35c79375f2749f73f4bf7671e5ebedff028ff6
Javran Cheng <Javran.c@gmail.com>
2013-09-20 23:29:49 -0400

    ex 2.92 - code refactor

--- sicp/ch02/exercise_2_92_polynominal_package.scm
+++ sicp/ch02/exercise_2_92_polynominal_package.scm
@@ -24,28 +24,24 @@
 
   ; verify variable equality before performing a binary op, say `f`
   (define (variable-verify f)
+    (define (wrap-poly outer-var p)
+      (make-poly
+        outer-var
+        (make-tl-from-args
+          'poly-termlist-sparse
+          0 (attach-tag 'polynominal p))))
     (lambda (p1 p2)
       (if (compatible-variable?  (variable p1) (variable p2))
         (f p1 p2)
         ; p1 and p2 are not compatible,
         ; in this case, neither p1 nor p2 are not wildcards
         ; we should determine which one should be put inside
-        (cond ((variable-less? 
-                 (variable p1) (variable p2))
-                (f p1 (make-poly
-                        (variable p1)
-                        (make-tl-from-args
-                          'poly-termlist-sparse
-                          0 (attach-tag 'polynominal p2)))))
-              ((variable-less?
-                 (variable p2) (variable p1))
-                (f (make-poly
-                     (variable p2)
-                     (make-tl-from-args
-                       'poly-termlist-sparse
-                       0 (attach-tag 'polynominal p1)))
-                   p2))
-              (else (error "unexpected case"))))))
+        (cond ((variable-less?  (variable p1) (variable p2))
+                (f p1 (wrap-poly (variable p1) p2)))
+              ((variable-less?  (variable p2) (variable p1))
+                (f (wrap-poly (variable p2) p1) p2))
+              (else
+                (error "unexpected case"))))))
 
   ; operations without variable verification
   ;   convert arguments between different termlist types on demand
