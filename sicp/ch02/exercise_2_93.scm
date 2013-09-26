43f5d73a717302abe1c36bb89edf2e29f4e94dce
Javran Cheng <Javran.c@gmail.com>
2013-09-25 22:34:31 -0400

    ex 2.93 - 'add' ported
    turns out that I SHOULD NEVER use identical names for both generic and
    inner-package version, which makes nothing but confusion

--- sicp/ch02/exercise_2_93.scm
+++ sicp/ch02/exercise_2_93.scm
@@ -19,6 +19,31 @@
   (out (to-string (numer r)))
   (out (to-string (denom r))))
 
+(newline)
+(let* ((p1 (make-poly
+             'x
+             (make-tl-from-cseq-num
+               'poly-termlist-sparse
+               1 1 1)))
+       (p2 (make-poly
+             'x
+             (make-tl-from-cseq-num
+               'poly-termlist-sparse
+               1 -1 -1)))
+       (p3 (make-poly
+             'x
+             (make-tl-from-cseq-num
+               'poly-termlist-sparse
+               1 1)))
+       (r1 (make-rational-p p1 p2))
+       (r2 (make-rational-p p2 p1))
+       (r3 (make-rational-p p3 p1)))
+  (out (to-string r1))
+  (out (to-string r2))
+  (out (to-string r3))
+  (out (to-string (add r2 r3)))
+  
+  )
 
 ; we start this exercise on the base of exercise 2.92
 ; My plan is:
