1198fc7a733fc72f5d2108590d9f0e5075d3f0b0
Javran Cheng <Javran.c@gmail.com>
2013-09-28 21:28:50 -0400

    ex 2.94 - use old package (w/o variable ordering)

--- sicp/ch02/exercise_2_94_setup.scm
+++ sicp/ch02/exercise_2_94_setup.scm
@@ -1,6 +1,6 @@
 (load "./exercise_2_93_setup.scm")
 
-;(load "./exercise_2_94_polynominal_package.scm")
-;(install-polynomial-package)
+(load "./exercise_2_94_polynominal_package.scm")
+(install-polynomial-package)
 
-;(run-test 'polynominal-package)
+(run-test 'polynominal-package)
