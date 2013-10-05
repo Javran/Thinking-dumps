999ed0fb519a79dafd965bbaaaf5c64a7061ac49
Javran Cheng <Javran.c@gmail.com>
2013-10-04 22:46:51 -0400

    ex 2.96 - start

---
+++ sicp/ch02/exercise_2_96_poly_termlist_sparse_package.scm
@@ -0,0 +1,179 @@
+(define (install-poly-termlist-sparse-package)
+  (define (make-empty) nil)
+  (define first-term car)
+  (define rest-terms cdr)
+  (define empty-termlist? null?)
+
+  (define coeff ((curry2 apply-generic) 'coeff))
+  (define order ((curry2 apply-generic) 'order))
+
+  ; (make-from-args order1 coeff1 order2 coeff2 ...)
+  (define (make-from-args . args)
+    (define (list-to-pair-list ls)
+      (cond ((null? ls) '())
+            ((>= (length ls) 2)
+              (cons 
+                (cons (car ls) (cadr ls))
+                (list-to-pair-list (cddr ls))))
+            (else
+              (error "invalid list length"))))
+    (define (pair-to-term p)
+      ((get 'make 'poly-term) (car p) (cdr p)))
+
+    (let ((terms (map pair-to-term
+                      (list-to-pair-list args))))
+      (fold-right adjoin-term (make-empty) terms)))
+
+  (define (adjoin-term term term-list)
+    (cond ((=zero? term)
+            ; case #1
+            term-list)
+          ; term is non-zero
+          ((empty-termlist? term-list)
+            ; case #2
+            (list term))
+          ; term-list is non-empty
+          (else 
+            (let* ((ft (first-term term-list))
+                   (ft-order (order ft))
+                   (ft-coeff (coeff ft))
+                   (t-order (order term))
+                   (t-coeff (coeff term)))
+              ; bind ft-* -> first term in the termlist
+              (cond ((> t-order ft-order)
+                      ; case #3
+                      ; if t-order > ft-order, insert in front of the termlist
+                      (cons term term-list))
+                    ((= t-order ft-order)
+                      ; if t-order = ft-order
+                      (let ((result-term (add ft term)))
+                        ; coeff = 0
+                        (if (=zero? result-term)
+                          ; case #4
+                          ; drop the first term
+                          (rest-terms term-list)
+                          ; case #5
+                          ; else
+                          (cons result-term
+                                (rest-terms term-list)))))
+                    ((< t-order ft-order)
+                      ; case #6
+                      ; if t-order < ft-order
+                      (cons (first-term term-list)
+                           (adjoin-term term (rest-terms term-list))))
+                    (else (error "impossible case")))))))
+
+  ; join two term-lists
+  (define add-terms
+    ((get 'add-terms-maker 'poly-generic)
+     first-term
+     rest-terms
+     empty-termlist?
+     adjoin-term))
+
+  (define mul-term-by-all-terms
+    ((get 'mul-term-by-all-terms-maker 'poly-generic)
+     first-term
+     rest-terms
+     empty-termlist?
+     make-empty
+     adjoin-term))
+
+  (define mul-terms
+    ((get 'mul-terms-maker 'poly-generic)
+     first-term
+     rest-terms
+     empty-termlist?
+     make-empty
+     add-terms
+     mul-term-by-all-terms))
+
+  (define neg-terms
+    ((get 'neg-terms-maker 'poly-generic)
+     mul-term-by-all-terms))
+
+  (define sub-terms
+    ((get 'sub-terms-maker 'poly-generic)
+     add-terms
+     neg-terms))
+  
+  (define termlist-equ?
+    ((get 'termlist-equ?-maker 'poly-generic)
+     first-term
+     rest-terms
+     empty-termlist?))
+
+  (define div-terms
+    ((get 'div-terms-maker 'poly-generic)
+     first-term
+     rest-terms
+     empty-termlist?
+     make-empty
+     sub-terms
+     mul-term-by-all-terms
+     adjoin-term))
+
+  ; a ^ b
+  (define (power a b)
+    (if (=zero? b)
+      (make-scheme-number 1)
+      (mul (power a (- b 1)) a)))
+
+  ; integerize p using (p,q), returns p
+  (define (integerize p q)
+    (let ((o1 (order (first-term p)))
+          (o2 (order (first-term q)))
+          (c  (coeff (first-term q))))
+      (mul-term-by-all-terms
+        (make-term-oc 0 (power c (+ 1 (- o1 o2))))
+        p)))
+
+  (define (quotient-terms tl1 tl2)
+    (car (div-terms tl1 tl2)))
+
+  (define (remainder-terms tl1 tl2)
+    (cadr (div-terms tl1 tl2)))
+
+  (define (gcd-terms a b)
+    (if (empty-termlist? b)
+      a
+      (gcd-terms b (remainder-terms a b))))
+
+  (define (test)
+    ((get 'test-poly-termlist 'poly-generic)
+     'poly-termlist-sparse
+     make-empty
+     make-from-args
+     first-term
+     rest-terms
+     adjoin-term
+     add-terms
+     sub-terms
+     mul-term-by-all-terms
+     mul-terms
+     div-terms
+     empty-termlist?
+     termlist-equ?))
+
+  (put 'make 'poly-termlist-sparse (tagged 'poly-termlist-sparse make-empty))
+  (put 'make-from-args 'poly-termlist-sparse (tagged 'poly-termlist-sparse make-from-args))
+  (put 'first-term '(poly-termlist-sparse) first-term)
+  (put 'rest-terms '(poly-termlist-sparse) (tagged 'poly-termlist-sparse rest-terms))
+  (put 'add '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse add-terms))
+  (put 'sub '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse sub-terms))
+  (put 'mul '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse mul-terms))
+  (put 'div '(poly-termlist-sparse poly-termlist-sparse)
+       (lambda (l1 l2)
+         (map ((curry2 attach-tag) 'poly-termlist-sparse) (div-terms l1 l2))))
+
+  (put 'quotient '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse quotient-terms))
+  (put 'remainder '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse remainder-terms))
+  (put 'gcd '(poly-termlist-sparse poly-termlist-sparse) (tagged 'poly-termlist-sparse gcd-terms))
+
+  (put 'empty? '(poly-termlist-sparse) empty-termlist?)
+  (put '=zero? '(poly-termlist-sparse) empty-termlist?)
+  (put 'equ? '(poly-termlist-sparse poly-termlist-sparse) termlist-equ?)
+
+  (put 'test 'poly-termlist-sparse-package test)
+
+  'done)
---
+++ sicp/ch02/exercise_2_96_setup.scm
@@ -0,0 +1,10 @@
+(load "./exercise_2_95_setup.scm")
+
+(load "./exercise_2_96_polynominal_package.scm")
+(load "./exercise_2_96_poly_termlist_sparse_package.scm")
+
+(install-polynomial-package)
+(install-poly-termlist-sparse-package)
+
+(run-test 'polynominal-package)
+(run-test 'poly-termlist-sparse-package)
