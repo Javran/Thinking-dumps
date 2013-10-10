7c50d715488816310928196f15b003f494184eb1
Javran Cheng <Javran.c@gmail.com>
2013-10-10 17:32:35 -0400

    3.1 - taking notes

--- sicp/ch03/3_modularity_objects_and_state.scm
+++ sicp/ch03/3_modularity_objects_and_state.scm
@@ -4,6 +4,7 @@
 ; 2 approaches:
 ;   * object based approach:
 ;     abandon our old subsitution model of computation
+;     -> environment model of computation
 ;   * stream-processing approach:
 ;     delayed evaluation
 
