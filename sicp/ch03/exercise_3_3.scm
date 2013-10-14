4e4cf3e4c55afbb3f513bc686f76628c5f6f3496
Javran Cheng <Javran.c@gmail.com>
2013-10-13 22:41:15 -0400

    ex 3.3 - done

--- sicp/ch03/exercise_3_3.scm
+++ sicp/ch03/exercise_3_3.scm
@@ -10,10 +10,17 @@
   (define (deposit amount)
     (set! balance (+ balance amount))
     (out balance))
+  ; support password changing in addition
+  ;   given that `change-password` can only be called when
+  ;   the old password has been confirmed (by `dispatch`)
+  ;   so here we don't need the old password
+  (define (change-password new-password)
+    (set! password new-password))
   (define (dispatch try-password m)
     (if (eq? password try-password)
       (cond ((eq? m 'withdraw) withdraw)
             ((eq? m 'deposit) deposit)
+            ((eq? m 'change-password) change-password)
             (else (error "Unknown request: MAKE-ACCOUNT"
                          m)))
       (lambda args (out "Incorrect password"))))
@@ -25,5 +32,10 @@
 ; 60
 ((acc 'some-other-password 'withdraw) 40)
 ; Incorrect password
+((acc 'secret-password 'change-password) 'new-secret)
+((acc 'secret-password 'withdraw) 40)
+; Incorrect password
+((acc 'new-secret 'withdraw) 40)
+; 20
 
 (end-script)
