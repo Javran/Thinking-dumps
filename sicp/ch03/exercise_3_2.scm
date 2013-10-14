86849b4d318a5c6c05e8c5e95416804127ed6c43
Javran Cheng <Javran.c@gmail.com>
2013-10-13 22:25:14 -0400

    ex 3.2 - done

--- sicp/ch03/exercise_3_2.scm
+++ sicp/ch03/exercise_3_2.scm
@@ -19,4 +19,26 @@
           (apply f args))))
     call-f))
 
+(define s (make-monitored sqrt))
+(out (s 'how-many-calls?))
+; 0
+(out (s 100))
+; 10
+(out (s 'how-many-calls?))
+; 1
+
+(newline)
+(define m+ (make-monitored +))
+(out (m+ 1 2 3 4))
+; 10
+(out (m+ 5 5))
+; 10
+(out (m+ 'how-many-calls?))
+; 2
+(m+ 'reset-count)
+(out (m+ 1 3 5 7))
+; 16
+(out (m+ 'how-many-calls?))
+; 1
+
 (end-script)
