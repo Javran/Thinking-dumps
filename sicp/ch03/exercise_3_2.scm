ebe952999085b03f94ea439fbc8fdaf2751ddb58
Javran Cheng <Javran.c@gmail.com>
2013-10-13 22:22:22 -0400

    ex 3.2 - impl

--- sicp/ch03/exercise_3_2.scm
+++ sicp/ch03/exercise_3_2.scm
@@ -1,6 +1,22 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
-; challenge: to make `make-monitored` work on not only unaries
+(define (make-monitored f)
+  (let ((counter 0))
+    (define (call-f . args)
+      (if (= (length args) 1)
+        (let ((cmd (car args)))
+          (cond ((eq? cmd 'how-many-calls?)
+                  counter)
+                ((eq? cmd 'reset-count)
+                  (set! counter 0)
+                  counter)
+                (else
+                  (set! counter (inc counter))
+                  (apply f args))))
+        (begin
+          (set! counter (inc counter))
+          (apply f args))))
+    call-f))
 
 (end-script)
