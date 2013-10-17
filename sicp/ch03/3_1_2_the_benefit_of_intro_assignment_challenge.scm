f3872d3ca037caa615d435380064b1534d064143
Javran Cheng <Javran.c@gmail.com>
2013-10-17 13:39:52 -0400

    3.1.2 - challenge accepted

---
+++ sicp/ch03/3_1_2_the_benefit_of_intro_assignment_challenge.scm
@@ -0,0 +1,8 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; this source code tries to challenge the statement in book
+;   that `makes it difficult for us to isolate the Monte
+;   Carlo idea`.
+
+(end-script)
