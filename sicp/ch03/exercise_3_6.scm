5e227543c3fab1f92bc680c01c7d2754febb867c
Javran Cheng <Javran.c@gmail.com>
2013-10-19 23:51:09 -0400

    ex 3.6 - weird...

--- sicp/ch03/exercise_3_6.scm
+++ sicp/ch03/exercise_3_6.scm
@@ -4,17 +4,20 @@
 (define random-range 1000)
 
 (define (rand action)
-  (define state (make-random-state))
-    ;(define (reset new-state)
-    ;  (set! state new-state))
+  (define state nil) 
+  (define (reset new-state)
+    (set! state new-state)
+    (pp state)
+    'done)
   (define (generate)
     (let ((result (random random-range state)))
-      (let ((new-state state))
-        ;(reset new-state)
-        result)))
+      ; `random` has side-effect on `state`
+      ;   simply assign `state` to itself
+      ;(reset state)
+      result))
   (cond ((eq? action 'generate) (generate))
         ((eq? action 'reset) reset)
-        (else (error "Unknown message: " m))))
+        (else (error "Unknown action: " action))))
 
 (define (generate-numbers count)
   (if (= count 0)
@@ -22,6 +25,10 @@
     (cons (rand 'generate)
           (generate-numbers (dec count)))))
 
-(out (generate-numbers 10))
+(out ((rand 'reset) (make-random-state #f)))
+
+(out (rand 'generate))
+(out (rand 'generate))
+(out (rand 'generate))
 
 (end-script)
