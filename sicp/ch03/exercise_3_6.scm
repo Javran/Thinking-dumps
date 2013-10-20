2b4ff421047ae82990f3d6aa9566e2b44e836d57
Javran Cheng <Javran.c@gmail.com>
2013-10-20 00:03:14 -0400

    ex 3.6 - done

--- sicp/ch03/exercise_3_6.scm
+++ sicp/ch03/exercise_3_6.scm
@@ -3,21 +3,32 @@
 
 (define random-range 1000)
 
-(define (rand action)
-  (define state nil) 
-  (define (reset new-state)
-    (set! state new-state)
-    (pp state)
-    'done)
-  (define (generate)
-    (let ((result (random random-range state)))
-      ; `random` has side-effect on `state`
-      ;   simply assign `state` to itself
-      ;(reset state)
-      result))
-  (cond ((eq? action 'generate) (generate))
-        ((eq? action 'reset) reset)
-        (else (error "Unknown action: " action))))
+; don't use (define (rand <args>) ...) here
+;   because every time we call it, it will generate a new
+;   environment that we're unable to use
+;   so instead, we write a `dispatch` so the inside environment
+;   can be persistently available
+(define rand
+  (let ((state nil))
+    (define (reset new-state)
+      ; make a copy so this generator will not change
+      ;   the state passed in
+      (set! state (make-random-state new-state))
+      'done)
+    (define (generate)
+      (if (null? state)
+        (error "rand state not initialized")
+        'pass)
+      (let ((result (random random-range state)))
+        ; `random` has side-effect on `state`
+        ;   simply assign `state` to itself
+        (reset state)
+        result))
+    (define (dispatch action)
+      (cond ((eq? action 'generate) (generate))
+            ((eq? action 'reset) reset)
+            (else (error "Unknown action: " action))))
+    dispatch))
 
 (define (generate-numbers count)
   (if (= count 0)
@@ -25,10 +36,18 @@
     (cons (rand 'generate)
           (generate-numbers (dec count)))))
 
-(out ((rand 'reset) (make-random-state #f)))
+(define state (make-random-state #t))
 
-(out (rand 'generate))
-(out (rand 'generate))
-(out (rand 'generate))
+((rand 'reset) state)
+
+(define result-1 (generate-numbers 10))
+(define result-2 (generate-numbers 10))
+
+((rand 'reset) state)
+(define result-3 (generate-numbers 10))
+
+(out result-1 result-2 result-3)
+(assert (equal? result-1 result-3))
+(out "random generator with state - test passed")
 
 (end-script)
