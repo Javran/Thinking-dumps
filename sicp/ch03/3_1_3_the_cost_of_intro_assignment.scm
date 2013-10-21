8b32bb7b223d024d00464c61db7389d37bb68336
Javran Cheng <Javran.c@gmail.com>
2013-10-20 23:42:06 -0400

    3.1.3 - working

--- sicp/ch03/3_1_3_the_cost_of_intro_assignment.scm
+++ sicp/ch03/3_1_3_the_cost_of_intro_assignment.scm
@@ -4,4 +4,29 @@
 ; programming without any use of assignments
 ;   is accordingly known as functional programming
 
+(define (make-simplified-withdraw balance)
+  (lambda (amount)
+    (set! balance (- balance amount))
+    balance))
+
+(define W (make-simplified-withdraw 25))
+
+(out (W 20))
+; 5
+
+(out (W 10))
+; -5
+
+; there's no accumulated effect over successive calls
+(define (make-decrementer balance)
+  (lambda (amount)
+    (- balance amount)))
+
+(define D (make-decrementer 25))
+
+(out (D 20))
+; 5
+(out (D 10))
+; 15
+
 (end-script)
