aa387a7529d398eac613e6bf5a4ca3d69260b3fc
Javran Cheng <Javran.c@gmail.com>
2013-10-23 00:36:36 -0400

    ex 3.7 - use obj-holder

--- sicp/ch03/exercise_3_7.scm
+++ sicp/ch03/exercise_3_7.scm
@@ -26,18 +26,26 @@
 (define (make-account balance password)
   ; use the convention that use exclamation mark
   ;   for methods that have side-effect
+
+  ; initialization:
+  (set! balance (obj-holder balance))
+
+  ; methods:
   (define (withdraw! amount)
-    (if (>= balance amount)
+    (define (suff? balance) (>= balance amount))
+    (define (withdraw balance) (- balance amount))
+    (if ((balance 'test) suff?)
       (begin
-        (set! balance (- balance amount))
-        balance)
+        ((balance 'modify) withdraw)
+        ((balance 'get)))
       (begin
         (warn "Insufficient funds")
         #f)
       ))
   (define (deposit! amount)
-    (set! balance (+ balance amount))
-    balance)
+    ((balance 'modify)
+      (lambda (balance) (+ balance amount)))
+    ((balance 'get)))
   (define (change-password! new-password)
     (set! password new-password))
   (define (dispatch try-password m)
@@ -56,6 +64,13 @@
 ;   we have to do something like this in case that changing the password
 ;   of one account has side-effect on another account.
 
+
+(define alice (make-account 1000 'pw))
+
+(out ((alice 'pw 'withdraw) 100))
+(out ((alice 'pw 'withdraw) 100))
+(out ((alice 'pw 'withdraw) 100))
+(out ((alice 'pw 'deposit) 1234))
 
 
 (end-script)
