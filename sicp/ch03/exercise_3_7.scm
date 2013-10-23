40118e856aa261b1ba1df54ee4856365301c62f8
Javran Cheng <Javran.c@gmail.com>
2013-10-22 23:56:06 -0400

    ex 3.7 - slight mod, make code clearer

--- sicp/ch03/exercise_3_7.scm
+++ sicp/ch03/exercise_3_7.scm
@@ -3,32 +3,37 @@
 
 ; excerpted from ex 3.3
 (define (make-account balance password)
-  (define (withdraw amount)
+  ; use the convention that use exclamation mark
+  ;   for methods that have side-effect
+  (define (withdraw! amount)
     (if (>= balance amount)
-      (begin (set! balance (- balance amount))
-             (out balance))
-      (out "Insufficient funds")))
-  (define (deposit amount)
+      (begin
+        (set! balance (- balance amount))
+        balance)
+      (begin
+        (warn "Insufficient funds")
+        #f)
+      ))
+  (define (deposit! amount)
     (set! balance (+ balance amount))
-    (out balance))
-  (define (change-password new-password)
+    balance)
+  (define (change-password! new-password)
     (set! password new-password))
   (define (dispatch try-password m)
     (if (eq? password try-password)
-      (cond ((eq? m 'withdraw) withdraw)
-            ((eq? m 'deposit) deposit)
-            ((eq? m 'change-password) change-password)
+      (cond ((eq? m 'withdraw) withdraw!)
+            ((eq? m 'deposit) deposit!)
+            ((eq? m 'change-password) change-password!)
             (else (error "Unknown request: MAKE-ACCOUNT"
                          m)))
-      (lambda args (out "Incorrect password"))))
+      (lambda args (warn "Incorrect password"))))
   dispatch)
 
 ; some modification required.
-; * not flexible enough to just simply output the result
 ; * should make the amount store in an environment that can be shared
 ;   because I made an extension that enables us to change the password
 ;   we have to do something like this in case that changing the password
 ;   of one account has side-effect on another account.
-;   
+
 
 (end-script)
