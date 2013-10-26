c4400c6563ca04256fce3e87b71a8b2fd6e56bf6
Javran Cheng <Javran.c@gmail.com>
2013-10-25 23:23:00 -0400

    3.2 - done

--- sicp/ch03/3_2_environment_model_of_evaluation.scm
+++ sicp/ch03/3_2_environment_model_of_evaluation.scm
@@ -17,4 +17,31 @@
 ;   we go through the frames from inside to outside, the first one takes effect
 ;   and others if any get omitted, and called `shadowed`
 
+; to show some examples
+(let ((x 3)
+      (y 5))
+  ; frame I
+  (out x y)
+  ; 3 5
+  (let ((z 6)
+        (x 7))
+    ; frame II, the `x` in frame I gets shadowed
+    (newline)
+    (out x y z)
+    ; 7 5 6 
+    )
+  (let ((m 1)
+        (y 2))
+    ; frame III
+    (newline)
+    (out x y m)
+    ; 3 2 1
+    ))
+
+; the interpretation of an expression can differ given different environment:
+(newline)
+(let ((+ (lambda (a b)
+           (out "this `+` does nothing"))))
+  (+ 1 2))
+
 (end-script)
