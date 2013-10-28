1bbee2a50490a9981bce2e62d9562e4f100f36c8
Javran Cheng <Javran.c@gmail.com>
2013-10-27 22:40:47 -0400

    ex 3.10 - end of code explanation

--- sicp/ch03/exercise_3_10.scm
+++ sicp/ch03/exercise_3_10.scm
@@ -66,6 +66,7 @@
 ;                  balance)
 ;                "Insufficient funds"))
 ;   result: a procedure in environment: [E2, E1, G]
+;   finally, bind `W1` to global env G
 
 ; verify my conclusion
 (pp W1)
@@ -95,9 +96,32 @@
 (out (W1 50))
 ; 50
 
+(define W2 (make-withdraw 100))
 
-
-
-
+; 4. evaluate `(make-withdraw 100)` and bind the result to `W2` (in G)
+;   new env (E4) += inital-amount, inital-amount = 100
+;   now evaluate the following expr in E4:
+;         ((lambda (balance)
+;            (lambda (amount)
+;              (if (>= balance amount)
+;                (begin
+;                  (set! balance (- balance amount))
+;                  balance)
+;                "Insufficient funds")))
+;          inital-amount)  
+;   new env (E5) += balance, balance = 100
+;   now evaluate the following expr in E5:
+;            (lambda (amount)
+;              (if (>= balance amount)
+;                (begin
+;                  (set! balance (- balance amount))
+;                  balance)
+;                "Insufficient funds"))
+;   result: a procedure in environment: [E5, E4, G]
+;   finally, bind `W2` to global env G
+(pp W2)
+; code here should be the same as that of W1's
+;   however, their environments are totally different:
+;   [E2,E1,G] for W1, [E5,E4,G] for W2
 
 (end-script)
