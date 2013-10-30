9c4dcffa1ad9d1baef87eadc66ae52c4366b16dd
Javran Cheng <Javran.c@gmail.com>
2013-10-29 22:56:44 -0400

    ex 3.11 - done

--- sicp/ch03/exercise_3_11.scm
+++ sicp/ch03/exercise_3_11.scm
@@ -72,4 +72,45 @@ balance
 ; re-evaluate `balance`, get 90.
 |#
 
+(out ((acc 'withdraw) 60))
+#|
+; the lambda is called, new environment
+; E4 <- {m= 'withdraw} : [E1, G]
+(cond ((eq? m 'withdraw) withdraw)
+      ((eq? m 'deposit) deposit)
+      (else
+        (error "Unknown request: MAKE-ACCOUNT" m)))
+; fall into the first case, evaluate `withdraw`, which is
+(define (withdraw amount)
+  (if (>= balance amount)
+    (begin (set! balance (- balance amount))
+           balance)
+    "Insufficient funds"))
+; the lambda is called, new environment.
+; E5 <- {amount = 60} : [E4, E1, G]
+(begin (set! balance (- balance amount))
+       balance)
+; the closest balance binding found in E1,
+; [E5, E4, E1 <- {balance = 30}, G]
+; re-evaluate `balance`, get 30
+|#
+
+; Where is the local state for acc kept?
+;
+;   it's E1, the environment created when `make-account` is called
+
+; Suppose we define another account, `(define acc2 (make-account 100))`
+;   How are the local states for the two accounts kept distinct?
+;
+;   Whenever `make-account` is called, it creates a new environment.
+;   This environment is accessible by the functions defined in this environment,
+;   i.e. `dispatch`, `withdraw`, `deposit`. Exposing `dispatch` makes it possible
+;   for any calls outside this environment to modify this environment indirectly.
+;   What we need to do is just storing the returned instance (i.e. `dispatch`) 
+;   somewhere for future use.
+
+; Which parts of the environment structure are shared between acc and acc2?
+;
+;   Only global environment.
+
 (end-script)
