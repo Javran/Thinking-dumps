bb4e219c934376a6918aa283103a23f021452455
Javran Cheng <Javran.c@gmail.com>
2013-10-29 23:14:13 -0400

    3.3 - modeling with mutable data

---
+++ sicp/ch03/3_3_modeling_with_mutable_data.scm
@@ -0,0 +1,8 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; mutators: modify data objects
+; mutable data objects: data objects for which
+;   mutators are defined
+
+(end-script)
