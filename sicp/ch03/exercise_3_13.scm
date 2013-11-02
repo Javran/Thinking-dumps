dde4fdea74b29766355ab37fde4d2ffc55987759
Javran Cheng <Javran.c@gmail.com>
2013-11-02 01:34:21 -0400

    ex 3.13 - done

--- sicp/ch03/exercise_3_13.scm
+++ sicp/ch03/exercise_3_13.scm
@@ -14,6 +14,27 @@
 ; define an auxiliary function to guaranteed that
 ; this code can be terminated.
 
+#|
+illustration:
+
+the list passed into `make-cycle`:
++-+-+  +-+-+  +-+-+
+|.|.-->|.|.-->|.|/|
++|+-+  +|+-+  +|+-+
+ v      v      v
+ a      b      c
+
+and after:
+ +---------------+
+ |               |
+ v               |
++-+-+  +-+-+  +-+|+
+|.|.-->|.|.-->|.|.|
++|+-+  +|+-+  +|+-+
+ v      v      v
+ a      b      c
+
+|#
 
 (define (take n ls)
   (if (or (<= n 0) (null? ls))
@@ -23,5 +44,8 @@
 (define z (make-cycle (list 'a 'b 'c)))
 
 (out (take 100 z))
+
+; uncomment to observe the infinite loop
+; (last-pair z)
 
 (end-script)
