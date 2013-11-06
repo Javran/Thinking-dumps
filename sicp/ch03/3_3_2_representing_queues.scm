103d93d8428fc8e24183c216d202da4394786ee2
Javran Cheng <Javran.c@gmail.com>
2013-11-05 23:55:24 -0500

    3.3.2 - queue abstraction

---
+++ sicp/ch03/3_3_2_representing_queues.scm
@@ -0,0 +1,14 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; queue abstraction:
+; constructor:
+;   (make-queue)
+; selector:
+;   (empty-queue? q)
+;   (front-queue q)
+; mutator:
+;   (insert-queue! q i)
+;   (delete-queue! q)
+
+(end-script)
