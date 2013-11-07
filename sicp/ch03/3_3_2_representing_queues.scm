57635c13036660e6a22ca5ee4dddf33eddac36c8
Javran Cheng <Javran.c@gmail.com>
2013-11-06 23:04:40 -0500

    3.3.2 - queue example

--- sicp/ch03/3_3_2_representing_queues.scm
+++ sicp/ch03/3_3_2_representing_queues.scm
@@ -37,4 +37,17 @@
 (define (delete-queue! queue)
   (set-front-ptr! queue (cdr (front-ptr queue))))
 
+(let ((q (make-queue)))
+  ; insert: 1,2,3,4,5
+  (for-each ((curry2 insert-queue!) q) '(1 2 3 4 5))
+  (let loop ()
+    (if (empty-queue? q)
+      'done
+      (begin
+        (out (front-queue q))
+        (delete-queue! q)
+        (loop))))
+  ; delete: 1,2,3,4,5 (FIFO)
+  )
+
 (end-script)
