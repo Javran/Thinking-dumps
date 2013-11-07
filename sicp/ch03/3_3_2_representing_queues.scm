51f7500d24b1279937151c75573520dc9503281c
Javran Cheng <Javran.c@gmail.com>
2013-11-06 23:13:36 -0500

    3.3.2 - few comment, + readability

--- sicp/ch03/3_3_2_representing_queues.scm
+++ sicp/ch03/3_3_2_representing_queues.scm
@@ -24,17 +24,21 @@
 (define front-queue (compose car front-ptr))
 
 (define (insert-queue! queue item)
+  ; make a new pair
   (let ((new-pair (cons item nil)))
     (cond ((empty-queue? queue)
+            ; front ptr = rear ptr = new one
             (set-front-ptr! queue new-pair)
             (set-rear-ptr! queue new-pair)
             queue)
           (else
+            ; attach new one after rear, update the rear
             (set-cdr! (rear-ptr queue) new-pair)
             (set-rear-ptr! queue new-pair)
             queue))))
 
 (define (delete-queue! queue)
+  ; move forward once, the first one got "disconnected"
   (set-front-ptr! queue (cdr (front-ptr queue))))
 
 (let ((q (make-queue)))
