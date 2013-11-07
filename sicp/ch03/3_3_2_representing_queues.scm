bfde9e34ca303a23b6862a78b5e0ad8f804385ae
Javran Cheng <Javran.c@gmail.com>
2013-11-06 23:00:04 -0500

    3.3.2 - code copied, want some test...

--- sicp/ch03/3_3_2_representing_queues.scm
+++ sicp/ch03/3_3_2_representing_queues.scm
@@ -11,4 +11,30 @@
 ;   (insert-queue! q i)
 ;   (delete-queue! q)
 
+; seems like primitives
+(define front-ptr car)
+(define rear-ptr cdr)
+(define set-front-ptr! set-car!)
+(define set-rear-ptr! set-cdr!)
+
+(define empty-queue? (compose null? front-ptr))
+(define (make-queue) (cons '() '()))
+
+; just let it crash
+(define front-queue (compose car front-ptr))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item nil)))
+    (cond ((empty-queue? queue)
+            (set-front-ptr! queue new-pair)
+            (set-rear-ptr! queue new-pair)
+            queue)
+          (else
+            (set-cdr! (rear-ptr queue) new-pair)
+            (set-rear-ptr! queue new-pair)
+            queue))))
+
+(define (delete-queue! queue)
+  (set-front-ptr! queue (cdr (front-ptr queue))))
+
 (end-script)
