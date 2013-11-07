9fc452f4831932ffcccd3fe34a08a50481445ccf
Javran Cheng <Javran.c@gmail.com>
2013-11-06 22:48:02 -0500

    Revert "ex 3.20 - working"
    This reverts commit 8cea5559cb77c92cd3d894b6712891ab280fb521.

--- sicp/ch03/exercise_3_20.scm
+++ sicp/ch03/exercise_3_20.scm
@@ -21,77 +21,9 @@
 (define (my-set-cdr! a v)
   ((a 'set-cdr!) v) v)
 
-
-#|
-
-G: (parent = nil)
-  my-cons = ...
-  my-car = ...
-  my-cdr = ...
-  my-set-car! = ...
-  my-set-cdr! = ...
-
-|#
-
 (define x (my-cons 1 2))
-
-#|
-
-G: (parent = nil)
-  ...
-  x = ..., env = E1
-
-E1: (parent = G)
-  x = 1
-  y = 2
-  set-x! = ...
-  set-y! = ...
-  dispatch = ...
-
-|#
-
 (define z (my-cons x x))
-
-#|
-
-G1: (parent = null)
-  ...
-  z = ..., env = E2
-
-E1: (parent = G)
-  ...
-
-E2: (parent = G)
-  x = ... (from x in G)
-  y = ... (from x in G)
-  set-x! = ...
-  set-y! = ...
-  dispatch = ...
-
-|#
-
 (my-set-car! (my-cdr z) 17)
-
-#|
-; evaluate (my-cdr z) first
-
-G: ...
-
-E1: ...
-
-E2: ...
-
-E3: (parent = G)
-  z = ... (from z in G)
-
-E4: (parent = E3)
-  m = 'cdr
-
-; then my-set-car!
-E5: (parent = G)
-
-|#
-
 (out (my-car x))
 
 (end-script)
