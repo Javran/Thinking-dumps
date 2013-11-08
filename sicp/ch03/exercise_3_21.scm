36f536fd08d0cedafee2a245c11a6715bc39a814
Javran Cheng <Javran.c@gmail.com>
2013-11-07 23:58:34 -0500

    ex 3.21 - reproduce problem

--- sicp/ch03/exercise_3_21.scm
+++ sicp/ch03/exercise_3_21.scm
@@ -1,4 +1,19 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(load "./3_3_2_representing_queues_procs.scm")
+
+(define q1 (make-queue))
+(insert-queue! q1 'a)
+(out q1)
+
+(insert-queue! q1 'b)
+(out q1)
+
+(delete-queue! q1)
+(out q1)
+
+(delete-queue! q1)
+(out q1)
+
 (end-script)
