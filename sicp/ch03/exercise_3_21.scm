97b577dbb58c763b4f74a6f4115ee6d86f82adcb
Javran Cheng <Javran.c@gmail.com>
2013-11-08 00:14:43 -0500

    ex 3.21 - explanation

--- sicp/ch03/exercise_3_21.scm
+++ sicp/ch03/exercise_3_21.scm
@@ -5,15 +5,38 @@
 
 (define q1 (make-queue))
 (insert-queue! q1 'a)
+; make a new pair: (cons 'a nil)
+; => (a)
+; front-ptr = a
+; rear-ptr = a
+; => (cons (cons 'a nil) (cons 'a nil))
+; because: '(a b) = (list 'a 'b) = (cons 'a (cons 'b nil))
+; => ((a) a)
 (out q1)
 
 (insert-queue! q1 'b)
+; make a new pair: (cons 'b nil)
+; (cdr a) set to (cons 'b nil)
+; => ((a b) a b)
+; then move rear-ptr
+; => ((a b) b)
+
 (out q1)
 
 (delete-queue! q1)
+; front-ptr move forward
+; ((b) b)
 (out q1)
 
 (delete-queue! q1)
+; front-ptr move forward
+; (() b)
 (out q1)
+
+; because the queue is indicated by the list
+;   that begins from the point where `front-ptr`
+;   points to what `rear-ptr` points to doesn't matter.
+; moveover, when the queue is empty, `rear-ptr` will be reset
+;   so the correctness is guaranteed
 
 (end-script)
