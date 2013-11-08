d8010c1717c3a5793414bae96618fb3630348810
Javran Cheng <Javran.c@gmail.com>
2013-11-07 23:56:03 -0500

    seperate queue impl as lib

--- sicp/ch03/3_3_2_representing_queues.scm
+++ sicp/ch03/3_3_2_representing_queues.scm
@@ -1,6 +1,7 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(load "./3_3_2_representing_queues_procs.scm")
 ; queue abstraction:
 ; constructor:
 ;   (make-queue)
@@ -10,36 +11,6 @@
 ; mutator:
 ;   (insert-queue! q i)
 ;   (delete-queue! q)
-
-; seems like primitives
-(define front-ptr car)
-(define rear-ptr cdr)
-(define set-front-ptr! set-car!)
-(define set-rear-ptr! set-cdr!)
-
-(define empty-queue? (compose null? front-ptr))
-(define (make-queue) (cons '() '()))
-
-; just let it crash
-(define front-queue (compose car front-ptr))
-
-(define (insert-queue! queue item)
-  ; make a new pair
-  (let ((new-pair (cons item nil)))
-    (cond ((empty-queue? queue)
-            ; front ptr = rear ptr = new one
-            (set-front-ptr! queue new-pair)
-            (set-rear-ptr! queue new-pair)
-            queue)
-          (else
-            ; attach new one after rear, update the rear
-            (set-cdr! (rear-ptr queue) new-pair)
-            (set-rear-ptr! queue new-pair)
-            queue))))
-
-(define (delete-queue! queue)
-  ; move forward once, the first one got "disconnected"
-  (set-front-ptr! queue (cdr (front-ptr queue))))
 
 (let ((q (make-queue)))
   ; insert: 1,2,3,4,5
