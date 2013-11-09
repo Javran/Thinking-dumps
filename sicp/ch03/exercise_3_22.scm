dc6318e7626baf4f0d91e165c5fad2e317d66db2
Javran Cheng <Javran.c@gmail.com>
2013-11-09 00:11:44 -0500

    ex 3.22 - done

--- sicp/ch03/exercise_3_22.scm
+++ sicp/ch03/exercise_3_22.scm
@@ -14,9 +14,26 @@
 (define (make-queue)
   (let ((front-ptr nil)
         (rear-ptr nil))
-    'todo
+    (define (empty?)
+      (null? front-ptr))
+    (define (front)
+      (car front-ptr))
+    (define (insert! i)
+      (let ((p (cons i nil)))
+        (if (empty?)
+          (begin
+            (set! front-ptr p)
+            (set! rear-ptr p))
+          (begin
+            (set-cdr! rear-ptr p)
+            (set! rear-ptr p)))))
+    (define (delete!)
+      (set! front-ptr (cdr front-ptr)))
     (define (dispatch m)
-      'todo)
+      (cond ((eq? m 'empty?) empty?)
+            ((eq? m 'front) front)
+            ((eq? m 'insert!) insert!)
+            ((eq? m 'delete!) delete!)))
     dispatch))
 
 (define (empty-queue? q)
@@ -31,5 +48,29 @@
 (define (delete-queue! q)
   ((q 'delete!)))
 
+(let ((q (make-queue)))
+  (for-each
+    ((curry2 insert-queue!) q)
+    '(1 2 3 4 5))
+  (define (pop-print-all q)
+    (let loop ()
+      (if (empty-queue? q)
+        'done
+        (begin 
+          (out (front-queue q))
+          (delete-queue! q)
+          (loop)))))
+  (pop-print-all q)
+  ; 1,2,3,4,5
+  (for-each
+    ((curry2 insert-queue!) q)
+    '(7 1 2 3))
+
+  (delete-queue! q)
+  (insert-queue! q 4)
+  (newline)
+  (pop-print-all q)
+  ; 1,2,3,4
+  )
 
 (end-script)
