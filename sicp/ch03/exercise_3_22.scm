de05dcb2fd46b2eb3053b81f813f36bca0a9ff22
Javran Cheng <Javran.c@gmail.com>
2013-11-08 23:52:48 -0500

    ex 3.22 - proxies

--- sicp/ch03/exercise_3_22.scm
+++ sicp/ch03/exercise_3_22.scm
@@ -1,13 +1,35 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+; queue abstraction:
+; constructor:
+;   (make-queue)
+; selector:
+;   (empty-queue? q)
+;   (front-queue q)
+; mutator:
+;   (insert-queue! q i)
+;   (delete-queue! q)
+
 (define (make-queue)
-  (let ((front-ptr 'todo)
-        (rear-ptr 'todo))
+  (let ((front-ptr nil)
+        (rear-ptr nil))
     'todo
     (define (dispatch m)
       'todo)
     dispatch))
+
+(define (empty-queue? q)
+  ((q 'empty?)))
+
+(define (front-queue q)
+  ((q 'front)))
+
+(define (insert-queue! q i)
+  ((q 'insert!) i))
+
+(define (delete-queue! q)
+  ((q 'delete!)))
 
 
 (end-script)
