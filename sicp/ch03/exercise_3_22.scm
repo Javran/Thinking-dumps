ccb4a9d90ce6e1a92df5a2d2299724cd06087fc4
Javran Cheng <Javran.c@gmail.com>
2013-11-08 23:41:11 -0500

    ex 3.22 - startup

---
+++ sicp/ch03/exercise_3_22.scm
@@ -0,0 +1,13 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(define (make-queue)
+  (let ((front-ptr 'todo)
+        (rear-ptr 'todo))
+    'todo
+    (define (dispatch m)
+      'todo)
+    dispatch))
+
+
+(end-script)
