24bca011015fa05ae3e0ff6a73e9b99a2b81af66
Javran Cheng <Javran.c@gmail.com>
2013-11-09 22:52:20 -0500

    ex 3.23 - abstraction

---
+++ sicp/ch03/exercise_3_23.scm
@@ -0,0 +1,16 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; deque abstraction:
+; constructor:
+;   make-deque
+; selector:
+;   front-deque
+;   rear-deque
+; mutator:
+;   front-insert-deque!
+;   rear-insert-deque!
+;   front-delete-deque!
+;   rear-delete-deque!
+
+(end-script)
