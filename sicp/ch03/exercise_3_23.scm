8248ae09dc11d4cd51052c67f6147a22947312ff
Javran Cheng <Javran.c@gmail.com>
2013-11-09 23:21:15 -0500

    ex 3.23 - front-insert

--- sicp/ch03/exercise_3_23.scm
+++ sicp/ch03/exercise_3_23.scm
@@ -4,6 +4,8 @@
 ; deque abstraction:
 ; constructor:
 ;   make-deque
+; predicate:
+;   empty-deque?
 ; selector:
 ;   front-deque
 ;   rear-deque
@@ -12,5 +14,42 @@
 ;   rear-insert-deque!
 ;   front-delete-deque!
 ;   rear-delete-deque!
+
+
+; actually the front-ptr is always the data container
+;   and rear-ptr keeps pointing to the last pair of
+;   that container (if possible)
+(define (make-deque)
+  (cons nil nil))
+
+(define front-ptr car)
+(define rear-ptr cdr)
+
+(define set-front-ptr! set-car!)
+(define set-rear-ptr! set-cdr!)
+
+(define empty-deque? (compose null? front-ptr))
+
+(define front-deque (compose car front-ptr))
+(define rear-deque (compose car rear-ptr))
+
+(define (front-insert-deque! q i)
+  (let ((pair (cons i nil)))
+    (if (empty-deque? q)
+      (begin
+        (set-front-ptr! q pair)
+        (set-rear-ptr! q pair))
+      (begin
+        (set-cdr! pair (front-ptr q))
+        (set-front-ptr! q pair)))))
+
+(let ((dq (make-deque)))
+  (for-each ((curry2 front-insert-deque!) dq)
+            '(3 2 1))
+  (out (front-ptr dq))
+  )
+
+
+
 
 (end-script)
