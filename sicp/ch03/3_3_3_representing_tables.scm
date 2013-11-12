3d9c5b5fab21c5ee2677cd208b3d5d4d5717d53d
Javran Cheng <Javran.c@gmail.com>
2013-11-11 23:32:27 -0500

    3.3.3 - my-assoc, few comments

--- sicp/ch03/3_3_3_representing_tables.scm
+++ sicp/ch03/3_3_3_representing_tables.scm
@@ -7,12 +7,19 @@
     '((a . 1) (b . 2) (c . 3))))
 
 (define (lookup key table)
-  (let ((record (assoc key (cdr table))))
+  (let ((record (my-assoc key (cdr table))))
     (if record
       (cdr record)
       false)))
 
+(define (my-assoc key records)
+  (cond ((null? records) #f)
+        ((equal? key (caar records)) (car records))
+        (else (my-assoc key (cdr records)))))
+
 (out
+  ; lookup elements corresponding to
+  ;   'a, 'b, 'c, 'd in t
   (map
     ((curry2 (flip lookup)) t)
     '(a b c d)))
