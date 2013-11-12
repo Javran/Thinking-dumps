6bdf009f6aeeff6c8b52f659a6fe469c73339de9
Javran Cheng <Javran.c@gmail.com>
2013-11-12 00:03:43 -0500

    3.3.3 - 2d-table test ok

--- sicp/ch03/3_3_3_two_dimentional_tables.scm
+++ sicp/ch03/3_3_3_two_dimentional_tables.scm
@@ -12,18 +12,45 @@
           #f))
       #f)))
 
-(define (insert! key-1 key-2 table)
+(define (insert! key-1 key-2 value table)
   (let ((subtable (assoc key-1 (cdr table))))
     (if subtable
       (let ((record (assoc key-2 (cdr subtable))))
         (if record
           (set-cdr! record value)
-          (set-cdr! (cons (cons key-2 value)
+          (set-cdr! subtable
+                    (cons (cons key-2 value)
                           (cdr subtable)))))
       (set-cdr! table
                 (cons (list key-1
                             (cons key-2 value))
                       (cdr table)))))
   'ok)
+
+; we can actually build 2d table using 1d table
+;   the idea is to let the table be
+;     key -> key-1
+;     val -> table of (key-2, value)
+; TODO: impl
+
+(define (make-table)
+  (list '*2d-table*))
+
+(let ((t (make-table)))
+  (insert! 'math '+ 43 t)
+  (insert! 'math '- 45 t)
+  (insert! 'math '* 42 t)
+  (insert! 'letters 'a 97 t)
+  (insert! 'letters 'b 98 t)
+
+  (let ((testcases
+          (list (mat 'math '+ t    43)
+                (mat 'letters 'b t 98)
+                (mat 'letters 'c t #f)
+                (mat 'math '* t    42)
+                (mat 'aaa 'bbb t   #f)
+                )))
+    (do-test lookup testcases))
+  )
 
 (end-script)
