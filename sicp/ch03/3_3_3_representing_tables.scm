2d7b178732c4036c14449925ada1d6b5cf04bfc5
Javran Cheng <Javran.c@gmail.com>
2013-11-12 23:37:47 -0500

    3.3.3 - seperate table impl

--- sicp/ch03/3_3_3_representing_tables.scm
+++ sicp/ch03/3_3_3_representing_tables.scm
@@ -1,27 +1,7 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
-(define (lookup key table)
-  (let ((record (my-assoc key (cdr table))))
-    (if record
-      (cdr record)
-      false)))
-
-(define (my-assoc key records)
-  (cond ((null? records) #f)
-        ((equal? key (caar records)) (car records))
-        (else (my-assoc key (cdr records)))))
-
-(define (insert! key value table)
-  (let ((record (my-assoc key (cdr table))))
-    (if record
-      (set-cdr! record value)
-      (set-cdr! table (cons (cons key value)
-                            (cdr table)))))
-  'ok)
-
-(define (make-table)
-  (list '*table*))
+(load "./3_3_3_representing_tables_lib.scm")
 
 (let ((t (cons
            '*table* 
