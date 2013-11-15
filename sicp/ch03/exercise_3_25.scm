c3ed568731bd80d063d3538b8cf3ccd0cfbe8afb
Javran Cheng <Javran.c@gmail.com>
2013-11-14 23:56:03 -0500

    ex 3.25 - broken impl

--- sicp/ch03/exercise_3_25.scm
+++ sicp/ch03/exercise_3_25.scm
@@ -12,9 +12,24 @@
         (lookupx (cdr keys) record)
         #f))))
 
+; insert and return the value
+(define (insertx! keys value table)
+  (if (null? keys)
+    value
+    (let ((record (lookup (car keys) table)))
+      (if record
+        (let ((return (insertx! (cdr keys) value record)))
+          (insert! (car keys) return record))
+        (let* ((subtable (make-table))
+               (return (insertx! (cdr keys) value subtable)))
+          (insert! (car keys) return subtable))))))
+
+
 (let ((test (make-tablex)))
   (insert! 'a 'val1 test)
   (insert! 'b 'val2 test)
-  (out (lookupx '(a) test)))
+  (out (lookupx '(a) test))
+  (insertx! '(a b c) 'val3 test)
+  (out test))
 
 (end-script)
