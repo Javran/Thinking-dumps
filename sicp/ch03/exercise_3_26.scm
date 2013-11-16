e33cac2b156b09aeb24afef4cb0d0ccd7a18cd82
Javran Cheng <Javran.c@gmail.com>
2013-11-15 23:23:34 -0500

    ex 3.26 - impl node for bin tree

--- sicp/ch03/exercise_3_26.scm
+++ sicp/ch03/exercise_3_26.scm
@@ -14,4 +14,25 @@
 ;   * to implement `insert!`, just follow the same way of `lookup`, find an empty leave
 ;   or find an existing key-value pair (depending on the `key`) and just insert the node
 
+(define (make-node key val)
+  (list key val nil nil))
+
+(define (key-node n)   (list-ref n 0))
+(define (val-node n)   (list-ref n 1))
+(define (ltree-node n) (list-ref n 2))
+(define (rtree-node n) (list-ref n 3))
+
+(define (set-key-node! n k)
+  (set-car! n k))
+(define (set-val-node! n v)
+  (set-car! (cdr n) v))
+(define (set-ltree-node! n l)
+  (set-car! (cddr n) l))
+(define (set-rtree-node! n r)
+  (set-car! (cdddr n) r))
+
+(define n (make-node 1 2))
+
+
+
 (end-script)
