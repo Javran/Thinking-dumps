e149ebc9da33e2b3c482a569d8bbedba82ef3378
Javran Cheng <Javran.c@gmail.com>
2013-11-16 00:15:51 -0500

    ex 3.26 - done and few comments

--- sicp/ch03/exercise_3_26.scm
+++ sicp/ch03/exercise_3_26.scm
@@ -30,14 +30,46 @@
   (lookup-intern key - (cdr table)))
 
 (define (insert! key val table)
+  ; mutator! is called when `insert-intern!` knows
+  ;   what is the value needed to insert in this place
+  ;   but doesn't know how to insert this value
   (define (insert-intern! key val cmp table mutator!)
     (if (null? table)
+      ; this node should be filled with a node,
+      ;   don't know how to do it, call mutator for help
       (mutator! (make-node key val))
       (let ((result (cmp key (key-node table))))
-        (cond ((< result 0) (insert-intern! key val cmp (ltree-node table) (lambda (new-ltree) (set-ltree-node! table new-ltree))))
-              ((> result 0) (insert-intern! key val cmp (rtree-node table) (lambda (new-rtree) (set-rtree-node! table new-rtree))))
+        (cond ((< result 0)
+                (insert-intern!
+                  key val cmp
+                  (ltree-node table)
+                  ; tell the callee, call this lambda
+                  ;   if you know what it should be
+                  ;   but don't know where it should be
+                  (lambda (new-ltree)
+                    (set-ltree-node! table new-ltree))))
+              ((> result 0)
+                (insert-intern!
+                  key val cmp
+                  (rtree-node table)
+                  (lambda (new-rtree)
+                    (set-rtree-node! table new-rtree))))
               ((= result 0) (set-val! table val))))))
-  (insert-intern! key val - (cdr table) (lambda (new-table) (set-cdr! table new-table))))
+  (insert-intern!
+    key val - (cdr table)
+    ; tell the callee how to manipulate the structure
+    (lambda (new-table) (set-cdr! table new-table))))
+
+(define (table->string t)
+  (define (table->string-intern t)
+    (if (null? t)
+      "nil"
+      (format #f "(k:~A v:~A l:~A r:~A)"
+              (key-node t)
+              (val-node t)
+              (table->string-intern (ltree-node t))
+              (table->string-intern (rtree-node t)))))
+  (table->string-intern (cdr t)))
 
 (define n (make-table))
 
@@ -48,5 +80,12 @@
 (insert! 3 'e n)
 (insert! 5 'f n)
 (insert! 7 'g n)
+
+(out (table->string n))
+; dump table to string and then output
+
+(out (map ((curry2 (flip lookup)) n)
+          (list-in-range 1 7)))
+; d b e a f c g
 
 (end-script)
