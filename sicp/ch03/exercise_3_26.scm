c0f8151de3f00910c5df1eaff6c9ede2ba9aabf4
Javran Cheng <Javran.c@gmail.com>
2013-11-16 00:01:16 -0500

    ex 3.26 - bin-tree impl

--- sicp/ch03/exercise_3_26.scm
+++ sicp/ch03/exercise_3_26.scm
@@ -14,24 +14,39 @@
 ;   * to implement `insert!`, just follow the same way of `lookup`, find an empty leave
 ;   or find an existing key-value pair (depending on the `key`) and just insert the node
 
-(define (make-node key val)
-  (let ((ltree nil)
-        (rtree nil))
-    (define (set-key! x) (set! key x) 'ok)
-    (define (set-val! x) (set! val x) 'ok)
-    (define (set-ltree! x) (set! ltree x) 'ok)
-    (define (set-rtree! x) (set! rtree x) 'ok)
+(load "./exercise_3_26_node.scm")
 
-    (define (dispatch m)
-      (cond ((eq? m 'key) key)
-            ((eq? m 'val) val)
-            ((eq? m 'set-key!) set-key!)
-            ((eq? m 'set-val!) set-val!)
-            ((eq? m 'ltree) ltree)
-            ((eq? m 'rtree) rtree)
-            ((eq? m 'set-ltree!) set-ltree!)
-            ((eq? m 'set-rtree!) set-rtree!)
-            ))
-    dispatch))
+(define (make-table)
+  (cons '*table-bintree* nil))
+
+(define (lookup key table)
+  (define (lookup-intern key cmp table)
+    (if (null? table)
+      #f
+      (let ((result (cmp key (key-node table))))
+        (cond ((< result 0) (lookup-intern key cmp (ltree-node table)))
+              ((> result 0) (lookup-intern key cmp (rtree-node table)))
+              ((= result 0) (val-node table))))))
+  (lookup-intern key - (cdr table)))
+
+(define (insert! key val table)
+  (define (insert-intern! key val cmp table mutator!)
+    (if (null? table)
+      (mutator! (make-node key val))
+      (let ((result (cmp key (key-node table))))
+        (cond ((< result 0) (insert-intern! key val cmp (ltree-node table) (lambda (new-ltree) (set-ltree-node! table new-ltree))))
+              ((> result 0) (insert-intern! key val cmp (rtree-node table) (lambda (new-rtree) (set-rtree-node! table new-rtree))))
+              ((= result 0) (set-val! table val))))))
+  (insert-intern! key val - (cdr table) (lambda (new-table) (set-cdr! table new-table))))
+
+(define n (make-table))
+
+(insert! 4 'a n)
+(insert! 2 'b n)
+(insert! 6 'c n)
+(insert! 1 'd n)
+(insert! 3 'e n)
+(insert! 5 'f n)
+(insert! 7 'g n)
 
 (end-script)
