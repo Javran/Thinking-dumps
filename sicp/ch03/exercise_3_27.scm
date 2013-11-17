bde819346fe808ae757fba4c0cc6eaf6bfec6014
Javran Cheng <Javran.c@gmail.com>
2013-11-17 00:07:27 -0500

    ex 3.27 - done

--- sicp/ch03/exercise_3_27.scm
+++ sicp/ch03/exercise_3_27.scm
@@ -58,6 +58,92 @@
   (time-test map memo2-fib queries))
 
 ; uncomment the following line for a heavy test
-(heavy)
+; (heavy)
+
+#|
+(memo-fib 3)
+; first calculate `memo-fib`
+(memoize f)
+; create new env E1(parent=G), f = (lambda (n) ...) 
+(let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result
+              (lookup x table)))
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result)))))
+; create new env E2(parent=E1), table = <impl hidden>
+(lambda (x)
+  (let ((previously-computed-result
+          (lookup x table)))
+    (or previously-computed-result
+        (let ((result (f x)))
+          (insert! x result table)
+          result))))
+; `memo-fib` evaluated
+; in global env G, bind `memo-fib` with this lambda
+; create new env E3(parent=E2), x=3
+(let ((previously-computed-result
+        (lookup x table)))
+  (or previously-computed-result
+      (let ((result (f x)))
+        (insert! x result table)
+        result)))
+; create new env E4(parent=E3), previously-computed-result= #f (not stored yet)
+(or previously-computed-result
+    (let ((result (f x)))
+      (insert! x result table)
+      result))
+; keep going
+(let ((result (f x)))
+  (insert! x result table)
+  result)
+; we need to evaluate (f x)
+; create new env E5(parent=E4), n = 3
+(cond ((= n 0) 0)
+      ((= n 1) 1)
+      (else (+ (memo-fib (- n 1))
+               (memo-fib (- n 2)))))
+; we need to evaluate (memo-fib 2)
+; `memo-fib` lies in G
+; create new env E6(parent=E2), x = 2
+(let ((previously-computed-result
+        (lookup x table)))
+  (or previously-computed-result
+      (let ((result (f x)))
+        (insert! x result table)
+        result)))
+; create new env E7(parent=E6), previously-computed-result = #f (not stored yet)
+(or previously-computed-result
+    (let ((result (f x)))
+      (insert! x result table)
+      result))
+; we need to evaluate (f x)
+; create new env E8(parent=E7), n = 2
+(cond ((= n 0) 0)
+      ((= n 1) 1)
+      (else (+ (memo-fib (- n 1))
+               (memo-fib (- n 2)))))
+; too verbose ... I'll omit all envs from now
+; x = 1, (memo-fib 1)
+; => previously-computed-result = #f
+; => n = 1, (lambda (n) ...) => 1
+; => (insert! x result table), x = 1, result = 1, table from E2
+; => result (1)
+; evaluate (memo-fib 0)
+; ...
+; => (insert! x result table), x = 0, result = 0, table from E2
+; back to (+ (memo-fib 1) (memo-fib 0))
+; => (insert! x result table), x = 2, result = 1, table from E2
+; back to (+ (memo-fib 2) (memo-fib 1))
+; => (+ 1 (memo-fib 1))
+; (memo-fib 1)
+; => 1 (result cached in table)
+; => (+ 1 1)
+; => (insert! x result table), x = 3, result = 2, table from E2
+; result = 2
+
+|#
 
 (end-script)
