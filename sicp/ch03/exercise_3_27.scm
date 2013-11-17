5c0647cb4dee118762770391f20c75428bb0a68c
Javran Cheng <Javran.c@gmail.com>
2013-11-16 22:41:41 -0500

    ex 3.27 - memoize2 inspired by y combinator

--- sicp/ch03/exercise_3_27.scm
+++ sicp/ch03/exercise_3_27.scm
@@ -27,8 +27,37 @@
             (else (+ (memo-fib (- n 1))
                      (memo-fib (- n 2))))))))
 
+(define (fib-pseudo f n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (f (- n 1))
+                 (f (- n 2))))))
+
+(define (memoize2 f)
+  (let ((mem (make-table)))
+    (define (memo-f n)
+      (let ((cached (lookup n mem)))
+        (if cached
+          cached
+          (let ((result (fib-pseudo memo-f n)))
+            (insert! n result mem)
+            result))))
+    memo-f))
+
+(define memo2-fib (memoize2 fib-pseudo))
+
 ; to see the speedup ...
-(out (time-test map fib      (list-in-range 1 20)))
-(out (time-test map memo-fib (list-in-range 1 20)))
+(out (time-test map fib       (list-in-range 1 20)))
+(out (time-test map memo-fib  (list-in-range 1 20)))
+(out (time-test map memo2-fib (list-in-range 1 20)))
+
+(define (heavy)
+  (define queries (map (lambda (x) (random-range-in 1 1000))
+                       (list-in-range 1 1000)))
+  (time-test map memo-fib  queries)
+  (time-test map memo2-fib queries))
+
+; uncomment the following line for a heavy test
+(heavy)
 
 (end-script)
