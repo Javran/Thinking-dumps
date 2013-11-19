fcf5e26ac857d42fd31bbaf3b0bc4a714dd04886
Javran Cheng <Javran.c@gmail.com>
2013-11-18 23:05:54 -0500

    ex 3.29 - done by needs test

--- sicp/ch03/exercise_3_29.scm
+++ sicp/ch03/exercise_3_29.scm
@@ -15,13 +15,35 @@
 
 
 #|
-           ------
-a -> o -> |      \
-          |  and  |-> o ->
-b -> o -> |      /
-           ------
+                   ------
+a -> o -> wire1 -> |      \
+                   |  and  |-> wire3 -> o ->
+b -> o -> wire2 -> |      /
+                   ------
 |#
 
+(define (valid-signal s)
+  (cond ((= s 0) #t)
+        ((= s 1) #t)
+        (else #f)))
 
+(define (or-gate a1 a2 output)
+  (assert (valid-signal a1))
+  (assert (valid-signal a2))
+  (let ((wire1 (make-wire))
+        (wire2 (make-wire))
+        (wire3 (make-wire)))
+    (inverter a wire1)
+    (inverter b wire2)
+    (and-gate wire1 wire2 wire3)
+    (inverter wire3 output)
+    'ok))
+
+; if signal is processed parallelly,
+;   or-gate-delay = inverter-delay*2 + and-gate-delay
+; else
+;   or-gate-delay = inverter-delay*3 + and-gate-delay
+
+; TODO: need ways to verify
 
 (end-script)
