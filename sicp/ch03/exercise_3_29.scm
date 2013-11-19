141a133f55181a8ee732bb508d0f38d2c693613f
Javran Cheng <Javran.c@gmail.com>
2013-11-18 22:54:34 -0500

    ex 3.29 - truth table

---
+++ sicp/ch03/exercise_3_29.scm
@@ -0,0 +1,16 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; truth table:
+; a b  '!a and !b' 'a or b'
+; 1 1  0            1
+; 1 0  0            1
+; 0 1  0            1
+; 0 0  1            0
+
+; (logical-or a b) = (logical-not
+;                      (logical-and
+;                        (logical-not a)
+;                        (logical-not b)))
+
+(end-script)
