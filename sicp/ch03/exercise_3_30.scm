32be78579fb15ccadfbbc2d98c3dcdf3d7535edd
Javran Cheng <Javran.c@gmail.com>
2013-11-20 22:03:04 -0500

    ex 3.30 - ascii art + readability improve

--- sicp/ch03/exercise_3_30.scm
+++ sicp/ch03/exercise_3_30.scm
@@ -20,18 +20,43 @@
     'ok))
 
 (define (ripple-carry-adder as bs ss c)
-  (define (ripple-carry-adder-intern as bs c0 ss c1)
+  (define (ripple-carry-adder-intern as bs cn ss c)
     (if (= (length as) 1)
       (begin
         (assert (= (length bs) 1))
         (assert (= (length ss) 1))
-        (full-adder (car as) (car bs) c0 (car ss) c1))
+        (full-adder (car as) (car bs) cn (car ss) c))
       (let ((c-mid (make-wire)))
-        (ripple-carry-adder-intern (cdr as) (cdr bs) c0 (cdr ss) c-mid)
-        (full-adder (car as) (car bs) c-mid (car ss) c1))))
-  (let ((c0 (make-wire)))
-    (set-signal! c0 0)
-    (ripple-carry-adder-intern as bs c0 ss c)
+        (ripple-carry-adder-intern (cdr as) (cdr bs) cn (cdr ss) c-mid)
+        (full-adder (car as) (car bs) c-mid (car ss) c))))
+  (let ((cn (make-wire)))
+    (set-signal! cn 0)
+    (ripple-carry-adder-intern as bs cn ss c)
     'ok))
+
+#|
+case 1: the last one
+ 
+    |An   |Bn   |Cn
+    v     v     v
+  +-+-----+-----+-+
+  |      FA       |
+  +----+------+---- 
+       |C     |Sn
+       v      v
+
+case 2: not the last one
+
+                +-----------+
+    |A1   |B1   |Cmid       |       |A2..An  |B2..Bn  |Cn
+    v     v     v           |       v        v        v       
+  +-+-----+-----+-+         |     +-+--------+--------+-+     
+  |      FA       |         |     |  ripple-carry-adder |     
+  +----+------+----         |     +----+------+---------+
+       |C     |S1           |          |Cmid  |S2..Sn    
+       v      v             +----------+      v        
+
+
+|#
 
 (end-script)
