65d2e9be1e046170ec4d5eec16584975073b3deb
Javran Cheng <Javran.c@gmail.com>
2013-11-25 23:42:01 -0500

    ex 3.28 - verified, done

--- sicp/ch03/exercise_3_28.scm
+++ sicp/ch03/exercise_3_28.scm
@@ -1,6 +1,8 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(load "./circuit_simulate.scm")
+
 (define (valid-signal s)
   (cond ((= s 0) #t)
         ((= s 1) #t)
@@ -23,6 +25,32 @@
   (add-action! a2 or-action-procedure)
   'ok)
 
-; TODO: no way to test for a while...
+; you might need to complete
+;   the all chapters about circuit simulation
+;   before you can use that.
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+
+(probe 'out-probe c)
+
+(or-gate a b c)
+
+(out "a -> 1")
+(set-signal! a 1)(propagate)
+; change to 1
+
+(out "b -> 1")
+(set-signal! b 1)(propagate)
+; nop
+
+(out "a -> 0")
+(set-signal! a 0)(propagate)
+; nop
+
+(out "b -> 0")
+(set-signal! b 0)(propagate)
+; change to 0
 
 (end-script)
