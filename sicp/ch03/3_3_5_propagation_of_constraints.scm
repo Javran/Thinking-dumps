88d14b86401c28ee0631b3029a0a0635b38f756c
Javran Cheng <Javran.c@gmail.com>
2013-11-27 22:20:55 -0500

    3.3.5 - propagation of constraints

---
+++ sicp/ch03/3_3_5_propagation_of_constraints.scm
@@ -0,0 +1,14 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; construct a constraint network
+;   that a single change in the network
+;   will cause all related variables to be changed
+;   accordingly
+
+; primitives:
+; * adder
+; * multiplier
+; * constant
+
+(end-script)
