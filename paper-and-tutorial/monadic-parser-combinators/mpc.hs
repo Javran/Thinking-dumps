9c4434cb25f8d64e6fce071d2369714a2b4dd7b1
Javran Cheng <Javran.c@gmail.com>
2013-11-29 18:21:59 -0500

    refactor till "3 parsers and monads"

--- paper-and-tutorial/monadic-parser-combinators/mpc.hs
+++ paper-and-tutorial/monadic-parser-combinators/mpc.hs
@@ -1,36 +1,39 @@
 import Control.Monad
 
-newtype Parser a = Parser (String -> [(a,String)])
+newtype Parser a = Parser
+    { runParser :: String -> [(a,String)]
+    }
 
-runParser :: Parser a -> String -> [(a,String)]
-runParser (Parser p) inp = p inp
+-- >>>> 2.2 primitive parsers
 
+-- return `v` without consuming anything
 result :: a -> Parser a
 result v = Parser $ \inp -> [(v,inp)]
 
+-- always fails
 zero :: Parser a
-zero = Parser $ \inp -> []
+zero = --Parser $ \inp -> []
+    Parser $ const []
 
+-- consume the first char, fail if this is impossible
 item :: Parser Char
 item = Parser $ \inp ->
     case inp of
-         [] -> []
+         []     -> []
          (x:xs) -> [(x,xs)]
 
+-- >>>> 2.3 parser combinators
 instance Monad Parser where
     return = result
-    (Parser m) >>= f = Parser $ \inp -> do
-        (v,inp') <- m inp
-        let (Parser newM) = f v
-        newM inp'
+    m >>= f = Parser $ \inp -> do
+        -- apply m on inp, which yields (v, inp')
+        (v,inp') <- runParser m inp
+        -- apply f on v, which ylelds next parser
+        -- run parser on the unconsumned parts inp'
+        runParser (f v) inp'
 
-instance MonadPlus Parser where
-    mzero = zero
-    mplus = plus
-
-inBetween :: (Ord a) => a -> a -> a -> Bool
-inBetween a b v = a <= v && v <= b
-
+-- consume and test if the first character satisfies
+--   a predicate
 sat :: (Char -> Bool) -> Parser Char
 sat p = do
     x <- item
@@ -38,38 +41,52 @@ sat p = do
        then return x
        else zero
 
+-- consume an exact `x`
 char :: Char -> Parser Char
 char x = sat (\y -> x == y)
 
+-- test if an Ord is within a given range
+inBetween :: (Ord a) => a -> a -> a -> Bool
+inBetween a b v = a <= v && v <= b
+
+-- consume a digit
 digit :: Parser Char
 digit = sat $ inBetween '1' '9'
 
+-- consume a lower case char
 lower :: Parser Char
 lower = sat $ inBetween 'a' 'z'
 
+-- consume a upper case char
 upper :: Parser Char
 upper = sat $ inBetween 'A' 'Z'
 
+-- use p and q to parse the same string
+--   return all possibilities
 plus :: Parser a -> Parser a -> Parser a
-p `plus` q = Parser $ f
-    where 
-        f inp = m1 ++ m2
-            where m1 = runParser p inp
-                  m2 = runParser q inp
+p `plus` q = Parser $ \inp ->
+    runParser p inp ++ runParser q inp
 
+-- consume letters (i.e. lower / upper)
 letter :: Parser Char
 letter = lower `plus` upper
 
+-- consume alphanum (i.e. letter / digit)
 alphanum :: Parser Char
 alphanum = letter `plus` digit
 
+-- consume a word (i.e. consecutive letters)
 word :: Parser String
-word = neWord `plus` result ""
+word = newWord `plus` result ""
     where
-        neWord = do
+        newWord = do
             x <- letter
             xs <- word
             return (x:xs)
+
+instance MonadPlus Parser where
+    mzero = zero
+    mplus = plus
 
 main = do
     let result = runParser word "Yes!"
