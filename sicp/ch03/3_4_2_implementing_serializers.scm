16b292abd20363be61d4943164d5565b62d73f37
Javran Cheng <Javran.c@gmail.com>
2013-12-14 23:32:29 -0500

    test-and-set!

--- sicp/ch03/3_4_2_implementing_serializers.scm
+++ sicp/ch03/3_4_2_implementing_serializers.scm
@@ -10,7 +10,7 @@
 ;     is released
 ; * release
 
-; need implementation of :
+; need implementation of:
 ; * make-mutex
 ; * (<mutex> 'acquire)
 ; * (<mutex> 'release)
@@ -24,5 +24,28 @@
           (mutex 'release)
           val))
       serialized-p)))
+
+(define (make-mutex)
+  (let ((cell (list false)))
+    (define (the-mutex m)
+      (cond ((eq? m 'acquire)
+              (if (test-and-set! cell)
+                (the-mutex 'acquire))) ; retry(warning: busy waiting)
+            ((eq? m 'release)
+              (clear! cell))))
+    the-mutex))
+
+; need implementation of:
+; * test-and-set!
+; * clear!
+
+; warning: this operation need to be atomic
+;   and the implementation here does not guarantee this.
+(define (test-and-set! cell)
+  (if (car cell)
+    true
+    (begin
+      (set-car! cell true)
+      false)))
 
 (end-script)
