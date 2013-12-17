8c42f6947ad3688d70053e293f0a15c38640ac4e
Javran Cheng <Javran.c@gmail.com>
2013-12-16 21:27:00 -0500

    ex 3.47.b - done

--- sicp/ch03/exercise_3_47.scm
+++ sicp/ch03/exercise_3_47.scm
@@ -2,7 +2,7 @@
 (load "../common/test-utils.scm")
 
 ; a. in terms of mutexes
-(define (semaphore n)
+(define (semaphore-a n)
   (let ((m (make-mutex))
         (count n))
     (define (the-semaphore msg)
@@ -26,6 +26,35 @@
                 (error "Unknown message:" msg)))))
     the-semaphore))
 
-; b. in terms of atomic test-and-test! operations
+; b. in terms of atomic test-and-set! operations
+; actually I don't know what makes the difference
+;   or is there any difference essentially?
+
+(define (semaphore-b n)
+  (let ((cell-count (list #f))
+        (count n))
+    (define (the-semaphore msg)
+      (if (test-and-set! cell-count)
+        ; keep waiting
+        (the-semaphore msg)
+        ; else keep going
+        (cond ((eq? msg 'acquire)
+                (if (> count 0)
+                  ; still can acquire
+                  (begin
+                    (set! count (- count 1))
+                    (clear! cell-count))
+                  ; need to wait
+                  (begin
+                    (clear! cell-count)
+                    (the-semaphore 'acquire))))
+              ((eq? msg 'release)
+                (set! count (+ count 1))
+                (clear! cell-count))
+              (else
+                (begin
+                  (clear! cell-count)
+                  (error "Unknown message:" msg))))))
+    the-semaphore))
 
 (end-script)
