1b0fe7488cb5bafae5076f8095e9e6f09f5ef6c4
Javran Cheng <Javran.c@gmail.com>
2013-12-16 23:04:10 -0500

    bank account exchange - basic functions

--- sicp/ch03/bank_account_exchange.rkt
+++ sicp/ch03/bank_account_exchange.rkt
@@ -8,21 +8,60 @@
 ;   and the final account balances
 ;   they should be the same.
 
+(define (out . args)
+  (for-each
+    (lambda (x) (display x) (newline))
+    args))
+
 ; each serializer needs an unique id in our implementation
+; doc: http://docs.racket-lang.org/reference/semaphore.html
 (define (make-serializer id)
-  #f)
+  (let ((sema (make-semaphore 1)))
+    (define (serialize p)
+      (define (serialized-p . args)
+        (semaphore-wait sema)
+        (let ((val (apply p args)))
+          (semaphore-post sema)
+          val))
+      serialized-p)
+    (define (dispatch m)
+      (cond ((eq? m 'serialize) serialize)
+            ((eq? m 'id) id)))
+    dispatch))
+
+(define (serializer-id s)
+  (s 'id))
+(define (serializer-proc s p)
+  ((s 'serialize) p))
+
+; it depends on the proper way of comparing id
+;   here I assume it being integers.
+(define (serializer-less-than? s1 s2)
+  (< (serializer-id s1)
+     (serializer-id s2)))
+
+(define *max-id* 0)
+
+; return the old of `*max-id*`,
+;   while increasing its value by 1
+(define (new-id)
+  (let ((val *max-id*))
+    (set! *max-id* (+ *max-id* 1))
+    val))
 
 (define (make-account balance)
+  ; not serialized version
   (define (withdraw amount)
     (if (>= balance amount)
       (begin
         (set! balance (- balance amount))
         balance)
       (error "Insufficient funds")))
+  ; not serialized version
   (define (deposit amount)
     (set! balance (+ balance amount))
     balance)
-  (let ((balance-serializer (make-serializer)))
+  (let ((balance-serializer (make-serializer (new-id))))
     (define (dispatch m)
       (cond ((eq? m 'withdraw) withdraw)
             ((eq? m 'deposit) deposit)
@@ -32,3 +71,50 @@
               (error "Unknown request: MAKE-ACCOUNT"
                      m))))
     dispatch))
+
+(define (exchange acc1 acc2)
+  (let ((difference (- (acc2 'balance)
+                       (acc1 'balance))))
+    ; a+(b-a) = b
+    ((acc1 'deposit) difference)
+    ; b-(b-a) = a
+    ((acc2 'withdraw) difference)
+    'done))
+
+; (apply-functions (list f g h) proc)
+; => (h (g (f proc)))
+(define (apply-functions functions proc)
+  (if (null? functions)
+    proc
+    (apply-functions
+      (cdr functions)
+      ((car functions) proc))))
+
+(define (serialize-account-operation op)
+  ; Assume all args are account objects
+  (lambda args
+    (let ((sorted-serializers
+            (sort
+              (map (lambda (x) (x 'serializer))
+                   args)
+              serializer-less-than?))
+          (serializer->serialize
+            (lambda (s)
+              (lambda (p)
+                (serializer-proc s p)))))
+      (apply
+        (apply-functions
+          (map serializer->serialize 
+               sorted-serializers)
+          op)
+        args))))
+
+(define serialized-exchange
+  (serialize-account-operation exchange))
+
+(define acc1 (make-account 100))
+(define acc2 (make-account 400))
+
+(out (acc1 'balance) (acc2 'balance))
+(serialized-exchange acc1 acc2)
+(out (acc1 'balance) (acc2 'balance))
