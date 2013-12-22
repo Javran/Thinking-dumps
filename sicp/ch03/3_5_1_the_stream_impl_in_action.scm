23312b4b1dceb1de03d94850a9077c80234e4519
Javran Cheng <Javran.c@gmail.com>
2013-12-21 23:46:44 -0500

    stream filter impl

--- sicp/ch03/3_5_1_the_stream_impl_in_action.scm
+++ sicp/ch03/3_5_1_the_stream_impl_in_action.scm
@@ -32,4 +32,48 @@
 
 (prime-computation)
 
+(define (my-stream-filter pred stream)
+  ; here even if the part
+  ;   `(my-stream-filter pred (stream-cdr stream)`
+  ; is the same, we cannot factor it out simply
+  ; because if we write it in a `let`, we cannot
+  ; control when it will be evaluated.
+  ; a workaround to get rid of redundant code
+  ; will be better.
+  (cond ((stream-null? stream)
+          the-empty-stream)
+        ((pred (stream-car stream))
+          (cons-stream (stream-car stream)
+                       (my-stream-filter
+                         pred
+                         (stream-cdr stream))))
+        (else
+          (my-stream-filter
+            pred
+            (stream-cdr stream)))))
+
+(define (stream->list s)
+  (if (stream-null? s)
+    nil
+    (cons (stream-car s)
+          (stream->list (stream-cdr s)))))
+
+(define (my-stream-filter-test)
+  (out "result from stream:")
+  (out (stream->list
+       (my-stream-filter
+         prime?
+         (stream-enumerate-inteval
+           10000
+           10100))))
+  (out "result from list:")
+  (out (filter
+         prime?
+         (list-in-range
+           10000
+           10100))))
+
+; uncomment to see the test
+(my-stream-filter-test)
+
 (end-script)
