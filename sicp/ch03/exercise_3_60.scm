a534fa3e202b740abdfa2cec106f5f94205900f8
Javran Cheng <Javran.c@gmail.com>
2013-12-25 14:33:10 -0500

    ex 3.60 - done

--- sicp/ch03/exercise_3_60.scm
+++ sicp/ch03/exercise_3_60.scm
@@ -22,20 +22,47 @@
 
 (load "./exercise_3_59_common.scm")
 
-(define result
-  (add-streams
-    (mul-series sine-series sine-series)
-    (mul-series cosine-series cosine-series)))
-
-(out
-  "result:"
-  (exact->inexact (series-sum 10 result)))
-
 (define (powers x)
   (define powers-aux
     (cons-stream
       1
       (scale-stream powers-aux x)))
   powers-aux)
+
+; verify that our estimation is correct
+(out (exact->inexact 
+       (series-sum
+         10 
+         ((zip-streams-with *) (powers 1.1) sine-series))))
+(out (sin 1.1))
+
+(out (exact->inexact 
+       (series-sum
+         10 
+         ((zip-streams-with *) (powers 1.1) cosine-series))))
+(out (cos 1.1))
+(newline)
+
+(define (result-x x)
+  (let ((  sine-series-x ((zip-streams-with *)
+                           (powers x)
+                             sine-series))
+        (cosine-series-x ((zip-streams-with *)
+                           (powers x)
+                           cosine-series)))
+    (add-streams
+      (mul-series   sine-series-x   sine-series-x)
+      (mul-series cosine-series-x cosine-series-x))))
+
+(define (test x)
+  (format #t
+          "result (x=~A):~%~A~%"
+          x
+          (exact->inexact
+            (series-sum 10 (result-x x)))))
+
+(test 1)
+(test 0.1)
+(test 1.1)
 
 (end-script)
