9d43aa1514b7ce28a4a755f6f8b5d92f8100140c
Javran Cheng <Javran.c@gmail.com>
2013-12-25 11:57:35 -0500

    ex 3.59.a - stream of x^0, x^1, x^2 ...

--- sicp/ch03/exercise_3_59_a.scm
+++ sicp/ch03/exercise_3_59_a.scm
@@ -14,4 +14,17 @@
   (integrate-series
     (scale-stream ones 17)))
 
+(define (x-seq x)
+  ; (x^0, x^1, x^2, ...)
+  ; => (x-seq x) = (1, x * (x-seq x))
+  (define x-seq-aux 
+    (cons-stream
+      1
+      (scale-stream x-seq-aux x)))
+  x-seq-aux)
+
+(print-few
+  10
+  (x-seq 2))
+
 (end-script)
