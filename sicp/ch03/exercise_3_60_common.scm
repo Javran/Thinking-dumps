fbd0e9e969ca7f67e9e281c535c4f9020178c1af
Javran Cheng <Javran.c@gmail.com>
2013-12-26 16:09:10 -0500

    ex 3.60 - fix laziness

--- sicp/ch03/exercise_3_60_common.scm
+++ sicp/ch03/exercise_3_60_common.scm
@@ -1,16 +1,14 @@
-; see "./exercise_3_60.pdf" for demonstration
+; see "./exercise_3_60.pdf" for demonstr(stream-cdr s1)ion
 
+; cannot use `let` here, which violates the intention of
+;   delayed computation
 (define (mul-series s1 s2)
-  (let ((ah (stream-car s1))
-        (at (stream-cdr s1))
-        (bh (stream-car s2))
-        (bt (stream-cdr s2)))
     (cons-stream
-      (* ah bh)
+      (* (stream-car s1) (stream-car s2))
       (add-streams
         (add-streams
-          (scale-stream at bh)
-          (scale-stream bt ah))
+          (scale-stream (stream-cdr s1) (stream-car s2))
+          (scale-stream (stream-cdr s2) (stream-car s1)))
         (cons-stream
           0
-          (mul-series at bt))))))
+          (mul-series (stream-cdr s1) (stream-cdr s2))))))
