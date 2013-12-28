c5b3b60dcd00e47a5934498122e2ed64c3f4c027
Javran Cheng <Javran.c@gmail.com>
2013-12-28 15:32:18 -0500

    3.5.3 - impl my pi-stream

--- sicp/ch03/3_5_3_formulating_iter_as_stream_procs.scm
+++ sicp/ch03/3_5_3_formulating_iter_as_stream_procs.scm
@@ -17,5 +17,48 @@
   guesses)
 
 (display-stream (take 5 (sqrt-stream 2)))
+(newline)
+
+
+; write my own version of pi-approximation
+(define (stream-sum s)
+  ; (stream-sum s) = s0, s0+s1, s0+s1+s2, ...
+  ;   where s = s0, s1, s2, ...
+  (define stream-sum-aux
+    (cons-stream
+      0
+      (add-streams s stream-sum-aux)))
+  (drop 1 stream-sum-aux))
+
+(define my-pi-stream
+  ; make an env that allows local definitions
+  ((lambda ()
+    ; 1, 3, 5, 7, ...
+    (define odd-stream
+      (cons-stream
+        1
+        (stream-map
+          ((curry2 +) 2)
+          odd-stream)))
+    ; 1, -1, 1, -1, ...
+    (define sign-stream
+      (cons-stream
+        1
+        (stream-map
+          -
+          sign-stream)))
+    ; 1, -1/3, 1/5, -1/7, ...
+    (define frac-stream
+      ((zip-streams-with
+         (lambda (sign odd)
+           (* sign (/ 1 odd))))
+       sign-stream
+       odd-stream))
+    (scale-stream
+      (stream-sum frac-stream)
+      4))))
+
+(display-stream
+  (take 8 (stream-map exact->inexact my-pi-stream)))
 
 (end-script)
