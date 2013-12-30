67b4e336c8840c3e3dde31e3fc2264d61b729913
Javran Cheng <Javran.c@gmail.com>
2013-12-30 00:41:35 -0500

    ex 1.17 - done

---
+++ essentials-of-programming-languages/ex-1.17.rkt
@@ -0,0 +1,14 @@
+#lang eopl
+
+(require "./common.rkt")
+
+; down :: [a] -> [[a]]
+; usage: wrap parentheses around each element of `lst`
+(define (down lst)
+  (map list lst))
+
+(out (down '(1 2 3))
+     (down '((a) (fine) (idea)))
+     (down '(a (more (complicated)) object))
+     (down '(foo () bar ()))
+     )
