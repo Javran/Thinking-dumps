b0797b87349d56a0b098fb4d2533907223bed29d
Javran Cheng <Javran.c@gmail.com>
2013-12-30 01:04:10 -0500

    ex 1.20 - done

--- essentials-of-programming-languages/common.rkt
+++ essentials-of-programming-languages/common.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (provide out)
+(provide foldl)
 
 ; output all arguments, line-by-line
 (define out
@@ -8,3 +9,12 @@
     (for-each
       (lambda (a) (display a) (newline))
       args)))
+
+; fold from left using `proc`
+(define (foldl proc init lst)
+  (if (null? lst)
+    init
+    (foldl
+      proc 
+      (proc init (car lst))
+      (cdr lst))))
---
+++ essentials-of-programming-languages/ex-1.20.rkt
@@ -0,0 +1,22 @@
+#lang eopl
+
+(require "./common.rkt")
+
+; count-occurrences :: (Sym, [SExp]) -> Int
+; usage: count occurrences of `s` in `slist`
+(define (count-occurrences s slist)
+  ; count occurrence on a s-exp
+  (define (count-occur-aux sexp)
+    (if (symbol? sexp)
+      (if (eq? s sexp) 1 0)
+      (count-occurrences s sexp)))
+  (foldl + 0
+         (map count-occur-aux slist)))
+
+(out (count-occurrences 'x '((f x) y (((x z) x))))
+     (count-occurrences 'x '((f x) y (((x z) () x))))
+     (count-occurrences 'w '((f x) y (((x z) x))))
+     (count-occurrences 'f '((f x) y (((x z) x))))
+     (count-occurrences 'y '((f x) y (((x z) x))))
+     (count-occurrences 'z '((f x) y (((x z) x))))
+     )
