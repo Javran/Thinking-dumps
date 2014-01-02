0ba46d99b1fea3c1fd0f1c948396fd355f3af251
Javran Cheng <Javran.c@gmail.com>
2014-01-01 22:19:32 -0500

    ex 3.70 - done

--- sicp/ch03/exercise_3_70.scm
+++ sicp/ch03/exercise_3_70.scm
@@ -34,10 +34,40 @@
 
 (define order-a ((curry2 apply) +))
 
+(define (order-b xs)
+  (let ((i (car xs))
+        (j (cadr xs)))
+    (+ (* 2 i)
+       (* 3 j) 
+       (* 5 i j))))
+
+(define (valid xs)
+  (define (divisible x y)
+    (= 0 (remainder x y)))
+  (define (not-divisible-by x ys)
+    (if (null? ys)
+      #t
+      (and (not (divisible x (car ys)))
+           ; short-circuit
+           (not-divisible-by x (cdr ys)))))
+  (and (not-divisible-by (car xs) '(2 3 5))
+       (not-divisible-by (cadr xs) '(2 3 5))))
+
 ; a. ordered according to the sum `i+j`
 (define stream-a
   (weighted-pairs integers integers order-a))
 
 (print-few 20 stream-a)
+
+; b. ordered according to the sum `2i+3j+5ij`,
+;  neither i nor j is divisible by 2,3 or 5.
+(define stream-b
+  (stream-filter
+    valid
+    (weighted-pairs integers integers order-b)))
+
+; see "./exercise_3_70_verify.hs" for verification
+
+(print-few 20 stream-b)
 
 (end-script)
