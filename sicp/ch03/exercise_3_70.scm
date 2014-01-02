79043a5a03369043a38c7ffe13419930c4d4572e
Javran Cheng <Javran.c@gmail.com>
2014-01-01 21:58:13 -0500

    ex 3.70 - a - done

--- sicp/ch03/exercise_3_70.scm
+++ sicp/ch03/exercise_3_70.scm
@@ -11,25 +11,33 @@
                 (s2h (head s2)))
             (let ((s1hw (weight s1h))
                   (s2hw (weight s2h)))
-              (cond ((< s1hw s2hw)
+              (cond ((<= s1hw s2hw)
                       (cons-stream
                         s1h
                         (merge-weighted (tail s1) s2 weight)))
                     ((> s1hw s2hw)
                       (cons-stream
                         s2h
-                        (merge-weighted s1 (tail s2) weight)))
-                    (else
-                      (cons-stream
-                        s1h
-                        (merge-weighted
-                          (tail s1)
-                          (tail s2)
-                          weight)))))))))
+                        (merge-weighted s1 (tail s2) weight)))))))))
 
-(define x (cons-stream 1 (scale-stream x 2)))
-(define y (cons-stream 1 (scale-stream y 3)))
+(define (weighted-pairs s t weight)
+  ; assumption: 
+  ;   (w (list (head s) (head t))) has the lowest value
+  (cons-stream
+    (list (head s) (head t))
+    (merge-weighted
+      (stream-map (lambda (x)
+                    (list (head s) x))
+                  (tail t))
+      (weighted-pairs (tail s) (tail t) weight)
+      weight)))
 
-(print-few 10 (merge-weighted x y identity))
+(define order-a ((curry2 apply) +))
+
+; a. ordered according to the sum `i+j`
+(define stream-a
+  (weighted-pairs integers integers order-a))
+
+(print-few 20 stream-a)
 
 (end-script)
