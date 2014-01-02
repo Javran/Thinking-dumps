3e826078582a897fb31f81d2becd39997720e5e0
Javran Cheng <Javran.c@gmail.com>
2014-01-01 23:32:14 -0500

    ex 1.25 - done

---
+++ essentials-of-programming-languages/ex-1.25.rkt
@@ -0,0 +1,16 @@
+#lang eopl
+
+(require "./common.rkt")
+
+; exists? :: (a -> Bool) -> [a] -> Bool
+; usage: return #t if there is an element in `lst`
+;   that satisfies `pred`, #f elsewise
+(define (exists? pred lst)
+  (if (null? lst)
+    #f
+    (or (pred (car lst))
+        (exists? pred (cdr lst)))))
+
+(out (exists? number? '(a b c 3 e))
+     (exists? number? '(a b c d e))
+     (exists? number? '(a a a)))
