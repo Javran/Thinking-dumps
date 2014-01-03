1f61c536ef421424d5b9a4b09d892babd5876143
Javran Cheng <Javran.c@gmail.com>
2014-01-03 17:28:00 -0500

    3.5.3 - estimate cos(x)

--- sicp/ch03/3_5_3_streams_as_signals.scm
+++ sicp/ch03/3_5_3_streams_as_signals.scm
@@ -17,4 +17,40 @@
 ; cos x = 1 - int(sin x dx)
 ; cos x = int( -sinx dx) + 1
 
+(define (estimate-cos upbound eps)
+  (define range
+    (scale-stream integers eps))
+  (define neg-sins
+    ; x -> - sin x
+    (stream-map
+      (compose - sin)
+      range))
+  (define the-int
+    (integral neg-sins 1 eps))
+  (define estimated-val 
+    (cdr
+      (head
+        ; drop until we've reached the upbound
+        (drop-while
+          (lambda (pair)
+            ; pair: (cons <x> < -sin x >)
+            (<= (car pair) upbound))
+          ((zip-streams-with cons)
+           range
+           the-int)))))
+  (format
+    #t
+    "x         = ~A~%~
+     cos x     = ~A~%~
+     eps       = ~A~%~
+     estimated = ~A~%~
+     diff      = ~A~%"
+    upbound
+    (cos upbound)
+    eps
+    estimated-val
+    (abs (- (cos upbound) estimated-val))))
+
+(estimate-cos 0.25 1e-6)
+
 (end-script)
