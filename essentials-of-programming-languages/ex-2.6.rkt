95d21405355b793bb85714be30758c4ef78f446a
Javran Cheng <Javran.c@gmail.com>
2014-01-05 00:04:21 -0500

    ex 2.6 - impl 2

--- essentials-of-programming-languages/ex-2.6.rkt
+++ essentials-of-programming-languages/ex-2.6.rkt
@@ -35,4 +35,47 @@
 
     (list empty-env extend-env apply-env)))
 
+
+(define impl2
+  (let ()
+    ; implementation 2: another alist 
+    ;   use alist but every represetation holds the entire binding list 
+
+    ; del-assv: Key x AList -> AList
+    ; usage: remove key `obj` from `alist`
+    (define (del-assv obj alist)
+      (cond ((null? alist) '())
+            ((eqv? (caar alist) obj)
+              (cdr alist))
+            (else
+              (cons (car alist)
+                    (del-assv obj (cdr alist))))))
+
+    ; assv: Key x AList -> #f or (Key, Value)
+    (define (assv obj alist)
+      (cond ((null? alist) #f)
+            ((eqv? (caar alist) obj)
+              (car alist))
+            (else
+              (assv obj (cdr alist)))))
+
+    ; empty-env: () -> Env
+    (define (empty-env)
+      '())
+
+    ; extend-env: Var x SchemeVal X Env -> Env
+    (define (extend-env key val env)
+      (cons (cons key val)
+            (del-assv key env)))
+
+    ; apply-env: Env x Var -> SchemeVal
+    (define (apply-env env var)
+      (let ((result (assv var env)))
+        (if result
+          (cdr result)
+          (report-no-binding-found var))))
+
+    (list empty-env extend-env apply-env)))
+
 (apply do-env-test impl1)
+(apply do-env-test impl2)
