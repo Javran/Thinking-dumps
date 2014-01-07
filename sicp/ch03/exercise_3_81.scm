1029d6b9c11dedd023c888cb0f1623eca44d7c9d
Javran Cheng <Javran.c@gmail.com>
2014-01-07 17:35:24 -0500

    ex 3.81 - impl

---
+++ sicp/ch03/exercise_3_81.scm
@@ -0,0 +1,7 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+; I'm afraid there's no simple way of setting a random seed
+;   using a specified number, let's try to do this in racket.
+
+(end-script)
