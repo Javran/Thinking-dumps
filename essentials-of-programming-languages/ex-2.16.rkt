1aecfb6503f5c23862718aede21496751cd1888a
Javran Cheng <Javran.c@gmail.com>
2014-01-08 23:10:25 -0500

    ex 2.16 - done

---
+++ essentials-of-programming-languages/ex-2.16.rkt
@@ -0,0 +1,97 @@
+#lang eopl
+
+(require "./common.rkt")
+
+; modified from "./ex-2.15.rkt"
+
+; # interfaces:
+; ## constructors
+; * var-exp
+; * lambda-exp
+; * app-exp
+; ## predicates
+; * var-exp?
+; * lambda-exp?
+; * app-exp?
+; ## extractors
+; * var-exp->var
+; * lambda-exp->bound-var
+; * lambda-exp->body
+; * app-exp->rator
+; * app-exp->rand
+
+; ==== constructors ====
+
+; var-exp: Var -> LcExp
+(define var-exp identity)
+
+; lambda-exp: Var x LcExp -> LcExp
+(define (lambda-exp var e)
+  (list
+    'lambda
+    ; modification #1: use `var` directly
+    var
+    e))
+
+; app-exp: LcExp x LcExp -> LcExp
+(define (app-exp operator-e operand-e)
+  (list
+    operator-e
+    operand-e))
+
+; ==== predicates ====
+
+; for simplicity, I assume the argument
+;   is guaranteed to be constructed properly.
+;   so I ignore all error handling
+;   regarding ill-formed values
+
+; var-exp?: LcExp -> Bool
+(define var-exp? symbol?)
+
+; lambda-exp?: LcExp -> Bool
+(define (lambda-exp? e)
+  (and (list? e)
+       (eq? (car e) 'lambda)))
+
+; app-exp?: LcExp -> Bool
+(define (app-exp? e)
+  (and (list? e)
+       (list? (car e))))
+
+; ==== extractors ====
+; var-exp->var: LcExp -> Var
+(define var-exp->var identity)
+
+; lambda-exp->bound-var: LcExp -> Var
+(define lambda-exp->bound-var
+  ; modification #2: accessor
+  cadr)
+
+; lambda-exp->body: LcExp -> LcExp
+(define lambda-exp->body caddr)
+
+; app-exp->rator: LcExp -> LcExp
+; usage: fetch the (ope)rator from an application
+(define app-exp->rator car)
+
+; app-exp->rand: LcExp -> LcExp
+; usage: fetch the (ope)rand from an application
+(define app-exp->rand cadr)
+
+(require "./ex-2.15-test.rkt")
+
+(do-lc-exp-test
+  var-exp
+  lambda-exp
+  app-exp
+  ;
+  var-exp?
+  lambda-exp?
+  app-exp?
+  ;
+  var-exp->var
+  lambda-exp->bound-var
+  lambda-exp->body
+  app-exp->rator
+  app-exp->rand)
