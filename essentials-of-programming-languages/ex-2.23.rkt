b57bd61f21b68ac44394be73c6f8eaf79bccc01d
Javran Cheng <Javran.c@gmail.com>
2014-01-10 21:07:32 -0500

    ex 2.23 - done

---
+++ essentials-of-programming-languages/ex-2.23.rkt
@@ -0,0 +1,23 @@
+#lang eopl
+
+(require "./common.rkt")
+(require "./test-utils.rkt")
+
+(define (identifier? s)
+  (and (not (eq? 'lambda s))
+       symbol?)) 
+
+(define-datatype lc-exp lc-exp?
+  (var-exp
+    (var identifier?))
+  (lambda-exp
+    (bound-var identifier?)
+    (body lc-exp?))
+  (app-exp
+    (rator lc-exp?)
+    (rand lc-exp?)))
+
+; will raise error when uncommented
+; (define exp-test1 (var-exp 'lambda))
+
+(define exp-test2 (var-exp 'x))
