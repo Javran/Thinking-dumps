d9dc7a0f4d4bda6272be94f4af783bce2c32d0e9
Javran Cheng <Javran.c@gmail.com>
2014-01-13 00:44:22 -0500

    ex 4.4 - impl done

--- sicp/ch04/exercise_4_4_a.scm
+++ sicp/ch04/exercise_4_4_a.scm
@@ -1,6 +1,8 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(define def-env user-initial-environment)
+
 (define true? identity)
 (define true-value #t)
 (define false-value #f)
@@ -34,10 +36,9 @@
                 (eval-or-aux (cdr exps) env))))))
   (eval-or-aux (cdr exp) env))
 
-(define def-env user-initial-environment)
-
 (let ((testcases
         (list
+          (mat '(and) #t)
           (mat '(and (= 1 1) (= 2 2)) #t)
           (mat '(and (= 1 1) #f (error 'wont-reach)) #f)))
       (proc
@@ -47,6 +48,7 @@
 
 (let ((testcases
         (list
+          (mat '(or) #f)
           (mat '(or #t (error 'wont-reach)) #t)
           (mat '(or (< 1 1) (> 2 2)) #f)))
       (proc
--- sicp/ch04/exercise_4_4_b.scm
+++ sicp/ch04/exercise_4_4_b.scm
@@ -1,6 +1,8 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(define def-env user-initial-environment)
+
 (define true? identity)
 (define true-value #t)
 (define false-value #f)
@@ -14,8 +16,75 @@
 ;       #f))
 ;  (eval <expr> env))
 (define (eval-and exp env)
-  (define (eval-and-aux exps env)
-    (cond ((null?  'todo
+  (define (and->if exp)
+    (cond ((null? exp)
+            ; this is something to be evaluated
+            ;   as an expr
+            ;   so I quote this value
+            'true-value)
+          ((null? (cdr exp))
+            ; last expression, simply return it
+            (car exp))
+          (else
+            (list
+              (list 'lambda (list 'result)
+                    (list 'if 'result
+                          (list 'and (and->if (cdr exp)))
+                          'false-value))
+              (list 'eval (car exp) 'env)))))
+  (eval (and->if (cdr exp))
+        ; extend the default environment
+        ;  to include the value of `env`
+        (extend-top-level-environment
+          env
+          '(env)
+          (list env))))
 
+; (or) => #f
+; (or <expr>) => <expr>
+; (or <expr1> ...) =>
+; ((lambda (result)
+;    (if result
+;       result
+;       (or ...)))
+;  (eval <expr> env))
+(define (eval-or exp env)
+  (define (or->if exp)
+    (cond ((null? exp)
+            'false-value)
+          ((null? (cdr exp))
+            (car exp))
+          (else
+            (list
+              (list 'lambda (list 'result)
+                    (list 'if 'result
+                          'result
+                          (list 'or (or->if (cdr exp)))))
+              (list 'eval (car exp) 'env)))))
+  (eval (or->if (cdr exp))
+        (extend-top-level-environment
+          env
+          '(env)
+          (list env))))
+                       
+(let ((testcases
+        (list
+          (mat '(and) #t)
+          (mat '(and (= 1 1) (= 2 2)) #t)
+          (mat '(and (= 1 1) #f (error 'wont-reach)) #f)))
+      (proc
+        (lambda (exp)
+          (eval-and exp def-env))))
+  (do-test proc testcases))
+
+(let ((testcases
+        (list
+          (mat '(or) #f)
+          (mat '(or #t (error 'wont-reach)) #t)
+          (mat '(or (< 1 1) (> 2 2)) #f)))
+      (proc
+        (lambda (exp)
+          (eval-or exp def-env))))
+  (do-test proc testcases))
 
 (end-script)
