fab7a5633b5b6aa658df81036538d1d8d1b14771
Javran Cheng <Javran.c@gmail.com>
2014-01-17 03:27:27 -0500

    avoid foldl confusion

--- eopl/ch3/ex-3.23/run.rkt
+++ eopl/ch3/ex-3.23/run.rkt
@@ -1,17 +1,26 @@
 #lang eopl
 
-(require "../../common.rkt")
+; I have a diff definition of foldl..
+; (require "../../common.rkt")
 (require "../../test-utils.rkt")
 
 (require "./top.rkt")
 (require "./data-structures.rkt")
 
 (require (only-in racket/file file->lines))
+(require (only-in racket/base foldl))
 
 ; no modification, skip tests
 ; (test-all)
 
 ; result in the book is 12
+
+(define (out . args)
+  (for-each
+    (lambda (x)
+      (display x)
+      (newline))
+    args))
 
 (out
   (expval->num
@@ -22,21 +31,21 @@
                                else -(((maker maker) -(x,1)), -4)
           in let times4 = proc (x) ((makemult makemult) x)
              in (times4 3)")))
+(newline)
 
 (define (run-source-file path)
   (run
     (foldl
       ; my impl ... two arguments are flipped
-      (lambda (acc i)
+      (lambda (i acc)
         (string-append acc " " i))
       ""
       (file->lines path))))
 
-(out
-  (expval->num
-    (run-source-file "./factorial.proc")))
-
 (define (factorial x)
   (if (= x 0) 1 (* x (factorial (- x 1)))))
 
-(out (factorial 8))
+(out
+  (expval->num
+    (run-source-file "./factorial.proc"))
+  (factorial 8))
