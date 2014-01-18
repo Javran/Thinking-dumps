08bef133e3b51e243ce6e8a46c852224a01aa6bb
Javran Cheng <Javran.c@gmail.com>
2014-01-18 00:21:38 -0500

    my-eval - put! and get

--- sicp/ch04/my-eval-data-directed.scm
+++ sicp/ch04/my-eval-data-directed.scm
@@ -1,41 +1,61 @@
 ; infrastructure for data-directed style
 
-(define my-eval-get! #f)
+(define my-eval-get #f)
 (define my-eval-put! #f) 
+
+; definition:
+;  slot: a symbol, the syntax structure name, the tag of the expression
+;    e.g.: `'let` should be the corresponding slot name for
+;          the handler that deals with let-expression
+; 
+; data structure:
+; `eval-handler-alist` is an alist
+;   key: a symbol the slot name
+;   value: a procedure of type: Exp x Env -> ExpVal
+;          this procedure is supposed to evaluate the expression
+;          and return the value of that expression
 
 (let ((eval-handler-alist '()))
   ; keep the handlers inside
 
-  (define (put-proc op type item table)
-    (let ((type-val (assoc type table)))
-      (if type-val
-        ; if the type exists
-        (let* ((op-table (cadr type-val))
-               (new-op-table (put-alist op item op-table)))
-          (put-alist type new-op-table table))
-        ; else
-        (put-alist type
-                   (list (list op item)) ; single key-value pair
-                   table))))
+  ; modify `alist` to insert / replace
+  ;   handler of `slot` with `handler`
+  ; return: a pair with 
+  ;   `car` set to the modified alist
+  ;   `cdr` set to the old handler, or #f
+  (define (put-handler slot handler alist)
+    (let ((slot-list (assq slot alist)))
+      (cons (cons (list slot handler)
+                  (del-assq slot alist))
+            (if slot-list
+              ; fetch the old handler
+              (cadr slot-list)
+              ; or it does not exist
+              #f))))
 
-  (define (put op type item)
-    (set! proc-table (put-proc op type item proc-table)))
+  ; put `handler` into the `slot` of `eval-handler-alist`
+  ; return: the old handler, or #f
+  (define (put! slot handler)
+    (let ((modified
+            (put-handler slot handler eval-handler-alist)))
+      (set! eval-handler-alist (car modified))
+      (out eval-handler-alist)
+      (cdr modified)))
 
-  (define (get-proc op type table)
-    (let ((type-val (assoc type table)))
-      (if type-val
-        (let* ((op-table (cadr type-val))
-               (op-proc-pair (assoc op op-table)))
-          (if op-proc-pair
-            (cadr op-proc-pair)
-            #f))
+  ; look up `slot` in `alist`
+  ; return: the corresponding handler, #f elsewise
+  (define (get-handler slot alist)
+    (let ((slot-list (assq slot alist)))
+      (if slot-list
+        ; fetch handler from list
+        (cadr slot-list)
         #f)))
 
-(define (get op type)
-  (get-proc op type proc-table))
+  ; look up `slot` in `eval-handler-alist`
+  ; return: the same as `get-handler`
+  (define (get slot)
+    (get-handler slot eval-handler-alist))
 
-
-  (set! my-eval-get! get)
-  (set! my-eval-put! 'put)
+  (set! my-eval-get get)
+  (set! my-eval-put! put!)
   'done)
-
--- sicp/ch04/my-eval.scm
+++ sicp/ch04/my-eval.scm
@@ -1,5 +1,2 @@
 ; my `eval` and `apply` implementaton.
 (load "./my-eval-data-directed.scm")
-
-(out my-eval-get my-eval-put)
-
