050eac6dc2216095b628816a7d23d4e0ec8e5944
Javran Cheng <Javran.c@gmail.com>
2014-01-20 03:41:01 -0500

    ex 3.33 - done

---
+++ eopl/ch3/ex-3.33/interp.rkt
@@ -0,0 +1,99 @@
+(module interp (lib "eopl.ss" "eopl")
+  
+  ;; interpreter for the LETREC language.  The \commentboxes are the
+  ;; latex code for inserting the rules into the code in the book.
+  ;; These are too complicated to put here, see the text, sorry.
+
+  (require "drscheme-init.rkt")
+
+  (require "lang.rkt")
+  (require "data-structures.rkt")
+  (require "environments.rkt")
+
+  (require (only-in racket foldl printf))
+
+  (provide value-of-program value-of)
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+
+  ;; value-of-program : Program -> ExpVal
+  (define value-of-program 
+    (lambda (pgm)
+      (cases program pgm
+        (a-program (exp1)
+          (value-of exp1 (init-env))))))
+
+  ;; value-of : Exp * Env -> ExpVal
+  ;; Page: 83
+  (define value-of
+    (lambda (exp env)
+      (cases expression exp
+
+        ;\commentbox{ (value-of (const-exp \n{}) \r) = \n{}}
+        (const-exp (num) (num-val num))
+
+        ;\commentbox{ (value-of (var-exp \x{}) \r) = (apply-env \r \x{})}
+        (var-exp (var) (apply-env env var))
+
+        ;\commentbox{\diffspec}
+        (diff-exp (exp1 exp2)
+          (let ((val1 (value-of exp1 env))
+                (val2 (value-of exp2 env)))
+            (let ((num1 (expval->num val1))
+                  (num2 (expval->num val2)))
+              (num-val
+                (- num1 num2)))))
+
+        ;\commentbox{\zerotestspec}
+        (zero?-exp (exp1)
+          (let ((val1 (value-of exp1 env)))
+            (let ((num1 (expval->num val1)))
+              (if (zero? num1)
+                (bool-val #t)
+                (bool-val #f)))))
+              
+        ;\commentbox{\ma{\theifspec}}
+        (if-exp (exp1 exp2 exp3)
+          (let ((val1 (value-of exp1 env)))
+            (if (expval->bool val1)
+              (value-of exp2 env)
+              (value-of exp3 env))))
+
+        ;\commentbox{\ma{\theletspecsplit}}
+        (let-exp (var exp1 body)       
+          (let ((val1 (value-of exp1 env)))
+            (value-of body
+              (extend-env var val1 env))))
+        
+        (proc-exp (vars body)
+          (proc-val (procedure vars body env)))
+
+        (call-exp (rator rands)
+          (let ((proc (expval->proc (value-of rator env)))
+                (args (map (lambda (rand) (value-of rand env)) rands)))
+            (apply-procedure proc args)))
+
+        (letrec-exp (p-names b-varlists p-bodys letrec-body)
+          (value-of letrec-body
+            (extend-env-rec p-names b-varlists p-bodys env)))
+        )))
+
+  ;; apply-procedure : Proc * ExpVal -> ExpVal
+
+  (define apply-procedure
+    (lambda (proc1 args)
+      (cases proc proc1
+        (procedure (vars body saved-env)
+          (define proc-env
+            (foldl
+              extend-env
+              saved-env
+              vars
+              args))
+          (value-of body proc-env)))))
+  
+  )
+  
+
+
+  
---
+++ eopl/ch3/ex-3.33/run.rkt
@@ -0,0 +1,12 @@
+#lang eopl
+
+(require "../../common.rkt")
+(require "../../test-utils.rkt")
+
+(require "./top.rkt")
+(require "./data-structures.rkt")
+
+; based on ex 3.31
+; + ex 3.32
+
+(test-all)
