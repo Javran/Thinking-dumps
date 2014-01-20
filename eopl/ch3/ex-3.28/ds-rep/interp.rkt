6854f9b14f702da05ff6fa5668f7ab758ba9f073
Javran Cheng <Javran.c@gmail.com>
2014-01-19 20:54:44 -0500

    ex 3.28 - ds version

---
+++ eopl/ch3/ex-3.28/ds-rep/interp.rkt
@@ -0,0 +1,138 @@
+(module interp (lib "eopl.ss" "eopl")
+  
+  ;; interpreter for the PROC language, using the data structure
+  ;; representation of procedures.
+
+  ;; The \commentboxes are the latex code for inserting the rules into
+  ;; the code in the book. These are too complicated to put here, see
+  ;; the text, sorry. 
+
+  (require "drscheme-init.rkt")
+
+  (require "lang.rkt")
+  (require "data-structures.rkt")
+  (require "environments.rkt")
+
+  (require (only-in racket foldl remove-duplicates flatten remove))
+
+  (provide value-of-program value-of)
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+
+  ;; value-of-program : Program -> ExpVal
+  (define value-of-program 
+    (lambda (pgm)
+      (cases program pgm
+        (a-program (exp1)
+          (value-of exp1 (init-env))))))
+
+  ;; value-of : Exp * Env -> ExpVal
+  (define value-of
+    (lambda (exp env)
+      (cases expression exp
+
+        ;\commentbox{ (value-of (const-exp \n{}) \r) = \n{}}
+        (const-exp (num) (num-val num))
+
+        ;\commentbox{ (value-of (var-exp \x{}) \r) = (apply-env \r \x{})}
+        (var-exp (var) (apply-env env var))
+
+        ;\commentbox{\diffspec}
+        (diff-exp (exp1 exp2)
+          (let ((val1 (value-of exp1 env))
+                (val2 (value-of exp2 env)))
+            (let ((num1 (expval->num val1))
+                  (num2 (expval->num val2)))
+              (num-val
+                (- num1 num2)))))
+
+        ;\commentbox{\zerotestspec}
+        (zero?-exp (exp1)
+          (let ((val1 (value-of exp1 env)))
+            (let ((num1 (expval->num val1)))
+              (if (zero? num1)
+                (bool-val #t)
+                (bool-val #f)))))
+              
+        ;\commentbox{\ma{\theifspec}}
+        (if-exp (exp1 exp2 exp3)
+          (let ((val1 (value-of exp1 env)))
+            (if (expval->bool val1)
+              (value-of exp2 env)
+              (value-of exp3 env))))
+
+        ;\commentbox{\ma{\theletspecsplit}}
+        (let-exp (var exp1 body)       
+          (let ((val1 (value-of exp1 env)))
+            (value-of body
+              (extend-env var val1 env))))
+        
+        (proc-exp (var body)
+          (define free-vars
+            (free-variables exp))
+          (proc-val (procedure var body free-vars)))
+
+        (call-exp (rator rand)
+          (let ((proc (expval->proc (value-of rator env)))
+                (arg (value-of rand env)))
+            (apply-procedure proc arg env)))
+
+        )))
+
+  ; copied from my answer to ex 3.26
+  ; free-variables: Exp -> [Identifier]
+  ; get a list of all free variables from `exp`
+  (define (free-variables exp)
+    (cases expression exp
+        (const-exp (num)
+          '())
+        (var-exp (var)
+          (list var))
+        (diff-exp (exp1 exp2)
+          (remove-duplicates
+            (flatten
+              (list
+                (free-variables exp1)
+                (free-variables exp2)))))
+        (zero?-exp (exp1)
+          (free-variables exp1))
+        (if-exp (exp1 exp2 exp3)
+          (remove-duplicates
+            (flatten
+              (list
+                (free-variables exp1)
+                (free-variables exp2)
+                (free-variables exp3)))))
+        (let-exp (var exp1 body)
+          (remove-duplicates
+            (flatten
+              (list
+                (free-variables exp1)
+                (remove var (free-variables body))))))
+        (proc-exp (var body)
+          (remove var (free-variables body)))
+        (call-exp (rator rand)
+          (remove-duplicates
+            (flatten
+              (list
+                (free-variables rator)
+                (free-variables rand)))))))
+
+  ;; apply-procedure : Proc * ExpVal -> ExpVal
+  ;; Page: 79
+  (define apply-procedure
+    ; now we need to pass in the environment
+    ;   in order to evaluate free variables
+    (lambda (proc1 val env)
+      (cases proc proc1
+        (procedure (var body free-vars)
+          (define proc-env
+            (foldl
+              extend-env
+              (init-env)
+              free-vars
+              (map (lambda (v) (apply-env env v))
+                   free-vars)))
+          (value-of body (extend-env var val proc-env))))))
+
+  )
---
+++ eopl/ch3/ex-3.28/ds-rep/tests.rkt
@@ -0,0 +1,77 @@
+(module tests mzscheme
+  
+  (provide test-list)
+
+  ;;;;;;;;;;;;;;;; tests ;;;;;;;;;;;;;;;;
+  
+  (define test-list
+    '(
+  
+      ;; simple arithmetic
+      (positive-const "11" 11)
+      (negative-const "-33" -33)
+      (simple-arith-1 "-(44,33)" 11)
+  
+      ;; nested arithmetic
+      (nested-arith-left "-(-(44,33),22)" -11)
+      (nested-arith-right "-(55, -(22,11))" 44)
+  
+      ;; simple variables
+      (test-var-1 "x" 10)
+      (test-var-2 "-(x,1)" 9)
+      (test-var-3 "-(1,x)" -9)
+      
+      ;; simple unbound variables
+      (test-unbound-var-1 "foo" error)
+      (test-unbound-var-2 "-(x,foo)" error)
+  
+      ;; simple conditionals
+      (if-true "if zero?(0) then 3 else 4" 3)
+      (if-false "if zero?(1) then 3 else 4" 4)
+      
+      ;; test dynamic typechecking
+      (no-bool-to-diff-1 "-(zero?(0),1)" error)
+      (no-bool-to-diff-2 "-(1,zero?(0))" error)
+      (no-int-to-if "if 1 then 2 else 3" error)
+
+      ;; make sure that the test and both arms get evaluated
+      ;; properly. 
+      (if-eval-test-true "if zero?(-(11,11)) then 3 else 4" 3)
+      (if-eval-test-false "if zero?(-(11, 12)) then 3 else 4" 4)
+      
+      ;; and make sure the other arm doesn't get evaluated.
+      (if-eval-test-true-2 "if zero?(-(11, 11)) then 3 else foo" 3)
+      (if-eval-test-false-2 "if zero?(-(11,12)) then foo else 4" 4)
+
+      ;; simple let
+      (simple-let-1 "let x = 3 in x" 3)
+
+      ;; make sure the body and rhs get evaluated
+      (eval-let-body "let x = 3 in -(x,1)" 2)
+      (eval-let-rhs "let x = -(4,1) in -(x,1)" 2)
+
+      ;; check nested let and shadowing
+      (simple-nested-let "let x = 3 in let y = 4 in -(x,y)" -1)
+      (check-shadowing-in-body "let x = 3 in let x = 4 in x" 4)
+      (check-shadowing-in-rhs "let x = 3 in let x = -(x,1) in x" 2)
+
+      ;; simple applications
+      (apply-proc-in-rator-pos "(proc(x) -(x,1)  30)" 29)
+      (apply-simple-proc "let f = proc (x) -(x,1) in (f 30)" 29)
+      (let-to-proc-1 "(proc(f)(f 30)  proc(x)-(x,1))" 29)
+
+
+      ;; note that dynamic binding is used. in the initial env, x = 10
+      ;; currying does not work as expected, because the value of x is not passed into
+      ;; the nested procedure. here x = 10, y = 6, result = 4
+      (nested-procs "((proc (x) proc (y) -(x,y)  5) 6)" 4)
+      ;; to verify, if we change the variable name, it should raise an error
+      (nested-procs2 "((proc (a) proc (b) -(a,b)  5) 6)" error)
+      ;; same problem, x is bound to 10 no matter how the environment is changed
+      ;; the result of `-(10,5)` is dropped, and `-(x,6)` yields 4
+      (nested-procs3 "let f = proc(x) proc (y) -(x,y) in ((f -(10,5)) 6)" 4)
+
+      (book-example "let a = 3 in let p = proc (x) -(x,a) in let a = 5 in -(a,(p 2))" 8)
+      
+      ))
+  )
