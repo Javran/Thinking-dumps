1c08871970b37cbb0b26ddd023cb9f6cfe06da78
Javran Cheng <Javran.c@gmail.com>
2014-01-25 00:16:00 -0500

    ex 4.20 - step 1. ref as valid expval
    just for impl purpose, I know ExpVal and DenVal are
    different stories, but here it seems to be the easiest way
    of getting things done.

---
+++ eopl/ch4/ex-4.20/run.rkt
@@ -0,0 +1,10 @@
+#lang eopl
+
+(require "../../common.rkt")
+(require "../../test-utils.rkt")
+
+(require "./top.rkt")
+(require "./data-structures.rkt")
+
+; (run "letrec f(x) = -(x,1) in (f 33)")
+(test-all)
