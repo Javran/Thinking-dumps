c0ac6bf4f0fae32435c714e5e41a553d814b6c8e
Javran Cheng <Javran.c@gmail.com>
2014-01-25 20:00:49 -0500

    ex 4.25 - done

---
+++ eopl/ch4/ex-4.25/run.rkt
@@ -0,0 +1,12 @@
+#lang eopl
+
+(require "../../common.rkt")
+(require "../../test-utils.rkt")
+
+(require "./top.rkt")
+(require "./data-structures.rkt")
+(require "./printer.rkt")
+
+; based on ex 4.22
+
+(test-all)
