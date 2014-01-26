5fa36105a03b170386b0a7bb3f57174ba513fb19
Javran Cheng <Javran.c@gmail.com>
2014-01-25 19:30:15 -0500

    ex 2.23 - done

---
+++ eopl/ch4/ex-4.23/run.rkt
@@ -0,0 +1,27 @@
+#lang eopl
+
+(require "../../common.rkt")
+(require "../../test-utils.rkt")
+
+(require "./top.rkt")
+(require "./data-structures.rkt")
+(require "./printer.rkt")
+
+(test-all)
+
+(print-to-screen #t)
+
+; read an integer `n`, print from 1 to `n`
+(out "please input an integer:")
+(run "
+  var n,i;
+  { read n;
+    i = 1;
+    while not(zero?(-(+(n,1),i)))
+    {
+      print i;
+      i = +(i,1)
+    }
+
+   }
+   ")
