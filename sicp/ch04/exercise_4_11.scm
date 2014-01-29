4981e13a6e02478c37cb9887ced1bc9dfbae077f
Javran Cheng <Javran.c@gmail.com>
2014-01-29 18:24:15 -0500

    ex 4.11 - done
    code simplified

--- sicp/ch04/exercise_4_11.scm
+++ sicp/ch04/exercise_4_11.scm
@@ -33,70 +33,54 @@
       (cdr frame)))
   'ok)
 
-(define (frame-binding-change! var val frame)
-  (cond ((assq var (cdr frame)) =>
-           (lambda (the-pair)
-             (set-cdr! the-pair val)))
-        (else
-          (error "binding not found in this frame"))))
-
 (define (extend-environment vars vals base-env)
   (cons (make-frame vars vals) base-env))
 
 (define (lookup-variable-value var env)
-  ; travel through environments
   (define (env-loop env)
     ; search a frame for the corresponding value
     (define (scan vars vals frame)
-      (cond ((null? vars)
-              ; empty frame, next one
-              (env-loop (enclosing-environment env)))
-            ((eq? var (car vars))
-              ; symbol found
-              (car vals))
+      (cond ((assq var (cdr frame)) => cdr)
             (else
-              (scan (cdr vars) (cdr vals) frame))))
+              (env-loop (enclosing-environment env)))))
     (if (eq? env the-empty-environment)
       (error "unbound variable" var)
-      (let ((frame (first-frame env)))
-        (scan (frame-variables frame)
-              (frame-values    frame)
-              frame))))
+      (let* ((frame (first-frame env))
+             (vars (frame-variables frame))
+             (vals (frame-values    frame)))
+        (cond ((assq var (cdr frame)) => cdr)
+              (else
+                (env-loop (enclosing-environment env)))))))
   (env-loop env))
 
 ; pretty similiar to `lookup-variable-value`
 (define (set-variable-value! var val env)
   (define (env-loop env)
-    (define (scan vars vals frame)
-      (cond ((null? vars)
-              (env-loop (enclosing-environment env)))
-            ((eq? var (car vars))
-              ; set-car!
-              (frame-binding-change! var val frame))
-            (else
-              (scan (cdr vars) (cdr vals) frame))))
     (if (eq? env the-empty-environment)
       (error "unbound variable" var)
-      (let ((frame (first-frame env)))
-        (scan (frame-variables frame)
-              (frame-values    frame)
-              frame))))
+      (let* ((frame (first-frame env))
+             (vars (frame-variables frame))
+             (vals (frame-values    frame)))
+        (cond ((assq var (cdr frame)) =>
+                 (lambda (the-pair)
+                   (set-cdr! the-pair val)
+                   'ok))
+              (else
+                (env-loop (enclosing-environment env)))))))
   (env-loop env))
 
 (define (define-variable! var val env)
   ; seems the `env` requires to be a non-empty env
   (assert (not (empty-environment? env))
           "define-variable! cannot work on an empty environment")
-  (let ((frame (first-frame env)))
-    (define (scan vars vals)
-      (cond ((null? vars)
-              (add-binding-to-frame! var val frame))
-            ((eq? var (car vars))
-              (frame-binding-change! var val frame))
-            (else
-              (scan (cdr vars) (cdr vals)))))
-    (scan (frame-variables frame)
-          (frame-values    frame))))
+  (let* ((frame (first-frame env))
+         (vars (frame-variables frame))
+         (vals (frame-values    frame)))
+    (cond ((assq var (cdr frame)) =>
+             (lambda (the-pair)
+               (set-cdr! the-pair val) 'ok))
+          (else
+            (add-binding-to-frame! var val frame)))))
 
 (test-environment
   lookup-variable-value
