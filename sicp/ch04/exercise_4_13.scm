26a52bca29e0f05a8480162716cdf744df6c20c0
Javran Cheng <Javran.c@gmail.com>
2014-01-31 22:36:36 -0500

    ex 4.13 - blacklist impl

--- sicp/ch04/exercise_4_13.scm
+++ sicp/ch04/exercise_4_13.scm
@@ -18,4 +18,113 @@
 ;     in the `marked` section, we just simply report
 ;     that the binding does not exist.
 
+; the following code is based on
+;   ./exercise_4_11.scm
+; as I think it's more natural to think the structure
+;   as an alist, rather than two lists of the same length.
+
+(load "./exercise_4_11_common.scm")
+
+(define the-empty-environment nil)
+(define empty-environment?    null?)
+(define first-frame           car)
+(define black-list            cadr)
+(define enclosing-environment caddr)
+
+(define (extend-environment vars vals base-env)
+  (list (make-frame vars vals) '() base-env))
+
+; check if `var` is blacklisted in the current frame
+;   the callee should take the responsibility of recursive checking
+(define (blacklisted? var env)
+  (if (empty-environment? env)
+    #f
+    (memq var (black-list env))))
+
+(define (remove-from-blacklist! var env)
+  (set-car! (cdr env) (delq var (black-list env))))
+
+(define (add-to-blacklist! var env)
+  (set-car! (cdr env) (cons var (black-list env))))
+
+; a frame contains a list,
+;   whose elements are name-value pairs
+(define (make-frame variables values)
+  (assert (= (length variables)
+             (length values))
+          "name-value list length mismatch") 
+  ; simply zip two lists together
+  ;   to make the pair
+  (cons 'frame
+        (map cons variables values)))
+
+(define (frame-variables frame)
+  (map car (cdr frame)))
+(define (frame-values    frame)
+  (map cdr (cdr frame)))
+
+(define (add-binding-to-frame! var val frame)
+  (set-cdr!  frame
+    (cons
+      ; the new pair
+      (cons var val)
+      ; existing bindings
+      (cdr frame)))
+  'ok)
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    ; search a frame for the corresponding value
+    (define (scan vars vals frame)
+      (cond ((assq var (cdr frame)) => cdr)
+            (else
+              (env-loop (enclosing-environment env)))))
+    (if (or (eq? env the-empty-environment)
+            (blacklisted? var env))
+      (error "unbound variable" var)
+      (let* ((frame (first-frame env))
+             (vars (frame-variables frame))
+             (vals (frame-values    frame)))
+        (cond ((assq var (cdr frame)) => cdr)
+              (else
+                (env-loop (enclosing-environment env)))))))
+  (env-loop env))
+
+; pretty similiar to `lookup-variable-value`
+(define (set-variable-value! var val env)
+  (define (env-loop env)
+    (if (or (eq? env the-empty-environment)
+            (blacklisted? var env))
+      (error "unbound variable" var)
+      (let* ((frame (first-frame env))
+             (vars (frame-variables frame))
+             (vals (frame-values    frame)))
+        (cond ((assq var (cdr frame)) =>
+                 (lambda (the-pair)
+                   (set-cdr! the-pair val)
+                   'ok))
+              (else
+                (env-loop (enclosing-environment env)))))))
+  (env-loop env))
+
+(define (define-variable! var val env)
+  ; seems the `env` requires to be a non-empty env
+  (assert (not (empty-environment? env))
+          "define-variable! cannot work on an empty environment")
+  (let* ((frame (first-frame env))
+         (vars (frame-variables frame))
+         (vals (frame-values    frame)))
+    (cond ((assq var (cdr frame)) =>
+             (lambda (the-pair)
+               (set-cdr! the-pair val) 'ok))
+          (else
+            (add-binding-to-frame! var val frame)))))
+
+(test-environment
+  lookup-variable-value
+  extend-environment
+  define-variable!
+  set-variable-value!)
+(out "Test passed.")
+
 (end-script)
