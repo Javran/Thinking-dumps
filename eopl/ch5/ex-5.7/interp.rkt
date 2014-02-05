a5633523e784095c511300de81e130abd6113735
Javran Cheng <Javran.c@gmail.com>
2014-02-05 11:08:34 -0500

    ex 5.7 - done

---
+++ eopl/ch5/ex-5.7/interp.rkt
@@ -0,0 +1,137 @@
+(module interp (lib "eopl.ss" "eopl")
+  
+  ;; cps interpreter for the LETREC language, using the data structure
+  ;; representation of continuations (Figure 5.3).
+
+  ;; exercise: rewrite this using the procedural representation of
+  ;; continuations (Figure 5.2).
+
+  ;; exercise: rewrite this using a trampoline (page 159).
+
+  (require "drscheme-init.rkt")
+
+  (require "lang.rkt")
+  (require "data-structures.rkt")
+  (require "environments.rkt")
+
+  (require (only-in racket foldl))
+
+  (provide value-of-program value-of/k)
+
+;;;;;;;;;;;;;;;; the interpreter ;;;;;;;;;;;;;;;;
+
+  ;; value-of-program : Program -> FinalAnswer
+  ;; Page: 143 and 154
+  (define value-of-program 
+    (lambda (pgm)
+      (cases program pgm
+        (a-program (exp1)
+          (value-of/k exp1 (init-env) (end-cont))))))  
+
+  ;; value-of/k : Exp * Env * Cont -> FinalAnswer
+  ;; Page: 143--146, and 154
+  (define value-of/k
+    (lambda (exp env cont)
+      (cases expression exp
+        (const-exp (num) (apply-cont cont (num-val num)))
+        (var-exp (var) (apply-cont cont (apply-env env var)))
+        (proc-exp (var body)
+          (apply-cont cont 
+            (proc-val (procedure var body env))))
+        (letrec-exp (p-name b-var p-body letrec-body)
+          (value-of/k letrec-body
+            (extend-env-rec p-name b-var p-body env)
+            cont))
+        (zero?-exp (exp1)
+          (value-of/k exp1 env
+            (zero1-cont cont)))
+        (let-exp (vars exps body)
+          (if (null? vars)
+            (value-of/k body env cont)
+            (value-of/k (car exps) env
+                        (let-exp-cont
+                          (car vars)
+                          (cdr vars)
+                          (cdr exps)
+                          '() '()
+                          body env cont))))
+        (if-exp (exp1 exp2 exp3)
+          (value-of/k exp1 env
+            (if-test-cont exp2 exp3 env cont)))
+        (diff-exp (exp1 exp2)
+          (value-of/k exp1 env
+            (diff1-cont exp2 env cont)))        
+        (call-exp (rator rand) 
+          (value-of/k rator env
+            (rator-cont rand env cont)))
+   )))
+
+  ;; apply-cont : Cont * ExpVal -> FinalAnswer
+  ;; Page: 148
+  (define apply-cont
+    (lambda (cont val)
+      (cases continuation cont
+        (end-cont () 
+          (begin
+            (eopl:printf
+              "End of computation.~%")
+            val))
+        ;; or (logged-print val)  ; if you use drscheme-init-cps.rkt
+        (zero1-cont (saved-cont)
+          (apply-cont saved-cont
+            (bool-val
+              (zero? (expval->num val)))))
+        (let-exp-cont (var
+                        vars exps
+                        vars-done vals-done
+                        body saved-env saved-cont)
+          (if (null? exps)
+            ; no more `exps`, evaluate the body
+            (let ((new-vals-done (cons val vals-done))
+                  (new-vars-done (cons var vars-done)))
+              (let ((rev-vals-done (reverse new-vals-done))
+                    (rev-vars-done (reverse new-vars-done)))
+                (let ((new-env (foldl extend-env saved-env rev-vars-done rev-vals-done)))
+                  (value-of/k body new-env saved-cont))))
+            ; evaluate one more expression
+            (value-of/k (car exps) saved-env
+              (let-exp-cont
+                (car vars)
+                (cdr vars) (cdr exps)
+                (cons var vars-done) (cons val vals-done)
+                body saved-env saved-cont))))
+        (if-test-cont (exp2 exp3 saved-env saved-cont)
+          (if (expval->bool val)
+             (value-of/k exp2 saved-env saved-cont)
+             (value-of/k exp3 saved-env saved-cont)))
+        (diff1-cont (exp2 saved-env saved-cont)
+          (value-of/k exp2
+            saved-env (diff2-cont val saved-cont)))
+        (diff2-cont (val1 saved-cont)
+          (let ((num1 (expval->num val1))
+                (num2 (expval->num val)))
+            (apply-cont saved-cont
+              (num-val (- num1 num2)))))
+        (rator-cont (rand saved-env saved-cont)
+          (value-of/k rand saved-env
+            (rand-cont val saved-cont)))
+        (rand-cont (val1 saved-cont)
+          (let ((proc (expval->proc val1)))
+            (apply-procedure/k proc val saved-cont)))
+        )))
+
+  ;; apply-procedure/k : Proc * ExpVal * Cont -> FinalAnswer
+  ;; Page 152 and 155
+  (define apply-procedure/k
+    (lambda (proc1 arg cont)
+      (cases proc proc1
+        (procedure (var body saved-env)
+          (value-of/k body
+            (extend-env var arg saved-env)
+            cont)))))
+  
+  )
+  
+
+
+  
