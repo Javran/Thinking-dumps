ed6a41dbb58e3f09c50df6b8a2c4cd95bb0fe54c
Javran Cheng <Javran.c@gmail.com>
2014-02-08 01:31:09 -0500

    ex 5.13 - extend to support "*"

---
+++ eopl/ch5/ex-5.13/run.rkt
@@ -0,0 +1,13 @@
+#lang eopl
+
+(require "../../common.rkt")
+(require "../../test-utils.rkt")
+
+(require "./top.rkt")
+(require "./data-structures.rkt")
+
+; based on ex 5.12
+
+(test-all)
+
+(instrument-cont #t)
