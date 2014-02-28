9ebead0d1c64d8776b6eee2af0dd7a210c9979ee
Javran Cheng <Javran.c@gmail.com>
2014-02-27 22:25:23 -0500

    4.2.2 - thunk - done

--- sicp/ch04/4_2_2_representing_thunks.scm
+++ sicp/ch04/4_2_2_representing_thunks.scm
@@ -42,6 +42,48 @@
 ;; we will observer two `evaluating...`
 ;; are outputed here.
 
+;; now we slight change the implementations..
+(define (evaluated-thunk? obj)
+  (and (non-empty? obj)
+       (eq? (car obj) 'evaluated-thunk)))
+
+(define thunk-value cadr)
+
+(define (force-it obj)
+  (cond ((thunk? obj)
+         (let ((result (eval (thunk-exp obj)
+                             (thunk-env obj))))
+           (set-car! obj 'evaluated-thunk)
+           (set-car! (cdr obj)
+                     result)
+           (set-cdr! (cdr obj)
+                     ;; I think this is just
+                     ;; an optimization for gc
+                     '())
+           result))
+        ((evaluated-thunk? obj) (thunk-value obj))
+        (else obj)))
+
+;; let's redo the test here
+(newline) (newline)
+(define delayed-val
+  (delay-it
+   `(begin
+      (display "evaluating...")
+      (newline)
+      (+ 1 2 3 4))
+   user-initial-environment))
+
+(out delayed-val)
+
+;; now the thunk is equipped with
+;; memoization, this means the side effects
+;; are one-shot, the next time we force a value,
+;; we will get the previous result.
+(out (force-it delayed-val))
+(out (force-it delayed-val))
+;; we can only observer one `evaluating...` now.
+
 (end-script)
 
 ;; Local variables:
