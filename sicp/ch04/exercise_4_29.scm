4b634fec06e0453f67668f9441e1add0008821a9
Javran Cheng <Javran.c@gmail.com>
2014-03-01 23:49:40 -0500

    ex 4.29 - memoization impl

--- sicp/ch04/exercise_4_29.scm
+++ sicp/ch04/exercise_4_29.scm
@@ -1,21 +1,38 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
-;; function memoization
 (define (memoize f)
   (let ((retval-alist nil))
-    (lambda args
+    (define (memoized-f . args)
       (cond ((assoc args retval-alist) =>
              cadr)
             (else
-             (let ((result (apply f args)))
+             (let ((result (apply (f memoized-f) args)))
                (set! retval-alist
                      (cons (list args result)
                            retval-alist))
-               result))))))
+               result))))
+    memoized-f))
+             
+
 
 ;; Exhibit a program that runs more slowly
 ;; without memoization
+(define (fib-aux fib)
+  (lambda (n)
+    (if (<= n 1)
+        n
+        (+ (fib (- n 1))
+           (fib (- n 2))))))
+
+(define (fib n)
+  ((fib-aux fib) n))
+
+(define (fib-1 n)
+  ((memoize fib-aux) n))
+
+(time-test fib 25)
+(time-test fib-1 25)
 
 (end-script)
 
