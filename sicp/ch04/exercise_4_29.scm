aee4ed19bd21d57bbbd4887edff06f4cb0137e84
Javran Cheng <Javran.c@gmail.com>
2014-03-02 20:26:22 -0500

    ex 4.29 - done

--- sicp/ch04/exercise_4_29.scm
+++ sicp/ch04/exercise_4_29.scm
@@ -55,6 +55,47 @@
 ;; here we expect the version without memoization
 ;; to take a significantly longer time to run
 
+(define count 0)
+(define (id x)
+  (set! count (+ count 1))
+  x)
+
+(define (square x)
+  (* (force-th x)
+     (force-th x)))
+
+(define new-env (the-environment))
+
+(out (square
+      (thunk-no-memo
+       ;; thunk inside of another thunk
+       ;; is not supported for simplicity
+       ;; here I think the question just want
+       ;; us to see the difference between
+       ;; call-by-name and call-by-need evaluation,
+       ;; thus the argument of `id`, i.e. 10,
+       ;; is kept to be evaluated eagerly.
+       ;; but this change shouldn't cause any difference
+       ;; in terms of program output.
+       `(id 10)
+       new-env)))
+;; 100
+
+(out count)
+;; 2
+
+(newline)
+(set! count 0)
+
+(out (square
+      (thunk-memo
+       `(id 10)
+       new-env)))
+;; 100
+(out count)
+;; 2
+
+
 (end-script)
 
 ;; Local variables:
