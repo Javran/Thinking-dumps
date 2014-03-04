c0e82cd95d28ca3aee4b8a229ae4362b64788585
Javran Cheng <Javran.c@gmail.com>
2014-03-03 23:06:33 -0500

    ex 4.31 - implement and test promise

---
+++ sicp/ch04/exercise_4_31.scm
@@ -0,0 +1,13 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./my-eval.scm")
+(load "./exercise_4_31_promise.scm")
+
+(test-promise)
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
