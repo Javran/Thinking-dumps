95e36cc12fee81b97629a59c3988f36ebdbaa257
Javran Cheng <Javran.c@gmail.com>
2014-03-04 23:34:04 -0500

    ex 4.31 - delay and force impl

--- sicp/ch04/exercise_4_31.scm
+++ sicp/ch04/exercise_4_31.scm
@@ -4,7 +4,17 @@
 (load "./my-eval.scm")
 (load "./exercise_4_31_promise.scm")
 
+(load "./exercise_4_31_delay_force.scm")
+
 (test-promise)
+
+(install-eval-delay)
+(install-eval-force)
+
+(newline)
+(out "===== new extensions are inserted, retesting ...")
+
+(my-eval-test-all)
 
 (end-script)
 
