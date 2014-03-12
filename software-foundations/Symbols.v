1091e0f30e83c27ef0463f3ede23b552a5ec1006
Javran Cheng <Javran.c@gmail.com>
2014-03-12 12:48:59 -0400

    first commit to software foundations

---
+++ software-foundations/Symbols.v
@@ -0,0 +1,27 @@
+(** * Symbols: Special symbols *)
+
+(* $Date: 2013-04-01 20:39:05 -0400 (Mon, 01 Apr 2013) $ *)
+
+(* This file defines some HTML symbols for use by the coqdoc
+   preprocessor.  It is not intended to be read by anybody. *)
+
+(** printing -> #<span style="font-family: arial;">&rarr;</span># *)
+(** printing || #<span style="font-family: arial;">&dArr;</span># *)
+(** printing ==> #<span style="font-family: arial;">&rArr;</span># *)
+(** printing ==>* #<span style="font-family: arial;">&rArr;*</span># *)
+(** printing ==>+ #<span style="font-family: arial;">&rArr;+</span># *)
+(** printing |- #<span style="font-family: arial;">&#x22A2;</span># *)
+(** printing <- #<span style="font-family: arial;">&larr;</span># *)
+(** printing <-> #<span style="font-family: arial;">&harr;</span># *)
+(** printing forall #<span style="font-family: arial;">&forall;</span># *)
+(** printing exists #<span style="font-family: arial;">&exist;</span># *)
+(** printing /\ #<span style="font-family: arial;">&and;</span># *)
+(** printing \/ #<span style="font-family: arial;">&or;</span># *)
+(** printing ->> #<span style="font-family: arial;">&#8702;</span># *)
+(** printing <<->> #<span style="font-family: arial;">&#8703;</span># *)
+(** printing |- #<span style="font-family: arial;">&#8866;</span># *)
+(** printing Gamma #<span style="font-family: serif; font-size:85%;">&Gamma;</span># *)
+(** printing Gamma' #<span style="font-family: serif; font-size:85%;">&Gamma;'</span># *)
+(** printing Gamma'' #<span style="font-family: serif; font-size:85%;">&Gamma;''</span># *)
+(** printing |-> #<span style="font-family: arial;">&#8614;</span># *)
+
