f1574b84788854f7eefe4a9860b5fcfd75d61f8f
Javran Cheng <Javran.c@gmail.com>
2014-03-15 00:19:18 -0400

    ex 4.36 - done without test

---
+++ sicp/ch04/exercise_4_36p.scm
@@ -0,0 +1,43 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+;; simply changing `an-integer-between` to
+;; `an-integer-staring-from` will not work
+;; because `try-again` will spend infinite time
+;; searching through the last integer (i.e. `k`
+;; in the previous exercise) while keep other group
+;; untouched.
+
+;; when we were dealing with streams in chapter 3,
+;; we can "merge" two streams together to allow both
+;; stream to get a chance of being used.
+;; but in `amb`, we don't have much control on
+;; the searching / iterating strategy.
+;; So I think constraining the search space using
+;; mathematical approach might be the easiest way
+;; for this problem:
+;;
+;; first consider the constraint:
+;;   i * i + j * j = k * k
+;; because this relation is equality,
+;; so we actually don't need to search through `k`
+;;
+;; now we only search through i and j, since i <= j
+;; we can first grow `j` and let `i` be an integer between
+;; `low` and `j`.
+;;
+;; by using the strategy above we are able to traverse all
+;; the possible solutions.
+
+(define (a-pythagorean-triple-staring-from low)
+  (let ((j (an-integer-staring-from low)))
+    (let ((i (an-integer-between low j)))
+      (let ((maybe-k (sqrt (* i i) (* j j))))
+        (require (integer? maybe-k))
+        (list i j k)))))
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
