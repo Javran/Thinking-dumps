c8174feaa26ce998bdad763c2f4ed077ba81ffe4
Javran Cheng <Javran.c@gmail.com>
2014-03-19 21:37:49 -0400

    ex 4.43 - a draft about relations

---
+++ sicp/ch04/exercise_4_43.hs
@@ -0,0 +1,13 @@
+-- <First>  <Last>    <Daughter First name>
+--          Moore     Mary Ann (! Lorna)
+-- Colonel  Downing   !Melissa
+-- ?        Hall      !Rosalind
+-- Barnacle Hood      !Gabrielle (Melissa)
+-- ?        Parker
+
+-- relations:
+-- <x> owns <y> -> <x>'s daughter is not <y>
+-- <x> daughterIs <y>
+
+main :: IO ()
+main = undefined
