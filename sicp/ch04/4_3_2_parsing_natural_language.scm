dd7a12a5c2aa4eda9047697f9f93944722a120cd
Javran Cheng <Javran.c@gmail.com>
2014-03-22 23:54:39 -0400

    4.3.2 - parsin nat lang, scm version from book

---
+++ sicp/ch04/4_3_2_parsing_natural_language.scm
@@ -0,0 +1,46 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(define nouns
+  '(noun
+    student professor cat class))
+
+(define verbs
+  '(verb
+    studies lectures eats sleeps))
+
+(define articles
+  '(article
+    the a))
+
+(define (parse-sentence)
+  (list 'sentence
+        (parse-noun-phrase)
+        (parse-word verbs)))
+
+(define (parse-noun-phrase)
+  (list 'noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*)
+                 (cdr word-list)))
+  (let ((found-word (car *unparsed*)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+(define *unparsed* '())
+
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
