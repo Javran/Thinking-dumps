27462b87d8b048278eb8b023dd76b79b11f084be
Javran Cheng <Javran.c@gmail.com>
2014-03-23 23:53:40 -0400

    4.3.2 - cont.

--- sicp/ch04/4_3_2_parsing_natural_language.scm
+++ sicp/ch04/4_3_2_parsing_natural_language.scm
@@ -15,12 +15,9 @@
   '(article
     the a))
 
-;; a sentence is a noun-phrase followed
-;; by a verb
-(define (parse-sentence)
-  (list 'sentence
-        (parse-noun-phrase)
-        (parse-word verbs)))
+(define prepositions
+  '(prep
+    for to in by with))
 
 ;; a noun-phrase is an article followed by a noun
 (define (parse-noun-phrase)
@@ -36,6 +33,32 @@
   (let ((found-word (car *unparsed*)))
     (set! *unparsed* (cdr *unparsed*))
     (list (car word-list) found-word)))
+
+;; prepostional phrase is a preposition followed by noun phrase
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
+;; now a sentence is noun phrase + verb phrase
+(define (parse-sentence)
+  (list 'sentence
+        (parse-noun-phrase)
+        (parse-verb-phrase)))
+
+;; verb phrase: a verb (maybe followed by prepositional phrase
+;; e.g.: * eats to a cat with the cat ..
+;;       * studies with a student
+;;       * lectures
+;; (well we don't do sanity check here)
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+         (maybe-extend
+          (list 'verb-phrase
+                verb-phrase
+                (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
 
 ;; the data to be converted
 (define *unparsed* '())
