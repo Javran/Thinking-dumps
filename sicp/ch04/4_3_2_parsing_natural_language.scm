0826f3bd6341c5c02174aa8eec0b24769cb218bd
Javran Cheng <Javran.c@gmail.com>
2014-03-23 23:41:11 -0400

    4.3.2 - parsing nat lang, some comments

--- sicp/ch04/4_3_2_parsing_natural_language.scm
+++ sicp/ch04/4_3_2_parsing_natural_language.scm
@@ -1,6 +1,8 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+;; definition of all valid objects
+
 (define nouns
   '(noun
     student professor cat class))
@@ -13,16 +15,20 @@
   '(article
     the a))
 
+;; a sentence is a noun-phrase followed
+;; by a verb
 (define (parse-sentence)
   (list 'sentence
         (parse-noun-phrase)
         (parse-word verbs)))
 
+;; a noun-phrase is an article followed by a noun
 (define (parse-noun-phrase)
   (list 'noun-phrase
         (parse-word articles)
         (parse-word nouns)))
 
+;; given a word list, try to parse the next data
 (define (parse-word word-list)
   (require (not (null? *unparsed*)))
   (require (memq (car *unparsed*)
@@ -31,11 +37,15 @@
     (set! *unparsed* (cdr *unparsed*))
     (list (car word-list) found-word)))
 
+;; the data to be converted
 (define *unparsed* '())
 
+;; parse input
 (define (parse input)
   (set! *unparsed* input)
   (let ((sent (parse-sentence)))
+    ;; when parsing is done,
+    ;; there shouldn't be anything remaining
     (require (null? *unparsed*))
     sent))
 
