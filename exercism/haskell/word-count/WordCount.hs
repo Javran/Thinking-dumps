9769a9e65cbfe7e1f461f91624b48fac4149d6a6
Javran Cheng <Javran.c@gmail.com>
2014-03-26 16:49:14 -0400

    word-count - strictness issue
    thanks to @soapie and @etrepum

--- exercism/haskell/word-count/WordCount.hs
+++ exercism/haskell/word-count/WordCount.hs
@@ -3,9 +3,13 @@
 module WordCount
 where
 
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Data.Char
 import Control.Arrow
+
+-- for Data.Map.Strict, see also:
+-- http://blog.ezyang.com/2011/05/an-insufficiently-lazy-map/
+-- (thanks to @soapie and @etrepum)
 
 wordCount :: String -> M.Map String Int
 wordCount = map (toLower . rmNoise) -- ^ map fusion, remains only lowers and nums
