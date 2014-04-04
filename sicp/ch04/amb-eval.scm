ed1dc11818a0d8498c3454a66cddb1cc0fc8564d
Javran Cheng <Javran.c@gmail.com>
2014-04-03 23:16:08 -0400

    "procedure applications"

--- sicp/ch04/amb-eval.scm
+++ sicp/ch04/amb-eval.scm
@@ -21,6 +21,58 @@
 (load "./amb-eval-e-define.scm")
 (load "./amb-eval-e-set.scm")
 
+(define (get-args aprocs env succeed fail)
+  (if (null? aprocs)
+      (succeed '() fail)
+      ((car aprocs)
+       ;; try evaluating this part..
+       env
+       ;; success continuation for this aproc
+       (lambda (arg fail2)
+         (get-args
+          (cdr aprocs)
+          env
+          ;; success continuation for
+          ;; recursive call to get-args
+          (lambda (args fail3)
+            (succeed (cons arg args) fail3))
+          fail2))
+       fail)))
+
+(define (execute-application proc args succeed fail)
+  (cond ((primitive-procedure? proc)
+         (succeed (apply-primitive-procedure proc args)
+                  fail))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment
+           (procedure-parameters proc)
+           args
+           (procedure-environment proc))
+          succeed
+          fail))
+        (else (error "Unknown procedure type: EXECUTE-APPLICATION"
+                     proc))))
+
+
+;; don't know where to put it for now...
+;; looks like written in CPS..
+(define (analyze-application exp)
+  (let ((fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env succeed fail)
+      (fproc env
+             ;; evaluate the program
+             ;; give it a suitable env to run
+             (lambda (proc fail2)
+               (get-args aprocs
+                         env
+                         (lambda (args fail3)
+                           (execute-application
+                            proc args succeed fail3))
+                         fail2))
+             fail))))
+
 (end-script)
 
 ;; Local variables:
