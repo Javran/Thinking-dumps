917d60b0d9dce648a1022ca5e883f47e5796c614
Javran Cheng <Javran.c@gmail.com>
2014-04-17 22:35:03 -0400

    amb-eval - amb form test cases using amb-eval-all

--- sicp/ch04/amb-eval-e-amb.scm
+++ sicp/ch04/amb-eval-e-amb.scm
@@ -29,7 +29,18 @@
         (mat `(amb "a" "b") env "a")
         (mat `(amb 'c 'd 'e) env 'c)
         )
-       (test-compare equal?)))
+       (test-compare equal?))
+      ;; test using amb-eval-all
+      (do-test
+       amb-eval-all
+       (list
+        (mat `(amb) env '())
+        (mat `(amb 1 2 3) env '(1 2 3))
+        (mat `(amb "a" "b") env '("a" "b"))
+        (mat `(amb 'c 'd 'e) env '(c d e))
+        (mat `(cons (amb 1 2) (amb 3 4)) env
+             '((1 . 3) (1 . 4) (2 . 3) (2 . 4)))
+        )))
     'ok)
 
   (define handler
