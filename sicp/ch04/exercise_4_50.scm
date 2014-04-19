4580c39f508b83749642fba783e13beb9ebcb9f1
Javran Cheng <Javran.c@gmail.com>
2014-04-18 23:35:48 -0400

    ex 4.50 - randomness might be implemented wrong

--- sicp/ch04/exercise_4_50.scm
+++ sicp/ch04/exercise_4_50.scm
@@ -26,6 +26,7 @@
 (define (install-amb-ramb)
 
   (define (analyze-ramb exp)
+    ;; the only difference: `cprocs` is shuffled
     (let ((cprocs (shuffle (map amb-analyze (amb-choices exp)))))
       (lambda (env succeed fail)
         (define (try-next choices)
@@ -51,6 +52,15 @@
   'ok)
 
 (install-amb-ramb)
+
+(run-all-slot-tests)
+
+;; maybe the randomness is not enough,
+;; consider put `random` inside the analyze,
+;; only do it on the fly, and as needed
+(out (amb-eval-all `(list (ramb 1 2)
+                          (ramb 3 4)
+                          (ramb 'a 'b 'c)) (init-env)))
 
 (end-script)
 
