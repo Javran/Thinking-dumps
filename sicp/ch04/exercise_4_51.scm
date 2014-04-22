4cfc75714964d63c8998ea6b264ecad504d47415
Javran Cheng <Javran.c@gmail.com>
2014-04-21 23:15:58 -0400

    ex 4.51 - done

--- sicp/ch04/exercise_4_51.scm
+++ sicp/ch04/exercise_4_51.scm
@@ -21,7 +21,22 @@
                fail))))
 
   (define (test)
-    'todo)
+    (let ((env (init-env)))
+    (do-test
+     amb-eval-all
+     (list
+      ;; we only need to verify that
+      ;; the side effect is permanently set.
+      ;; which cannot be undone by calling the "fail" program
+      (mat `(begin
+              (define count 0)
+              (amb 'a 'b 'c 'd)
+              (permanent-set! count (+ count 1))
+              count)
+           env
+           `(1 2 3 4))
+           )))
+    'ok)
 
   (define handler
     (make-amb-handler
@@ -35,20 +50,31 @@
 (install-amb-permanent-set!)
 (run-all-slot-tests)
 
-(out
- (amb-eval-all
-  `(begin
-     (define (not x)
-       (if x #f #t))
-     (define (require x)
-       (if x 'pass (amb)))
-     (define count 0)
-     (let ((x (amb 'a 'b 'c))
-           (y (amb 'a 'b 'c)))
-       (permanent-set! count (+ count 1))
-       (require (not (eq? x y)))
-       (list x y count)))
-  (init-env)))
+(define (run-test-with symb-set)
+  (out
+   (amb-eval-all
+    `(begin
+       (define (not x)
+         (if x #f #t))
+       (define (require x)
+         (if x 'pass (amb)))
+       (define count 0)
+       (let ((x (amb 'a 'b 'c))
+             (y (amb 'a 'b 'c)))
+         (,symb-set count (+ count 1))
+         (require (not (eq? x y)))
+         (list x y count)))
+    (init-env))))
+
+;; permanent-set! cannot be undone,
+;; therefore "count" will keep counting
+(run-test-with `permanent-set!)
+;; however for "set!" every time before
+;; a new candidate is about to be attempted,
+;; the previous effect caused by "set!" is cancelled
+;; causing "count" to stay at 1 and never get a chance
+;; of increasing further.
+(run-test-with `set!)
 
 (end-script)
 
