365f9a884dc921c636626952be1f0811e8a74efd
Javran Cheng <Javran.c@gmail.com>
2014-04-23 22:13:53 -0400

    ex 4.52 - done

--- sicp/ch04/exercise_4_52.scm
+++ sicp/ch04/exercise_4_52.scm
@@ -18,21 +18,31 @@
   (define try-body cadr)
   (define fallback-val caddr)
 
+  ;; "if-fail" as derived expression
   (define (analyze-if-fail-derived exp)
+    ;; we need to provide a fallback if some expression have failed,
+    ;; to do the trick, we observe that in "(amb <exp1> <exp2>)"
+    ;; if "exp1" has failed, the second one will be attempted
+    ;; therefore we can put "<try-body>" before "<fallback-val>"
+    ;; in an "amb" special form.
     (amb-analyze `(amb ,(try-body exp)
                        ,(fallback-val exp))))
 
   (define (analyze-if-fail exp)
     (let ((try-body-a (amb-analyze (try-body exp)))
           (fallback-val-a (amb-analyze (fallback-val exp))))
+      ;; will be called with env + succ cont + fail cont
       (lambda (env succeed fail)
         (try-body-a
          env
          succeed
          (lambda ()
+           ;; when failed, evaluate fallback value
            (fallback-val-a
             env
             succeed
+            ;; if fallback still fail, resort on the "fail" provided
+            ;; from outside
             fail))))))
 
   (define (test)
