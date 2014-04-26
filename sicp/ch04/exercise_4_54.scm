a54dcd8ca578b15e7c4d0fb554a4bb8285be6fb9
Javran Cheng <Javran.c@gmail.com>
2014-04-25 22:31:17 -0400

    ex 4.54 - implemented

--- sicp/ch04/exercise_4_54.scm
+++ sicp/ch04/exercise_4_54.scm
@@ -12,13 +12,32 @@
       (lambda (env succeed fail)
         (pproc env
                (lambda (pred-value fail2)
+                 ;; the negation is not necessary
                  (if (not pred-value)
-                     ???
+                     (fail2)
                      (succeed 'ok fail2)))
                fail))))
 
   (define (test)
-    'todo)
+    (let ((env (init-env)))
+      (do-test
+       amb-eval-all
+       (list
+        (mat `(let ((x (amb 1 2 3)))
+                (require (even? x))
+                x)
+             env '(2))
+        (mat `(let ((x (amb 1 3 5)))
+                (require (even? x))
+                x)
+             env '())
+        (mat `(let ((x (amb 1 2 3 4))
+                    (y (amb 5 6 7 8)))
+                (require (= (+ x y) 8))
+                (cons x y))
+             env '((1 . 7) (2 . 6) (3 . 5)))
+        )))
+    'ok)
 
   (define handler
     (make-amb-handler
