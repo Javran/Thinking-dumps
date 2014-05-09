8e7a851595671b1d57a2140e63b9a036855f45f9
Javran Cheng <Javran.c@gmail.com>
2014-05-08 22:19:06 -0400

    4.4.1 - updating db...

--- sicp/ch04/4_4_1_a_sample_database.pl
+++ sicp/ch04/4_4_1_a_sample_database.pl
@@ -2,6 +2,24 @@
 :- initialization(['common.pl']).
 :- initialization(run).
 
-address(bitdiddleBen, slumerville, ridgeRoad, 10).
+% seems like in prolog the rules are not allowed to interleave
+% otherwise only the latest definition will be used.
 
-query(findall(A, address(A,B,C,D), _)).
+address(bitdiddleBen, slumerville, ridgeRoad, 10).
+address(hackerAlyssaP, cambridge, massAve, 78).
+address(fectCyD, cambridge, amesStreet, 3).
+
+
+job(bitdiddleBen, computerWizard).
+job(hackerAlyssaP, computerProgrammer).
+job(fectCyD, computerProgrammer).
+
+salary(bitdiddleBen, 60000).
+salary(hackerAlyssaP, 40000).
+salary(fectCyD, 35000).
+
+supervisor(hackerAlyssaP, bitdiddleBen).
+supervisor(fectCyD, bitdiddleBen).
+
+% who's address is near Cambridge?
+query(findall(A, address(A,cambridge,_,_), _)).
