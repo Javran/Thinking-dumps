0bb84e6d374123f324356850abe266f936ba5b5c
Javran Cheng <Javran.c@gmail.com>
2014-05-15 23:13:43 -0400

    4.4.4 - compound rule: outranked-by

--- sicp/ch04/4_4_1_rules.pl
+++ sicp/ch04/4_4_1_rules.pl
@@ -13,9 +13,20 @@ wheel(P) :-
     supervisor(Mid,P),
     supervisor(_,Mid).
 
+% a staff person is outranked by a boss
+%   in the organization if:
+% - the boss is the person's supervisor
+% - or if the person's supervisor is
+%   outranked by the boss
+outranked-by(StaffPerson,Boss) :-
+    supervisor(StaffPerson,Boss);
+    (supervisor(StaffPerson,Mid),
+     outranked-by(Mid,Boss)).
+
 % query all possible combinations
 query(findall([P1,P2], lives-near(P1,P2), _)).
 query(findall(P, wheel(P), _)).
+query(findall([S,B], outranked-by(S,B), _)).
 
 % query just for some cases
 query(findall(X, lives-near(X,bitdiddleBen), _)).
