d95f9ebd53a705bcda711d6afe1a978609334c68
Javran Cheng <Javran.c@gmail.com>
2014-05-16 20:50:32 -0400

    ex 4.57 - done

--- sicp/ch04/exercise_4_57.pl
+++ sicp/ch04/exercise_4_57.pl
@@ -9,3 +9,22 @@ can_replace(P1,P2) :-
 
 % query for a full set of "can-replace" without condition
 query(findall([P1,P2], can_replace(P1,P2), _)).
+
+% a. all people who can replace Cy D. Fect
+query(findall(P, can_replace(P,fectCyD), _)).
+
+% b. all people who can replace someone who is being paid
+% more than they are, together with the two salaries.
+
+% I'm not sure about what does it mean by saying
+%   "a person is being paid more than they are."
+%   but I think this should be the right answer:
+%   the result is of form: (<Person1,Salary1,Person2,Salary2>),
+%   which means Person1 can replace Person2,
+%   and Person2 is being paid more than Person1.
+query(findall([P1,S1,P2,S2],
+              (can_replace(P1,P2),
+               salary(P1,S1),
+               salary(P2,S2),
+               S1<S2),
+              _)).
