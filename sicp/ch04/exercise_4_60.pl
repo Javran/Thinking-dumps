81d548bbbda97edb2d8dace57a0780ff3049931c
Javran Cheng <Javran.c@gmail.com>
2014-05-17 21:52:25 -0400

    ex 4.60 - more comments

--- sicp/ch04/exercise_4_60.pl
+++ sicp/ch04/exercise_4_60.pl
@@ -2,15 +2,25 @@
 :- initialization(['4_4_1_rules.pl']).
 :- initialization(run).
 
+% compare two lists and determine
+%   if the first one is less than the second one
 list_less_than(A,B) :-
     length(A,LA),
     length(B,LB),
+    % if the length is not equal, we can simply compare the length
     LA < LB.
 list_less_than(A,B) :-
     length(A,LA),
     length(B,LB),
+    % if the length are equal, two list can't be exactly the same
+    %   if the inequality holds.
+    % a consequence follows immediately that A and B cannot be empty
     LA=LB,
     \+(A=B),
+    % therefore we pattern match the list, compare their head elements
+    %   if there is an equality in the head, that's it.
+    %   or otherwise, we clamin recursively that the tail of the first one
+    %   shall be less than the tail of the second one
     [HA|TA]=A,
     [HB|TB]=B,
     (HA < HB;
