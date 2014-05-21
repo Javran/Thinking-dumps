50d5f6a9f36eaffeb7a34dca512bae173b88b94a
Javran Cheng <Javran.c@gmail.com>
2014-05-20 22:19:36 -0400

    ex 4.62 - done (prolog), and also some explanation

--- sicp/ch04/exercise_4_62.pl
+++ sicp/ch04/exercise_4_62.pl
@@ -11,3 +11,13 @@ last_pair([_|T],X) :-
 query(findall(X,last_pair([3],X),_)).
 query(findall(X,last_pair([1,2,3],X),_)).
 query(findall(X,last_pair(2,X),_)).
+
+% the following line will not work.
+% and also I don't think this can work neither.
+% simply because there are too many ways to find a list
+% which has [3] as the last pair.
+% and prologd doesn't know how to exhaustively try
+% all the lists and print them out
+% (and also that this list can be infinite so
+% the program might not terminate)
+% query(findall(X,last_pair(X,[3]),_)).
