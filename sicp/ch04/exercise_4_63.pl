5ebc54a2d1bd456f8c6a94b25d56e23115b033bc
Javran Cheng <Javran.c@gmail.com>
2014-05-20 22:24:50 -0400

    ex 4.63 - just the database

---
+++ sicp/ch04/exercise_4_63.pl
@@ -0,0 +1,13 @@
+:- initialization(['common.pl']).
+:- initialization(run).
+
+son(adam,cain).
+son(cain,enoch).
+son(enoch,irad).
+son(irad,mehujael).
+son(mehujael,methushael).
+son(methushael,lamech).
+son(ada,jabal).
+son(ada,jubal).
+
+wife(lamech,ada).
