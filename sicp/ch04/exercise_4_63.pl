45c7f014120876ece792051e77850b80cf73a4f2
Javran Cheng <Javran.c@gmail.com>
2014-05-21 23:40:52 -0400

    ex 4.63 - done

--- sicp/ch04/exercise_4_63.pl
+++ sicp/ch04/exercise_4_63.pl
@@ -11,12 +11,24 @@ son(mehujael,methushael).
 son(methushael,lamech).
 son(ada,jabal).
 son(ada,jubal).
+son(M,S) :-
+    % M's wife is W
+    wife(M,W),
+    % W's son is S
+    son(W,S).
+    
 
+% wife(M,W): M's wife is W
 wife(lamech,ada).
 
 % grandson: G's grandson is S
 grandson(G,S) :-
+    % We'd better say:
+    % F's son is S
     son(F,S),
+    % G's son is F
     son(G,F).
 
 query(findall(X,grandson(cain,X),_)).
+query(findall(X,son(lamech,X),_)).
+query(findall(X,grandson(methushael,X),_)).
