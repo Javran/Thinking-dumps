72a813edd2570202db31dbbcfa9c9b10dc5f8e79
Javran Cheng <Javran.c@gmail.com>
2014-05-25 14:36:58 +0800

    problem 9 - some comments

--- h-99/problem-09.hs
+++ h-99/problem-09.hs
@@ -1,8 +1,15 @@
 pack :: Eq a => [a] -> [[a]]
+-- packing an empty list results in another empty list
 pack [] = []
+-- packing a non-empty list is first packing the tail of it
+-- and then:
 pack (h:t)
+ -- if the tail is empty, head forms a list of itself
  | null grouped = [[h]]
+ -- otherwise we examine the grouped list
+ -- if h is equal to the first group, then we simply insert it in front of it
  | head (head grouped) == h = (h:head grouped):tail grouped
+ -- otherwise, h forms a group of itself
  | otherwise = [h]:grouped
     where grouped = pack t
 
