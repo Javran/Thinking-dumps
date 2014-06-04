3cf25f4b557f32501c9e5c77fb10c38a36164747
Javran Cheng <Javran.c@gmail.com>
2014-06-05 05:35:14 +0900

    a little more about guards

--- lyah/ch04/guards-guards.hs
+++ lyah/ch04/guards-guards.hs
@@ -6,6 +6,9 @@
   (or otherwise it will be a relation (like in Prolog) which
   can return multiple results for one input value)
 -}
+
+import Data.List
+
 bmiTell :: (RealFrac a) => a -> String
 bmiTell bmi
     | bmi <= 18.5 = "underweight"
@@ -19,8 +22,15 @@ max' a b
      | a > b     = a
      | otherwise = b
 
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+     | a > b     = GT
+     | a < b     = LT
+     | otherwise = EQ
+
 main :: IO ()
 main = do
     print $ bmiTell (23.4 :: Float)
     print $ bmiTell (14.0 :: Float)
     print $ max' 'a' 'b'
+    print $ sortBy myCompare [1 :: Int ,9,2,8,3,7,4,6,5]
