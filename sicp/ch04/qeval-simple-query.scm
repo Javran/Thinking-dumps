197b38a14aa9f0d1fc9723dbcbcb8d28887997bf
Javran Cheng <Javran.c@gmail.com>
2014-06-09 22:38:36 +0800

    qeval - simple queries

---
+++ sicp/ch04/qeval-simple-query.scm
@@ -0,0 +1,7 @@
+(define (simple-query query-pattern frame-stream)
+  (stream-flatmap
+   (lambda (frame)
+     (stream-append-delayed
+      (find-assertions query-pattern frame)
+      (delay (apply-rules query-pattern frame))))
+   frame-stream))
