2529083e08831b5da37a6351a3ede6b0d48efe63
Javran Cheng <Javran.c@gmail.com>
2014-06-10 22:02:37 +0800

    qeval - main file

--- sicp/ch04/qeval-driver-loop.scm
+++ sicp/ch04/qeval-driver-loop.scm
@@ -53,4 +53,3 @@
 ;; Local variables:
 ;; proc-entry: ""
 ;; End:
-
---
+++ sicp/ch04/qeval.scm
@@ -0,0 +1,12 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+;; this should be the main file.
+;; TODO list:
+;; * data directed dispatch
+;;   (use my handler-register style instead,
+;;    which also enables some unit tests)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
