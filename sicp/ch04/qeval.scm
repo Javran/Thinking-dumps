a348829b02ea2df3e041702184ca55933064506e
Javran Cheng <Javran.c@gmail.com>
2014-06-11 23:21:23 +0800

    qeval - pattern matching
    still no clue WTF I'm writing.
    I really hate the style which introduces
    important code without any building block given

--- sicp/ch04/qeval.scm
+++ sicp/ch04/qeval.scm
@@ -23,6 +23,26 @@
         the-empty-stream
         (singleton-stream match-result))))
 
+(define (pattern-match pat dat frame)
+  (cond ((eq? frame 'failed) 'failed)
+        ((equal? pat dat) frame)
+        ((var? pat) (extend-if-consistent pat dat frame))
+        ((and (pair? pat) (pair? dat))
+         (pattern-match
+          (cdr pat)
+          (cdr dat)
+          (pattern-match (car pat)
+                         (car dat)
+                         frame)))
+        (else 'failed)))
+
+(define (extend-if-consistent var dat frame)
+  (let ((binding (binding-in-frame var frame)))
+    (if binding
+        (pattern-match
+         (binding-value binding) dat frame)
+        (extend var dat frame))))
+
 ;; Local variables:
 ;; proc-entry: ""
 ;; End:
