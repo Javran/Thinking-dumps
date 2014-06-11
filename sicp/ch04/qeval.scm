82450c5c5ca198a6fcb79bf078c5f0b8003a0b6f
Javran Cheng <Javran.c@gmail.com>
2014-06-11 23:17:04 +0800

    qeval - find and check assertions

--- sicp/ch04/qeval.scm
+++ sicp/ch04/qeval.scm
@@ -7,6 +7,22 @@
 ;;   (use my handler-register style instead,
 ;;    which also enables some unit tests)
 
+(define (find-assertions pattern frame)
+  (stream-flatmap
+   (lambda (datum)
+     (check-an-assertion datum pattern frame))
+   (fetch-assertions pattern frame)))
+
+(define (check-an-assertion
+         assertion
+         query-pat
+         query-frame)
+  (let ((match-result
+         (pattern-match query-pat assertion query-frame)))
+    (if (eq? match-result 'failed)
+        the-empty-stream
+        (singleton-stream match-result))))
+
 ;; Local variables:
 ;; proc-entry: ""
 ;; End:
