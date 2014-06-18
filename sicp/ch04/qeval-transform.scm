928e6c07f595e008a5a45168e428b3645c0aa757
Javran Cheng <Javran.c@gmail.com>
2014-06-18 22:45:48 +0800

    qeval - comments and some refactors (transform)

--- sicp/ch04/qeval-transform.scm
+++ sicp/ch04/qeval-transform.scm
@@ -20,26 +20,63 @@
                (substring chars 1 (string-length chars))))
         symbol)))
 
-(define (test-expand-question-mark)
-  (do-test
-   expand-question-mark
-   (list
-    (mat '?test '(? test))
-    (mat 'whatever 'whatever)
-    (mat '?symbol '(? symbol)))))
+;; deal with the expression after transformation
 
-(define (test-query-syntax-process)
-  (do-test
-   query-syntax-process
-   (list
-    (mat '(job ?x ?y) '(job (? x) (? y)))
-    (mat '?symbol '(? symbol))
-    (mat '(job x y) '(job x y)))))
+;; a variable is of form (? <var>)
+(define var?
+  (list-tagged-with '?))
+;; a constant symbol is a scheme symbol
+(define constant-symbol? symbol?)
+
+;; unique variables constructed during rule application
+;; is generated using the following code
+
+;; global rule counter to ensure the uniqueness
+(define rule-counter 0)
+;; generate a new rule application id,
+;; I think it's better to have "!" at the end
+;; to indicate that this procedure has side effects
+(define (new-rule-application-id!)
+  (set! rule-counter (add1 rule-counter))
+  rule-counter)
+
+;; TODO: not sure about what's the structure of "var"
+;; seems like "var" should be of form "(? <var>)"
+;; the generated variable is of form "(? <num> <var>)"
+(define (make-new-variable var rule-application-id)
+  `(? ,rule-application-id ,@(cdr var)))
+
+;; convert back the transformed expression
+(define (contract-question-mark variable)
+  (string->symbol
+   (string-append
+    "?"
+    (if (number? (cadr variable))
+        (string-append (symbol->string (caddr variable))
+                       "-"
+                       (number->string (cadr variable)))
+        (symbol->string (cadr variable))))))
+
+(define (test-qeval-transform)
+  (define (test-expand-question-mark)
+    (do-test
+     expand-question-mark
+     (list
+      (mat '?test '(? test))
+      (mat 'whatever 'whatever)
+      (mat '?symbol '(? symbol)))))
+
+  (define (test-query-syntax-process)
+    (do-test
+     query-syntax-process
+     (list
+      (mat '(job ?x ?y) '(job (? x) (? y)))
+      (mat '?symbol '(? symbol))
+      (mat '(job x y) '(job x y)))))
+  'ok)
 
 (if *qeval-tests*
-    (begin
-      (test-expand-question-mark)
-      (test-query-syntax-process))
+    (test-qeval-transform)
     'ok)
 
 ;; local variables:
