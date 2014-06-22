603f3c9263b56fee795f6f9c53bfad2adda3099d
Javran Cheng <Javran.c@gmail.com>
2014-06-22 23:09:39 +0800

    qeval - some testcases for rules

--- sicp/ch04/qeval-base.scm
+++ sicp/ch04/qeval-base.scm
@@ -50,6 +50,8 @@
 (define args cdr)
 
 ;; syntax of rules
+;; rules are of the form:
+;; '(rule <conclusion> [rule-body])
 (define rule?
   (list-tagged-with 'rule))
 (define conclusion cadr)
@@ -57,3 +59,34 @@
   (if (null? (cddr rule))
       '(always-true)
       (caddr rule)))
+
+(define (qeval-base-tests)
+  ;; test about rules
+  (do-test
+   rule?
+   (list
+    (mat 'not-a-rule #f)
+    (mat '(not-a-rule) #f)
+    (mat '(rule conclusion) #t)
+    (mat '(rule c (a b)) #t)))
+
+  (do-test
+   conclusion
+   (list
+    (mat '(rule concl) 'concl)
+    (mat '(rule c (a b)) 'c)))
+
+  (do-test
+   rule-body
+   (list
+    (mat '(rule concl) '(always-true))
+    (mat '(rule c (a b)) '(a b))))
+  'ok)
+
+(if *qeval-tests*
+    (qeval-base-tests)
+    'ok)
+
+;; Local variables:
+;; proc-entry: "./qeval.scm"
+;; End:
