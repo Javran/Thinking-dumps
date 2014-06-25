76d52494d3ca4f417be3425d014274ecc305e74c
Javran Cheng <Javran.c@gmail.com>
2014-06-25 23:45:28 +0800

    qeval - first test succeeded

--- sicp/ch04/qeval.scm
+++ sicp/ch04/qeval.scm
@@ -14,11 +14,38 @@
 (load "./qeval-pattern.scm")
 (load "./qeval-rules-and-unif.scm")
 
+(load "./qeval-simple-query.scm")
 (load "./qeval-compound-queries.scm")
 (load "./qeval-filters.scm")
 
-;; the following code is still unconfirmed
-;; this should be the main file.
+(load "./qeval-driver-loop.scm")
+
+(for-each
+ add-rule-or-assertion!
+ '((lisps mit-scheme)
+   (lisps racket)
+   (lisps elisp)
+   (lisps clojure)
+   (doge wow cool)
+   (doge such scheme)))
+
+(define (inflate-query q)
+  (lambda (frame)
+    (instantiate-exp
+     q
+     frame
+     (lambda (v f)
+       (contract-question-mark v)))))
+
+(out (stream->list
+      (stream-map
+       (inflate-query '(lisps (? x)))
+       (qeval '(lisps (? x)) (singleton-stream '())))))
+
+(out (stream->list
+      (stream-map
+       (inflate-query '(doge (? x) (? y)))
+       (qeval '(doge (? x) (? y)) (singleton-stream '())))))
 
 ;; Local variables:
 ;; proc-entry: ""
