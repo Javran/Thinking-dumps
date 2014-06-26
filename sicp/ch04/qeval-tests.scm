a1f07705e5a46b3b82502d3a830aeb776b7a6fd1
Javran Cheng <Javran.c@gmail.com>
2014-06-26 23:53:36 +0800

    qeval - moving testcases around

--- sicp/ch04/qeval-tests.scm
+++ sicp/ch04/qeval-tests.scm
@@ -2,6 +2,32 @@
 
 (define (qeval-tests)
   (qeval-initialize!)
+  (for-each
+   add-rule-or-assertion!
+   '((lisps mit-scheme)
+     (lisps racket)
+     (lisps elisp)
+     (lisps clojure)
+     (doge wow cool)
+     (doge such scheme)))
+
+  (define (inflate-query q)
+    (lambda (frame)
+      (instantiate-exp
+       q
+       frame
+       (lambda (v f)
+         (contract-question-mark v)))))
+
+  (out (stream->list
+        (stream-map
+         (inflate-query '(lisps (? x)))
+         (qeval '(lisps (? x)) (singleton-stream '())))))
+
+  (out (stream->list
+        (stream-map
+         (inflate-query '(doge (? x) (? y)))
+         (qeval '(doge (? x) (? y)) (singleton-stream '())))))
 
   (qeval-initialize!)
   'ok)
