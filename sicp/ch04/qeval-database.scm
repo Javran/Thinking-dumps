4f6977e8c88cef6f5362c650f321fc3e74bae565
Javran Cheng <Javran.c@gmail.com>
2014-07-01 00:32:25 +0800

    qeval - hide more procs from database

--- sicp/ch04/qeval-database.scm
+++ sicp/ch04/qeval-database.scm
@@ -70,19 +70,17 @@
 ;; get a stream, return an empty stream if not found
 (define (get-stream key1 key2)
   (let ((s (get key1 key2)))
-    (if s s the-empty-stream)))
-
-;; assertion is indexed in a way that
-;; the first key is the constant symbol,
-;; and the second key is the symbol "assertion-frame"
-(define (get-indexed-assertions pattern)
-  (get-stream (index-key-of pattern) 'assertion-stream))
-
-(define (get-all-assertions) THE-ASSERTIONS)
+    (or s the-empty-stream)))
 
 ;; "frame" is not use in the body of "fetch-assertions"
 ;; not sure what's the point of having this parameter.
 (define (fetch-assertions pattern frame)
+  (define (get-all-assertions) THE-ASSERTIONS)
+  ;; assertion is indexed in a way that
+  ;; the first key is the constant symbol,
+  ;; and the second key is the symbol "assertion-frame"
+  (define (get-indexed-assertions pattern)
+    (get-stream (index-key-of pattern) 'assertion-stream))
   (if (use-index? pattern)
       (get-indexed-assertions pattern)
       (get-all-assertions)))
@@ -91,62 +89,60 @@
 (define THE-RULES the-empty-stream)
 
 (define (fetch-rules pattern frame)
+  (define (get-all-rules) THE-RULES)
+  (define (get-indexed-rules pattern)
+    (stream-append
+     ;; different from assertions, rules might have variables
+     ;; in conclusion, we put all possible rules together to form another stream
+     (get-stream (index-key-of pattern) 'rule-stream)
+     (get-stream '? 'rule-stream)))
   (if (use-index? pattern)
       (get-indexed-rules pattern)
       (get-all-rules)))
 
-(define (get-all-rules) THE-RULES)
-(define (get-indexed-rules pattern)
-  (stream-append
-   ;; different from assertions, rules might have variables
-   ;; in conclusion, we put all possible rules together to form another stream
-   (get-stream (index-key-of pattern) 'rule-stream)
-   (get-stream '? 'rule-stream)))
-
 ;; add rules or assertions to database
 (define (add-rule-or-assertion! assertion)
+  ;; TODO
+  ;; more compact, but I think more detailed explanation should be written here
+  (define (indexable? pat)
+    (or (constant-symbol? (car pat))
+        (var? (car pat))))
+  (define (add-assertion! assertion)
+    (define (store-assertion-in-index assertion)
+      (if (indexable? assertion)
+          (let ((key (index-key-of assertion)))
+            (let ((current-assertion-stream
+                   (get-stream key 'assertion-stream)))
+              (put key
+                   'assertion-stream
+                   (cons-stream
+                    assertion
+                    current-assertion-stream))))))
+
+    (store-assertion-in-index assertion)
+    (let ((old-assertions THE-ASSERTIONS))
+      (set! THE-ASSERTIONS
+            (cons-stream assertion old-assertions))
+      'ok))
+
+  (define (add-rule! rule)
+    (define (store-rule-in-index rule)
+      (let ((pattern (conclusion rule)))
+        (if (indexable? pattern)
+            (let ((key (index-key-of pattern)))
+              (let ((current-rule-stream
+                     (get-stream key 'rule-stream)))
+                (put key
+                     'rule-stream
+                     (cons-stream rule
+                                  current-rule-stream)))))))
+    (store-rule-in-index rule)
+    (let ((old-rules THE-RULES))
+      (set! THE-RULES (cons-stream rule old-rules))
+      'ok))
   (if (rule? assertion)
       (add-rule! assertion)
       (add-assertion! assertion)))
-
-(define (indexable? pat)
-  (or (constant-symbol? (car pat))
-      (var? (car pat))))
-
-(define (store-assertion-in-index assertion)
-  (if (indexable? assertion)
-      (let ((key (index-key-of assertion)))
-        (let ((current-assertion-stream
-               (get-stream key 'assertion-stream)))
-          (put key
-               'assertion-stream
-               (cons-stream
-                assertion
-                current-assertion-stream))))))
-
-(define (add-assertion! assertion)
-  (store-assertion-in-index assertion)
-  (let ((old-assertions THE-ASSERTIONS))
-    (set! THE-ASSERTIONS
-          (cons-stream assertion old-assertions))
-    'ok))
-
-(define (store-rule-in-index rule)
-  (let ((pattern (conclusion rule)))
-    (if (indexable? pattern)
-        (let ((key (index-key-of pattern)))
-          (let ((current-rule-stream
-                 (get-stream key 'rule-stream)))
-            (put key
-                 'rule-stream
-                 (cons-stream rule
-                              current-rule-stream)))))))
-
-(define (add-rule! rule)
-  (store-rule-in-index rule)
-  (let ((old-rules THE-RULES))
-    (set! THE-RULES (cons-stream rule old-rules))
-    'ok))
 
 ;; Local variables:
 ;; proc-entry: "./qeval.scm"
