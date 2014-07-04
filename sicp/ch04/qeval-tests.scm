b86aef7bbc63a4b1eda9b67e5064dd00e8104121
Javran Cheng <Javran.c@gmail.com>
2014-07-04 17:21:56 +0800

    qeval - remove duplicates

--- sicp/ch04/qeval-tests.scm
+++ sicp/ch04/qeval-tests.scm
@@ -15,25 +15,6 @@
      (inflate-query q)
      (qeval q (singleton-stream '())))))
 
-;; if two sets are equal
-;; (order is not taken into account)
-(define (set-equal? s1 s2)
-  (cond ((null? s1) (null? s2))
-        ((null? s2) (null? s1))
-        (else
-         (set-equal?
-          (cdr s1)
-          (delete (car s1) s2)))))
-
-(do-test
- set-equal?
- (list
-  (mat '(1 2 3) '(3 2 1) #t)
-  (mat '(1 (a b (c (d))))
-       '((a b (c (d))) 1) #t)
-  (mat '() '(1 2) #f)
-  (mat '(1 2) '() #f)))
-
 (define (qeval-database-tests-db)
   (qeval-initialize!)
 
