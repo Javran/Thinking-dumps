7d914104265cba58ebdf0940a8c4a2b70d6e6670
Javran Cheng <Javran.c@gmail.com>
2014-07-05 17:52:17 +0800

    qeval - make display-stream work better

--- sicp/ch04/qeval-driver-loop.scm
+++ sicp/ch04/qeval-driver-loop.scm
@@ -1,12 +1,27 @@
-(define (display-stream s)
-  (for-each out (stream->list s)))
-
 (define (query-driver-loop)
+  (define (display-stream s)
+    (define (display-stream-intern s)
+      ;; stream->list would work, but
+      ;; not for cases that the stream is infinite.
+      ;; we print out lines as many as possible,
+      ;; even we know that it does not terminate.
+      (if (stream-null? s)
+          'ok
+          (begin
+            (out (stream-car s))
+            (display-stream-intern (stream-cdr s)))))
+    (if (stream-null? s)
+        ;; it's better to display something
+        ;; instead of keeping scilence
+        ;; when no result is available
+        (out "qeval: no result")
+        (display-stream-intern s)))
+
   (display "qeval> ")
   (let ((q (query-syntax-process (read))))
     (cond ((assertion-to-be-added? q)
            (add-rule-or-assertion! (add-assertion-body q))
-           (out "qeval: Assertion added to database.")
+           (out "qeval: assertion added to database.")
            (query-driver-loop))
           (else
            (display-stream
