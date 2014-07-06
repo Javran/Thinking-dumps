f11ac80f0c044ab0706009d2e76f9c88e6056660
Javran Cheng <Javran.c@gmail.com>
2014-07-06 15:51:01 +0800

    qeval - done, turn off tests

--- sicp/ch04/qeval.scm
+++ sicp/ch04/qeval.scm
@@ -1,10 +1,9 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
-(define *qeval-tests* #t)
+(define *qeval-tests* #f)
 
 (load "./qeval-utils.scm")
-
 (load "./qeval-stream.scm")
 (load "./qeval-data-directed.scm")
 (load "./qeval-frame.scm")
@@ -15,7 +14,6 @@
 (load "./qeval-handlers.scm")
 (load "./qeval-driver-loop.scm")
 (load "./qeval-export.scm")
-
 (load "./qeval-tests.scm")
 
 ;; Local variables:
