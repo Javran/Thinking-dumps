fe2834aa9ed6ae6dcf4aead109436e4dc9017ca1
Javran Cheng <Javran.c@gmail.com>
2014-07-11 23:23:39 +0800

    ex 4.66 - "lisp-eval" plan

---
+++ sicp/ch04/exercise_4_66.scm
@@ -0,0 +1,15 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+;; planning to have a new handler:
+;; (lisp-eval function return-var arg1 arg2 ...)
+;; where (function arg1 arg2 ...) is evaluated
+;; as lisp expression, and the result is pattern matched
+;; against return-var. and then we can write lisp
+;; functions to do whatever we want.
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
