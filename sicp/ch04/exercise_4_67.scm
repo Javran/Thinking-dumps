5d45eb554a67a0a6dea23d4f7e300a5810a1f722
Javran Cheng <Javran.c@gmail.com>
2014-07-12 23:32:36 +0800

    ex 4.67 - try to reproduce the problem

---
+++ sicp/ch04/exercise_4_67.scm
@@ -0,0 +1,35 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+;; from: 4_4_1_rules.scm
+;; "outranked-by" modified according to exercise 4.64.
+(load "./4_4_1_deductive_information_retrieval.scm")
+
+(apply
+ qe-asserts!
+ '(
+   (rule (lives-near ?person-1 ?person-2)
+         (and (address ?person-1 (?town . ?rest-1))
+              (address ?person-2 (?town . ?rest-2))
+              (not (same ?person-1 ?person-2))))
+
+   (rule (same ?x ?x))
+
+   (rule (wheel ?person)
+         (and (supervisor ?middle-manager ?person)
+              (supervisor ?x ?middle-manager)))
+
+   (rule (outranked-by ?staff-person ?boss)
+         (or (supervisor ?staff-person ?boss)
+             (and (outranked-by ?middle-manager ?boss)
+                  (supervisor ?staff-person ?middle-manager))))
+
+   ))
+
+(qe-all '(outranked-by (Bitdiddle Ben) ?who))
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
