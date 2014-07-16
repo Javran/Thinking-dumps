bc934e7928b0ffaa63ae8a86f0f5eeffc38fa29e
Javran Cheng <Javran.c@gmail.com>
2014-07-16 23:39:22 +0800

    ex 4.74 - comparison

--- sicp/ch04/exercise_4_74.scm
+++ sicp/ch04/exercise_4_74.scm
@@ -1,7 +1,42 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./qeval.scm")
+
 (define (simple-stream-flatmap proc s)
+  ;; analogy to concat-map
   (simple-flatten (stream-map proc s)))
 
 (define (simple-flatten stream)
-  (stream-map stream-car
-              (stream-filter (lambda (s) (not (stream-null? s)))
-                             stream)))
+  (stream-map
+   ;; after the filter is applied,
+   ;; we only have singletons
+   ;; safe to convert them back into elements
+   stream-car
+   (stream-filter
+    ;; the stream is either an empty one or a singleton
+    (lambda (s) (not (stream-null? s)))
+    stream)))
+
+(define (2d-list->stream xs)
+  (list->stream
+   (map list->stream xs)))
+
+(do-test
+ (lambda (x)
+   (let ((s (2d-list->stream x)))
+     (map stream->list
+          (list (stream-intermap identity s)
+                (simple-stream-flatmap identity s)))))
+ (list
+  (mat '((1) () () (2) (3) (4)) 'ignored)
+  (mat '(() () () ()) 'ignored)
+  (mat '((a) (b) (c) (d)) 'ignored))
+ (lambda (actual expected)
+   (apply equal? actual)))
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
