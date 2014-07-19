a91aff72785202b44fd1080d307e63ef138cff8b
Javran Cheng <Javran.c@gmail.com>
2014-07-19 23:47:06 +0800

    ex 4.75 - done

--- sicp/ch04/exercise_4_75.scm
+++ sicp/ch04/exercise_4_75.scm
@@ -3,23 +3,51 @@
 
 (load "./qeval.scm")
 
-(define (singleton-stream? s)
-  (and (not (stream-null? s))
-       (stream-null? (stream-cdr s))))
+(load "./exercise_4_75_common.scm")
 
-(define (uniquely-asserted query-pattern frame-stream)
-  (stream-intermap
-   (lambda (frame)
-     (let ((result
-            (qeval (car query-pattern)
-                   (singleton-stream frame))))
-       (if (singleton-stream? result)
-           result
-           the-empty-stream)))
-   frame-stream))
+(apply
+ qe-fresh-asserts!
+ '(
+   (one a)
+   (two a)
+   (two b)
+   ))
 
-(define (install-handler-unique)
-  (put 'unique 'qeval uniquely-asserted))
+(install-handler-unique)
+
+(out (qe-all '(unique (one ?x))))
+(out (qe-all '(unique (two ?x))))
+(out (qe-all '(unique (no-such ?x))))
+
+(load "./4_4_1_rules.scm")
+
+(install-handler-unique)
+
+(apply
+ qe-asserts!
+ '(
+   ;; should find Ben
+   (rule (ex-4-75-a ?x)
+         (unique (job ?x (computer wizard))))
+
+   ;; should print the empty stream
+   (rule (ex-4-75-b ?x)
+         (unique (job ?x (computer programmer))))
+
+   ;; should list all the jobs that are filled by only one person
+   (rule (ex-4-75-c ?x ?j)
+         (and (job ?x ?j) (unique (job ?anyone ?j))))
+
+   ;; list all people who supervise precisely one person.
+   (rule (ex-4-75-d ?sv)
+         (and (supervisor ?one ?sv)
+              (unique (supervisor ?anyone ?sv))))
+   ))
+
+(out (qe-all '(ex-4-75-a ?x)))
+(out (qe-all '(ex-4-75-b ?x)))
+(out (qe-all '(ex-4-75-c ?x ?j)))
+(out (qe-all '(ex-4-75-d ?x)))
 
 (end-script)
 
