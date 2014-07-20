7ce6e628eb04907c0e369f812f7c571a2870cb97
Javran Cheng <Javran.c@gmail.com>
2014-07-20 16:46:34 +0800

    Problem40 - improve performance

--- h-99/Problem40.hs
+++ h-99/Problem40.hs
@@ -1,9 +1,9 @@
 module Problem40 where
 
-import Problem31 (primes)
+import Problem31 (primes,isPrime)
 
 goldbach :: Int -> (Int, Int)
-goldbach n = head [(p,n-p) | p <- primes', (n-p) `elem` primes']
+goldbach n = head [(p,n-p) | p <- primes', isPrime (n-p)]
     where
         primes' = takeWhile (< n) primes
 
