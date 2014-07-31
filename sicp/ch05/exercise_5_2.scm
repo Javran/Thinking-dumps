975cfc7ec1316bf8add78939bc66e008c54dac6e
Javran Cheng <Javran.c@gmail.com>
2014-07-31 21:59:01 +0800

    ex 5.2 - done without verification

---
+++ sicp/ch05/exercise_5_2.scm
@@ -0,0 +1,15 @@
+(define factorial-machine
+  '(controller
+      (assign product (const 1))
+      (assign counter (const 1))
+    test-counter-gt-n
+      (test (op >) (reg counter) (reg n))
+      (branch (label factorial-done))
+      (assign result (op *) (reg counter) (reg product))
+      (assign t1 (reg result))
+      (assign product (reg t1))
+      (assign result (op +) (reg counter) (const 1))
+      (assign t2 (reg result))
+      (assign counter (reg t2))
+      (goto (label test-counter-gt-n))
+    factorial-done))
