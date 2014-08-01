885d5e6f23a4f11f01f1f8571e2991798997aa41
Javran Cheng <Javran.c@gmail.com>
2014-08-01 23:37:25 +0800

    ex 5.3 - controller v2

--- sicp/ch05/exercise_5_3.scm
+++ sicp/ch05/exercise_5_3.scm
@@ -9,3 +9,23 @@
     (goto (label test-good))
     iter-done
     (assign result (reg guess))))
+
+;; version 2 expands "good-enough?"
+(define sqrt-machine-v2
+  '(controller
+    (assign guess (const 1.0))
+    test-good
+    ;; expanded "good-enough?"
+    (assign result (op square) (reg guess))
+    (assign t1 (reg result))
+    (assign result (op -) (reg t1) (reg x))
+    (assign t1 (reg result))
+    (assign result (op abs) (reg t1))
+    (test (op <) (reg result) (const 0.001))
+    ;; "good-enough?" ends here
+    (branch (label iter-done))
+    (assign result (op improve) (reg guess))
+    (assign guess (reg result))
+    (goto (label test-good))
+    iter-done
+    (assign result (reg guess))))
