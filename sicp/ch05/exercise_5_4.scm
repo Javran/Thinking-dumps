b6c0cfb7e566db3d89c9ca6125bb4cc00d45169e
Javran Cheng <Javran.c@gmail.com>
2014-08-04 00:17:25 +0800

    ex 5.4.b - controller

--- sicp/ch05/exercise_5_4.scm
+++ sicp/ch05/exercise_5_4.scm
@@ -22,3 +22,28 @@
     (assign result (const 1))
     (goto (reg continue))
     expt-done))
+
+(define expt-machine-2
+  '(controller
+    (assign counter (reg n))
+    (assign product (const 1))
+    (assign continue (label expt-done))
+    expt-loop
+    (test (op =) (reg counter) (const 0))
+    (branch expt-immediate)
+    (assign result (op -) (reg counter) (const 1))
+    (assign counter (reg result))
+    (assign result (op *) (reg b) (reg product))
+    (assign product (reg result))
+    (save continue)
+    (assign continue (label after-expt))
+    (goto (label expt-loop))
+    after-expt
+    (restore continue)
+    (goto (reg continue))
+
+    expt-immediate
+    (assign result (reg product))
+    (goto (reg continue))
+
+    expt-done))
