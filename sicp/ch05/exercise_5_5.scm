beddecc148756a1936b55d7f7cdd28adf1e4eca5
Javran Cheng <Javran.c@gmail.com>
2014-08-04 22:14:50 +0800

    ex 5.5 - handler for "save"

--- sicp/ch05/exercise_5_5.scm
+++ sicp/ch05/exercise_5_5.scm
@@ -7,45 +7,66 @@
        (eq? 'controller (car c))))
 
 ;; let's define machine state structure
-;; (list 'machine-state <insns> <lbl-insn-alist> <stack> <regs>)
+;; (list 'machine-state <insns> <jump-alist> <stack> <regs>)
 ;; where `insns` is the current list of pending instructions
-;; `lbl-insn-alist` is a list of label - instruction list pairs
+;; `jump-alist` is a list of label-instruction list pairs
 ;; `stack` is the current stack
 ;; `regs` is the current registers
 (define ms-insns cadr)
+(define ms-jump-alist caddr)
 (define (ms-get-insns label ms)
-  (cadr (assoc label (caddr ms))))
+  (cadr (assoc label (ms-jump-alist ms))))
 (define ms-stack cadddr)
+(define (ms-stack-top ms)
+  (car (ms-stack ms)))
+(define (ms-stack-pop ms)
+  (list 'machine-state
+        (ms-insns ms)
+        (ms-jump-alist ms)
+        (cdr (ms-stack ms))
+        (ms-regs ms)))
+(define (ms-stack-push v ms)
+  (list 'machine-state
+        (ms-insns ms)
+        (ms-jump-alist ms)
+        (cons v (ms-stack ms))
+        (ms-regs ms)))
 (define ms-regs  (compose car cddddr))
+(define (ms-get-reg name ms)
+  (let ((result (assoc name (ms-regs ms))))
+    (if result
+        (cadr result)
+        #f)))
+
+;; accepts a list of instructions
+(define (make-jump-alist instructions)
+  (let loop ((insns instructions)
+             (jump-alist '()))
+    (if (null? insns)
+        jump-alist
+        (if (symbol? (car insns))
+            ;; add a new label
+            (loop
+             (cdr insns)
+             (cons (list (car insns) (cdr insns))
+                   jump-alist))
+            ;; skip it
+            (loop
+             (cdr insns)
+             jump-alist)))))
 
 ;; instead of hand-simulation, let's try to write one simulator
 (define (run-machine controller-desc)
   (let ((instructions (cdr controller-desc)))
-    (let ((label-insn-alist
-           (let loop ((insns instructions)
-                      (lbl-insn-alist '()))
-             (if (null? insns)
-                 lbl-insn-alist
-                 (if (symbol? (car insns))
-                     ;; add a new label
-                     (loop
-                      (cdr insns)
-                      (cons (list (car insns) (cdr insns))
-                            lbl-insn-alist))
-                     ;; skip it
-                     (loop
-                      (cdr insns)
-                      lbl-insn-alist))))))
-      (out label-insn-alist))))
+    (let ((label-insn-alist (make-jump-alist instructions)))
+      'todo)))
 
-(run-machine
- '(controller
-   label-1
-   (assign n (reg aaa))
-   label-2
-   (assign n (reg bbb))
-   label-3
-   label-4))
+(define (handle-save body ms)
+  (let* ((var (car body))
+         (val (ms-get-reg var ms)))
+    (ms-stack-push val ms)))
+
+(out (handle-save '(n) (list 'machine-state '(a b c) '() '(1 2 3) '((x 100) (n 400)))))
 
 (end-script)
 
