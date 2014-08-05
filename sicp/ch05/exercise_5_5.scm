a8060d41a5343ec7cfbde0792e020c70f0fdaa64
Javran Cheng <Javran.c@gmail.com>
2014-08-06 00:20:15 +0800

    ex 5.5 - redo machine state impl

--- sicp/ch05/exercise_5_5.scm
+++ sicp/ch05/exercise_5_5.scm
@@ -1,50 +1,15 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(load "./data-directed.scm")
+
 (define (controller-description? c)
   (and (list? c)
        (not (null? c))
        (eq? 'controller (car c))))
 
-;; let's define machine state structure
-;; (list 'machine-state <insns> <jump-alist> <stack> <regs>)
-;; where `insns` is the current list of pending instructions
-;; `jump-alist` is a list of label-instruction list pairs
-;; `stack` is the current stack
-;; `regs` is the current registers
-(define ms-insns cadr)
-(define ms-jump-alist caddr)
-(define (ms-get-insns label ms)
-  (cadr (assoc label (ms-jump-alist ms))))
-(define ms-stack cadddr)
-(define (ms-stack-top ms)
-  (car (ms-stack ms)))
-(define (ms-stack-pop ms)
-  (list 'machine-state
-        (ms-insns ms)
-        (ms-jump-alist ms)
-        (cdr (ms-stack ms))
-        (ms-regs ms)))
-(define (ms-stack-push v ms)
-  (list 'machine-state
-        (ms-insns ms)
-        (ms-jump-alist ms)
-        (cons v (ms-stack ms))
-        (ms-regs ms)))
-(define ms-regs  (compose car cddddr))
-(define (ms-get-reg name ms)
-  (let ((result (assoc name (ms-regs ms))))
-    (if result
-        (cadr result)
-        #f)))
 
-(define (ms-set-reg name val ms)
-  (list 'machine-state
-        (ms-insns ms)
-        (ms-jump-alist ms)
-        (ms-stack ms)
-        (cons (list name val)
-              (del-assoc name (ms-regs ms)))))
+(load "./exercise_5_5_machine_state.scm")
 
 ;; accepts a list of instructions
 (define (make-jump-alist instructions)
@@ -71,18 +36,30 @@
 
 (define (handle-save body ms)
   (let* ((var (car body))
-         (val (ms-get-reg var ms)))
+         (val (ms-reg-get var ms)))
     (ms-stack-push val ms)))
 
 (define (handle-restore body ms)
   (let* ((var (car body))
          (val (ms-stack-top ms)))
-    (ms-set-reg var val
+    (ms-reg-set var val
                 (ms-stack-pop ms))))
 
-(out (handle-save '(n) (list 'machine-state '(a b c) '() '(1 2 3) '((x 100) (n 400)))))
-(out (handle-restore '(new-var)
-                     (list 'machine-state '(a b c) '() '(1 2 3) '((x 100)))))
+(out (handle-save
+      '(n)
+      '(machine-state
+        ((insns (a b c))
+         (jump-alist ())
+         (stack (1 2 3))
+         (regs ((x 100) (n 400)))))))
+
+(out (handle-restore
+      '(new-var)
+      '(machine-state
+        ((insns (a b c))
+         (jump-alist ())
+         (stack (1 2 3))
+         (regs ((x 100)))))))
 
 (end-script)
 
---
+++ sicp/ch05/exercise_5_5_machine_state.scm
@@ -0,0 +1,75 @@
+;; a machine state is a list:
+;; (list 'machine-state <alist>)
+;;
+;; where each element of <alist> is a list of:
+;; (list <key> <value>)
+;; at least containing the following fields:
+;; `insns` is the current list of pending instructions
+;; `jump-alist` is a list of label-instruction list pairs
+;; `stack` is the current stack
+;; `regs` is the current registers
+
+(define (machine-state? data)
+  (and (list? data)
+       (not (null? data))
+       (eq? (car data) 'machine-state)))
+
+(define (ensure-machine-state data)
+  (if (machine-state? data)
+      'ok
+      (error "invalid machine state object")))
+
+(define (ms-get-field key data)
+  (ensure-machine-state data)
+  (let ((alist (cadr data)))
+    (cadr (assoc key alist))))
+
+(define (ms-set-field key value data)
+  (ensure-machine-state data)
+  (let ((alist (cadr data)))
+    (list 'machine-state
+          (cons (list key value)
+                (del-assoc key
+                           (cadr data))))))
+
+(define (ms-modify-field key proc data)
+  (let ((old-val (ms-get-field key data)))
+    (ms-set-field key (proc old-val) data)))
+
+(define (make-empty-machine)
+  `(machine-state
+    ((insns ())
+     (jump-alist ())
+     (stack ())
+     (regs ()))))
+
+(define ms-insns
+  ((curry2 ms-get-field) 'insns))
+(define ms-jump-alist
+  ((curry2 ms-get-field) 'jump-alist))
+(define ms-stack
+  ((curry2 ms-get-field) 'stack))
+(define ms-regs
+  ((curry2 ms-get-field) 'regs))
+
+(define (ms-query-label lbl ms)
+  (let ((jump-alist (ms-jump-alist ms)))
+    (cadr (assoc lbl jump-alist))))
+
+(define ms-stack-top
+  (compose car ms-stack))
+(define (ms-stack-pop ms)
+  (ms-modify-field 'stack cdr ms))
+(define (ms-stack-push v ms)
+  (ms-modify-field 'stack ((curry2 cons) v) ms))
+
+(define (ms-reg-set reg val ms)
+  (ms-modify-field
+   'regs
+   (lambda (alist)
+     (cons (list reg val)
+           (del-assoc reg alist)))
+   ms))
+
+(define (ms-reg-get reg ms)
+  (cadr (assoc reg (ms-regs ms))))
