392dcbaed4df82fac58b1f9e144ee714e004ff2a
Javran Cheng <Javran.c@gmail.com>
2014-08-06 20:52:09 +0800

    ex 5.3 - done (verified)

--- sicp/ch05/exercise_5_3.scm
+++ sicp/ch05/exercise_5_3.scm
@@ -1,3 +1,17 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./exercise_5_5_common.scm")
+
+;; fix x = 144
+(define test-const 144)
+
+(define x test-const)
+(define (good-enough? guess)
+  (< (abs (- (square guess) x)) 0.001))
+(define (improve guess)
+  (average guess (/ x guess)))
+
 (define sqrt-machine-v1
   '(controller
     (assign guess (const 1.0))
@@ -9,6 +23,14 @@
     (goto (label test-good))
     iter-done
     (assign result (reg guess))))
+
+;; test sqrt-machine-v1
+(out "sqrt-machine-v1:")
+(ms-pretty-print
+ (execute-controller-with-regs
+  sqrt-machine-v1
+  `((x ,test-const))))
+(newline)(newline)
 
 ;; version 2 expands "good-enough?"
 (define sqrt-machine-v2
@@ -29,6 +51,16 @@
     (goto (label test-good))
     iter-done
     (assign result (reg guess))))
+
+(set! good-enough? #f)
+
+;; test sqrt-machine-v2
+(out "sqrt-machine-v2:")
+(ms-pretty-print
+ (execute-controller-with-regs
+  sqrt-machine-v2
+  `((x ,test-const))))
+(newline)(newline)
 
 ;; version 3 expands "improve"
 (define sqrt-machine-v3
@@ -54,4 +86,13 @@
     iter-done
     (assign result (reg guess))))
 
-;; TODO: run it if possible
+(set! improve #f)
+(set! x #f)
+
+;; test sqrt-machine-v3
+(out "sqrt-machine-v3:")
+(ms-pretty-print
+ (execute-controller-with-regs
+  sqrt-machine-v3
+  `((x ,test-const))))
+(newline)(newline)
