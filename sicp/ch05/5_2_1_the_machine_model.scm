6cc5eb9dbf02d3fa5cfc61e91cb523d392c6756e
Javran Cheng <Javran.c@gmail.com>
2014-08-07 23:58:20 +0800

    5.2.1 - start impl (registers implemented)

---
+++ sicp/ch05/5_2_1_the_machine_model.scm
@@ -0,0 +1,38 @@
+(define (make-machine register-names
+                      ops
+                      controller-text)
+  ;; register-names: a list of register names
+  ;;   to be used in this machine model
+  (let ((machine (make-new-machine)))
+    (for-each
+     (lambda (register-name)
+       ((machine 'allocate-register) register-name))
+     register-names)
+    ((machine 'install-opeartions) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
+
+;; registers
+(define (make-register name)
+  (let ((contents '*unassigned*))
+    (define (dispatch message)
+      (cond
+       ((eq? message 'get) contents)
+       ((eq? message 'set)
+        (lambda (value)
+          (set! contents value)))
+       ;; seems like the "name" is not used anywhere...
+       ;; let's make it useful.
+       ((eq? message 'name) name)
+       (else
+        (error "unknown request: REGISTER"
+               message))))
+    dispatch))
+
+(define (get-contents register)
+  (register 'get))
+(define (set-contents! register value)
+  ((register 'set) value))
+(define (get-reg-name register)
+  (register 'name))
