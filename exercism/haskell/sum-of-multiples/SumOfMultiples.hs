f8606293d208a78eda7085b8eecde1f4e8994c69
Javran Cheng <Javran.c@gmail.com>
2014-08-09 00:09:47 +0800

    SumOfMultiples - first version

---
+++ exercism/haskell/sum-of-multiples/SumOfMultiples.hs
@@ -0,0 +1,22 @@
+module SumOfMultiples
+    ( sumOfMultiples
+    , sumOfMultiplesDefault
+    )
+where
+
+import qualified Data.List.Ordered as OL
+
+sumOfMultiples :: [Int] -> Int -> Int
+sumOfMultiples xs upBound = sum $ takeWhile (< upBound) multiples
+    where
+        --   [a,b,c...] =>
+        -- [ [a,2a,3a,...]
+        -- , [b,2b,3b,...]
+        -- , ...
+        -- ]
+        xsMultiples = map (\x -> map (* x) [1..]) xs
+        -- ordered list merge, thanks to laziness.
+        multiples = foldr OL.union [] xsMultiples
+
+sumOfMultiplesDefault :: Int -> Int
+sumOfMultiplesDefault = sumOfMultiples [3,5]
