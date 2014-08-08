bf77113fcb1b5cd0e25dc9e4185d64e43e765496
Javran Cheng <Javran.c@gmail.com>
2014-08-08 23:18:00 +0800

    5.2.1 - make machine

--- sicp/ch05/5_2_1_the_machine_model.scm
+++ sicp/ch05/5_2_1_the_machine_model.scm
@@ -63,3 +63,68 @@
 (define (pop stack) (stack 'pop))
 (define (push stack value) ((stack 'push) value))
 
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))))
+          (register-table
+           ;; will this pollute the namespace?
+           (list (list 'pc pc)
+                 (list 'flag flag))))
+      ;; allocate a new register
+      ;; a unique name is enforced
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: "
+                   name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:"
+                     name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq)
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register)
+               allocate-register)
+              ((eq? message 'get-register)
+               lookup-register)
+              ((eq? message 'install-opeartions)
+               (lambda (ops)
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else
+               (error "Unknown request: MACHINE"
+                      message))))
+      dispatch)))
+
+(define (stack machine) (machine 'start))
+(define (get-register-contents machine register-name)
+  (get-contents (get-register machine register-name)))
+(define (set-register-contents! machine register-name value)
+  (set-contents! (get-register machine register-name)
+                 value)
+  'done)
+(define (get-register machine reg-name)
+  ((machine 'get-register) reg-name))
