980dfd5f094a1a9ea361309da40beac2d3570ca3
Javran Cheng <Javran.c@gmail.com>
2014-08-09 00:29:48 +0800

    SpaceAge - first version

---
+++ exercism/haskell/space-age/SpaceAge.hs
@@ -0,0 +1,29 @@
+module SpaceAge
+where
+
+data Planet
+    = Earth
+    | Mercury
+    | Venus
+    | Mars
+    | Jupiter
+    | Saturn
+    | Uranus
+    | Neptune
+
+ageInEarthYear :: Planet -> Float
+ageInEarthYear p = case p of
+    Earth   -> 1
+    Mercury -> 0.2408467
+    Venus   -> 0.61519726
+    Mars    -> 1.8808158
+    Jupiter -> 11.862615
+    Saturn  -> 29.447498
+    Uranus  -> 84.016846
+    Neptune -> 164.79132
+
+ageInSec :: Planet -> Float
+ageInSec = (* 31557600) . ageInEarthYear
+
+ageOn :: Planet -> Float -> Float
+ageOn p = ( / ageInSec p)
