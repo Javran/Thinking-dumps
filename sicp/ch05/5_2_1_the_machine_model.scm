8d7add757866a8b6eff56af9de7aee918b814c8d
Javran Cheng <Javran.c@gmail.com>
2014-08-08 23:44:28 +0800

    5.2.1 - instructions

--- sicp/ch05/5_2_1_the_machine_model.scm
+++ sicp/ch05/5_2_1_the_machine_model.scm
@@ -128,3 +128,60 @@
   'done)
 (define (get-register machine reg-name)
   ((machine 'get-register) reg-name))
+
+(define (assemble controller-text machine)
+  (extract-labels
+   controller-text
+   (lambda (insts labels)
+     (update-insts! insts labels machine)
+     insts)))
+
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels
+       (cdr text)
+       ;; kind of hating this unnessary complexity...
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+               ;; why we have to distinct them here??
+               ;; and why we have to "make-instruction"
+               ;; when they are already called "instructions"??
+               (receive insts
+                        (cons (make-label-entry next-inst
+                                                insts)
+                              labels))
+               (receive (cons (make-instruction next-inst)
+                              insts)
+                        labels)))))))
+
+(define (update-insts! insts labels machine)
+  (let ((pc (get-register machine 'pc))
+        (flag (get-register machine 'flag))
+        (stack (machine 'stack))
+        (ops (machine 'operations)))
+    (for-each
+     (lambda (inst)
+       (set-instruction-execution-proc!
+        inst
+        (make-execution-procedure
+         (instruction-text inst)
+         labels machine pc flag stack ops)))
+     insts)))
+
+(define (make-instruction text)
+  (cons text '()))
+(define instruction-text car)
+(define instruction-execution-proc cdr)
+(define (set-instruction-execution-proc! inst proc)
+  (set-cdr! inst proc))
+
+(define (make-label-entry label-name insts)
+  (cons label-name insts))
+(define (lookup-label labels label-name)
+  (let ((val (assoc label-name labels)))
+    (if val
+        (cdr val)
+        (error "Undefined label: ASSEMBLE"
+               label-name))))
