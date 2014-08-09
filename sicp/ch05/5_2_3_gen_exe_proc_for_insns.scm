addc6da03807b1c9de87fd0011cca7d154c42f79
Javran Cheng <Javran.c@gmail.com>
2014-08-09 23:31:00 +0800

    5.2.3 - start
    some serious redesign must be done to eliminate those ugly implementation

---
+++ sicp/ch05/5_2_3_gen_exe_proc_for_insns.scm
@@ -0,0 +1,23 @@
+;; TODO: unacceptable code
+;; some serious redesign must be done after we get
+;; the whole system to work
+(define (make-execution-procedure
+         inst labels machine pc flag stack ops)
+  ;; NO! ... THIS PART IS EXTREMELY UGLY!
+  (cond ((eq? (car inst) 'assign)
+         (make-assign inst machine labels ops pc))
+        ((eq? (car inst) 'test)
+         (make-test inst machine labels ops pc))
+        ((eq? (car inst) 'branch)
+         (make-branch inst machine labels flag pc))
+        ((eq? (car inst) 'goto)
+         (make-goto inst machine labels pc))
+        ((eq? (car inst) 'save)
+         (make-save inst machine stack pc))
+        ((eq? (car inst) 'restore)
+         (make-restore inst machine stack pc))
+        ((eq? (car inst) 'perform)
+         (make-perform inst machine labels ops pc))
+        (else
+         (error "Unknown instruction type: ASSEMBLE"
+                inst))))
