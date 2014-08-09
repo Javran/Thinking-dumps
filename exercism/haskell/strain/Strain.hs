5b99406cfef2ad8b001299896dd803a89a585e9e
Javran Cheng <Javran.c@gmail.com>
2014-08-09 23:35:49 +0800

    Strain - follow README

--- exercism/haskell/strain/Strain.hs
+++ exercism/haskell/strain/Strain.hs
@@ -5,7 +5,9 @@ module Strain
 where
 
 keep :: (a -> Bool) -> [a] -> [a]
-keep = filter
+keep _ [] = []
+keep p (x:xs) = let rest = keep p xs
+                in if p x then x:rest else rest
 
 discard :: (a -> Bool) -> [a] -> [a]
-discard p = filter (not . p)
+discard p = keep (not . p)
