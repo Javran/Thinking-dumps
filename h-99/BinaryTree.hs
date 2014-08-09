539748ebdacdf43d6346d93dbfb84193b8cb893f
Javran Cheng <Javran.c@gmail.com>
2014-08-09 14:21:14 +0800

    Problem61 - done

--- h-99/BinaryTree.hs
+++ h-99/BinaryTree.hs
@@ -8,3 +8,20 @@ data Tree a
 singleton, leaf :: a -> Tree a
 singleton v = Branch v Empty Empty
 leaf = singleton
+
+tree1 :: Tree Char
+tree1 = Branch 'a' (Branch 'b' (leaf 'd')
+                               (leaf 'e'))
+                   (Branch 'c' Empty
+                               (Branch 'f' (leaf 'g')
+                                           Empty))
+
+tree2 :: Tree Char
+tree2 = Branch 'a' Empty Empty
+
+tree3 :: Tree Char
+tree3 = Empty
+
+tree4 :: Tree Int
+tree4 = Branch 1 (Branch 2 Empty (Branch 4 Empty Empty))
+                 (Branch 2 Empty Empty)
