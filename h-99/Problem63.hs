c564a25abac9878d7eb3e38de7a367218e3545bf
Javran Cheng <Javran.c@gmail.com>
2014-08-09 17:09:53 +0800

    Problem63 - done

--- h-99/Problem63.hs
+++ h-99/Problem63.hs
@@ -1,9 +1,10 @@
 module Problem63
 where
 
-import Control.Arrow
+import Control.Applicative
 import qualified Data.Sequence as Seq
 import Data.Sequence (viewl,ViewL(..),(|>))
+import Text.Printf
 import BinaryTree
 
 completeBinaryTree :: Int -> Tree Char
@@ -20,5 +21,25 @@ completeBinaryTree n = reduce (Seq.fromList bottomLayer)
                 EmptyL -> t1
                 (t2 :< rest2) -> reduce (rest2 |> Branch 'x' t1 t2)
 
+linearize :: Tree a -> [Tree a]
+linearize = concat . toLayers
+    where
+        -- break tree into layers
+        toLayers Empty = [[Empty]]
+        toLayers t@(Branch _ l r) = [t] : zipWith (++) (toLayers l) (toLayers r)
+
+isEmpty :: Tree a -> Bool
+isEmpty Empty = True
+isEmpty _ = False
+
+isCompleteBinaryTree :: Tree a -> Bool
+isCompleteBinaryTree = all isEmpty . dropWhile (not . isEmpty) . linearize
+
 main :: IO ()
-main = mapM_ (print . (id &&& completeBinaryTree)) [0..10]
+main = do
+    mapM_ (printf "%d => %s\n" <*> show . completeBinaryTree) [0..10]
+    print (all (isCompleteBinaryTree . completeBinaryTree) [0..100])
+    print $ completeBinaryTree 4
+    print $ isCompleteBinaryTree $ Branch 'x' (Branch 'x' Empty Empty) (Branch 'x' Empty Empty)
+    print $ isCompleteBinaryTree Empty
+    print $ isCompleteBinaryTree tree4
