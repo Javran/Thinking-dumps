da40b29270870d86a12e99a0eae30864a3561347
Javran Cheng <Javran.c@gmail.com>
2014-08-10 22:24:10 +0800

    5.2.3 - assign

--- sicp/ch05/5_2_3_gen_exe_proc_for_insns.scm
+++ sicp/ch05/5_2_3_gen_exe_proc_for_insns.scm
@@ -4,6 +4,8 @@
 (define (make-execution-procedure
          inst labels machine pc flag stack ops)
   ;; NO! ... THIS PART IS EXTREMELY UGLY!
+  ;; at least we can consider making arguments exactly the same
+  ;; therefore dispatching will be enabled
   (cond ((eq? (car inst) 'assign)
          (make-assign inst machine labels ops pc))
         ((eq? (car inst) 'test)
@@ -21,3 +23,24 @@
         (else
          (error "Unknown instruction type: ASSEMBLE"
                 inst))))
+
+;; assign
+(define (make-assign inst machine labels operations pc)
+  (let ((target
+         (get-register machine (assign-reg-name inst)))
+        (value-exp
+         (assign-value-exp inst)))
+    (let ((value-proc
+           (if (opeartion-exp? value-exp)
+               (make-opeartion-exp
+                value-exp machine labels operations)
+               (make-primitive-exp
+                (car value-exp) machine labels))))
+      (lambda ()
+        (set-contents! target (value-proc))
+        (advance-pc pc)))))
+
+(define (assign-reg-name assign-instruction)
+  (cadr assign-instruction))
+(define (assign-value-exp assign-instruction)
+  (cddr assign-instruction))
