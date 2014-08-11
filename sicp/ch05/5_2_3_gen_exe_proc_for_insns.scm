caef86c94e8d7a59d4743210ae621d7ed76f73be
Javran Cheng <Javran.c@gmail.com>
2014-08-11 21:34:56 +0800

    restore and perform

--- sicp/ch05/5_2_3_gen_exe_proc_for_insns.scm
+++ sicp/ch05/5_2_3_gen_exe_proc_for_insns.scm
@@ -100,3 +100,33 @@
                   inst)))))
 (define (goto-dest goto-instruction)
   (cadr goto-instruction))
+
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (push stack (get-contents reg))
+      (advance-pc pc))))
+
+;; restore
+(define (make-restore inst machine stack pc)
+  (let ((reg (get-register machine
+                           (stack-inst-reg-name inst))))
+    (lambda ()
+      (set-contents! reg (pop stack))
+      (advance-pc pc))))
+
+(define (stack-inst-reg-name stack-instruction)
+  (cadr stack-instruction))
+
+;; perform
+(define (make-perform inst machine labels operations pc)
+  (let ((action (perform-action inst)))
+    (if (operation-exp? action)
+        (let ((action-proc
+               (make-operation-exp
+                action machine labels operations)))
+          (lambda ()
+            (action-proc) (advance-pc pc)))
+        (error "Bad PERFORM instruction: ASSEMBLE" inst))))
+(define (perform-action inst) (cdr inst))
