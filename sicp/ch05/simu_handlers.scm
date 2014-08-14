3208f8e5c8f6d03c3090d0ba67a11e542addc030
Javran Cheng <Javran.c@gmail.com>
2014-08-14 19:11:00 +0800

    simu - more comments on "assign" handler

--- sicp/ch05/simu_handlers.scm
+++ sicp/ch05/simu_handlers.scm
@@ -12,23 +12,31 @@
   (set! get-handler get1)
   (set! init-handler-table! init1))
 
+;; analyze the list of instructions once for all,
+;; yielding "thunks" which can be used multiple times
+;; without too much time consumption.
+
 ;; handler type:
 ;; (<handler> inst labels machine pc flag stack ops)
 (define (assign-handler inst labels machine pc flag stack ops)
+  ;; TODO: wouldn't it be a better idea to let
+  ;; instruction handlers destruct the instruction in question?
   (let ((target
          (get-register machine (assign-reg-name inst)))
         (value-exp
          (assign-value-exp inst)))
     (let ((value-proc
+           ;; yields a value when run as a procedure
            (if (opeartion-exp? value-exp)
-               (make-opeartion-exp
+               (make-operation-exp
                 value-exp machine labels operations)
                (make-primitive-exp
                 (car value-exp) machine labels))))
       (lambda ()
+        ;; execute an operation or fetch register or constants according
+        ;; to the instruction, advance "pc"
         (set-contents! target (value-proc))
         (advance-pc pc)))))
-
 
 (define (test-handler inst labels machine pc flag stack ops)
   'todo)
