2ec9d603ecf50f855b070521d7886b8c1aa176ce
Javran Cheng <Javran.c@gmail.com>
2014-08-21 18:33:11 -0400

    simu - rework assign handler

--- sicp/ch05/simu_handlers.scm
+++ sicp/ch05/simu_handlers.scm
@@ -22,65 +22,70 @@
 ;; yielding "thunks" which can be used multiple times
 ;; without too much time consumption.
 
-(define (make-primitive-exp exp machine labels)
-  (cond ((constant-exp? exp)
-         (let ((c (constant-exp-value exp)))
-           (lambda ()
-             c)))
-        ((label-exp? exp)
-         (let ((insts (lookup-label
-                       labels
-                       (label-exp-label exp))))
-           (lambda () insts)))
-        ((register-exp? exp)
-         (let ((r (get-register machine
-                                (register-exp-reg exp))))
-           (lambda () (get-contents r))))
-        (else
-         (error "Unknown expression type: ASSEMBLE"
-                exp))))
+(define (make-primitive-exp exp m labels)
+  ;; accessors
+  (define (constant-exp? exp)
+    (tagged-list? exp 'const))
+  (define (constant-exp-value exp)
+    (cadr exp))
 
-(define (register-exp? exp)
-  (tagged-list? exp 'reg))
-(define (register-exp-reg exp)
-  (cadr exp))
-(define (constant-exp? exp)
-  (tagged-list? exp 'const))
-(define (constant-exp-value exp)
-  (cadr exp))
-(define (label-exp? exp)
-  (tagget-list? exp 'label))
-(define (label-exp-label exp)
-  (cadr exp))
+  (define (label-exp? exp)
+    (tagget-list? exp 'label))
+  (define (label-exp-label exp)
+    (cadr exp))
+
+  (define (register-exp? exp)
+    (tagged-list? exp 'reg))
+  (define (register-exp-reg exp)
+    (cadr exp))
+
+  (cond ((constant-exp? exp)
+         ;; (const <value>)
+         (let ((c (constant-exp-value exp)))
+           ;; we write in this way so that "c"
+           ;; is known before the thunk get executed.
+           (lambda () c)))
+        ((label-exp? exp)
+         ;; (label <label>)
+         ;; TODO
+         'todo)
+        ((register-exp? exp)
+         ;; (reg <reg>)
+         (let ((r (machine-find-register
+                   m (register-exp-reg exp))))
+           (lambda () (register-get r))))
+        (else
+         (error "unexpected expression:" exp))))
+
+(define (advance-pc m)
+  (machine-reg-set!
+   m 'pc
+   (cdr (machine-reg-get m 'pc))))
 
 ;; handler type:
-;; (<handler> inst labels machine pc flag stack ops)
-(define (assign-handler inst labels machine pc flag stack ops)
+;; (<handler> insn machine)
+(define (assign-handler insn m)
   ;; accessors for "assign"
   ;; (assign <reg> @<value-exp> ..)
   (define assign-reg-name cadr)
   (define assign-value-exp cddr)
 
   (let ((target-reg
-         (machine-find-register machine (assign-reg-name inst)))
+         (machine-find-register m (assign-reg-name insn)))
         (value-exp
-         (assign-value-exp inst)))
+         (assign-value-exp insn)))
     (let ((value-proc
            ;; yields a value when run as a procedure
            (if (operation-exp? value-exp)
-               (make-operation-exp
-                value-exp machine labels operations)
+               'todo
                (make-primitive-exp
-                (car value-exp) machine labels))))
+                (car value-exp) m labels))))
       (lambda ()
         (register-set! target-reg (value-proc))
-        ;; advance pc
-        (machine-reg-set!
-         machine 'pc
-         (cdr (machine-reg-get machine 'pc))
-         )))))
+        (advance-pc m)))))
 (set-handler 'assign assign-handler)
 
+#|
 (define (test-handler inst labels machine pc flag stack ops)
   ;; instruction destruction
   (let ((condition (test-condition inst)))
@@ -130,3 +135,4 @@
 
 (define (perform-handler inst labels machine pc flag stack ops)
   'todo)
+|#
