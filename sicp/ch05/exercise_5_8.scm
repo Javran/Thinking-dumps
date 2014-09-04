ebc26bffc4ce24aa0b620356f7becbefa2914e97
Javran Cheng <Javran.c@gmail.com>
2014-09-03 22:03:51 -0400

    ex 5.8 - some TODOs

--- sicp/ch05/exercise_5_8.scm
+++ sicp/ch05/exercise_5_8.scm
@@ -5,17 +5,26 @@
 ;; `a` will be 3 when reaching the label `there`
 
 ;; guess we would have some problem here
-;; using the most naive approach, we will be warned when
+;; using the most naive approach below, we will be warned when
 ;; the second duplicated label counting from the last instruction
 ;; to the first one is found.
 ;; what we usually expect is receiving a warning when the second duplicated
 ;; label from first instruction to the last one is found.
 ;; TODO: verification when working
+;; TODO: having an extra pass to solve this problem:
+;;   extracting labels only and test if every label is unique
+;;   therefore we can scan from the beginning to the end
+;;   I don't actually care about the performance
+;;   as we only need to assemble it once, the "simulation" performance
+;;   should be more important than this.
+;; TODO: apply this duplicate-label protection
 (define (extract-labels text receive)
   (if (null? text)
       (receive '() '())
       (extract-labels
        (cdr text)
+       ;; note this procedure gets executed
+       ;; *after* this inner recursive call to "extract-labels" is done
        (lambda (insts labels)
          (let ((next-inst (car text)))
            (if (symbol? next-inst)
