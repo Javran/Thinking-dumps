bc6cb9cd0a2764afcc0a7c647a43f89f96b6ac99
Javran Cheng <Javran.c@gmail.com>
2014-09-09 23:31:22 -0400

    ex 5.11.b - legacy - good and working

--- sicp/ch05/exercise_5_11_b_legacy.scm
+++ sicp/ch05/exercise_5_11_b_legacy.scm
@@ -8,15 +8,55 @@
 ;; and only "save" and "restore" can do that.
 
 (define (make-save inst machine stack pc)
-  (let ((reg (get-register machine
-                           (stack-inst-reg-name inst))))
+  (let* ((reg-name (stack-inst-reg-name inst))
+         (reg (get-register machine reg-name)))
     (lambda ()
-      (push stack (get-contents reg))
+      (push stack (cons reg-name (get-contents reg)))
       (advance-pc pc))))
 
 (define (make-restore inst machine stack pc)
-  (let ((reg (get-register machine
-                           (stack-inst-reg-name inst))))
+  (let* ((reg-name (stack-inst-reg-name inst))
+        (reg (get-register machine reg-name)))
     (lambda ()
-      (set-contents! reg (pop stack))
-      (advance-pc pc))))
+      (let* ((top-element (pop stack))
+             (top-reg-name (car top-element))
+             (top-reg-value (cdr top-element)))
+        (if (eq? top-reg-name reg-name)
+            (begin
+              (set-contents! reg top-reg-value)
+              (advance-pc pc))
+            (error (format
+                    #f
+                    "register name '~A' expected, but '~A' found"
+                    top-reg-name reg-name)))))))
+
+(let ((m (make-and-execute
+          '(controller
+            (assign a (const 1))
+            (assign b (const 2))
+            (save a)
+            (save b)
+            (assign a (const 10))
+            (assign b (const 20))
+            (restore b)
+            (restore a))
+          '())))
+  (out (get-register-contents m 'a))
+  (out (get-register-contents m 'b)))
+
+(assert-error
+ (lambda ()
+   (let ((m (make-and-execute
+          '(controller
+            (assign a (const 1))
+            (assign b (const 2))
+            (save a)
+            (save b)
+            (assign a (const 10))
+            (assign b (const 20))
+            (restore a) ;; this part will raise an error
+            (restore b))
+          '())))
+  (out (get-register-contents m 'a))
+  (out (get-register-contents m 'b))))
+ "restoring from a different register causes an error")
