9e355058ac89f8e2bdc13bac34bfb1505d8c5e95
Javran Cheng <Javran.c@gmail.com>
2014-09-13 17:43:38 -0400

    ex 5.12 - insn without dup, sorted

--- sicp/ch05/exercise_5_12.scm
+++ sicp/ch05/exercise_5_12.scm
@@ -7,4 +7,25 @@
 ;; "simu.scm" but later dependencies will be minimized
 ;; so that something can be shared between different simulators
 
+(load "./figure_5_12.scm")
+
+;; just comparing their string output will do
+(define (to-str-compare a b)
+  (string<=? (format #f "~A" a)
+             (format #f "~A" b)))
+
+;; well, we agree on one thing: labels are symbols.
+(define (remove-labels insns)
+  (filter (compose not symbol?) insns))
+
+;; a list of all instructions, with duplicates removed,
+;; sorted by instruction type.
+(define (sorted-uniq-instructions insns)
+  (sort
+   (remove-duplicates
+    (remove-labels insns))
+   to-str-compare))
+
+(for-each out (sorted-uniq-instructions fib-machine-controller))
+
 (end-script)
