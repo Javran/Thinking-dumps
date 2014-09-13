ef4276ffcb10c74993c3f964a24ebe4d340e9933
Javran Cheng <Javran.c@gmail.com>
2014-09-12 23:04:43 -0400

    ex 5.11.c - register push / pop

--- sicp/ch05/exercise_5_11_c_simu.scm
+++ sicp/ch05/exercise_5_11_c_simu.scm
@@ -15,10 +15,35 @@
   (stack-top (vector-ref reg 0)))
 
 (define (register-set! reg val)
-  (let* ((st (vector-ref reg 0)))
+  (let ((st (vector-ref reg 0)))
     (stack-pop! st)
     (stack-push! st val)
-    (vector-set! reg 0 st)))
+    ;; "st" is itself stateful, don't have to
+    ;; update the reference here
+    ;; aka the following line is not necessary
+    ;; (vector-set! reg 0 st)
+    ))
+
+(define (register-push! reg)
+  (let* ((st (vector-ref reg 0))
+         (t (stack-top st)))
+    ;; to push is to duplicate the top element
+    (stack-push! st t)))
+
+(define (register-pop! reg)
+  (let* ((st (vector-ref reg 0)))
+    (stack-pop! st)
+    ;; even we have one element, but
+    ;; we won't be able to access the register
+    ;; after the last element is popped from the stack
+    ;; in order to have the same behavior
+    ;; as what we have done for legacy simulator
+    ;; I make the error happens earlier
+    (if (stack-empty? st)
+        (error "stack underflow")
+        'ok)))
+
+
 
 (let ((m (build-and-execute
           '(controller
