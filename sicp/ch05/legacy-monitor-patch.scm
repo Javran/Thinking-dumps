8c6904296db761047b601accdd9d4496ece51db7
Javran Cheng <Javran.c@gmail.com>
2014-09-19 20:47:36 -0400

    legacy - a performance monitor patch

---
+++ sicp/ch05/legacy-monitor-patch.scm
@@ -0,0 +1,100 @@
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 ;; adding a new primitive
+                 (list 'print-statistics
+                       (lambda () (stack 'print-statistics)))))
+          (register-table
+           (list (list 'pc pc)
+                 (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register:"
+                   name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:"
+                     name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq)
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register)
+               allocate-register)
+              ((eq? message 'get-register)
+               lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops)
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else
+               (error "Unknown request: MACHINE"
+                      message))))
+      dispatch)))
+
+(define (make-stack)
+  (let ((s '())
+        ;; more statistics
+        (number-pushes 0)
+        (max-depth 0)
+        (current-depth 0))
+    ;; do some tracking while manipulating the stack
+    (define (push x)
+      (set! s (cons x s))
+      (set! number-pushes (add1 number-pushes))
+      (set! current-depth (add1 current-depth))
+      (set! max-depth
+            (max current-depth max-depth)))
+    (define (pop)
+      (if (null? s)
+          (error "empty stack: POP")
+          (let ((top (car s)))
+            (set! s (cdr s))
+            (set! current-depth (- current-depth 1))
+            top)))
+    (define (initialize)
+      (set! s '())
+      (set! number-pushes 0)
+      (set! max-depth 0)
+      (set! current-depth 0)
+      'done)
+    (define (print-statistics)
+      (format
+       #t
+       "~%~
+        total-pushes  = ~A~%~
+        maximum-depth = ~A~%"
+       number-pushes
+       max-depth))
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+            ((eq? message 'pop) (pop))
+            ((eq? message 'initialize) (initialize))
+            ((eq? message 'print-statistics)
+             (print-statistics))
+            (else
+             (error "Unknown request: STACK"
+                    message))))
+    dispatch))
