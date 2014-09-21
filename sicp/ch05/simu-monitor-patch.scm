696ae7265384cd7fa1351d4ed1a8fcf8546452ac
Javran Cheng <Javran.c@gmail.com>
2014-09-20 22:24:54 -0400

    simu - track on push

--- sicp/ch05/simu-monitor-patch.scm
+++ sicp/ch05/simu-monitor-patch.scm
@@ -2,6 +2,9 @@
 (define (empty-stack)
   (vector
    '()
+   ;; store some metadata
+   ;; to help keep track of
+   ;; some statistics
    '((number-pushes 0)
      (max-depth     0)
      (current-depth 0))))
@@ -13,11 +16,35 @@
     `((,key ,(proc old-val))
       ,@(del-assoc key alist))))
 
+(define (stack-modify-meta! st proc)
+  (vector-modify! st 1 proc))
+
+(define (stack-meta-get st key)
+  (cadr (assoc key (vector-ref st 1))))
+
 (define (stack-push! st e)
   (vector-modify!
    st 0
    (lambda (stack)
-     (cons e stack))))
+     (cons e stack)))
+  ;; do statistics
+  (stack-modify-meta!
+   st
+   (lambda (meta)
+     (modify-assoc 'number-pushes add1 meta)))
+  (stack-modify-meta!
+   st
+   (lambda (meta)
+     (modify-assoc 'current-depth add1 meta)))
+  (stack-modify-meta!
+   st
+   (lambda (meta)
+     (modify-assoc
+      'max-depth
+      (lambda (old-max-depth)
+        (let ((cur-depth (stack-meta-get st 'current-depth)))
+          (max cur-depth old-max-depth)))))))
+
 (define (stack-pop! st)
   (vector-modify! st 0 cdr))
 (define (stack-top st)
