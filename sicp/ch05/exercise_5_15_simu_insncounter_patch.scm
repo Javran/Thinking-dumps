0856047e29dd21e3e211eddcb04eb9c40e455c07
Javran Cheng <Javran.c@gmail.com>
2014-10-01 22:27:41 -0400

    ex 5.15 - patch first impl

---
+++ sicp/ch05/exercise_5_15_simu_insncounter_patch.scm
@@ -0,0 +1,39 @@
+(define (machine-fresh-start! m)
+  ;; right before bringing up the machine,
+  ;; make "instruction-counter" field available
+  (machine-extra-set! m 'instruction-counter 0)
+  (machine-reset-pc! m)
+  (machine-execute! m))
+
+(define (machine-execute! m)
+  (let ((insns (machine-reg-get m 'pc)))
+    (if (null? insns)
+        'done
+        (begin
+          ((cdr (car insns)))
+          ;; after an instruction is execute,
+          ;; we bump the counter
+          (machine-extra-modify!
+           m 'instruction-counter add1)
+          (machine-execute! m)))))
+
+(define (machine-instruction-counter m)
+  (machine-extra-get m 'instruction-counter))
+
+(define (machine-reset-instruction-counter! m)
+  (machine-extra-set! m 'instruction-counter 0))
+
+(define default-ops-builder
+  ;; should be safe if the expression
+  ;; in "define" form gets evaluates immediately
+  (let ((old-builder default-ops-builder))
+    (lambda (m)
+      `((print-insn-counter
+         (lambda ()
+           (format
+            #t "# instruction executed: ~A~%"
+            (machine-instruction-counter m))))
+        (reset-insn-counter
+         (lambda ()
+           (machine-reset-instruction-counter! m)))
+        ,@(old-builder m)))))
