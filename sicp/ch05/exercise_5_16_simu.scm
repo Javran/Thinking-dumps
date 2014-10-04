0ee6f77c209f0e48f4139c1b3c5daa37f7b5f6cd
Javran Cheng <Javran.c@gmail.com>
2014-10-03 23:51:14 -0400

    ex 5.16 - some TODOs

--- sicp/ch05/exercise_5_16_simu.scm
+++ sicp/ch05/exercise_5_16_simu.scm
@@ -6,6 +6,12 @@
 
 (load "./figure_5_12.scm")
 
+;; TODO: make trace controllable through
+;; instructions
+
+;; TODO: write a small controller text
+;; to test turn on / off functionality
+
 (let ((m (build-with fib-machine-controller
                      '((n 5))
                      default-ops-builder)))
