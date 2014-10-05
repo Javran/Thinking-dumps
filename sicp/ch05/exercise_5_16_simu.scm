eeb9e85ba5948b39e23b204a2443eb8c0ebe6b68
Javran Cheng <Javran.c@gmail.com>
2014-10-04 23:32:46 -0400

    ex 5.16 - simu - done

--- sicp/ch05/exercise_5_16_simu.scm
+++ sicp/ch05/exercise_5_16_simu.scm
@@ -6,9 +6,6 @@
 
 (load "./figure_5_12.scm")
 
-;; TODO: make trace controllable through
-;; instructions
-
 (define test-tracing-controller
   '(controller
     ;; input: register n expects a number
@@ -18,28 +15,22 @@
     (test (op odd?) (reg n))
     (branch (label p-trace-on))
     (perform (op trace-off))
+    (perform (op print) (reg n))
     (goto (label p-loop-next))
     p-trace-on
     (perform (op trace-on))
+    (perform (op print) (reg n))
     p-loop-next
     (assign n (op -) (reg n) (const 1))
     (goto (label p-begin))
     p-done))
 
-(let ((m (build-with fib-machine-controller
-                     '((n 5))
-                     default-ops-builder)))
+(let ((m (build-with
+          test-tracing-controller
+          '((n 5))
+          default-ops-builder)))
   (machine-trace-on! m)
   (machine-fresh-start! m))
-
-(out "===")
-
-(let ((m (build-with test-tracing-controller
-                     '((n 5))
-                     default-ops-builder)))
-  (machine-trace-on! m)
-  (machine-fresh-start! m))
-
 
 (end-script)
 
