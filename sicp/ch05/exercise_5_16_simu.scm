7a41f4009622149ecf81b16b962fb28dee0083ef
Javran Cheng <Javran.c@gmail.com>
2014-10-04 23:23:20 -0400

    ex 5.16 - small program for testing tracing

--- sicp/ch05/exercise_5_16_simu.scm
+++ sicp/ch05/exercise_5_16_simu.scm
@@ -9,14 +9,37 @@
 ;; TODO: make trace controllable through
 ;; instructions
 
-;; TODO: write a small controller text
-;; to test turn on / off functionality
+(define test-tracing-controller
+  '(controller
+    ;; input: register n expects a number
+    p-begin
+    (test (op zero?) (reg n))
+    (branch (label p-done))
+    (test (op odd?) (reg n))
+    (branch (label p-trace-on))
+    ;; (perform (op trace-off))
+    (goto (label p-loop-next))
+    p-trace-on
+    ;; (perform (op trace-on))
+    p-loop-next
+    (assign n (op -) (reg n) (const 1))
+    (goto (label p-begin))
+    p-done))
 
 (let ((m (build-with fib-machine-controller
                      '((n 5))
                      default-ops-builder)))
   (machine-trace-on! m)
   (machine-fresh-start! m))
+
+(out "===")
+
+(let ((m (build-with test-tracing-controller
+                     '((n 5))
+                     default-ops-builder)))
+  (machine-trace-on! m)
+  (machine-fresh-start! m))
+
 
 (end-script)
 
