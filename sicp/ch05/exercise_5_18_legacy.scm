573abaed6047251cc36be2fb5df87e9663ad9a9b
Javran Cheng <Javran.c@gmail.com>
2014-10-11 22:21:58 -0400

    ex 5.18 - working example

--- sicp/ch05/exercise_5_18_legacy.scm
+++ sicp/ch05/exercise_5_18_legacy.scm
@@ -4,14 +4,26 @@
 (load "./legacy-easy.scm")
 (load "./exercise_5_18_legacy_regtrace_patch.scm")
 
-(let ((r (make-register "test-reg")))
-  (register-trace-on! r)
-  (set-contents! r 10)
-  (set-contents! r 20)
-  (register-trace-off! r)
-  (set-contents! r 30)
-  (register-trace-on! r)
-  (set-contents! r 40))
+(let ((m (make-and-execute
+          '(controller
+            (assign n (const 10))
+            (assign m (const 20))
+            ;; turn on flag on "n" only
+            (perform (op trace-reg-on) (const n))
+            (assign n (const a))
+            (assign m (const b))
+            ;; turn on flag on "m", turn off flag on "n"
+            (perform (op trace-reg-on) (const m))
+            (perform (op trace-reg-off) (const n))
+            (assign n (const aaa))
+            (assign m (const bbb))
+            ;; turn on flags on "m" an "n"
+            (perform (op trace-reg-on) (const n))
+            (perform (op trace-reg-on) (const m))
+            (assign n (const 123))
+            (assign m (const 456)))
+          '())))
+  (out "done"))
 
 (end-script)
 
