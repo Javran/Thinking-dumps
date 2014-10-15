2291c4907eb9dfbb26a57c43b86b6fd4a19b75ee
Javran Cheng <Javran.c@gmail.com>
2014-10-14 22:15:51 -0400

    ex 5.18 - re-impl

--- sicp/ch05/exercise_5_18_simu_regtrace_patch.scm
+++ sicp/ch05/exercise_5_18_simu_regtrace_patch.scm
@@ -1,28 +1,37 @@
-(define (machine-register-set-trace! m reg flag)
-  ;; the trace flag is set by maintaining a set of
-  ;; tracing flags in the machine extra-data field
-  (define (register-trace-on tracing-regs)
-    ;; to set register flag is to add the register name into
-    ;; that set, this "set" is allowed to contain duplicates
-    ;; for simplicity.
-    (cons reg tracing-regs))
-  (define (register-trace-off tracing-regs)
-    ;; to unset register flag is to remove the register name from
-    ;; this set, "delete" is guaranteed to work even if the element
-    ;; to be removed has duplicates.
-    (delete reg tracing-regs))
-  (machine-extra-modify!
-   m
-   'tracing-regs
-   (if flag
-       register-trace-on
-       register-trace-off)
-   '()))
+(define (new-register)
+  ;; adding a new field for register-setter callback
+  (vector '*unassigned* #f))
 
-(define (machine-register-tracing? m reg)
-  (member
-   reg
-   (machine-extra-get m 'tracing-regs '())))
+;; callback procedure:
+;; (callback <register-object> <old-value> <new-value>)
+
+(define (register-setter-callback reg)
+  (vector-ref reg 1))
+(define (register-set-setter-callback! reg callback)
+  (vector-set! reg 1 callback))
+
+(define (register-set! reg val)
+  (let ((old-val (register-get reg))
+        (callback (register-setter-callback reg)))
+    (if callback
+        (callback reg old-val val)
+        'skipped)
+    (vector-set! reg 0 val)))
+
+(define (machine-register-set-trace! m name flag)
+  (let ((reg-obj (machine-find-register m name))
+        (callback (lambda (reg-obj old-val new-val)
+                    (format #t "reg: ~A~%~
+                                old-val: ~A~%~
+                                new-val: ~A~%"
+                            name
+                            old-val
+                            new-val))))
+    (register-set-setter-callback!
+     reg-obj
+     (if flag
+         callback
+         #f))))
 
 (define default-ops-builder
   (let ((old-builder default-ops-builder))
@@ -35,15 +44,4 @@
             (machine-register-set-trace! m name #f)))
         ,@(old-builder m)))))
 
-(define (machine-reg-set! m reg val)
-  (let ((reg-obj (machine-find-register m reg)))
-    (if (machine-register-tracing? m reg)
-        (let ((old-value (register-get reg-obj)))
-          (format #t  "reg: ~A~%~
-                       old-val: ~A~%~
-                       new-val: ~A~%"
-                  reg
-                  old-value
-                  val))
-        'skipped)
-    (register-set! reg-obj val)))
+
