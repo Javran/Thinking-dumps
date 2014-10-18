345eefcbebd768ed1b799e891f12089f26f6bc7b
Javran Cheng <Javran.c@gmail.com>
2014-10-17 22:32:59 -0400

    ex 5.19 - introducing a "resuming flag"

--- sicp/ch05/exercise_5_19_simu.scm
+++ sicp/ch05/exercise_5_19_simu.scm
@@ -6,40 +6,6 @@
 
 (load "./figure_5_12.scm")
 
-(let ((m (empty-machine)))
-  (machine-set-breakpoint! m 'lbla 10)
-  (machine-set-breakpoint! m 'lbla 2)
-  (machine-set-breakpoint! m 'lblb 4)
-  (machine-set-breakpoint! m 'lbla 1)
-  (machine-set-breakpoint! m 'lbla 1)
-
-  (out
-   (machine-breakpoint? m 'lbla 10)     ; #t
-   (machine-breakpoint? m 'lblb 2)      ; #f
-   (machine-breakpoint? m 'lbla 2)      ; #t
-   )
-
-  (machine-cancel-breakpoint! m 'lbla 2)
-  (machine-cancel-breakpoint! m 'lbla 3)
-  (machine-cancel-breakpoint! m 'lblb 4)
-
-  (out
-   (machine-breakpoint? m 'lbla 10)     ; #t
-   (machine-breakpoint? m 'lblb 2)      ; #f
-   (machine-breakpoint? m 'lbla 2)      ; #f
-   )
-
-  (machine-cancel-all-breakpoints! m)
-
-  (out
-   (machine-breakpoint? m 'lbla 10)     ; #f
-   (machine-breakpoint? m 'lblb 2)      ; #f
-   (machine-breakpoint? m 'lbla 2)      ; #f
-   )
-
-  'done)
-
-
 (end-script)
 
 ;; Local variables:
--- sicp/ch05/exercise_5_19_simu_breakpoint_patch.scm
+++ sicp/ch05/exercise_5_19_simu_breakpoint_patch.scm
@@ -43,6 +43,8 @@
   (let ((result (assoc lbl tbl)))
     (and result (member offset (cadr result)))))
 
+;; corresponding to "set-breakpoint" feature
+;; requested by the exercise
 (define (machine-set-breakpoint! m lbl n)
   (machine-extra-modify!
    m
@@ -51,6 +53,8 @@
      (breakpoint-table-add lbl n tbl))
    '()))
 
+;; corresponding to "cancel-breakpoint" feature
+;; requested by the exercise
 (define (machine-cancel-breakpoint! m lbl n)
   (machine-extra-modify!
    m
@@ -58,6 +62,23 @@
    (lambda (tbl)
      (breakpoint-table-del lbl n tbl))
    '()))
+
+;; when machine gets resumed, the same lines of expressions
+;; gets executed, and we need to make a difference between
+;; reaching a breakpoint and resuming from a breakpoint
+;; and this difference is made by this resuming flag
+;; if the flag is set, the breakpoint will be bypassed
+;; and the flag will be unset immediately
+;; or otherwise the breakpoint takes effect
+(define (machine-resuming-flag? m)
+  (machine-extra-get m 'resuming-flag #f))
+(define (machine-set-resumming-flag! m flag)
+  (machine-extra-set m 'resuming-flag flag))
+
+;; corresponding to "cancel-all-breakpoints" feature
+;; requested by the exercise
+(define (machine-cancel-all-breakpoints! m)
+  (machine-extra-set! m 'breakpoint-table '()))
 
 ;; check if there is a breakpoint in this place?
 ;; NOTE: we could have cached the breakpoint table
@@ -68,9 +89,6 @@
   (breakpoint-table-check?
    lbl n
    (machine-extra-get m 'breakpoint-table '())))
-
-(define (machine-cancel-all-breakpoints! m)
-  (machine-extra-set! m 'breakpoint-table '()))
 
 ;; based on ex 5.17 patch
 (define (machine-execute! m)
