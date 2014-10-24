d7c0a1003a099b2e6fe7b2290538f1deed3d92bd
Javran Cheng <Javran.c@gmail.com>
2014-10-23 23:08:09 -0400

    simu - list primitive patch

---
+++ sicp/ch05/simu_listprim_patch.scm
@@ -0,0 +1,13 @@
+;; we are missing details of the implementation
+;; so we just pretend we have them implmented correctly
+;; since "(const <data>)" works for any data,
+;; we can use (const ()) to make an empty list
+(define default-ops-builder
+  (let ((old-builder default-ops-builder))
+    (lambda (m)
+      `((car ,car)
+        (cdr ,cdr)
+        (cons ,cons)
+        (null? ,null?)
+        (pair? ,pair?)
+        ,@(old-builder m)))))
