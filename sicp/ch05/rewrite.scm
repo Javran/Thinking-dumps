cb742a5bc6aa0e5a6201101c4ff36a214d9b9d03
Javran Cheng <Javran.c@gmail.com>
2014-10-29 19:40:44 -0400

    rewrite - comments

--- sicp/ch05/rewrite.scm
+++ sicp/ch05/rewrite.scm
@@ -1,3 +1,5 @@
+;; a template variable is a symbol
+;; that begins with "$"
 (define (template-variable? sym)
   (and (symbol? sym)
        (eq? (string-ref
@@ -5,14 +7,24 @@
              0)
             #\$)))
 
+;; pattern match against data
+;; with know variable-value bindings in env
+;; values found in "env" shouldn't contain any
+;; template variable
+;; returns the expanded environment (variable-value bindings)
+;; if the pattern match fails, #f is returned
 (define (pattern-match pattern data env)
   (cond ((template-variable? pattern)
+         ;; check if the variable has a known binding
          (let ((result (assoc pattern env)))
            (if result
+               ;; see if varaible binding and data agree
                (pattern-match (cadr result) data env)
+               ;; if a binding isn't present, bind this variable
                (cons (list pattern data) env))))
         ((and (pair? pattern)
               (pair? data))
+         ;; do pattern matching recursively
          (let ((env1 (pattern-match
                       (car pattern)
                       (car data)
@@ -26,6 +38,10 @@
          env)
         (else #f)))
 
+;; apply a template using bindings in env
+;; all template variables should be available
+;; in `env`, and values in `env` shouldn't contain
+;; template variables
 (define (template-apply temp env)
   (cond ((template-variable? temp)
          (let ((result (assoc temp env)))
@@ -37,11 +53,3 @@
          (cons (template-apply (car temp) env)
                (template-apply (cdr temp) env)))
         (else temp)))
-
-(display
- (template-apply
-  '($a $c ($a $b))
-  '(($a 100)
-    ($b 200))))
-
-(newline)
