cb4a05022378714635f631d11a3ddfa90e3c60ff
Javran Cheng <Javran.c@gmail.com>
2014-11-02 23:02:52 -0500

    simu - modify rewrite rules

--- sicp/ch05/simu_listprim2_patch.scm
+++ sicp/ch05/simu_listprim2_patch.scm
@@ -1,5 +1,7 @@
 ;; the real implementation
 ;; of list primitives
+;; I guess this will eventually replace the ad hoc
+;; implementation of "listprim"
 
 (define list-primitives-rules
   `( (;; rewrite `car`
@@ -24,23 +26,25 @@
      ;; to do this change,
      ;; but as we have more exercises available,
      ;; the intention of doing so will be more clear.
+
      (;; rewrite `set-car!`
-      (perform (op set-car!) (reg $reg1) (reg $reg2))
-      ;;
-      ((perform (op vector-set!) (reg the-cars) (reg $reg1) (reg $reg2))
+      (perform (op set-car!) (reg $reg1) $value)
+      ;; instead of capturing `(reg $reg2)`, we capture the whole value
+      ;; this would allow settings the `car` part of a pair to some constants
+      ((perform (op vector-set!) (reg the-cars) (reg $reg1) $value)
        ))
      (;; rewrite `set-cdr!`
-      (perform (op set-cdr!) (reg $reg1) (reg $reg2))
+      (perform (op set-cdr!) (reg $reg1) $value)
       ;;
-      ((perform (op vector-set!) (reg the-cdrs) (reg $reg1) (reg $reg2))
+      ((perform (op vector-set!) (reg the-cdrs) (reg $reg1) $value)
        ))
      (;; rewrite `cons`
-      (assign $reg1 (op cons) (reg $reg2) (reg $reg3))
+      (assign $reg1 (op cons) $val1 $val2)
       ;;
-      ((perform (op vector-set!) (reg the-cars) (reg free) (reg $reg2))
-       (perform (op vector-set!) (reg the-cdrs) (reg free) (reg $reg3))
+      ((perform (op vector-set!) (reg the-cars) (reg free) $val1)
+       (perform (op vector-set!) (reg the-cdrs) (reg free) $val2)
        (assign $reg1 (reg free))
-       (assign free (op +) (reg free) (const 1))
+       (assign free (op ptr-inc) (reg free))
        ))
      ))
 
