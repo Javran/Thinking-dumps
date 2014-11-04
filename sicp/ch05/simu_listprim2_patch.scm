243e815589d8db8b2dc648614a15d3a48f2ca27d
Javran Cheng <Javran.c@gmail.com>
2014-11-03 22:25:35 -0500

    simu - rule rewrite explained

--- sicp/ch05/simu_listprim2_patch.scm
+++ sicp/ch05/simu_listprim2_patch.scm
@@ -3,6 +3,13 @@
 ;; I guess this will eventually replace the ad hoc
 ;; implementation of "listprim"
 
+;; usually we rewrite an instruction
+;; into a sequence of instructions that does
+;; some lower level operations
+;; so here a single rule consists of a pattern matching
+;; on an instruction and rewriting rule to rewrite it into
+;; *a list* of instructions.
+
 (define list-primitives-rules
   `( (;; rewrite `car`
       (assign $reg1 (op car) (reg $reg2))
