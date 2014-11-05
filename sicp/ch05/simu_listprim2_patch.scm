fa22286f10eba00ef17c8e67198a7ebf0d87e639
Javran Cheng <Javran.c@gmail.com>
2014-11-04 21:53:15 -0500

    simu - simplify code, more comments

--- sicp/ch05/simu_listprim2_patch.scm
+++ sicp/ch05/simu_listprim2_patch.scm
@@ -60,7 +60,32 @@
        ))
      ))
 
-;; TODO: we need a function here to expand the list of instructions
+;; this function is for internal use only,
+;; it returns a pair, whose `car` should be considered a boolean value
+;; indicating if there's any rule application happens in this round
+;; and `cdr` part being the resulting instruction list
+(define (rewrite-instructions-intern rules insns)
+  ;; 3 things we need to keep
+  ;; * a new instruction list
+  ;; * rest of the old instruction list to be processed
+  ;; * a flag indicating if any rule application has happened
+  (let loop ((new-insns '())
+             (curr-insns insns)
+             (at-least-once #f))
+    (if (null? curr-insns)
+        (cons at-least-once new-insns)
+        (let ((result (try-rewrite-once rules (car curr-insns))))
+          (loop (append new-insns
+                        (or result (list (car curr-insns))))
+                (cdr curr-insns)
+                (or at-least-once result))))))
+
+;; this rewrite function applies only the first applicable rule
+;; and do the application only once
+(define (rewrite-instructions rules insns)
+  (cdr (rewrite-instructions-intern rules insns)))
+
+;; expand the list of instructions
 ;; as long as there is at least one applicable rule
 ;; and also we need to make sure that we don't write rules that expands
 ;; infinitely.
@@ -69,29 +94,7 @@
 ;; rewrite system is that we are trying to avoid doing these tasks manually.
 ;; it will be great if we can just write down the most intuitive code
 ;; and leave all the boring tasks to computer.
-
-(define (rewrite-instructions rules insns)
-  (let loop ((new-insns '())
-             (curr-insns insns))
-    (if (null? curr-insns)
-        new-insns
-        (let ((result (try-rewrite-once rules (car curr-insns))))
-          (loop (append new-insns
-                        (or result (list (car curr-insns))))
-                (cdr curr-insns))))))
-
 (define (rewrite-instructions* rules insns)
-  (define (rewrite-instructions-intern rules insns)
-    (let loop ((new-insns '())
-               (curr-insns insns)
-               (at-least-once #f))
-      (if (null? curr-insns)
-          (cons at-least-once new-insns)
-          (let ((result (try-rewrite-once rules (car curr-insns))))
-            (loop (append new-insns
-                          (or result (list (car curr-insns))))
-                  (cdr curr-insns)
-                  (or at-least-once result))))))
   (let ((result (rewrite-instructions-intern rules insns)))
     (if (car result)
         (rewrite-instructions* rules (cdr result))
