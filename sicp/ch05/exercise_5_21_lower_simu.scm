a8d4afa555696d274610fa9b8ead0134ebe6ab2b
Javran Cheng <Javran.c@gmail.com>
2014-11-08 23:52:54 -0500

    ex 5.21 - using lower abstraction

--- sicp/ch05/exercise_5_21_lower_simu.scm
+++ sicp/ch05/exercise_5_21_lower_simu.scm
@@ -5,25 +5,42 @@
 (load "./simu_listprim2_patch.scm")
 
 (load "./exercise_5_21_controllers.scm")
+(load "./exercise_5_21_common.scm")
 
-(define test-controller
-  `(controller
-    ,@(tree->instruction-list '(1 2 (4 5) (a b . c) d e))
-    (assign tree (reg result))
-    ,@(cdr count-leaves-r-controller)))
+(define (count-leaves-r-machine tree)
+  ;; evaluate the function by running the machine
+  (let ((m (build-and-execute
+            `(controller
+              ,@(tree->instruction-list tree)
+              (assign tree (reg result))
+              ,@(cdr count-leaves-r-controller))
+            '())))
+    (machine-reg-get m 'result)))
 
-(let ((m (build-and-execute
-          test-controller
-          '())))
-  (out (machine-reg-get m 'result)))
+(define (count-leaves-i-machine tree)
+  ;; evaluate the function by running the machine
+  (let ((m (build-and-execute
+            `(controller
+              ,@(tree->instruction-list tree)
+              (assign tree (reg result))
+              ,@(cdr count-leaves-i-controller))
+            '())))
+    (machine-reg-get m 'result)))
 
-;; TODO: maybe we can remove some handlers like (save ???) and (restore ???)
-;; to show that we are indeed replacing them with lower level stuff.
+;; two different approaches should have the same result
+(test-machine
+ count-leaves-r
+ count-leaves-i)
 
-;; TODO: our target is to keep some compatibility between list-prim and list-prim2
-;; maybe we'll eventually rename list-prim into list-prim-fast so that the program
-;; works on scheme list primitives, and rename list-prim2 to list-prim
-;; so we have a slower implementation but relies less on scheme primitives
+;; scheme impl vs. machine (recursive version)
+(test-machine
+ count-leaves-r
+ count-leaves-r-machine)
+
+;; scheme impl vs. machine (iterative version)
+(test-machine
+ count-leaves-i
+ count-leaves-i-machine)
 
 (end-script)
 
