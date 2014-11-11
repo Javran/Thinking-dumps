aff2ea48cfb97762dcca3dac785cffb0ff8c232a
Javran Cheng <Javran.c@gmail.com>
2014-11-10 22:03:45 -0500

    legacy - redo machine initialization

--- sicp/ch05/legacy_lower_patch.scm
+++ sicp/ch05/legacy_lower_patch.scm
@@ -55,3 +55,71 @@
     ;; because they are way more special
     ;; thanks to the original design
     '(pc flag))))
+
+(define memory-size 65536)
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))))
+          (register-table
+           (list (list 'pc pc)
+                 (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register:"
+                   name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:"
+                     name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+                ((instruction-execution-proc (car insts)))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               ;; initialize some reserved registers here
+               ;; when the machine starts
+
+               ;; TODO initialize free
+               (set-contents!
+                (lookup-register 'the-cars)
+                (make-vector memory-size))
+               (set-contents!
+                (lookup-register 'the-cdrs)
+                (make-vector memory-size))
+               (set-contents!
+                (lookup-register 'the-stack)
+                '())
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq)
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register)
+               allocate-register)
+              ((eq? message 'get-register)
+               lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops)
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else
+               (error "Unknown request: MACHINE"
+                      message))))
+      dispatch)))
