898e2c9c121d8a4b915935b6cb796bab27f63be7
Javran Cheng <Javran.c@gmail.com>
2014-11-20 23:03:45 -0500

    gc - rename registers and labels
    need to avoid name confliction

--- sicp/ch05/gc-code.scm
+++ sicp/ch05/gc-code.scm
@@ -15,109 +15,113 @@
 ;; when garbage collection is done, I guess this can be done
 ;; by generating unique symbols before we start to assemble the code
 
+
+;; except for "free", "root", "new-cars", "the-cars", "new-cdrs" and "the-cdrs",
+;; all registers are prefixed with a "gc-" to avoid name confliction with
+;; the program
 (define gc-code
   '(begin-garbage-collection
     ;; free: points to the first free memory address
     (assign free (op to-pointer) (const 0))
     ;; scan: used by gc-loop, points to the first shallow copy
-    (assign scan (op to-pointer) (const 0))
+    (assign gc-scan (op to-pointer) (const 0))
     ;; prepare to copy the first pair
-    (assign old (reg root))
-    (assign relocate-continue (label reassign-root))
-    (goto (label relocate-old-result-in-new))
+    (assign gc-old (reg root))
+    (assign gc-relocate-continue (label gc-reassign-root))
+    (goto (label gc-relocate-old-result-in-new))
 
     ;; after the first pair is shallow-copied,
     ;; we update root and get started
     reassign-root
-    (assign root (reg new))
+    (assign root (reg gc-new))
     (goto (label gc-loop))
 
     gc-loop
     ;; TODO: implement ptr-=
-    (test (op =) (reg scan) (reg free))
+    (test (op =) (reg gc-scan) (reg free))
     ;; scan == free means we have copied everything necessary
     ;; and it's time for swaping memories
     (branch (label gc-flip))
     ;; otherwise we still have some jobs to do.
 
     ;; relocate old pointers in `car` part
-    (assign old (op vector-ref) (reg new-cars) (reg scan))
-    (assign relocate-continue (label update-car))
-    (goto (label relocate-old-result-in-new))
+    (assign gc-old (op vector-ref) (reg new-cars) (reg gc-scan))
+    (assign gc-relocate-continue (label gc-update-car))
+    (goto (label gc-relocate-old-result-in-new))
 
-    update-car
+    gc-update-car
     (perform (op vector-set!)
              (reg new-cars)
-             (reg scan)
-             (reg new))
+             (reg gc-scan)
+             (reg gc-new))
     ;; relocate old pointers in `cdr` part
-    (assign old (op vector-ref) (reg new-cdrs) (reg scan))
-    (assign relocate-continue (label update-cdr))
-    (goto (label relocate-old-result-in-new))
+    (assign gc-old (op vector-ref) (reg new-cdrs) (reg gc-scan))
+    (assign gc-relocate-continue (label gc-update-cdr))
+    (goto (label gc-relocate-old-result-in-new))
 
-    update-cdr
+    gc-update-cdr
     (perform (op vector-set!)
              (reg new-cdrs)
-             (reg scan)
-             (reg new))
+             (reg gc-scan)
+             (reg gc-new))
     ;; scan next one
-    (assign scan (op ptr-inc) (reg scan))
+    (assign gc-scan (op ptr-inc) (reg gc-scan))
     (goto (label gc-loop))
 
     ;; this subroutine relocates the data pointed by `old` register
     ;; resulting in the corresponsing deep copy pointed by `new` register
-    relocate-old-result-in-new
-    (test (op pointer-to-pair?) (reg old))
-    (branch (label pair))
+    gc-relocate-old-result-in-new
+    (test (op pointer-to-pair?) (reg gc-old))
+    (branch (label gc-pair))
     ;; if "old" does not contain a pair,
     ;; there's no ref in data, just copy it to the new one
-    (assign new (reg old))
-    (goto (reg relocate-continue))
+    (assign gc-new (reg gc-old))
+    (goto (reg gc-relocate-continue))
 
-    pair
+    gc-pair
     ;; test if we have found a broken-heart flag
-    (assign oldcr (op vector-ref) (reg the-cars) (reg old))
+    (assign gc-oldcr (op vector-ref) (reg the-cars) (reg gc-old))
     ;; for an already-moved structure, car is a broken-heart flag
     ;; and cdr is the new location (of this pair)
-    (test (op broken-heart?) (reg oldcr))
-    (branch (label already-moved))
+    (test (op broken-heart?) (reg gc-oldcr))
+    (branch (label gc-already-moved))
     ;; if no broken-heart flag is found,
     ;; we need to relocate the old data.
-    (assign new (reg free)) ; new location for pair
+    (assign gc-new (reg free)) ; new location for pair
     ;; update free pointer
     (assign free (op ptr-inc) (reg free))
     ;; copy the whole pair
     (perform (op vector-set!)
              (reg new-cars)
-             (reg new)
-             (reg oldcr))
-    (assign oldcr (op vector-ref) (reg the-cdrs) (reg old))
+             (reg gc-new)
+             (reg gc-oldcr))
+    (assign gc-oldcr (op vector-ref) (reg the-cdrs) (reg gc-old))
     (perform (op vector-set!)
              (reg new-cdrs)
-             (reg new)
-             (reg oldcr))
+             (reg gc-new)
+             (reg gc-oldcr))
     ;; construct the broken heart
     (perform (op vector-set!)
              (reg the-cars)
-             (reg old)
+             (reg gc-old)
              (const broken-heart))
     (perform
-     (op vector-set!) (reg the-cdrs) (reg old) (reg new))
-    (goto (reg relocate-continue))
+     (op vector-set!) (reg the-cdrs) (reg gc-old) (reg gc-new))
+    (goto (reg gc-relocate-continue))
 
-    already-moved
-    (assign new (op vector-ref) (reg the-cdrs) (reg old))
-    (goto (reg relocate-continue))
+    gc-already-moved
+    (assign gc-new (op vector-ref) (reg the-cdrs) (reg gc-old))
+    (goto (reg gc-relocate-continue))
 
     ;; gc-flip swaps the working memories and the free memories
     gc-flip
     ;; the-cdrs <-> new-cdrs
-    (assign temp (reg the-cdrs))
+    (assign gc-temp (reg the-cdrs))
     (assign the-cdrs (reg new-cdrs))
-    (assign new-cdrs (reg temp))
+    (assign new-cdrs (reg gc-temp))
     ;; the-cars <-> new-cars
-    (assign temp (reg the-cars))
+    (assign gc-temp (reg the-cars))
     (assign the-cars (reg new-cars))
-    (assign new-cars (reg temp))
+    (assign new-cars (reg gc-temp))
 
     ))
