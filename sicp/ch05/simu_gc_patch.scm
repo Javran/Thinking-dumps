2b5e3f4e46bb36b451356c23b7d508d9d82ce60b
Javran Cheng <Javran.c@gmail.com>
2014-11-20 23:34:06 -0500

    gc - check free agains mem size

--- sicp/ch05/simu_gc_patch.scm
+++ sicp/ch05/simu_gc_patch.scm
@@ -5,12 +5,13 @@
 
 ;; registers that shouldn't be stored by "root" register
 (define machine-reserved-gc-registers
-  (cons 'gc-resume-point
-        (extract-register-names gc-code)))
+  (remove-duplicates
+   `(gc-resume-point
+     gc-flag
+     ,@(extract-register-names gc-code))))
 
 ;; TODO: think about how to deal with "continue" register,
 ;; which stores non-regular values?
-
 
 ;; TODO: forbid access to gc-related registers in user instruction list
 (define (machine-do-insn-list-preprocess insns)
@@ -21,10 +22,10 @@
          ;; note that "extract-register-names" removes pc and flag registers
          ;; for pc register, we don't need to store it,
          ;; but we need to keep flag register
-         ;; as gc routine might use it.
+         ;; as gc routine might use it. (TODO: now gc-flag keeps it)
          (user-registers
           (remove-duplicates
-           (set-diff (cons 'flag (extract-register-names expanded-insns))
+           (set-diff (extract-register-names expanded-insns)
                      machine-reserved-gc-registers)))
          ;; we assume preallocating space for register traversal
          ;; will never used up the space
@@ -52,10 +53,19 @@
       (goto (label ,prog-entry-label))
       ;; subroutines here
       gc-maybe-start
+      (assign gc-flag (reg flag))
+      ;; if free = memory size, we need gc
+      (assign gc-temp (op to-pointer) (const ,machine-memory-size))
+      (test (op ptr=?) (reg free) (reg gc-temp))
+      (branch (label gc-needed))
+      (goto (label gc-all-done))
+      gc-needed
       ;; TODO: for now gc is never triggered
       ;; TODO: just save and restore to see if it works
       ,@save-registers-insns
       ,@restore-registers-insns
+      gc-all-done
+      (assign flag (reg gc-flag))
       (goto (reg gc-resume-point))
       ;; program entry
       ,prog-entry-label
