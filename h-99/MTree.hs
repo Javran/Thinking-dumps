1c0025e20c425ff2b241dcc498a1d4033d271949
Javran Cheng <Javran.c@gmail.com>
2014-11-23 01:21:36 -0500

    Problem70B - done

---
+++ h-99/MTree.hs
@@ -0,0 +1,16 @@
+module MTree where
+
+-- MTree for MultiWayTree
+
+import Data.Tree
+
+tree1, tree2, tree3, tree4, tree5 :: Tree Char
+
+tree1 = Node 'a' []
+tree2 = Node 'a' [Node 'b' []]
+tree3 = Node 'a' [Node 'b' [Node 'c' []]]
+tree4 = Node 'b' [Node 'd' [], Node 'e' []]
+tree5 = Node 'a' [Node 'f' [Node 'g' []]
+                 ,Node 'c' []
+                 , Node 'b' [Node 'd' [], Node 'e' []]
+                 ]
