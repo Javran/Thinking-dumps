8a3b26aac43819deb861e717f641ab41fda7c7dd
Javran Cheng <Javran.c@gmail.com>
2014-11-23 23:40:39 -0500

    some warnings about extract-register-names

--- sicp/ch05/legacy-easy.scm
+++ sicp/ch05/legacy-easy.scm
@@ -37,7 +37,12 @@
      (not ,not)
      ))
 
-;; from "simu" modules
+
+;; TODO: fix this
+;; NOTE: note that "extract-register-names" from
+;; "simu.scm" and "legacy-easy.scm" are actually two different functions:
+;; one found in "legacy-easy.scm" will try to remove "pc" and "flag" from
+;; the resulting list while one found in "simu.scm" does not.
 (define (extract-register-names instructions)
   (define (extract insn)
     (if (symbol? insn)
