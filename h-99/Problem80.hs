58e80b8dea72c417ec25541e98ca94a8efe993fa
Javran Cheng <Javran.c@gmail.com>
2014-11-24 20:43:46 -0500

    Problem80 - some fixs

--- h-99/Problem80.hs
+++ h-99/Problem80.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses,TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses,TupleSections,FlexibleInstances #-}
 module Problem80 where
 
 import Data.List
@@ -10,15 +10,26 @@ import Control.Arrow
 data GraphForm v e = GraphForm
     { gfVertices :: S.Set v
     , gfEdges    :: [e]
-    }
+    } deriving Show
 
-data AdjForm v e = AdjForm (M.Map v [e])
+data AdjForm v e = AdjForm (M.Map v [e]) deriving Show
 
-data FndForm v e = FndForm [Either v e]
 
-class VertexEdge v e where
+data FndForm v e = FndForm [Either v e] deriving Show
+
+data Edge v = Edge v v deriving Show
+
+instance Eq v => Eq (Edge v) where
+    (Edge a b) == (Edge c d) = (a,b) == (c,d) || (a,b) == (d,c)
+
+class Eq v => VertexEdge v e where
     terminals :: e -> (v,v)
     terminalOf :: v -> e -> Bool
+
+    v `terminalOf` e = let (a,b) = terminals e in v == a || v == b
+
+instance Eq v => VertexEdge v (Edge v) where
+    terminals (Edge a b) = (a,b)
 
 pairToList :: (a,a) -> [a]
 pairToList ~(x,y) = [x,y]
@@ -26,7 +37,7 @@ pairToList ~(x,y) = [x,y]
 graphFormToAdjForm :: (Ord v, VertexEdge v e) => GraphForm v e -> AdjForm v e
 graphFormToAdjForm (GraphForm vs es) = AdjForm (M.fromListWith (++) pairs)
   where
-    pairs = concatMap edgeToPair es
+    pairs = concatMap edgeToPair es ++ (map (,[]) . S.toList $ vs)
     edgeToPair e = map (,[e]) . pairToList . terminals $ e
 
 adjFormToGraphForm :: (Eq e, Ord v, VertexEdge v e) => AdjForm v e -> GraphForm v e
@@ -47,5 +58,19 @@ graphFormToFndForm (GraphForm vs es) = FndForm (vs' ++ es')
     vs' = map Left . S.toList $ vs
     es' = map Right es
 
+example :: GraphForm Char (Edge Char)
+example = GraphForm vs es
+  where
+    vs = S.fromList "ghbcfkd"
+    es = map (uncurry Edge) [ ('g','h')
+                            , ('b','c')
+                            , ('b','f')
+                            , ('f','c')
+                            , ('f','k')
+                            ]
+
 main :: IO ()
-main = _
+main = do
+    print example
+    -- TODO: missing some vertices
+    print (graphFormToAdjForm example)
