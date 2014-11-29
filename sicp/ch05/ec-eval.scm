d6599b44fd3f800cf2cea74f2dd5327e92ab5409
Javran Cheng <Javran.c@gmail.com>
2014-11-28 22:42:13 -0500

    ec-eval - explict control evaluator

---
+++ sicp/ch05/ec-eval.scm
@@ -0,0 +1,30 @@
+(define core-dispatch
+  '(eval-dispatch
+    (test (op self-evaluating?) (reg exp))
+    (branch (label ev-self-eval))
+
+    (test (op variable?) (reg exp))
+    (branch (label ev-variable))
+
+    (test (op quoted?) (reg exp))
+    (branch (label ev-quoted))
+
+    (test (op assignment?) (reg exp))
+    (branch (label ev-assignment))
+
+    (test (op definition?) (reg exp))
+    (branch (label ev-definition))
+
+    (test (op if?) (reg exp))
+    (branch (label ev-if))
+
+    (test (op lambda?) (reg exp))
+    (branch (label ev-lambda))
+
+    (test (op begin?) (reg exp))
+    (branch (label ev-begin))
+
+    (test (op application?) (reg exp))
+    (branch (label ev-application))
+
+    (goto (label unknown-expression-type))))
