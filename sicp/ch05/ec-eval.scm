b9fd68b1c12cdcf055b16033b7b35bfb25e13a7e
Javran Cheng <Javran.c@gmail.com>
2014-11-28 23:07:49 -0500

    ec - applications

--- sicp/ch05/ec-eval.scm
+++ sicp/ch05/ec-eval.scm
@@ -1,3 +1,6 @@
+;; for now I'm not sure what to do
+;; guess if we copy the code here,
+;; this will soon become useful
 (define core-dispatch
   '(eval-dispatch
     (test (op self-evaluating?) (reg exp))
@@ -28,3 +31,74 @@
     (branch (label ev-application))
 
     (goto (label unknown-expression-type))))
+
+(define simple-expressions
+  '(ev-self-eval
+    (assign val (reg exp))
+    (goto (reg continue))
+
+    ev-variable
+    (assign
+     val (op lookup-variable-value) (reg exp) (reg env))
+    (goto (reg continue))
+
+    ev-quoted
+    (assign val (op text-of-quotation) (reg exp))
+    (goto (reg continue))
+
+    ev-lambda
+    (assign unev (op lambda-parameters) (reg exp))
+    (assign exp (op lambda-body) (reg exp))
+    (assign val (op make-procedure)
+                (reg unev) (reg exp) (reg env))
+    (goto (reg continue))))
+
+(define proc-application
+  '(ev-application
+    (save continue) ; stack: [continue ..]
+    (save env) ; stack: [env continue ..]
+    (assign unev (op operands) (reg exp))
+    (save unev) ; stack: [unev env continue ..]
+    (assign exp (op operator) (reg exp))
+    (assign continue (label ev-appl-did-operator))
+    (goto (label eval-dispatch))
+
+    ;; back
+    ev-appl-did-operator
+    (restore unev) ; stack: [env continue ..]
+    (restore env) ; stack: [continue ..]
+    ;; things evaluated so far
+    (assign argl (op empty-arglist))
+    ;; evaluated procedure
+    (assign proc (reg val))
+    ;; call without operands, do application
+    (test (op no-operands?) (reg unev))
+    (branch (label apply-dispatch))
+    (save proc) ; stack: [proc continue ..]
+    ev-appl-operand-loop
+    (save argl) ; stack: [argl proc continue ..]
+    (assign exp (op first-operand) (reg unev))
+    (test (op last-operand?) (reg unev))
+    (branch (label ev-appl-last-arg))
+    (save env) ; stack: [env argl proc continue ..]
+    (save unev) ; stack: [unev env argl proc continue ..]
+    (assign continue (label ev-appl-accumulate-arg))
+    (goto (label eval-dispatch))
+    ;; back
+    ev-appl-accumulate-arg
+    (restore unev) ; stack: [env argl proc continue ..]
+    (restore env) ; stack: [argl proc continue ..]
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+    (assign unev (op rest-operands) (reg unev))
+    (goto (label ev-appl-operand-loop))
+
+    ev-appl-last-arg
+    (assign continue (label ev-appl-accum-last-arg))
+    (goto (label eval-dispatch))
+    ev-appl-accum-last-arg
+    (restore argl) ; stack: [proc continue ..]
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+    (restore proc) ; stack: [continue ..]
+    (goto (label apply-dispatch))
+    ;; TODO: stack not balanced here?
+    ))
