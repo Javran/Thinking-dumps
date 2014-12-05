5d1b01ac7616b363fe3cf176edde8d7db76d3036
Javran Cheng <Javran.c@gmail.com>
2014-12-04 21:08:01 -0500

    ec - two kinds of primitives

--- sicp/ch05/simu_ec_patch.scm
+++ sicp/ch05/simu_ec_patch.scm
@@ -10,7 +10,7 @@
 
 ;; evaluate a symbol under the current toplevel
 ;; environment, and make it an primitive entry
-(define (to-prim-entry sym)
+(define (to-machine-prim-entry sym)
   `(,sym ,(eval sym user-initial-environment)))
 
 (define default-ops-builder
@@ -30,12 +30,25 @@
           ;;   them somewhere in our toplevel user environment
           ;;   we can evaluate them directly to convert each operation symbol
           ;;   to its corresponding primitive entry
-          ,@(map to-prim-entry new-prim-symbols)
+          ,@(map to-machine-prim-entry new-prim-symbols)
           ,@(old-builder m))))))
 
 ;; TODO: guess we can now abstract out the machine part
 ;; making the whole process a scheme procedure
 ;; and write some unit tests
+
+;; note that we have 2 kinds of primitives:
+;; one kind of the primitives are for the machine,
+;; which implement the functionality of an evaluator
+;; and another kind of the primitives for the implemented language,
+;; which provides the functionality so that we can do something useful
+;; in the implemented language.
+;; "to-machine-prim-entry" lifts primitives to the machine
+;; (TODO) "to-eval-prim-entry" lifts primitives so that it's visible
+;; from the implemented language.
+
+;; TODO: need some primitives to build up the default environment
+;; maybe we can copy from ch04
 
 (out
  (default-ops-builder (empty-machine)))
