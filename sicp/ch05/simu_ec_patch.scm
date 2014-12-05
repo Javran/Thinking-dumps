56795d3d5279520c603560be8c2ee2717a36ad46
Javran Cheng <Javran.c@gmail.com>
2014-12-05 00:26:24 -0500

    ec - initial env from ch04

--- sicp/ch05/simu_ec_patch.scm
+++ sicp/ch05/simu_ec_patch.scm
@@ -50,7 +50,54 @@
 ;; TODO: need some primitives to build up the default environment
 ;; maybe we can copy from ch04
 
+(define (lift-primitive-pair sym)
+  `(,sym ,(lift-primitive (eval sym user-initial-environment))))
+
+;; initialize an environment that contains basic stuff
+(define (init-env)
+  (let ((proc-list
+          (list
+            (lift-primitive-pair '+)
+            (lift-primitive-pair '-)
+            (lift-primitive-pair '*)
+            (lift-primitive-pair '/)
+            (lift-primitive-pair '=)
+            (lift-primitive-pair '>)
+            (lift-primitive-pair '>=)
+            (lift-primitive-pair '<)
+            (lift-primitive-pair '<=)
+            (lift-primitive-pair 'zero?)
+            (lift-primitive-pair 'eq?)
+            (lift-primitive-pair 'eqv?)
+            (lift-primitive-pair 'car)
+            (lift-primitive-pair 'cdr)
+            (lift-primitive-pair 'cons)
+            (lift-primitive-pair 'null?)
+            (lift-primitive-pair 'list)
+            (lift-primitive-pair 'even?)
+            (lift-primitive-pair 'odd?)
+            (lift-primitive-pair 'not)
+            (lift-primitive-pair 'remainder)
+            (lift-primitive-pair 'quotient)
+            (lift-primitive-pair 'sqrt)
+            (lift-primitive-pair 'integer?)
+            (lift-primitive-pair 'member)
+            (lift-primitive-pair 'memq)
+            (lift-primitive-pair 'delete)
+            (lift-primitive-pair 'abs)
+            (lift-primitive-pair 'append)
+            )))
+    (extend-environment
+      '(true false)
+      '(#t   #f)
+      (extend-environment
+        (map car proc-list)
+        (map cadr proc-list)
+        the-empty-environment))))
+
 (out
  (default-ops-builder (empty-machine)))
+
+(out (init-env))
 
 (end-script)
