36bec4237684215e3255eaa6328cc1c08a061295
Javran Cheng <Javran.c@gmail.com>
2014-12-21 22:28:39 -0500

    ex 5.24 - few more tests

--- sicp/ch05/exercise_5_24.scm
+++ sicp/ch05/exercise_5_24.scm
@@ -25,14 +25,33 @@
                 (+ x y y)))
     (cond (#t 10)
           (else 20))
-    ;; looks like a bug here..
     (cond (#f 10)
           (#t 20))
+    (cond ((= (+ 10 20) 10) 1)
+          ((= (+ 10 20) 20) 2)
+          ((= (+ 10 20) 30) 3)
+          (else 4))
+    (begin
+      (define x 10)
+      ;; in general I think it's not recommended
+      ;; to have side effects when evaluating the condition
+      ;; here we are just testing the functionality
+      (cond ((begin
+               (set! x (+ x 1))
+               (= x 12)) 10)
+            ((begin
+               (set! x (+ x 1))
+               (= x 12)) 20)
+            (else 30)))
+    (begin
+      (define y 10)
+      (cond ((= 1 2) (set! y (+ y 10)))
+            ((= 1 1) (set! y (+ y 100))))
+      y)
     ))
 
 (for-each (test-evaluator machine-eval) test-exps)
 (newline)
-;; TODO some tests
 
 (end-script)
 
