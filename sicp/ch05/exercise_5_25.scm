8ed60163df9e184fa8c565ffaeddbe3586aab4da
Javran Cheng <Javran.c@gmail.com>
2014-12-22 23:18:07 -0500

    ex 5.25 - normal-order vs applicative-order

--- sicp/ch05/exercise_5_25.scm
+++ sicp/ch05/exercise_5_25.scm
@@ -1,7 +1,23 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+;; there are actually two kinds of normal-order evaluations:
+;; * call-by-name (no memorization)
+;; * call-by-need (memorization, lazy evaluation)
+;; recall the difference between applicative-order and normal-order
+;; is how an expression gets evaluated: before function application,
+;; applicative-order evaluation evaluates all the arguments
+;; while normal-order evaluation only evaluates a value as needed
+;; there are two kinds of situations:
+;; * a primitive function needs that value, in which case
+;;   the evaluation is forced because the primitives have no
+;;   knowledge about expressions
+;; * a condition depends on that value, in which case
+;;   we need the exact value not the unevaluated expression in order to
+;;   proceed
+
 ;; * guess we first need to implement thunk?
+
 
 (end-script)
 
