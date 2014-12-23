f781a49c143ed76fee4cb6db026dff47c24b2aa3
Javran Cheng <Javran.c@gmail.com>
2014-12-22 23:23:29 -0500

    ex 5.25 - plan

--- sicp/ch05/exercise_5_25.scm
+++ sicp/ch05/exercise_5_25.scm
@@ -21,6 +21,12 @@
 ;; * guess we first need to implement thunk?
 ;; * few parts are needed to be modified:
 ;;   + function application (need to force the operator)
+;;   + force all expressions before passing it to primitives
+;;   + use delayed values for compound functions
+;;   + force the condition expression value when dealing with "if" expressions
+;;   + careful when dealing with derived forms like "cond",
+;;     if later on we want to implement it independently instead of converting it
+;;     to a nested if-form, we need to at least leave a warning
 
 (end-script)
 
