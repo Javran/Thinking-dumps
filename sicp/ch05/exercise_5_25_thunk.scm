d5014f78e172f6182482b3ee0a2d77863dd8528f
Javran Cheng <Javran.c@gmail.com>
2014-12-23 23:45:48 -0500

    ex 5.25 - no force-it here :(

--- sicp/ch05/exercise_5_25_thunk.scm
+++ sicp/ch05/exercise_5_25_thunk.scm
@@ -14,9 +14,16 @@
 
 (define thunk-value cadr)
 
+;; no "force-it" function here
+;; as on this level, we don't know how the implemented language
+;; is going to evaluate it.
+;; therefore the only we can do is to define this "thunk-set-value!"
+;; procedure so that when an expression gets evaluated, we can turn an
+;; unevaluated thunk into an evaluated one
 (define (thunk-set-value! obj value)
   (assert (thunk? obj)
           "the object is not an unevaluated thunk")
   (set-car! obj 'evaluated-thunk?)
   (set-car! (cdr obj) value)
   (set-cdr! (cdr obj) '()))
+
