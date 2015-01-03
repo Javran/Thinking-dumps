048da1e6a6c65485f595a26d549aff950e42a519
Javran Cheng <Javran.c@gmail.com>
2015-01-02 21:39:13 -0500

    ex 5.28 - evaluator modified

---
+++ sicp/ch05/exercise_5_28_ec-eval.scm
@@ -0,0 +1,276 @@
+(load "./ec-prim.scm")
+(load "./ec-init-env.scm")
+
+(define evaluator-insns
+  '(
+    eval-dispatch
+
+    (test (op self-evaluating?) (reg exp))
+    (branch (label ev-self-eval))
+
+    (test (op variable?) (reg exp))
+    (branch (label ev-variable))
+
+    (test (op quoted?) (reg exp))
+    (branch (label ev-quoted))
+
+    (test (op assignment?) (reg exp))
+    (branch (label ev-assignment))
+
+    (test (op definition?) (reg exp))
+    (branch (label ev-definition))
+
+    (test (op if?) (reg exp))
+    (branch (label ev-if))
+
+    (test (op lambda?) (reg exp))
+    (branch (label ev-lambda))
+
+    (test (op begin?) (reg exp))
+    (branch (label ev-begin))
+
+    (test (op cond?) (reg exp))
+    (branch (label ev-cond))
+
+    (test (op let?) (reg exp))
+    (branch (label ev-let))
+
+    (test (op application?) (reg exp))
+    (branch (label ev-application))
+
+    (goto (label unknown-expression-type))
+
+    ev-self-eval
+    (assign val (reg exp))
+    (goto (reg continue))
+
+    ev-variable
+    (assign
+     val (op lookup-variable-value) (reg exp) (reg env))
+    (goto (reg continue))
+
+    ev-quoted
+    (assign val (op text-of-quotation) (reg exp))
+    (goto (reg continue))
+
+    ev-lambda
+    (assign unev (op lambda-parameters) (reg exp))
+    (assign exp (op lambda-body) (reg exp))
+    (assign val
+            (op make-procedure)
+            (reg unev) (reg exp) (reg env))
+    (goto (reg continue))
+
+    ev-application
+    (save continue)                     ; stack: [continue ..]
+    (save env)                          ; stack: [env continue ..]
+    (assign unev (op operands) (reg exp))
+    (save unev)                        ; stack: [unev env continue ..]
+    (assign exp (op operator) (reg exp))
+    (assign continue (label ev-appl-did-operator))
+    (goto (label eval-dispatch))
+
+    ev-appl-did-operator
+    (restore unev)                      ; stack: [env continue ..]
+    (restore env)                       ; stack: [continue ..]
+    (assign argl (op empty-arglist))
+    (assign proc (reg val))
+    (test (op no-operands?) (reg unev))
+    (branch (label apply-dispatch))
+    (save proc)                         ; stack: [proc continue ..]
+    ev-appl-operand-loop
+    (save argl)                       ; stack: [argl proc continue ..]
+    (assign exp (op first-operand) (reg unev))
+    (test (op last-operand?) (reg unev))
+    (branch (label ev-appl-last-arg))
+
+    (save env)               ; stack: [env argl proc continue ..]
+    (save unev)              ; stack: [unev env argl proc continue ..]
+    (assign continue (label ev-appl-accumulate-arg))
+    (goto (label eval-dispatch))
+    ev-appl-accumulate-arg
+
+    (restore unev)                ; stack: [env argl proc continue ..]
+    (restore env)                 ; stack: [argl proc continue ..]
+    (restore argl)                ; stack: [proc continue ..]
+
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+
+    (assign unev (op rest-operands) (reg unev))
+    (goto (label ev-appl-operand-loop))
+
+    ev-appl-last-arg
+    (assign continue (label ev-appl-accum-last-arg))
+    (goto (label eval-dispatch))
+    ev-appl-accum-last-arg
+    (restore argl)                      ; stack: [proc continue ..]
+    (assign argl (op adjoin-arg) (reg val) (reg argl))
+    (restore proc)                      ; stack: [continue ..]
+    (goto (label apply-dispatch))
+
+    apply-dispatch
+    (test (op primitive-procedure?) (reg proc))
+    (branch (label primitive-apply))
+    (test (op compound-procedure?) (reg proc))
+    (branch (label compound-apply))
+    (goto (label unknown-procedure-type))
+
+    primitive-apply
+    (assign val
+            (op apply-primitive-procedure)
+            (reg proc)
+            (reg argl))
+    (restore continue)                  ; stack: <balanced>
+    (goto (reg continue))
+
+    compound-apply
+    (assign unev (op procedure-parameters) (reg proc))
+    (assign env (op procedure-environment) (reg proc))
+    (assign env
+            (op extend-environment)
+            (reg unev) (reg argl) (reg env))
+    (assign unev (op procedure-body) (reg proc))
+    (goto (label ev-sequence))
+
+    ev-begin
+    (assign unev (op begin-actions) (reg exp))
+    (save continue)                     ; stack: [continue ..]
+    (goto (label ev-sequence))
+
+    ;; ev-sequence subroutine modified according to
+    ;; section 5.4.2 "tail recursion"
+    ev-sequence
+    (test (op no-more-exps?) (reg unev))
+    (branch (label ev-sequence-end))
+    (assign exp (op first-exp) (reg unev))
+    (save unev)                        ; stack: [unev continue ..]
+    (save env)                         ; stack: [env unev continue ..]
+    (assign continue (label ev-sequence-continue))
+    (goto (label eval-dispatch))
+    ev-sequence-continue
+    (restore env)                       ; stack: [unev continue ..]
+    (restore unev)                      ; stack: [continue ..]
+    (assign unev (op rest-exps) (reg unev))
+    (goto (label ev-sequence))
+    ev-sequence-end
+    (restore continue)                  ; stack: <balanced>
+    (goto (reg continue))
+
+    ev-if
+    (save exp)                          ; stack: [exp ..]
+    (save env)                          ; stack: [env exp ..]
+    (save continue)                     ; stack: [continue env exp ..]
+    (assign continue (label ev-if-decide))
+    (assign exp (op if-predicate) (reg exp))
+    (goto (label eval-dispatch))        ; evaluate the predicate
+    ev-if-decide
+    (restore continue)                  ; stack: [env exp ..]
+    (restore env)                       ; stack: [exp ..]
+    (restore exp)                       ; stack: <balanced>
+
+    (test (op true?) (reg val))
+    (branch (label ev-if-consequent))
+    ev-if-alternative
+    (assign exp (op if-alternative) (reg exp))
+    (goto (label eval-dispatch))
+    ev-if-consequent
+    (assign exp (op if-consequent) (reg exp))
+    (goto (label eval-dispatch))
+
+    ev-assignment
+    (assign unev (op assignment-variable) (reg exp))
+    (save unev)                         ; stack: [unev ..]
+    (assign exp (op assignment-value) (reg exp))
+    (save env)                         ; stack: [env unev ..]
+    (save continue)                    ; stack: [continue env unev ..]
+    (assign continue (label ev-assignment-1))
+
+    (goto (label eval-dispatch))
+    ev-assignment-1
+    (restore continue)                  ; stack: [env unev ..]
+    (restore env)                       ; stack: [unev ..]
+    (restore unev)                      ; stack: <balanced>
+    (perform
+     (op set-variable-value!) (reg unev) (reg val) (reg env))
+    (assign val (const ok))
+    (goto (reg continue))
+
+    ev-definition
+    (assign exp (op normalize-define) (reg exp))
+    (assign unev (op definition-variable) (reg exp))
+    (save unev)                         ; stack: [unev ..]
+    (assign exp (op definition-value) (reg exp))
+    (save env)                         ; stack: [env unev ..]
+    (save continue)                    ; stack: [continue env unev ..]
+    (assign continue (label ev-definition-1))
+    (goto (label eval-dispatch))
+    ev-definition-1
+    (restore continue)                  ; stack: [env unev ..]
+    (restore env)                       ; stack: [unev ..]
+    (restore unev)                      ; stack: <balanced>
+    (perform
+     (op define-variable!) (reg unev) (reg val) (reg env))
+    (assign val (const ok))
+    (goto (reg continue))
+
+    ev-cond
+    (assign exp (op cond->if) (reg exp))
+
+    (goto (label eval-dispatch))
+
+    ev-let
+    (assign exp (op let->combination) (reg exp))
+    (goto (label eval-dispatch))
+
+    read-eval-print-loop-init
+    (assign env (op init-env))
+
+    read-eval-print-loop
+    (perform (op initialize-stack))
+    (perform
+     (op prompt-for-input) (const "ec-repl> "))
+    (assign exp (op read))
+    (assign continue (label print-result))
+    (goto (label eval-dispatch))
+
+    print-result
+    (perform (op print) (reg val))
+    (perform (op print-stack-statistics))
+    (goto (label read-eval-print-loop))
+
+    unknown-expression-type
+    (assign val (const unknown-expression-type-error))
+    (goto (label signal-error))
+
+    unknown-procedure-type
+    (restore continue)
+    (assign val (const unknown-procedure-type-error))
+    (goto (label signal-error))
+
+    signal-error
+    (perform (op print) (const "error signaled:"))
+    (perform (op print) (reg val))
+    (goto (label read-eval-print-loop-init))
+    ))
+
+(define (extract-operations insns)
+  (define (insn->operation insn)
+    (if (pair? insn)
+        (let ((head (car insn)))
+          (cond ((and (eq? head 'assign)
+                      (eq? (car (caddr insn)) 'op))
+                 (list (list (cadr (caddr insn))
+                             (- (length insn) 3))))
+                ((or (eq? head 'test)
+                     (eq? head 'perform))
+                 (list (list (cadr (cadr insn))
+                             (- (length insn) 2))))
+                (else
+                 '())))
+        '()))
+  (remove-duplicates
+   (concat-map insn->operation
+               insns)))
+
+(define ec-required-operations
+  (map car (extract-operations evaluator-insns)))
---
+++ sicp/ch05/exercise_5_28_simu_ec_patch.scm
@@ -0,0 +1,48 @@
+;; it sounds really stupid to have multiple files
+;; that looks almost the same.
+;; Believe me, I also hate it.
+
+(load "./exercise_5_28_ec-eval.scm")
+(load "./ec-tests.scm")
+
+(load "./simu-monitor-patch.scm")
+(load "./exercise_5_23_common.scm")
+
+(define (to-machine-prim-entry sym)
+  `(,sym ,(eval sym user-initial-environment)))
+
+(define default-ops-builder
+  (let ((old-builder default-ops-builder))
+    (lambda (m)
+      (let* ((old-ops (old-builder m))
+             (new-prim-symbols
+              (set-diff ec-required-operations
+                        (map car old-ops))))
+        `(,@(map to-machine-prim-entry new-prim-symbols)
+          (error ,(lambda args
+                    (apply error args)))
+          ,@old-ops)))))
+
+(define (machine-eval exp env)
+  (let* ((entry-label (gensym))
+         (exit-label (gensym))
+         (eval-label (gensym))
+         (m (build-and-execute
+             `(controller
+               (goto (label ,entry-label))
+               ,eval-label
+               ,@evaluator-insns
+               ,entry-label
+               (assign continue (label ,exit-label))
+               (goto (label ,eval-label))
+               ,exit-label)
+             `((exp ,exp)
+               (env ,env)))))
+    (machine-reg-get m 'val)))
+
+(define (ec-repl)
+  (build-and-execute
+   `(controller
+     (goto (label read-eval-print-loop-init))
+     ,@evaluator-insns)
+   '()))
