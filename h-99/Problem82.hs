744d391a3f961018555981669a6256c3f6beaabe
Javran Cheng <Javran.c@gmail.com>
2015-01-06 00:39:09 -0500

    TODO done

--- h-99/Problem82.hs
+++ h-99/Problem82.hs
@@ -4,30 +4,16 @@ module Problem82 where
 import Control.Monad
 import Control.Applicative
 import qualified Data.Set as S
-import qualified Data.Map.Strict as M
-import Data.Maybe
-import Data.Foldable
 
 import Graph
 import Problem80
-import Problem81 (Arc(..),toFndForm)
-
--- TODO: we can have a general "nextVertices"
--- and have it implemented correct once for all.
+import Problem81 (Arc(..),toFndForm,nextVertices)
 
 findCycles :: forall a . Ord a => a -> GraphForm a (Arc a) -> [ [a] ]
 findCycles from g = (from:) <$> findCycles' initNexts (S.singleton from)
   where
-    initNexts = nextVertices from
-    (AdjForm gph) = graphFormToAdjForm g
-    -- be extreme careful here that the adjacent form gets *all the edges*
-    -- related to a certain vertex in its map,
-    -- in order to extract vertices that it points to (excluding those
-    -- that points to it) we need "extraceDstFrom" function
-    extractDstFrom vFrom (v1,v2) = [ v2 | v1 == vFrom ]
-    nextVertices v = foldMap (extractDstFrom v . terminals)
-                   . fromJust
-                   $ M.lookup v gph
+    gph = graphFormToAdjForm g
+    initNexts = nextVertices gph from
     findCycles' :: Ord a => [a] -> S.Set a -> [ [a] ]
     findCycles' candidates visited = do
         next <- candidates
@@ -35,7 +21,7 @@ findCycles from g = (from:) <$> findCycles' initNexts (S.singleton from)
            then return [next]
            else do
              guard $ next `S.notMember` visited
-             restCycle <- findCycles' (nextVertices next) (S.insert next visited)
+             restCycle <- findCycles' (nextVertices gph next) (S.insert next visited)
              return (next:restCycle)
 
 cycles :: Ord a => a -> [ (a,a) ] -> [ [a] ]
