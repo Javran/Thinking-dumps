c4e4bb3c11d24f787c02db3c42c111dfa268450d
Javran Cheng <Javran.c@gmail.com>
2015-01-06 23:22:05 -0500

    ex 5.30 - trivial work

---
+++ sicp/ch05/exercise_5_30.scm
@@ -0,0 +1,16 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./simu.scm")
+(load "./simu_ec_patch.scm")
+(load "./exercise_5_23_common.scm")
+
+(load "./exercise_5_30_ec-eval.scm")
+
+(ec-repl)
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
