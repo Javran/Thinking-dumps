e53b59441e53c03350856dfb956502167bac51be
Javran Cheng <Javran.c@gmail.com>
2015-01-07 23:30:03 -0500

    ex 5.30 - runtime error only

--- sicp/ch05/exercise_5_30.scm
+++ sicp/ch05/exercise_5_30.scm
@@ -8,6 +8,13 @@
 (load "./exercise_5_30_error.scm")
 (load "./exercise_5_30_machine.scm")
 
+;; for simplicity, we just assume there is no syntax error.
+;; I think this is a fair assumption since syntax error
+;; can be detected even before running the program.
+
+;; In addition, the errors suggested by the exercise are
+;; also limited to runtime errors.
+
 (ec-repl)
 
 ;; Local variables:
