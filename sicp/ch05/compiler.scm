9cd86faf6d8a63cae0f7fac4b3f9b964c1c8fbb0
Javran Cheng <Javran.c@gmail.com>
2015-01-11 23:53:20 -0500

    compiler - linkage stuff

--- sicp/ch05/compiler.scm
+++ sicp/ch05/compiler.scm
@@ -45,3 +45,21 @@
 
 (define (empty-instruction-sequence)
   (make-instruction-sequence '() '() '()))
+
+(define (compile-linkage linkage)
+  (cond ((eq? linkage 'return)
+         (make-instruction-sequence
+          '(continue) '()
+          '((goto (reg continue)))))
+        ((eq? linkage 'next)
+         (empty-instruction-sequence))
+        (else
+         (make-instruction-sequence
+          '() '()
+          '((goto (label ,linkage)))))))
+
+(define (end-with-linkage linkage instruction-sequence)
+  (preserving
+   '(continue)
+   instruction-sequence
+   (compile-linkage linkage)))
