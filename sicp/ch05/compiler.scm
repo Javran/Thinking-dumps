4f9c9436e42e4f79c6026382fc7072befbd27fb1
Javran Cheng <Javran.c@gmail.com>
2015-01-12 20:35:28 -0500

    compiler - assignments and definitions

--- sicp/ch05/compiler.scm
+++ sicp/ch05/compiler.scm
@@ -96,3 +96,36 @@
               (op lookup-variable-value)
               (const ,exp)
               (reg env))))))
+
+;; assignments and definitions
+(define (compile-assignment exp target linkage)
+  (let ((var (assignment-variable exp))
+        (get-value-code
+         (compile (assignment-value exp) 'val 'next)))
+    (end-with-linkage
+     (preserving
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val) (list target)
+       `((perform (op set-variable-value!)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
+
+(define (compile-definition exp target linkage)
+  (let ((var (definition-variable exp))
+        (get-value-code
+         (compile (definition-value exp) 'val 'next)))
+    (end-with-linkage
+     (preserving
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val) (list target)
+       '((perform (op define-variable!)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
