a860d02c39a8dfdac3dac965696df8a100c0042f
Javran Cheng <Javran.c@gmail.com>
2015-01-12 20:47:07 -0500

    compiler - conditionals

--- sicp/ch05/compiler.scm
+++ sicp/ch05/compiler.scm
@@ -129,3 +129,42 @@
                   (reg val)
                   (reg env))
          (assign ,target (const ok))))))))
+
+;; conditionals
+(define label-counter 0)
+(define (new-label-number)
+  (set! label-counter (+ 1 label-counter))
+  label-counter)
+(define (make-label name)
+  (string->symbol
+   (string-append
+    (symbol->string name)
+    (number->string (new-label-number)))))
+
+(define (compile-if exp target linkage)
+  (let ((t-branch (make-label 'true-branch))
+        (f-branch (make-label 'false-branch))
+        (after-if (make-label 'after-if)))
+    (let ((consequent-linkage
+           (if (eq? linkage 'next)
+               after-if
+               linkage)))
+      (let ((p-code (compile (if-predicate exp) 'val 'next))
+            (c-code (compile (if-consequent exp)
+                             target
+                             consequent-linkage))
+            (a-code (compile (if-alternative exp)
+                             target
+                             linkage)))
+        (preserving
+         '(env continue)
+         p-code
+         (append-instruction-sequences
+          (make-instruction-sequence
+           '(val) '()
+           `((test (op false?) (reg val))
+             (branch (label ,f-branch))))
+          (parallel-instruction-sequences
+           (append-instruction-sequences t-branch c-code)
+           (append-instruction-sequences f-branch a-code))
+          after-if))))))
