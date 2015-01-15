0423c5c9536fbca2d6aa613719f0df29e981e532
Javran Cheng <Javran.c@gmail.com>
2015-01-14 23:50:09 -0500

    compiler - combination application

--- sicp/ch05/compiler.scm
+++ sicp/ch05/compiler.scm
@@ -239,3 +239,39 @@
       '(proc continue)
       (construct-arglist operand-codes)
       (compile-procedure-call target linkage)))))
+
+(define (construct-arglist operand-codes)
+  (let ((operand-codes (reverse operand-codes)))
+        (if (null? operand-codes)
+            (make-instruction-sequence
+             '() '(argl)
+             '((assign argl (const ()))))
+            (let ((code-to-get-last-arg
+                   (append-instruction-sequences
+                    (car operand-codes)
+                    (make-instruction-sequence
+                     '(val) '(argl)
+                     '((assign argl (op list) (reg val)))))))
+              (if (null? (cdr operand-codes))
+                  code-to-get-last-arg
+                  (preserving
+                   '(env)
+                   code-to-get-last-arg
+                   (code-to-get-rest-args
+                    (cdr operand-codes))))))))
+
+(define (code-to-get-rest-args operand-codes)
+  (let ((code-for-next-arg
+         (preserving
+          '(argl)
+          (car operand-codes)
+          (make-instruction-sequence
+           '(val argl) '(argl)
+           '((assign argl
+                     (op cons) (reg val) (reg argl)))))))
+    (if (null? (cdr operand-codes))
+        code-for-next-arg
+        (preserving
+         '(env)
+         code-for-next-arg
+         (code-to-get-rest-args (cdr operand-codes))))))
