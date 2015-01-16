d8b8b8637ff196636e3c5260464bfa3c881fdb2a
Javran Cheng <Javran.c@gmail.com>
2015-01-15 23:02:22 -0500

    compiler - preserving

--- sicp/ch05/compiler.scm
+++ sicp/ch05/compiler.scm
@@ -377,3 +377,37 @@
          (car seqs)
          (append-seq-list (cdr seqs)))))
   (append-seq-list seqs))
+
+;; changed to use "member" because "memq" doesn't work
+;; for more general cases
+(define (list-union s1 s2)
+  (cond ((null? s1) s2)
+        ((member (car s1) s2) (list-union (cdr s1) s2))
+        (else
+         (cons (car s1) (list-union (cdr s1) s2)))))
+
+(define (list-difference s1 s2)
+  (cond ((null? s1) '())
+        ((member (car s1) s2) (list-difference (cdr s1) s2))
+        (else
+         (cons (car s1)
+               (list-difference (cdr s1) s2)))))
+
+(define (preserving regs seq1 seq2)
+  (if (null? regs)
+      (append-instruction-sequences seq1 seq2)
+      (let ((first-reg (car regs)))
+        (if (and (needs-register? seq2 first-reg)
+                 (modifies-register? seq1 first-reg))
+            (preserving
+             (cdr regs)
+             (make-instruction-sequence
+              (list-union (list first-reg)
+                          (registers-needed seq1))
+              (list-difference (registers-modified seq1)
+                               (list first-reg))
+              (append `((save ,first-reg))
+                      (statements seq1)
+                      `((restore ,first-reg))))
+             seq2)
+            (preserving (cdr regs) seq1 seq2)))))
