72e2d101abff8c382219e5c9ef97901cc914cacb
Javran Cheng <Javran.c@gmail.com>
2015-01-18 21:13:36 -0500

    set - equality test

--- sicp/ch05/set-tests.scm
+++ sicp/ch05/set-tests.scm
@@ -3,6 +3,20 @@
 
 (load "./set.scm")
 
-(out (set-difference '(1 2 3 4 5 20) '(3 6 5 4 9 1 2)))
+;; test set equality and establish other things on it
+(do-test
+ set-equal?
+ (list (mat '(1 2 3) '(1 2 3) #t)
+       (mat '(1 2 3) '(3 2 1) #t)
+       (mat '(1 2 3) '(4 5 6) #f)
+       (mat '() '() #t)
+       (mat '() '(1 2 3) #f)
+       (mat '(a b) '(b c) #f)
+       (mat '(a b c) '(a c) #f)
+       (mat '(a c d) '(b a c g d) #f)
+       (mat '(a c b d) '(d b c a) #t))
+ (lambda (actual expect)
+   (or (and actual expect)
+       (and (not actual) (not expect)))))
 
 (end-script)
