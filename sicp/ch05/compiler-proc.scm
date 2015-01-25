9a87026b34016f1761dd287ab480cb8f166e06f6
Javran Cheng <Javran.c@gmail.com>
2015-01-24 23:16:09 -0500

    compiler - a little more comments

--- sicp/ch05/compiler-proc.scm
+++ sicp/ch05/compiler-proc.scm
@@ -158,7 +158,15 @@
 (define (compile-procedure-call target linkage)
   ;; function application for compiled procedures
   (define (compile-proc-appl target linkage)
-    (cond ((and (eq? target 'val) (not (eq? linkage 'return)))
+    ;; note that the linkage will never be "next"
+    ;; case coverage:
+    ;;         | target = val | target != val
+    ;; return  | covered      | error
+    ;; <other> | covered      | covered
+    (cond ((and (eq? target 'val)
+                (not (eq? linkage 'return)))
+           ;; - no need to move result value
+           ;; - do an immediate jump afterwards
            (make-instruction-sequence
             '(proc) all-regs
             `((assign continue (label ,linkage))
@@ -167,6 +175,8 @@
               (goto (reg val)))))
           ((and (not (eq? target 'val))
                 (not (eq? linkage 'return)))
+           ;; - need to move result value "val" to the target
+           ;; - do an immediate jump afterwards
            (let ((proc-return (make-label 'proc-return)))
              (make-instruction-sequence
               '(proc) all-regs
@@ -175,17 +185,26 @@
                         (reg proc))
                 (goto (reg val))
                 ,proc-return
+                ;; need this extra step to transfer value
                 (assign ,target (reg val))
                 (goto (label ,linkage))))))
           ((and (eq? target 'val) (eq? linkage 'return))
+           ;; - no need to move result value
+           ;; - let the compiled procedure do the return job
            (make-instruction-sequence
-            '(proc continue)
-            all-regs
+            '(proc continue) all-regs
             `((assign val (op compiled-procedure-entry)
                       (reg proc))
               (goto (reg val)))))
           ((and (not (eq? target 'val))
                 (eq? linkage 'return))
+           ;; - conversion is broken here: whenever we return,
+           ;;   the resulting value should be available in "val"
+           ;; - the only place where a "return" linkage used
+           ;;   is in the body of "compile-lambda-body",
+           ;;   and that code sets its target to "val".
+           ;; so no code (at least for those found in book)
+           ;; will hit this branch
            (error "return linkage, target not val: COMPILE"
                   target))))
   ;; ====
@@ -204,6 +223,8 @@
         (append-instruction-sequences
          ;; otherwise the procedure must be a compiled one
          compiled-branch
+         ;; note that it's not possible for compiled-linkage to
+         ;; take value "next"
          (compile-proc-appl target compiled-linkage))
         (append-instruction-sequences
          primitive-branch
