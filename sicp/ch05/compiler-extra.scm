5c1f2f5bebf491f57730c98043bd84537837108f
Javran Cheng <Javran.c@gmail.com>
2015-01-26 18:18:22 -0500

    compiler - compile-and-run checks labels

--- sicp/ch05/compiler-extra.scm
+++ sicp/ch05/compiler-extra.scm
@@ -1,4 +1,5 @@
 ;; some extra functionalities for the compiler
+(load "./simu.scm")
 
 (define (print-instruction-sequence insn-seq)
   (format #t "Registers needed: ~A~%~
@@ -11,12 +12,18 @@
             (statements insn-seq))
   (out ";;;; End of listing"))
 
-
 ;; compile the expression
 ;; and run it on the machine
 (define (compile-and-run exp)
   (let* ((compiled (compile exp 'val 'next))
          (insn-seq (statements compiled)))
+    (if (check-labels insn-seq)
+        (out "Labels are checked.")
+        ;; not actually reachable
+        (out "Error regarding labels occurred."))
     ;; TODO
-    (for-each out (map car (extract-operations insn-seq)))
+    ;; - collect all needed operations
+    ;; - lift from scheme if necessary
+    ;; - execute the code
+    ;; - get result
     ))
