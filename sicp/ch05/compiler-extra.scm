c8cde97b553d7e7f22e246f85f5990f361e97410
Javran Cheng <Javran.c@gmail.com>
2015-01-27 21:47:59 -0500

    compiler - missing operations
    Signed-off-by: Javran Cheng <Javran.c@gmail.com>

--- sicp/ch05/compiler-extra.scm
+++ sicp/ch05/compiler-extra.scm
@@ -12,22 +12,39 @@
             (statements insn-seq))
   (out ";;;; End of listing"))
 
+;; TODO: actually we know which set of operations
+;; we will be using,
+;; lift them beforehands,
+;; instead of lifting them on the fly
+
 ;; compile the expression
 ;; and run it on the machine
 (define (compile-and-run exp)
   (let* ((compiled (compile exp 'val 'next))
          (insn-seq (statements compiled)))
+    ;; verify labels
     (if (check-labels insn-seq)
         (out "Labels are checked.")
         ;; not actually reachable
         (out "Error regarding labels occurred."))
-    (define ops
+
+    ;; extract required operations
+    (define req-ops
       (map car (extract-operations insn-seq)))
-    (out "Required operations:")
-    (for-each
-     (lambda (x)
-       (format #t "* ~A~%" x))
-     ops)
+    ;; create operation table builder
+    (define (ops-builder m)
+      (let* ((old-ops (default-ops-builder m))
+             (missing-opnames (set-difference
+                                req-ops
+                                (map car old-ops))))
+        (out "Missing operations:")
+        (for-each
+         (lambda (x)
+           (format #t "* ~A~%" x))
+         missing-opnames)
+        old-ops))
+    (ops-builder (empty-machine))
+
     ;; TODO
     ;; - lift from scheme if necessary
     ;; - execute the code
