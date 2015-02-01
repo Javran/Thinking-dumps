ccbd95a7bbfe2cb6342bb546f7fd306b5d3fbc98
Javran Cheng <Javran.c@gmail.com>
2015-01-31 21:17:49 -0500

    ex 5.31 - code from ec eval v2

---
+++ sicp/ch05/exercise_5_31.scm
@@ -0,0 +1,17 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./simu.scm")
+(load "./simu_ec_patch.scm")
+
+(load "./exercise_5_23_common.scm")
+(load "./exercise_5_31_machine.scm")
+
+(for-each (test-evaluator machine-eval)
+          test-exps)
+
+(newline)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
