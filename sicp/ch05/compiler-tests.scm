cb1e3b381afb7c8c27a544630f9e7da0b23d5e99
Javran Cheng <Javran.c@gmail.com>
2015-02-03 15:19:27 -0500

    compiler: having individual tests

---
+++ sicp/ch05/compiler-tests.scm
@@ -0,0 +1,7 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "./compiler.scm")
+
+(compiler-insn-seq-tests)
+(newline)
--- sicp/ch05/simu_compiler_patch_tests.scm
+++ sicp/ch05/simu_compiler_patch_tests.scm
@@ -6,18 +6,18 @@
 
 (load "./simu.scm")
 (load "./simu_compiler_patch.scm")
+
 ;; borrow test framework from ec-tests
 (load "./ec-tests.scm")
 ;; borrow testcases from ex 5.23
 ;; for testing basic expresssion & derived expressions
 (load "./exercise_5_23_tests.scm")
 
-;; TODO: compiler needs independent tests
-(compiler-insn-seq-tests)
-(newline)
-
 (for-each
  (test-evaluator
+  ;; compile and evalute the code
+  ;; and compare it with the result of evaluating
+  ;; the code using native evaluator from scheme
   compile-and-run-with-env)
  test-exps)
 
