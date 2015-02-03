e54ac57c4ce8a852c306c347e0ea58974f41042c
Javran Cheng <Javran.c@gmail.com>
2015-02-03 15:23:41 -0500

    compiler: reason for decoupling

--- sicp/ch05/compiler-tests.scm
+++ sicp/ch05/compiler-tests.scm
@@ -5,3 +5,11 @@
 
 (compiler-insn-seq-tests)
 (newline)
+
+;; the correctness of the compiler is further verified using
+;; any valid machine implementation.
+;; because the compiler shouldn't have knowledge
+;; about machines on its own.
+;; one of the possible implementation is "simu_compiler_patch.scm"
+;; run testcases in "simu_compiler_patch_tests.scm" for further
+;; verification.
