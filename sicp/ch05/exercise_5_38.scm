ef5886a42150dea05ecd860d6169e5f7f96e2bf3
Javran Cheng <Javran.c@gmail.com>
2015-02-09 21:37:10 -0500

    ex 5.38: get started.

---
+++ sicp/ch05/exercise_5_38.scm
@@ -0,0 +1,13 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+;; * does "spread-argument" only spread
+;;   operands to "arg1" and "arg2"?
+(define (spread-arguments operands)
+  'todo)
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
