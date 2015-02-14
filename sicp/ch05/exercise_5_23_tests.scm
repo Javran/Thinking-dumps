6aa843a46db5b268cd49519bcc839ae1ad5ad81a
Javran Cheng <Javran.c@gmail.com>
2015-02-13 23:21:02 -0500

    ex 5.23: append tests

--- sicp/ch05/exercise_5_23_tests.scm
+++ sicp/ch05/exercise_5_23_tests.scm
@@ -1,86 +1,10 @@
-(define test-exps
+;; adding extra test examples into test-exps
+;; please make sure "test-exps" from ec-tests.scm is loaded before
+;; loading this module
+;; also it's recommended to load this module only once.
+
+(define test-exps-ex-5-23
   `(
-    ;; ==== basic tests from ec-tests.scm
-    ;; test self-evaluating
-    10
-    1.3
-    "wow"
-    #\space
-    #\a
-    #t
-    ;; test variable
-    (begin
-      (define x +)
-      (x 1 2 3))
-    (begin
-      (define x 10)
-      (define y 20)
-      (- x y))
-    ;; test quoted
-    (quote ())
-    '(a b c)
-    'a
-    '"wow"
-    '(a (b c) (d e))
-    ;; test assignment
-    (begin
-      (define y 10)
-      (set! y 20)
-      (set! y (+ y y))
-      y)
-    ;; test definition
-    (begin
-      ;; tested already
-      (define a 'a)
-      a)
-    ;; test if
-    (begin
-      (define x 1)
-      (if #t
-          (set! x 10)
-          (set! x 20))
-      x)
-    (begin
-      (define y 1)
-      (if #f
-          (set! y 10)
-          (set! y 20))
-      y)
-    ;; test lambda
-    (begin
-      (define f
-        (lambda (x)
-          (lambda (y)
-            (+ x x y))))
-      ((f 10) 20))
-    ;; test currying
-    (((lambda (x) (lambda (y) (- x y))) 10) 20)
-    ;; test begin
-    (begin
-      1)
-    (begin
-      (+ 10 20)
-      (+ 20 30))
-    (begin
-      (define x 10)
-      (define y 20)
-      (+ x x y))
-    (begin
-      (define x 10)
-      (set! x 20)
-      x)
-    ;; test application
-    (begin
-      (define f
-        (lambda (a b c d)
-          (* (+ a b) (- c d))))
-      (f 10 20 30 40))
-    ;; test complex function application
-    ((lambda (x y z)
-       (+ x (* y z) z))
-     (+ 10 10)
-     (* 2 20)
-     ((lambda (t) (* t t)) 100))
     ;; ==== tests about derived forms
     ;; test cond
     ;; - normal expressions
@@ -128,3 +52,6 @@
           acc
           (loop (+ i 1) (+ acc i))))
     ))
+
+(set! test-exps
+      (append test-exps test-exps-ex-5-23))
