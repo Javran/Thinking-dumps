374e23732564b727cc76f5400cace9ce955e228f
Javran Cheng <Javran.c@gmail.com>
2015-02-14 22:13:26 -0500

    ex 5.38.c: done

--- sicp/ch05/exercise_5_38.scm
+++ sicp/ch05/exercise_5_38.scm
@@ -5,8 +5,18 @@
 (load "./simu.scm")
 (load "./simu_compiler_patch.scm")
 
+(define factorial-code
+  `(define (factorial n)
+     (if (= n 1)
+         1
+         (* (factorial (- n 1)) n))))
+;; compile using the original version
+(define compiled-before
+  (compile-and-check factorial-code))
+
 (load "./exercise_5_38_compiler.scm")
 
+;; run tests on the modified compiler
 (load "./ec-tests.scm")
 (load "./exercise_5_23_tests.scm")
 (for-each
@@ -14,6 +24,16 @@
   compile-and-run-with-env)
  test-exps)
 (newline)
+
+;; compile again using new compiler
+(define compiled-after
+  (compile-and-check factorial-code))
+
+(out ";; ==== original version:")
+(print-instruction-sequence compiled-before)
+(newline)
+(out ";; ==== with open-code primitives:")
+(print-instruction-sequence compiled-after)
 
 (end-script)
 
