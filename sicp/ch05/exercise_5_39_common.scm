0ac98cb81f0733ae228f61484ca2f8440ef1e7cc
Javran Cheng <Javran.c@gmail.com>
2015-02-17 23:34:29 -0500

    ex 5.39: signal error on lookup unassigned

--- sicp/ch05/exercise_5_39_common.scm
+++ sicp/ch05/exercise_5_39_common.scm
@@ -1,6 +1,13 @@
+;; lookup variable in the environment using lexical addressing
 ;; lx-addr: '(layer-index . in-layer-position)
+;; error will be signalled if the value of the variable is *unassigned*
 (define (lexical-address-lookup lx-addr env)
-  (let ((layer-i (car lx-addr))
-        (pos-i   (cdr lx-addr)))
-    (list-ref
-     (cdr (list-ref env layer-i)) pos-i)))
+  (define (lexical-address-lookup-intern lx-addr env)
+    (let ((layer-i (car lx-addr))
+          (pos-i   (cdr lx-addr)))
+      (list-ref
+       (cdr (list-ref env layer-i)) pos-i)))
+  (let ((result (lexical-address-lookup-intern lx-addr env)))
+    (assert (not (eq? result '*unassigned*))
+            "the value of the variable is not yet assigned")
+    result))
