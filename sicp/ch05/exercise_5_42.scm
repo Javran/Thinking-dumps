99b2072532ee0fee423bd80e3a0462056090d11b
Javran Cheng <Javran.c@gmail.com>
2015-02-23 23:27:43 -0500

    ex 5.42: link-in some modules

---
+++ sicp/ch05/exercise_5_42.scm
@@ -0,0 +1,15 @@
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
+
+(load "simu.scm")
+(load "simu_compiler_patch.scm")
+
+(load "exercise_5_39_common.scm")
+(load "exercise_5_40_compiler.scm")
+(load "exercise_5_41_common.scm")
+
+(end-script)
+
+;; Local variables:
+;; proc-entry: ""
+;; End:
