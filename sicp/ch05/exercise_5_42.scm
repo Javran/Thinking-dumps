0ed4072e24e71f2728dd59d29330e985ae2287ec
Javran Cheng <Javran.c@gmail.com>
2015-02-23 23:55:28 -0500

    ex 5.42 - compile-variable

--- sicp/ch05/exercise_5_42.scm
+++ sicp/ch05/exercise_5_42.scm
@@ -8,6 +8,65 @@
 (load "exercise_5_40_compiler.scm")
 (load "exercise_5_41_common.scm")
 
+(define primitive-operations
+  '(false?
+    lookup-variable-value
+    set-variable-value!
+    define-variable!
+    make-compiled-procedure
+    compiled-procedure-env
+    extend-environment
+    list
+    cons
+    compiled-procedure-entry
+    primitive-procedure?
+    apply-primitive-procedure
+    lexical-address-lookup
+    ))
+
+(define (compile-variable exp target linkage ctenv)
+  ;; it's guaranteed that "exp" is a variable
+  ;; because it's the reason that the expression
+  ;; gets dispatched here...
+  (let ((ct-result (find-variable exp ctenv)))
+    (if (equal? ct-result 'not-found)
+        ;; fallback to lookup runtime environment if not found
+        (end-with-linkage
+         linkage
+         (make-instruction-sequence
+          '(env)
+          (list target)
+          `((assign ,target
+                    (op lookup-variable-value)
+                    (const ,exp)
+                    (reg env)))))
+        ;; or use lexical addressing directly
+        (end-with-linkage
+         linkage
+         (make-instruction-sequence
+          '(env)
+          (list target)
+          `((assign ,target (op lexical-address-lookup)
+                            (const ,ct-result)
+                            (reg env))))))))
+
+(define (compile-assignment exp target linkage ctenv)
+  (let ((var (assignment-variable exp))
+        (get-value-code
+         (compile (assignment-value exp) 'val 'next ctenv)))
+    (end-with-linkage
+     linkage
+     (preserving
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val) (list target)
+       `((perform (op set-variable-value!)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
+
 (end-script)
 
 ;; Local variables:
