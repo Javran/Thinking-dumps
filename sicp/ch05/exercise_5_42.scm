aecc59eb10f69b83289baf10a7ac82468d0b95d7
Javran Cheng <Javran.c@gmail.com>
2015-02-24 00:13:38 -0500

    ex 5.42: working!

--- sicp/ch05/exercise_5_42.scm
+++ sicp/ch05/exercise_5_42.scm
@@ -1,6 +1,7 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
+(load "compiler.scm")
 (load "simu.scm")
 (load "simu_compiler_patch.scm")
 
@@ -81,6 +82,27 @@
                       (reg env)
                       (reg val))
              (assign ,target (const ok)))))))))
+
+(load "ec-tests.scm")
+(load "exercise_5_23_tests.scm")
+
+;; TODO: for now the test cases here are not doing much
+;; except making sure things don't break
+;; let's see if we want to change it when we come back...
+
+;; TODO: tmp fix, will remove later
+;; needs one more arg to compile
+(define (compile-and-check exp)
+  (let ((compiled (compile exp 'val 'next (empty-ctenv) )))
+    (assert (check-instruction-sequence compiled)
+            ;; the error message is not actually reachable
+            "instruction sequence check failed.")
+    compiled))
+
+(for-each
+ (test-evaluator
+  compile-and-run-with-env)
+ test-exps)
 
 (end-script)
 
