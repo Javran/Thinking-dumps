b301ca8923587d77fa9b676b50500b34786724bd
Javran Cheng <Javran.c@gmail.com>
2015-02-24 22:05:38 -0500

    ex 5.42: some more TODOs

--- sicp/ch05/exercise_5_42.scm
+++ sicp/ch05/exercise_5_42.scm
@@ -7,6 +7,11 @@
 
 (load "exercise_5_42_compiler.scm")
 
+;; TODO: see if it is applicable to apply "get-global-environment"
+;; TODO: figure out what happens when there is a "define" form
+;; inside a lambda expression
+;; hope this does not screw up lexical addressing
+
 (load "ec-tests.scm")
 (load "exercise_5_23_tests.scm")
 
