3ac67c59863ec60dbe13601f2a77d5b7cd1c8aa9
Javran Cheng <Javran.c@gmail.com>
2015-02-24 21:54:40 -0500

    ex 5.42: simplify

--- sicp/ch05/exercise_5_42.scm
+++ sicp/ch05/exercise_5_42.scm
@@ -31,52 +31,40 @@
   ;; because it's the reason that the expression
   ;; gets dispatched here...
   (let ((ct-result (find-variable exp ctenv)))
-    (if (equal? ct-result 'not-found)
-        ;; fallback to lookup runtime environment if not found
-        (end-with-linkage
-         linkage
-         (make-instruction-sequence
-          '(env)
-          (list target)
+    (end-with-linkage
+     linkage
+     (make-instruction-sequence
+      '(env)
+      (list target)
+      ;; if the compile-time environment does not contain
+      ;; the variable, we have to lookup runtime environment instead
+      (if (equal? ct-result 'not-found)
           `((assign ,target
                     (op lookup-variable-value)
                     (const ,exp)
-                    (reg env)))))
-        ;; or use lexical addressing directly
-        (end-with-linkage
-         linkage
-         (make-instruction-sequence
-          '(env)
-          (list target)
+                    (reg env)))
           `((assign ,target (op lexical-address-lookup)
-                            (const ,ct-result)
-                            (reg env))))))))
+                    (const ,ct-result)
+                    (reg env))))))))
 
 (define (compile-assignment exp target linkage ctenv)
   (let ((var (assignment-variable exp))
         (get-value-code
          (compile (assignment-value exp) 'val 'next ctenv))
         (ct-result (find-variable exp ctenv)))
-    (if (equal? ct-result 'not-found)
-        (end-with-linkage
-         linkage
-         (preserving
-          '(env)
-          get-value-code
-          (make-instruction-sequence
-           '(env val) (list target)
+    (end-with-linkage
+     linkage
+     (preserving
+      '(env)
+      get-value-code
+      (make-instruction-sequence
+       '(env val) (list target)
+       (if (equal? ct-result 'not-found)
            `((perform (op set-variable-value!)
                       (const ,var)
                       (reg val)
                       (reg env))
-             (assign ,target (const ok))))))
-        (end-with-linkage
-         linkage
-         (preserving
-          '(env)
-          get-value-code
-          (make-instruction-sequence
-           '(env val) (list target)
+             (assign ,target (const ok)))
            `((perform (op lexical-address-set!)
                       (const ,ct-result)
                       (reg env)
