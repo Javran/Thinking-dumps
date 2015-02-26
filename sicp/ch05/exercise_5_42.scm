9535a5b42707acacf0de2bcee439d13ac05d142e
Javran Cheng <Javran.c@gmail.com>
2015-02-25 21:41:46 -0500

    ex 5.42: problem with lexical addressing

--- sicp/ch05/exercise_5_42.scm
+++ sicp/ch05/exercise_5_42.scm
@@ -7,10 +7,25 @@
 
 (load "exercise_5_42_compiler.scm")
 
+;; NOTE: for the idea of lexical addressing
+;; to be applicable here, we assume there is no "non-top-level"
+;; definitions
+;; This means the following expression will not work under
+;; our new evaluator:
+;; ((lambda (x y)
+;;    (define z 17)
+;;    (define t 23)
+;;    (- z x t y))
+;;  11
+;;  100)
+;; the reason is because that definitions will call "add-binding-to-frame"
+;; function to manipulate the frame. the binding is added
+;; in front of the existing bindings of that frame,
+;; which makes the runtime environment and compile-time environment
+;; out-of-sync. A potential fix would be to make "add-binding-to-frame"
+;; insert in the back of the frame.
+
 ;; TODO: see if it is applicable to apply "get-global-environment"
-;; TODO: figure out what happens when there is a "define" form
-;; inside a lambda expression
-;; hope this does not screw up lexical addressing
 
 (load "ec-tests.scm")
 (load "exercise_5_23_tests.scm")
