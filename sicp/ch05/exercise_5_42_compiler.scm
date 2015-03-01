e49ac0f6ffba0497e874d2ab6450f6e91067140c
Javran Cheng <Javran.c@gmail.com>
2015-02-28 21:42:32 -0500

    ex 5.42: toplevel runtime env lookup

--- sicp/ch05/exercise_5_42_compiler.scm
+++ sicp/ch05/exercise_5_42_compiler.scm
@@ -14,40 +14,54 @@
   (let ((ct-result (find-variable exp ctenv)))
     (end-with-linkage
      linkage
-     (make-instruction-sequence
-      '(env)
-      (list target)
-      ;; if the compile-time environment does not contain
-      ;; the variable, we have to lookup runtime environment instead
-      (if (equal? ct-result 'not-found)
-          `((assign ,target
-                    (op lookup-variable-value)
-                    (const ,exp)
-                    (reg env)))
+     (if (equal? ct-result 'not-found)
+         ;; if the compile-time environment does not contain
+         ;; the variable, we have to lookup runtime environment instead
+         (make-instruction-sequence
+          '()
+          (list target 'env)
+          `( (assign env (op get-global-environment))
+             (assign ,target
+                     (op lookup-variable-value)
+                     (const ,exp)
+                     (reg env))))
+         (make-instruction-sequence
+          '(env)
+          (list target)
+          ;; if the compile-time environment does not contain
+          ;; the variable, we have to lookup runtime environment instead
           `((assign ,target (op lexical-address-lookup)
-                    (const ,ct-result)
-                    (reg env))))))))
+                        (const ,ct-result)
+                        (reg env))))))))
 
 (define (compile-assignment exp target linkage ctenv)
-  (let ((var (assignment-variable exp))
+  (let* ((var (assignment-variable exp))
         (get-value-code
          (compile (assignment-value exp) 'val 'next ctenv))
-        (ct-result (find-variable exp ctenv)))
+        (ct-result (find-variable var ctenv)))
     (end-with-linkage
      linkage
      (preserving
       '(env)
       get-value-code
-      (make-instruction-sequence
-       '(env val) (list target)
-       (if (equal? ct-result 'not-found)
-           `((perform (op set-variable-value!)
+      (if (equal? ct-result 'not-found)
+          (make-instruction-sequence
+           '(env val)
+           (list target 'env)
+           `((assign env (op get-global-environment))
+             (perform (op set-variable-value!)
                       (const ,var)
                       (reg val)
                       (reg env))
-             (assign ,target (const ok)))
+             (assign ,target (const ok))))
+          (make-instruction-sequence
+           '(env val) (list target)
            `((perform (op lexical-address-set!)
                       (const ,ct-result)
                       (reg env)
                       (reg val))
              (assign ,target (const ok)))))))))
+
+;; Local variables:
+;; proc-entry: "./exercise_5_42.scm"
+;; End:
