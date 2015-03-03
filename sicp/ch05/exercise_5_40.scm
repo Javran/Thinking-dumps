bd4011a4681628d97902fc7f63840a679d8e346b
Javran Cheng <Javran.c@gmail.com>
2015-03-02 21:25:52 -0500

    ex 5.40: TODO removed

--- sicp/ch05/exercise_5_40.scm
+++ sicp/ch05/exercise_5_40.scm
@@ -10,9 +10,15 @@
 (load "ec-tests.scm")
 (load "exercise_5_23_tests.scm")
 
-;; testcases here are for merely sanity checks.
-;; TODO: for now "ctenv" are just passed around and
-;; we really want to see if it works for "compile-lambda-body"
+;; Testcases here are for merely sanity checks.
+;; We might want some tests to see if "ctenv" gets extended properly,
+;; but the problem is that the compile-time environment - as its name suggests -
+;; is available only at compile time, to see it in action,
+;; it is more practical to write test cases in modules that uses the functionality
+;; of this one.
+;; Since "exercise_5_42.scm" is based on this module,
+;; as long as that module does not go wrong,
+;; we pretend the implementation here is good.
 (for-each
  (test-evaluator
   compile-and-run-with-env)
