d9f7791358e4007a3154a78d6d412119cb1c940f
Javran Cheng <Javran.c@gmail.com>
2015-03-05 18:25:47 -0500

    simu-ec: fixed

--- sicp/ch05/simu_ec_patch.scm
+++ sicp/ch05/simu_ec_patch.scm
@@ -10,27 +10,38 @@
 (define (to-machine-prim-entry sym)
   `(,sym ,(eval sym user-initial-environment)))
 
-(define default-ops-builder
-  (let ((old-builder default-ops-builder))
-    (lambda (m)
-      (let* ((old-ops (old-builder m))
-             (new-prim-symbols
-              ;; only add those that don't show up
-              ;; in the old primitive list ...
-              (set-diff (ec-get-required-operations)
-                        (map car old-ops))))
-        `(
-          ;; we are trying to be lazy here by:
-          ;; * extract the list of required operation names direcly
-          ;;   from the code of the evaluator
-          ;; * operation names are symbols, and as we have implemented
-          ;;   them somewhere in our toplevel user environment
-          ;;   we can evaluate them directly to convert each operation symbol
-          ;;   to its corresponding primitive entry
-          ,@(map to-machine-prim-entry new-prim-symbols)
-          (error ,(lambda args
-                    (apply error args)))
-          ,@old-ops)))))
+;; note that this is an "operation list builder modifier"
+;; since ec's ops-builder need to examine the current builder
+;; and add missing operations, rather than offering a static list
+(define (ec-ops-builder-modifier current-ops-builder)
+  (lambda (m)
+    (let* ((old-ops (current-ops-builder m))
+           (new-prim-symbols
+            ;; only add those that don't show up
+            ;; in the old primitive list ...
+            (set-diff (ec-get-required-operations)
+                      (map car old-ops))))
+      `(
+        ;; we are trying to be lazy here by:
+        ;; * extract the list of required operation names direcly
+        ;;   from the code of the evaluator
+        ;; * operation names are symbols, and as we have implemented
+        ;;   them somewhere in our toplevel user environment
+        ;;   we can evaluate them directly to convert each operation symbol
+        ;;   to its corresponding primitive entry
+        ,@(map to-machine-prim-entry new-prim-symbols)
+        (error ,(lambda args
+                  (apply error args)))
+        ,@old-ops))))
+
+(define (build-and-execute controller-text reg-bindings)
+  (build-and-execute-with
+   controller-text
+   reg-bindings
+   (ec-ops-builder-modifier
+    (ops-builder-union
+     monitor-patch-ops-builder-extra
+     default-ops-builder))))
 
 ;; use the machine to evlauate a lisp expression
 (define (machine-eval exp env)
