10c318d66e306ab1f343238ac44ca61de9370731
Javran Cheng <Javran.c@gmail.com>
2015-03-07 22:41:41 -0500

    ex 5.43: have to go deeper...

--- sicp/ch05/exercise_5_43.scm
+++ sicp/ch05/exercise_5_43.scm
@@ -4,12 +4,35 @@
 (load "simu_utils.scm")
 (load "ec-prim.scm")
 
+#|
+;; what happens when we have the following expression:
+(lambda (x)
+  (begin
+    (define y 1)
+    (define z 2)
+    (if (= x 0)
+        (begin
+          (define a 10)
+          (+ x a))
+        (begin
+          (define b 320)
+          (* x 3 b)))))
+
+;; TODO
+;; conclusion: we have to go deeper
+;; until we can reach another lambda-subexpression
+|#
+
 ;; based on exercise 4.16
 ;; only scans definitions directly appear
 ;; in the procedure body
 ;; TODO: I think we can do something recursive
 ;; to go into deeper internal definitions
 (define (scan-out-defines p-body)
+  ;; p-body is a sequence of expression
+  (define internal-definition
+    (filter definition? p-body))
+
   (define intern-def-exps
     (filter
      intern-define?
