923d755edadebc727256dda6460c592bf3539eea
Javran Cheng <Javran.c@gmail.com>
2015-03-07 22:33:47 -0500

    ex 5.43: link to libs

--- sicp/ch05/exercise_5_43.scm
+++ sicp/ch05/exercise_5_43.scm
@@ -1,32 +1,34 @@
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
-(define (scan-out-defines p-body)
-  (define (intern-define? exp)
-    (and (non-empty? exp)
-         ((list-tagged-with 'define) exp)))
+(load "simu_utils.scm")
+(load "ec-prim.scm")
 
+;; based on exercise 4.16
+;; only scans definitions directly appear
+;; in the procedure body
+;; TODO: I think we can do something recursive
+;; to go into deeper internal definitions
+(define (scan-out-defines p-body)
   (define intern-def-exps
     (filter
-      intern-define?
-      p-body))
+     intern-define?
+     p-body))
   (define intern-def-vars
     (map definition-variable intern-def-exps))
   (define intern-def-vals
     (map definition-value    intern-def-exps))
   (define (def->set exp)
-    (if (intern-define? exp)
-      `(set! ,(definition-variable exp)
-             ,(definition-value    exp))
-      exp))
+    (if (definition? exp)
+        `(set! ,(definition-variable exp)
+               ,(definition-value    exp))
+        exp))
   `(let
-     ; generate var-unassigned pairs
-     ,(map (lambda (var)
-             `(,var '*unassigned*))
-           intern-def-vars)
-     ; let body
+       ;; generate var-unassigned pairs
+       ,(map (lambda (var)
+               `(,var '*unassigned*))
+             intern-def-vars)
+     ;; let body
      ,@(map def->set p-body)))
-
-(out list-tagged-with)
 
 (end-script)
