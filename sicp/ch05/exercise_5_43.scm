534ab3b431cbdce4b425d8b6824ec4083f68633a
Javran Cheng <Javran.c@gmail.com>
2015-03-08 22:56:29 -0400

    ex 5.43: everything but lambda

--- sicp/ch05/exercise_5_43.scm
+++ sicp/ch05/exercise_5_43.scm
@@ -3,6 +3,7 @@
 
 (load "simu_utils.scm")
 (load "ec-prim.scm")
+(load "exercise_5_23_common.scm")
 
 #|
 ;; what happens when we have the following expression:
@@ -47,10 +48,64 @@
 ;; as the transformation might introduce s-expressions in derived
 ;; form which would require expansion.
 
-;; I think it's a good idea to make explicit the idea that
-;; the abstract syntax tree is a functor.
-(define (map-sexp f exp)
-  'todo)
+(define (transform-sexp exp)
+  ;; invariant:
+  ;; * the inner-expressions are always transformed before
+  ;;   its outer-expression
+  ;; * the input is a valid s-exp
+  ;;   and the output is a valid s-exp but without local definitions
+  (cond
+   ((or (self-evaluating? exp)
+        (quoted? exp)
+        (variable? exp))
+    ;; forms that couldn't contain a sub-expression,
+    ;; the transformation will just leave them unchanged
+    exp)
+   ((assignment? exp)
+    ;; (set! <var> <exp>)
+    `(set! ,(assignment-variable exp)
+           ,(transform-sexp (assignment-value exp))))
+   ((definition? exp)
+    ;; We are not going to take two cases into account.
+    ;; Instead, we "normalize" the definition so we are sure to
+    ;; deal with a normalized form later
+    ;; (this makes "lambda" the only form that the transformation cares about)
+    (let ((exp (normalize-define exp)))
+      ;; overwritten exp shadowing the original one
+      `(define
+         ,(definition-variable exp)
+         ,(transform-sexp (definition-value exp)))))
+   ((if? exp)
+    ;; (if <pred> <cons> <alt>)
+    ;; since the accessor assigns a value
+    ;; when there is no alternative expression
+    ;; the assumed syntax here is safe
+    `(if ,(transform-sexp (if-predicate exp))
+         ,(transform-sexp (if-consequent exp))
+         ,(transform-sexp (if-alternative exp))))
+   ((lambda? exp)
+    ;; here we need to:
+    ;; * scan exposed definitions
+    ;; * eliminate them
+    (error 'todo))
+   ((begin? exp)
+    ;; (begin <exp> ...)
+    `(begin ,@(map
+               transform-sexp
+               (begin-actions exp))))
+   ((cond? exp)
+    ;; well, let's desugar it
+    (transform-sexp (cond->if exp)))
+   ((let? exp)
+    ;; well, let's desugar it
+    (transform-sexp (let->combination exp)))
+   ((application? exp)
+    ;; (<exp1> <exp2s> ...)
+    `(,(transform-sexp (operator exp))
+      ,@(map transform-sexp (operands exp))))
+   (else
+    (error "invalid s-expression: "
+           exp))))
 
 (define (scan-out-defines p-body)
   ;; p-body is a sequence of expression
