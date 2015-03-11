9739f21954d97f0e6bb17e17f05114245793f4a8
Javran Cheng <Javran.c@gmail.com>
2015-03-10 22:29:10 -0400

    ex 5.43: infinite loop

--- sicp/ch05/exercise_5_43.scm
+++ sicp/ch05/exercise_5_43.scm
@@ -9,26 +9,90 @@
 (load "exercise_5_43_scan.scm")
 (load "exercise_5_43_transform.scm")
 
+(define (make-exp-from-scan-result scan-result)
+  (let* ((binding-set (car scan-result))
+         (transformed-exp (cdr scan-result)))
+    `(let ,(map (lambda (var)
+                  `(,var '*unassigned*))
+                binding-set)
+       ,transformed-exp)))
+
 ;; test scan
 (define (check-scan-consistency exp)
-  (let* ((scan-result (scan-definitions-and-transform exp))
-         (binding-set (car scan-result))
-         (transformed-exp (cdr scan-result))
-         (new-exp `(let ,(map (lambda (var)
-                                `(,var '*unassigned*))
-                              binding-set)
-                     ,transformed-exp)))
-    (pretty-print new-exp)
+  (let* ((new-exp (make-exp-from-scan-result
+                   (scan-definitions-and-transform exp))))
     (equal? (eval exp user-initial-environment)
             (eval new-exp user-initial-environment))))
 
-(out (check-scan-consistency
-      '(begin
-         (define x 10)
-         (define y 20)
-         (define (f x y)
-           (* x y))
-         (+ x y (f x y)))))
+;; "and" as a function
+;; note that since "andf" is a normal function
+;; its variable will be evaluated *before* entering
+;; the function body.
+;; therefore:
+;; (and #f (error)) is fine, but
+;; (andf #f (error)) is not.
+(define (andf . args)
+  (fold-left (lambda (a b)
+               (and a b))
+             #t
+             args))
+
+(assert
+ (andf
+  (map
+   check-scan-consistency
+   ;; test expressions
+   '(1
+     'a
+     ;; complex one
+     (begin
+       ;; definition/variable
+       (define x 10)
+       ;; variable
+       x
+       ;; begin
+       (begin
+         (define y
+           (if #t
+               20
+               40))
+         ;; assignment
+         (set! y 30))
+       ;; definition/function
+       (define (f u . vs)
+         (* u (apply + vs)))
+       ;; explicit lambda
+       (define t (lambda (x) x))
+       ;; application
+       (+ (t x)
+          ;; cond
+          (cond (#f (t y))
+                (else (t (t y))))
+          ;; let
+          (let ()
+            (define k (+ 2 x))
+            (+ k (f x y y y x)))))
+     ;; another example
+     ;; TODO: for now it's causing infinite loop...
+     ((lambda (x)
+        (begin
+          (define y 1)
+          (define z 2)
+          (if (= x 0)
+              (let ()
+                (define a 10)
+                (+ x a))
+              (let ()
+                (define b 320)
+                (* x 3 b)))))
+      200)
+     ))))
+
+(pretty-print
+ (make-exp-from-scan-result
+  (scan-and-transform-exps
+       ')))
+
 
 #|
 ;; what happens when we have the following expression:
