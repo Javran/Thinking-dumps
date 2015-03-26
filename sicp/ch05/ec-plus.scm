4d847c4bd6709208b3da86a1ae8b1c36166fccba
Javran Cheng <Javran.c@gmail.com>
2015-03-25 22:08:35 -0400

    ec-plus: splitted document

--- sicp/ch05/ec-plus.scm
+++ sicp/ch05/ec-plus.scm
@@ -1,49 +1,22 @@
 ;;; explicit control evaluator with compiled-code supports
 
-;; originally, there is an inconsistency
-;; between our evaluator and compiler:
-;; when a function is applied with arguments,
-;; the evaluator evaluates arguments from left to right while
-;; the code produces by the compiler evaluates arguments from right to left.
-;; this might lead to many confusions
-;; so the first problem to be solved is to make this consistent:
-;; we modify the compiler so that it produces code that evaluates
-;; the argument from left to right.
-;; this problem is fixed and now both compiler and evaluator
-;; will evaluate arguments from left to right
-
-;; to verify the order of argument evaluation,
-;; we can use the following expression:
-
-#|
-(begin
-  (define x 1)
-  (let ((a (begin
-             (set! x (+ x 10))
-             x))
-        (b (begin
-             (set! x (* x 2))
-             x)))
-    (cons a b)))
-|#
-
-;; the result will be:
-;; * (11 . 22) if arguments are evaluated from left to right
-;; * (12 . 2)  if arguments are evaluated from right to left
+;; see document in "ec-plus.md" for more details
 
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
 (load "simu.scm")
-;; this is actually not necessary
+;; this is actually not necessary -- TODO: more details
 (load "simu_additive-assemble_patch.scm")
 (load "simu_ec_patch.scm")
 (load "exercise_5_23_common.scm")
 (load "ec-plus-eval.scm")
 
 (load "compiler.scm")
+
 ;; apply this compiler patch
 ;; to make argument evaluation consistent
+;;(check document for more details)
 (load "exercise_5_36_compiler.scm")
 
 ;; TODO: too many comments, let's create a text file
@@ -142,6 +115,10 @@
              `(controller
                (goto (label external-entry))
                ,@evaluator-insns
+               ;; we will always append extra code to the tail
+               ;; of the previous instruction sequence
+               ;; so that the behavior is consistent with
+               ;; "additive assemble" patch.
                external-entry
                (perform (op initialize-stack))
                (assign env (op get-global-environment))
