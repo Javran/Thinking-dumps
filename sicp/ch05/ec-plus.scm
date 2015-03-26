bf6d4cdc5626e8203c85f8de70a64fdf6917ae92
Javran Cheng <Javran.c@gmail.com>
2015-03-25 22:13:28 -0400

    ec-plus: TODO: fix syntax

--- sicp/ch05/ec-plus.scm
+++ sicp/ch05/ec-plus.scm
@@ -19,6 +19,7 @@
 ;;(check document for more details)
 (load "exercise_5_36_compiler.scm")
 
+;; TODO: fix syntax errors in the document
 ;; TODO: too many comments, let's create a text file
 ;; TODO: break procedures into meaningful modules
 
