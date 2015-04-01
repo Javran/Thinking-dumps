ff6b381e8010c33e589bf798437ec3566c3f9c95
Javran Cheng <Javran.c@gmail.com>
2015-04-01 00:00:37 -0400

    ec-plus: a little more comments

--- sicp/ch05/ec-plus-machine.scm
+++ sicp/ch05/ec-plus-machine.scm
@@ -40,8 +40,14 @@
 
 (define (ec-ops-builder-modifier current-ops-builder)
   (lambda (m)
-    (let* ((old-ops (current-ops-builder m))
+    (let* ((old-ops
+            ;; step #1: passing the machine to generate
+            ;; a list of operations
+            (current-ops-builder m))
            (current-ops
+            ;; step #2: adding some more (these primitives
+            ;; usually need to know the machine object,
+            ;; thus cannot be lifted from scheme)
             `((get-global-environment
                ,(lambda ()
                   (machine-extra-get m 'global-env 'error)))
@@ -49,6 +55,8 @@
                         (apply error args)))
               ,@old-ops))
            (missing-prim-symbols
+            ;; step #3: find the list of missing operations
+            ;; and lift them from scheme
             (set-diff
              (ec-get-required-operations)
              (remove-duplicates
