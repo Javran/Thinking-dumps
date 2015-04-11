47e821188d0fc66dfb91562732141069b7660da1
Javran Cheng <Javran.c@gmail.com>
2015-04-10 22:10:12 -0400

    ex 5.48: comments & all TODOs done

--- sicp/ch05/exercise_5_48.scm
+++ sicp/ch05/exercise_5_48.scm
@@ -5,6 +5,37 @@
 (load "compiler.scm")
 (load "ec-plus.scm")
 
+;; NOTE:
+;; turns out it is not necessary to quote the expression
+;; if we add "compile" as a machine operation.
+;;
+;; In the book, it seems to suggest installing
+;; "compile-and-run" as a primitive procedure so that calling the compiler
+;; could be a function application.
+;; However, this approach did not work out for me because
+;; primitive procedure in implemented language can only produce
+;; a value, but does not have the direct ability to control the machine
+;; (however, in theory this is possible, since the machine object is there)
+
+;; here is my approach:
+;; * a special form "(compile-and-run (quote <exp))"
+;;   is added. ("exercise_5_48_eval.scm" for evaluator support,
+;;   "exercise_5_48_prim.scm" for expression predicate and accessor)
+;; * new label "ev-compile-and-run" for handling:
+;;   - it extract the expression, and call "magic-compile"
+;;     for compilation.
+;;   - the compiled instructions get assembled
+;;     and the entry point for evaluating this expression
+;;     get written to "val" register
+;;   - we proceed to call "val" register,
+;;     and "continue" register will lead us back
+;;     to complete the REPL loop.
+;; * as mentioned before new primitive machine operation
+;;   "magic-compile" is inserted in "exercise_5_48_machine.scm",
+;;   it compiles the code using "return" as linkage
+;;   (so that after the call "continue" register can
+;;   be directly lead us back)
+
 ;; based on ex 5.47
 (load "exercise_5_47_compiler.scm")
 
@@ -14,32 +45,3 @@
 
 ;; load this module to play with it dirrectly
 (compile-and-go ''())
-
-;; TODO:
-;; For now I can't see why we need an extra quote.
-;; Namely we need to implement something that runs in REPL:
-;;
-;; (compile-and-run (quote expr))
-;;
-;; but if "compile-and-run" is a special form,
-;; we don't have to quote the expression inside.
-
-;; TODO:
-;; * implement "compile-and-run" as a special form
-;; * something corresponding to "compile-and-run" as a machine primitive
-;;   (this something need to have a different name, the distinction is
-;;   important here, because we want to insert assembled instructions
-;;   into the machine object, which means lifting the primitive from
-;;   scheme directly is not going to work.)
-;; * run the code after assembling, transfer control back to REPL
-
-;; TODO: now I guess the exercise want us to implement
-;; "compile-and-run" as a function available at run time.
-;; and then the quotation makes sense. so there are many ways of doing things,
-;; let's first take the way our exercise wants and then think about other approaches.
-
-;; NOTE: I find it's difficult to implement "compile" as a
-;; primitive procedure, because after compilation,
-;; we still need to transfer control to call the newly generated code
-;; which can be tricky to make it right by allowing a primitive procedure
-;; to manipulate machine states. let's go back and try the special-form method
