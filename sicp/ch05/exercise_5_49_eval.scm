7c8a742652d755abc2269a3ebaec579c70c6e83a
Javran Cheng <Javran.c@gmail.com>
2015-04-11 22:23:20 -0400

    ex 5.49: few comments

--- sicp/ch05/exercise_5_49_eval.scm
+++ sicp/ch05/exercise_5_49_eval.scm
@@ -241,14 +241,29 @@
     (goto (reg val))
 
     ;; TODO: label name
+    ;; RCEPL for:
+    ;; Read-Compile-Execute-Print-Loop
     read-eval-print-loop
     ;; read
     (perform
      (op prompt-for-input) (const "ec-repl+> "))
     (assign exp (op read))
+
     ;; compile
+    ;; NOTE: because our "compile" operation
+    ;; is available at the level of machine operation
+    ;; when things fail to compile, it will simply
+    ;; be a machine failure. (we can still do
+    ;; better than this, because as a machine operation
+    ;; we still have the whole machine object available.
+    ;; but converting error signals to RCEPL level
+    ;; might need more assumptions.)
     (assign val (op magic-compile) (reg exp))
+
     ;; execute
+    ;; since compiling doesn't any register except "exp"
+    ;; to be set, we can delay all initialization tasks
+    ;; to here.
     (perform (op initialize-stack))
     (assign env (op get-global-environment))
     (assign continue (label print-result))
