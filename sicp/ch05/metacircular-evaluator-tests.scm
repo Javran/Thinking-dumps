d71459c92f59993dea4c852c4dde056e66f1fb70
Javran Cheng <Javran.c@gmail.com>
2015-04-19 18:54:34 -0400

    meta-circ: update comments

--- sicp/ch05/metacircular-evaluator-tests.scm
+++ sicp/ch05/metacircular-evaluator-tests.scm
@@ -1,9 +1,7 @@
-;; tests in the original metacircular should be moved to here
-;; and instead of having more fine-grained tests
-;; we will just test the behavior: if our metacircular evaluator
-;; gives the same result as the one given by the evaluator in our implementing
-;; language, then it is fine.
-
+;; all tests in the original metacircular evaluator is abandoned here.
+;; for simplicity, we will instead just test the behavior of our evaluator:
+;; we reuse tests and the tester in "ec-tests.scm", if the metacircular evaluator
+;; agrees with our evaluator in the implementing language, then it is fine.
 (load "../common/utils.scm")
 (load "../common/test-utils.scm")
 
