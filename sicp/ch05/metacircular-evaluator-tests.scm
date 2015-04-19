dcf2d019739d228bb834ed153d032d6f197cd949
Javran Cheng <Javran.c@gmail.com>
2015-04-18 21:50:10 -0400

    meta-circ: use ec-tests

--- sicp/ch05/metacircular-evaluator-tests.scm
+++ sicp/ch05/metacircular-evaluator-tests.scm
@@ -4,70 +4,19 @@
 ;; gives the same result as the one given by the evaluator in our implementing
 ;; language, then it is fine.
 
-;; TODO: I think we can just use ec-tests.
-;; as we don't necessarily need a complete set of testcases.
+(load "../common/utils.scm")
+(load "../common/test-utils.scm")
 
-(define test-exprs
-  '(
-    ;; self-evaluating & quotation
-    'abc
-    1
-    '(1 2 3)
-    "a"
-    #\a
-    '"a"
-    '1
-    ;; if-expression
-    (if 1 10)
-    (if 1 10 20)
-    (if #t 10 20)
-    (if 'a 10 20)
-    (if #f 10 20)
-    (if (= 0 1) (+ 10 20) (* 10 20))
+(load "metacircular-evaluator.scm")
 
-    ;; begin-form
-    (begin (if #f 10 20) 30)
-    (begin 1 2 3)
-    (begin 30 (if #t 10 20))
+(load "ec-tests.scm")
 
-    ;; cond-form
-    (begin
-      (define a 0)
-      (cond ((= a 0) 2)
-            ((= a 1) 1)
-            ((= a 2) 0)))
-    (begin
-      (define a 2)
-      (cond ((= a 0) 2)
-            ((= a 1) 1)
-            ((= a 2) 0)))
-    (begin
-      (define a 1)
-      (cond ((= a 0) => (lambda (x) (if x 10 20)))
-            ((= a 1) => (lambda (x) (if x 30 40)))
-            ((= a 2) 50)
-            (else 60)))
-    (begin
-      (define a 3)
-      (cond ((= a 0) => (lambda (x) (if x 10 20)))
-            ((= a 1) => (lambda (x) (if x 30 40)))
-            ((= a 2) 50)
-            (else 60)))
-    ;; env functionality
-    (+ 1 2 3)
-    (- 7 1 2 3)
-    (/ 1024 256)
-    (= 1 1)
-    (= 1 0)
-    (zero? 0)
-    (eq? 'a 'a)
-    (eq? 'a 'b)
-    (= (* 1 2 3 4) (* 2 (+ 10 2)))
-    (cons 1 '(2 3))
-    (cons 'a 'b)
-    (car '(1 2))
-    (cdr '(1 2))
-    (null? '())
-    (null? '(1))
-    (list 'a 'b)
-    ))
+(my-eval-select-approach 'interpret)
+(for-each
+ (test-evaluator my-eval)
+ test-exps) (newline)
+
+(my-eval-select-approach 'analyze)
+(for-each
+ (test-evaluator my-eval)
+ test-exps) (newline)
