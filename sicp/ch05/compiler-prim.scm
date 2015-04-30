79247e910f52f0c9507703717af625f1c3d71a77
Javran Cheng <Javran.c@gmail.com>
2015-04-29 22:30:37 -0400

    compiler: and/or transformation

--- sicp/ch05/compiler-prim.scm
+++ sicp/ch05/compiler-prim.scm
@@ -1,8 +1,5 @@
 ;;; primitives for the compiler
 
-(load "../common/utils.scm")
-(load "../common/test-utils.scm")
-
 ;; import primitives from our explicit control evaluator
 ;; NOTE: the primitives are more than necessary,
 ;; for example "prompt-for-input" is defined
@@ -12,10 +9,11 @@
 ;; include syntax extensions from ex 5.23
 (load "exercise_5_23_common.scm")
 
-;; TODO: developing and/or transformation inplace...
+;; TODO: tests
+(define and? (list-tagged-with 'and))
+(define or? (list-tagged-with 'or))
 
-
-;; based on exercise 4.4,
+;; based on exercise 4.4
 (define (and->if exp)
   (assert (eq? 'and (car exp)))
   (define (exprs->if exps)
@@ -35,19 +33,23 @@
            #f))))
   (exprs->if (cdr exp)))
 
-
 (define (or->if exp)
-  (cond ((null? exp)
-         #f)
-        ((null? (cdr exp))
-         (car exp))
-        (else
-         (let ((result-sym (gensym)))
-           (list
-            (list 'lambda (list result-sym)
-                  (list 'if result-sym
-                        result-sym
-                        (list 'or (or->if (cdr exp)))))
-            (car exp))))))
-
-(pp (and->if '(and 1 2 3 4 5)))
+  (assert (eq? 'or (car exp)))
+  (define (exprs->if exps)
+    (cond
+     ((null? exps)
+      #t)
+     ((null? (cdr exps))
+      (car exps))
+     (else
+      ;; here we need to store the value
+      ;; or otherwise the same expression
+      ;; will be evaluated twice
+      ;; (or (begin (display 'x) 1)) for example
+      (let ((result-sym (gensym)))
+        `((lambda (,result-sym)
+            (if ,result-sym
+                ,result-sym
+                ,(exprs->if (cdr exps))))
+          ,(car exps))))))
+  (exprs->if (cdr exp)))
