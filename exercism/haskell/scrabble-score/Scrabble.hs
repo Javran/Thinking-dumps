c5e1cf80d9937edda92362f38da7482777c468cf
Javran Cheng <Javran.c@gmail.com>
2015-05-10 00:20:34 -0400

    update according to suggestions

--- exercism/haskell/scrabble-score/Scrabble.hs
+++ exercism/haskell/scrabble-score/Scrabble.hs
@@ -1,24 +1,37 @@
+{-# LANGUAGE TupleSections #-}
 module Scrabble
     ( scoreLetter
     , scoreWord
     )
 where
 
-import Data.Char (toLower)
+import Data.Char
+import qualified Data.Array.Unboxed as UA
 
 scoreLetter :: Char -> Int
 scoreLetter = value . toLower
-    where
-        value x
-            | oneOf "aeioulnrst" = 1
-            | oneOf "dg"         = 2
-            | oneOf "bcmp"       = 3
-            | oneOf "fhvwy"      = 4
-            | oneOf "k"          = 5
-            | oneOf "jx"         = 8
-            | oneOf "qz"         = 10
-            | otherwise          = undefined
-            where oneOf = (x `elem`)
+  where
+    value x =
+      if UA.inRange scoreBnd x
+        then scoreArr UA.! x
+        else 0
+
+scoreBnd :: (Char,Char)
+scoreBnd = ('a','z')
+
+scoreArr :: UA.UArray Char Int
+scoreArr = UA.array scoreBnd funcDef
+  where
+    inp ~= val = map (,val) inp
+    funcDef = concat
+      [ "aeioulnrst" ~= 1
+      , "dg"         ~= 2
+      , "bcmp"       ~= 3
+      , "fhvwy"      ~= 4
+      , "k"          ~= 5
+      , "jx"         ~= 8
+      , "qz"         ~= 10
+      ]
 
 scoreWord :: String -> Int
 scoreWord = sum . map scoreLetter
