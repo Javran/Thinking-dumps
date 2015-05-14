2601d79ee5d6e790e8de594e71fef7d1f331aa44
Javran Cheng <Javran.c@gmail.com>
2015-05-13 20:54:59 -0400

    token type and payload

--- sicp/ch05/exercise_5_51/evaluator.h
+++ sicp/ch05/exercise_5_51/evaluator.h
@@ -1,6 +1,24 @@
 #ifndef _EVALUATOR_H_
 #define _EVALUATOR_H_
 
+typedef enum {
+    tok_eof,
+    tok_lparen,
+    tok_rparen,
+    tok_quote,
+    tok_true,
+    tok_false,
+    tok_string,
+    tok_symbol,
+    tok_integer
+} TokenType;
+
+typedef union {
+    char *string_content;
+    char *symbol_name;
+    long int *integer_content;
+} TokenFields;
+
 // syntax related functions
 int isLetter(char);
 int isSpecialInitial(char);
