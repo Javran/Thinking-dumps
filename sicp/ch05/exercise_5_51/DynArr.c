1674a767d5a54302b45380c9027fe389000d24d9
Javran Cheng <Javran.c@gmail.com>
2015-05-16 22:33:53 -0400

    ex 5.51: dynarr, structure

--- sicp/ch05/exercise_5_51/DynArr.c
+++ sicp/ch05/exercise_5_51/DynArr.c
@@ -1,5 +1,7 @@
 // a simple self-adjusting array
 
+#include <stdlib.h>
+
 // TODO: about free:
 // so far I'm coding but haven't make
 // any principle about coding in C.
@@ -10,3 +12,16 @@
 //   for freeing the objects' reachable inner structures.
 //   and the responsiblity of freeing the object itself
 //   should be taken care by the caller to this free procedure.
+
+typedef struct {
+    // base pointer for this dynamic array
+    void *base;
+    // size of each element
+    // should always be a result of sizeof operator
+    // or we might get into alignment-related troubles
+    size_t elemSize;
+    // number of elements currently have
+    unsigned int elemMax;
+    // current capacity
+    unsigned int elemCap;
+} DynArr;
