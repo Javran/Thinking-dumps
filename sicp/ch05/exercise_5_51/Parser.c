50436f96efe6c147203ed51d19383ea052af71d0
Javran Cheng <Javran.c@gmail.com>
2015-05-22 16:40:10 -0400

    ex 5.51: parser, draft.

--- sicp/ch05/exercise_5_51/Parser.c
+++ sicp/ch05/exercise_5_51/Parser.c
@@ -7,3 +7,25 @@
 SExp *parseList(DynArr *tl, void *it) {
     return NULL;
 }
+
+SExp *parseAtom(DynArr *tl, void *it) {
+    // TODO: iterator should make progress
+    // TODO: parsing state
+    Token *p = it;
+    switch (p->tag) {
+    case tokInteger:
+        return newInteger(p->fields.integerContent);
+    case tokSymbol:
+        return newSymbol(p->fields.symbolName);
+    case tokTrue:
+        return newBool(1);
+    case tokFalse:
+        return newBool(0);
+    case tokString:
+        return newString(p->fields.stringContent);
+    default:
+        // TODO
+        assert(0);
+    }
+    return NULL;
+}
