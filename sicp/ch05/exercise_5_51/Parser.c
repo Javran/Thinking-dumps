f261544b8ac57c94445e64dd25114d3040c6e781
Javran Cheng <Javran.c@gmail.com>
2015-05-22 16:33:11 -0400

    ex 5.51: parser interface?

---
+++ sicp/ch05/exercise_5_51/Parser.c
@@ -0,0 +1,9 @@
+#include "Common.h"
+#include "DynArr.h"
+#include "Token.h"
+#include "SExp.h"
+
+// accepts token list and an iterator
+SExp *parseList(DynArr *tl, void *it) {
+    return NULL;
+}
