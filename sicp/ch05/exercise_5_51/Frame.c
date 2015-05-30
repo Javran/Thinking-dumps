c5754195f9356855c476967d4415112b8c85637d
Javran Cheng <Javran.c@gmail.com>
2015-05-30 18:06:37 -0400

    ex 5.51: frame

---
+++ sicp/ch05/exercise_5_51/Frame.c
@@ -0,0 +1,5 @@
+// a frame contains environment bindings,
+// * keys are unique
+// * deletion is not allowed
+// therefore ... we can use DynArr
+// search functionality can be handled through a left-fold
