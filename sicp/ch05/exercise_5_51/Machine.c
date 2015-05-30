4bd5a394b8e3987e8ea46b86bc148236e8acfbfe
Javran Cheng <Javran.c@gmail.com>
2015-05-30 17:40:32 -0400

    ex 5.51: handle self-evaluating

--- sicp/ch05/exercise_5_51/Machine.c
+++ sicp/ch05/exercise_5_51/Machine.c
@@ -11,12 +11,34 @@ typedef struct {
 } Machine;
 
 // S-expresion related.
-typedef char (*SExpPredicate)(SExp *);
+typedef char (*SExpPredicate)(const SExp *);
 typedef void (*SExpEval)(Machine *);
 
 // an s-expression handler
 // consists of a predicate and an evaluator
 typedef struct {
+    // invariant: the pointer can never be NULL
     SExpPredicate pred;
+    // invariant: the s-exp must meet the requirement
+    // that "pred" has specified
     SExpEval eval;
 } SExpHandler;
+
+char isSelfEvaluating(const SExp *p) {
+    switch (p->tag) {
+    case sexpInteger:
+    case sexpString:
+    case sexpBool:
+        return 1;
+    default:
+        return 0;
+    }
+}
+
+void evalSelfEvaluating(Machine *m) {
+    m->val = m->exp;
+}
+
+SExpHandler selfEvaluatingHandler = {
+    isSelfEvaluating,
+    evalSelfEvaluating };
