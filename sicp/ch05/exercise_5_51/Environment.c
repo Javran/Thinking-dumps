ae06436f7eea0809e3e35fb633d2396ccda2890a
Javran Cheng <Javran.c@gmail.com>
2015-06-04 18:08:12 -0400

    ex 5.51: environment impl

--- sicp/ch05/exercise_5_51/Environment.c
+++ sicp/ch05/exercise_5_51/Environment.c
@@ -9,7 +9,7 @@ typedef struct Environment {
     // * following parent recursively should
     //   not get stuck in a loop
     Frame *frame;
-    struct Environment *parent;
+    const struct Environment *parent;
 } Environment;
 
 void envInit(Environment *env) {
@@ -19,6 +19,23 @@ void envInit(Environment *env) {
             && !env->parent );
     env->frame = calloc(1, sizeof(Frame));
     frameInit(env->frame);
+}
+
+// set or unset parent
+void envSetParent(Environment *env, const Environment *parent) {
+    env->parent = parent;
+}
+
+FrameEntry *envLookup(const Environment *env, const char *keyword) {
+    if (NULL == env) return NULL;
+    FrameEntry *result = frameLookup(env->frame,keyword);
+    return result
+        ? result
+        : envLookup(env->parent, keyword);
+}
+
+void envInsert(Environment *env, const char *key, void *val) {
+    frameInsert(env->frame,key,val);
 }
 
 void envFree(Environment *env) {
