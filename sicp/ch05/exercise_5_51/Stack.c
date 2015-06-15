96a480865221fcb690db5abc71e7338089431705
Javran Cheng <Javran.c@gmail.com>
2015-06-14 21:16:32 -0400

    ex 5.51: stack interface

---
+++ sicp/ch05/exercise_5_51/Stack.c
@@ -0,0 +1,8 @@
+#include "Stack.h"
+
+// void stackInit(Stack *, size_t);
+// void *stackPush(Stack *);
+// void *stackTop(Stack *);
+// void stackPop(Stack *);
+// char stackIsEmpty(Stack *);
+// void stackFree(Stack *);
--- sicp/ch05/exercise_5_51/Stack.h
+++ sicp/ch05/exercise_5_51/Stack.h
@@ -8,4 +8,11 @@ typedef struct {
     int size;
 } Stack;
 
+void stackInit(Stack *, size_t);
+void *stackPush(Stack *);
+void *stackTop(Stack *);
+void stackPop(Stack *);
+char stackIsEmpty(Stack *);
+void stackFree(Stack *);
+
 #endif
