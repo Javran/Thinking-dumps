f837c393de1e4614cad7df11e83a22df5dd08d83
Javran Cheng <Javran.c@gmail.com>
2015-06-15 21:07:28 -0400

    ex 5.51: ev-sequence?

--- sicp/ch05/exercise_5_51/Machine.c
+++ sicp/ch05/exercise_5_51/Machine.c
@@ -3,19 +3,38 @@
 #include "Environment.h"
 #include "Machine.h"
 
+void evalDispatch(Machine *);
 
 char isDefinition(const SExp *p) {
     return sexpPair == p->tag
         && isSymbol("define", p->fields.pairContent.car);
 }
 
+// the last element of an S-exp is of form (a . ())
+char isLastExp(const SExp *e) {
+    return sexpPair == e->tag
+        && sexpNil == e->fields.pairContent.cdr->tag;
+}
+
 // correponding to ev-sequence
 // requires unev to store the sequence of expressions
 void evSequence(Machine *m) {
     SExp *firstExp = m->unev.data.asSExp->fields.pairContent.car;
-    // save?
-    // we need a stack.
-    // TODO: wrap dynarr to implement stack?
+    m->exp.tag = regSExp;
+    m->exp.data.asSExp = firstExp;
+    if (isLastExp(m->unev.data.asSExp)) {
+        // I am actually just hoping the compiler
+        // can do the optimization so I do not have to
+        // manipulate "continue" myself.
+        evalDispatch(m);
+        return;
+    } else {
+        evalDispatch(m);
+        m->unev.tag = regSExp;
+        m->unev.data.asSExp = m->unev.data.asSExp->fields.pairContent.cdr;
+        evSequence(m);
+        return;
+    }
 }
 
 void evDefinition(Machine *m) {
