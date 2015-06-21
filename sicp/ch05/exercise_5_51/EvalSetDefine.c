421979785db52184ee330741188769d3279975b5
Javran Cheng <Javran.c@gmail.com>
2015-06-20 20:56:45 -0400

    ex 5.51: define impl (partial)

--- sicp/ch05/exercise_5_51/EvalSetDefine.c
+++ sicp/ch05/exercise_5_51/EvalSetDefine.c
@@ -15,28 +15,17 @@ void evAssignment(const SExp *exp, Machine *m) {
     // envInsert(env, varName, ???)
 }
 
+char isDefinition(const SExp *p) {
+    return sexpPair == p->tag
+        && isSymbol("define", sexpCar(p));
+}
 
-/*
-
-
-(define assignment-variable cadr)
-(define assignment-value caddr)
-
-(define (definition? exp)
-  (tagged-list? exp 'define))
-(define (definition-variable exp)
-  (if (symbol? (cadr exp))
-    ; form: (define <var> <value>)
-    (cadr exp)
-    ; `(cadr exp)` is a list
-    (caadr exp)))
-(define (definition-value exp)
-  (if (symbol? (cadr exp))
-    ;; first form
-    (caddr exp)
-    ;; second form, not supported here
-    ;; however, exercise 5.23 provides a desugarizer
-    ;; that transforms this second form into the first form
-    ;; see "normalize-define" in "./exercise_5_23_trans.scm"
-    (error "not supported")))
-*/
+// evaluate normalized definitions
+void evNormalizedDefinition(const SExp *exp, Machine *m) {
+    // form: (define <var> <value>)
+    char *varName = sexpCadr( exp )->fields.symbolName;
+    SExp *expVal = sexpCddr( exp );
+    Environment *env = m->env.data.asEnv;
+    evalDispatch(expVal, m);
+    // TODO?
+}
