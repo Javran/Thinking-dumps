3919d492f18853302e0b22d0c4a73d02dcab185c
Javran Cheng <Javran.c@gmail.com>
2015-06-23 22:23:56 -0400

    ex 5.51: handlers put together

--- sicp/ch05/exercise_5_51/Machine.c
+++ sicp/ch05/exercise_5_51/Machine.c
@@ -3,6 +3,26 @@
 #include "Environment.h"
 #include "Machine.h"
 
+#include "EvalSimple.h"
+#include "EvalCond.h"
+#include "EvalSeq.h"
+#include "EvalSetDefine.h"
+
+SExpHandler *evalHandlers[] = {
+    &selfEvaluatingHandler,
+    &variableHandler,
+    &quotedHandler,
+    &assignmentHandler,
+    &definitionHandler,
+    &ifHandler,
+    &lambdaHandler,
+    &beginHandler
+    // TODO: application missing
+};
+
+const size_t evalHandlerCount =
+    sizeof(evalHandlers) / sizeof(SExpHandler *);
+
 // TODO:
 // * definition
 // * assignment
@@ -16,4 +36,14 @@
 // I still think we can benefit from this.
 void evalDispatch(const SExp *exp, Machine *m) {
     // INVARIANT: every branch should end with a return
+    size_t i;
+    for (i=0; i<evalHandlerCount; ++i) {
+        SExpHandler *h = evalHandlers[i];
+        if (h->pred(exp)) {
+            h->eval(exp,m);
+            return;
+        }
+    }
+
+    // TODO: not handled expressions
 }
