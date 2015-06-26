d6c9460ac85248c0af94e55b7ed004ecac15c016
Javran Cheng <Javran.c@gmail.com>
2015-06-25 21:10:56 -0400

    ex 5.51: might be able to get rid of val and env

--- sicp/ch05/exercise_5_51/Machine.h
+++ sicp/ch05/exercise_5_51/Machine.h
@@ -7,6 +7,8 @@ typedef struct {
     // TODO: potential memory leak might be caused
     // when the old value of a register
     // is not deallocated properly?
+    // TODO: it might be possible to totally get rid of "env" and "val"
+    // registers since they don't, and shouldn't be something available globally
     Register env,val;
     Register cont,proc,argl;
     // TODO: by being aware of the fact that we
