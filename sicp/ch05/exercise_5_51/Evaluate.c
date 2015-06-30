75ac03aacb5153677ebd42d0cd2a3e165c30373e
Javran Cheng <Javran.c@gmail.com>
2015-06-29 22:20:52 -0400

    ex 5.51: evaluate interface

---
+++ sicp/ch05/exercise_5_51/Evaluate.c
@@ -0,0 +1,17 @@
+#include "Evaluate.h"
+// TODO: eventually we will replace Machine by Evaluate
+// eliminating the simulated machine in favor of the built-in
+// abstract machine of c language itself
+
+SExp *evalProgramText(const char *programText, FILE *errF) {
+    DynArr *pSExpList = parseSExps(programText, errF);
+    // it is guaranteed that parseStateCurrent always produces
+    // a valid pointer. no check is necessary.
+    char parseFailed = !pSExpList;
+    if (!parseFailed) {
+        // now we are ready for interpreting these s-expressions
+    }
+    // releasing resources
+    freeSExps(pSExpList);
+    return NULL;
+}
---
+++ sicp/ch05/exercise_5_51/Evaluate.h
@@ -0,0 +1,10 @@
+#include "Common.h"
+#include "Parser.h"
+#include "Machine.h"
+
+#ifndef _JAVEV_EVALUATE_H_
+#define _JAVEV_EVALUATE_H_
+
+SExp *evalProgramText(const char *, FILE *);
+
+#endif
