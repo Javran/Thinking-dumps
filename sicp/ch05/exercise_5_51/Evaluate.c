b45137bfe6f58f02aa36552719b2827292de51ec
Javran Cheng <Javran.c@gmail.com>
2015-07-04 20:51:38 -0400

    ex 5.51: pointer manager setup

--- sicp/ch05/exercise_5_51/Evaluate.c
+++ sicp/ch05/exercise_5_51/Evaluate.c
@@ -1,8 +1,9 @@
+#include "PointerManager.h"
 #include "Evaluate.h"
+
 // TODO: eventually we will replace Machine by Evaluate
 // eliminating the simulated machine in favor of the built-in
 // abstract machine of c language itself
-
 SExp *evalProgramText(const char *programText, FILE *errF) {
     DynArr *pSExpList = parseSExps(programText, errF);
     // it is guaranteed that parseStateCurrent always produces
@@ -10,6 +11,10 @@ SExp *evalProgramText(const char *programText, FILE *errF) {
     char parseFailed = !pSExpList;
     if (!parseFailed) {
         // now we are ready for interpreting these s-expressions
+        pointerManagerInit();
+
+
+        pointerManagerFinalize();
     }
     // releasing resources
     freeSExps(pSExpList);
