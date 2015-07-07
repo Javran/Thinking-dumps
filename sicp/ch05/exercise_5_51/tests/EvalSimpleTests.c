b732508400149ff781542a3b7ba95314e4badd5c
Javran Cheng <Javran.c@gmail.com>
2015-07-07 19:52:18 -0400

    ex 5.51: string example

--- sicp/ch05/exercise_5_51/tests/EvalSimpleTests.c
+++ sicp/ch05/exercise_5_51/tests/EvalSimpleTests.c
@@ -1,34 +1,41 @@
 #include <stdlib.h>
 #include <check.h>
 #include "../Evaluate.h"
+#include "../EvalSimple.h"
 
 // evaluating simple expressions
 START_TEST (test_EvalSimple_int) {
     DynArr *pSExpList = parseSExps("1234", stderr);
     ck_assert_ptr_ne(pSExpList, NULL);
     ck_assert_int_eq(dynArrCount(pSExpList), 1);
-    // TODO: environment?
 
     SExp expect = { sexpInteger, { .integerContent  = 1234} };
-    SExp **pActual = dynArrBegin(pSExpList);
-    ck_assert(isSExpEqual(&expect,*pActual));
+    SExp **pExp = dynArrBegin(pSExpList);
+    // TODO: environment?
+    const SExp *result = evSelfEval(*pExp, NULL);
 
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert(isSExpEqual(&expect,result));
     freeSExps(pSExpList);
 } END_TEST
 
-/*
+
 START_TEST (test_EvalSimple_str) {
     DynArr *pSExpList = parseSExps("\"string!\"", stderr);
     ck_assert_ptr_ne(pSExpList, NULL);
     ck_assert_int_eq(dynArrCount(pSExpList), 1);
 
     SExp expect = { sexpString, { .stringContent = "string!"} };
-    SExp **pActual = dynArrBegin(pSExpList);
-    ck_assert(isSExpEqual(&expect,*pActual));
+    SExp **pExp = dynArrBegin(pSExpList);
+    // TODO: environment?
+    const SExp *result = evSelfEval(*pExp, NULL);
 
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert(isSExpEqual(&expect,result));
     freeSExps(pSExpList);
 } END_TEST
 
+/*
 START_TEST (test_EvalSimple_quote1) {
     DynArr *pSExpList = parseSExps("(quote quoted)", stderr);
     ck_assert_ptr_ne(pSExpList, NULL);
@@ -49,7 +56,7 @@ Suite * evalSimpleSuite(void) {
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_EvalSimple_int);
-//    tcase_add_test(tc_core, test_EvalSimple_str);
+    tcase_add_test(tc_core, test_EvalSimple_str);
 //    tcase_add_test(tc_core, test_EvalSimple_quote1);
 
     s = suite_create("EvalSimple");
