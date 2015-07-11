774a81fe48cf74074568d56ead669e505cf67cbc
Javran Cheng <Javran.c@gmail.com>
2015-07-10 23:43:11 -0400

    ex 5.51: tests for non-false

--- sicp/ch05/exercise_5_51/tests/EvalCondTests.c
+++ sicp/ch05/exercise_5_51/tests/EvalCondTests.c
@@ -9,7 +9,39 @@ START_TEST (test_EvalCond_true) {
     ck_assert_ptr_ne(pSExpList, NULL);
     ck_assert_int_eq(dynArrCount(pSExpList), 1);
 
-    SExp expect = { sexpInteger, { .integerContent  = 1234} };
+    SExp expect = { sexpInteger, { .integerContent = 1234} };
+    SExp **pExp = dynArrBegin(pSExpList);
+    const SExp *result = evIf(*pExp, NULL);
+
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert(isSExpEqual(&expect,result));
+    freeSExps(pSExpList);
+} END_TEST
+
+// if-expression, false-branch
+START_TEST (test_EvalCond_false) {
+    DynArr *pSExpList = parseSExps("(if #f 1234 5678)", stderr);
+    ck_assert_ptr_ne(pSExpList, NULL);
+    ck_assert_int_eq(dynArrCount(pSExpList), 1);
+
+    SExp expect = { sexpInteger, { .integerContent = 5678} };
+    SExp **pExp = dynArrBegin(pSExpList);
+    const SExp *result = evIf(*pExp, NULL);
+
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert(isSExpEqual(&expect,result));
+    freeSExps(pSExpList);
+} END_TEST
+
+// if-expression, non-false-branch
+// non-false is anything not equal to "#f", which should be considered
+// true
+START_TEST (test_EvalCond_non_false) {
+    DynArr *pSExpList = parseSExps("(if '(a b c) 1234 5678)", stderr);
+    ck_assert_ptr_ne(pSExpList, NULL);
+    ck_assert_int_eq(dynArrCount(pSExpList), 1);
+
+    SExp expect = { sexpInteger, { .integerContent = 1234} };
     SExp **pExp = dynArrBegin(pSExpList);
     const SExp *result = evIf(*pExp, NULL);
 
@@ -25,6 +57,8 @@ Suite * evalCondSuite(void) {
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_EvalCond_true);
+    tcase_add_test(tc_core, test_EvalCond_false);
+    tcase_add_test(tc_core, test_EvalCond_non_false);
 
     s = suite_create("EvalCond");
     suite_add_tcase(s, tc_core);
