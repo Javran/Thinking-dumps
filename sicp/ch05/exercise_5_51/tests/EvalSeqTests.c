617d25594599f53a74df519d821f30b19e1a8b7e
Javran Cheng <Javran.c@gmail.com>
2015-07-11 20:45:44 -0400

    ex 5.51: EvalSeq test

---
+++ sicp/ch05/exercise_5_51/tests/EvalSeqTests.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <check.h>
+#include "../Evaluate.h"
+#include "../EvalSeq.h"
+
+// with one expression
+START_TEST (test_EvalSeq_one) {
+    DynArr *pSExpList = parseSExps("(begin 1234)", stderr);
+    ck_assert_ptr_ne(pSExpList, NULL);
+    ck_assert_int_eq(dynArrCount(pSExpList), 1);
+
+    SExp expect = { sexpInteger, { .integerContent  = 1234} };
+    SExp **pExp = dynArrBegin(pSExpList);
+    // note that NULL is now a valid environment with no bindings
+    const SExp *result = evSelfEval(*pExp, NULL);
+
+    ck_assert_ptr_ne(result, NULL);
+    ck_assert(isSExpEqual(&expect,result));
+    freeSExps(pSExpList);
+} END_TEST
+
+Suite * evalSeqSuite(void) {
+    Suite *s;
+    TCase *tc_core;
+
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, test_EvalSeq_one);
+
+    s = suite_create("EvalSeq");
+    suite_add_tcase(s, tc_core);
+    return s;
+}
