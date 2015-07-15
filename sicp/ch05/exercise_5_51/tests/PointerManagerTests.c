a3096a3e466859737a8c753629c7f1bee247b60c
Javran Cheng <Javran.c@gmail.com>
2015-07-14 21:57:29 -0400

    ex 5.51: passing tests

--- sicp/ch05/exercise_5_51/tests/PointerManagerTests.c
+++ sicp/ch05/exercise_5_51/tests/PointerManagerTests.c
@@ -12,6 +12,35 @@ START_TEST (test_PointerManager_default_release) {
     pointerManagerFinalize();
 } END_TEST
 
+typedef struct {
+    void *a;
+    void *b;
+} TestResource;
+
+TestResource *newTestResource() {
+    TestResource *retVal = calloc(1,sizeof(TestResource));
+    retVal->a = malloc(16);
+    retVal->b = malloc(32);
+    return retVal;
+}
+
+void freeResource(TestResource *h) {
+    free(h->a); free(h->b);
+    free(h);
+}
+
+START_TEST (test_PointerManager_custom) {
+    pointerManagerInit();
+    pointerManagerRegisterCustom(newTestResource(),
+                                 (PFreeCallback)freeResource);
+    pointerManagerRegister(malloc(4000));
+    pointerManagerRegister(malloc(6000));
+    pointerManagerRegister(malloc(6000));
+    pointerManagerRegister(malloc(2000));
+    pointerManagerRegister(malloc(20));
+    pointerManagerFinalize();
+} END_TEST
+
 Suite * pointerManagerSuite(void) {
     Suite *s;
     TCase *tc_core;
@@ -19,6 +48,7 @@ Suite * pointerManagerSuite(void) {
     tc_core = tcase_create("Core");
 
     tcase_add_test(tc_core, test_PointerManager_default_release);
+    tcase_add_test(tc_core, test_PointerManager_custom);
 
     s = suite_create("PointerManager");
     suite_add_tcase(s, tc_core);
