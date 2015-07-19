6a0c4d25ee19587cbbdf1ada4b3f15e2471d3927
Javran Cheng <Javran.c@gmail.com>
2015-07-18 21:03:10 -0400

    ex 5.51: application predicate

--- sicp/ch05/exercise_5_51/EvalApp.c
+++ sicp/ch05/exercise_5_51/EvalApp.c
@@ -1,7 +1,14 @@
 #include "EvalApp.h"
 
+// an application is a non-empty proper list
 char isApplication(const SExp *p) {
-
+    // application should be the last handler
+    // at this point we can tell for sure that "p" is not nil
+    // so the only thing to do here is to test
+    // whether p is a proper list
+    return sexpNil == p->tag
+        || (sexpPair == p->tag &&
+            isApplication(sexpCdr(p)));
 }
 
 const SExp *evApplication(const SExp *exp, Environment *env) {
