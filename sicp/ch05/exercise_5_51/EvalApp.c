0a2c2ba7f5b80b230b7b9c8c22f6aa592843f17f
Javran Cheng <Javran.c@gmail.com>
2015-07-19 15:26:38 -0400

    ex 5.51: explicit loop ...

--- sicp/ch05/exercise_5_51/EvalApp.c
+++ sicp/ch05/exercise_5_51/EvalApp.c
@@ -6,13 +6,12 @@ char isApplication(const SExp *p) {
     // at this point we can tell for sure that "p" is not nil
     // so the only thing to do here is to test
     // whether p is a proper list
-    return sexpNil == p->tag
-        || (sexpPair == p->tag &&
-            isApplication(sexpCdr(p)));
+    while (sexpPair == p->tag)
+        p = sexpCdr(p);
+    return sexpNil == p->tag;
 }
 
 const SExp *evApplication(const SExp *exp, Environment *env) {
-
 }
 
 SExpHandler applicationHandler = {
