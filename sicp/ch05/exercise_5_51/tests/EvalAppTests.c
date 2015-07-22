f586b1e36ec9307c17b65302d42622c8a1a71be7
Javran Cheng <Javran.c@gmail.com>
2015-07-21 21:31:29 -0400

    ex 5.51: second example

--- sicp/ch05/exercise_5_51/tests/EvalAppTests.c
+++ sicp/ch05/exercise_5_51/tests/EvalAppTests.c
@@ -5,8 +5,26 @@
 #include "../PointerManager.h"
 
 // simple application, identity function
-START_TEST (test_EvalApp_simple) {
+START_TEST (test_EvalApp_simple1) {
     DynArr *pSExpList = parseSExps("((lambda (x) x) 1234)", stderr);
+    ck_assert_ptr_ne(pSExpList, NULL);
+    ck_assert_int_eq(dynArrCount(pSExpList), 1);
+
+    SExp expect = { sexpInteger, { .integerContent = 1234} };
+    SExp **pExp = dynArrBegin(pSExpList);
+    pointerManagerInit();
+    Environment env = {0};
+    envInit(&env);
+    const SExp *actual = evApplication(*pExp, &env);
+    ck_assert(isSExpEqual(actual, &expect));
+    envFree(&env);
+    pointerManagerFinalize();
+    freeSExps(pSExpList);
+} END_TEST
+
+// simple application, const function this time
+START_TEST (test_EvalApp_simple2) {
+    DynArr *pSExpList = parseSExps("(((lambda (x) (lambda (y) x)) 1234) 4567)", stderr);
     ck_assert_ptr_ne(pSExpList, NULL);
     ck_assert_int_eq(dynArrCount(pSExpList), 1);
 
@@ -28,7 +46,8 @@ Suite * evalAppSuite(void) {
 
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_EvalApp_simple);
+    tcase_add_test(tc_core, test_EvalApp_simple1);
+    tcase_add_test(tc_core, test_EvalApp_simple2);
 
     s = suite_create("EvalApp");
     suite_add_tcase(s, tc_core);
