ba240587d8736ecfcc87a4103649435798ed5f68
Javran Cheng <Javran.c@gmail.com>
2015-07-22 22:17:37 -0400

    ex 5.51: rearranging some code...

--- sicp/ch05/exercise_5_51/FunctionObject.c
+++ sicp/ch05/exercise_5_51/FunctionObject.c
@@ -1,31 +1,3 @@
-#include "SExp.h"
-// every function and lambda expression
-// is eventually represented in a FunctionObject,
-// which can either be a primitive operation or
-// an lambda expression (with closure)
+#include "FunctionObject.h"
 
-typedef enum {
-    funcPrim,
-    funcCompound
-} FuncObjTag;
 
-typedef struct {
-    SExp *parameters;
-    SExp *body;
-    Environment *env;
-} FuncCompound;
-
-// a primtive function handler consumes a S-expression
-// of proper list
-// TODO: consider converting it to list which might work better?
-typedef const SExp * (*FuncPrimHandler)(const SExp *);
-
-typedef union {
-    FuncPrimHandler primHdlr;
-    FuncCompound compObj;
-} FuncFields;
-
-typedef struct {
-    FuncObjTag tag;
-
-} FuncObj;
---
+++ sicp/ch05/exercise_5_51/FunctionObject.h
@@ -0,0 +1,37 @@
+#ifndef JAVEV_FUNCTIONOBJECT_H
+#define JAVEV_FUNCTIONOBJECT_H
+
+#include "SExp.h"
+
+// every function and lambda expression
+// is eventually represented in a FunctionObject,
+// which can either be a primitive operation or
+// an lambda expression (with closure)
+
+typedef enum {
+    funcPrim,
+    funcCompound
+} FuncObjTag;
+
+typedef struct {
+    SExp *parameters;
+    SExp *body;
+    Environment *env;
+} FuncCompound;
+
+// a primtive function handler consumes a S-expression
+// of proper list
+// TODO: consider converting it to list which might work better?
+typedef const SExp * (*FuncPrimHandler)(const SExp *);
+
+typedef union {
+    FuncPrimHandler primHdlr;
+    FuncCompound compObj;
+} FuncFields;
+
+typedef struct {
+    FuncObjTag tag;
+    FuncFields fields;
+} FuncObj;
+
+#endif
