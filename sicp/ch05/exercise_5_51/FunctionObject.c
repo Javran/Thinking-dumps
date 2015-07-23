028dfce0e0549e3c82eb95cce6230f1fceaa90aa
Javran Cheng <Javran.c@gmail.com>
2015-07-23 19:19:32 -0400

    ex 5.51: impl application for all types of funcobj

--- sicp/ch05/exercise_5_51/FunctionObject.c
+++ sicp/ch05/exercise_5_51/FunctionObject.c
@@ -1,4 +1,5 @@
 #include "FunctionObject.h"
+#include "EvalSeq.h"
 
 FuncObj *newCompoundFunc(const SExp *p,
                               const SExp *bd,
@@ -9,6 +10,45 @@ FuncObj *newCompoundFunc(const SExp *p,
     fc->fields.compObj.body = bd;
     fc->fields.compObj.env = e;
     return fc;
+}
+
+// apply arguments to a function object
+// this function assmes that all operands are evaluated
+// thus it is not necessary to provide an environment
+const SExp *funcObjApp(const FuncObj *rator, const SExp *rands) {
+    // TODO:
+    switch (rator->tag) {
+    case funcPrim: {
+        FuncPrimHandler handler = rator->fields.primHdlr;
+        return handler(rands);
+    }
+    case funcCompound: {
+        FuncCompound fc = rator->fields.compObj;
+        const SExp *ps = fc.parameters;
+        const SExp *bd = fc.body;
+        Environment *env = fc.env;
+
+        Environment *appEnv = calloc(1,sizeof(Environment));
+        envInit(appEnv);
+        // pointerManagerRegisterCustom(pEnvArgs, (PFreeCallback)releaseTempEnv);
+        envSetParent(appEnv, env);
+
+        while (sexpNil != rands->tag && sexpNil != ps->tag ) {
+            if (sexpSymbol != sexpCar( ps )->tag)
+                return NULL;
+            char *varName = sexpCar( ps )->fields.symbolName;
+            const SExp *rand = sexpCar( rands );
+            envInsert(appEnv, varName, (void *)rand);
+            rands = sexpCdr(rands);
+            ps = sexpCdr(ps);
+        }
+        if (! (sexpNil == rands->tag && sexpNil == ps->tag) )
+            return NULL;
+        // execute body under new environment
+        return evSequence(bd, appEnv);
+    }
+    }
+    assert(0 /* invalid function object */);
 }
 
 void freeFuncObject(FuncObj *p) {
