588d50fbe245fb2a59bf26afcf55d9c3ce113eab
Javran Cheng <Javran.c@gmail.com>
2015-07-28 19:17:40 -0400

    ex 5.51: prim plus

--- sicp/ch05/exercise_5_51/Primitives.c
+++ sicp/ch05/exercise_5_51/Primitives.c
@@ -1,9 +1,21 @@
 #include "Primitives.h"
 #include "SExp.h"
+#include "PointerManager.h"
 
- const SExp * primPlus(const SExp *args) {
+long *primPlusFoldHelper(long *acc, const SExp *elem) {
+    assert(sexpInteger == elem->tag);
+    *acc = *acc + elem->fields.integerContent;
+    return acc;
+}
+
+const SExp *primPlus(const SExp *args) {
      DynArr *argsA = sexpProperListToDynArr(args);
-
+     long seed = 0;
+     dynArrFoldLeft(argsA, (DynArrFoldLeftAccumulator)primPlusFoldHelper,&seed);
      dynArrFree(argsA);
      free(argsA);
+
+     SExp *retVal = newInteger( seed );
+     pointerManagerRegisterCustom(retVal, (PFreeCallback)freeSExp);
+     return retVal;
 }
