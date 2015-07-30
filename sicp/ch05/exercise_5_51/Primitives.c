7f5c79329c3601a3d7141eddceb176e6f5dc6b12
Javran Cheng <Javran.c@gmail.com>
2015-07-29 22:50:23 -0400

    ex 5.51: error handling

--- sicp/ch05/exercise_5_51/Primitives.c
+++ sicp/ch05/exercise_5_51/Primitives.c
@@ -3,21 +3,27 @@
 #include "PointerManager.h"
 
 long *primPlusFoldHelper(long *acc, const SExp *elem) {
+    if (! acc)
+        return NULL;
     assert(sexpInteger == elem->tag);
     *acc = *acc + elem->fields.integerContent;
     return acc;
 }
 
 const SExp *primPlus(const SExp *args) {
-     DynArr *argsA = sexpProperListToDynArr(args);
-     long seed = 0;
-     dynArrFoldLeft(argsA, (DynArrFoldLeftAccumulator)primPlusFoldHelper,&seed);
-     dynArrFree(argsA);
-     free(argsA);
+    DynArr *argsA = sexpProperListToDynArr(args);
+    long seed = 0;
+    long *result = dynArrFoldLeft(argsA, (DynArrFoldLeftAccumulator)primPlusFoldHelper,&seed);
+    dynArrFree(argsA);
+    free(argsA);
 
-     SExp *retVal = newInteger( seed );
-     pointerManagerRegisterCustom(retVal, (PFreeCallback)freeSExp);
-     return retVal;
+    if (!result) {
+        return NULL;
+    } else {
+        SExp *retVal = newInteger( seed );
+        pointerManagerRegisterCustom(retVal, (PFreeCallback)freeSExp);
+        return retVal;
+    }
 }
 
 
