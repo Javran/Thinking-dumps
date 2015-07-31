7c7869f2d4a5f2f574cd0bd07d02d2f4b6a7f9c0
Javran Cheng <Javran.c@gmail.com>
2015-07-30 23:02:40 -0400

    ex 5.51: few comments

--- sicp/ch05/exercise_5_51/InitEnv.c
+++ sicp/ch05/exercise_5_51/InitEnv.c
@@ -2,6 +2,7 @@
 
 Environment *mkInitEnv() {
     Environment *env = calloc(1, sizeof(Environment));
+    // parent environment is implictily set to NULL
     envInit(env);
     envInsert(env, "+", &primPlusSExp);
     return env;
