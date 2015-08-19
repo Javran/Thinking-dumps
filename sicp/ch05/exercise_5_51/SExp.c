c5475af7eaa4c1c3ad9699c9cd5178549b4f3b85
Javran Cheng <Javran.c@gmail.com>
2015-08-18 22:55:36 -0400

    ex 5.51: need to introduce some convention here

--- sicp/ch05/exercise_5_51/SExp.c
+++ sicp/ch05/exercise_5_51/SExp.c
@@ -7,6 +7,11 @@
 // TODO: consider registering dynamic allocated objects
 // so that other parts doesn't have to deal with the mess
 
+// we can have some convention here:
+// * newXXX is like allocating some space, but the user should take care of the de-allocation
+// * freeXXX is not usually recursive (TODO: for now freeSExp violates this, we need to fix it)
+// * mkXXX is not allocating space, but fills in some information
+
 // statically allocated objects,
 // which are intended for sharing
 // (to reduce runtime-allocation overhead)
