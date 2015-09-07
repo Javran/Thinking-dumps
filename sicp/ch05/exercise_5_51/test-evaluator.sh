cbdd974b91ac1608b96aeeb3bbdc3367513d19db
Javran Cheng <Javran.c@gmail.com>
2015-09-06 23:43:05 -0400

    ex 5.51: compare outpu

--- sicp/ch05/exercise_5_51/test-evaluator.sh
+++ sicp/ch05/exercise_5_51/test-evaluator.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+VALGRIND_ARGS="--trace-children=yes --leak-check=full --error-exitcode=1"
+
 # Test evaluator by running it using a test program
 
 make evaluator || exit -1
 
-./evaluator test.scm
+if ./evaluator test.scm | diff - test-expected.out;
+then
+    echo "Test passed"
+else
+    echo "Test failed"
+fi
+
+if valgrind $VALGRIND_ARGS -- ./evaluator test.scm;
+then
+    echo "Memcheck passed"
+else
+    echo "Memcheck failed"
+fi
 
 # TODO:
 # * standard output
