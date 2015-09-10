5c1a3634e0bb1d64d6a870a8d6467fd5c6c2a4a3
Javran Cheng <Javran.c@gmail.com>
2015-09-09 22:33:04 -0400

    ex 5.52: TODO cleanup

--- sicp/ch05/exercise_5_52/main.c
+++ sicp/ch05/exercise_5_52/main.c
@@ -3,48 +3,8 @@
 
 // copied from ex 5.51
 
-char *loadFile(const char* fileName) {
-    FILE* fp = fopen(fileName,"r");
-    // find file length
-    fseek(fp,0,SEEK_END);
-    size_t srcSize = ftell(fp);
-    fseek(fp,0,SEEK_SET);
-
-    // read into memory
-    // one extra bit for string termination
-    char *retVal = malloc(srcSize+1);
-    fread(retVal, 1, srcSize, fp);
-    retVal[srcSize] = 0;
-    fclose(fp);
-    return retVal;
-}
-
-void helpAndQuit(char *execName) {
-    fprintf(stderr,
-            "Usage: %s <lisp source file>\n",
-            execName);
-    exit(1);
-}
-
-int main(int argc, char *argv[]) {
-    // TODO: we need a program that takes no input --
-    // the program is fed to our compiler and here we will
-    // just have generated codes!
-    if ( argc != 1+1 )
-        helpAndQuit(argv[0]);
-
-    const char *fileName = argv[1];
-    if ( !isFileExist(fileName) ||
-         !isFileReadable(fileName) ) {
-        fprintf(stderr,
-                "File is not available.\n");
-        exit(errno);
-    }
-
-    char *srcText = loadFile( fileName );
-    char result = 0;
-//    char result = evalProgramText(srcText, stderr);
-    free(srcText); srcText = NULL;
-
-    return result ? EXIT_SUCCESS : EXIT_FAILURE;
+// all args are ignored
+int main() {
+    // TODO: function body
+    return EXIT_FAILURE;
 }
