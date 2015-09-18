749414cbbbd0d713d45f4ef2bc7e8080258529a2
Javran Cheng <Javran.c@gmail.com>
2015-09-17 23:14:33 -0400

    genPrime?

--- exercism/haskell/prime-factors/PrimeFactors.hs
+++ exercism/haskell/prime-factors/PrimeFactors.hs
@@ -1,5 +1,6 @@
 module PrimeFactors
   ( primeFactors
+  , main
   ) where
 
 -- | list difference, assuming the input list is ordered
@@ -11,5 +12,13 @@ orderedDiff x@(a:as) y@(b:bs) = case a `compare` b of
   EQ -> orderedDiff as bs
   GT -> orderedDiff x bs
 
+-- | expect infinite list '[2,3...]' for sieving
+genPrimes :: Integral a => [a] -> [a]
+genPrimes [] = undefined
+genPrimes (x:xs) = x : genPrimes (xs `orderedDiff` [x,x+x..])
+
 primeFactors :: Integral a => a -> [a]
 primeFactors = undefined
+
+main :: IO ()
+main = print (take 2 (dropWhile (< 894119) (genPrimes [2..])))
