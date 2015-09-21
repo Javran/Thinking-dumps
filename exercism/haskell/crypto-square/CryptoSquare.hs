08d97a1866e51fdaaa49d3971712bead862062ba
Javran Cheng <Javran.c@gmail.com>
2015-09-20 23:46:15 -0400

    cryptosquare - match type

---
+++ exercism/haskell/crypto-square/CryptoSquare.hs
@@ -0,0 +1,22 @@
+module CryptoSquare
+  ( normalizePlaintext
+  , squareSize
+  , plaintextSegments
+  , ciphertext
+  , normalizeCiphertext
+  ) where
+
+normalizePlaintext :: [a] -> [a]
+normalizePlaintext = undefined
+
+squareSize :: [a] -> Int
+squareSize = undefined
+
+plaintextSegments :: [a] -> [[a]]
+plaintextSegments = undefined
+
+normalizeCiphertext :: String -> String
+normalizeCiphertext = undefined
+
+ciphertext :: String -> String
+ciphertext = undefined
