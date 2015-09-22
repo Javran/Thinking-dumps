3d2f574e9c77242f8188174403c871aedffe6351
Javran Cheng <Javran.c@gmail.com>
2015-09-22 19:32:01 -0400

    garden - impl

--- exercism/haskell/kindergarten-garden/Garden.hs
+++ exercism/haskell/kindergarten-garden/Garden.hs
@@ -5,6 +5,11 @@ module Garden
   , Plant(..)
   ) where
 
+import Data.Maybe
+import Data.List.Split
+import Data.List
+import qualified Data.Map.Strict as M
+
 data Plant
   = Grass
   | Clover
@@ -14,16 +19,51 @@ data Plant
 
 type Name = String
 
-data Garden -- TODO
+type Garden = M.Map Name [Plant]
+
+toPlant :: Char -> Plant
+toPlant c = case c of
+    'G' -> Grass
+    'C' -> Clover
+    'R' -> Radishes
+    'V' -> Violets
+    _ -> error "unknown plant type"
 
 garden :: [Name] -> String -> Garden
-garden ns raw = undefined
+garden ns raw = M.fromList (zip (sort ns) twoCols)
+  where
+    -- "ABCD\nEFGH" ->
+    -- [ [A,B,C,D]
+    -- , [E,F,G,H]
+    -- ]
+    rows :: [[Plant]]
+    rows = (map . map) toPlant . lines $ raw
+    -- step 1:
+    -- [ [[A,B],[C,D]]
+    -- , [[E,F],[G,H]]
+    -- ]
+    -- step 2:
+    -- [ [[A,B],[E,F]]
+    -- , [[C,D],[G,H]]
+    -- ]
+    -- step 3:
+    -- [ [A,B,E,F]
+    -- , [C,D,G,H]
+    -- ]
+    twoCols :: [[Plant]]
+    twoCols = map concat -- step 3
+            . transpose -- step 2
+            . map (chunksOf 2) -- step 1
+            $ rows
 
 defaultNames :: [Name]
-defaultNames = undefined
+defaultNames = words
+    "Alice Bob Charlie David \
+    \Eve Fred Ginny Harriet \
+    \Ileana Joseph Kincaid Larry "
 
 defaultGarden :: String -> Garden
 defaultGarden = garden defaultNames
 
 lookupPlants :: Name -> Garden -> [Plant]
-lookupPlants = undefined
+lookupPlants n = fromJust . M.lookup n
