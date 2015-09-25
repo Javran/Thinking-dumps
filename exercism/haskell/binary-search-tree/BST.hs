f317fe79dfc04f167acc1e2554ec7dfe66ed1d92
Javran Cheng <Javran.c@gmail.com>
2015-09-24 23:16:54 -0400

    bst - comments

--- exercism/haskell/binary-search-tree/BST.hs
+++ exercism/haskell/binary-search-tree/BST.hs
@@ -3,13 +3,16 @@ module BST
   , singleton
   , insert
   , fromList
-  , toList
+  , toList -- from Data.Foldable
   ) where
 
 import Data.Foldable
 import Data.Monoid
 import Data.Maybe
 
+-- from testcases, it seems this BST will always
+-- contain at least one element,
+-- so there is no "Nil" alternative constructor in this definition
 data BinTree a = BinTree
   { btVal :: a
   , btLeft :: Maybe (BinTree a)
@@ -39,8 +42,10 @@ insert :: Ord a => a -> BinTree a -> BinTree a
 insert newV bt@(BinTree v l r) =
     if newV <= v
       then
+        -- update left branch
         bt { btLeft = Just $ maybe newLeaf (insert newV) l }
       else
+        -- update right branch
         bt { btRight = Just $ maybe newLeaf (insert newV) r }
   where
     newLeaf = singleton newV
