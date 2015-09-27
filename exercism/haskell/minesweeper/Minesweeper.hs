b5994f4ff039eceeb90bfc611421266bc51fa8f3
Javran Cheng <Javran.c@gmail.com>
2015-09-26 21:42:27 -0400

    minesweeper - to minefield

--- exercism/haskell/minesweeper/Minesweeper.hs
+++ exercism/haskell/minesweeper/Minesweeper.hs
@@ -2,5 +2,19 @@ module Minesweeper
   ( annotate
   ) where
 
+import qualified Data.Set as S
+
+type MineField = S.Set (Int,Int)
+
 annotate :: [String] -> [String]
 annotate = undefined
+
+toMineField :: [String] -> MineField
+toMineField xs = S.fromList ys
+  where
+    rows = length xs
+    cols = length (head xs)
+    coords = [(x,y) | x <- [1..rows], y <- [1..cols]]
+    ys = map fst
+       $ filter ((== '*') . snd)
+       $ zip coords (concat xs)
