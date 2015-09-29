01c126fa3199b96782fa732fd71a59abd0389b7f
Javran Cheng <Javran.c@gmail.com>
2015-09-29 00:09:30 -0400

    wordy - impl done

--- exercism/haskell/wordy/WordProblem.hs
+++ exercism/haskell/wordy/WordProblem.hs
@@ -4,15 +4,20 @@ module WordProblem
 
 import Text.ParserCombinators.ReadP
 import Data.Char
+import Data.Maybe
 
-answer :: String -> Int
-answer = undefined
+answer :: String -> Maybe Integer
+answer = parse question
 
 number :: ReadP Integer
 number = do
+    -- an optional sign is parsed as a function that
+    -- transforms the number immediately followed
     sgnF <- option id (char '-' *> return negate)
     sgnF . read <$> munch1 isDigit
 
+-- | reads operation (consumes leading and following consecutive spaces)
+--   and produce the corresponding operation
 operation :: ReadP (Integer -> Integer -> Integer)
 operation = skipSpaces *> op <* skipSpaces
   where
@@ -21,13 +26,13 @@ operation = skipSpaces *> op <* skipSpaces
      +++ (string "multiplied by" *> return (*))
      +++ (string "divided by" *> return div)
 
+-- | parse the question, calculate results as parser runs
 question :: ReadP Integer
-question = do
-    string "What is "
-    result <- chainl1 number operation
-    char '?'
-    return result
+question = string "What is "
+        *> chainl1 number operation <* char '?'
 
--- patterns:
--- number : (-?)(\d+)
--- operations: "plus", "minus", "multipled by", "divided by"
+parse :: ReadP a -> String -> Maybe a
+parse rp inp = listToMaybe
+             . map fst
+             . filter (null . snd)
+             $ readP_to_S (rp <* eof) inp
