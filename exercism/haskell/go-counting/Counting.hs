6b9e362d2a70830224604e16d5a6d052430045b8
Javran Cheng <Javran.c@gmail.com>
2015-09-29 21:27:28 -0400

    go - solution explained

--- exercism/haskell/go-counting/Counting.hs
+++ exercism/haskell/go-counting/Counting.hs
@@ -15,8 +15,17 @@ type Coord = (Int, Int)
 
 type Board = [ [] Char ] -- shut up hlint
 
+-- one corollary from the rule is that:
+-- + two neighboring empty cells must be of the same side.
+--   otherwise these two cells cannot be counted as their sides
+--   in the first place.
+-- so one solution is to first clustering neigboring cells to figure out
+-- empty intersections, and then we can determine which side owns the
+-- territory.
+
 territories :: Board -> [(S.Set Coord, Maybe Color)]
 territories = undefined
 
 territoryFor :: Board -> Coord -> Maybe (S.Set Coord, Maybe Color)
 territoryFor = undefined
+
