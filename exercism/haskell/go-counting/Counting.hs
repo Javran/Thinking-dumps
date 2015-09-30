f18f35163984ca5fe10b9c61bc6ec262bc2ff653
Javran Cheng <Javran.c@gmail.com>
2015-09-29 21:47:39 -0400

    go - transform to array

--- exercism/haskell/go-counting/Counting.hs
+++ exercism/haskell/go-counting/Counting.hs
@@ -5,6 +5,8 @@ module Counting
   ) where
 
 import qualified Data.Set as S
+import Data.Array
+import Data.List
 
 data Color
   = Black
@@ -12,8 +14,10 @@ data Color
     deriving (Eq, Ord, Show)
 
 type Coord = (Int, Int)
-
 type Board = [ [] Char ] -- shut up hlint
+type Owner = Maybe Color
+type CellArray = Array Coord Owner
+
 
 -- one corollary from the rule is that:
 -- + two neighboring empty cells must be of the same side.
@@ -23,9 +27,19 @@ type Board = [ [] Char ] -- shut up hlint
 -- empty intersections, and then we can determine which side owns the
 -- territory.
 
-territories :: Board -> [(S.Set Coord, Maybe Color)]
+territories :: Board -> [(S.Set Coord, Owner)]
 territories = undefined
 
-territoryFor :: Board -> Coord -> Maybe (S.Set Coord, Maybe Color)
+territoryFor :: Board -> Coord -> Maybe (S.Set Coord, Owner)
 territoryFor = undefined
 
+toCellArray :: Board -> CellArray
+toCellArray b = listArray ((1,1),(cols,rows)) (map toOwner (concat (transpose b)))
+  where
+    rows = length b
+    cols = length (head b)
+    toOwner ch = case ch of
+        'W' -> Just White
+        'B' -> Just Black
+        ' ' -> Nothing
+        _ -> error "invalid board"
