f56637fce6f7318b2bb23d0a403b391851665374
Javran Cheng <Javran.c@gmail.com>
2015-09-29 23:05:35 -0400

    go - cluster empty cells

--- exercism/haskell/go-counting/Counting.hs
+++ exercism/haskell/go-counting/Counting.hs
@@ -7,6 +7,7 @@ module Counting
 import qualified Data.Set as S
 import Data.Array
 import Data.List
+import Data.Foldable
 
 data Color
   = Black
@@ -18,6 +19,26 @@ type Board = [ [] Char ] -- shut up hlint
 type Owner = Maybe Color
 type CellArray = Array Coord Owner
 
+-- for manual testing
+board5x5 :: [[Char]]
+board5x5 =
+    ["  B  ",
+     " B B ",
+     "B W B",
+     " W W ",
+     "  W  "]
+
+board9x9 :: [[Char]]
+board9x9 =
+    ["  B   B  ",
+     "B   B   B",
+     "WBBBWBBBW",
+     "W W W W W",
+     "         ",
+     " W W W W ",
+     "B B   B B",
+     " W BBB W ",
+     "   B B   "]
 
 -- one corollary from the rule is that:
 -- + two neighboring empty cells must be of the same side.
@@ -27,8 +48,38 @@ type CellArray = Array Coord Owner
 -- empty intersections, and then we can determine which side owns the
 -- territory.
 
+-- TODO: determine cluster owner
 territories :: Board -> [(S.Set Coord, Owner)]
-territories = undefined
+territories b = map (\x -> (x,Nothing)) (clusterCells emptyCells S.empty [])
+  where
+    ca = toCellArray b
+    -- get coords of empty cells
+    emptyCells = map fst $ filter ((== Nothing). snd) (assocs ca)
+
+    clusterCells :: [Coord] -- cells to be visited
+                 -> S.Set Coord -- current set of cells
+                 -> [S.Set Coord] -- current set of clusters
+                 -> [S.Set Coord] -- return
+    clusterCells [] curCells clusters =
+        if S.null curCells
+          then clusters
+          else curCells : clusters
+    clusterCells todos@(x:xs) curCells clusters =
+        -- if the current set of coords is empty
+        -- then we are not ready to expand the set,
+        if S.null curCells
+          then
+            -- pick one element to form the initial set
+            clusterCells xs (S.singleton x) clusters
+          else
+            let neighbor (a,b) = [(a-1,b),(a+1,b),(a,b-1),(a,b+1)]
+                neighbors = S.fromList . concatMap neighbor . toList $ curCells
+                todoSet = S.fromList todos
+                found = neighbors `S.intersection` todoSet
+                remaining = toList (todoSet `S.difference` found)
+            in if S.null found
+                 then clusterCells todos S.empty (curCells : clusters)
+                 else clusterCells remaining (found `S.union` curCells) clusters
 
 territoryFor :: Board -> Coord -> Maybe (S.Set Coord, Owner)
 territoryFor b coord = find (\(cs,_) -> coord `S.member` cs) t
