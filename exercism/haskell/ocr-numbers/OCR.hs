e800ff4b47c19a8cec8e82756215de88ab5905d4
Javran Cheng <Javran.c@gmail.com>
2015-10-09 21:21:53 -0400

    ocr - ocrTable

---
+++ exercism/haskell/ocr-numbers/OCR.hs
@@ -0,0 +1,16 @@
+module OCR
+  ( convert
+  ) where
+
+convert :: String -> String
+convert = undefined
+
+ocrTable :: [(Int, [String])]
+ocrTable = undefined
+  where
+    ocrs =
+        [ " _     _  _     _  _  _  _  _ "
+        , "| |  | _| _||_||_ |_   ||_||_|"
+        , "|_|  ||_  _|  | _||_|  ||_| _|"
+        , "                              "
+        ]
