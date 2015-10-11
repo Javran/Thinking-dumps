8c1eab712ce1e093288041b7f4412289cdade2fc
Javran Cheng <Javran.c@gmail.com>
2015-10-10 22:01:22 -0400

    luhn - break into digits

--- exercism/haskell/luhn/Luhn.hs
+++ exercism/haskell/luhn/Luhn.hs
@@ -7,7 +7,18 @@ module Luhn
   where
 
 checkDigit :: Integer -> Integer
-checkDigit = undefined
+checkDigit = (`rem` 10)
+
+toDigits :: Integer -> [Integer]
+toDigits 0 = [0]
+toDigits v =
+      map snd
+    . reverse
+    . takeWhile (\(x,y) -> x > 0 || y > 0)
+    . tail
+    -- [(123,0),(12,3),(1,2),(0,1),(0,0]
+    $ iterate (\(x,_) -> x `quotRem` 10) (v,0)
+    -- e.g. v=123
 
 addends :: Integer -> [Integer]
 addends = undefined
