ee27088be4d816c9a6aeb9be785f822c1bc7239f
Javran Cheng <Javran.c@gmail.com>
2015-10-10 21:49:02 -0400

    luhn - matching type

---
+++ exercism/haskell/luhn/Luhn.hs
@@ -0,0 +1,22 @@
+module Luhn
+  ( checkDigit
+  , addends
+  , checksum
+  , isValid
+  , create)
+  where
+
+checkDigit :: Integer -> Integer
+checkDigit = undefined
+
+addends :: Integer -> [Integer]
+addends = undefined
+
+checksum :: Integer -> Integer
+checksum = undefined
+
+isValid :: Integer -> Bool
+isValid = undefined
+
+create :: Integer -> Integer
+create = undefined
