51bd253ed0dbd53046e31a46d5e7d89e7fcfbd21
Javran Cheng <Javran.c@gmail.com>
2015-10-12 00:00:44 -0400

    zipper - impl (WIP)

--- exercism/haskell/zipper/Zipper.hs
+++ exercism/haskell/zipper/Zipper.hs
@@ -12,6 +12,8 @@ module Zipper
   , setRight
   ) where
 
+import Data.List
+
 -- | A binary tree.
 data BinTree a = BT
   { btValue :: a -- ^ Value
@@ -19,25 +21,37 @@ data BinTree a = BT
   , btRight :: Maybe (BinTree a) -- ^ Right child
   } deriving (Eq, Show)
 
+type BinTreeContext a = (a, Bool, Maybe (BinTree a))
+    -- (a, False, tree) for context: (<focus>, btValue, tree)
+    -- (a, True, tree) for context: (tree, btValue, <focus>)
+
+
 -- | A zipper for a binary tree.
 data Zipper a = Zipper
   { zFocus :: BinTree a -- ^ the binary tree under focus
-  , zContext :: [(a, Bool, BinTree a)] -- ^ a stack of contexts
-                -- (a, False, tree) for context: (<focus>, btValue, tree)
-                -- (a, True, tree) for context: (tree, btValue, <focus>)
+  , zContext :: [BinTreeContext a] -- ^ a stack of contexts
   }
 
 -- | Get a zipper focussed on the root node.
 fromTree :: BinTree a -> Zipper a
-fromTree = undefined
+fromTree t = Zipper t []
+
+-- I'm using case-expression on purpose
+-- because here Bool values are used as markers
+-- rather than expression true or false
+{-# ANN applyContext "HLint: ignore Use if" #-}
+applyContext :: BinTreeContext a -> BinTree a -> BinTree a
+applyContext (v, dir, t) tFocus = case dir of
+    False -> BT v (Just tFocus) t
+    True  -> BT v t (Just tFocus)
 
 -- | Get the complete tree from a zipper.
 toTree :: Zipper a -> BinTree a
-toTree = undefined
+toTree (Zipper tFocus ctxt) = foldl' (flip applyContext) tFocus ctxt
 
 -- | Get the value of the focus node.
 value :: Zipper a -> a
-value = undefined
+value = btValue . zFocus
 
 -- | Get the left child of the focus node, if any.
 left :: Zipper a -> Maybe (Zipper a)
