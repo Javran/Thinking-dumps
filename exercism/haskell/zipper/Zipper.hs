2311131402518659b23f2e1010d9d8911d6a5ed3
Javran Cheng <Javran.c@gmail.com>
2015-10-12 23:06:52 -0400

    zipper - impl

--- exercism/haskell/zipper/Zipper.hs
+++ exercism/haskell/zipper/Zipper.hs
@@ -25,12 +25,11 @@ type BinTreeContext a = (a, Bool, Maybe (BinTree a))
     -- (a, False, tree) for context: (<focus>, btValue, tree)
     -- (a, True, tree) for context: (tree, btValue, <focus>)
 
-
 -- | A zipper for a binary tree.
 data Zipper a = Zipper
   { zFocus :: BinTree a -- ^ the binary tree under focus
   , zContext :: [BinTreeContext a] -- ^ a stack of contexts
-  }
+  } deriving (Eq, Show)
 
 -- | Get a zipper focussed on the root node.
 fromTree :: BinTree a -> Zipper a
@@ -55,24 +54,28 @@ value = btValue . zFocus
 
 -- | Get the left child of the focus node, if any.
 left :: Zipper a -> Maybe (Zipper a)
-left = undefined
+left (Zipper (BT v (Just l) r) ctxt) = Just (Zipper l ((v,False,r):ctxt))
+left _ = Nothing
 
 -- | Get the right child of the focus node, if any.
 right :: Zipper a -> Maybe (Zipper a)
-right = undefined
+right (Zipper (BT v l (Just r)) ctxt) = Just (Zipper r ((v,True,l):ctxt))
+right _ = Nothing
 
 -- | Get the parent of the focus node, if any.
 up :: Zipper a -> Maybe (Zipper a)
-up = undefined
+up (Zipper t ctxt) = case ctxt of
+    [] -> Nothing
+    (x:xs) -> Just (Zipper (applyContext x t) xs)
 
 -- | Set the value of the focus node.
 setValue :: a -> Zipper a -> Zipper a
-setValue = undefined
+setValue v (Zipper (BT _ l r) ctxt) = Zipper (BT v l r) ctxt
 
 -- | Replace a left child tree.
 setLeft :: Maybe (BinTree a) -> Zipper a -> Zipper a
-setLeft = undefined
+setLeft l (Zipper (BT v _ r) ctxt) = Zipper (BT v l r) ctxt
 
 -- | Replace a right child tree.
 setRight :: Maybe (BinTree a) -> Zipper a -> Zipper a
-setRight = undefined
+setRight r (Zipper (BT v l _) ctxt) = Zipper (BT v l r) ctxt
