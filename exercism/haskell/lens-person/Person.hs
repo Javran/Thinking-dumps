2df208b39b061958aa4d249b3211730ee382a4f3
Javran Cheng <Javran.c@gmail.com>
2015-10-13 23:17:58 -0400

    person - fix format

--- exercism/haskell/lens-person/Person.hs
+++ exercism/haskell/lens-person/Person.hs
@@ -1,37 +1,37 @@
 module Person where
 
-import           Data.Time.Calendar
+import Data.Time.Calendar
 
-data Person = Person {
-      _name    :: Name,
-      _born    :: Born,
-      _address :: Address
-    }
+data Person = Person
+  { _name :: Name
+  , _born :: Born
+  , _address :: Address
+  }
 
-data Name = Name {
-      _foreNames :: String, -- Space separated
-      _surName   :: String
-    }
+data Name = Name
+  { _foreNames :: String -- Space separated
+  , _surName   :: String
+  }
 
-data Born = Born {
-      _bornAt :: Address,
-      _bornOn :: Day
-    }
+data Born = Born
+  { _bornAt :: Address
+  , _bornOn :: Day
+  }
 
-data Address = Address {
-      _street      :: String,
-      _houseNumber :: Int,
-      _place       :: String, -- Village / city
-      _country     :: String
-    }
+data Address = Address
+  { _street :: String
+  , _houseNumber :: Int
+  , _place :: String -- Village / city
+  , _country :: String
+  }
 
 -- Valid values of Gregorian are those for which 'Data.Time.Calendar.fromGregorianValid'
 -- returns Just.
-data Gregorian = Gregorian {
-      _year  :: Integer,
-      _month :: Int,
-      _day   :: Int
-    }
+data Gregorian = Gregorian
+  { _year :: Integer
+  , _month :: Int
+  , _day :: Int
+  }
 
 -- Implement these.
 
