6b1d0c05cbe0ff2c3b69026736f0d012ce8bc6c4
Javran Cheng <Javran.c@gmail.com>
2015-10-16 23:59:48 -0400

    connect - assumption

--- exercism/haskell/connect/Connect.hs
+++ exercism/haskell/connect/Connect.hs
@@ -5,5 +5,8 @@ module Connect
 
 data Color = Black | White
 
+-- first of all we assume there should at most be one winner
+-- because paths got to have intersections
+
 resultFor :: [String] -> Maybe Color
 resultFor = undefined
