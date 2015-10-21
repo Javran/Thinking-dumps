a9cd2e13a5afd87eb180edc80e4e5bb0f3ecd3b5
Javran Cheng <Javran.c@gmail.com>
2015-10-20 23:13:07 -0400

    matrix - parsing

--- exercism/haskell/matrix/Matrix.hs
+++ exercism/haskell/matrix/Matrix.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Matrix
   ( Matrix
   , row, column
@@ -11,7 +12,9 @@ module Matrix
   ) where
 
 import qualified Data.Vector as V
-data Matrix a
+import Text.ParserCombinators.ReadP
+
+type Matrix a = V.Vector (V.Vector a)
 
 row, column :: Int -> Matrix a -> V.Vector a
 
@@ -36,7 +39,18 @@ flatten :: Matrix a -> V.Vector a
 flatten = undefined
 
 fromString :: Read a => String -> Matrix a
-fromString = undefined
+fromString raw = fromList (map parseLine rawRows)
+  where
+    rawRows = lines raw
 
 fromList :: [[a]] -> Matrix a
-fromList = undefined
+fromList = V.fromList . map V.fromList
+
+parseLine :: forall a. Read a => String -> [a]
+parseLine raw = result
+  where
+    element :: ReadP a
+    element = readS_to_P reads
+
+    line = element `sepBy` skipSpaces
+    ((result,""):_) = readP_to_S (skipSpaces *> line <* skipSpaces <* eof) raw
