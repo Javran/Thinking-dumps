8f0f9acad4297b9e0fc3f644dbdbfd40afdf3ef9
Javran Cheng <Javran.c@gmail.com>
2015-10-27 23:52:57 -0400

    trinary - readTri

--- exercism/haskell/trinary/Trinary.hs
+++ exercism/haskell/trinary/Trinary.hs
@@ -2,30 +2,34 @@ module Trinary
   ( showTri
   , readTri
   ) where
- 
-{-
-       module Binary
-  (toDecimal)
-  where
 
-import Data.Maybe
 import Data.List
+import Data.Maybe
 
--- | converts a binary presentation (in String)
+-- | converts a trinary presentation (in String)
 --   to integer, invalid presentation results in 0
-toDecimal :: Integral a => String -> a
-toDecimal = foldl' go 0 . parseBinary
+readTri :: Integral a => String -> a
+readTri = foldl' go 0 . parseBinary
   where
     -- accumulate value
-    go acc i = acc*2 + fromIntegral i
+    go acc i = acc*3 + fromIntegral i
 
 -- | parses a character as 0/1
 parseDigit :: Char -> Maybe Int
 parseDigit '0' = Just 0
 parseDigit '1' = Just 1
+parseDigit '2' = Just 2
 parseDigit _ = Nothing
 
 -- | parses binary strings
 parseBinary :: String -> [Int]
 parseBinary = fromMaybe [] . mapM parseDigit
+
+showTri :: a
+showTri = undefined
+
+{-
+
+import Data.Maybe
+import Data.List
 -}
