783e9c372a798db7c4b2f84a8cae341b77cbcbce
Javran Cheng <Javran.c@gmail.com>
2015-11-02 21:03:10 -0500

    linked-list: follow lens' convention

--- exercism/haskell/linked-list/Deque.hs
+++ exercism/haskell/linked-list/Deque.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Deque
   ( mkDeque
   , push
@@ -10,6 +11,7 @@ module Deque
 
 import Control.Monad.Fix
 import Data.IORef
+import Control.Lens
 
 -- TODO: thread-safe
 
@@ -18,14 +20,16 @@ type Deque a = IORef (Element a)
 
 data Element a
   = Guard
-      { ePrev :: IORef (Element a)
-      , eNext :: IORef (Element a)
+      { _ePrev :: IORef (Element a)
+      , _eNext :: IORef (Element a)
       }
   | Item
-      { ePrev :: IORef (Element a)
-      , eNext :: IORef (Element a)
-      , eContent :: a
+      { _ePrev :: IORef (Element a)
+      , _eNext :: IORef (Element a)
+      , _eContent :: a
       }
+
+makeLenses ''Element
 
 mkDeque :: IO (Deque a)
 mkDeque = mfix $ \r -> newIORef (Guard r r)
@@ -36,7 +40,7 @@ push, unshift :: Deque a -> a -> IO (Deque a)
 unshift refGuard v = do
     guardNode <- readIORef refGuard
     -- guard's next pointer points to the head
-    let refHead = eNext guardNode
+    let refHead = _eNext guardNode
         newNode = Item refGuard refHead v
     refNew <- newIORef newNode
     -- there are subtle issues regarding using "writeIORef":
@@ -45,18 +49,18 @@ unshift refGuard v = do
     -- the updated data (otherwise another writeIORef will overwrite
     -- our update)
     -- therefore here we choose to use "modifyIORef" in an atomic manner
-    atomicModifyIORef' refGuard (\gn -> (gn { eNext = refNew },()))
-    atomicModifyIORef' refHead (\hn -> (hn { ePrev = refNew }, ()))
+    atomicModifyIORef' refGuard (\gn -> (gn { _eNext = refNew },()))
+    atomicModifyIORef' refHead (\hn -> (hn { _ePrev = refNew }, ()))
     return refGuard
 
 -- | push inserts elements at back
 push refGuard v = do
     guardNode <- readIORef refGuard
-    let refTail = ePrev guardNode
+    let refTail = _ePrev guardNode
         newNode = Item refTail refGuard v
     refNew <- newIORef newNode
-    atomicModifyIORef' refGuard (\gn -> (gn { ePrev = refNew },()))
-    atomicModifyIORef' refTail (\tn -> (tn { eNext = refNew }, ()))
+    atomicModifyIORef' refGuard (\gn -> (gn { _ePrev = refNew },()))
+    atomicModifyIORef' refTail (\tn -> (tn { _eNext = refNew }, ()))
     return refGuard
 
 pop, shift :: Deque a -> IO (Maybe a)
@@ -70,12 +74,12 @@ shift dq = do
       else do
         guardNode <- readIORef refGuard
         -- XNode is the node to be removed
-        let refXNode = eNext guardNode
+        let refXNode = _eNext guardNode
         xNode <- readIORef refXNode
-        let refNewHead = eNext xNode
-        atomicModifyIORef' refGuard (\gn -> (gn { eNext = eNext xNode},()))
-        atomicModifyIORef' refNewHead (\hn -> (hn { ePrev = ePrev xNode }, ()))
-        return (Just (eContent xNode))
+        let refNewHead = _eNext xNode
+        atomicModifyIORef' refGuard (\gn -> (gn { _eNext = _eNext xNode},()))
+        atomicModifyIORef' refNewHead (\hn -> (hn { _ePrev = _ePrev xNode }, ()))
+        return (Just (_eContent xNode))
 
 pop dq = do
     let refGuard = dq
@@ -85,32 +89,32 @@ pop dq = do
       else do
         guardNode <- readIORef refGuard
         -- XNode is the node to be removed
-        let refXNode = ePrev guardNode
+        let refXNode = _ePrev guardNode
         xNode <- readIORef refXNode
-        let refNewTail = ePrev xNode
-        atomicModifyIORef' refGuard (\gn -> (gn { ePrev = ePrev xNode},()))
-        atomicModifyIORef' refNewTail (\tn -> (tn { eNext = eNext xNode }, ()))
-        return (Just (eContent xNode))
+        let refNewTail = _ePrev xNode
+        atomicModifyIORef' refGuard (\gn -> (gn { _ePrev = _ePrev xNode},()))
+        atomicModifyIORef' refNewTail (\tn -> (tn { _eNext = _eNext xNode }, ()))
+        return (Just (_eContent xNode))
 
 -- a null deque contains nothing but the guard element
 -- which means guard's next item is still guard itself
 -- assume this deque is properly maintained, this check
 -- should be sufficient
 nullDeque :: Deque a -> IO Bool
-nullDeque dq = readIORef dq >>= \node -> return (eNext node == dq)
+nullDeque dq = readIORef dq >>= \node -> return (_eNext node == dq)
 
 -- | perform action on each non-guard nodes, debugging function
 visitNodes :: (a -> IO b) -> Deque a -> IO [b]
-visitNodes op refGuard = do
+visitNodes action refGuard = do
     headNode <- readIORef refGuard
-    visitNodes' (eNext headNode)
+    visitNodes' (_eNext headNode)
   where
     visitNodes' refCurrent =
         if refGuard == refCurrent
           then return []
           else do
             node <- readIORef refCurrent
-            result <- op (eContent node)
-            let refNext = eNext node
+            result <- action (_eContent node)
+            let refNext = _eNext node
             rs <- visitNodes' refNext
             return (result:rs)
