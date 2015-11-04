93286d396753377ddc0f8a9b73b7454c43b8f27b
Javran Cheng <Javran.c@gmail.com>
2015-11-03 22:43:17 -0500

    house: working impl

--- exercism/haskell/house/House.hs
+++ exercism/haskell/house/House.hs
@@ -2,6 +2,8 @@ module House
   ( rhyme
   ) where
 
+import Control.Monad.State
+
 -- a context is some text with a hole in it
 type Context = String -> String
 type TransformInfo = (String, Context)
@@ -11,6 +13,15 @@ rhymeInfos =
     [ makeItem "that lay in" "the house"
     , makeItem "that ate" "the malt"
     , makeItem "that killed" "the rat"
+    , makeItem "that worried" "the cat"
+    , makeItem "that tossed" "the dog"
+    , makeItem "that milked" "the cow with the crumpled horn"
+    , makeItem "that kissed" "the maiden all forlorn"
+    , makeItem "that married" "the man all tattered and torn"
+    , makeItem "that woke" "the priest all shaven and shorn"
+    , makeItem "that kept" "the rooster that crowed in the morn"
+    , makeItem "that belonged to" "the farmer sowing his corn"
+    , makeItem "<placeholder>" "the horse and the hound and the horn"
     ]
   where
     makeItem xs ys = (ys, (++ "\n" ++ xs ++ " " ++ ys))
@@ -18,47 +29,13 @@ rhymeInfos =
 initContext :: Context
 initContext xs = unwords [ "This is", xs, "that Jack built." ]
 
-applyTransformInfo :: TransformInfo -> Context -> (String, Context)
-applyTransformInfo (obj, oCtxt) ctxt = (ctxt obj, ctxt . oCtxt)
-
--- 1: fst (applyTransformInfo (rhymeInfos !! 0) initContext)
--- 2: fst $ applyTransformInfo (rhymeInfos !! 1) $ snd (applyTransformInfo (rhymeInfos !! 0) initContext)
--- 3: fst $ applyTransformInfo (rhymeInfos !! 2) $ snd $ applyTransformInfo (rhymeInfos !! 1) $ snd (applyTransformInfo (rhymeInfos !! 0) initContext)
+applyTransformInfo :: TransformInfo -> State Context String
+applyTransformInfo (obj, oCtxt) = do
+    ctxt <- get
+    modify (. oCtxt)
+    return (ctxt obj)
 
 rhyme :: String
-rhyme = ""
-
-{-
-
-This is <the house> that Jack built.
-
-{ the house ==> the malt \n that lay in the house }
-
-This is the malt \n that lay in the house that Jack built.
-
-{ the malt => the rat \n that ate the malt }
-
-This is the rat \n that ate the malt \n that lay in the house that Jack built.
-
-{ the rat => the cat \n that killed the rat }
-
-This is the cat \n  that killed the rat \n that ate the malt \n that lay in the house that Jack built.
-
-...
-
--}
-
-{-
-
-start: This is <> that Jack built
-insert <the house> into the hole we'll get first sentense
-
-insert another context: "<> that lay in the house"
-gives us:
-
-This is <> that lay in the house that Jack built
-
-we can category things by: context / normal text,
-apply them to a context could give either a result or another context
-
--}
+rhyme = concatMap (++ "\n\n") parts
+  where
+    parts = evalState (mapM applyTransformInfo rhymeInfos) initContext
