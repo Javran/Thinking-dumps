4f152f931e710d3e54637c809a72ddfe1c9fc6b8
Javran Cheng <Javran.c@gmail.com>
2015-11-03 21:47:30 -0500

    house: pattern

---
+++ exercism/haskell/house/House.hs
@@ -0,0 +1,26 @@
+module House
+  ( rhyme
+  ) where
+
+rhyme :: String
+rhyme = ""
+
+{-
+
+This is <the house> that Jack built.
+
+{ the house ==> the malt \n that lay in the house }
+
+This is the malt \n that lay in the house that Jack built.
+
+{ the malt => the rat \n that ate the malt }
+
+This is the rat \n that ate the malt \n that lay in the house that Jack built.
+
+{ the rat => the cat \n that killed the rat }
+
+This is the cat \n  that killed the rat \n that ate the malt \n that lay in the house that Jack built.
+
+...
+
+-}
