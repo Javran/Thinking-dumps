469e9cfdcf8e7c3a8cc061ee478b04ce87232bfc
Javran Cheng <Javran.c@gmail.com>
2015-11-04 23:15:20 -0500

    say: say a single chunk

--- exercism/haskell/say/Say.hs
+++ exercism/haskell/say/Say.hs
@@ -27,3 +27,37 @@ chunksOfThousands v
         in if length chunks <= 4
              then Just chunks
              else Nothing
+
+-- | say a value in English
+--   note that for 'sayChunk v' to be correct,
+--   it's required that '0 <= v <= 999'
+sayChunk :: Int -> [String]
+sayChunk v
+    | v <  20 = [belowTwenties !! v]
+    | v <= 99 =
+        -- 20 <= v <= 99
+        let (q,r) = v `quotRem` 10
+            tenStr = fooTies !! (q - 2)
+        in if r == 0
+             then [tenStr]
+                  -- since we know 0 <= r < 10
+                  -- the 'head' operation is safe
+             else [tenStr ++ "-" ++ head (sayChunk r)]
+    | otherwise =
+        -- 100 <= v <= 999
+        let (q,r) = v `quotRem` 100
+            thouStrs = sayChunk q ++ ["hundred"]
+        in if r == 0
+             then thouStrs
+             else thouStrs ++ sayChunk r
+  where
+    belowTwenties = words
+      "zero \
+      \one two three four five \
+      \six seven eight nine ten \
+      \eleven twelve thirteen fourteen fifteen \
+      \sixteen seventeen eighteen nineteen"
+    fooTies = words
+    -- offset:
+    --      0      1     2     3     4       5      6      7
+      "twenty thirty forty fifty sixty seventy eighty ninety"
