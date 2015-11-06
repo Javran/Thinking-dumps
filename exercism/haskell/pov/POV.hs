8cc3a84244cc64eecc465cf4fcf16a1fa1715817
Javran Cheng <Javran.c@gmail.com>
2015-11-05 23:22:22 -0500

    pov: type sig

---
+++ exercism/haskell/pov/POV.hs
@@ -0,0 +1,15 @@
+module POV
+  ( Graph(..)
+  , fromPOV
+  , tracePathBetween
+  ) where
+
+data Graph a
+  = Graph a [Graph a]
+  deriving (Eq, Show)
+
+fromPOV :: Eq a => a -> Graph a -> Maybe (Graph a)
+fromPOV = undefined
+
+tracePathBetween :: Eq a => a -> a -> Graph a -> Maybe [a]
+tracePathBetween = undefined
