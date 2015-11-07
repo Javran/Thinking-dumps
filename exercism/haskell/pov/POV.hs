09a47295fe26a4a16f239c0d50e0d6bd23045ff6
Javran Cheng <Javran.c@gmail.com>
2015-11-06 23:46:00 -0500

    pov: zipper?

--- exercism/haskell/pov/POV.hs
+++ exercism/haskell/pov/POV.hs
@@ -8,6 +8,18 @@ data Graph a
   = Graph a [Graph a]
   deriving (Eq, Show)
 
+{-
+  plan: we need a zipper, as we walk through
+  the tree, we will try to find the tag, at that time
+  some context will be accumulated, which includes enough
+  information for us to construct the new tree..
+-}
+data GraphContext a = GContext
+  { parentTag :: a
+    -- (<visited>, <rest of the elements>)
+  , bothers :: ([Graph a], [Graph a])
+  }
+
 fromPOV :: Eq a => a -> Graph a -> Maybe (Graph a)
 fromPOV = undefined
 
