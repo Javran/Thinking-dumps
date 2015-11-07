9a746a6a717d662a45a76571726e156a7dfaaf47
Javran Cheng <Javran.c@gmail.com>
2015-11-06 23:58:25 -0500

    pov: use list zipper?

--- exercism/haskell/pov/POV.hs
+++ exercism/haskell/pov/POV.hs
@@ -4,6 +4,8 @@ module POV
   , tracePathBetween
   ) where
 
+import Data.List
+
 data Graph a
   = Graph a [Graph a]
   deriving (Eq, Show)
@@ -19,6 +21,17 @@ data GraphContext a = GContext
     -- (<visited>, <rest of the elements>)
   , bothers :: ([Graph a], [Graph a])
   }
+
+getChildZippers :: Graph a -> [(Graph a, GraphContext a)]
+getChildZippers (Graph t xs) = undefined
+
+-- create list zippers: [1,2,3] -> [([],[1,2,3]), ([1],[2,3]), ([2,1],[3]), ([3,2,1],[])]
+listZippers :: [a] -> [([a],[a])]
+listZippers xs = unfoldr f ([],xs)
+  where
+    f (st,xs) = case xs of
+        [] -> Nothing
+        (x:xs) -> let v = (x:st,xs) in Just (v,v)
 
 fromPOV :: Eq a => a -> Graph a -> Maybe (Graph a)
 fromPOV = undefined
