0f5f71122b54cc59079f8614e8e6a4a8d00212a1
Javran Cheng <Javran.c@gmail.com>
2015-11-08 23:15:13 -0500

    pov: rebuild tree

--- exercism/haskell/pov/POV.hs
+++ exercism/haskell/pov/POV.hs
@@ -6,6 +6,8 @@ module POV
 
 import Data.List
 
+-- I'm still not sure why this is called "Graph" when
+-- it's actually a tree-representation
 data Graph a
   = Graph a [Graph a]
   deriving (Eq, Show)
@@ -22,6 +24,16 @@ data GraphContext a = GContext
     -- INVARIANT: fst of "bothers" must not be empty
   , bothers :: ([Graph a], [Graph a])
   } deriving (Show)
+
+-- | create list zippers: [1,2,3] -> [([1],[2,3]), ([2,1],[3]), ([3,2,1],[])]
+--   note that the first element of "fst" also contains the focus,
+--   so it cannot be empty
+listZippers :: [a] -> [([a],[a])]
+listZippers cs = unfoldr f ([],cs)
+  where
+    f (st,xs) = case xs of
+        [] -> Nothing
+        (y:ys) -> let v = (y:st,ys) in Just (v,v)
 
 -- | get all zipppers of immediate children of a tree
 getChildZippers :: Graph a -> [(Graph a, GraphContext a)]
@@ -40,15 +52,12 @@ getAllZippers curZipper@(g,gcs) =
   where
     expand (g1, gc1) = getAllZippers (g1, gc1:gcs)
 
--- create list zippers: [1,2,3] -> [([1],[2,3]), ([2,1],[3]), ([3,2,1],[])]
--- note that the first element of "fst" also contains the focus,
--- so it cannot be empty
-listZippers :: [a] -> [([a],[a])]
-listZippers cs = unfoldr f ([],cs)
+rebuildFromZipper :: (Graph a, [GraphContext a]) -> Graph a
+rebuildFromZipper (t,[]) = t
+rebuildFromZipper (Graph tg children,GContext pt (bsL,bsR):cs) =
+    Graph tg (children ++ [ps])
   where
-    f (st,xs) = case xs of
-        [] -> Nothing
-        (y:ys) -> let v = (y:st,ys) in Just (v,v)
+    ps = rebuildFromZipper (Graph pt (reverse bsL ++ bsR), cs)
 
 fromPOV :: Eq a => a -> Graph a -> Maybe (Graph a)
 fromPOV = undefined
