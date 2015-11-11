e26005ccd4311ed0d73ddfb71b30c6af039e148f
Javran Cheng <Javran.c@gmail.com>
2015-11-10 20:50:08 -0500

    palindromes: match type

---
+++ exercism/haskell/palindrome-products/Palindromes.hs
@@ -0,0 +1,10 @@
+module Palindromes
+  ( largestPalindrome
+  , smallestPalindrome
+  ) where
+
+largestPalindrome, smallestPalindrome :: Integral a => a -> a -> (a, [(a,a)])
+
+largestPalindrome = undefined
+smallestPalindrome = undefined
+
