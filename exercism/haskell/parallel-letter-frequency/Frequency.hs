e0b200c456febc7e9f18a04432dd4ade9bcfc0ac
Javran Cheng <Javran.c@gmail.com>
2015-11-12 23:37:10 -0500

    frequency: apply parallelism?

--- exercism/haskell/parallel-letter-frequency/Frequency.hs
+++ exercism/haskell/parallel-letter-frequency/Frequency.hs
@@ -7,11 +7,7 @@ import qualified Data.Text as T
 import qualified Data.Map.Strict as M
 
 import Data.Char
-import Data.Monoid
+import Control.Parallel.Strategies
 
 frequency :: Int -> [T.Text] -> M.Map Char Int
-frequency n = M.unionsWith (+)
-              . map (M.fromListWith (+)
-                     . map ((,1) . toLower)
-                     . filter isLetter
-                     . T.unpack)
+frequency n ts = M.unionsWith (+) $ (map (M.fromListWith (+) . map ((,1) . toLower) . filter isLetter . T.unpack) ts `using` parBuffer n rdeepseq)
