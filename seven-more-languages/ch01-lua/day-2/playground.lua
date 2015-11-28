09a382d297e06f45e2b5f5f0f8c01373d1e66aa6
Javran Cheng <Javran.c@gmail.com>
2015-11-27 21:07:41 -0500

    coroutine yield

---
+++ seven-more-languages/ch01-lua/day-2/playground.lua
@@ -0,0 +1,28 @@
+#!/usr/bin/env lua
+
+function five_numbers_start_from(n)
+   -- note that we must return a function object
+   return function ()
+      local v = n
+      local c = 0
+      while c < 5 do
+         coroutine.yield(v)
+         v = v+1
+         c = c+1
+      end
+      -- when all values are yielded,
+      -- the function's return value will be produced
+      return "done"
+   end 
+end
+
+function consume_and_print(g)
+   local succeeded, v = coroutine.resume(g)
+   print (succeeded, v)
+end
+
+g = coroutine.create(five_numbers_start_from(10))
+
+for i = 1,10 do
+   consume_and_print(g)
+end
