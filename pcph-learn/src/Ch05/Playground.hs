61183da5a3558d3f37c6980af987ffa23730bde2
Javran Cheng <Javran.c@gmail.com>
2015-12-03 23:57:59 -0500

    few more imports

--- pcph-learn/src/Ch05/Playground.hs
+++ pcph-learn/src/Ch05/Playground.hs
@@ -3,14 +3,19 @@ module Playground
   where
 
 -- a playground lib meant to be imported in GHCi
+
+import qualified Data.Array.Repa as Repa
 import Data.Array.Repa
     ( Array
     , Z(..)
     , (:.)(..)
+    , (!)
     , U
     , DIM0, DIM1, DIM2
     , fromListUnboxed
+    , toIndex
+    , fromFunction
     )
 
-testArray :: Array U DIM1 Int
-testArray = fromListUnboxed (Z :. 10) [1..10]
+testArray :: Array U DIM2 Int
+testArray = fromListUnboxed (Z :. 3 :. 5) [1..15]
