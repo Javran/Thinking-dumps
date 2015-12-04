c3bb4507a17fd1de9fe11833487491f8e3f6d694
Javran Cheng <Javran.c@gmail.com>
2015-12-03 23:42:03 -0500

    fix import list

--- pcph-learn/src/Ch05/Playground.hs
+++ pcph-learn/src/Ch05/Playground.hs
@@ -3,16 +3,12 @@ module Playground
   where
 
 -- a playground lib meant to be imported in GHCi
-import qualified Data.Array.Repa as Repa
-import Data.Array.Repa.Index
-    ( (:.)
-    , DIM0, DIM1, DIM2
-    )
-
 import Data.Array.Repa
     ( Array
-    , Z
+    , Z(..)
+    , (:.)(..)
     , U
+    , DIM0, DIM1, DIM2
     , fromListUnboxed
     )
 
