bad3bd54e1cef6457f82bd9d3002f3f6e58d1315
Javran Cheng <Javran.c@gmail.com>
2015-12-05 23:11:13 -0500

    test run

--- pcph-learn/src/Ch06/Playground.hs
+++ pcph-learn/src/Ch06/Playground.hs
@@ -7,6 +7,7 @@ module Playground
 
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.Interpreter as I
+import Control.Arrow
 
 testArray :: Vector Int
 testArray = fromList (Z :. 10) [1..10]
@@ -16,3 +17,7 @@ testArray2 = fromList (Z :. 3 :. 4) [1..]
 
 testArrayTup :: Array DIM2 (Int, Int)
 testArrayTup = fromList (Z :. 2 :. 3) (Prelude.zip [1..] [1..])
+
+-- can (+ 1) be arbitrary operation? "succ" doesn't seem to work
+arrResult :: Array DIM2 Int
+arrResult = run (A.map (+ 1) (use testArray2))
