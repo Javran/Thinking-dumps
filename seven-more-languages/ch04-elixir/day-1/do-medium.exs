1df432dfa3588df34c479bbf4ed85c96dd6bb980
Javran Cheng <Javran.c@gmail.com>
2016-01-03 21:10:35 -0500

    impl find_min

--- seven-more-languages/ch04-elixir/day-1/do-medium.exs
+++ seven-more-languages/ch04-elixir/day-1/do-medium.exs
@@ -3,17 +3,11 @@ defmodule Day1Medium do
   def list_length([]), do: 0
   def list_length([_|tl]), do: 1+list_length(tl)
 
-  def find_max_aux(cur_max,[]), do: cur_max
+  def fold_left(_, seed, []), do: seed
+  def fold_left(g, seed, [hd|tl]), do: fold_left(g, g.(seed,hd), tl)
 
-  def find_max_aux(cur_max,[hd|tl]) do
-    if cur_max < hd do
-      find_max_aux(hd,tl)
-    else
-      find_max_aux(cur_max,tl)
-    end
-  end
-
-  def find_max([hd|tl]), do: find_max_aux(hd,tl)
+  def find_max([hd|tl]), do: fold_left( &max/2, hd, tl )
+  def find_min([hd|tl]), do: fold_left( &min/2, hd, tl )
 
 end
 
@@ -21,9 +15,14 @@ ls1 = []
 ls2 = [1,2,3]
 ls3 = [1468,2560,897,3610,3713,2348]
 
-IO.puts (inspect Day1Medium.list_length(ls1))
-IO.puts (inspect Day1Medium.list_length(ls2))
-IO.puts (inspect Day1Medium.list_length(ls3))
+p = fn (x) -> IO.puts (inspect x) end
 
-IO.puts (inspect Day1Medium.find_max(ls2))
-IO.puts (inspect Day1Medium.find_max(ls3))
+p.( Day1Medium.list_length(ls1))
+p.( Day1Medium.list_length(ls2))
+p.( Day1Medium.list_length(ls3))
+
+p.( Day1Medium.find_max(ls2) )
+p.( Day1Medium.find_max(ls3) )
+
+p.( Day1Medium.find_min(ls2) )
+p.( Day1Medium.find_min(ls3) )
