ecf7dd328478b60be3e3b0947a92f49f8c2228f7
Javran Cheng <Javran.c@gmail.com>
2016-01-04 23:50:54 -0500

    strategy for the tic-tac-toe

--- seven-more-languages/ch04-elixir/day-1/do-hard.exs
+++ seven-more-languages/ch04-elixir/day-1/do-hard.exs
@@ -12,6 +12,18 @@ defmodule Day1Hard do
   end
 
   def traverse(data), do: traverse_with_level(data,0,&make_indent/1)
+
+  # tic-tac-toe best move:
+  # we will represent a board as a tuple: { {a,b,c}, {d,e,f}, {g,h,i} }
+  # all variables are one of: nil, :x, :o
+  # * first determine who is doing the next move (by counting cells taken)
+  # * if there's an obvious way to win, do it
+  # * if enemy has an obvious way of winning, block it
+  # * otherwise we examine empty cells, take the one that has best "potential"
+  # * to estimate "potential", we count lines that might lead to a win (i.e. not
+  #   yet taken by the enemy) (note that if the whole board is empty, the center cell
+  #   will have a potential of 4, the corners 3, and sides 2, leaving the center cell
+  #   the strongest candidate, which is what we want and how a game usually begins)
 end
 
 data = {"See Spot.", {"See Spot sit.", "See Spot run.", {"Go Deeper"}, "Back one level"}}
