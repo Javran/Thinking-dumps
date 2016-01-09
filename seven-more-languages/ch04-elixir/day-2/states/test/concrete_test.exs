943a7fa7a67b243a64f2eed410c20e1557cecdc7
Javran Cheng <Javran.c@gmail.com>
2016-01-08 22:43:33 -0500

    metaprogramming - should

--- seven-more-languages/ch04-elixir/day-2/states/test/concrete_test.exs
+++ seven-more-languages/ch04-elixir/day-2/states/test/concrete_test.exs
@@ -1,6 +1,7 @@
 defmodule ConcreteTest do
   use ExUnit.Case
-  test "should update count" do
+  import Should
+  should "update count" do
     rented_video = VideoStore.renting(video)
     assert rented_video.times_rented == 1
   end
--- seven-more-languages/ch04-elixir/day-2/states/test/test_helper.exs
+++ seven-more-languages/ch04-elixir/day-2/states/test/test_helper.exs
@@ -1,1 +1,9 @@
 ExUnit.start()
+
+defmodule Should do
+  defmacro should(name, options) do
+    quote do
+      test("should #{unquote name}", unquote(options))
+    end
+  end
+end
