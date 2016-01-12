42bc1d0d17c182f9f85440ebbea1d7902759b1df
Javran Cheng <Javran.c@gmail.com>
2016-01-11 22:28:40 -0500

    spec do hard

--- seven-more-languages/ch04-elixir/day-2/states/lib/state_machine_behavior.ex
+++ seven-more-languages/ch04-elixir/day-2/states/lib/state_machine_behavior.ex
@@ -1,4 +1,19 @@
 defmodule StateMachine.Behavior do
+  # we assume the representation of state machines
+  # is of the following structure:
+  # - a Keyword
+  #   - states as keys
+  #   - Keywords as values
+  #     - events as keys
+  #     - Keywords as values
+  #       - to: state after the event is fired
+  #       - calls: callbacks to transform the context
+  #         all calls accepts a context, return the transformed version of it
+  #       - (extended) before_calls: unary functions that take a "transition tuple"
+  #       - (extended) after_calls: unary function that take a "transition tuple"
+  # (an example can be found in VideoStore.Concrete)
+  # the last two are marked "extended", which is part of the exercise.
+  # a "transition tuple" is defined as {before_state, event, after_state}
   def fire(context, event) do
     # NOTE: x |> f(a,b,c) is f(x,a,b,c)
     %{context | state: event[:to]}
