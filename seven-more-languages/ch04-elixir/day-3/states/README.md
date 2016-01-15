9ca12038ab937d62b9091e549ccfa6a64d8507e7
Javran Cheng <Javran.c@gmail.com>
2016-01-14 23:51:52 -0500

    probably need one copy before change

---
+++ seven-more-languages/ch04-elixir/day-3/states/README.md
@@ -0,0 +1,19 @@
+# States
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add states to your list of dependencies in `mix.exs`:
+
+        def deps do
+          [{:states, "~> 0.0.1"}]
+        end
+
+  2. Ensure states is started before your application:
+
+        def application do
+          [applications: [:states]]
+        end
---
+++ seven-more-languages/ch04-elixir/day-3/states/test/concrete_test.exs
@@ -0,0 +1,23 @@
+defmodule ConcreteTest do
+  use ExUnit.Case
+  import Should
+  should "update count" do
+    rented_video = VideoStore.renting(video)
+    assert rented_video.times_rented == 1
+  end
+
+  should "rent video" do
+    rented_video = VideoStore.Concrete.rent video
+    assert :rented = rented_video.state
+    assert 1 == Enum.count( rented_video.log )
+  end
+
+  should "handle multiple transitions" do
+    import VideoStore.Concrete
+    vid = video |> rent |> return |> rent |> return |> rent
+    assert 5 == Enum.count( vid.log )
+    assert 3 == vid.times_rented
+  end
+
+  def video, do: %Video{title: "XMen"}
+end
