f2b2215a8625790484bb91c74b455574eb76822f
Javran Cheng <Javran.c@gmail.com>
2016-01-21 23:53:29 -0500

    action performing on state

--- seven-more-languages/ch04-elixir/day-3/states/lib/vsagent.ex
+++ seven-more-languages/ch04-elixir/day-3/states/lib/vsagent.ex
@@ -1,8 +1,20 @@
 defmodule VSAgent do
   require Agent
+  require VidStore
   
   def start_link(state) do
     Agent.start_link(fn -> state end, name: :vs_agent)
+  end
+
+  def perform_action({action,item}) do
+    Agent.get_and_update(
+      :vs_agent,
+      fn videos ->
+        video = videos[item]
+        new_video = apply VidStore, action, [video]
+        new_state = Keyword.put(videos,item,new_video)
+        { new_video, new_state }
+      end)
   end
 
 end
