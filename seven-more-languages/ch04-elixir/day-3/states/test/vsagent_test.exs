45b4ca662273cc632ddffccb8a6078b0f8173a9a
Javran Cheng <Javran.c@gmail.com>
2016-01-22 23:19:30 -0500

    tests (modified from db)

--- seven-more-languages/ch04-elixir/day-3/states/test/vsagent_test.exs
+++ seven-more-languages/ch04-elixir/day-3/states/test/vsagent_test.exs
@@ -4,11 +4,35 @@ defmodule VSAgentTest do
   require VSAgent
 
   setup do
-    {:ok, pid} = VSAgent.start_link([])
+    VSAgent.start_link([])
+    install_database()
+    :ok
+  end
 
+  def install_database() do
+    VSAgent.cast {:add, {:wolverine, %Video{title: "Wolverine"}}}
+    VSAgent.cast {:add, {:xmen, %Video{title: "X men"}}}
+    VSAgent.cast {:add, {:et, %Video{title: "ET"}}}
+  end
 
+  test "rent all", _ do
+    VSAgent.call {:rent, :wolverine}
+    VSAgent.call {:rent, :xmen}
+    VSAgent.call {:rent, :et}
+  end
 
-    {:ok, [pid: pid]}
+  test "rent multiple times", _ do
+    VSAgent.call {:rent, :wolverine}
+    VSAgent.call {:rent, :xmen}
+    VSAgent.call {:return, :wolverine}
+
+    VSAgent.call {:rent, :wolverine}
+    VSAgent.call {:return, :xmen}
+    result_w = VSAgent.call {:return, :wolverine}
+    assert 2 == result_w.times_rented
+
+    result_x = VSAgent.call {:return, :xmen}
+    assert 1 == result_x.times_rented
   end
 
 end
