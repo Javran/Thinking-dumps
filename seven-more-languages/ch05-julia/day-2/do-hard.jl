e84819734d99fc196f145217422dfb50938930e2
Javran Cheng <Javran.c@gmail.com>
2016-01-29 22:42:03 -0500

    do-hard done

--- seven-more-languages/ch05-julia/day-2/do-hard.jl
+++ seven-more-languages/ch05-julia/day-2/do-hard.jl
@@ -1,5 +1,5 @@
 function test_flip(func)
-    result = @time pflip_coins(100000)
+    result = @time pflip_coins(1000000)
     println( result )
 end
 
@@ -43,4 +43,19 @@ function pflip_coins_fast(times)
     reduce(+, [0,0], results)
 end
 
-println( pflip_coins_fast(10023) )
+test_flip(pflip_coins)
+test_flip(pflip_coins_fast)
+
+# the optimization is significant:
+
+# with "-p 8":
+#   1.053630 seconds (467.31 k allocations: 20.020 MB, 0.32% gc time)
+# [500551,1000000]
+#   0.087363 seconds (8.71 k allocations: 670.547 KB)
+# [499360,1000000]
+
+# without any extra arguments:
+#   0.316822 seconds (3.20 M allocations: 176.912 MB, 8.76% gc time)
+# [500329,1000000]
+#   0.102915 seconds (3.00 M allocations: 167.848 MB, 6.96% gc time)
+# [499460,1000000]
