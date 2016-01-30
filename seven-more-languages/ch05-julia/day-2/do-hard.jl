5189e4f76f94e126aa833ffd672277aacdf51694
Javran Cheng <Javran.c@gmail.com>
2016-01-29 22:37:38 -0500

    done impl

--- seven-more-languages/ch05-julia/day-2/do-hard.jl
+++ seven-more-languages/ch05-julia/day-2/do-hard.jl
@@ -15,6 +15,20 @@ end
 
 # we can optimize "pflip_coins" by partitioning tasks into "bigger" pieces:
 # instead of fliping one single coins, each task can flip coin multiple times.
+
+# this is the task to be performed by each worker.
+function flip_coin_experiment(times)
+    total = 0
+    head = 0
+    for i = 1:times
+        total = total + 1
+        if rand(Bool)
+            head = head + 1
+        end
+    end
+    [head, total]
+end
+
 function pflip_coins_fast(times)
     chunk_size = 1000
     tasklist_size = Int64( ceil(times/chunk_size) )
@@ -25,7 +39,8 @@ function pflip_coins_fast(times)
         tasks[end] = last
     end
 
-    println(tasks)
+    results = map( (x -> @fetch flip_coin_experiment(x)), tasks)
+    reduce(+, [0,0], results)
 end
 
-pflip_coins_fast(2000)
+println( pflip_coins_fast(10023) )
