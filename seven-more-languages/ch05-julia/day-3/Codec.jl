44ae51ff15ec5b008ace6cd8565a5a3aeeede731
Javran Cheng <Javran.c@gmail.com>
2016-01-30 23:45:35 -0500

    codec, not working

--- seven-more-languages/ch05-julia/day-3/Codec.jl
+++ seven-more-languages/ch05-julia/day-3/Codec.jl
@@ -13,11 +13,36 @@ function blockdct6(img)
     mask = zeros(8,8)
     mask[1:3, 1:3] = [1 1 1; 1 1 0; 1 0 0]
     freqs = Array(Float32, (outy*8, outx*8))
+
+    for i = bx, j = by
+        freqs[j:j+7, i:i+7] = dct(pixels[j:j+7, i:i+7])
+        freqs[j:j+7, i:i+7] .*= mask
+    end
+
+    freqs
 end
 
-using TestImages
+function blockidct(freqs)
+    y,x = size(freqs)
+    bx, by = 1:8:x, 1:8:y
+    
+    pixels = Array(Float32, size(freqs))
+    for i = bx, j = by
+        pixels[j:j+7,i:i+7] = idct(freqs[j:j+7,i:i+7]) ./ 255.0
+    end
+    grayim(pixels)
+end
+
+using TestImages, ImageView
 
 img = testimage("cameraman")
-blockdct6(img)
+
+freqs = blockdct6(img)
+img2 = blockidct(freqs)
+
+view(img)
+view(img2)
+
+readline(STDIN)
 
 end
