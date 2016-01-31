8d8a701028091bece010d65a627cca28712bdf19
Javran Cheng <Javran.c@gmail.com>
2016-01-30 23:38:28 -0500

    fix deprecated syntax

---
+++ seven-more-languages/ch05-julia/day-3/Codec.jl
@@ -0,0 +1,23 @@
+module Codec
+
+using Images
+
+function blockdct6(img)
+    pixels = convert(Array{Float32}, img.data)
+    y,x = size(pixels)
+
+    # break into parts
+    outx, outy = floor(Integer, x/8), floor(Integer, y/8)
+    bx, by = 1:8:outx*8, 1:8:outy*8
+
+    mask = zeros(8,8)
+    mask[1:3, 1:3] = [1 1 1; 1 1 0; 1 0 0]
+    freqs = Array(Float32, (outy*8, outx*8))
+end
+
+using TestImages
+
+img = testimage("cameraman")
+blockdct6(img)
+
+end
