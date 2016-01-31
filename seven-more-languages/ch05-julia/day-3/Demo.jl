13809502a1d591445a15d6be686e9ec093334ede
Javran Cheng <Javran.c@gmail.com>
2016-01-30 23:34:15 -0500

    conversion not working

--- seven-more-languages/ch05-julia/day-3/Demo.jl
+++ seven-more-languages/ch05-julia/day-3/Demo.jl
@@ -5,8 +5,12 @@ using TestImages, ImageView
 img = testimage("cameraman")
 
 pixels = img.data[1:8,1:8]
-pixels = convert(Array{Float32}, pixels)
 
-println(pixels)
+pixels = convert(Array{Float32}, pixels)
+freqs = dct(pixels)
+
+# the following line causes an "Inexact error"
+# and I don't know how to fix it
+# pixels2 = convert(Array{UInt8}, idct(freqs))
 
 end
