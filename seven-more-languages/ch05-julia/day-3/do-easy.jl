b514c55d4e88eb892f311e91bb0a728fec45e40e
Javran Cheng <Javran.c@gmail.com>
2016-02-02 21:33:10 -0500

    implement adding high-freq noise

--- seven-more-languages/ch05-julia/day-3/do-easy.jl
+++ seven-more-languages/ch05-julia/day-3/do-easy.jl
@@ -33,6 +33,16 @@ function view_with_noise(img,noise_scale)
     view(img2)
 end
 
-map( x-> view_with_noise(img,x), [0,0.1,1,10])
+function view_with_hf_noise(img,noise_scale)
+    freqs = blockdct6_with_noise_and_high_freq(img,noise_scale,true)
+    img2 = blockidct(freqs)
+    view(img2)
+end
 
+# as noise becomes more significant, the image becomes whiter
+# and we can see a clear boundary between 8x8 blocks when scale goes up.
+map( x-> view_with_noise(img,x), [0,0.1,1,10])
+map( x-> view_with_hf_noise(img,x), [0,0.1,1,10])
+
+println("Input anything to proceed")
 readline(STDIN)
