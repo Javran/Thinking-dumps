a4504fb18a340bd3b46f3c36ef4916f009e39155
Javran Cheng <Javran.c@gmail.com>
2016-02-04 21:02:09 -0500

    do-medium: small output array

--- seven-more-languages/ch05-julia/day-3/do-medium.jl
+++ seven-more-languages/ch05-julia/day-3/do-medium.jl
@@ -52,10 +52,47 @@ using Codec
 
 img = testimage("cameraman")
 
-freqs = blockdct(img,6)
-img2 = blockidct(freqs)
+function task1(img)
+    freqs = blockdct(img,6)
+    img2 = blockidct(freqs)
 
-view(img)
-view(img2)
+    view(img)
+    view(img2)
 
-wait_input()
+    wait_input()
+end
+
+# task1(img)
+
+function blockdct6_small(img)
+    pixels = convert(Array{Float32}, img.data)
+    y,x = size(pixels)
+
+    outx, outy = floor(Integer, x/8), floor(Integer, y/8)
+    bx, by = 1:8:outx*8, 1:8:outy*8
+
+    freqs = Array{Vector{Float32}}(outy, outx)
+    
+    to_freq_ind = x -> 1 + div(x-1,8)
+
+    for i = bx, j = by
+        tmp = dct(pixels[j:j+7, i:i+7])
+        y2,x2 = to_freq_ind(j), to_freq_ind(i)
+        freqs[y2,x2] = Array{Float32,1}(6)
+
+        # NOTE: do explicit separators to allow
+        # arrange elements in an arbitrary manner
+
+        # otherwise Julia thing you are declaring a 2d matrix
+        # and expect every line to be filled completely,
+        # which in my opinion is stupid.
+        # why should the meaning of an array declaration changed
+        # simply because there are newlines characters?
+        freqs[y2,x2][1:6] = [tmp[1,1]; tmp[1,2]; tmp[1,3]; 
+                             tmp[2,1]; tmp[2,2]; 
+                             tmp[2,3]]
+    end
+    freqs
+end
+
+blockdct6_small(img)
