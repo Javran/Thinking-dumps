48efdd57c2f0f31dd2ce849aece78f5ec14a9e69
Javran Cheng <Javran.c@gmail.com>
2016-02-07 23:13:15 -0500

    working single-value conversion

--- seven-more-languages/ch05-julia/day-3/do-hard.jl
+++ seven-more-languages/ch05-julia/day-3/do-hard.jl
@@ -2,9 +2,15 @@ push!(LOAD_PATH, pwd())
 using Images
 using TestImages, ImageView
 using Codec
+using ColorTypes
 
 img = testimage("mandrill")
 
-view(img)
+# color experiment
+v = img[1,1]
 
-wait_input()
+println(typeof(v))
+println(v)
+
+println(convert(YCbCr,v))
+
