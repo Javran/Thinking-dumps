e2bddb6a8dc9027c4881fe0fa1fd90ab6c47c864
Javran Cheng <Javran.c@gmail.com>
2016-02-08 21:26:19 -0500

    address rotation, comments

--- seven-more-languages/ch05-julia/day-3/do-hard.jl
+++ seven-more-languages/ch05-julia/day-3/do-hard.jl
@@ -6,14 +6,19 @@ using ColorTypes
 
 img = testimage("mandrill")
 
+# looks like this conversion also permutes dimension
+# see: http://timholy.github.io/Images.jl/function_reference/
 mat = convert(Array{YCbCr,2}, img.data)
 
 mat_y  = map(x -> x.y , mat)
 mat_cb = map(x -> x.cb, mat)
 mat_cr = map(x -> x.cr, mat)
 
+# we are dealing with matrix of data instead of images
+# so it's unnecessary to convert them from or back to images
+# "blockdct6" and "blockidct" are both modified to remove
+# this step and renamed with postfix "_mat"
 function blockdct6_mat(pixels)
-    # pixels = convert(Array{Float32}, img.data)
     y,x = size(pixels)
 
     # break into parts
@@ -34,20 +39,39 @@ function blockdct6_mat(pixels)
     freqs
 end
 
-f = x -> blockidct( blockdct6_mat(x) )
+function blockidct_mat(freqs)
+    y,x = size(freqs)
+    bx, by = 1:8:x, 1:8:y
+
+    pixels = Array(Float32, size(freqs))
+    for i = bx, j = by
+        # https://forums.pragprog.com/forums/351/topics/13474
+        pixels[j:j+7,i:i+7] = idct(freqs[j:j+7,i:i+7])
+    end
+    pixels
+end
+
+f = x -> blockidct_mat( blockdct6_mat(x) )
 
 result_y = f(mat_y)
 result_cb = f(mat_cb)
 result_cr = f(mat_cr)
 
-y,x = size(mat)
-result = Array(YCbCr, (y, x))
+# in case the transform crops the image,
+# we use the resulting size instead of the original one
+y,x = size(result_y)
+# note that in result we permute two dimensions
+# because the previously "convert" permutes it
+# and for now I simply don't know how to convert it back
+# without explicitly permute two dimensions here.
+# (looks like "colorim" is not working for a 2-d RGB matrix)
+result = Array(YCbCr, (x,y))
 
 # why we got a rotated image?
 for i=1:x, j=1:y
     # need to find an explanation to this rotation, for now
     # we just switch two coordinates to rotate it back
-    result[j,i] = YCbCr(result_y[i,j],result_cb[i,j],result_cr[i,j])
+    result[i,j] = YCbCr(result_y[j,i],result_cb[j,i],result_cr[j,i])
 end
 
 result2 = convert(Array{RGB,2},result)
