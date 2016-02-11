3c5ed6946d828d4b18bc0e6967cd2f31b7a042ce
Javran Cheng <Javran.c@gmail.com>
2016-02-10 21:47:39 -0500

    encode some more facts

--- seven-more-languages/ch06-minikanren/day-1/logical/src/logical/day1_easy.clj
+++ seven-more-languages/ch06-minikanren/day-1/logical/src/logical/day1_easy.clj
@@ -1,7 +1,27 @@
 (ns logical.day1-easy)
 
 (use 'clojure.core.logic)
+(use 'clojure.core.logic.pldb)
 (use 'logical.utils)
+(use 'logical.day1-book)
+
+(db-rel languageo x l)
+(db-rel system x s)
+
+(def facts1
+  (-> facts
+      ;; I don't have a clear idea about what should be categorized
+      ;; as "systemo", so don't be mad at me if I've got something wrong xD
+      (db-fact systemo :alan-turing :bombe)
+      (db-fact systemo :grace-hopper :univac)
+      (db-fact languageo :grace-hopper :cobol)
+      (db-fact systemo :leslie-lamport :distributed-systems)
+      (db-fact languageo :leslie-lamport :latex)
+      (db-fact languageo :alonzo-church :lambda-calculus)
+      (db-fact systemo :ada-lovelace :analytical-engine)
+      (db-fact systemo :barbara-liskov :liskov-substitiion-principle)
+      (db-fact languageo :john-mccarthy :lisp)
+      ))
 
 (defn day1-easy
   []
