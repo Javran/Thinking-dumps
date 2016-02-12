478a626202a9d27f1b9bbfedc76d74df7dda995d
Javran Cheng <Javran.c@gmail.com>
2016-02-11 23:26:53 -0500

    example from SICP

--- seven-more-languages/ch06-minikanren/day-1/logical/src/logical/day1_medium.clj
+++ seven-more-languages/ch06-minikanren/day-1/logical/src/logical/day1_medium.clj
@@ -10,6 +10,30 @@
    [(mano p)]
    [(womano p)]))
 
+;; c is p's child
+(db-rel childo p c)
+;; p2 is p1's wife
+(db-rel wifeo p1 p2)
+
+(defn spouseo [p1 p2]
+  (conde
+   [(wifeo p1 p2)]
+   [(wifeo p2 p1)]))
+
+(def genealogy-facts
+  ;; see: SICP ex 4.63 xD
+  (db
+   [childo :adam :cain]
+   [childo :cain :enoch]
+   [childo :enoch :irad]
+   [childo :irad :mehujael]
+   [childo :mehujael :methushael]
+   [childo :methushael :lamech]
+   [childo :ada :jabal]
+   [childo :ada :jubal]
+
+   [wifeo :lamech :ada]))
+
 (defn day1-medium
   []
   (p "day 1 - do medium")
