71dfe29b5576fd9ae6c3897f8a73a5d1e8653da0
Javran Cheng <Javran.c@gmail.com>
2016-02-20 22:56:57 -0500

    make sure it's a list of scientists

--- seven-more-languages/ch06-minikanren/logical/src/logical/day2_medium.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day2_medium.clj
@@ -1,9 +1,23 @@
 (ns logical.day2-medium)
 
 (use 'clojure.core.logic)
+(use 'clojure.core.logic.pldb)
 (use 'logical.utils)
 (use 'logical.day1-book)
+(use 'logical.day1-medium)
+
+(defn unsungo [xs]
+  (matche [xs]
+    ([[]])
+    ([ [hd . tl]]
+     ;; hd should be a scientist at the first place
+     (scientisto hd)
+     (unsungo tl)
+     )))
 
 (defn day2-medium
   []
-  (p "day 2 - do medium"))
+  (p "day 2 - do medium")
+  (p
+   (with-db facts (run* [q] (unsungo [:alan-turing :grace-hopper]))))
+  )
