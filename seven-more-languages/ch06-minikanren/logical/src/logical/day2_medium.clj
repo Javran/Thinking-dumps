0174c0510a73ced273503e970672a3e003bfb037
Javran Cheng <Javran.c@gmail.com>
2016-02-20 23:01:42 -0500

    hopefully this will work

--- seven-more-languages/ch06-minikanren/logical/src/logical/day2_medium.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day2_medium.clj
@@ -12,8 +12,14 @@
     ([ [hd . tl]]
      ;; hd should be a scientist at the first place
      (scientisto hd)
-     (unsungo tl)
-     )))
+     (conda
+      [(fresh [y]
+         (turingo hd y))
+       ;; make it impossible to unify in this branch
+       ;; at this point there's no way to go back
+       ;; so we've somehow managed to express negation
+       (== 1 2)]
+      [(unsungo tl)]))))
 
 (defn day2-medium
   []
