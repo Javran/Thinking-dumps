03ad308b69eca83978daa6feae771f46c04ddf2a
Javran Cheng <Javran.c@gmail.com>
2016-02-21 21:10:13 -0500

    day 2 - do hard explained

--- seven-more-languages/ch06-minikanren/logical/src/logical/day2_hard.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day2_hard.clj
@@ -8,6 +8,25 @@
   ([_ [_ . t]]
    (insideo e t)))
 
+;; the problem with "insideo" is:
+;; when "e" matches the head of "l", it goes to both branches
+;; and in second branch we cannot tell whether the current head element
+;; is a duplicated one (because that info is on "another universe")
+;; our solution is to keep track of this information in the second branch
+;; one way to do this is to use constraint "!=" (let's call it "insideo1")
+;; when we run "insideo1" backwards, it goes to both branch, and at the same
+;; time, one branch has the fact that e == h while another e != h.
+;; as we traverse through the list, the second branch will accumulate
+;; "!=" constraints, this has the effect of eliminating duplicated elements
+;; let's make an example to see how this works:
+;; say we are running "(insideo1 q [:a :b :a])":
+;; * first branch succeeds, with solution that "q == :a"
+;; * second branch succeeds, with constraint that "q != :a"
+;;   * recursively, first branch succeeds with "q != :a and q == :b"
+;;   * second branch succeeds with constraint that "q != a and q != b"
+;;     * recursively, first branch fails because we already know "q != a"
+;;     * second branch succeeds, with constraints still being "q != a and q != b"
+;;       * recursively, no branch will match the empty list, and the query fails.
 (defne insideo1 [e l]
   ([_ [h . t]]
    (conde
