44cf4a4715f2bd9b6a7af7747778c35745da7e57
Javran Cheng <Javran.c@gmail.com>
2016-02-23 21:46:31 -0500

    some example with conde

--- seven-more-languages/ch06-minikanren/logical/src/logical/day3_easy.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day3_easy.clj
@@ -15,4 +15,15 @@
      (fd/<= a b)
      (fd/<= b c)
      (fd/eq
-      (= (+ (* a a) (* b b)) (* c c))))))
+      (= (+ (* a a) (* b b)) (* c c)))))
+  ;; some examples to see "conde" working in action
+  ;; every point within [0:10,0:10] that satisfies
+  ;; either x + y == 10 or x == y
+  (p
+   (run* [a b]
+     (fd/in a b (fd/interval 0 10))
+     (conde
+      [(fd/eq
+        (= a b))]
+      [(fd/eq
+        (= (+ a b) 10))]))))
