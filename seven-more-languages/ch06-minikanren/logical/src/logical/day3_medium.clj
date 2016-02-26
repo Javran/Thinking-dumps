5ae89ec92fe2f3efef962d94bb2eeca326dd9746
Javran Cheng <Javran.c@gmail.com>
2016-02-25 23:08:32 -0500

    action application

--- seven-more-languages/ch06-minikanren/logical/src/logical/day3_medium.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day3_medium.clj
@@ -5,10 +5,14 @@
 (use 'logical.day3-book)
 (use 'logical.utils)
 
-;; we need to push stroy events to the end to learn about murderers.
+;; We need to push stroy events to the end to learn about murderers.
 ;; to do so, we can get all resources available after generated events,
 ;; try applying more events to consume these resources (and get new resources)
 ;; until nothing can be further consumed.
+;; A potential problem of this might be the inconsistency of the stroy,
+;; since we are not using the logic framework anymore, we have to ensure
+;; the consistency ourselves. However, in this case this is not a problem
+;; as long as we guarantee that one resource can only be consumed once.
 
 ;; make an dictionary of actions from story-elements
 (defn mk-action-dict
@@ -18,8 +22,16 @@
     (assoc ad k (conj (get ad k []) v)))
   (reduce combine {} story-elements))
 
+(defn apply-action
+  [state [k v]]
+  (let [old-res-ind (.indexOf state k)]
+    (assert (not= old-res-ind -1)
+            (str "resource not available: " k))
+    (conj (filter #(not= k %) state) v)))
+
 (defn day3-medium
   []
   (p "day 3 - do medium")
-  (p (mk-action-dict story-elements))
+  (p (apply-action [:maybe-motorist :dead-telegram-girl]
+                   [:maybe-motorist :guilty-peacock]))
   )
