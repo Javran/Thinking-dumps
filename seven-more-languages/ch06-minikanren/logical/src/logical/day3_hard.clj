ad93e91ba2c4de6e7688b47037fd5c595011b15d
Javran Cheng <Javran.c@gmail.com>
2016-03-03 23:41:54 -0500

    cell val from symbol?

--- seven-more-languages/ch06-minikanren/logical/src/logical/day3_hard.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day3_hard.clj
@@ -17,6 +17,13 @@
 
 (defn solve-sudoku
   [puzzle]
+  ;; get cell value from symbol
+  (defn get-cell
+    [sym]
+    (let [s (name sym)
+          row (- (int (get s 1)) (int \0))
+          col (- (int (get s 2)) (int \0))]
+      (get (get puzzle row) col)))
   (run* [c00 c01 c02 c03 c04 c05 c06 c07 c08
          c10 c11 c12 c13 c14 c15 c16 c17 c18
          c20 c21 c22 c23 c24 c25 c26 c27 c28
@@ -28,7 +35,6 @@
          c60 c61 c62 c63 c64 c65 c66 c67 c68
          c70 c71 c72 c73 c74 c75 c76 c77 c78
          c80 c81 c82 c83 c84 c85 c86 c87 c88]
-
     (fd/in c00 (fd/interval 1 9))))
 
 (defn day3-hard
@@ -40,8 +46,9 @@
   ;; in order to write constraints done,
   ;; I don't know if we can do something better,
   ;; but let's first get it to work.
-  (p (parse-sudoku
-      ["1___"
-       "_21_"
-       "__3_"
-       "___4"])))
+  (p (solve-sudoku
+      (parse-sudoku
+       ["1___"
+        "_21_"
+        "__3_"
+        "___4"]))))
