40caae326bc2d91f5fb1a923a12a923a998e48f8
Javran Cheng <Javran.c@gmail.com>
2016-03-03 23:33:13 -0500

    table of variables

--- seven-more-languages/ch06-minikanren/logical/src/logical/day3_hard.clj
+++ seven-more-languages/ch06-minikanren/logical/src/logical/day3_hard.clj
@@ -15,10 +15,31 @@
       nil))
   (vec (map #(vec (map parse-char %)) rawvec)))
 
+(defn solve-sudoku
+  [puzzle]
+  (run* [c00 c01 c02 c03 c04 c05 c06 c07 c08
+         c10 c11 c12 c13 c14 c15 c16 c17 c18
+         c20 c21 c22 c23 c24 c25 c26 c27 c28
+
+         c30 c31 c32 c33 c34 c35 c36 c37 c38
+         c40 c41 c42 c43 c44 c45 c46 c47 c48
+         c50 c51 c52 c53 c54 c55 c56 c57 c58
+
+         c60 c61 c62 c63 c64 c65 c66 c67 c68
+         c70 c71 c72 c73 c74 c75 c76 c77 c78
+         c80 c81 c82 c83 c84 c85 c86 c87 c88]
+
+    (fd/in c00 (fd/interval 1 9))))
+
 (defn day3-hard
   []
   (p "day 3 - do hard")
   (p "exercise 1")
+  ;; remember that when we were dealing with prolog in
+  ;; "Seven Languages in Seven Weeks", we have to list all variables
+  ;; in order to write constraints done,
+  ;; I don't know if we can do something better,
+  ;; but let's first get it to work.
   (p (parse-sudoku
       ["1___"
        "_21_"
