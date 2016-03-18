3f56cdc53893ed1287e186efb07dbbefc5c2acc1
Javran Cheng <Javran.c@gmail.com>
2016-03-17 23:48:40 -0400

    codes in day 1

--- seven-more-languages/ch03-elm/my-package/src/Main.elm
+++ seven-more-languages/ch03-elm/my-package/src/Main.elm
@@ -1,6 +1,16 @@
 import Html exposing (..)
 import List
 
+product : List number -> number
+product xs =
+  case xs of
+    [] -> 1 
+    hd :: tl -> hd * product tl
+
 main =
   div [] (List.map (\x -> div [] [text x])
-            ["Hello", "second line", "excited"])
+            [ "Hello"
+            , "second line"
+            , "excited"
+            , "product of 2,3,5,7: " ++ toString (product [2,3,5,7])
+            ])
