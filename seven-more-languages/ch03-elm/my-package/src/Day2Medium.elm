851b9312d820ed3bbc991c6df30471e7bcd8e7b6
Javran Cheng <Javran.c@gmail.com>
2016-03-21 23:49:34 -0400

    day 2 medium, might need to work on coords

--- seven-more-languages/ch03-elm/my-package/src/Day2Medium.elm
+++ seven-more-languages/ch03-elm/my-package/src/Day2Medium.elm
@@ -3,9 +3,20 @@ module Day2Medium where
 import Tools exposing (..)
 import Time
 import Signal
+import Mouse exposing (..)
+import Window exposing (..)
+
+import Graphics.Collage exposing (..)
+import Color exposing (..)
+
+drawShape (w,h) (x,y) =
+  collage w h [ filled black (rect 10 20) |> move (toFloat x, toFloat y)]
+
+main = 
+  Signal.map2 drawShape Window.dimensions Mouse.position
 
 -- counts up from zero with one count per second
-main =
+main2 =
   Time.every Time.second
   |> Signal.foldp (\_ s -> s + 1) 0
   |> Signal.map asDiv
