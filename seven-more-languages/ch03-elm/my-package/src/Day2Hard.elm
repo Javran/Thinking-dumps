a618b058e117bd96b0ba110c5a2e9c3279401fd3
Javran Cheng <Javran.c@gmail.com>
2016-03-22 23:52:35 -0400

    not sure about the exercise

---
+++ seven-more-languages/ch03-elm/my-package/src/Day2Hard.elm
@@ -0,0 +1,20 @@
+module Day2Hard where
+
+import Color exposing (..)
+import Graphics.Collage exposing (..)
+
+carBottom = filled black (rect 160 50)
+carTop =    filled black (rect 100 60)
+tire = filled red (circle 24)
+
+drawCar = collage 300 300
+  [ carBottom
+  , carTop |> moveY 30
+  , tire |> move (-40, -28)
+  , tire |> move ( 40, -28) 
+  ]
+
+-- for now I'm not sure about
+-- what the exercise is asking us to do
+-- if we just move cars from left to right
+-- or right to left, how can we move across the bottom of the screen?
