bc8ccf3fa07cb39b73622f122aad61e583af3bcd
Javran Cheng <Javran.c@gmail.com>
2016-03-25 23:09:47 -0400

    make most of the stuff work

--- seven-more-languages/ch03-elm/my-package/src/LanguageHeads.elm
+++ seven-more-languages/ch03-elm/my-package/src/LanguageHeads.elm
@@ -1,9 +1,18 @@
 module LanguageHead where 
 
-import Keyboard
-import Mouse
-import Random
+import Keyboard exposing (..)
+import Mouse exposing (..)
+import Text exposing (..)
+import Time exposing (..)
+import Signal.Extra exposing (..)
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+import Color exposing (..)
 import Text
+
+import List
+import Signal
+import Random
 
 type State = Play | Pause | GameOver
 
@@ -51,14 +60,14 @@ bottom = 550
 secsPerFrame = 1.0 / 50.0  
 delta = inSeconds <~ fps 50
 
-input = sampleOn delta (Input <~ Keyboard.space
+input = Signal.sampleOn delta (Input <~ Keyboard.space
                                ~ Mouse.x
                                ~ delta
                                ~ Random.range 0 4 (every secsPerFrame))
 
-main = lift display gameState
+main = Signal.map display gameState
 
-gameState = foldp stepGame defaultGame input
+gameState = Signal.foldp stepGame defaultGame input
 -- END:part3
 
 -- START:part4a
@@ -77,7 +86,7 @@ stepGameOver x heads =
   if allHeadsSafe (toFloat x) heads then Play else GameOver
   
 allHeadsSafe x heads =
-    all (headSafe x) heads
+    List.all (headSafe x) heads
 
 headSafe x head =
     head.y < bottom || abs (head.x - x) < 50
@@ -91,11 +100,11 @@ stepHeads heads delta x score rand =    -- (5)
   |> moveHeads delta
   
 spawnHead score heads rand =   -- (6)
-  let addHead = length heads < (score // 5000 + 1) 
-    && all (\head -> head.x > 107.0) heads in 
+  let addHead = List.length heads < (score // 5000 + 1) 
+    && List.all (\head -> head.x > 107.0) heads in 
   if addHead then defaultHead rand :: heads else heads
 
-bounceHeads heads = map bounce heads      -- (7)
+bounceHeads heads = List.map bounce heads      -- (7)
 
 bounce head = 
   { head | vy = if head.y > bottom && head.vy > 0 
@@ -106,7 +115,7 @@ removeComplete heads = filter (\x -> not (complete x)) heads  -- (8)
 
 complete {x} = x > 750
 
-moveHeads delta heads = map moveHead heads     -- (9)
+moveHeads delta heads = List.map moveHead heads     -- (9)
 
 moveHead ({x, y, vx, vy} as head) = 
   { head | x = x + vx * secsPerFrame
@@ -123,7 +132,7 @@ stepPlayer player mouseX heads =     -- (10)
 stepScore player heads =   -- (11)
   player.score + 
   1 + 
-  1000 * (length (filter complete heads))
+  1000 * (List.length (List.filter complete heads))
 -- END:part4c
 
 -- START:part4d
@@ -158,7 +167,7 @@ drawBuilding w h =
   filled red (rect 100 (toFloat h)) 
   |> moveX (-(half w) + 50)
 
-drawHeads w h heads = map (drawHead w h) heads   -- (17)
+drawHeads w h heads = Signal.map (drawHead w h) heads   -- (17)
 
 drawHead w h head = 
   let x = half w - head.x
@@ -179,9 +188,9 @@ drawScore w h player =     -- (19)
   toForm (fullScore player) 
   |> move (half w - 150, half h - 40)
 
-fullScore player = txt (Text.height 50) (show player.score)
+fullScore player = txt (Text.height 50) (toString player.score)
 
-txt f = leftAligned << f << monospace << Text.color blue << toText
+txt f = leftAligned << f << monospace << Text.color blue << Text.fromString
 
 drawMessage w h state =    -- (20)
   toForm (txt (Text.height 50) (stateMessage state)) 
@@ -189,4 +198,3 @@ drawMessage w h state =    -- (20)
   
 stateMessage state = 
   if state == GameOver then "Game Over" else "Language Head"
--- END:part5
