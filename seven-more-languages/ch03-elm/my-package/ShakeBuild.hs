928ed059b2bee3ab785215334f7049b82c96dd7a
Javran Cheng <Javran.c@gmail.com>
2016-04-05 00:16:32 -0400

    build up index file

--- seven-more-languages/ch03-elm/my-package/ShakeBuild.hs
+++ seven-more-languages/ch03-elm/my-package/ShakeBuild.hs
@@ -7,6 +7,11 @@ main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
     want ["output/Day3.html"]
 
+    priority 2 $ "output//index.html" %> \out -> do
+        need ["index.md"]
+        () <- cmd "pandoc" "index.md" "-o" out
+        pure ()
+
     "output//*.html" %> \out -> do
         let src = "src" </> dropDirectory1 (out -<.> "elm")
         () <- cmd "elm-make" src "--output" out
