29cc6e42eb30620eb9cead39b341bfcb2ee77045
Javran Cheng <Javran.c@gmail.com>
2016-04-04 23:59:37 -0400

    testing..

--- seven-more-languages/ch03-elm/my-package/ShakeBuild.hs
+++ seven-more-languages/ch03-elm/my-package/ShakeBuild.hs
@@ -2,7 +2,12 @@ import Development.Shake
 import Development.Shake.Command
 import Development.Shake.FilePath
 import Development.Shake.Util
- 
+
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+    want ["output/Day3.html"]
+
+    "output/Day3.html" %> \out -> do
+        pure ()
+
     pure ()
