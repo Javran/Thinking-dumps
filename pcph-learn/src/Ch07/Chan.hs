7901986623c25ca2aeedb69ce04a9a8f3b744eae
Javran Cheng <Javran.c@gmail.com>
2016-04-07 23:50:17 -0400

    writeChan

--- pcph-learn/src/Ch07/Chan.hs
+++ pcph-learn/src/Ch07/Chan.hs
@@ -1,4 +1,4 @@
-import Control.Concurrent
+import Control.Concurrent hiding (Chan)
 import Control.Monad
 import System.IO
 
@@ -8,6 +8,23 @@ data Chan a
 data Item a = Item a (Stream a)
 
 type Stream a = MVar (Item a)
+
+newChan :: IO (Chan a)
+newChan = do
+    -- new channel with empty content
+    hole <- newEmptyMVar
+    readVar <- newMVar hole
+    writeVar <- newMVar hole
+    pure (Chan readVar writeVar)
+
+writeChan :: Chan a -> a -> IO ()
+writeChan (Chan _ writeVar) val = do
+    newHole <- newEmptyMVar
+    oldHole <- takeMVar writeVar
+    -- put value under writer pointer
+    putMVar oldHole (Item val newHole)
+    -- move write pointer forward
+    putMVar writeVar newHole
 
 main :: IO ()
 main = pure ()
