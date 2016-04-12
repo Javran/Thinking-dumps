4bc744e4d28609fb4d08792bd6e3928629bec4e0
Javran Cheng <Javran.c@gmail.com>
2016-04-11 23:25:15 -0400

    helper: oneAndRests

--- pcph-learn/src/Ch08/Merging.hs
+++ pcph-learn/src/Ch08/Merging.hs
@@ -5,6 +5,7 @@ import GetURL
 import Control.Concurrent
 import Control.Monad
 import Control.Exception
+import Data.List
 
 -- in the book `waitAny` is used to wait for one of the actions to be completed
 -- and here I would like to experiment extending `waitAny` a bit
@@ -28,6 +29,20 @@ wait a = do
     case r of
         Left e -> throwIO e
         Right v -> pure v
+
+{-# ANN module "HLint: ignore Use zipWith" #-}
+takeSameLengthAs :: [a] -> [b] -> [b]
+takeSameLengthAs xs ys = map snd (zip xs ys)
+
+-- transform a list so that each element turns into that element together
+-- with rest of the elements. order in the list is not preserved.
+oneAndRests :: [a] -> [(a,[a])]
+oneAndRests [] = []
+oneAndRests xs = cut (map (f . cut) . tails $ cycle xs)
+  where
+    cut = takeSameLengthAs xs
+    f (y:ys) = (y,ys)
+    f _ = error "impossible"
 
 main :: IO ()
 main = pure ()
