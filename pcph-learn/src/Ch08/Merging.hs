73140d8e9b869954387baf0913877fd9b6dafbe0
Javran Cheng <Javran.c@gmail.com>
2016-04-11 23:03:32 -0400

    start an experiment

---
+++ pcph-learn/src/Ch08/Merging.hs
@@ -0,0 +1,9 @@
+module Merging where
+
+-- in the book `waitAny` is used to wait for one of the actions to be completed
+-- and here I would like to experiment extending `waitAny` a bit
+-- so that we can not only tell which action completes first, but also
+-- get a full list of completed results in order.
+
+main :: IO ()
+main = pure ()
