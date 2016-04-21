a550060a7771be2432040cb82d02e4f6a0096172
Javran Cheng <Javran.c@gmail.com>
2016-04-20 21:42:17 -0400

    ex 2.1

--- purely-functional-data-structures/Ch02Exercise1.hs
+++ purely-functional-data-structures/Ch02Exercise1.hs
@@ -1,5 +1,29 @@
 module Ch02Exercise1 where
 
+import Test.Hspec
+import Test.QuickCheck
+import Data.List
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+{-
+
+"suffixes" runs in O(n) time because it only traverses
+it argument list once.
+
+on each branch of it, only constant number of nodes are created
+(either "[[]]" or an new node by using "(:)")
+
+therefore it takes O(n) space for representing the resulting list.
+
+-}
+
 suffixes :: [a] -> [[a]]
 suffixes [] = [[]]
 suffixes l@(_:xs) = l : suffixes xs
+
+main :: IO ()
+main = hspec $ do
+    describe "suffixes" $ do
+      it "is the same as tails" $ do
+        property $ \xs -> tails xs == suffixes (xs :: [Int])
