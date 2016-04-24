a82373acbe8767a90f6912f1047cf3af25fa23d0
Javran Cheng <Javran.c@gmail.com>
2016-04-23 23:59:18 -0400

    comments

--- purely-functional-data-structures/Ch02Exercise3.hs
+++ purely-functional-data-structures/Ch02Exercise3.hs
@@ -6,6 +6,10 @@ data BST a = E | T (BST a) a (BST a) deriving (Show)
 
 -- the proper way of implementing "exception" is to make use of Maybe
 -- let's try doing it.
+-- instead of duplicating nodes before the recursive call,
+-- we do insertion from leaf to root, if we can find
+-- the value in the tree, then we immediately know
+-- there is no need duplicating any node of the tree.
 insertM :: Ord a => a -> BST a -> Maybe (BST a)
 insertM v E = Just $ T E v E
 insertM v (T l x r)
