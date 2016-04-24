13095055da0a3ba0bd04f4d634eb765c7321c2ba
Javran Cheng <Javran.c@gmail.com>
2016-04-24 00:03:49 -0400

    testcase

--- purely-functional-data-structures/Ch02Exercise3.hs
+++ purely-functional-data-structures/Ch02Exercise3.hs
@@ -1,6 +1,12 @@
 module Ch02Exercise3 where
 
+import Test.Hspec
+import Test.QuickCheck
+import qualified Data.IntSet as IS
+
 import Data.Maybe
+
+{-# ANN module "HLint: ignore Redundant do" #-}
 
 data BST a = E | T (BST a) a (BST a) deriving (Show)
 
@@ -20,3 +26,33 @@ insertM v (T l x r)
 
 insert :: Ord a => a -> BST a -> BST a
 insert v t = fromMaybe t (insertM v t)
+
+-- the "member" function we all know
+member :: Ord a => a -> BST a -> Bool
+member _ E = False
+member v (T l x r)
+    | v < x = member v l
+    | v > x = member v r
+    | otherwise = v == x
+
+fromList :: Ord a => [a] -> BST a
+fromList = foldr insert E
+
+toAscList :: BST a -> [a]
+toAscList E = []
+toAscList (T l v r) = toAscList l ++ v : toAscList r
+
+-- using the same code to test set behavior
+-- but this time we just want to test insertion
+main :: IO ()
+main = hspec $ do
+    describe "member" $ do
+      it "should be the same as Data.IntSet" $ do
+        property $ do
+            -- randomly picking up a list of elements
+            xs <- listOf (choose (0,1000 :: Int))
+            let s1 = IS.fromList xs
+                s2 = fromList xs
+            -- then test membership on all values within this range
+            -- and their results should be the same.
+            pure $ all (\x -> member x s2 == IS.member x s1) [0..1000]
