4542708a6ba21eec25cf49d739739d67fdfe6350
Javran Cheng <Javran.c@gmail.com>
2016-04-25 22:22:00 -0400

    implement "balanced"

--- purely-functional-data-structures/Ch02Exercise5.hs
+++ purely-functional-data-structures/Ch02Exercise5.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Ch02Exercise5 where
 import Test.Hspec
 import Test.QuickCheck
@@ -19,6 +20,50 @@ complete _ 0 = E
 complete v s = T sub v sub
   where
     sub = complete v (s-1)
+
+balanced :: forall a. a -> Int -> BST a
+balanced _ 0 = E
+balanced v n
+    | odd n =
+        let sub = balanced v halfN
+        in T sub v sub
+    | otherwise =
+        let (l,r) = create2 halfN
+        in T l v r
+  where
+    halfN = n `div` 2
+
+    create2 :: Int -> (BST a, BST a)
+    create2 m
+        | odd m =
+            let subL = balanced v m
+            in (subL, oneMore m subL)
+        | otherwise =
+            let mp1 = m+1
+                subR = balanced v mp1
+            in (oneLess mp1 subR, subR)
+
+    -- | INVARIANT for "oneMore n t" and "oneLess n t"
+    -- * size t == n
+    -- * t is always balanced
+    -- * one of the following must be true for all nodes t1 in t:
+    --     * size (left t1)    == size (right t1)
+    --     * size (left t1) +1 == size (right t1)
+    oneMore :: Int -> BST a -> BST a
+    oneLess :: Int -> BST a -> BST a
+
+    oneMore 0 E = T E v E
+    oneMore 1 (T E _ E) = T E v (T E v E)
+    oneMore m (T l _ r)
+        | even (m-1) = T l v (oneMore ((m-1) `div` 2) r)
+        | otherwise = T (oneMore ((m-1) `div` 2) l) v r
+    oneMore _ _ = error "oneMore: invariant violated"
+
+    oneLess 1 (T E _ E) = E
+    oneLess m (T l _ r)
+        | even (m-1) = T (oneLess ((m-1) `div` 2) l) v r
+        | otherwise = T l v (oneLess ((m-1) `div` 2 + 1) r)
+    oneLess _ _ = error "oneLess: invariant violated"
 
 -- but this time we just want to test insertion
 main :: IO ()
