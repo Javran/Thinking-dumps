fa9fd2b948ccf551d1839d749d076b2dfed8553e
Javran Cheng <Javran.c@gmail.com>
2016-04-26 23:36:00 -0400

    member and insert

--- purely-functional-data-structures/Ch02Exercise6.hs
+++ purely-functional-data-structures/Ch02Exercise6.hs
@@ -1,6 +1,7 @@
 module Ch02Exercise6 where
 
 import Prelude hiding (lookup)
+import Data.Maybe
 import Ch02BST (BST(..))
 
 type FiniteMap k v = BST (k,v)
@@ -12,3 +13,14 @@ lookup x (T l (k,v) r)
     | x > k = lookup x r
     | x == k = Just v
     | otherwise = error "lookup: impossible"
+
+member :: Ord a => a -> FiniteMap a b -> Bool
+member x = isJust . lookup x
+
+insert :: Ord a => a -> b -> FiniteMap a b -> FiniteMap a b
+insert k v E = T E (k,v) E
+insert k v (T l e@(curK,_) r)
+    | k < curK = T (insert k v l) e r
+    | k > curK = T l e (insert k v r)
+    | k == curK = T l (k,v) r
+    | otherwise = error "insert: impossible"
