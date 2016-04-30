9a8a288ba8c7969f5648a13bd171242f867af762
Javran Cheng <Javran.c@gmail.com>
2016-04-29 23:54:36 -0400

    basic operation impl

--- purely-functional-data-structures/Ch03Leftist.hs
+++ purely-functional-data-structures/Ch03Leftist.hs
@@ -8,3 +8,20 @@ empty = E
 isEmpty :: Heap a -> Bool
 isEmpty E = True
 isEmpty _ = False
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+merge l E = l
+merge E r = r
+merge h1@(T _ x a1 b1) h2@(T _ y a2 b2) =
+  if x <= y
+    then makeT x a1 (merge b1 h2)
+    else makeT y a2 (merge h1 b2)
+
+rank :: Heap a -> Int
+rank E = 0
+rank (T rnk _ _ _) = rnk
+
+makeT :: Ord a => a -> Heap a -> Heap a -> Heap a
+makeT x a b = if rank a >= rank b
+  then T (rank b+1) x a b
+  else T (rank a+1) x b a
