fe74cac2b7e9c8c80f56b8e666028df2289a928a
Javran Cheng <Javran.c@gmail.com>
2016-05-01 00:40:03 -0400

    expanded definition

--- purely-functional-data-structures/Ch03Exercise2.hs
+++ purely-functional-data-structures/Ch03Exercise2.hs
@@ -7,12 +7,24 @@ import Ch03Leftist hiding (insert, sortByHeap, main)
 
 {-# ANN module "HLint: ignore Redundant do" #-}
 
+-- to define "insert" without using "merge"
+-- we just need to expand the definition of "merge"
+-- and manually replace some sub-expressions with more efficient ones
+-- thanks to referential transparency, the correctness can be preserved.
 insert :: Ord a => a -> Heap a -> Heap a
 insert x E = singleton x
 insert x h2@(T _ y a2 b2) =
     if x <= y
-      then makeT x E (merge E h2)
-      else makeT y a2 (merge (singleton x) b2)
+      then
+        -- originally this is:
+        -- > makeT x E h2
+        -- swaping last 2 arguments should make no difference:
+        -- > makeT x h2 E
+        -- obviously rank h2 >= rank E = 0 always holds
+        -- so let's just replace the sub-expression in that branch to here.
+        T 1 x h2 E
+      else
+        makeT y a2 (insert x b2)
 
 sortByHeap :: Ord a => [a] -> [a]
 sortByHeap = toAscList . foldr insert empty
