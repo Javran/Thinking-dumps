6d4177373550645625a176fa9652d260cfc682b9
Javran Cheng <Javran.c@gmail.com>
2016-05-07 22:39:14 -0400

    few annotation

--- purely-functional-data-structures/Ch03Binominal.hs
+++ purely-functional-data-structures/Ch03Binominal.hs
@@ -14,9 +14,22 @@ link t1@(Node r x1 c1) t2@(Node _ x2 c2) =
 rank :: Tree a -> Int
 rank (Node v _ _) = v
 
+-- | "insTree" is an internal function,
+-- | "insTree t ts" should only be called when rank t <= rank t'
+-- | otherwise trees of different ranks will be passed to "link"
+-- | breaking its invariant.
 insTree :: Ord a => Tree a -> Heap a -> Heap a
 insTree t [] = [t]
 insTree t ts@(t':ts') =
     if rank t < rank t'
       then t : ts
       else insTree (link t t') ts'
+
+singleton :: a -> Tree a
+singleton v = Node 0 v []
+
+{-# ANN insert "HLint: ignore Eta reduce" #-}
+-- creates a singleton tree, carries it until an unoccupied rank
+-- is found in the list of trees
+insert :: Ord a => a -> Heap a -> Heap a
+insert x ts = insTree (singleton x) ts
