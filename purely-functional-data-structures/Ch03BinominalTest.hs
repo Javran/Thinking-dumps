93bf7dfaa49cd57b82a6f256a4ab0541a1fbf3ac
Javran Cheng <Javran.c@gmail.com>
2016-05-08 23:50:01 -0400

    testcases for binomial

---
+++ purely-functional-data-structures/Ch03BinominalTest.hs
@@ -0,0 +1,24 @@
+module Ch03BinominalTest where
+
+import Test.Hspec
+import Test.QuickCheck
+import qualified Data.List as L
+import Data.Foldable
+
+import Ch03Binominal
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+toAscList :: Ord a => Heap a -> [a]
+toAscList h = case viewMin h of
+    Nothing -> []
+    Just (v, h') -> v : toAscList h'
+
+sortByHeap :: Ord a => [a] -> [a]
+sortByHeap = toAscList . foldl' (flip insert) empty
+
+main :: IO ()
+main = hspec $ do
+    describe "Binomial" $ do
+      it "can sort elements" $ do
+        property $ \xs -> sortByHeap (xs :: [Int]) == L.sort xs
