f5b06eb57c8f7e7089c99537eba0d566d0d85441
Javran Cheng <Javran.c@gmail.com>
2016-05-09 22:55:03 -0400

    link and insTree

--- purely-functional-data-structures/Ch03Exercise6.hs
+++ purely-functional-data-structures/Ch03Exercise6.hs
@@ -18,3 +18,23 @@ empty = []
 
 isEmpty :: Heap a -> Bool
 isEmpty = null
+
+-- | "link" should only be used on trees of the same rank
+link :: Ord a => Tree a -> Tree a -> Tree a
+link t1@(Node x1 c1) t2@(Node x2 c2) =
+    if x1 <= x2
+      then Node x1 (t2 : c1)
+      else Node x2 (t1 : c2)
+
+insTree :: Ord a => Tree a -> Heap a -> Heap a
+insTree t [] = [(rank t, t)]
+insTree t ts@((_,t'):ts') =
+    if rank t < rank t'
+      then (rank t, t) : ts
+      else insTree (link t t') ts'
+
+{-# ANN insert "HLint: ignore Eta reduce" #-}
+-- creates a singleton tree, carries it until an unoccupied rank
+-- is found in the list of trees
+insert :: Ord a => a -> Heap a -> Heap a
+insert x ts = insTree (singleton x) ts
