7a5175f81d2eb0b071f595b3b6d7961045f65f58
Javran Cheng <Javran.c@gmail.com>
2016-05-11 23:59:33 -0400

    balance function

--- purely-functional-data-structures/Ch03RedBlack.hs
+++ purely-functional-data-structures/Ch03RedBlack.hs
@@ -1,6 +1,6 @@
 module Ch03RedBlack where
 
-data Color = Rd | Bk
+data Color = Red | Black
 
 data Tree a = E | T Color (Tree a) a (Tree a)
 
@@ -13,3 +13,10 @@ member x (T _ a y b)
     | x < y = member x a
     | x > y = member x b
     | otherwise = True
+
+balance :: Color -> Tree a -> a -> Tree a -> Tree a
+balance Black (T Red (T Red a x b) y c) z d = T Red (T Black a x b) y (T Black c z d)
+balance Black (T Red a x (T Red b y c)) z d = T Red (T Black a x b) y (T Black c z d)
+balance Black a x (T Red (T Red b y c) z d) = T Red (T Black a x b) y (T Black c z d)
+balance Black a x (T Red b y (T Red c z d)) = T Red (T Black a x b) y (T Black c z d)
+balance color l v r = T color l v r
