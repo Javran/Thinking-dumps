c5c84b46c7c56b15a70ea1419bbf169231db950c
Javran Cheng <Javran.c@gmail.com>
2016-05-12 23:58:31 -0400

    testcase boilerplate

---
+++ purely-functional-data-structures/Ch03RedBlackTest.hs
@@ -0,0 +1,13 @@
+module Ch03RedBlackTest where
+
+import Test.Hspec
+import Test.QuickCheck
+import qualified Data.List as L
+
+import Ch03RedBlack
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+main :: IO ()
+main = hspec $ do
+    pure ()
