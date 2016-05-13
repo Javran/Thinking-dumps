44a8cb2b577f690ddc44012dde3c2e0759d3134a
Javran Cheng <Javran.c@gmail.com>
2016-05-13 00:10:46 -0400

    testcase created by comparing with IntSet

--- purely-functional-data-structures/Ch03RedBlackTest.hs
+++ purely-functional-data-structures/Ch03RedBlackTest.hs
@@ -2,12 +2,22 @@ module Ch03RedBlackTest where
 
 import Test.Hspec
 import Test.QuickCheck
-import qualified Data.List as L
+import qualified Data.IntSet as IS
+import Data.Foldable
 
 import Ch03RedBlack
 
 {-# ANN module "HLint: ignore Redundant do" #-}
 
+fromList :: Ord a => [a] -> Tree a
+fromList = foldl' (flip insert) empty
+
+sortByTree :: Ord a => [a] -> [a]
+sortByTree = toAscList . fromList
+
 main :: IO ()
 main = hspec $ do
-    pure ()
+    describe "RedBlack" $ do
+      it "can sort elements (no duplicate elements)" $ do
+        property $ \xs -> sortByTree (xs :: [Int]) ==
+                          (IS.toAscList . IS.fromList $ xs)
