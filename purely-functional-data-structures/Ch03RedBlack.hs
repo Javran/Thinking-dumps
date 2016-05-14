8b7aa0bcc5ae9b01e3839a0a8c71abfc3460d15a
Javran Cheng <Javran.c@gmail.com>
2016-05-13 21:14:22 -0400

    black node depth property?

--- purely-functional-data-structures/Ch03RedBlack.hs
+++ purely-functional-data-structures/Ch03RedBlack.hs
@@ -1,5 +1,7 @@
 module Ch03RedBlack where
 
+import Control.Monad
+
 data Color = Red | Black
 
 data Tree a = E | T Color (Tree a) a (Tree a)
@@ -19,18 +21,34 @@ balance Black (T Red (T Red a x b) y c) z d = T Red (T Black a x b) y (T Black c
 balance Black (T Red a x (T Red b y c)) z d = T Red (T Black a x b) y (T Black c z d)
 balance Black a x (T Red (T Red b y c) z d) = T Red (T Black a x b) y (T Black c z d)
 balance Black a x (T Red b y (T Red c z d)) = T Red (T Black a x b) y (T Black c z d)
-balance color l v r = T color l v r
+balance c l v r = T c l v r
 
 insert :: Ord a => a -> Tree a -> Tree a
 insert x s = T Black a y b
   where
     ins E = T Red E x E
-    ins s1@(T color a1 y1 b1)
-        | x < y1 = balance color (ins a1) y1 b1
-        | x > y1 = balance color a1 y1 (ins b1)
+    ins s1@(T c1 a1 y1 b1)
+        | x < y1 = balance c1 (ins a1) y1 b1
+        | x > y1 = balance c1 a1 y1 (ins b1)
         | otherwise = s1
     (T _ a y b) = ins s
 
 toAscList :: Tree a -> [a]
 toAscList E = []
 toAscList (T _ l v r) = toAscList l ++ v : toAscList r
+
+color :: Tree a -> Color
+color E = Black
+color (T c _ _ _) = c
+
+-- count the number of black nodes from root to nil.
+-- any violation of the black node depth property should result in failure
+countBlackDepth :: Tree a -> Maybe Int
+countBlackDepth E = Just 1 -- empty nodes are considered black
+countBlackDepth (T c l _ r) = do
+    lDep <- countBlackDepth l
+    rDep <- countBlackDepth r
+    guard $ lDep == rDep
+    case c of
+        Black -> pure (lDep + 1)
+        Red -> pure lDep
