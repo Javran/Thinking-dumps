b8cd7b8609a3a16b877149c59921024c14846368
Javran Cheng <Javran.c@gmail.com>
2016-05-13 21:20:56 -0400

    verify black node depth property

--- purely-functional-data-structures/Ch03RedBlackTest.hs
+++ purely-functional-data-structures/Ch03RedBlackTest.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 import Test.QuickCheck
 import qualified Data.IntSet as IS
 import Data.Foldable
+import Data.Maybe
 
 import Ch03RedBlack
 
@@ -21,3 +22,5 @@ main = hspec $ do
       it "can sort elements (no duplicate elements)" $ do
         property $ \xs -> sortByTree (xs :: [Int]) ==
                           (IS.toAscList . IS.fromList $ xs)
+      it "should satisfy property of black node depth" $ do
+        property $ \xs -> isJust . countBlackDepth .  fromList $ (xs :: [Int])
