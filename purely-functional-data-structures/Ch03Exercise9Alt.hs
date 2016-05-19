955ea931992894e5f33a8a1adff7e465c319b18c
Javran Cheng <Javran.c@gmail.com>
2016-05-18 22:53:19 -0400

    no violation so far?

--- purely-functional-data-structures/Ch03Exercise9Alt.hs
+++ purely-functional-data-structures/Ch03Exercise9Alt.hs
@@ -1,6 +1,7 @@
-module Ch03Exercise9 where
+module Ch03Exercise9Alt where
 
 import Ch03RedBlack
+import Control.DeepSeq
 
 buildTree :: Int -> (Int, Tree ())
 buildTree 0 = (0, E)
@@ -10,9 +11,9 @@ buildTree n = (if rootColor == Red then rH else rH+1, T rootColor lTree () rTree
   where
     lCount = (n - 1) `div` 2
     rCount = n - 1 - lCount
-    (lH, lTree) = buildTree lCount
-    (rH, rTree') = buildTree rCount
-    rTree = if lH < rH then redToBlack rTree' else rTree'
+    (lH, lTree') = buildTree lCount
+    (rH, rTree) = buildTree rCount
+    lTree = if lH < rH then redToBlack lTree' else lTree'
 
     redToBlack (T Red l v r) = T Black l v r
     redToBlack _ = error "violation found."
@@ -21,3 +22,9 @@ buildTree n = (if rootColor == Red then rH else rH+1, T rootColor lTree () rTree
         if color lTree == Red || color rTree == Red
           then Black
           else Red
+
+experiment :: Int -> IO ()
+experiment n = buildTree n `deepseq` pure ()
+
+main :: IO ()
+main = mapM_ experiment [1..1000]
