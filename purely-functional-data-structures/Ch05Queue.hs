576559466a02344304637724cb9b2da20878a71e
Javran Cheng <Javran.c@gmail.com>
2016-05-23 22:45:03 -0400

    checkF

--- purely-functional-data-structures/Ch05Queue.hs
+++ purely-functional-data-structures/Ch05Queue.hs
@@ -6,14 +6,20 @@ empty :: Queue a
 empty = ([], [])
 
 isEmpty :: Queue a -> Bool
-isEmpty ([], []) = True
+isEmpty ([], _) = True -- according to the invariant
 isEmpty _ = False
 
 qHead :: Queue a -> a
 qHead (x:f, r) = x
 
 qTail :: Queue a -> Queue a
-qTail (x:f, r) = (f,r)
+qTail (x:f, r) = checkF (f,r)
 
 qSnoc :: Queue a -> a -> Queue a
-qSnoc (f,r) x = (f, x:r)
+qSnoc (f,r) x = checkF (f, x:r)
+
+-- maintain the invariant that "f" part is empty
+-- only if "r" is also empty
+checkF :: Queue a -> Queue a
+checkF ([], r) = (reverse r, [])
+checkF v = v
