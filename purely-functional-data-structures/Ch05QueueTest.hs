ddad9441aa9f8985f8cd12473f049abb10bc0513
Javran Cheng <Javran.c@gmail.com>
2016-05-25 00:02:44 -0400

    test insertion

--- purely-functional-data-structures/Ch05QueueTest.hs
+++ purely-functional-data-structures/Ch05QueueTest.hs
@@ -1,16 +1,15 @@
 module Ch05QueueTest where
 
-
 import Test.Hspec
 import Test.QuickCheck
 import Data.Foldable
-import Data.Maybe
 
-import Ch05Queue
+import Ch05Queue as Q
 
 {-# ANN module "HLint: ignore Redundant do" #-}
-
+main :: IO ()
 main = hspec $ do
     describe "Queue" $ do
       it "inserts correctly" $ do
-          property $ True
+          property $ \ xs ->
+              Q.toList (foldl' Q.qSnoc Q.empty xs) == (xs :: [Int])
