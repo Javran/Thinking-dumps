2d3cd38b406df0210911c416f08524d5757f2aaf
Javran Cheng <Javran.c@gmail.com>
2016-05-25 23:54:31 -0400

    rewrite using case analysis

--- purely-functional-data-structures/Ch05Exercise1.hs
+++ purely-functional-data-structures/Ch05Exercise1.hs
@@ -10,12 +10,21 @@ isEmpty ([], []) = True
 isEmpty _ = False
 
 checkDq :: Deque a -> Deque a
-checkDq dq@([], []) = dq
-checkDq ([], [v]) = ([v],[])
-checkDq dq@([_], []) = dq
-checkDq (f, []) | (f1,f2) <- half f = (f1, reverse f2)
-checkDq ([], r) | (r1,r2) <- half r = (reverse r2, r1)
-checkDq dq = dq
+checkDq dq = case dq of
+    ([], []) -> dq
+    -- when front is empty
+    ([], r) -> case r of
+        [_] -> (r,[])
+        -- cut rear into half
+        _ | (r1,r2) <- half r
+            -> (reverse r2, r1)
+    -- when rear is empty
+    (f, []) -> case f of
+        [_] -> dq
+        -- cut front into half
+        _ | (f1,f2) <- half f
+            -> (f1, reverse f2)
+    _ -> dq
 
 half :: [a] -> ([a],[a])
 half xs = splitAt (length xs `div` 2) xs
