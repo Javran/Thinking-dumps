2633eee654b29a6532e203636bfe5fd930af5ee7
Javran Cheng <Javran.c@gmail.com>
2016-05-26 21:17:05 -0400

    testcase explained

--- purely-functional-data-structures/Ch05Exercise1Test.hs
+++ purely-functional-data-structures/Ch05Exercise1Test.hs
@@ -15,6 +15,11 @@ main = hspec $ do
     describe "Deque" $ do
       it "inserts correctly" $ do
         property $ do
+            -- we make a list of numbers of both non-negatives and negatives
+            -- if the number is non-negative, we insert it in front of the deque
+            -- otherwise we insert it to the back of the current deque
+            -- so we can end up with operations that randomly insert either in front of
+            -- or to the back of the current deque, and the result is predictable
             xs <- listOf (choose (-500,500 :: Int))
             let ins dq i =
                     if i >= 0
