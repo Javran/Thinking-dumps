637b430455b5aa15ab7f95b63dea78667f38bd59
Javran Cheng <Javran.c@gmail.com>
2016-06-01 23:51:34 -0400

    insert & toAscList

--- purely-functional-data-structures/Ch05Exercise4Test.hs
+++ purely-functional-data-structures/Ch05Exercise4Test.hs
@@ -3,3 +3,12 @@ module Ch05Exercise4Test where
 import Ch05Splay
 import Ch05Exercise4
 
+-- putting things together
+insert :: Ord a => a -> Tree a -> Tree a
+insert x t = T (smaller x t) x (bigger x t)
+
+-- converting a splay tree into a list
+toAscList :: Tree a -> [a]
+toAscList t = case viewMin t of
+    Nothing -> []
+    Just (v,t') -> v : toAscList t'
