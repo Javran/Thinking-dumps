dc8e7ef509072733cf57ac0b3e124929558cec08
Javran Cheng <Javran.c@gmail.com>
2016-06-01 23:54:38 -0400

    finish writing testcases and it works

--- purely-functional-data-structures/Ch05Exercise4Test.hs
+++ purely-functional-data-structures/Ch05Exercise4Test.hs
@@ -1,7 +1,14 @@
 module Ch05Exercise4Test where
 
+import Test.Hspec
+import Test.QuickCheck
+import Data.List (sort)
+import Data.Foldable
+
 import Ch05Splay
 import Ch05Exercise4
+
+{-# ANN module "HLint: ignore Redundant do" #-}
 
 -- putting things together
 insert :: Ord a => a -> Tree a -> Tree a
@@ -12,3 +19,9 @@ toAscList :: Tree a -> [a]
 toAscList t = case viewMin t of
     Nothing -> []
     Just (v,t') -> v : toAscList t'
+
+main :: IO ()
+main = hspec $ do
+    describe "Splay" $ do
+      it "can sort elements" $ do
+          property $ \xs -> sort (xs :: [Int]) == toAscList (foldl' (flip insert) E xs)
