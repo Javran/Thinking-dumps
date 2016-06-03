64e5faf0f589a90dc3de0ddbd34bfa2fed6d7b7b
Javran Cheng <Javran.c@gmail.com>
2016-06-03 00:08:23 -0400

    smaller' impl

--- purely-functional-data-structures/Ch05Exercise4.hs
+++ purely-functional-data-structures/Ch05Exercise4.hs
@@ -11,13 +11,13 @@ smaller pivot (T a x b) =
        else T a x (smaller pivot b)
 
 smaller' :: Ord a => a -> Tree a -> Tree a
+smaller' _ E = E
 smaller' pivot (T a x b) =
     if x > pivot
        then smaller' pivot a
-       else case a of
-              E -> T a x E
-              T a1 y a2 ->
-                  if y > pivot
-                     then T (smaller' pivot a2) x b
-                     else
-                       T (smaller' pivot a1) y (T a2 x b)
+       else case b of
+          E -> T a x E
+          T b1 y b2 ->
+              if y > pivot
+                 then T a x (smaller' pivot b1)
+                 else T (T a x b1) y (smaller' pivot b2)
