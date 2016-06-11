9e0bc3b4f94b8c369d540e5ceef2a927e05e63f1
Javran Cheng <Javran.c@gmail.com>
2016-06-11 09:58:26 +0800

    full verification

--- h-99/Problem90Test.hs
+++ h-99/Problem90Test.hs
@@ -3,6 +3,7 @@ module Problem90Test where
 import Test.Hspec
 import Test.QuickCheck
 import qualified Data.List as L
+import qualified Data.IntSet as IS
 import Problem90
 
 {-# ANN module "HLint: ignore Redundant do" #-}
@@ -13,5 +14,12 @@ main = hspec $ do
       it "gives correct answers" $
         let test n = all verify (queens n)
               where
-                verify solution = L.sort solution == [1..n]
+                verify solution = check1 && check2 && check3
+                  where
+                    allUnique :: [Int] -> Bool
+                    allUnique xs = length xs == IS.size (IS.fromList xs)
+                    fullSolution = zip [1..n] solution
+                    check1 = L.sort solution == [1..n]
+                    check2 = allUnique (map (uncurry (+)) fullSolution)
+                    check3 = allUnique (map (uncurry subtract) fullSolution)
         in property $ all test [1..10]
