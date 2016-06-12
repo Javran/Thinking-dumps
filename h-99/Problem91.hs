82d997a000cc0168a5688826995b3ef4f96bd63d
Javran Cheng <Javran.c@gmail.com>
2016-06-12 22:25:58 +0800

    slow search

--- h-99/Problem91.hs
+++ h-99/Problem91.hs
@@ -1,6 +1,8 @@
 module Problem91 where
 
 import Control.Monad
+import qualified Data.Set as S
+import Debug.Trace
 
 type Coord = (Int, Int)
 
@@ -13,3 +15,15 @@ jump n (x,y) = do
     guard $ newX >= 1 && newX <= n
          && newY >= 1 && newY <= n
     pure (newX, newY)
+
+search n todo visited current path
+    | S.null todo = pure $ current : path
+    | otherwise = do
+        let newVisited = S.insert current visited
+        next <- jump n current
+        guard $ S.notMember next newVisited
+        -- traceM (show (length path))
+        search n (S.delete next todo) newVisited next (current:path)
+-- try: (very slow, might not work)
+-- > head $ search 8 (S.delete (1,1) xs) S.empty (1,1) []
+
