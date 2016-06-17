7abea50a8a7592f0c4a18f0446fd121b5bf17b5f
Javran Cheng <Javran.c@gmail.com>
2016-06-17 22:48:48 +0800

    impl search

--- h-99/Problem92.hs
+++ h-99/Problem92.hs
@@ -3,10 +3,12 @@ module Problem92 where
 
 import Data.List
 import Data.Maybe
+import Control.Monad
 import qualified Data.IntMap.Strict as IM
 import qualified Data.IntSet as IS
 
 {-# ANN module "HLint: ignore Eta reduce" #-}
+{-# ANN module "HLint: ignore Use uncurry" #-}
 
 -- "pick" from Problem90
 pick :: forall a. [a] -> [(a,[a])]
@@ -23,5 +25,29 @@ addEdge (a,b) = insertEdge a b . insertEdge b a
   where
     insertEdge x y = IM.alter (Just . IS.insert x . fromMaybe IS.empty) y
 
+-- all undirected edges
+allUndirEdges :: Tree -> [(Int,Int)]
+allUndirEdges t = filter (\(x,y) -> x < y) edges
+  where
+    edges = do
+      (k,vs) <- IM.toList t
+      v <- IS.toList vs
+      pure (k,v)
+
 neighbors :: Int -> Tree -> IS.IntSet
 neighbors k m = fromMaybe IS.empty $ IM.lookup k m
+
+uniqueInts :: [Int] -> Bool
+uniqueInts xs = length xs == IS.size (IS.fromList xs)
+
+search t edges assigned remainings todo cur
+    | IS.null todo = pure assigned
+    | otherwise = do
+        let newTodo = IS.delete cur todo
+        (newNum, newRemainings) <- pick remainings
+        let newAssigned = IM.insert cur newNum assigned
+            validEdges = filter (\(x,y) -> IM.member x assigned && IM.member y assigned) edges
+            find' k m = fromJust $ IM.lookup k m
+            edgeDiffs = map (\(x,y) -> abs (find' x assigned - find' y assigned)) validEdges
+        guard $ uniqueInts edgeDiffs
+        search t edges newAssigned newRemainings newTodo (fst . fromJust $ IS.minView newTodo)
