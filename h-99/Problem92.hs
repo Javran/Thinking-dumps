f0d6fa69a3898b9d7c172ef8305ac9d076bed7c3
Javran Cheng <Javran.c@gmail.com>
2016-06-17 22:04:59 +0800

    start p92

---
+++ h-99/Problem92.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Problem92 where
+
+import Data.List
+
+-- "pick" from Problem90
+pick :: forall a. [a] -> [(a,[a])]
+pick xs = map split (init $ zip (inits xs) (tails xs))
+  where
+    split :: ([a], [a]) -> (a,[a])
+    split (ls,v:rs) = (v,ls++rs)
+    split _ = error "cannot split empty list"
