5e1c5416f88b1305496a246f106dc2f90ffb81f1
Javran Cheng <Javran.c@gmail.com>
2016-06-17 22:20:15 +0800

    tree impl as maps and sets

--- h-99/Problem92.hs
+++ h-99/Problem92.hs
@@ -2,6 +2,11 @@
 module Problem92 where
 
 import Data.List
+import Data.Maybe
+import qualified Data.IntMap.Strict as IM
+import qualified Data.IntSet as IS
+
+{-# ANN module "HLint: ignore Eta reduce" #-}
 
 -- "pick" from Problem90
 pick :: forall a. [a] -> [(a,[a])]
@@ -10,3 +15,13 @@ pick xs = map split (init $ zip (inits xs) (tails xs))
     split :: ([a], [a]) -> (a,[a])
     split (ls,v:rs) = (v,ls++rs)
     split _ = error "cannot split empty list"
+
+type Tree = IM.IntMap IS.IntSet
+
+addEdge :: (Int, Int) -> Tree -> Tree
+addEdge (a,b) = insertEdge a b . insertEdge b a
+  where
+    insertEdge x y = IM.alter (Just . IS.insert x . fromMaybe IS.empty) y
+
+neighbors :: Int -> Tree -> IS.IntSet
+neighbors k m = fromMaybe IS.empty $ IM.lookup k m
