f7cfcdce63a4e6d648cf0c9db45d41c2b6a2cfcb
Javran Cheng <Javran.c@gmail.com>
2016-06-18 22:42:39 +0800

    p92 tests template

---
+++ h-99/Problem92Test.hs
@@ -0,0 +1,12 @@
+module Problem92Test where
+
+import Test.Hspec
+import Test.QuickCheck
+import qualified Data.List as L
+import qualified Data.IntSet as IS
+import Problem92
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+main :: IO ()
+main = pure ()
