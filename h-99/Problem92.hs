e936fef4c82133b461897e4cb48e07f05e463e16
Javran Cheng <Javran.c@gmail.com>
2016-06-19 21:27:03 +0800

    comment about the solution

--- h-99/Problem92.hs
+++ h-99/Problem92.hs
@@ -34,11 +34,30 @@ uniqueInts xs = length xs == IS.size (IS.fromList xs)
 
 type NodeAssigns = IM.IntMap Int
 
--- t: tree, edges: all edges (cached result)
--- assigned: current node number assignments
--- remainings: not-yet-assigned numbers
--- todo: nodes not visited
--- cur: the node we are looking at
+{-
+  summary of arguments
+  * t: the tree
+  * missingEdges: a list of edges that have not yet taken into account
+  * missingDiffs: a set of Ints that has not yet observed as an "edge difference"
+  * assigned: current (partial) assignment of numbers to nodes
+  * remainings: not-yet-assigned numbers
+  * todo: nodes not visited
+
+  our target is to assigned different numbers to nodes to satisfy the condition.
+  for search algorithms in general, "t" is the graph info we can refer to, and "assigned"
+  make it possible to construct full solutions (if any)
+  and "todo" serves as a queue of not yet considered nodes.
+
+  several things are kept in order to reduce the amount of duplicated work:
+  * missingEdges: only those whose both ends are assigned with a number are picked and checked,
+    as more and more nodes are assigned, this collection will eventually become empty
+  * missingDiffs: to save us from checking whether all current differences are unique over and over
+    again, we take another view on the problem: since all unique differences
+    has to be assigned to an edge, we just need to test whether all newly added edges
+    has unique differences.
+
+  TODO: let todo being a list should be good enough
+-}
 search :: Tree -> [Edge] -> IS.IntSet -> NodeAssigns -> [Int] -> IS.IntSet -> [NodeAssigns]
 search t missingEdges missingDiffs assigned remainings todo
     | IS.null todo = do
