2f91fef87f41ad74deac2c2a1b59d83322dea7a6
Javran Cheng <Javran.c@gmail.com>
2016-06-24 22:08:44 +0800

    monadic actions for disjoint set

--- h-99/DisjointSet.hs
+++ h-99/DisjointSet.hs
@@ -1,6 +1,7 @@
 module DisjointSet where
 
 import qualified Data.Map.Strict as M
+import Control.Monad.State
 
 type DisjointSet a = M.Map a a
 
@@ -19,3 +20,17 @@ getRoot v ds = do
 {-# ANN union "HLint: ignore Eta reduce" #-}
 union :: Ord a => a -> a -> DisjointSet a -> DisjointSet a
 union x y ds = M.insert x y ds
+
+initM :: Ord a => [a] -> State (DisjointSet a) ()
+initM = put . mkSet
+
+getRootM :: Ord a => a -> State (DisjointSet a) a
+getRootM v = do
+    result <- gets (getRoot v)
+    case result of
+      Just (r,ds) -> put ds >> pure r
+      Nothing -> fail "getRootM: element not found"
+
+{-# ANN unionM "HLint: ignore Use infix" #-}
+unionM :: Ord a => a -> a -> State (DisjointSet a) ()
+unionM x y = modify (union x y)
