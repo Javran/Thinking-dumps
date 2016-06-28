1ecf062f808342fa76107b95e2a430cc46d4057e
Javran Cheng <Javran.c@gmail.com>
2016-06-28 23:53:27 +0800

    a table of vertex degree

--- h-99/Problem85.hs
+++ h-99/Problem85.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Problem85 where
 
 {-
@@ -9,12 +10,27 @@ module Problem85 where
 
 -}
 
+
 import Graph
 import Problem80
+
 import qualified Data.Set as S
+import qualified Data.Map.Strict as M
+import Data.Tuple
 
 mkGraph :: Ord a => [a] -> [(a,a)] -> AdjForm a (Edge a)
 mkGraph vs es = graphFormToAdjForm (GraphForm vSet eSet)
   where
     vSet = S.fromList vs
     eSet = S.fromList . map (uncurry Edge) $ es
+
+degreeTable :: forall a b. AdjForm a b -> M.Map Int (S.Set a)
+degreeTable (AdjForm g) = _
+  where
+    -- the degree of a vertex is the number of edges connected to it.
+    -- (undirected)
+    degrees :: [(a, Int)]
+    degrees = M.toList $ M.map S.size g
+
+    degreeGroups :: [(Int, [a])]
+    degreeGroups = S.toAscList $ S.map _ _
