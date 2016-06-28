e809398de24a8faf6b1a0046758c206de12159e7
Javran Cheng <Javran.c@gmail.com>
2016-06-29 00:10:52 +0800

    degreeTable checking

--- h-99/Problem85.hs
+++ h-99/Problem85.hs
@@ -16,6 +16,7 @@ import Problem80
 
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
+import Control.Arrow
 
 mkGraph :: Ord a => [a] -> [(a,a)] -> AdjForm a (Edge a)
 mkGraph vs es = graphFormToAdjForm (GraphForm vSet eSet)
@@ -35,3 +36,20 @@ degreeTable (AdjForm g) =
     -- (undirected)
     degrees :: [(a, Int)]
     degrees = M.toList $ M.map S.size g
+
+-- for 2 graphs to be isomorphic, the degree table must match
+-- returns more properly structured data when succeeded: [(Int, ([a],[b]))]
+-- in which first Int is still the degree, and ([a],[b]) are 2 lists of the same size.
+-- if 2 graphs are indeed isomorphic, then vertices' mapping must be established inside
+-- of every group.
+-- note that it's not necessary for vertices of 2 graphs to have the same type
+-- as long as the mapping can be established (which means at least "Ord" needs to
+-- be supported by the corresponding type), everything should work.
+checkDegreeTables :: [(Int,[a])] -> [(Int,[b])] -> Maybe [(Int, ([a],[b]))]
+checkDegreeTables dt1 dt2
+    | convert dt1 == convert dt2 = Just (zip (map fst dt1)
+                                             (zip (map snd dt1)
+                                                  (map snd dt2)))
+    | otherwise = Nothing
+  where
+    convert = map (second length)
