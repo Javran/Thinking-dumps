ecb3082e3730af5004989aef8849c54010f1dd43
Javran Cheng <Javran.c@gmail.com>
2016-06-28 23:57:54 +0800

    impl degreeTable

--- h-99/Problem85.hs
+++ h-99/Problem85.hs
@@ -16,7 +16,6 @@ import Problem80
 
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
-import Data.Tuple
 
 mkGraph :: Ord a => [a] -> [(a,a)] -> AdjForm a (Edge a)
 mkGraph vs es = graphFormToAdjForm (GraphForm vSet eSet)
@@ -24,13 +23,15 @@ mkGraph vs es = graphFormToAdjForm (GraphForm vSet eSet)
     vSet = S.fromList vs
     eSet = S.fromList . map (uncurry Edge) $ es
 
-degreeTable :: forall a b. AdjForm a b -> M.Map Int (S.Set a)
-degreeTable (AdjForm g) = _
+degreeTable :: forall a b. Ord a => AdjForm a b -> [(Int,[a])]
+degreeTable (AdjForm g) =
+        M.toAscList
+      . M.map S.toList
+      . M.fromListWith mappend
+      . map (\(v,d) -> (d,S.singleton v))
+      $ degrees
   where
     -- the degree of a vertex is the number of edges connected to it.
     -- (undirected)
     degrees :: [(a, Int)]
     degrees = M.toList $ M.map S.size g
-
-    degreeGroups :: [(Int, [a])]
-    degreeGroups = S.toAscList $ S.map _ _
