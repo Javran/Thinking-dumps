0a41b819ff3f86ab9572079aef328a9232b27452
Javran Cheng <Javran.c@gmail.com>
2016-07-01 23:45:59 +0800

    few useful links and notes

--- h-99/Problem86.hs
+++ h-99/Problem86.hs
@@ -23,4 +23,12 @@ degreeDecreasingNodes = concatMap snd . reverse . degreeTable
   TODO
   c. use Welsh-Powell's algorithm to paint the nodes of a graph
   in such a way that adjacent nodes have different colors.
+  related:
+  http://graphstream-project.org/doc/Algorithms/Welsh-Powell/
+  https://en.wikipedia.org/wiki/Graph_coloring#Greedy_coloring
+
+  NOTE:
+  - according to the algorithm, we can take iterations to pick one color and traverse
+    the vertex list, coloring nodes that haven't been colored and don't have
+    any adjacent vertex that has that color.
 -}
