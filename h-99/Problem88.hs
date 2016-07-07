8c19018e54664b2ecfac3e2bbbd1a5853621fe12
Javran Cheng <Javran.c@gmail.com>
2016-07-07 23:09:05 +0800

    p88: comments

--- h-99/Problem88.hs
+++ h-99/Problem88.hs
@@ -3,6 +3,15 @@ module Problem88 where
 import qualified Problem87 as P87
 import qualified Data.Set as S
 
+{-
+  to find all connected components of a graph,
+  we can keep track of not-yet-visited vertices,
+  and pick one vertex from it as the starting point of "depthFirst".
+  by doing so we can find one connected component from the graph.
+  we then make this run through iterations until all vertices
+  are visited (and belongs to one of the connected components)
+
+-}
 connectedComponents :: Ord a => ([a],[(a,a)]) -> [ [a] ]
 connectedComponents p@(vs,_) = connectedComponents' initTodos
   where
