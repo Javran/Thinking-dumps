4f5315fc2f6fac989447a5e28593b3d0c0bb2d9c
Javran Cheng <Javran.c@gmail.com>
2016-07-11 22:54:30 +0800

    p94: more examples

--- h-99/Problem94Test.hs
+++ h-99/Problem94Test.hs
@@ -14,7 +14,23 @@ main = hspec $ do
                                  , "==", show expected]) $ example $
                     length (regular n k) `shouldBe` expected
         testRegular 3 2 1
+
         testRegular 4 2 1
         testRegular 4 3 1
 
+        testRegular 5 2 1
+        testRegular 5 3 0
+        testRegular 5 4 1
+
+        testRegular 6 2 2
         testRegular 6 3 2
+        testRegular 6 4 1
+        testRegular 6 5 1
+
+        testRegular 7 2 2
+        testRegular 7 3 0
+        testRegular 7 4 2
+        testRegular 7 5 0
+        testRegular 7 6 1
+
+        -- testRegular 9 2 4
