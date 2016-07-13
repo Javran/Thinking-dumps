ec9d308e416bbeb703d4c6a56b2894777e95f927
Javran Cheng <Javran.c@gmail.com>
2016-07-13 22:34:55 +0800

    disjoint set test comment

--- h-99/DisjointSetTest.hs
+++ h-99/DisjointSetTest.hs
@@ -14,6 +14,16 @@ main :: IO ()
 main = hspec $ do
     describe "insert & fromList" $ do
         specify "basic property of union-ing & querying" $ do
+            {-
+              we can simulate a set using disjoint set:
+              * first we pick "0" as a "base element" (which should not
+                 conflict with value domain)
+              * when we want to insert something into the set, we simply union it
+                with the base element
+
+              the idea above in mind, here we have a simple test: simulate a set
+              and see if that works.
+            -}
             property $ do
                 xs1 <- replicateM 50 $ choose (1,500 :: Int)
                 let s1 = IS.fromList xs1
@@ -23,3 +33,5 @@ main = hspec $ do
                     verifyQuery k =
                         IS.member k s1 == fst (DS.inSameSet 0 k s2)
                 pure (all verifyQuery [1..500])
+        specify "example 1" $ example $ do
+            pending
