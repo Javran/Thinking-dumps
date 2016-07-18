9458a28fa3c2f90b2d120a7dd9e28255ec9318dd
Javran Cheng <Javran.c@gmail.com>
2016-07-18 23:46:15 +0800

    p95: comments

--- h-99/Problem95.hs
+++ h-99/Problem95.hs
@@ -1,8 +1,18 @@
-module Problem95 where
+module Problem95
+  ( breakDigits
+  , fullWords
+  ) where
 
 import qualified Data.Array as Arr
 import Data.List
 
+{-
+  (internal only) an array of digit-to-word mapping.
+  TODO: I'm making this value top-level in hope that
+  we don't recalculate it every time we demands it.
+  but I think this is an easy-to-spot chance of optimization,
+  and doubt we should do this manually.
+-}
 digitToWordArr :: Arr.Array Int String
 digitToWordArr = Arr.array (0,9) (zip [0..9] digitWords)
   where
@@ -10,9 +20,16 @@ digitToWordArr = Arr.array (0,9) (zip [0..9] digitWords)
         "zero one two three four \
         \five six seven eight nine"
 
+{-
+  (internal only) converting digits to words,
+  note that the input should only be [0..9]
+-}
 digitToWord :: Int -> String
 digitToWord = (digitToWordArr Arr.!)
 
+-- | breaks non-negative integer into an non-empty list of digits.
+--   it is guaranteed that the resulting list is non-empty, and every
+--   element of it is within range [0..9]
 breakDigits :: Integer -> [Int]
 breakDigits 0 = [0]
 breakDigits v = brkDigits [] v
