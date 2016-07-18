7b725ef23e4ddb7f559601fba3b95c34cd4522f1
Javran Cheng <Javran.c@gmail.com>
2016-07-18 23:26:43 +0800

    p95: break int into digits

--- h-99/Problem95.hs
+++ h-99/Problem95.hs
@@ -11,3 +11,13 @@ digitToWordArr = Arr.array (0,9) (zip [0..9] digitWords)
 
 digitToWord :: Int -> String
 digitToWord = (digitToWordArr Arr.!)
+
+breakDigits :: Integer -> [Int]
+breakDigits 0 = [0]
+breakDigits v = brkDigits [] v
+  where
+    brkDigits acc 0 = acc
+    brkDigits acc x = brkDigits (r':acc) q
+        where
+          (q,r) = x `quotRem` 10
+          r' = fromInteger r
