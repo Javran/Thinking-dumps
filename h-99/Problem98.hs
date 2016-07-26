35cae1545cffcddca4011b200b24b68f9126c57b
Javran Cheng <Javran.c@gmail.com>
2016-07-26 20:41:06 +0800

    p98: wrap solveRule to hide the init cell

--- h-99/Problem98.hs
+++ h-99/Problem98.hs
@@ -30,29 +30,31 @@ ruleView (Rule [] _) = Nothing
 ruleView (Rule [x] l) = Just ((x,l), Rule [] 0)
 ruleView (Rule (x:xs) l) = Just ((x,l), Rule xs (l-x-1))
 
--- TODO: let's say to satisfy the next new rule we always fill in a "False" as the separator
--- and caller of this function should be responsible for prepending a Nothing in front of the [CellContent]
 solveRule :: Rule -> [CellContent] -> [ [Bool] ]
-solveRule r xs = case ruleView r of
-    -- all rules have been satisfied, we fill rest of the cells with False
-    Nothing -> fst <$> checkedFill False (length xs) xs
-    -- now we are trying to have one or more "False" and "curLen" consecutive "True"s
-    Just ((curLen,leastL), r') -> do
-        -- we can fail immediately here if we have insufficient number of cells.
-        guard $ length xs >= leastL + 1
-        -- always begin with one "False"
-        (filled1,remained1) <- checkedFill False 1 xs
-        -- now we have 2 options, either start filling in these cells, or
-        startFill <- [True, False]
-        if startFill
-            then do
-                (filled2, remained2) <- checkedFill True curLen remained1
-                filled3 <- solveRule r' remained2
-                pure (filled1 ++ filled2 ++ filled3)
-            else do
-                filled2 <- solveRule r remained1
-                pure (filled1 ++ filled2)
+solveRule r1 xs1 = map tail (solveRule' r1 (Nothing:xs1))
   where
+    -- TODO: let's say to satisfy the next new rule we always fill in a "False" as the separator
+    -- and caller of this function should be responsible for prepending a Nothing in front of the [CellContent]
+    solveRule' :: Rule -> [CellContent] -> [ [Bool] ]
+    solveRule' r xs = case ruleView r of
+        -- all rules have been satisfied, we fill rest of the cells with False
+        Nothing -> fst <$> checkedFill False (length xs) xs
+        -- now we are trying to have one or more "False" and "curLen" consecutive "True"s
+        Just ((curLen,leastL), r') -> do
+            -- we can fail immediately here if we have insufficient number of cells.
+            guard $ length xs >= leastL + 1
+            -- always begin with one "False"
+            (filled1,remained1) <- checkedFill False 1 xs
+            -- now we have 2 options, either start filling in these cells, or
+            startFill <- [True, False]
+            if startFill
+                then do
+                    (filled2, remained2) <- checkedFill True curLen remained1
+                    filled3 <- solveRule' r' remained2
+                    pure (filled1 ++ filled2 ++ filled3)
+                else do
+                    filled2 <- solveRule' r remained1
+                    pure (filled1 ++ filled2)
     -- "checkedFill b count ys" tries to fill "count" number of Bool value "b"
     -- into cells, results in failure if cell content cannot match with the indended value.
     checkedFill :: Bool -> Int -> [CellContent] -> [ ([Bool], [CellContent]) ]
