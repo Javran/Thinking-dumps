c9fe2544592c7e1438412153f3cc978c113d08c6
Javran Cheng <Javran.c@gmail.com>
2016-07-31 22:30:29 +0800

    p98: testcase, few raw puzzles

---
+++ h-99/Problem98Test.hs
@@ -0,0 +1,68 @@
+module Problem98Test where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import qualified Data.Array as Arr
+import Control.Monad
+import Problem98
+
+{-# ANN module "HLint: ignore Redundant do" #-}
+
+type RawPuzzle = [String]
+
+puzzle1 :: RawPuzzle
+puzzle1 =
+    [ " XXX    "
+    , "XX X    "
+    , " XXX  XX"
+    , "  XXXXXX"
+    , "X XXXXX "
+    , "    X  "
+    , "   XX   "
+    ]
+
+-- intentionally leaving blank rules to see if it works properly
+puzzle2 :: RawPuzzle
+puzzle2 =
+    [ "   XXXXX    "
+    , " XXXXXXXXX  "
+    , "XX X X X XX "
+    , "XXXXXXXXXXX "
+    , "     X      "
+    , "     X      "
+    , "     X      "
+    , "     X      "
+    , "     X X    "
+    , "     XXX    "
+    , "            "
+    ]
+
+puzzle3 :: RawPuzzle
+puzzle3 =
+    [ "XX XX X X   XX  XXXXX X  "
+    , "X   X XXXX  XXX XXX X X X"
+    , "X XXXX  XXXXXX   XXX  XX "
+    , " XXXXXXXXX   X  X XXXXXXX"
+    , " XXX X XXX  XXXXX  XXXXXX"
+    , " XXXX  X XX XX XXX   XXXX"
+    , " XX XX  XX XX  X XXXXXX  "
+    , "  XX XXXXX       XXXXX  X"
+    , "XXXX  XXX     XXX  XX  X "
+    , "XXXX    X    XXXX  XX XXX"
+    , "     XX    XXXX XXXXXX X "
+    , "XXXXXXXXXX   XX X X  X XX"
+    , "XXX X XX X   XXXXXXXXX X "
+    , "XXXXXXXXXX   XXXXXXX XX X"
+    , " X XXXXXXXXXX XX  XXXXXX "
+    , "   XXX X XXXXXXXXXXXXX XX"
+    , " XXXXXXXXXXXXXX XXX XXXXX"
+    , " X  XX XXXXX XXXX X X XXX"
+    , "X X X  XXXX X X X    XXX "
+    , " XXXXXXXXX XX XX X XX XXX"
+    , " X XX XXXXXXXXXX XX XXX  "
+    , "X XX  X   X  XX XXXX     "
+    , " XX  X XXXXX      XXX X X"
+    , " XX XXXXXXXX  XXXXXX  X X"
+    , " XXXXXXXX XXXX  X XXXXXX "
+    ]
