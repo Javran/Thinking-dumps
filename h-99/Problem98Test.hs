3cce48e9e5671310154459e05393ab4e23d4c7a7
Javran Cheng <Javran.c@gmail.com>
2016-07-31 22:42:09 +0800

    p98: simple puzzle parser

--- h-99/Problem98Test.hs
+++ h-99/Problem98Test.hs
@@ -2,6 +2,7 @@ module Problem98Test where
 
 import Test.Hspec
 import Test.QuickCheck
+import Data.List
 
 import qualified Data.Array as Arr
 import Control.Monad
@@ -66,3 +67,23 @@ puzzle3 =
     , " XX XXXXXXXX  XXXXXX  X X"
     , " XXXXXXXX XXXX  X XXXXXX "
     ]
+
+mkPuzzle :: RawPuzzle -> ([[Int]], [[Int]])
+mkPuzzle raw = (rowRules, colRules)
+  where
+    raw1 = (map . map) cov raw
+      where
+        cov 'X' = True
+        cov ' ' = False
+        cov _ = error "wrong puzzle format"
+    raw2 = transpose raw1
+
+    rowRules = map lineToRule raw1
+    colRules = map lineToRule raw2
+
+    lineToRule = lineToRule' . dropWhile (/= True)
+
+    lineToRule' [] = []
+    lineToRule' xs
+        | (trues,remained) <- span (== True) xs =
+            length trues : lineToRule' (dropWhile (/= True) remained)
