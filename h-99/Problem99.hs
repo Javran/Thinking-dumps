219e93b79e3dd5525441715d22202d43207e9fb1
Javran Cheng <Javran.c@gmail.com>
2016-08-03 00:43:07 +0800

    p99: need more planning, call it a day.

--- h-99/Problem99.hs
+++ h-99/Problem99.hs
@@ -2,19 +2,51 @@ module Problem99 where
 
 import qualified Data.Array as Arr
 import qualified Data.IntMap.Strict as IM
+import qualified Data.Map.Strict as M
+
+import Data.Foldable
 
 type Words = IM.IntMap [String]
 
 type Coord = (Int,Int)
-type Framework = Arr.Array Coord Char
+
+-- site
+data Dir = DV | DH -- vertical or horizontal
+data Site = Site Int Dir
+
+-- at most 2 sites on the same coord (one v and one h)
+type Framework = (M.Map Coord [Site], Arr.Array Coord (Maybe Char))
 
 data Crossword = CW Words Framework
 
-fromWordList :: [String] -> Words
-fromWordList = foldr update IM.empty
+mkWords :: [String] -> Words
+mkWords = foldr update IM.empty
   where
     update w = IM.alter ins lw
       where
         lw = length w
         ins Nothing = Just [w]
         ins (Just xs) = Just (w:xs)
+
+mkFramework :: [String] -> Framework
+mkFramework [] = error "empty input"
+mkFramework xs@(y:ys)
+    | y == [] = error "first line empty"
+    | allLengthEqual = undefined -- TODO
+    | otherwise = error "inconsistent length"
+  where
+    allLengthEqual = all (lengthEq y) ys
+
+    nCols = length y
+    nRows = length xs
+
+    rect =
+        Arr.array
+          ((1,1),(nRows,nCols))
+          (zip [(r,c) | r<-[1..nRows], c<-[1..nCols]]
+               (concat xs))
+
+lengthEq :: [a] -> [b] -> Bool
+lengthEq [] [] = True
+lengthEq (_:as) (_:bs) = lengthEq as bs
+lengthEq _ _ = False
