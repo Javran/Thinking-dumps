39e99c9713693a73d70cb8548c606122b1fcbeb6
Javran Cheng <Javran.c@gmail.com>
2016-08-05 22:55:26 +0800

    p99: few more plans

--- h-99/Problem99Test.hs
+++ h-99/Problem99Test.hs
@@ -8,6 +8,15 @@ import Data.Maybe
 {-# ANN module "HLint: ignore Redundant do" #-}
 
 -- TODO: more detailed check
+{-
+  TODO: things need to be addressed:
+  - for simplicity we rely on the correctness of "crosswordFromFile"
+  - shape is correct
+  - pre-defined letters are in right places
+  - all words are used and nothing more
+  - we don't want to rely on the uniqueness of the puzzle
+    so we just take some input files but no hard-coded result for comparison
+-}
 checkResult :: Crossword -> Bool -> Maybe Rect -> Bool
 checkResult cw expectJust actual = expectJust == isJust actual
 
