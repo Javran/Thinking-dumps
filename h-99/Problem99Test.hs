641a87e41d4edd77c34290e216ae617d37a52db1
Javran Cheng <Javran.c@gmail.com>
2016-08-06 22:31:24 +0800

    p99: all checks are impl-ed

--- h-99/Problem99Test.hs
+++ h-99/Problem99Test.hs
@@ -5,9 +5,11 @@ import Test.Hspec
 import Problem99
 import Control.Arrow
 import Data.Maybe
+import Data.List
 import qualified Data.Array as Arr
 import qualified Data.Set as S
 import qualified Data.Map.Strict as M
+import qualified Data.IntMap.Strict as IM
 
 {-# ANN module "HLint: ignore Redundant do" #-}
 
@@ -27,7 +29,6 @@ checkResult (CW ws (FW dim sites hints)) expectJust actual = case actual of
     Nothing -> expectJust == False
     Just rect ->
         let areDimsMatch = ((1,1), dim) == Arr.bounds rect
-            (nRows,nCols) = dim
             siteCoords (Site l c dir) = take l (iterate next c)
               where
                 next = case dir of
@@ -42,9 +43,16 @@ checkResult (CW ws (FW dim sites hints)) expectJust actual = case actual of
             areHintsMatching = all checkHint (M.toList hints)
               where
                 checkHint (c,ch) = rect Arr.! c == Just ch
+            -- extract the corresponding word given a site
+            -- "fromJust" should be safe as the check should have been done
+            -- with "isMatchingShape"
+            extractWord = map (fromJust . (rect Arr.!)) . siteCoords
+            allFilledWords = map extractWord sites
+            correctWordOccurrence = sort (concat $ IM.elems ws) == sort allFilledWords
         in areDimsMatch -- 1. dimension should match
            && isMatchingShape -- 2. in desired shape: cells are properly filled.
            && areHintsMatching -- 3. hints should be respected
+           && correctWordOccurrence -- 4. all words are used, no less, no more
 
 main :: IO ()
 main = hspec $ do
