f67631fa81610d3181427325b0fb44698f1f683e
Javran Cheng <Javran.c@gmail.com>
2016-08-25 18:22:55 -0400

    normal

--- Isabelle-playground/BoolExprs.thy
+++ Isabelle-playground/BoolExprs.thy
@@ -34,4 +34,36 @@ lemma "valif (bool2if b) env = value b env"
 apply(induct_tac b) apply(auto)
 done
 
+primrec normif :: "ifex \<Rightarrow> ifex \<Rightarrow> ifex \<Rightarrow> ifex" where
+"normif (CIF b) t e = IF (CIF b) t e" |
+"normif (VIF x) t e = IF (VIF x) t e" |
+"normif (IF b t e) u f = normif b (normif t u f) (normif e u f)"
+
+primrec norm :: "ifex \<Rightarrow> ifex" where
+"norm (CIF b) = CIF b" |
+"norm (VIF x) = VIF x" |
+"norm (IF b t e) = normif b (norm t) (norm e)"
+
+
+lemma [simp]: "\<forall> t e. valif (normif b t e) env = valif (IF b t e) env"
+apply(induct_tac b) apply(auto)
+done
+
+primrec normal :: "ifex \<Rightarrow> bool" where
+"normal (CIF b) = True" |
+"normal (VIF _) = True" |
+"normal (IF b t e) = (normal t \<and> normal e \<and>
+  (case b of
+    CIF b \<Rightarrow> True |
+    VIF x \<Rightarrow> True |
+    IF x y z \<Rightarrow> False))"
+
+lemma [simp]: "\<forall> t e . normal(normif b t e) = (normal t \<and> normal e)"
+apply(induct_tac b) apply(auto)
+done
+
+theorem "valif (norm b) env = valif b env"
+apply(induct_tac b) apply(auto)
+done
+
 end
