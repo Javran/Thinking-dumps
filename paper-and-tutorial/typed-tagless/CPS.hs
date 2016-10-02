33498e31c859734cf934b934329d60f7c2bec205
Javran Cheng <Javran.c@gmail.com>
2016-10-01 20:25:32 -0400

    aux functions

--- paper-and-tutorial/typed-tagless/CPS.hs
+++ paper-and-tutorial/typed-tagless/CPS.hs
@@ -28,3 +28,17 @@ data Falsum
 
 newtype CPS repr w a =
     CPS { cpsr :: repr ((CPSTypeTr w a -> w) -> w) }
+
+-- transforms a value into a lambda which feeds the value to whatever function
+-- it is given.
+-- note that the type of the actual representation is really what "CPSTypeTr w a" computes,
+-- but the result is of type "CPS repr w a"
+cpsv :: Semantics repr => repr (CPSTypeTr w a) -> CPS repr w a
+cpsv v = CPS . lam $ \k -> app k v
+
+-- this seems to just wrap a continuation inside datatype
+cpsk :: Semantics repr => (repr (CPSTypeTr w a -> w) -> repr w) -> CPS repr w a
+cpsk = CPS . lam
+
+appk :: Semantics repr => CPS repr w a -> (repr (CPSTypeTr w a) -> repr w) -> repr w
+appk (CPS e) f = app e (lam f)
