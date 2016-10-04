5017cba2d94000462dd96f098404ca2592965e4f
Javran Cheng <Javran.c@gmail.com>
2016-10-03 23:54:19 -0400

    few notes answering original comment

--- paper-and-tutorial/typed-tagless/TDPE.hs
+++ paper-and-tutorial/typed-tagless/TDPE.hs
@@ -20,3 +20,48 @@ infixr 8 -->
 r1 --> r2 = RR { reify = \m -> lam (reify r2 . m . reflect r1)
                , reflect = \o -> reflect r2 . app o . reify r1
                }
+{-
+
+bbf1 :: a -> b -> a
+
+the only function (ignoring "undefined" and non-terminating terms)
+that makes sense is "bbf1 = \x -> \y -> x"
+
+bbf2 :: (c -> c) -> c -> c
+
+we actually have 2 options for this:
+
+1. bbf2 f x = f x
+
+2. bbf2 _ x = x
+   (this one actually has a more general type:
+    bbf2 :: b -> a -> a)
+
+for the next part I don't get it why reify_aa :: (a -> a) -> repr (a -> a)
+is actually reify_aa (repr a -> repr a) -> repr (a -> a).
+when we assign a := repr a,
+shouldn't we also change "repr (a -> a)" to "repr (repr a -> repr a)"?
+
+what if:
+
+reify_aa :: (forall a. a -> a) -> forall a. repr (a -> a) ?
+
+or:
+
+reify_aa :: forall b. (forall a. a -> a) -> repr (b -> b)
+
+let a ~ repr a
+
+we'll have:
+
+reify_aa :: forall b. (repr a -> repr a) -> repr (b -> b)
+
+so if we fix b ~ a, we'll have:
+
+reify_aa :: (repr a -> repr a) -> repr (a -> a)
+
+which we have already seen one of the functions that has this type:
+
+reify_aa = lam
+
+-}
