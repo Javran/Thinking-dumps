9cfa01ae64c72ac30aac23f1b3f886c959af85a3
Javran Cheng <Javran.c@gmail.com>
2016-10-13 22:31:00 -0400

    complete type level bubble sort

--- paper-and-tutorial/embedding-eff-sys/EffSys.hs
+++ paper-and-tutorial/embedding-eff-sys/EffSys.hs
@@ -26,3 +26,26 @@ type family Pass (l :: [*]) :: [*] where
 -- Min and Max are sorting mechanism
 type family Min (a :: k) (b :: k) :: k
 type family Max (a :: k) (b :: k) :: k
+
+type family Bubble l l' where
+    -- note that here "l'" is used as a list length counter:
+    -- when the counter reaches 0 ("'[]"), we stop applying it to "Pass"
+    Bubble l '[] = l
+    -- when the counter is non-zero (guaranteed by having (_ ': _) pattern
+    -- we recursively deal with "Bubble l xs" first, and then apply a "Pass"
+    -- to the resulting type
+    Bubble l (x ': xs) = Pass (Bubble l xs)
+{- say if we have a type level list of size 3, [a,b,c]:
+
+   Bubble [a,b,c] [a,b,c]
+=> Pass (Bubble [a,b,c] [b,c])
+=> Pass (Pass (Bubble [a,b,c] [b]))
+=> Pass (Pass (Pass (Bubble [a,b,c] [])))
+=> Pass (Pass (Pass [a,b,c]))
+
+so if the list length is n, we will have n passes applications "Pass"
+-}
+
+-- the interface to bubble sort, the output won't
+-- always be correct if Bubble's two argument list mismatch in length
+type Sort s = Bubble s s
