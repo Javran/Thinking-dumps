68898a26ca74bc1c201e5604a3a21344d22d3f22
Javran Cheng <Javran.c@gmail.com>
2016-11-04 23:39:21 -0400

    ArrowChoice for conditional computation

--- paper-and-tutorial/monads-to-arrows/Interp.hs
+++ paper-and-tutorial/monads-to-arrows/Interp.hs
@@ -6,14 +6,21 @@ import Data.Maybe
 data Exp
   = Var String
   | Add Exp Exp
+  | If Exp Exp Exp
 
-type Val = Int
+data Val = VNum Int | VBl Bool
 
 type Env = [(String,Val)]
 
 liftA2 :: Arrow a => (b -> c -> d) -> a e b -> a e c -> a e d
 liftA2 op f g = (f &&& g) >>> arr (uncurry op)
 
-eval :: Arrow a => Exp -> a Env Val
+eval :: (Arrow a, ArrowChoice a) => Exp -> a Env Val
 eval (Var s) = arr (fromJust . lookup s)
-eval (Add e1 e2) = liftA2 (+) (eval e1) (eval e2)
+eval (Add e1 e2) = liftA2 add (eval e1) (eval e2)
+  where
+    add (VNum x) (VNum y) = VNum (x+y)
+eval (If eIf eThen eElse) =
+    (eval eIf &&& arr id) >>>
+    arr (\(VBl b,env) -> if b then Left env else Right env) >>>
+    (eval eThen ||| eval eElse)
