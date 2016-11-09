a8ff4350c8d4e1c69a455c326c533bdc9c5dc06a
Javran Cheng <Javran.c@gmail.com>
2016-11-09 00:05:12 -0500

    Arrow version partial impl

---
+++ paper-and-tutorial/monads-to-arrows/SP2.hs
@@ -0,0 +1,22 @@
+module SP2 where
+
+import Control.Arrow
+import Data.List
+import qualified Control.Category as Cat
+
+-- TODO: not sure how does "b" affect things, but let's go ahead and try it out.
+data StaticParser s = SP Bool [s]
+
+newtype DynamicParser s a b = DP ((a,[s]) -> (b,[s]))
+
+data Parser s a b = P (StaticParser s) (DynamicParser s a b)
+
+instance Eq s => Cat.Category (Parser s) where
+    P (SP empty2 starters2) (DP p2) .
+      P (SP empty1 starters1) (DP p1) =
+        P (SP (empty1 && empty2)
+              (starters1 `union` if empty1 then starters2 else []))
+          (DP (p2 . p1))
+
+instance Eq s => Arrow (Parser s) where
+    arr f = P (SP True []) (DP (\(b,s) -> (f b,s)))
