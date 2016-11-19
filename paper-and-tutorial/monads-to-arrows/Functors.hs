c65b92e5a892da0644d31e64230a2ec52eea44d5
Javran Cheng <Javran.c@gmail.com>
2016-11-18 23:51:25 -0500

    shorten impl

--- paper-and-tutorial/monads-to-arrows/Functors.hs
+++ paper-and-tutorial/monads-to-arrows/Functors.hs
@@ -7,11 +7,14 @@ newtype StateMT s m a = SM (s -> m (a,s)) deriving (Functor)
 
 instance Monad m => Applicative (StateMT s m) where
     pure v = SM (\s -> pure (v,s))
-    SM f <*> SM v = SM (\s -> f s >>= \(f',s') -> v s' >>= \(v',s'') -> pure (f' v',s''))
+    (<*>) = ap
+
+bindStateMT :: Monad m => StateMT s m a -> (a -> StateMT s m b) -> StateMT s m b
+bindStateMT x f = SM (\s ->
+                      let SM x' = x
+                      in x' s >>= \(v,s') ->
+                      let SM f' = f v
+                      in f' s')
 
 instance Monad m => Monad (StateMT s m) where
-    x >>= f = SM (\s ->
-                  let SM x' = x
-                  in x' s >>= \(v,s') ->
-                  let SM f' = f v
-                  in f' s')
+    (>>=) = bindStateMT
