f62e6162fe83cecb5bc9de2a5a1962377c5f8a89
Javran Cheng <Javran.c@gmail.com>
2016-11-19 18:34:55 -0500

    state monad transformer facilities

--- paper-and-tutorial/monads-to-arrows/Functors.hs
+++ paper-and-tutorial/monads-to-arrows/Functors.hs
@@ -16,3 +16,16 @@ instance Monad m => Monad (StateMT s m) where
     (SM x) >>= f = SM
          (x >=> \(v,s') ->
           let SM f' = f v in f' s')
+
+liftState :: Monad m => m a -> StateMT s m a
+liftState m = SM $ \s -> m >>= \a -> pure (a,s)
+
+fetch :: Monad m => StateMT s m s
+fetch = SM $ \s -> pure (s,s)
+
+-- returns the old state
+store :: Monad m => s -> StateMT s m s
+store s = SM $ \oldS -> pure (oldS,s)
+
+store_ :: Monad m => s -> StateMT s m ()
+store_ = void . store
