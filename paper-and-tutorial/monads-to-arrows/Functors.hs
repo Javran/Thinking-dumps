8ee6f6e0556faff5725fe91c1f798fed15453f56
Javran Cheng <Javran.c@gmail.com>
2016-11-19 18:51:19 -0500

    arrow impl for MaybeFunctor

--- paper-and-tutorial/monads-to-arrows/Functors.hs
+++ paper-and-tutorial/monads-to-arrows/Functors.hs
@@ -1,6 +1,8 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveFunctor, TupleSections #-}
 module Functors where
 
+import qualified Control.Category as Cat
+import Control.Arrow
 import Control.Monad
 
 newtype StateMT s m a = SM (s -> m (a,s)) deriving (Functor)
@@ -29,3 +31,22 @@ store s = SM $ \oldS -> pure (oldS,s)
 
 store_ :: Monad m => s -> StateMT s m ()
 store_ = void . store
+
+newtype MaybeFunctor a b c = MF (a b (Maybe c))
+
+-- the implementation can be guided just by type
+liftMaybe :: Arrow a => a b c -> MaybeFunctor a b c
+liftMaybe f = MF (f >>> arr Just)
+
+instance (Arrow a, ArrowChoice a) => Cat.Category (MaybeFunctor a) where
+    id = MF (arr Just)
+    (MF g) . (MF f) = MF (f >>> arr dispatch >>> (g ||| arr id))
+      where
+        dispatch z = case z of
+            Just c -> Left c
+            Nothing -> Right Nothing
+
+instance (Arrow a, ArrowChoice a) => Arrow (MaybeFunctor a) where
+    arr f = MF (arr (Just . f))
+    first (MF f) = MF (first f >>>
+                       arr (\(c',d) -> (,d) <$> c'))
