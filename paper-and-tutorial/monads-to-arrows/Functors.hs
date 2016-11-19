841ecfe86c42260741116fbb1bf39a7fa084ec77
Javran Cheng <Javran.c@gmail.com>
2016-11-18 23:59:20 -0500

    further simplify

--- paper-and-tutorial/monads-to-arrows/Functors.hs
+++ paper-and-tutorial/monads-to-arrows/Functors.hs
@@ -5,16 +5,14 @@ import Control.Monad
 
 newtype StateMT s m a = SM (s -> m (a,s)) deriving (Functor)
 
+-- for "ap" to be usable the constrain "m" is necessary
+-- although we can write the implementation, let just save some human work
+-- and let machine do its job
 instance Monad m => Applicative (StateMT s m) where
     pure v = SM (\s -> pure (v,s))
     (<*>) = ap
 
-bindStateMT :: Monad m => StateMT s m a -> (a -> StateMT s m b) -> StateMT s m b
-bindStateMT x f = SM (\s ->
-                      let SM x' = x
-                      in x' s >>= \(v,s') ->
-                      let SM f' = f v
-                      in f' s')
-
 instance Monad m => Monad (StateMT s m) where
-    (>>=) = bindStateMT
+    (SM x) >>= f = SM
+         (x >=> \(v,s') ->
+          let SM f' = f v in f' s')
