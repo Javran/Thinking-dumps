9cf2b16b47a7cf56be9f63852281836ef40c21e3
Javran Cheng <Javran.c@gmail.com>
2016-11-27 23:49:49 -0500

    stuff about currying

---
+++ paper-and-tutorial/arrows-and-computation/Currying.hs
@@ -0,0 +1,24 @@
+module Currying where
+
+import Control.Arrow
+
+-- curryA gives us a way to refer to input to arrows
+-- the same way as we refer to the argument to a function
+
+{-# ANN curryA "HLint: ignore Redundant bracket" #-}
+curryA :: Arrow a => a (b,c) d -> (b -> a c d)
+curryA f b = arr (\c -> (b,c)) >>> f
+
+{-
+
+now if we want to find the inverse of curryA,
+we will look for a function of the following type:
+
+(b -> a c d) -> a (b,c) d
+
+in the paper, we try to plug in "id :: forall a. a -> a", which
+gives us a function of type:
+
+(a c d -> a c d) -> a (a c d, c) d
+
+-}
