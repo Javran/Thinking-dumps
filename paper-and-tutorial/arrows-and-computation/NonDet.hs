222bfd0d513d0a5c40a7aaed2c6657a06afc1a65
Javran Cheng <Javran.c@gmail.com>
2016-11-28 20:45:45 -0500

    NonDet as Arrow

---
+++ paper-and-tutorial/arrows-and-computation/NonDet.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TupleSections #-}
+module NonDet where
+
+import qualified Control.Category as Cat
+import Control.Arrow
+import Control.Monad
+
+-- the Kleisli arrow of list monad in disguise
+newtype NonDet i o = ND { runND :: i -> [o] }
+
+arrS :: (i -> o) -> NonDet i o
+arrS f = ND (pure . f)
+
+compS :: NonDet a b -> NonDet b c -> NonDet a c
+compS (ND f) (ND g) = ND (f >=> g)
+
+firstS :: NonDet a b -> NonDet (a,d) (b,d)
+firstS (ND f) = ND $ \(a,d) -> (,d) <$> f a
+
+instance Cat.Category NonDet where
+    id = arrS id
+    g . f = compS f g
+
+instance Arrow NonDet where
+    arr = arrS
+    first = firstS
