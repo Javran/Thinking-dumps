e8ef768b2216200f93afb264ff20513d7683a74f
Javran Cheng <Javran.c@gmail.com>
2016-12-03 17:39:23 -0500

    simplify compE

--- paper-and-tutorial/arrows-and-computation/Except.hs
+++ paper-and-tutorial/arrows-and-computation/Except.hs
@@ -9,10 +9,12 @@ arrE :: Arrow a => (i -> o) -> Except a i o
 arrE f = E (arr (Right . f))
 
 compE :: ArrowChoice arr => Except arr a b -> Except arr b c -> Except arr a c
-compE (E f) (E g) = E $ f >>> right g >>> arr (\e -> case e of
-                                                   Left msg -> Left msg
-                                                   Right (Left msg) -> Left msg
-                                                   Right (Right v) -> Right v)
+compE (E f) (E g) = E $ f >>> right g >>> arr collapse
+  where
+    collapse e = case e of
+        Left m -> Left m
+        Right (Left m) -> Left m
+        Right (Right v) -> Right v
 
 instance ArrowChoice a => Cat.Category (Except a) where
     id = arrE id
