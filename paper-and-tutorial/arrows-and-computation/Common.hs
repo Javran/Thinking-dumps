412ac5c32f4bae20b24b99331475be36f4aedaa8
Javran Cheng <Javran.c@gmail.com>
2016-12-11 23:00:20 -0500

    is every ArrowApply ArrowChoice?

--- paper-and-tutorial/arrows-and-computation/Common.hs
+++ paper-and-tutorial/arrows-and-computation/Common.hs
@@ -1,5 +1,7 @@
 module Common where
 
+import Control.Arrow
+
 trace :: ((a,s) -> (b,s)) -> a -> b
 trace f a = let (b,s) = f (a,s) in b
 
@@ -13,3 +15,12 @@ distr :: (Either a b, c) -> Either (a,c) (b,c)
 distr ~(e,c) = case e of
     Left a -> Left (a,c)
     Right b -> Right (b,c)
+
+-- TODO: I think there must be a way to turn instance of ArrowApply into ArrowChoice.
+-- if so, all we need is to try implementing the following function.
+-- (for now I'm not sure how to do so though: probably the following stuff
+-- is entirely wrong because we have to do it in a somehow point-free manner)
+arrAppToChoice :: ArrowApply arrow => arrow i o -> arrow (Either i a) (Either o a)
+arrAppToChoice arrow = arr $ \ e -> case e of
+    Left i -> let x = returnA (arrow,i) in _
+    Right a -> Right a
