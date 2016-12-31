143a913078c59da6dfa28d0da4df194bd9f31774
Javran Cheng <Javran.c@gmail.com>
2016-12-30 23:11:13 -0500

    Category instance for AutoFunctor

--- paper-and-tutorial/arrows-and-computation/AutoFunctor.hs
+++ paper-and-tutorial/arrows-and-computation/AutoFunctor.hs
@@ -9,8 +9,13 @@ newtype AutoFunctor a i o = AF (a i (o, AutoFunctor a i o))
 arrAF :: Arrow a => (i -> o) -> AutoFunctor a i o
 arrAF f = fix $ \arrow -> AF (arr (\i -> (f i,arrow)))
 
-compAF :: Arrow ar => AutoFunctor ar a b -> AutoFunctor ar b c -> AutoFunctor ar a c
-compAF (AF f) (AF g) =
-    AF (f >>> first g >>> arr assoc >>> second (arr (\(g',f') -> compAF f' g')))
+-- note that argument order are changed to allow us using "uncurry" to same some work
+compAF :: Arrow ar => AutoFunctor ar b c -> AutoFunctor ar a b -> AutoFunctor ar a c
+compAF (AF g) (AF f) =
+    AF (f >>> first g >>> arr assoc >>> second (arr (uncurry compAF)))
   where
     assoc ((a,b),c) = (a,(b,c))
+
+instance Arrow ar => Cat.Category (AutoFunctor ar) where
+    id = arrAF id
+    (.) = compAF
