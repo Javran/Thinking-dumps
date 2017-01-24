8c51ce03004cd42097dbdb2c1ad87ca6eb5bde0b
Javran Cheng <Javran.c@gmail.com>
2017-01-23 22:17:06 -0500

    runFFState for FFree (StateEff s)

--- paper-and-tutorial/free-monad/StateEff.hs
+++ paper-and-tutorial/free-monad/StateEff.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GADTs #-}
 module StateEff where
 
-import Free hiding (FFState)
+import Free hiding (FFState, runFFState)
 
 -- the definition of StateEff alone does not do anything,
 -- but it still make (FFree (StateEff s)) a monad.
@@ -33,3 +33,19 @@ testFFState :: FFState Int Int
 testFFState = do
     putEff 10
     modFFState (+ 20)
+
+{-
+but actually this is a lie: we have to give an implementation somewhere,
+and for freer monad this relies on the "runXXX" function.
+
+so if I understand it correctly: Freer monad (by the approach described by this file)
+let us write a description of what we are doing without the requirement of actually
+implementing it. and the task of implementation is then moved to the interpreter
+of the monad.
+-}
+runFFState :: FFState s a -> s -> (a,s)
+runFFState m s = case m of
+    FPure v -> (v,s)
+    FImpure gx q -> case gx of
+        Get -> runFFState (q s) s
+        Put s' -> runFFState (q ()) s'
