eb012a4de8cabf144c5f12d6e84f365458b58889
Javran Cheng <Javran.c@gmail.com>
2017-01-27 22:43:31 -0500

    cleanup

--- paper-and-tutorial/free-monad/TList.hs
+++ paper-and-tutorial/free-monad/TList.hs
@@ -32,7 +32,8 @@ TODO: could we use GHC.TypeLits for this?
 - https://hackage.haskell.org/package/base-4.9.1.0/docs/GHC-TypeLits.html
 - http://ponies.io/posts/2014-07-30-typelits.html
 
-I think this can eliminate the need for TEQ
+I think this can eliminate the need for TEQ: we can just use "CmpNat n1 n2",
+if we get 'EQ as the result, n1 and n2 are the same, otherwise they are not.
 
 -}
 
@@ -40,7 +41,7 @@ data Nat = Z | S Nat
 
 {-
 to be TCode seems to be a way of converting a type into a natural number
-so it can be distinguished from other types
+so every type (that TCode can encode) can be distinguished from other types
 -}
 -- TCode :: * -> Nat
 type family TCode (n :: * -> *) :: Nat
@@ -81,7 +82,6 @@ instance Includes e t => Includes' 'False e e1 t where
     prj' _ (H _) = Nothing
     prj' _ (T e) = prj e
 
--- TEQ :: Nat -> Nat -> Bool
 type family TEQ (n1 :: Nat) (n2 :: Nat) :: Bool
 type instance TEQ 'Z 'Z           = 'True
 type instance TEQ ('S _) 'Z       = 'False
