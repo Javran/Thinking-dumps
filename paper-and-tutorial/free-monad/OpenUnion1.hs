f2adab0a4bb614b7c16f33098f14119381b92910
Javran Cheng <Javran.c@gmail.com>
2017-01-31 23:38:06 -0500

    injection and projection?

--- paper-and-tutorial/free-monad/OpenUnion1.hs
+++ paper-and-tutorial/free-monad/OpenUnion1.hs
@@ -15,7 +15,7 @@ import Data.Typeable
 -- TODO: why this is necessary?
 newtype Id x = Id x
 
-data Union (r :: * -> *) v where
+data Union r v where
     Union :: (Functor t, Typeable t) => Id (t v) -> Union r v
 
 instance Functor (Union r) where
@@ -25,6 +25,18 @@ instance Functor (Union r) where
 infixr 1 :>
 data ((a :: * -> *) :> b)
 
+-- means "t" is a member of "r" ("r" is phantom?)
 class Member (t :: * -> *) r
 instance Member t (t :> r)
 instance Member t r => Member t (t' :> r)
+
+inj :: (Functor t, Typeable t, Member t r) => t v -> Union r v
+inj x = Union (Id x)
+
+-- is "gcast1" casting across type of kind * -> * ?
+-- by using typed hole, the value "v" below is of type (Id (_ v)), where
+-- the "_" part is still a myth to me .. but somehow it is possible for that to match
+-- "Id (t v)" thus cast-able
+prj :: (Functor t, Typeable t, Member t r) => Union r v -> Maybe (t v)
+prj (Union v) | Just (Id (x :: t v)) <- gcast1 v = Just x
+prj _ = Nothing
