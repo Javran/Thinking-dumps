9b32bc4fea8f554d26a4b2e02c9d082c27e2462c
Javran Cheng <Javran.c@gmail.com>
2017-02-03 23:55:53 -0500

    fix typecheck problem.
    seems we need the index evidence.

--- paper-and-tutorial/free-monad/OpenUnion5.hs
+++ paper-and-tutorial/free-monad/OpenUnion5.hs
@@ -70,10 +70,10 @@ decomp (Union ind v)
     | otherwise = Left (Union (pred ind) v)
 
 class Member t r => MemberU2 (tag :: k -> * -> *) (t :: * -> *) r | tag r -> t
--- TODO: seems the following line no longer typechecks
--- instance (MemberU' (EQU t1 t2) tag t1 (t2 ': r)) => MemberU2 tag t1 (t2 ': r)
+instance ( KnownNat (FindElem t1 (t2 : r))
+         , MemberU' (EQU t1 t2) tag t1 (t2 ': r)) => MemberU2 tag t1 (t2 ': r)
 
 class Member t r => MemberU' (f :: Bool) (tag :: k -> * -> *) (t :: * -> *) r | tag r -> t
-instance MemberU' True tag (tag e) (tag e ': r)
+instance MemberU' 'True tag (tag e) (tag e ': r)
 instance (Member t (t' ': r), MemberU2 tag t r) =>
-    MemberU' False tag t (t' ': r)
+    MemberU' 'False tag t (t' ': r)
