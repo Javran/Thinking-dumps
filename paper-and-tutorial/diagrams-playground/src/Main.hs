76bc7ae5451793daa96bfd20ae3f6a3124f3f34a
Javran Cheng <Javran.c@gmail.com>
2017-03-14 23:28:11 -0400

    finish ex2

--- paper-and-tutorial/diagrams-playground/src/Main.hs
+++ paper-and-tutorial/diagrams-playground/src/Main.hs
@@ -14,6 +14,14 @@ import Diagrams.TwoD.Vector (e)
 ex1 :: Diagram B
 ex1 = fromOffsets (concat (replicate 5 [V2 1 1, V2 1 (-1)]))
 
+ex2 :: Diagram B
+ex2 = atPoints points (repeat node)
+  where
+    points :: [Point V2 Double]
+    points = [ origin .+^ (5 *^ e (r @@ rad)) | r <- take 7 [tau/4, tau/4-step ..] ]
+    step = tau / 2 / 6
+    node = circle 1 # fc blue
+
 ex3 :: Diagram B
 ex3 = mconcat $ do
     r <- [step, tau/10 + step .. tau]
@@ -23,4 +31,4 @@ ex3 = mconcat $ do
     step = tau/30
 
 main :: IO ()
-main = mainWith ex3
+main = mainWith ex2
