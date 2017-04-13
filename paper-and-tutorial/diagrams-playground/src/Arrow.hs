fd33a81f8b180e03f6177ef5c19192b05bce1fd4
Javran Cheng <Javran.c@gmail.com>
2017-04-12 23:47:42 -0400

    ex 1 done

--- paper-and-tutorial/diagrams-playground/src/Arrow.hs
+++ paper-and-tutorial/diagrams-playground/src/Arrow.hs
@@ -17,10 +17,11 @@ arrBundle = nest "arr" $ Actioned
     ]
 
 ex1_1 :: Diagram B
-ex1_1 = sDot <> eDot <> circle 1
+ex1_1 = (sDot <> eDot <> xArr <> circle 1) # centerXY # pad 1.1
   where
     spot = circle 0.02 # lw none
     ptStart = origin .+^ (unitX # rotateBy (1/8))
     ptEnd = origin .+^ (unitX # rotateBy (1/2))
     sDot = spot # fc blue # moveTo ptStart
     eDot = spot # fc red # moveTo ptEnd
+    xArr = arrowBetween' (with & headLength .~ veryLarge) ptStart ptEnd
