bd85c56976d531b635873054d4359757087b02d1
Javran Cheng <Javran.c@gmail.com>
2018-07-09 23:38:15 -0400

    allvers

--- javascript-algorithms/knuth-morris-pratt.js
+++ javascript-algorithms/knuth-morris-pratt.js
@@ -3,7 +3,7 @@
  */
 
 // as indicated in wikipedia
-const KMP = (str, pat) => {
+const KMP1 = (str, pat) => {
   const T = new Array(pat.length)
   let pos = 1
   let cnd = 0
@@ -25,7 +25,26 @@ const KMP = (str, pat) => {
     }
   }
   T[pos] = cnd
-  console.log(T)
+
+  let j = 0
+  let k = 0
+  const ans = []
+  while (j < str.length) {
+    if (pat.codePointAt(k) === str.codePointAt(j)) {
+      ++k, ++j
+      if (k === pat.length) {
+        // found
+        ans.push(j - k)
+        k = T[k]
+      }
+    } else {
+      k = T[k]
+      if (k < 0) {
+        ++k, ++j
+      }
+    }
+  }
+  return ans
 }
 
 // from geeksforgeeks
@@ -51,12 +70,52 @@ const KMP2 = (str, pat) => {
       }
     }
   }
-  console.log(T)
+
+  let i = 0, j = 0
+  const ans = []
+  while (i < str.length) {
+    const isMatch = pat.codePointAt(j) === str.codePointAt(i)
+    if (isMatch) {
+      ++j, ++i
+    }
+    if (j === pat.length) {
+      ans.push(i-j)
+      j = T[j-1]
+    } else if (i < str.length && !isMatch) {
+      if (j != 0) {
+        j = T[j-1]
+      } else {
+        ++i
+      }
+    }
+  }
+  return ans
 }
 
-[KMP, KMP2].forEach(KMPImpl => {
-  console.log(KMPImpl('', 'ABCDABD'))
-  console.log(KMPImpl('', 'PARTICIPATE IN PARACHUTE'))
-  console.log(KMPImpl('', "AAACAAAAAC"))
-  console.log(KMPImpl('', 'AABAACAABAA'))
+// modified from CS 97SI Stanford University By Jaehyun Park
+const KMP3 = (str, pat) => {
+  const pi = new Array(pat.length + 1)
+  pi[0] = -1
+  let k = -1
+  for (let i = 1; i <= pat.length; ++i) {
+    while (k >= 0 && pat.codePointAt(k) !== pat.codePointAt(i-1))
+      k = pi[k]
+    pi[i] = ++k
+  }
+  k = 0
+  const ans = []
+  for (let i = 0; i < str.length; ++i) {
+    while (k >= 0 && pat.codePointAt(k) !== str.codePointAt(i))
+      k = pi[k]
+    ++k
+    if (k === pat.length) {
+      ans.push(i-pat.length+1)
+      k = pi[k]
+    }
+  }
+  return ans
+}
+
+[KMP1, KMP2, KMP3].forEach(KMPImpl => {
+  console.log(KMPImpl('ABCABCABABCABABCABABABC', 'ABABC'))
 })
