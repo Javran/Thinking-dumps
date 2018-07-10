f250437587c0103b5516a6de547dc8cac41e7ead
Javran Cheng <Javran.c@gmail.com>
2018-07-09 21:29:05 -0400

    min heap and stuff

---
+++ javascript-algorithms/README.md
@@ -0,0 +1,1 @@
+Data structure and algorithm implemented in JavaScript.
---
+++ javascript-algorithms/binary-heap.js
@@ -0,0 +1,89 @@
+function BinHeap(elemToKey) {
+  this.size = 0
+  this.container = []
+  this.elemToKey = elemToKey
+}
+
+const siftUp = pq => ind => {
+  const {container, elemToKey} = pq
+  while (ind !== 0) {
+    const parentInd = (ind - 1) >> 1
+    if (elemToKey(container[parentInd]) > elemToKey(container[ind])) {
+      const tmp = container[parentInd]
+      container[parentInd] = container[ind]
+      container[ind] = tmp
+      ind = parentInd
+    } else {
+      break
+    }
+  }
+}
+
+const siftDown = pq => ind => {
+  const {size, container, elemToKey} = pq
+  while (true) {
+    const lcInd = ind*2+1
+    const rcInd = ind*2+2
+    if (lcInd >= size)
+      break
+    let preferInd = ind
+    if (
+      elemToKey(container[lcInd]) <= elemToKey(container[preferInd])
+    )
+      preferInd = lcInd
+    if (
+      rcInd < size &&
+      elemToKey(container[rcInd]) <= elemToKey(container[preferInd])
+    )
+      preferInd = rcInd
+
+    if (preferInd !== ind) {
+      const tmp = container[preferInd]
+      container[preferInd] = container[ind]
+      container[ind] = tmp
+      ind = preferInd
+    } else {
+      break
+    }
+  }
+}
+
+BinHeap.prototype.insert = function(e) {
+  const eInd = this.size
+  this.container[this.size] = e
+  ++this.size
+  siftUp(this)(eInd)
+}
+
+BinHeap.prototype.extractMin = function() {
+  if (this.size === 0)
+    return null
+  const ret = this.container[0]
+  this.container[0] = this.container[this.size-1]
+  --this.size
+  siftDown(this)(0)
+  return ret
+}
+
+const test = () => {
+  const xs = []
+  const hp = new BinHeap(x => x)
+  for (let i = 0; i < 40000; ++i) {
+    const val = Math.floor(Math.random() * (1 << 30))
+    xs.push(val)
+    hp.insert(val)
+  }
+  console.time('stock')
+  xs.sort((x, y) => x - y)
+  console.timeEnd('stock')
+  const ys = []
+  console.time('min heap')
+  while (hp.size > 0)
+    ys.push(hp.extractMin())
+  console.timeEnd('min heap')
+  for (let i = 0; i < xs.length; ++i)
+    console.assert(xs[i] === ys[i])
+  console.log(`${xs.length} elements sorted, test ok`)
+}
+
+test()
