4b282180ac37d9aae1f0fd48a603ba8ffdeed248
Javran Cheng <javran@google.com>
2019-05-07 16:57:42 -0700

    initial stuff

---
+++ the-haskell-school-of-music/src/Ch02Demo.hs
@@ -0,0 +1,11 @@
+module Ch02Demo where
+
+import Euterpea
+
+t251 :: Music Pitch
+t251 = dMinor :+: gMajor :+: cMajor
+  where
+    dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+    gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+    cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+    
