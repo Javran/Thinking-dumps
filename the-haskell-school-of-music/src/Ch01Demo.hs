9392c3c8619581a9616f19f4af265d2c0dab22ca
Javran Cheng <Javran.c@gmail.com>
2019-05-08 21:56:58 -0700

    catching up on Ch01

--- the-haskell-school-of-music/src/Ch01Demo.hs
+++ the-haskell-school-of-music/src/Ch01Demo.hs
@@ -1,12 +1,15 @@
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Ch01Demo where
 
 import Euterpea
 
+
+hNote :: Dur -> Pitch -> Music Pitch
+hNote d p = note d p :=: note d (trans (-3) p)
+
+hList :: Dur -> [Pitch] -> Music Pitch
+hList d = foldr (\p r -> hNote d p :+: r) (rest 0)
+
 mel :: Pitch -> Pitch -> Pitch -> Music Pitch
-mel p1 p2 p3 =
-    (nq p1 :=: nq (tm3 p1))
-    :+: (nq p2 :=: nq (tm3 p2))
-    :+: (nq p3 :=: nq (tm3 p3))
-  where
-    nq = note qn
-    tm3 = trans (-3)
+mel p1 p2 p3 = hList qn [p1,p2,p3]
+
