33c6cc5f17e3efb46892fe7e951793b49a0a933b
Javran Cheng <Javran.c@gmail.com>
2019-05-08 21:59:47 -0700

    done ex 1.4

--- the-haskell-school-of-music/src/Ch01Demo.hs
+++ the-haskell-school-of-music/src/Ch01Demo.hs
@@ -3,13 +3,14 @@ module Ch01Demo where
 
 import Euterpea
 
+{- Ex 1.4 for adding interval as argument -}
 
-hNote :: Dur -> Pitch -> Music Pitch
-hNote d p = note d p :=: note d (trans (-3) p)
+hNote :: Int -> Dur -> Pitch -> Music Pitch
+hNote inv d p = note d p :=: note d (trans inv p)
 
-hList :: Dur -> [Pitch] -> Music Pitch
-hList d = foldr (\p r -> hNote d p :+: r) (rest 0)
+hList :: Int -> Dur -> [Pitch] -> Music Pitch
+hList inv d = foldr (\p r -> hNote inv d p :+: r) (rest 0)
 
 mel :: Pitch -> Pitch -> Pitch -> Music Pitch
-mel p1 p2 p3 = hList qn [p1,p2,p3]
+mel p1 p2 p3 = hList (-3) qn [p1,p2,p3]
 
