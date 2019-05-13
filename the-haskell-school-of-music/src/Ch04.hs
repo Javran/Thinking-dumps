91dcefdb14fc1dfaaff1f053800e1ec586509b7e
Javran Cheng <Javran.c@gmail.com>
2019-05-12 22:36:13 -0700

    impl times the efficient way

--- the-haskell-school-of-music/src/Ch04.hs
+++ the-haskell-school-of-music/src/Ch04.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Ch04 where
 
-import Euterpea
+import Euterpea hiding (times)
 
 twinkle :: Music Pitch
 twinkle = line1 [pat0, pat1, pat1, pat0]
@@ -11,3 +11,13 @@ twinkle = line1 [pat0, pat1, pat1, pat0]
     mkLine =
       line1
       . zipWith (\d pc -> note d (pc,4)) (replicate 6 qn <> [hn])
+
+times :: Int -> Music a -> Music a
+times n m
+  | n < 0 = error "expected non-negative input"
+  | n == 0 = rest 0
+  | n == 1 = m
+  | otherwise = case n `quotRem` 2 of
+      (n',0) -> let half = times n' m in half :+: half
+      (n',1) -> let half = times n' m in m :+: half :+: half
+      _ -> error "unreachable"
