384b413448b1a347ea5d99596c1972f6e4efc3fe
Javran Cheng <Javran.c@gmail.com>
2019-05-13 23:00:08 -0700

    Ch05 done

--- the-haskell-school-of-music/src/Ch05.hs
+++ the-haskell-school-of-music/src/Ch05.hs
@@ -48,3 +48,56 @@ remainder' = fix $ \f a b -> if a < b then a else f (a-b) b
 remaindersAgree :: Bool
 remaindersAgree = and
   [ remainder a b == remainder' a b | a <- [1..10], b <- [1..10] ]
+
+{- Ex 5.4 -}
+apPairs :: [AbsPitch] -> [AbsPitch] -> [(AbsPitch, AbsPitch)]
+apPairs xs ys = [(x,y) | x <- xs, y <- ys, let diff = x - y, diff > 2, diff < 8]
+
+{-
+  Ex 5.5
+
+  just point-free refactor this. I wouldn't recommend spending time
+  understandng this point-less form.
+
+ -}
+hList' :: Dur -> [Pitch] -> Music Pitch
+hList' = (line .) . map . Ch01.hNote (-3)
+
+{- Ex 5.6 is already done in Ch04 -}
+
+{- Ex 5.7 -}
+ex5_7 :: [Double] -> [Double]
+ex5_7 = (((/2) . (+1)) <$>)
+
+
+{-
+  Ex 5.8
+
+  map f (map g xs) === map (f . g) xs [map fusion]
+
+  and:
+
+  map (\x -> (x+1)/2) xs
+  => map (/2) (map (+1) xs)
+
+ -}
+
+{- Ex 5.9 skipped as I've been using the standard practice -}
+
+{-
+  Ex 5.10
+
+  [5,10,15,20]
+  => [(*) 1 5, (*) 2 5, (*) 3 5, (*) 4 5]
+  => f1 [(*) 1, (*) 2, (*) 3, (*) 4] 5 = [(*) 1 5, (*) 2 5, (*) 3 5, (*) 4 5]
+  => f1 = flip (map . flip id)
+  => f2 (*) [1,2,3,4] = [(*) 1, (*) 2, (*) 3, (*) 4]
+  => f2 = map
+
+ -}
+ex5_10_Check :: Bool
+ex5_10_Check = f1 (f2 (*) [1,2,3,4]) 5 == [5,10,15,20 ::Int]
+  where
+    -- note that with same type signature, `id` and `($)` are the same thing.
+    f1 = flip (map . flip id)
+    f2 = map
