8d8ed1ac0d348b39a8cd819ce7262213c0c027fb
Javran Cheng <Javran.C@gmail.com>
2020-08-16 22:47:39 -0700

    some passing examples for the proper row.

--- the-haskell-school-of-music/test/Ch06Spec.hs
+++ the-haskell-school-of-music/test/Ch06Spec.hs
@@ -3,9 +3,15 @@ module Ch06Spec where
 import Ch06
 import Euterpea
 import Test.Hspec
+import Data.List
 
 spec :: Spec
-spec =
+spec = do
+  describe "properRow" $
+    specify "examples" $ do
+      properRow (line melProper) `shouldBe` True
+      properRow (line (reverse melProper)) `shouldBe` True
+      properRow (line (intersperse (rest en) melProper)) `shouldBe` True
   describe "retroPitches" $
     specify "examples" $
       retroPitches (line [c 4 en, d 4 qn])
