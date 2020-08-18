8c99788966404425d9ba1f17e15c406853c22faa
Javran Cheng <Javran.C@gmail.com>
2020-08-17 22:33:38 -0700

    some examples that produce False.

--- the-haskell-school-of-music/test/Ch06Spec.hs
+++ the-haskell-school-of-music/test/Ch06Spec.hs
@@ -1,17 +1,27 @@
 module Ch06Spec where
 
 import Ch06
+import Data.List
 import Euterpea
 import Test.Hspec
-import Data.List
 
 spec :: Spec
 spec = do
   describe "properRow" $
     specify "examples" $ do
       properRow (line melProper) `shouldBe` True
+      -- no matter of ordering.
       properRow (line (reverse melProper)) `shouldBe` True
+      -- rests should be ignored.
       properRow (line (intersperse (rest en) melProper)) `shouldBe` True
+      properRow
+        (line $
+           fmap
+             (note en . pitch)
+             (take 12 [20 ..]))
+        `shouldBe` True
+      properRow (line (drop 1 melProper)) `shouldBe` False
+      properRow (line $ head melProper : melProper) `shouldBe` False
   describe "retroPitches" $
     specify "examples" $
       retroPitches (line [c 4 en, d 4 qn])
