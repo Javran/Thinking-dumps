c78bb476126c6f90aa1f4f250c61ede29c798b4e
Javran Cheng <Javran.C@gmail.com>
2020-12-27 23:07:28 -0800

    begin with few exercises.

---
+++ exercism/new/go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting contains a simple function that prints hello world message to stdout.
+package greeting
+
+// Prints hello world message.
+func HelloWorld() string {
+	return "Hello, World!"
+}
