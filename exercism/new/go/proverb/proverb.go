b952caebe57cea945b8adfcde7456e6c790d5e0e
Javran Cheng <Javran.C@gmail.com>
2021-01-10 23:50:52 -0800

    golint and stupid statement.

--- exercism/new/go/proverb/proverb.go
+++ exercism/new/go/proverb/proverb.go
@@ -3,15 +3,16 @@ package proverb
 
 import "fmt"
 
-// Returns a slice of proverb given a slice of any length.
+// Proverb returns a slice of proverb given a slice of any length.
 func Proverb(rhyme []string) []string {
 	if len(rhyme) == 0 {
 		return nil
 	}
-	// Note: the exercise and its tests are not specific about the use of indefinite articles,
+	// Note: the exercise and its tests are not specific
+	// about the use of indefinite articles,
 	// here I choose to stick with "a" to keep it simple.
 	var results = make([]string, len(rhyme))
-	for i := 1; i < len(rhyme); i += 1 {
+	for i := 1; i < len(rhyme); i++ {
 		results[i-1] = fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i-1], rhyme[i])
 	}
 	results[len(rhyme)-1] = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
