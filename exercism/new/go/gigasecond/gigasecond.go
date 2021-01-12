1884b22712760bc152e486de8a51bea7ed4ad34a
Javran Cheng <Javran.C@gmail.com>
2021-01-11 22:20:10 -0800

    simplify a bit.

--- exercism/new/go/gigasecond/gigasecond.go
+++ exercism/new/go/gigasecond/gigasecond.go
@@ -2,20 +2,10 @@
 package gigasecond
 
 import (
-	"fmt"
 	"time"
 )
 
-var gigasecond time.Duration
-
-func init() {
-	var err error
-	gigasecond, err =
-		time.ParseDuration(fmt.Sprintf("%ds", 1_000_000_000))
-	if err != nil {
-		panic(err)
-	}
-}
+var gigasecond = time.Duration(1_000_000_000) * time.Second
 
 // AddGigasecond returns input time plus 1 gigaseconds.
 func AddGigasecond(t time.Time) time.Time {
