cc10d0c9887ac86448a8f637439d3e653db55145
Javran Cheng <Javran.C@gmail.com>
2021-01-23 17:14:31 -0800

    elim map.

--- exercism/new/go/isogram/isogram.go
+++ exercism/new/go/isogram/isogram.go
@@ -3,7 +3,6 @@ package isogram
 
 import (
 	"strings"
-	"unicode"
 )
 
 // sanitizeInput returns the input string without ' ' or '-'.
@@ -15,14 +14,13 @@ var sanitizeInput func(string) string = strings.NewReplacer(
 // IsIsogram tests whether a word or phase is an isogram
 // note that hyphens and spaces are allowed to appear multiple times.
 func IsIsogram(input string) bool {
-	exist := make(map[rune]struct{})
-	for _, ch := range sanitizeInput(input) {
-		upper := unicode.ToUpper(ch)
-		_, ok := exist[upper]
-		if ok {
+	// overwrite input with normalized version (sanitized & ToUpper)
+	// since there is no need refering to original string after this.
+	input = strings.ToUpper(sanitizeInput(input))
+	for i, ch := range input {
+		if strings.Contains(input[:i], string(ch)) {
 			return false
 		}
-		exist[upper] = struct{}{}
 	}
 	return true
 }
