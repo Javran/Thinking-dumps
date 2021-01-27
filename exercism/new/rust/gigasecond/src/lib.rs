7f7dc1ab2d65eb598b72059a255778807f8eb960
Javran Cheng <Javran.C@gmail.com>
2021-01-26 22:05:03 -0800

    gigasecond.
    turns out DateTime does support Add with a Duration.

--- exercism/new/rust/gigasecond/src/lib.rs
+++ exercism/new/rust/gigasecond/src/lib.rs
@@ -1,6 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!("What time is a gigasecond later than {}", start);
+    start + Duration::seconds(1_000_000_000)
 }
