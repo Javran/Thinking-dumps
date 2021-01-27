0c638b36e9def4c8c2b3b7c6bd75067077ca0a12
Javran Cheng <Javran.C@gmail.com>
2021-01-26 21:47:49 -0800

    gigasecond (placeholder)

---
+++ exercism/new/rust/gigasecond/src/lib.rs
@@ -0,0 +1,6 @@
+use chrono::{DateTime, Utc};
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    unimplemented!("What time is a gigasecond later than {}", start);
+}
