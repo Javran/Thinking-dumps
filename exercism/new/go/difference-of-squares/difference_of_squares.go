998603903d94b4d2ed02bc2b9230d623da9e9d9d
Javran Cheng <Javran.C@gmail.com>
2021-01-27 23:35:12 -0800

    remove fast flag.

--- exercism/new/go/difference-of-squares/difference_of_squares.go
+++ exercism/new/go/difference-of-squares/difference_of_squares.go
@@ -4,43 +4,21 @@
 // while the naive version doesn't)
 package diffsquares
 
-// fast determines which implementation to use.
-var fast bool = true
-
 // SquareOfSum computes (1 + 2 + ... + n)^2 given n.
 func SquareOfSum(n int) int {
-	var sum int
-	if fast {
-		sum = n * (n + 1) / 2
-	} else {
-		for i := 1; i <= n; i++ {
-			sum += i
-		}
-	}
+	sum := n * (n + 1) / 2
 	return sum * sum
 }
 
 // SumOfSquares computes 1^2 + 2^2 + ... + n^2 given n.
 func SumOfSquares(n int) int {
-	if fast {
-		// http://oeis.org/A000330
-		return n * (n + 1) * (n + n + 1) / 6
-	}
-
-	result := 0
-	for i := 1; i <= n; i++ {
-		result += i * i
-	}
-	return result
+	// http://oeis.org/A000330
+	return n * (n + 1) * (n + n + 1) / 6
 }
 
 // Difference computes the difference between SquareOfSum(n)
 // and SumOfSquares(n) given n.
 func Difference(n int) int {
-	if fast {
-		// http://oeis.org/A052149
-		return n * (n - 1) * (n + 1) * (3*n + 2) / 12
-	}
-
-	return SquareOfSum(n) - SumOfSquares(n)
+	// http://oeis.org/A052149
+	return n * (n - 1) * (n + 1) * (3*n + 2) / 12
 }
