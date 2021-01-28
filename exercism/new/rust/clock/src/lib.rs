e39a48bbae32fd0746767b58cb158181728274a6
Javran Cheng <Javran.C@gmail.com>
2021-01-27 18:47:03 -0800

    clock - version for submission.

--- exercism/new/rust/clock/src/lib.rs
+++ exercism/new/rust/clock/src/lib.rs
@@ -1,40 +1,45 @@
+use divrem::DivRem;
 use std::fmt;
+
+const MINS_PER_DAY: i32 = 24 * 60;
 
 #[derive(PartialEq, Debug)]
 pub struct Clock {
-    minute: u32,
+    // INVARIANT: 0 <= minute && minute < MINS_PER_DAY
+    minute: i32,
 }
 
-fn normalize(val: i32, divisor: u32) -> u32 {
-    let result = val % (divisor as i32);
+/// Performs division and wraps the result `r` so that `0 <= r && r < divisor`.
+///
+/// It is required that both `divisor` and `-divisor` are representable in `i32`,
+/// but this condition is unchecked.
+fn div_nonneg(val: i32, divisor: i32) -> i32 {
+    let result: i32 = val % divisor;
     if result < 0 {
-        ((divisor as i32) + result) as u32
+        divisor + result
     } else {
-        result as u32
+        result
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let hh = normalize(hours, 24);
-        let mm = normalize(minutes, 24 * 60);
+        let hh = div_nonneg(hours, 24);
+        let mm = div_nonneg(minutes, MINS_PER_DAY);
+        // INVARIANT: hh >= 0 && hh < 24 && mm >= 0 && mm < MINS_PER_DAY
         Clock {
-            minute: (hh * 60 + mm) % (24 * 60),
+            minute: (hh * 60 + mm) % MINS_PER_DAY,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mm_add = normalize(minutes, 24 * 60);
-        Clock {
-            minute: (self.minute + mm_add) % (24 * 60),
-        }
+        Clock::new(0, self.minute + div_nonneg(minutes, MINS_PER_DAY))
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mm = self.minute % 60;
-        let hh = self.minute / 60;
+        let (hh, mm) = self.minute.div_rem(60);
         write!(f, "{:02}:{:02}", hh, mm)
     }
 }
