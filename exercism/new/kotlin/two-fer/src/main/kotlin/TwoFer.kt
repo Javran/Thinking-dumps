63e815e4e503a978a2edfda1f172175d343e2097
Javran Cheng <Javran.C@gmail.com>
2021-01-31 10:50:48 -0800

    simplify.

--- exercism/new/kotlin/two-fer/src/main/kotlin/TwoFer.kt
+++ exercism/new/kotlin/two-fer/src/main/kotlin/TwoFer.kt
@@ -1,6 +1,3 @@
-internal fun twofer(): String =
-    "One for you, one for me."
-
-internal fun twofer(name: String): String =
+internal fun twofer(name: String = "you"): String =
     "One for $name, one for me."
 
