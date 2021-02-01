e4604b0429ece3847a06fce45f9c244732d207f4
Javran Cheng <Javran.C@gmail.com>
2021-01-31 22:18:03 -0800

    use yield because fancy.

--- exercism/new/kotlin/beer-song/src/main/kotlin/BeerSong.kt
+++ exercism/new/kotlin/beer-song/src/main/kotlin/BeerSong.kt
@@ -1,26 +1,27 @@
 object BeerSong {
-    fun verses(startBottles: Int, takeDown: Int): String {
-        val xs: MutableList<String> = mutableListOf()
-        for (i in startBottles downTo takeDown) {
-            xs.add(
-                when (i) {
-                    0 -> "No more bottles of beer on the wall, no more bottles of beer."
-                    1 -> "1 bottle of beer on the wall, 1 bottle of beer."
-                    else -> "$i bottles of beer on the wall, $i bottles of beer."
-                }
-            )
-            xs.add(
-                when (val j = i - 1) {
-                    -1 -> "Go to the store and buy some more, 99 bottles of beer on the wall."
-                    0 -> "Take it down and pass it around, no more bottles of beer on the wall."
-                    1 -> "Take one down and pass it around, 1 bottle of beer on the wall."
-                    else -> "Take one down and pass it around, $j bottles of beer on the wall."
-                }
-            )
-            if (i != takeDown) {
-                xs.add("")
+    private fun verse(n: Int, notLast: Boolean): Sequence<String> = sequence {
+        yield(
+            when (n) {
+                0 -> "No more bottles of beer on the wall, no more bottles of beer."
+                1 -> "1 bottle of beer on the wall, 1 bottle of beer."
+                else -> "$n bottles of beer on the wall, $n bottles of beer."
             }
+        )
+        yield(
+            when (val r = n - 1) {
+                -1 -> "Go to the store and buy some more, 99 bottles of beer on the wall."
+                0 -> "Take it down and pass it around, no more bottles of beer on the wall."
+                1 -> "Take one down and pass it around, 1 bottle of beer on the wall."
+                else -> "Take one down and pass it around, $r bottles of beer on the wall."
+            }
+        )
+        if (notLast) {
+            yield("")
         }
-        return xs.joinToString("\n", postfix = "\n")
     }
+
+    fun verses(startBottles: Int, takeDown: Int): String =
+        (startBottles downTo takeDown)
+            .flatMap { i -> verse(i, i != takeDown).toList() }
+            .joinToString("\n", postfix = "\n")
 }
