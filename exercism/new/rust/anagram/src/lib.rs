0e0b319c6b6cf2a688c4b72b8f6c7f3e1bc67557
Javran Cheng <Javran.C@gmail.com>
2021-02-02 23:55:36 -0800

    now a working version.

--- exercism/new/rust/anagram/src/lib.rs
+++ exercism/new/rust/anagram/src/lib.rs
@@ -3,16 +3,18 @@ use std::collections::HashSet;
 fn normalize(word: &str) -> Vec<char> {
     let mut word_norm: Vec<char> = word.chars().collect();
     word_norm.sort_unstable();
-    return word_norm;
+    word_norm
 }
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let word_norm: Vec<char> = normalize(word);
+    let word_lower = word.to_lowercase();
+    let word_norm: Vec<char> = normalize(&word_lower);
     let mut result = HashSet::new();
     for cur_word in possible_anagrams {
-        if normalize(cur_word) == word_norm && word != *cur_word {
+        let cur_word_lower = cur_word.to_lowercase();
+        if normalize(&cur_word_lower) == word_norm && cur_word_lower != word_lower {
             result.insert(*cur_word);
         }
     }
-    return result;
+    result
 }
