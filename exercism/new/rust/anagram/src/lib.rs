c3e22b29d003fdbe48a68aa9caff6b2d22342f35
Javran Cheng <Javran.C@gmail.com>
2021-02-03 23:05:10 -0800

    anagram - WIP.

--- exercism/new/rust/anagram/src/lib.rs
+++ exercism/new/rust/anagram/src/lib.rs
@@ -11,15 +11,15 @@ fn normalize(word: &str) -> (String, Vec<char>) {
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let (word_lower, word_norm) = normalize(&word);
-    let mut result = HashSet::new();
-    for cur_word in possible_anagrams {
-        let (cur_word_lower, cur_word_norm) = normalize(&cur_word);
-        // The goal is to find words that:
-        // - has same bag of chars, achieved by sorting by chars.
-        // - but not the same sequence of values, achieved by comparing them over lowered strings.
-        if cur_word_norm == word_norm && cur_word_lower != word_lower {
-            result.insert(*cur_word);
-        }
-    }
-    result
+    // TODO: have trouble compiling.
+    possible_anagrams
+        .iter()
+        .filter(|cur_word| {
+            let (cur_word_lower, cur_word_norm) = normalize(cur_word);
+            // The goal is to find words that:
+            // - has same bag of chars, achieved by sorting by chars.
+            // - but not the same sequence of values, achieved by comparing them over lowered strings.
+            cur_word_norm == word_norm && cur_word_lower != word_lower
+        })
+        .collect()
 }
