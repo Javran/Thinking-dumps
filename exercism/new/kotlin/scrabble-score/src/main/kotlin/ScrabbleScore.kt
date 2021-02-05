0d505225404ff8f783d445723dc917e2fc5dec6c
Javran Cheng <Javran.C@gmail.com>
2021-02-04 19:25:59 -0800

    use scope function.

--- exercism/new/kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
+++ exercism/new/kotlin/scrabble-score/src/main/kotlin/ScrabbleScore.kt
@@ -1,18 +1,23 @@
 object ScrabbleScore {
-    private val scoreMap: Map<Char, Int> = listOf(
-        "AEIOULNRST" to 1,
-        "DG" to 2,
-        "BCMP" to 3,
-        "FHVWY" to 4,
-        "K" to 5,
-        "JX" to 8,
-        "QZ" to 10
-    ).flatMap { (ks, score) ->
-        ks.flatMap { k ->
-            // cover both uppercase and lowercase.
-            listOf(k to score, k.toLowerCase() to score)
+    private val scoreMap: Map<Char, Int> = run {
+        val m: MutableMap<Char, Int> = mutableMapOf()
+        for ((ks, score) in listOf(
+            "AEIOULNRST" to 1,
+            "DG" to 2,
+            "BCMP" to 3,
+            "FHVWY" to 4,
+            "K" to 5,
+            "JX" to 8,
+            "QZ" to 10
+        )) {
+            for (k in ks) {
+                // cover both uppercase and lowercase.
+                m[k] = score
+                m[k.toLowerCase()] = score
+            }
         }
-    }.toMap()
+        m.toMap()
+    }
 
     private fun scoreLetter(c: Char): Int = scoreMap[c] ?: 0
 
