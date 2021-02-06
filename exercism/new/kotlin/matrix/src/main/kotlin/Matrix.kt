9dcda206bad78ca47e7d7a3af3c3fb5eef4752a5
Javran Cheng <Javran.C@gmail.com>
2021-02-06 14:18:06 -0800

    simplify.

--- exercism/new/kotlin/matrix/src/main/kotlin/Matrix.kt
+++ exercism/new/kotlin/matrix/src/main/kotlin/Matrix.kt
@@ -1,16 +1,17 @@
 class Matrix(private val matrixAsString: String) {
 
     // Parsed matrix in row-major order.
-    private val mat: Array<IntArray> =
+    private val mat: List<List<Int>> =
         matrixAsString
-            .split("\n")
+            .lines()
             .map { rawLine ->
-                rawLine.trim().split(Regex("""\s+""")).map {
-                    it.toInt(10)
-                }.toIntArray()
-            }.toTypedArray()
+                Regex("""(\d+)""")
+                    .findAll(rawLine)
+                    .map { it.groupValues[1].toInt(10) }
+                    .toList()
+            }
 
     fun column(colNr: Int): List<Int> = mat.map { it[colNr - 1] }
 
-    fun row(rowNr: Int): List<Int> = mat[rowNr - 1].toList()
+    fun row(rowNr: Int): List<Int> = mat[rowNr - 1]
 }
