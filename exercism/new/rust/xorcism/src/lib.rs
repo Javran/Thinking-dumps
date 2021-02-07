570a7b3e283a2b66f4633b2753fc8e4d56107a65
Javran Cheng <Javran.C@gmail.com>
2021-02-07 02:18:36 -0800

    xorcism: got some stuff to work...

--- exercism/new/rust/xorcism/src/lib.rs
+++ exercism/new/rust/xorcism/src/lib.rs
@@ -1,22 +1,22 @@
 /// A munger which XORs a key with some data
 #[derive(Clone)]
 pub struct Xorcism<'a> {
-    key : &'a[u8],
+    key: &'a [u8],
     pos: usize,
 }
 
 impl<'a> Xorcism<'a> {
-    // Note: I don't know why new needs to be parametrized over <Key> while
-    // not giving any constraint or instructions about what Key is supposed to be.
-    // so instead I changed that to just [u8]
-
     /// Create a new Xorcism munger from a key
     ///
     /// Should accept anything which has a cheap conversion to a byte slice.
-    pub fn new<Key: IntoIterator<Item=u8>>(key: &Key) -> Xorcism<'a> {
-        let xs = key.into_iter().collect::<Vec<u8>>().as_slice();
-        // Xorcism{key: ), pos: 0}
-        unimplemented!();
+    pub fn new<Key>(key: &'a Key) -> Xorcism<'a>
+    where
+        Key: AsRef<[u8]> + ?Sized,
+    {
+        Xorcism {
+            key: key.as_ref(),
+            pos: 0,
+        }
     }
 
     fn next(&mut self) -> u8 {
@@ -33,8 +33,8 @@ impl<'a> Xorcism<'a> {
     /// Note that this is stateful: repeated calls are likely to produce different results,
     /// even with identical inputs.
     pub fn munge_in_place(&mut self, data: &mut [u8]) {
-        for i in 0..data.len() {
-            data[i] ^= self.next()
+        for x in data.iter_mut() {
+            *x ^= self.next()
         }
     }
 
