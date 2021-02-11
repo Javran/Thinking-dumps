cd859bebd3d5ee643a0d6b50a2a847df4ad01230
Javran Cheng <Javran.C@gmail.com>
2021-02-10 20:13:01 -0800

    some plumbering work to get it to compile.

--- exercism/new/rust/dot-dsl/src/lib.rs
+++ exercism/new/rust/dot-dsl/src/lib.rs
@@ -1,36 +1,84 @@
-
 pub mod graph {
-    use std::collections::HashSet;
-
+    use std::collections::HashMap;
     // What is the purpose of having this unnecessarily nested module structure???
-    use graph_items::node::Node;
     use graph_items::edge::Edge;
+    use graph_items::node::Node;
 
-    pub struct Graph<'a> {
-        pub nodes: HashSet<Node<'a>>,
+    pub struct Graph {
+        pub nodes: Vec<Node>,
         pub edges: Vec<Edge>,
-        pub attrs: Vec<u32>,
+        pub attrs: HashMap<String, String>,
     }
 
-    impl <'a> Graph<'a> {
+    impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: vec![],
+                edges: vec![],
+                attrs: HashMap::new(),
+            }
+        }
+
+        pub fn with_nodes<T>(&mut self, _nodes: T) -> Self {
+            unimplemented!()
+        }
+
+        pub fn with_edges<T>(&mut self, _edges: T) -> Self {
+            unimplemented!()
+        }
+
+        pub fn with_attrs<T>(&mut self, _attrs: T) -> Self {
+            unimplemented!()
+        }
+
+        pub fn get_node<T>(self, _node: T) -> Option<Node> {
+            unimplemented!()
         }
     }
 
     pub mod graph_items {
         pub mod edge {
-            pub struct Edge;
+            #[derive(PartialEq, Debug)]
+            pub struct Edge {
+                pub src: String,
+                pub dst: String,
+            }
+
+            impl Edge {
+                pub fn new(src: &str, dst: &str) -> Self {
+                    Edge {
+                        src: src.to_string(),
+                        dst: dst.to_string(),
+                    }
+                }
+
+                pub fn with_attrs<T>(&mut self, _attrs: T) -> Self {
+                    unimplemented!()
+                }
+            }
         }
 
         pub mod node {
-            pub struct Node<'a> {
-                pub name: &'a str,
+            #[derive(PartialEq, Debug, Clone)]
+            pub struct Node {
+                pub name: String,
+                pub attrs: Vec<(String, String)>,
             }
 
-            impl <'a> Node <'a> {
-                pub fn new(name: &'a str) -> Self {
-                    Node{name}
+            impl Node {
+                pub fn new(name: &str) -> Self {
+                    Node {
+                        name: name.to_string(),
+                        attrs: vec![],
+                    }
+                }
+
+                pub fn with_attrs<T>(&mut self, _attrs: T) -> Self {
+                    unimplemented!()
+                }
+
+                pub fn get_attr<T>(&self, _key: T) -> Option<&str> {
+                    unimplemented!()
                 }
             }
         }
