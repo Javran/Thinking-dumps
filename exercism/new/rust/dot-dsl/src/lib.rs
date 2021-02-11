7f4102d80fe296089c2e9f491c34239bec96e958
Javran Cheng <Javran.C@gmail.com>
2021-02-10 20:42:19 -0800

    node with attrs.

--- exercism/new/rust/dot-dsl/src/lib.rs
+++ exercism/new/rust/dot-dsl/src/lib.rs
@@ -60,22 +60,28 @@ pub mod graph {
         }
 
         pub mod node {
+            use std::collections::HashMap;
+
             #[derive(PartialEq, Debug, Clone)]
             pub struct Node {
                 pub name: String,
-                pub attrs: Vec<(String, String)>,
+                pub attrs: HashMap<String, String>,
             }
 
             impl Node {
                 pub fn new(name: &str) -> Self {
                     Node {
                         name: name.to_string(),
-                        attrs: vec![],
+                        attrs: HashMap::new(),
                     }
                 }
 
-                pub fn with_attrs<T>(&mut self, _attrs: T) -> Self {
-                    unimplemented!()
+                pub fn with_attrs(mut self, attrs: &[(&str, &str)]) -> Self {
+                    self.attrs = attrs
+                        .iter()
+                        .map(|(k, v)| (k.to_string(), v.to_string()))
+                        .collect();
+                    self
                 }
 
                 pub fn get_attr<T>(&self, _key: T) -> Option<&str> {
--- exercism/new/rust/dot-dsl/tests/dot-dsl.rs
+++ exercism/new/rust/dot-dsl/tests/dot-dsl.rs
@@ -28,7 +28,6 @@ fn test_graph_with_one_node() {
 }
 
 #[test]
-#[ignore]
 fn test_graph_with_one_node_with_keywords() {
     let nodes = vec![Node::new("a").with_attrs(&[("color", "green")])];
 
