18807a4df01927e201dc1df7487d521e08f245c2
Javran Cheng <Javran.C@gmail.com>
2021-02-12 16:55:21 -0800

    let's start migrating old stuff.

---
+++ exercism/new/haskell/hello-world/test/Tests.hs
@@ -0,0 +1,13 @@
+import HelloWorld (hello)
+import Test.Hspec (Spec, it, shouldBe)
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
+main :: IO ()
+main = hspecWith defaultConfig {configFastFail = True} specs
+
+specs :: Spec
+specs =
+  it "hello" $
+    hello `shouldBe` "Hello, World!"
+
+-- 5c542864b011fb742aa95ca950d3473ce168ec8c
---
+++ exercism/new/haskell/pangram/src/Pangram.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Pangram
+  ( isPangram
+  )
+where
+
+import Data.Char
+import qualified Data.Set as S
+
+alphabet :: S.Set Char
+alphabet = S.fromDistinctAscList ['A' .. 'Z']
+
+isPangram :: String -> Bool
+isPangram = notNull . dropWhile notNull . scanl go alphabet
+  where
+    notNull = not . null
+    go missingChars (toUpper -> ch) =
+      S.delete ch missingChars
