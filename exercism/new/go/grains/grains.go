35d23b8ad586e62dfec85d669294d1b358d0f3b2
Javran Cheng <Javran.C@gmail.com>
2021-02-14 13:50:44 -0800

    literal.

--- exercism/new/go/grains/grains.go
+++ exercism/new/go/grains/grains.go
@@ -7,7 +7,7 @@ import (
 // Total returns the total number of grains on the chessboard.
 func Total() uint64 {
 	// 2^0 + 2^1 + ... + 2^63 = 2^64 - 1
-	return 0xFFFF_FFFF_FFFF_FFFF
+	return 1<<64 - 1
 }
 
 // Square returns the number of grains on a given square (1-based).
