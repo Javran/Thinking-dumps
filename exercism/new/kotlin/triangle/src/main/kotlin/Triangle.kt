58e90e822b87cd4655cb4a8f1612a64a219e1e41
Javran Cheng <Javran.C@gmail.com>
2021-02-14 13:50:53 -0800

    simplify sort.

--- exercism/new/kotlin/triangle/src/main/kotlin/Triangle.kt
+++ exercism/new/kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,44 +1,21 @@
 // Note that the problem template uses <out T> but I changed it to <T>
 // because my solution does require consumption of the type for comparing and arithmetic operations.
-class Triangle<T>(private val a0: T, private val b0: T, private val c0: T)
+class Triangle<T>(private val t: Triple<T, T, T>)
         where T : Comparable<T>,
               T : Number {
-    // INVARIANT: a <= b <= c, where a, b, c are the same as a0, b0, c0 up to permutation.
-    private val a: T
-    private val b: T
-    private val c: T
+    // INVARIANT: t.a <= t.b <= t.c, where a, b, c are the same as a0, b0, c0 up to permutation.
+    constructor(a0: T, b0: T, c0: T) : this(
+        listOf(a0, b0, c0)
+            .sorted().let { (a, b, c) -> Triple(a, b, c) }
+    )
 
     val isEquilateral: Boolean
     val isIsosceles: Boolean
     val isScalene: Boolean
 
     init {
-        fun sort(x: T, y: T) =
-            if (x <= y) {
-                x to y
-            } else {
-                y to x
-            }
-
-        /*
-          Sorting network:
-          a0 ----> a1 ----= a1 ----> a2
-              x                 x
-          b0 ----> b1 ----> b2 ----> b3
-                       x
-          c0 ----= c0 ----> c1 ----= c1
-
-          "x" marks where a sorter is applied
-         */
-        val (a1, b1) = sort(a0, b0)
-        val (b2, c1) = sort(b1, c0)
-        val (a2, b3) = sort(a1, b2)
-
-        a = a2
-        b = b3
-        c = c1
-
-        if (c == 0) {
+        val (a, b, c) = t
+        if (a == 0) {
             throw IllegalArgumentException("All sides are non-positive.")
         }
         // No good solution here to invoke type-specific arithmetic methods:
