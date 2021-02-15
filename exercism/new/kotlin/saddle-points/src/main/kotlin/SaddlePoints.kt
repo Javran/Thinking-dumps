f3e8894c212ab201aec9690166a0bcd44e8817c8
Javran Cheng <Javran.C@gmail.com>
2021-02-14 23:00:19 -0800

    saddle points.

---
+++ exercism/new/kotlin/saddle-points/src/main/kotlin/SaddlePoints.kt
@@ -0,0 +1,24 @@
+data class MatrixCoordinate(val row: Int, val col: Int)
+
+class Matrix(mat: List<List<Int>>) {
+
+    val saddlePoints: Set<MatrixCoordinate> = run {
+        val rows = mat.size
+        if (rows == 0) {
+            return@run emptySet()
+        }
+        val cols = mat.first().size
+        val rowMaxs: List<Int> = mat.map { it.max() ?: Int.MIN_VALUE }
+        val colMins: List<Int> = (0 until cols).map { c -> mat.map { it[c] }.min() ?: Int.MAX_VALUE }
+
+        mat.mapIndexed { r, row ->
+            row.mapIndexed { c, v ->
+                if (v == rowMaxs[r] && v == colMins[c]) {
+                    listOf(MatrixCoordinate(r + 1, c + 1))
+                } else {
+                    emptyList()
+                }
+            }.flatten()
+        }.flatten().toSet()
+    }
+}
