5e4eb19074827ecab31b84bdf5f6f3c9eb459691
Javran Cheng <Javran.C@gmail.com>
2021-02-16 12:16:32 -0800

    keep it simple .. sort of.

--- exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ exercism/haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE MonadComprehensions #-}
 module CollatzConjecture (collatz) where
 
+import Control.Monad
 import Data.List
 
 collatz :: Integer -> Maybe Integer
-collatz i = [genericLength . takeWhile (/= 1) . iterate next $ i | i > 0]
+collatz i = do
+  guard $ i > 0
+  pure . genericLength . takeWhile (/= 1) . iterate next $ i
   where
     next x = if even x then x `quot` 2 else x * 3 + 1
