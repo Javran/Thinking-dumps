fe54f78bdbab95e450c19d2cfec1def2d4d58cc9
Javran Cheng <Javran.C@gmail.com>
2021-02-16 12:16:25 -0800

    migration.

--- exercism/haskell/triangle/src/Triangle.hs
+++ exercism/haskell/triangle/src/Triangle.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Triangle
-  ( TriangleType(..)
+  ( TriangleType (..)
   , triangleType
   )
 where
@@ -8,38 +10,41 @@ data TriangleType
   = Equilateral
   | Isosceles
   | Scalene
-  | Illogical
-    deriving (Eq, Show)
+  | Illegal
+  deriving (Eq, Show)
 
--- invariant: (a,b,c) = triSort _ implies a <= b <= c
---   and the output is a permutation of input
-triSort :: (Int,Int,Int) -> (Int,Int,Int)
-triSort (a,b,c)
-  | a <= b && a <= c = if b <= c then (a,b,c) else (a,c,b)
-  | b <= a && b <= c = if a <= c then (b,a,c) else (b,c,a)
-  | c <= a && c <= b = if a <= b then (c,a,b) else (c,b,a)
-  | otherwise = error "impossible"
-
-triangleType :: Int -> Int -> Int -> TriangleType
-triangleType a b c =
-    -- a triangle must meet the following condition
-    -- * y < z + x (implied)
-    -- * z < x + y (to be verified)
-    -- * x < z + y (implied)
-    if z < x + y && x > 0
-      then if x == z
-              -- x == y == z (implied)
-              then Equilateral
-              else if x == y || y == z
-                     -- by definition
-                     then Isosceles
-                     else Scalene
-      else Illogical
+{-
+  INVARIANT: (a,b,c) = triSort _ implies a <= b <= c
+  and the output is a permutation of input
+ -}
+triSort :: Ord a => (a, a, a) -> (a, a, a)
+triSort (a, b, c) = (a2, b3, c1)
   where
-    -- invariant: x <= y <= z, assume x > 0
-    -- therefore:
-    -- * x <= z
-    -- * x < z + y
-    -- * y <= z
-    -- * y < z + x
-    (x,y,z) = triSort (a,b,c)
+    minMax x y = if x <= y then (x, y) else (y, x)
+    {-
+      Sorting network:
+      a ----> a1 ----= a1 ----> a2
+          x                 x
+      b ----> b1 ----> b2 ----> b3
+                   x
+      c ----= c  ----> c1 ----= c1
+     -}
+    (a1, b1) = minMax a b
+    (b2, c1) = minMax b1 c
+    (a2, b3) = minMax a1 b2
+
+triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
+triangleType a b c =
+  {-
+    a triangle must meet the following condition:
+    - y < z + x (implied)
+    - z < x + y (to be verified)
+    - x < z + y (implied)
+   -}
+  if
+      | x <= 0 || z >= x + y -> Illegal
+      | x == z -> Equilateral
+      | x == y || y == z -> Isosceles
+      | otherwise -> Scalene
+  where
+    (x, y, z) = triSort (a, b, c)
