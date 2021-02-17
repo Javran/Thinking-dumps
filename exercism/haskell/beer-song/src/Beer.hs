6933ba2b983a2a654a49087f03698bf838b16bbf
Javran Cheng <Javran.C@gmail.com>
2021-02-17 03:04:16 -0800

    something that works.

--- exercism/haskell/beer-song/src/Beer.hs
+++ exercism/haskell/beer-song/src/Beer.hs
@@ -1,62 +1,71 @@
 module Beer
-    ( verse
-    , sing
-    )
+  ( verse
+  , sing
+  , song
+  )
 where
 
-import Text.Printf
 import Data.Char
 import Data.Function
+import Text.Printf
+
+-- TODO: looks awful, will go back after migration is done.
+
+song :: String
+song = sing 99 0
 
 sing :: Int -> Int -> String
-sing x y = (unlines . map verse) [x,x-1..y]
+sing x y = init $ (unlines . map verse) [x, x -1 .. y]
 
 verse :: Int -> String
-verse x = unlines [ firstLine x
-                  , secondLine x
-                  ]
+verse x =
+  unlines
+    [ firstLine x
+    , secondLine x
+    ]
 
 -- | for sake of readability, should be invisible outside the module
 _onTheWall, _end :: Bool
-_onTheWall       = True
-_end             = False
+_onTheWall = True
+_end = False
 
 -- | construct "<n> bottle(s) of beer (on the wall)"
 xBottleOfBeer :: Int -> Bool -> String
-n `xBottleOfBeer` otw = unwords [nStr, bottles,"of beer" , otwS]
-    where
-        nStr =    if n == 0 then "no more" else show n
-        bottles = if n == 1 then "bottle"  else "bottles"
-        otwS =    if otw then "on the wall" else ""
+n `xBottleOfBeer` otw = unwords [nStr, bottles, "of beer", otwS]
+  where
+    nStr = if n == 0 then "no more" else show n
+    bottles = if n == 1 then "bottle" else "bottles"
+    otwS = if otw then "on the wall" else ""
 
 {-# ANN makeSentence "HLint: ignore Eta reduce" #-}
+
 -- | construct a sentence from two parts
 makeSentence :: String -> String -> String
 makeSentence (fstH : fstT) sndL =
-    (printf "%s, %s." `on` stripR) fstL' sndL
-    -- ^ create the sentence on stripped arguments
-    where
-        fstL' = toUpper fstH : fstT
-        -- ^ take care of the first char in sentences
-        stripR = reverse . dropWhile isSpace . reverse
-        -- ^ might have tailing zeros, remove it.
+  -- create the sentence on stripped arguments
+  (printf "%s, %s." `on` stripR) fstL' sndL
+  where
+    -- take care of the first char in sentences
+    fstL' = toUpper fstH : fstT
+    -- might have tailing zeros, remove it.
+    stripR = reverse . dropWhile isSpace . reverse
 
 firstLine :: Int -> String
 firstLine x =
-    makeSentence
-        (x `xBottleOfBeer` _onTheWall)
-        (x `xBottleOfBeer` _end)
+  makeSentence
+    (x `xBottleOfBeer` _onTheWall)
+    (x `xBottleOfBeer` _end)
 
 secondLine :: Int -> String
 secondLine 0 =
-    makeSentence
-        "Go to the store and buy some more"
-        (99 `xBottleOfBeer` _onTheWall)
+  makeSentence
+    "Go to the store and buy some more"
+    (99 `xBottleOfBeer` _onTheWall)
 secondLine 1 =
-    makeSentence
-        "Take it down and pass it around"
-        (0 `xBottleOfBeer` _onTheWall)
+  makeSentence
+    "Take it down and pass it around"
+    (0 `xBottleOfBeer` _onTheWall)
 secondLine x =
-    makeSentence
-        "Take one down and pass it around"
-        ((x-1) `xBottleOfBeer` _onTheWall)
+  makeSentence
+    "Take one down and pass it around"
+    ((x -1) `xBottleOfBeer` _onTheWall)
