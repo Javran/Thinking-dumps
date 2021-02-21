36a9e44049746d83b649262a4be64d2af73d4dcb
Javran Cheng <Javran.C@gmail.com>
2021-02-21 15:26:43 -0800

    dnd.

---
+++ exercism/haskell/dnd-character/src/DND.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecursiveDo #-}
+module DND
+  ( Character (..)
+  , ability
+  , modifier
+  , character
+  )
+where
+
+import Control.Monad
+import Test.QuickCheck
+
+data Character = Character
+  { strength :: Int
+  , dexterity :: Int
+  , constitution :: Int
+  , intelligence :: Int
+  , wisdom :: Int
+  , charisma :: Int
+  , hitpoints :: Int
+  }
+  deriving (Show, Eq)
+
+modifier :: Int -> Int
+modifier = (`div` 2) . subtract 10
+
+ability :: Gen Int
+ability = do
+  xs <- replicateM 4 (choose (1, 6))
+  pure $ sum xs - minimum xs
+
+character :: Gen Character
+character = mdo
+  ch@Character {constitution} <-
+    Character
+      <$> ability
+      <*> ability
+      <*> ability
+      <*> ability
+      <*> ability
+      <*> ability
+      <*> pure (10 + modifier constitution)
+  pure ch
