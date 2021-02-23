0b8a56ff613fc346a7aee581fc967cfc1ac5207a
Javran Cheng <Javran.C@gmail.com>
2021-02-22 18:43:17 -0800

    test on just odd primes.

--- exercism/rust/nth-prime/src/lib.rs
+++ exercism/rust/nth-prime/src/lib.rs
@@ -1,10 +1,7 @@
 
-fn is_prime(n: u32) -> bool {
+fn is_odd_prime(n: u32) -> bool {
     if n <= 1 {
         return false
-    }
-    if n % 2 == 0 {
-        return n == 2
     }
     let up_bound = (n as f64).sqrt().ceil() as u32;
     for i in 3 ..= up_bound {
@@ -20,8 +17,8 @@ pub fn nth(n: u32) -> u32 {
         return 2
     }
     let mut count = 0;
-    for i in 3.. {
-        if is_prime(i) {
+    for i in (3..).step_by(2) {
+        if is_odd_prime(i) {
             count += 1;
         }
         if count == n {
