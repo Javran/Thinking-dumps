85e0be67054245dba0294812001d860ca5ae7360
Javran Cheng <Javran.C@gmail.com>
2021-02-22 18:06:48 -0800

    simplify this a bit
    indeed this is way too complicated.

--- exercism/haskell/leap/src/LeapYear.hs
+++ exercism/haskell/leap/src/LeapYear.hs
@@ -3,12 +3,8 @@ module LeapYear
   )
 where
 
--- | true if the given year is leap year
+-- | Tests whether a given year is a leap year.
 isLeapYear :: Int -> Bool
-isLeapYear n
-  | not (n `divisibleBy` 4) = False
-  -- every cond below satisfies "n `divisibleBy` 4"
-  | n `divisibleBy` 400 = True
-  | otherwise = not (n `divisibleBy` 100)
-  where
-    x `divisibleBy` y = x `mod` y == 0
+isLeapYear n =
+  (n `rem` 4 == 0)
+    && ((n `rem` 100 /= 0) || (n `rem` 400 == 0))
