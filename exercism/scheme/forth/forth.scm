93fe503a044fbe63c0dc5e1a7a56479e6f7271df
Javran Cheng <Javran.C@gmail.com>
2021-02-28 21:53:46 -0800

    further simplification.

--- exercism/scheme/forth/forth.scm
+++ exercism/scheme/forth/forth.scm
@@ -99,37 +99,32 @@
                  (parse tl))))]))
 
 (define (perform-action! state action)
-  (cond
-   [(procedure? action) (action state)]
-   [(forth-closure? action)
-    (let ([;; make a new forth-state for closure to be executed in isolated environment.
-           clo-state
-           (make-forth-state
-            (forth-state-stack state)
-            (forth-closure-env action))])
-      (interpret-all clo-state (forth-closure-body action))
-      ;; while it's possible the one might define words inside a word definition,
-      ;; in which case env in clo-state might be updated,
-      ;; but test suite doesn't specify what to do in that case.
-      ;; here I choose to simply ignore (potentially updated) env from clo-state.
-      (forth-state-stack-set! state (forth-state-stack clo-state)))]))
+  (match action
+         [(? procedure?) (action state)]
+         [;; I'm not exactly sure why there is a #f in the ignored position...
+          ($ forth-closure _ env body)
+            (let ([;; make a new forth-state for closure to be executed in isolated environment.
+                   clo-state
+                   (make-forth-state
+                    (forth-state-stack state)
+                    env)])
+              (interpret-all clo-state body)
+              ;; while it's possible the one might define words inside a word definition,
+              ;; in which case env in clo-state might be updated,
+              ;; but test suite doesn't specify what to do in that case.
+              ;; here I choose to simply ignore (potentially updated) env from clo-state.
+              (forth-state-stack-set! state (forth-state-stack clo-state)))]))
 
 (define (interpret state stmt)
-  (cond
-   [(number? stmt)
-    (forth-state-push! state stmt)]
-   [(symbol? stmt)
-    (perform-action!
-     state
-     (cdr (assoc stmt (forth-state-env state))))]
-   [(word-def? stmt)
-    (let* ([cur-env (forth-state-env state)]
-           [clo (make-forth-closure cur-env (word-def-body stmt))])
-      (forth-state-env-set!
-       state
-       (cons (cons (word-def-name stmt) clo)
-             cur-env)))]
-    [else (raise 'unknown-statement)]))
+  (match stmt
+         [(? number?) (forth-state-push! state stmt)]
+         [(? symbol?)
+          (perform-action! state (cdr (assoc stmt (forth-state-env state))))]
+         [($ word-def _ name body)
+          (let* ([cur-env (forth-state-env state)]
+                 [clo (make-forth-closure cur-env body)])
+            (forth-state-env-set! state (cons (cons name clo) cur-env)))]
+         [_ (raise 'unknown-statement)]))
 
 (define (interpret-all state stmts)
   (for-each (lambda (stmt) (interpret state stmt)) stmts))
