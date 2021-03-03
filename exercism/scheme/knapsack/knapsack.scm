fe51b333e68cf4bb9f04041601c866c2e97a268a
Javran Cheng <Javran.C@gmail.com>
2021-03-02 20:52:27 -0800

    change to dynamic programming.

--- exercism/scheme/knapsack/knapsack.scm
+++ exercism/scheme/knapsack/knapsack.scm
@@ -4,30 +4,34 @@
   (let ([ws (list->vector weights)]
         [vs (list->vector values)])
     (let* ([len (vector-length ws)]
-           [memo
+           [fv
             (let ([v (make-vector len)])
               (let fill ([i 0])
                (if (< i len)
                    (begin
-                     (vector-set! v i (make-vector (+ capacity 1) #f))
+                     (vector-set! v i (make-vector (+ capacity 1) 0))
                      (fill (+ i 1)))
                    v)))])
-      (define (best i weight)
-        ;; (best i weight) searches from 0th to i-th item and return the maximum value
-        ;; with an exact weight.
-        (if (or (< i 0) (<= weight 0))
-            0
-            (let ([cached (vector-ref (vector-ref memo i) weight)])
-              (or cached
-                  (let* ([item-weight (vector-ref ws i)]
-                         [item-value (vector-ref vs i)]
-                         [result
-                          (if (> item-weight weight)
-                              (best (- i 1) weight)
-                              (max
-                               (best (- i 1) weight)
-                               (+ (best (- i 1) (- weight item-weight)) item-value)))])
-                    (vector-set! (vector-ref memo i) weight result)
-                    result
-                    result)))))
-      (best (- len 1) capacity))))
+      (let loopi ([i 0])
+        (if (= i len)
+            'done
+            (let ([item-weight (vector-ref ws i)]
+                  [item-value (vector-ref vs i)])
+              (let loopw ([w 0])
+                (if (> w capacity)
+                    (loopi (+ i 1))
+                    (begin
+                      (if (> item-weight w)
+                          (vector-set! (vector-ref fv i) w (if (> i 0)
+                                                               (vector-ref (vector-ref fv (- i 1)) w) 0))
+                          (vector-set! (vector-ref fv i) w (if (> i 0)
+                                                               (max
+                                                                (vector-ref (vector-ref fv (- i 1)) w)
+                                                                (+ (vector-ref (vector-ref fv (- i 1)) (- w item-weight)) item-value))
+                                                               (max
+                                                                0
+                                                                item-value))))
+                      (loopw (+ w 1))))))))
+      (if (= len 0)
+          0
+          (vector-ref (vector-ref fv (- len 1)) capacity)))))
