019fd243eaf3821c96b5478809a755715416a7de
Javran Cheng <Javran.C@gmail.com>
2021-03-03 00:45:09 -0800

    fix bug on 1 and <= 0.

--- exercism/scheme/perfect-numbers/perfect-numbers.scm
+++ exercism/scheme/perfect-numbers/perfect-numbers.scm
@@ -1,14 +1,16 @@
 (import (rnrs))
 
 (define (classify n)
-  (let loop ([i 2]
-             [sum 1])
-    (if (= i n)
-        (cond
-         [(= sum n) 'perfect]
-         [(> sum n) 'abundant]
-         [else 'deficient])
-        (loop (+ i 1)
-              (if (= (remainder n i) 0)
-                  (+ sum i)
-                  sum)))))
+  (if (<= n 0)
+      (raise 'invalid)
+      (let loop ([i 1]
+                 [sum 0])
+        (if (= i n)
+            (cond
+             [(= sum n) 'perfect]
+             [(> sum n) 'abundant]
+             [else 'deficient])
+            (loop (+ i 1)
+                  (if (= (remainder n i) 0)
+                      (+ sum i)
+                      sum))))))
