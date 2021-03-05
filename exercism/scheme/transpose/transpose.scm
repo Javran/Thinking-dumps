e12b45580f6629e54cf63592ac7eba8db51a3ab4
Javran Cheng <Javran.C@gmail.com>
2021-03-04 22:12:59 -0800

    simplify transpose.

--- exercism/scheme/transpose/transpose.scm
+++ exercism/scheme/transpose/transpose.scm
@@ -1,17 +1,6 @@
 (import (rnrs))
 
-(use-modules
- ((srfi srfi-1)
-  #:select (every)))
-
-;; TODO: I suspect there's a way simpler solution.
-
 (define (transpose matrix)
-  (let loop ([mat matrix]
-             [result '()])
-        (if (every null? mat)
-            (reverse! result)
-            (let ([col (map car mat)]
-                  [remaining (map cdr (filter (lambda (x)
-                                                (not (null? x))) mat))])
-              (loop remaining (cons col result))))))
+  (if (null? matrix)
+      '()
+      (apply map (cons list matrix))))
