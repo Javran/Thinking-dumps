0900f29c84f1e936673a36d585600476b8afce7a
Javran Cheng <Javran.C@gmail.com>
2021-03-05 17:30:48 -0800

    factor out group-encoded
    somehow this arbitrarily encode-groupping logic appears to be useful
    in other exercises. I'd better factor this out in order to not deal
    with those random and irrational decisions again.

--- exercism/scheme/affine-cipher/affine-cipher.scm
+++ exercism/scheme/affine-cipher/affine-cipher.scm
@@ -43,6 +43,25 @@
         (let ([val (- (char->integer ch) base)])
           (integer->char (+ (modify val) base)))))))
 
+(define (group-encoded chars)
+  ;; takes a list of chars and turns it into a string of space-separated
+  ;; groups, in which each group consists of 5 characters.
+  (let loop ([result ""]
+             [xs chars])
+    (define (pack-new-item cs)
+      (if (null? cs)
+          result
+          ;; pack cs and generate a new result string, taking into account spaces.
+          (let ([packed (list->string cs)])
+            (if (string=? result "")
+                packed
+                (string-append result " " packed)))))
+    (if (< (length xs) 5)
+        (pack-new-item xs)
+        (call-with-values (lambda () (split-at xs 5))
+          (lambda (ys zs)
+            (loop (pack-new-item ys) zs))))))
+
 (define (encode key text)
   (match
    key
@@ -58,24 +77,7 @@
        #f
        ;; otherwise
        (lambda (_) #f)))
-    (define (group chars)
-      ;; implements 5 letter group size.
-      (let loop ([result ""]
-             [xs chars])
-        (define (pack-new-item cs)
-          (if (null? cs)
-              result
-              ;; pack cs and generate a new result string, taking into account spaces.
-              (let ([packed (list->string cs)])
-                (if (string=? result "")
-                    packed
-                    (string-append result " " packed)))))
-        (if (< (length xs) 5)
-            (pack-new-item xs)
-            (call-with-values (lambda () (split-at xs 5))
-              (lambda (ys zs)
-                (loop (pack-new-item ys) zs))))))
-    (group
+    (group-encoded
      (filter-map encode-char (string->list (string-downcase text))))]))
 
 (define (decode key text)
