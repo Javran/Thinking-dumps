d37f6bf3e44bdf2a6f4cdfb4d18d72e74162371a
Javran Cheng <Javran.C@gmail.com>
2021-03-06 14:30:42 -0800

    boring and useless stuff.

---
+++ exercism/scheme/bob/bob.scm
@@ -0,0 +1,18 @@
+(import (rnrs))
+
+(use-modules ((srfi srfi-13)))
+
+(define (response-for msg)
+  (let ([msg (string-trim-both msg)])
+    (if (string=? msg "")
+        "Fine. Be that way!"
+        (let ([allcap?
+               (and (string-index msg char-alphabetic?)
+                    (string=? msg (string-upcase msg)))]
+              [q?
+               (eq? (string-ref msg (- (string-length msg) 1)) #\?)])
+          (cond
+           [(and allcap? q?) "Calm down, I know what I'm doing!"]
+           [allcap? "Whoa, chill out!"]
+           [q? "Sure."]
+           [else "Whatever."])))))
