d7aba6cf05cf9094b4d58ce172be83399c75c808
Javran Cheng <Javran.C@gmail.com>
2021-03-07 17:14:55 -0800

    a bit improvement and some notes about the design.

--- exercism/kotlin/robot-name/src/main/kotlin/RobotName.kt
+++ exercism/kotlin/robot-name/src/main/kotlin/RobotName.kt
@@ -12,22 +12,27 @@ class Robot(private val id: Long) {
     fun reset() = invalidateRobotName(id)
 
     companion object {
-        private var rnd: Random = Random()
+        // A bit of design decision made here:
+        // While the straightforward approach could use a single Set (i.e. "Set approach" below) and insert all used
+        // named up robot reset, this solution addresses a potential issue:
+        // in Set approach there's no a single name returned to the pool when resetting a robot,
+        // meaning the name pool "drains" faster than it could have been and never replenish.
+        // Having `names` and `usedNames` in addition to the usual Set allows a name to be reused by any robot
+        // but those owned the same name before.
+
         // Since robot can be renamed, identity can only be establish by reference to object
         // or by maintaining an id pool.
         private var idPool: Long = 0
-        // Keeps track of names of all existing robots.
-        private var namePool: MutableSet<String> = mutableSetOf()
-        // Current robot id to name mapping.
-        private var names: MutableMap<Long, String> = mutableMapOf()
-        // After a robot is renamed, its old names can no longer be used (for that robot).
-        private var usedNames: MutableMap<Long, MutableSet<String>> = mutableMapOf()
 
-        fun nextId(): Long {
-            val id = idPool
-            ++idPool
-            return id
-        }
+        private val rnd: Random = Random()
+        // Keeps track of names of all existing robots.
+        private val namePool: MutableSet<String> = mutableSetOf()
+        // Current robot id to name mapping.
+        private val names: MutableMap<Long, String> = mutableMapOf()
+        // After a robot is renamed, its old names can no longer be used (for that robot).
+        private val usedNames: MutableMap<Long, MutableSet<String>> = mutableMapOf()
+
+        fun nextId(): Long = ++idPool
 
         private fun createNewName(id: Long): String {
             val robotUsedNames: MutableSet<String> = when (val m = usedNames[id]) {
