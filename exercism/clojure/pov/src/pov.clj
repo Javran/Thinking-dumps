6876da11034aa71dd3e32635d8c3df1dbf9bca52
Javran Cheng <Javran.C@gmail.com>
2021-03-13 16:30:46 -0800

    dfs to top level.

--- exercism/clojure/pov/src/pov.clj
+++ exercism/clojure/pov/src/pov.clj
@@ -56,35 +56,22 @@
                     [(-> x :focus first) x])
                   (build-zippers tree [])))
         zipper (hm node-tag nil)]
-    (if zipper
-      (let [{focus :focus
-             contexts :contexts} zipper]
-        (reconstruct-pov focus contexts))
-      nil)))
+    (and
+     zipper
+     (let [{focus :focus
+            contexts :contexts} zipper]
+       (reconstruct-pov focus contexts)))))
+
+(defn dfs [target tree path]
+  (let [[cur-tag & children] tree
+        new-path (conj path cur-tag)]
+    (if (= cur-tag target)
+      new-path
+      (reduce
+       (fn [acc t] (or acc (dfs target t new-path)))
+       nil
+       children))))
 
 (defn path-from-to [x y tree]
   (let [x-pov (of x tree)]
-    (defn dfs [cur-tree path]
-      (let [[cur-tag & children] cur-tree
-            new-path (conj path cur-tag)]
-        (if (= cur-tag y)
-          new-path
-          (reduce (fn [acc t] (or acc (dfs t new-path))) nil children))))
-    (dfs x-pov [])))
-
-(def e-tree
-  [:grand-parent
-   [:parent
-    [:x
-     [:child-1]
-     [:child-2]]
-    [:sibling
-     [:nephew]
-     [:niece]]]
-   [:aunt
-    [:cousin-1
-     [:2nd-cousin-1]
-     [:2nd-cousin-2]]
-    [:cousin-2
-     [:2nd-cousin-3]
-     [:2nd-cousin-4]]]])
+    (dfs y x-pov [])))
