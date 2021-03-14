ad2463edc33b0390cc6bc80c554bd0e67c35f392
Javran Cheng <Javran.C@gmail.com>
2021-03-13 16:29:54 -0800

    final version.

--- exercism/clojure/reverse-string/src/reverse_string.clj
+++ exercism/clojure/reverse-string/src/reverse_string.clj
@@ -1,8 +1,4 @@
 (ns reverse-string)
 
-(defn reverse-string [s]
-  (comp
-   (partial apply str)
-   #(% [])
-   ;; reverse by constructing diff-list
-   (partial reduce (fn [acc i] (comp acc #(conj % i))) identity)))
+(defn reverse-string [n]
+  (apply str (into () n)))
