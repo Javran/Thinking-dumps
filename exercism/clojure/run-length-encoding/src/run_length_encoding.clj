89beae9efebdac025720a5f7537f25648bbb715e
Javran Cheng <Javran.C@gmail.com>
2021-03-25 15:23:28 -0700

    don't convert to str every step.

--- exercism/clojure/run-length-encoding/src/run_length_encoding.clj
+++ exercism/clojure/run-length-encoding/src/run_length_encoding.clj
@@ -20,19 +20,19 @@
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  ((reduce
-    comp
-    identity
-    (map
-     (fn [xs]
-       (if (char-numeric? (first xs))
-         (let [cnt (Integer/parseInt (apply str xs))]
-           (fn [s]
-             (let [[hd & tl] (map identity s)]
-               (str (apply str (repeat cnt hd)) (apply str tl)))))
-         (fn [ys] (str (apply str xs) ys))))
-     (partition-by char-numeric? cipher-text)))
-   ""))
+  (apply str
+         ((reduce
+           comp
+           identity
+           (map
+            (fn [xs]
+              (if (char-numeric? (first xs))
+                (let [cnt (Integer/parseInt (apply str xs))]
+                  (fn [[hd & tl]]
+                    (concat (repeat cnt hd) tl)))
+                #(concat xs %)))
+            (partition-by char-numeric? cipher-text)))
+          "")))
 
 (def
   e
