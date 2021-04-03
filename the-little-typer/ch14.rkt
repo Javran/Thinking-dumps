c9374f77de542746cc2f084ad322715676ad5444
Javran Cheng <Javran.C@gmail.com>
2021-04-03 02:36:01 -0700

    done ch14 stuff.

--- the-little-typer/ch14.rkt
+++ the-little-typer/ch14.rkt
@@ -72,3 +72,70 @@
 (list-ref Atom 3 sample)
 (list-ref Atom 5 sample)
 (list-ref Atom 6 sample)
+
+(claim Fin
+  (-> Nat U))
+(define Fin
+  (lambda (n)
+    (iter-Nat n
+      Absurd
+      Maybe)))
+(Fin 2)
+(nothing (Fin 2))
+(just (Fin 2) (nothing (Fin 1)))
+
+(claim fzero
+  ;; the zero index of a Vec of length n+1
+  ;; (since a Vec of length 0 contains no value)
+  (Pi ((n Nat))
+    (Fin (add1 n))))
+(define fzero
+  (lambda (n)
+    (nothing (Fin n))))
+(claim fadd1
+  (Pi ((n Nat))
+    (-> (Fin n)
+        (Fin (add1 n)))))
+(define fadd1
+  (lambda (n i-1)
+    (just (Fin n) i-1)))
+;; observation:
+;; like 0 and add1,
+;; - fzero has "right" on top
+;; - fadd1 has "left" on top
+
+;; those are the exact same two values as before:
+(fzero 2)
+(fadd1 2 (fzero 1))
+
+(claim vec-ref
+  (Pi ((E U)
+       (l Nat))
+    (-> (Fin l) (Vec E l) E)))
+(define vec-ref
+  (lambda (E l)
+    (ind-Nat l
+      (lambda (k) (-> (Fin k) (Vec E k) E))
+      (lambda (a _) (ind-Absurd a E))
+      (lambda (_ r index xs)
+        (ind-Either index
+          (lambda (_) E)
+          ;; fadd
+          (lambda (index-1)
+            (r index-1 (tail xs)))
+          ;; fzero
+          (lambda (_) (head xs)))))))
+(claim example
+  (Vec Atom 7))
+(define example
+  (vec:: 'e
+    (vec:: 'x
+      (vec:: 'a
+        (vec:: 'm
+          (vec:: 'p
+            (vec:: 'l
+              (vec:: 'e
+                vecnil))))))))
+(vec-ref Atom 7 (fzero 6) example)
+(vec-ref Atom 7 (fadd1 6 (fzero 5)) example)
+(vec-ref Atom 7 (fadd1 6 (fadd1 5 (fzero 4))) example)
