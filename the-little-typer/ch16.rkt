94470a279099d9df96c72c5fd12c27cb3f2cbc51
Javran Cheng <Javran.C@gmail.com>
2021-04-03 15:36:19 -0700

    ch16 stuff.
    proof done mostly without book.

---
+++ the-little-typer/ch16.rkt
@@ -0,0 +1,115 @@
+#lang pie
+
+(claim Dec (-> U U))
+(define Dec (lambda (X) (Either X (-> X Absurd))))
+
+(claim =consequence (-> Nat Nat U))
+(define =consequence
+  (lambda (x y)
+    (which-Nat x
+      (which-Nat y
+        Trivial
+        (lambda (y-1)
+          Absurd))
+      (lambda (x-1)
+        (which-Nat y
+          Absurd
+          (lambda (y-1)
+            (= Nat x-1 y-1)))))))
+(claim =consequence-same
+  (Pi ((n Nat))
+    (=consequence n n)))
+(define =consequence-same
+  (lambda (n)
+    (ind-Nat n
+      (lambda (k) (=consequence k k))
+      sole
+      (lambda (n-1 _)
+        (same n-1)))))
+(claim use-Nat=
+  (Pi ((x Nat)
+       (y Nat))
+    (-> (= Nat x y)
+        (=consequence x y))))
+(define use-Nat=
+  (lambda (x y x=y)
+    (replace x=y
+      (lambda (ky) (=consequence x ky))
+      (=consequence-same x))))
+(claim zero-not-add1
+  (Pi ((n Nat))
+    (-> (= Nat zero (add1 n)) Absurd)))
+(define zero-not-add1
+  (lambda (n 0=1+n)
+    (use-Nat= 0 (add1 n) 0=1+n)))
+
+(claim zero?
+  (Pi ((n Nat))
+    (Dec (= Nat zero n))))
+(define zero?
+  (lambda (n)
+    (ind-Nat n
+      (lambda (k) (Dec (= Nat zero k)))
+      (left (same 0))
+      (lambda (n-1 _)
+        (right (zero-not-add1 n-1))))))
+
+(claim + (-> Nat Nat Nat))
+(define +
+  (lambda (m n)
+    (iter-Nat m
+      n
+      (lambda (i) (add1 i)))))
+
+(claim nat=?
+  (Pi ((x Nat)
+       (y Nat))
+    (Dec (= Nat x y))))
+(define nat=?
+  (lambda (x y)
+    ((ind-Nat x
+       (lambda (kx)
+         ;; the trick here is that `j` is passed from outside
+         ;; as we want to build proof from `(_ x-1 j-1)` to `(_ x j)`,
+         ;; and `y` does not have the desired shape.
+         (Pi ((j Nat))
+           (Dec (= Nat kx j))))
+       ;; when x = 0, it's decidable through `zero?`
+       zero?
+       (lambda (x-1 nat=?-x-1 j)
+         ;; induction on `j`, we want to work on `j=0`
+         ;; and on the other front work from `(_ x-1 j-1)`
+         ;; to build `(_ x j)`
+         ;; or `(_ (add1 x-1) (add1 j-1))`
+         (ind-Nat j
+           (lambda (k) (Dec (= Nat (add1 x-1) k)))
+           ;; nonzeros are, well, not zero.
+           (right (use-Nat= (add1 x-1) 0))
+           (lambda (j-1 _dec)
+             (ind-Either
+               ;; obtain the proof that (= Nat x-1 j-1) is decidable.
+               (nat=?-x-1 j-1)
+               ;; motive
+               (lambda (_) (Dec (= Nat (add1 x-1) (add1 j-1))))
+               (lambda (x-1=j-1)
+                 ;; (_ x-1 j-1) => (_ x j)
+                 (left (cong x-1=j-1 (+ 1))))
+               (lambda (x-1=j-1-false)
+                 ;; x-1 /= j-1 so it's the right branch.
+                 (right
+                   ;; the goal is to establish absurdity
+                   ;; through a proof that x-1 = j-1
+                   (lambda (x-1=j-1)
+                     (x-1=j-1-false
+                       (use-Nat=
+                         (add1 x-1)
+                         (add1 j-1)
+                         x-1=j-1))))))))))
+      y)))
+
+;; The proof that (= Nat x y) is decidable also serves
+;; the purpose of equality check.
+;; right => not equal
+(nat=? 2 1)
+;; left => equal
+(nat=? 5 5)
